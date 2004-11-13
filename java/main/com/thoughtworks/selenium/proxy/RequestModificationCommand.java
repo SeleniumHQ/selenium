befc993b4a2393981b124c218a4b00b596f4b496
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2004-11-13 05:53:22 +0000

    Simplified test using JMock
    r123

--- java/main/com/thoughtworks/selenium/proxy/RequestModificationCommand.java
+++ java/main/com/thoughtworks/selenium/proxy/RequestModificationCommand.java
@@ -1,23 +1,23 @@
-package com.thoughtworks.selenium.proxy;
 /*
-  Copyright 2004 ThoughtWorks, Inc. 
-  
-  Licensed under the Apache License, Version 2.0 (the "License"); 
-  you may not use this file except in compliance with the License. 
-  You may obtain a copy of the License at 
-  
-      http://www.apache.org/licenses/LICENSE-2.0 
-  
-  Unless required by applicable law or agreed to in writing, software 
-  distributed under the License is distributed on an "AS IS" BASIS, 
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-  See the License for the specific language governing permissions and 
-  limitations under the License. 
+  Copyright 2004 ThoughtWorks, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
+package com.thoughtworks.selenium.proxy;
 
 /**
  * Defines the common interface for commands.
- * @version $Id: RequestModificationCommand.java,v 1.1 2004/11/11 12:19:48 mikemelia Exp $
+ * @version $Id: RequestModificationCommand.java,v 1.2 2004/11/13 05:53:22 ahelleso Exp $
  */
 public interface RequestModificationCommand {
     public void execute(HTTPRequest httpRequest);
