0ee1741b2e15c1dece97d12f4a15caf387b494fa
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2004-11-15 18:35:02 +0000

    Update licence (SEL-54) to java files
    r154

--- java/main/com/thoughtworks/selenium/proxy/ClientConnectionThread.java
+++ java/main/com/thoughtworks/selenium/proxy/ClientConnectionThread.java
@@ -1,46 +1,19 @@
 /*
-Copyright (c) 2003 ThoughtWorks, Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-   1. Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-
-   2. Redistributions in binary form must reproduce the above copyright notice,
-      this list of conditions and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-
-   3. The end-user documentation included with the redistribution, if any, must
-      include the following acknowledgment:
-
-          This product includes software developed by ThoughtWorks, Inc.
-          (http://www.thoughtworks.com/).
-
-      Alternately, this acknowledgment may appear in the software itself, if and
-      wherever such third-party acknowledgments normally appear.
-
-   4. The names "CruiseControl", "CruiseControl.NET", "CCNET", and
-      "ThoughtWorks, Inc." must not be used to endorse or promote products derived
-      from this software without prior written permission. For written permission,
-      please contact opensource@thoughtworks.com.
-
-   5. Products derived from this software may not be called "Selenium" or
-      "ThoughtWorks", nor may "Selenium" or "ThoughtWorks" appear in their name,
-      without prior written permission of ThoughtWorks, Inc.
-
-
-THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THOUGHTWORKS
-INC OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
-OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-DAMAGE.
-*/
+ * Copyright 2004 ThoughtWorks, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.thoughtworks.selenium.proxy;
 
 import com.thoughtworks.selenium.utils.Assert;
@@ -52,7 +25,7 @@ import java.net.Socket;
 
 
 /**
- * @version $Id: ClientConnectionThread.java,v 1.6 2004/11/15 10:44:38 mikemelia Exp $
+ * @version $Id: ClientConnectionThread.java,v 1.7 2004/11/15 18:35:00 ahelleso Exp $
  */
 public class ClientConnectionThread extends Thread implements ConnectionThread {
     private final Socket socket;
