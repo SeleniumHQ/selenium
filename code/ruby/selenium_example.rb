653018b03d967adc0927dbd660a916f94477bda5
Paul Hammant <PaulHammant@gmail.com>
2004-12-28 17:59:04 +0000

    sensible rename
    r396

--- code/ruby/selenium_example.rb
+++ code/ruby/selenium_example.rb
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-require 'selenese'
+require 'selenium'
 
 puts "Go to http://localhost:7896/SeleneseRunner.html"
 
