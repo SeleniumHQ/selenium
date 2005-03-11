3548e7868cfdcc199de8c18052c2334243687217
Jason Huggins <jrhuggin@example.com>
2005-03-11 03:57:21 +0000

    Fixed script so the printed output is more readable in the console.
    r775

--- code/python/twisted/src/examples/google-test-xmlrpc.pl
+++ code/python/twisted/src/examples/google-test-xmlrpc.pl
@@ -15,10 +15,18 @@ $server->call('setTimeout',15);
 system('start run_firefox.bat');
 
 print $server->call('open','http://localhost:8080/AUT/000000A/http/www.google.com/');
+print "\n";
 print $server->call('verifyTitle','Google');
+print "\n";
 print $server->call('type','q','Selenium ThoughtWorks');
+print "\n";
 print $server->call('verifyValue','q','Selenium ThoughtWorks');
+print "\n";
 print $server->call('clickAndWait','btnG');
+print "\n";
 print $server->call('verifyTextPresent','selenium.thoughtworks.com','');
+print "\n";
 print $server->call('verifyTitle','Google Search: Selenium ThoughtWorks');
+print "\n";
 print $server->call('testComplete');
+print "\n";
