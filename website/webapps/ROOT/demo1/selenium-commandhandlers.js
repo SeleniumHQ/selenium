ccfcd4967c08f8781ff93c504a1cfc7026a3d31f
Paul Hammant <PaulHammant@gmail.com>
2005-05-08 00:40:20 +0000

    tested with logos
    r961

--- website/webapps/ROOT/demo1/selenium-commandhandlers.js
+++ website/webapps/ROOT/demo1/selenium-commandhandlers.js
@@ -1,19 +1,19 @@
 /*
- * Copyright 2004 ThoughtWorks, Inc
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
+* Copyright 2004 ThoughtWorks, Inc
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*/
 function CommandHandlerFactory() {
     this.actions = {};
     this.asserts = {};
@@ -24,27 +24,27 @@ function CommandHandlerFactory() {
     this.registerAction = function(name, action, wait) {
         var handler = new ActionHandler(action, wait);
         this.actions[name] = handler;
-    }
+    };
 
     this.registerAccessor = function(name, accessor) {
         var handler = new AccessorHandler(accessor);
         this.accessors[name] = handler;
-    }
+    };
 
     this.registerAssert = function(name, assertion, haltOnFailure) {
         var handler = new AssertHandler(assertion, haltOnFailure);
         this.asserts[name] = handler;
-    }
+    };
 
     this.getCommandHandler = function(name) {
         return this.actions[name] || this.accessors[name] || this.asserts[name] || null;
-    }
+    };
 
     this.registerAll = function(commandObject) {
         registerAllActions(commandObject);
         registerAllAsserts(commandObject);
         registerAllAccessors(commandObject);
-    }
+    };
 
     var registerAllActions = function(commandObject) {
         for (var functionName in commandObject) {
@@ -61,7 +61,7 @@ function CommandHandlerFactory() {
                 self.registerAction(waitActionName, action, true);
             }
         }
-    }
+    };
 
     var registerAllAsserts = function(commandObject) {
         for (var functionName in commandObject) {
@@ -78,7 +78,7 @@ function CommandHandlerFactory() {
                 self.registerAssert(verifyName, assert, false);
             }
         }
-    }
+    };
 
     var registerAllAccessors = function(commandObject) {
         for (var functionName in commandObject) {
@@ -87,7 +87,7 @@ function CommandHandlerFactory() {
                 self.registerAccessor(functionName, accessor);
             }
         }
-    }
+    };
 
     function toCamelCase(aString) {
         return aString.charAt(0).toLowerCase() + aString.substr(1);
@@ -102,14 +102,12 @@ function CommandHandler(type, haltOnFailure, executor) {
     this.haltOnFailure = haltOnFailure;
     this.executor = executor;
 }
-
 CommandHandler.prototype.execute = function(seleniumApi, command) {
     return new CommandResult(this.executor.call(seleniumApi, command.target, command.value));
-}
+};
 
 function ActionHandler(action, wait) {
-    this.base = CommandHandler;
-    this.base("action", true, action);
+    CommandHandler.call(this, "action", true, action);
     if (wait) {
         this.wait = true;
     }
@@ -117,36 +115,33 @@ function ActionHandler(action, wait) {
 ActionHandler.prototype = new CommandHandler;
 ActionHandler.prototype.execute = function(seleniumApi, command) {
     if ( seleniumApi.browserbot.hasAlerts() ) {
-       throw new Error("There was an unexpected Alert! [" + seleniumApi.browserbot.getNextAlert() + "]");
+        throw new Error("There was an unexpected Alert! [" + seleniumApi.browserbot.getNextAlert() + "]");
     }
     if ( seleniumApi.browserbot.hasConfirmations() ) {
         throw new Error("There was an unexpected Confirmation! [" + seleniumApi.browserbot.getNextConfirmation() + "]");
     }
-    var processState = this.executor.call(seleniumApi, command.target, command.value)
+    var processState = this.executor.call(seleniumApi, command.target, command.value);
     // If the handler didn't return a wait flag, check to see if the
     // handler was registered with the wait flag.
     if (processState == undefined && this.wait) {
-       processState = SELENIUM_PROCESS_WAIT;
+        processState = SELENIUM_PROCESS_WAIT;
     }
     return new CommandResult(processState);
-}
+};
 
 function AccessorHandler(accessor) {
-    this.base = CommandHandler;
-    this.base("accessor", true, accessor);
+    CommandHandler.call(this, "accessor", true, accessor);
 }
 AccessorHandler.prototype = new CommandHandler;
-
 AccessorHandler.prototype.execute = function(seleniumApi, command) {
-	var returnValue = this.executor.call(seleniumApi, command.target, command.value);
+    var returnValue = this.executor.call(seleniumApi, command.target, command.value);
     var result = new CommandResult();
     result.result = returnValue;
     return result;
-}
+};
 
 function AssertHandler(assertion, haltOnFailure) {
-    this.base = CommandHandler;
-    this.base("assert", haltOnFailure || false, assertion);
+    CommandHandler.call(this, "assert", haltOnFailure || false, assertion);
 }
 AssertHandler.prototype = new CommandHandler;
 AssertHandler.prototype.execute = function(seleniumApi, command) {
@@ -155,18 +150,20 @@ AssertHandler.prototype.execute = function(seleniumApi, command) {
         var processState = this.executor.call(seleniumApi, command.target, command.value);
         result.passed = true;
     } catch (e) {
-        // If this is not a JsUnitException, or we should haltOnFailure, rethrow.
-        if (!e.isJsUnitException) {
+        // If this is not a AssertionFailedError, or we should haltOnFailure, rethrow.
+        if (!e.isAssertionFailedError) {
             throw e;
         }
         if (this.haltOnFailure) {
-            throw new Error(e.jsUnitMessage);
+            var error = new Error(e.failureMessage);
+            error.message = e.failureMessage;
+            throw error;
         }
         result.failed = true;
-        result.failureMessage = e.jsUnitMessage;
+        result.failureMessage = e.failureMessage;
     }
     return result;
-}
+};
 
 function CommandResult(processState) {
     this.processState = processState;
--- website/webapps/ROOT/demo2/selenium-api.js
+++ website/webapps/ROOT/demo2/selenium-api.js
@@ -14,18 +14,28 @@
  *  limitations under the License.
  *
  */
-
+var assert = new Assert();
 function Selenium(browserbot) {
     this.browserbot = browserbot;
-    this.page = function() {return browserbot.getCurrentPage()};
+    this.page = function() {
+        return browserbot.getCurrentPage();
+    };
 
     var self = this;
     this.callOnNextPageLoad = function(callback) {
         if (callback) {
             self.browserbot.callOnNextPageLoad(callback);
         }
-    }
+    };
 }
+
+/*
+ * Click on the located element, and attach a callback to notify
+ * when the page is reloaded.
+ */
+Selenium.prototype.doModalDialogTest = function(returnValue) {
+    this.browserbot.doModalDialogTest(returnValue);
+};
 
 /*
  * Click on the located element, and attach a callback to notify
@@ -34,7 +44,7 @@ function Selenium(browserbot) {
 Selenium.prototype.doClick = function(locator) {
     var element = this.page().findElement(locator);
     this.page().clickElement(element);
-}
+};
 
 /**
  * Overwrite the text in the located text element.
@@ -43,7 +53,7 @@ Selenium.prototype.doClick = function(locator) {
 Selenium.prototype.doType = function(locator, newText) {
     var element = this.page().findElement(locator);
     this.page().replaceText(element, newText);
-}
+};
 
 /**
  * Select the option by label from the located select element.
@@ -52,7 +62,7 @@ Selenium.prototype.doType = function(locator, newText) {
 Selenium.prototype.doSelect = function(locator, optionText) {
     var element = this.page().findElement(locator);
     this.page().selectOptionWithLabel(element, optionText);
-}
+};
 
 /*
  * Open the browser to a new location.
@@ -60,21 +70,21 @@ Selenium.prototype.doSelect = function(locator, optionText) {
 Selenium.prototype.doOpen = function(newLocation) {
     this.browserbot.openLocation(newLocation);
     return SELENIUM_PROCESS_WAIT;
-}
+};
 
 /*
  * Select the named window to be the active window.
  */
 Selenium.prototype.doSelectWindow = function(windowName) {
     this.browserbot.selectWindow(windowName);
-}
+};
 
 /*
  * Instruct Selenium to click Cancel on the next confirm dialog it encounters
  */
 Selenium.prototype.doChooseCancelOnNextConfirmation = function() {
     this.browserbot.cancelNextConfirmation();
-}
+};
 
 /*
  *  Asserts that the supplied message was received as an alert
@@ -84,13 +94,13 @@ Selenium.prototype.doChooseCancelOnNextConfirmation = function() {
         
         receivedAlert = this.browserbot.getNextAlert();
         if ( receivedAlert != expectedAlert ) {
-           fail("The alert was [" + receivedAlert + "]");   
+           assert.fail("The alert was [" + receivedAlert + "]");
         }
                           
     } else {
-        fail("There were no alerts"); 
+        assert.fail("There were no alerts");
     }
- } 
+ };
 
   /*
   *  Asserts that the supplied message was received as a confirmation
@@ -100,27 +110,40 @@ Selenium.prototype.doChooseCancelOnNextConfirmation = function() {
          
          receivedConfirmation = this.browserbot.getNextConfirmation();
          if ( receivedConfirmation != expectedConfirmation ) {
-            fail("The confirmation message was [" + receivedConfirmation + "]");   
+            assert.fail("The confirmation message was [" + receivedConfirmation + "]");
          }
                            
      } else {
-         fail("There were no confirmations"); 
+         assert.fail("There were no confirmations");
      }
-  } 
+  };
  
 /*
  * Verify the location of the current page.
  */
+Selenium.prototype.assertAbsoluteLocation = function(expectedLocation) {
+    this.assertMatches(expectedLocation, this.page().location);
+};
+
+
+/*
+ * Verify the location of the current page ends with the expected location
+ */
 Selenium.prototype.assertLocation = function(expectedLocation) {
-    assertEquals(expectedLocation, this.page().location);
-}
+    var docLocation = this.page().location.toString();
+    if (docLocation.length != docLocation.indexOf(expectedLocation) + expectedLocation.length)
+    {
+        assert.fail("Expected location to end with '" + expectedLocation
+             + "' but was '" + docLocation + "'");
+    }
+};
 
 /*
  * Verify the title of the current page.
  */
 Selenium.prototype.assertTitle = function(expectedTitle) {
-    assertEquals(expectedTitle, this.page().title());
-}
+    this.assertMatches(expectedTitle, this.page().title());
+};
 
 /*
  * Verify the value of a form element.
@@ -128,17 +151,17 @@ Selenium.prototype.assertTitle = function(expectedTitle) {
 Selenium.prototype.assertValue = function(locator, expectedValue) {
     var element = this.page().findElement(locator);
     var actualValue = getInputValue(element);
-    assertEquals(expectedValue, actualValue.trim());
-}
+    this.assertMatches(expectedValue, actualValue.trim());
+};
 
 /*
- * Verifies that the entire text of the page matches the expected content.
+ * Verifies that the text of the located element matches the expected content.
  */
 Selenium.prototype.assertText = function(locator, expectedContent) {
     var element = this.page().findElement(locator);
     var actualText = getText(element);
-    assertEquals(expectedContent, actualText.trim());
-}
+    this.assertMatches(expectedContent, actualText.trim());
+};
 
 /*
  * Asserts that the text for a single cell within and HTML table matches the expected content.
@@ -147,10 +170,10 @@ Selenium.prototype.assertText = function(locator, expectedContent) {
 Selenium.prototype.assertTable = function(tableLocator, expectedContent) {
     // This regular expression matches "tableName.row.column"
     // For example, "mytable.3.4"
-    pattern = /(.*)\.(\d)+\.(\d+)/
+    pattern = /(.*)\.(\d+)\.(\d+)/;
 
     if(!pattern.test(tableLocator)) {
-        error("Invalid target format. Correct format is tableName.rowNum.columnNum");
+        assert.fail("Invalid target format. Correct format is tableName.rowNum.columnNum");
     }
 
     pieces = tableLocator.match(pattern);
@@ -160,13 +183,17 @@ Selenium.prototype.assertTable = function(tableLocator, expectedContent) {
     col = pieces[3];
 
     var table = this.page().findElement(tableName);
-    if (row > table.rows.length || col > table.rows[row].cells.length)
-        fail("No such row or column in table");
+    if (row > table.rows.length) {
+        assert.fail("Cannot access row " + row + " - table has " + table.rows.length + " rows");
+    }
+    else if (col > table.rows[row].cells.length) {
+        assert.fail("Cannot access column " + col + " - table row has " + table.rows[row].cells.length + " columns");
+    }
     else {
         actualContent = getText(table.rows[row].cells[col]);
-        assertEquals(expectedContent, actualContent.trim());
+        this.assertMatches(expectedContent, actualContent.trim());
     }
-}
+};
 
 /**
  * Verify the label of the option that is selected.
@@ -174,8 +201,8 @@ Selenium.prototype.assertTable = function(tableLocator, expectedContent) {
 Selenium.prototype.assertSelected = function(target, expectedLabel) {
     var element = this.page().findElement(target);
     var selectedLabel = element.options[element.selectedIndex].text;
-    assertEquals(expectedLabel, selectedLabel);
-}
+    this.assertMatches(expectedLabel, selectedLabel);
+};
 
 /**
  * Verify the label of all of the options in the drop=down.
@@ -183,18 +210,18 @@ Selenium.prototype.assertSelected = function(target, expectedLabel) {
 Selenium.prototype.assertSelectOptions = function(target, options) {
     // Handle escpaced commas, by substitutine newlines.
     options = options.replace("\\,", "\n");
-	var expectedOptions = options.split(",");
-	var element = this.page().findElement(target);
+    var expectedOptions = options.split(",");
+    var element = this.page().findElement(target);
 
-    assertEquals("wrong number of options", expectedOptions.length, element.options.length);
+    assert.equals("Wrong number of options.", expectedOptions.length, element.options.length);
 
     for (var i = 0; i < element.options.length; i++) {
         var option = element.options[i];
         // Put the escaped commas back in.
         var expectedOption = expectedOptions[i].replace("\n", ",");
-        assertEquals(expectedOption, option.text);
+        this.assertMatches(expectedOption, option.text);
     }
-}
+};
 
 /**
  * Verify the value of an element attribute. The syntax for returning an element attribute
@@ -202,8 +229,8 @@ Selenium.prototype.assertSelectOptions = function(target, options) {
  */
 Selenium.prototype.assertAttribute = function(target, expected) {
     var attributeValue = this.page().findAttribute(target);
-    assertEquals(expected, attributeValue);
-}
+    this.assertMatches(expected, attributeValue);
+};
 
 /*
  * Asserts that the specified text is present in the page content.
@@ -212,11 +239,24 @@ Selenium.prototype.assertTextPresent = function(expectedText) {
     var allText = this.page().bodyText();
 
     if(allText == "") {
-        error("Page text not found")
+        assert.fail("Page text not found");
     } else if(allText.indexOf(expectedText) == -1) {
-        fail("'" + expectedText + "' not found in page text.");
+        assert.fail("'" + expectedText + "' not found in page text.");
     }
-}
+};
+
+/*
+ * Asserts that the specified text is NOT present in the page content.
+ */
+Selenium.prototype.assertTextNotPresent = function(unexpectedText) {
+    var allText = this.page().bodyText();
+
+    if(allText == "") {
+        assert.fail("Page text not found");
+    } else if(allText.indexOf(unexpectedText) != -1) {
+        assert.fail("'" + unexpectedText + "' was found in page text.");
+    }
+};
 
 /*
  * Asserts that the specified element can be found.
@@ -225,9 +265,9 @@ Selenium.prototype.assertElementPresent = function(locator) {
     try {
         this.page().findElement(locator);
     } catch (e) {
-        fail("Element " + locator + " not found.");
+        assert.fail("Element " + locator + " not found.");
     }
-}
+};
 
 /*
  * Asserts that the specified element cannot be found.
@@ -239,27 +279,204 @@ Selenium.prototype.assertElementNotPresent = function(locator) {
     catch (e) {
         return;
     }
-    fail("Element " + locator + " found.");
-}
+    assert.fail("Element " + locator + " found.");
+};
 
+/*
+ * Asserts that the specified element is visible
+ */
+Selenium.prototype.assertVisible = function(locator) {
+    var element;
+    try {
+        element = this.page().findElement(locator);
+    } catch (e) {
+        assert.fail("Element " + locator + " not present.");
+    }
+    if (! this.isVisible(element)) {
+        assert.fail("Element " + locator + " not visible.");
+    }
+};
+
+/*
+ * Asserts that the specified element is visible
+ */
+Selenium.prototype.assertNotVisible = function(locator) {
+    var element;
+    try {
+        element = this.page().findElement(locator);
+    } catch (e) {
+        return;
+    }
+    if (this.isVisible(element)) {
+        assert.fail("Element " + locator + " is visible.");
+    }
+};
+
+Selenium.prototype.isVisible = function(element) {
+    var visibility = this.getEffectiveStyleProperty(element, "visibility");
+    var isDisplayed = this.isDisplayed(element);
+    return (visibility != "hidden" && isDisplayed);
+};
+
+Selenium.prototype.getEffectiveStyleProperty = function(element, property) {
+    var effectiveStyle = this.getEffectiveStyle(element);
+    var propertyValue = effectiveStyle[property];
+    if (propertyValue == 'inherit' && element.parentNode.style) {
+        return this.getEffectiveStyleProperty(element.parentNode, property);
+    }
+    return propertyValue;
+};
+
+Selenium.prototype.isDisplayed = function(element) {
+    var display = this.getEffectiveStyleProperty(element, "display");
+    if (display == "none") return false;
+    if (element.parentNode.style) {
+        return this.isDisplayed(element.parentNode);
+    }
+    return true;
+};
+
+Selenium.prototype.getEffectiveStyle = function(element) {
+    if (element.style == undefined) {
+        return undefined; // not a styled element
+    }
+    var window = this.browserbot.getContentWindow();
+    if (window.getComputedStyle) { 
+        // DOM-Level-2-CSS
+        return window.getComputedStyle(element, null);
+    }
+    if (element.currentStyle) {
+        // non-standard IE alternative
+        return element.currentStyle;
+        // TODO: this won't really work in a general sense, as
+        //   currentStyle is not identical to getComputedStyle()
+        //   ... but it's good enough for "visibility"
+    }
+    throw new Error("cannot determine effective stylesheet in this browser");
+};
+
+/**
+ * Asserts that the specified element accepts user input visible
+ */
+Selenium.prototype.assertEditable = function(locator) {
+    var element = this.page().findElement(locator);
+    if (element.value == undefined) {
+        assert.fail("Element " + locator + " is not an input.");
+    }
+    if (element.disabled) {
+        assert.fail("Element " + locator + " is disabled.");
+    }
+};
+
+/**
+ * Asserts that the specified element does not accept user input
+ */
+Selenium.prototype.assertNotEditable = function(locator) {
+    var element = this.page().findElement(locator);
+    if (element.value == undefined) {
+        return; // not an input
+    }
+    if (element.disabled == false) {
+        assert.fail("Element " + locator + " is editable.");
+    }
+};
 
  /*
   * Return all buttons on the screen.
   */
 Selenium.prototype.getAllButtons = function() {
- 		return this.page().getAllButtons();
-}
+        return this.page().getAllButtons();
+};
 
  /*
   * Return all links on the screen.
   */
 Selenium.prototype.getAllLinks = function() {
- 		return this.page().getAllLinks();
-}
+        return this.page().getAllLinks();
+};
 
  /*
   * Return all fields on the screen.
   */
 Selenium.prototype.getAllFields = function() {
- 		return this.page().getAllFields();
+        return this.page().getAllFields();
+};
+
+/*
+  * Set the context for the current Test
+  */
+Selenium.prototype.doContext = function(context) {
+        return this.page().setContext(context);
+};
+
+function Assert() {
+    this.equals = function()
+    {
+        if (arguments.length == 2)
+        {
+            var comment = "";
+            var expected = arguments[0];
+            var actual = arguments[1];
+        }
+        else {
+            var comment = arguments[0] + " ";
+            var expected = arguments[1];
+            var actual = arguments[2];
+        }
+
+        if (expected === actual) {
+            return;
+        }
+        var errorMessage = comment + "Expected '" + expected + "' but was '" + actual + "'";
+
+        throw new AssertionFailedError(errorMessage);
+    };
+
+    this.fail = function(message)
+    {
+        throw new AssertionFailedError(message);
+    };
+}
+
+function AssertionFailedError(message) {
+    this.isAssertionFailedError = true;
+    this.failureMessage = message;
+}
+
+/*
+ * assertMatches(comment?, pattern, actual)
+ */
+Selenium.prototype.assertMatches = function() {
+    if (arguments.length == 2)
+    {
+        var comment = "";
+        var pattern = arguments[0];
+        var actual = arguments[1];
+    }
+    else {
+        var comment = arguments[0] + "; ";
+        var pattern = arguments[1];
+        var actual = arguments[2];
+    }
+
+    if (this.matches(pattern, actual)) {
+        return;
+    }
+
+    var errorMessage = comment + 
+        "Actual value '" + actual + "' did not match '" + pattern + "'";
+    assert.fail(errorMessage);
+};
+
+Selenium.prototype.globToRegexp = function(glob) {
+    var pattern = glob;
+    pattern = pattern.replace(/([.^$+(){}[\]\\|])/g, "\\$1");
+    pattern = pattern.replace(/\?/g, ".");
+    pattern = pattern.replace(/\*/g, ".*");
+    return "^" + pattern + "$";
+}
+
+Selenium.prototype.matches = function(pattern, actual) {
+    var regexp = new RegExp(this.globToRegexp(pattern));
+    return regexp.test(actual);
 }
