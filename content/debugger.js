15fb09078f033aeb174ca4a72258224a56345470
Shinya Kasatani <shinya@example.com>
2005-12-07 16:23:18 +0000

    running test inside Selenium IDE window
    r1310

--- content/debugger.js
+++ content/debugger.js
@@ -14,73 +14,45 @@
  * limitations under the License.
  */
 
-testLoop = null;
-
 function Debugger() {
-	this.runInterval = 1000;
-
-	var self = this;
-
-	this.initialiseTestLoop = function() {
-		var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
-		var window = wm.getMostRecentWindow('navigator:browser');
-		var contentWindow = window.getBrowser().contentWindow;
+	this.init = function() {
+		if (this.runner != null) return;
 		
-		self.selenium = self.Selenium.createForFrame(window.getBrowser());
-		self.selenium.browserbot.getCurrentPage();
-		self.commandFactory = new self.CommandHandlerFactory();
-		self.commandFactory.registerAll(self.selenium);
-		self.testCase.debugIndex = -1;
+		this.runner = new Object();
+		this.state = 'paused';
 		
-		testLoop = new self.TestLoop(self.commandFactory);
-		
-		testLoop.getCommandInterval = function() { return self.runInterval; };
-		testLoop.nextCommand = function() {
-			if (++self.testCase.debugIndex >= self.testCase.commands.length) {
-				return null;
-			}
-			var command = self.testCase.commands[self.testCase.debugIndex];
-			return new self.SeleniumCommand(command.command, command.target, command.value);
+		this.runner.testCase = recorder.testCase;
+		const subScriptLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+	    .getService(Components.interfaces.mozIJSSubScriptLoader);
+		//subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-logging.js', this.runner);
+		subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-api.js', this.runner);
+		if (recorder.options.userExtensionsURL) {
+			subScriptLoader.loadSubScript(recorder.options.userExtensionsURL, this.runner);
 		}
-		testLoop.firstCommand = function() {
-			self.testCase.debugIndex = -1;
-			//testLoop.nextCommand.apply(this);
-			if (++self.testCase.debugIndex >= self.testCase.commands.length) {
-				return null;
-			}
-			var command = self.testCase.commands[self.testCase.debugIndex];
-			return new self.SeleniumCommand(command.command, command.target, command.value);
-		}
-		testLoop.commandStarted = function() {
-			recorder.view.rowUpdated(self.testCase.debugIndex);
-		}
-		testLoop.commandComplete = testLoop.commandStarted;
-		testLoop.commandError = testLoop.commandStarted;
-		testLoop.testComplete = function() {}
-		testLoop.pause = function() {}
-		//self.testLoop = testLoop;
-	}
+		subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-commandhandlers.js', this.runner);
+		subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-executionloop.js', this.runner);
+		subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium-runner.js', this.runner);
 
-	this.getBaseURL = function() {
-		return recorder.document.getElementById("baseURL").value;
+		this.runner.getInterval = function() {
+			return document.getElementById("runInterval").selectedItem.value;
+		}
 	}
 }
 
 Debugger.prototype.start = function() {
-	this.testCase = recorder.testCase;
-	const subScriptLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-	    .getService(Components.interfaces.mozIJSSubScriptLoader);
-	subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-api.js', this);
-	if (recorder.options.userExtensionsURL) {
-		subScriptLoader.loadSubScript(recorder.options.userExtensionsURL, this);
-	}
-	subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium-debug-patch.js', this);
-	subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-commandhandlers.js', this);
-	subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-executionloop.js', this);
-	//subScriptLoader.loadSubScript('chrome://selenium-ide/content/selenium/selenium-logging.js', this);
-	this.initialiseTestLoop();
-	//this.testLoop.start();
-	testLoop.start();
-}
+	document.getElementById("enableRecording").checked = false;
+	toggleRecordingEnabled(false);
 
-var seleniumDebugger = new Debugger();
+	this.init();
+	this.runner.start();
+};
+
+Debugger.prototype.doContinue = function() {
+	document.getElementById("enableRecording").checked = false;
+	toggleRecordingEnabled(false);
+
+	this.init();
+	this.runner.continueCurrentTest();
+};
+
+seleniumDebugger = new Debugger();
---
+++ content/selenium-runner.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2005 Shinya Kasatani
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// Code for running Selenium inside Selenium IDE window.
+// This file should be read every time the debugger starts running the test
+// using SubScript Loader.
+//
+
+var baseURL = recorder.document.getElementById("baseURL").value;
+
+Selenium.prototype.real_doOpen = Selenium.prototype.doOpen;
+
+Selenium.prototype.doOpen = function(newLocation) {
+	if (baseURL && newLocation) {
+		if (!newLocation.match(/^\w+:\/\//)) {
+			if (baseURL[baseURL.length - 1] == '/' && newLocation[0] == '/') {
+				newLocation = baseURL + newLocation.substr(1);
+			} else {
+				newLocation = baseURL + newLocation;
+			}
+		}
+	}
+	return this.real_doOpen(newLocation);
+};
+
+BrowserBot.prototype.setIFrameLocation = function(iframe, location) {
+	if (iframe.src) {
+		iframe.src = location;
+	} else {
+		iframe.contentWindow.location.href = location;
+	}
+};
+
+testLoop = null;
+
+function start() {
+	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+	var window = wm.getMostRecentWindow('navigator:browser');
+	
+	selenium = Selenium.createForFrame(window.getBrowser());
+	selenium.browserbot.getCurrentPage();
+	commandFactory = new CommandHandlerFactory();
+	commandFactory.registerAll(selenium);
+	testCase.debugIndex = -1;
+	
+	testLoop = new TestLoop(commandFactory);
+		
+	testLoop.getCommandInterval = function() { return getInterval(); }
+	testLoop.nextCommand = function() {
+		if (++testCase.debugIndex >= testCase.commands.length) {
+			return null;
+		}
+		var command = testCase.commands[testCase.debugIndex];
+		return new SeleniumCommand(command.command, command.target, command.value);
+	}
+	testLoop.firstCommand = function() {
+		testCase.debugIndex = -1;
+		//testLoop.nextCommand.apply(this);
+		if (++testCase.debugIndex >= testCase.commands.length) {
+			return null;
+		}
+		var command = testCase.commands[testCase.debugIndex];
+		return new SeleniumCommand(command.command, command.target, command.value);
+	}
+	testLoop.commandStarted = function() {
+		recorder.view.rowUpdated(testCase.debugIndex);
+	}
+	testLoop.commandComplete = testLoop.commandStarted;
+	testLoop.commandError = testLoop.commandStarted;
+	testLoop.testComplete = function() {
+		testLoop = null;
+	}
+	testLoop.pause = function() {}
+
+	testLoop.start();
+}
+
+function continueCurrentTest() {
+	if (testLoop != null) {
+		testLoop.finishCommandExecution();
+	} else {
+		LOG.error("testLoop is null");
+	}
+}
