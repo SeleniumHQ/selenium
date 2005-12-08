613f61fae219e4e34d2117a0713233728b26becd
Paul Hammant <PaulHammant@gmail.com>
2005-12-08 05:31:54 +0000

    more user friendly version of the selenium driver from Dave Hoover
    r1312

--- code/ruby/selenium_example.rb
+++ code/ruby/selenium_example.rb
@@ -1,42 +1,17 @@
-# Copyright 2004 ThoughtWorks, Inc
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
+
+require 'seletest'
 require 'selenium'
 
-selenium = Selenium::WebrickCommandProcessor.new.proxy
+class ExampleTest < Test::Unit::TestCase
 
-#browser = Selenium::WindowsIEBrowserLauncher.new
-browser = Selenium::WindowsDefaultBrowserLauncher.new
-#browser = Selenium::UnixSpecifiedPathBrowserLauncher.new('open -a /Applications/Firefox.app')
-browser.launch("http://localhost:7896/selenium-driver/SeleneseRunner.html")
+    def test_something
 
-# Send some commands to the browser
-puts selenium.open('/test_click_page1.html')
-puts selenium.verify_text('link', 'Click here for next page')
-puts selenium.click_and_wait('link')
-puts selenium.verify_location('/test_click_page2.html')
-puts selenium.click_and_wait('previousPage')
+        open '/test_click_page1.html'
+        verify_text 'link', 'Click here for next page'
+        click_and_wait 'link'
+        verify_location '/test_click_page2.html'
+        click_and_wait 'previousPage'
+        verify_element_present 'link'
 
-begin
-  puts selenium.verify_text("link", "This is WRONG") # should fail
-rescue SeleniumCommandError
-  puts 'expected error occurred'
-else
-  puts 'FAIL -- expected error not thrown'
+    end
 end
-
-puts selenium.verify_element_present("link")
-puts selenium.test_complete()
-
-browser.close
