f99dea49c60631c275423434206a3f7d24450398
Dan Fabulich <dan@fabulich.com>
2006-03-15 01:52:09 +0000

    Retrying 3: Copying thin dotnet driver back to trunk, on top of thick driver.  Hopefully, SVN will be clever enough to merge changes properly...
    r1869

---
+++ code/dotnet/src/BridgeWebApp/Readme.txt
@@ -0,0 +1,3 @@
+This is the Selenium Driver Bridge Web Application
+
+It should be mounted in IIS as a virtual directory called 'selenium-driver'
---
+++ code/dotnet/src/Core/HttpCommandProcessor.cs
@@ -0,0 +1,103 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Selenium;
+
+namespace Selenium
+{
+	/// <summary>
+	/// Summary description for DefaultCommandProcessor.
+	/// </summary>
+	public class HttpCommandProcessor : ICommandProcessor
+	{
+		private readonly string url;
+		private string sessionId;
+		private string browserStartCommand;
+		private string browserURL;
+
+		public string Url
+		{
+			get { return url; }
+		}
+
+		public HttpCommandProcessor(string serverHost, int serverPort, string browserStartCommand, string browserURL) 
+		{
+			this.url = "http://" + serverHost + 
+				":"+ serverPort + "/selenium-server/driver/";
+			this.browserStartCommand = browserStartCommand;
+			this.browserURL = browserURL;
+		}
+
+		public HttpCommandProcessor(string serverURL, string browserStartCommand, string browserURL) 
+		{
+			this.url = serverURL;
+			this.browserStartCommand = browserStartCommand;
+			this.browserURL = browserURL;
+		}
+
+		public string DoCommand(string command, string argument1, string argument2)
+		{
+			ISeleneseCommand seleneseCommand = new DefaultSeleneseCommand(command, argument1, argument2);
+			using (HttpWebResponse response = (HttpWebResponse) CreateWebRequest(seleneseCommand).GetResponse())
+			{
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+					throw new SeleniumException(response.StatusDescription);
+				}
+				return ReadResponse(response);
+
+			}
+		}
+
+		public virtual string ReadResponse(HttpWebResponse response)
+		{
+			using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		public virtual WebRequest CreateWebRequest(ISeleneseCommand command)
+		{
+			WebRequest request = WebRequest.Create(BuildCommandString(command.CommandString));
+			request.Timeout = Timeout.Infinite;
+			return request;
+		}
+
+		private string BuildCommandString(string commandString)
+		{
+			string result = url + "?commandRequest=" + commandString;
+			if (sessionId != null)
+			{
+				result += "&sessionId=" + sessionId;
+			}
+			return result;
+		}
+
+		public void Start() 
+		{
+			string result = DoCommand("getNewBrowserSession", browserStartCommand, browserURL);
+			long id;
+			try 
+			{
+				// If the result isn't a long, it's probably an error message
+				id = System.Convert.ToInt64(result);
+			} 
+			catch (Exception) 
+			{
+				throw new SeleniumException(result);
+			}
+			sessionId = System.Convert.ToString(id);
+        
+		}
+
+		public void Stop() 
+		{
+			DoCommand("testComplete", "", "");
+			sessionId = null;
+		}
+
+
+	}
+}
