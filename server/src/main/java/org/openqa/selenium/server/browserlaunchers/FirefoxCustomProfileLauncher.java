4c416ce4c5cc95f803b36664ee16be1356d801da
Dan Fabulich <dan@fabulich.com>
2006-03-15 23:59:43 +0000

    Adding MOZ_NO_REMOTE environment variable; setting just one environment variable is a pain in the butt, so we'll be using Ant code to execute the process.
    r1887

--- server/src/main/java/org/openqa/selenium/server/browserlaunchers/FirefoxCustomProfileLauncher.java
+++ server/src/main/java/org/openqa/selenium/server/browserlaunchers/FirefoxCustomProfileLauncher.java
@@ -8,6 +8,7 @@ import java.io.*;
 
 import org.apache.tools.ant.*;
 import org.apache.tools.ant.taskdefs.*;
+import org.apache.tools.ant.taskdefs.condition.*;
 import org.openqa.selenium.server.*;
 
 public class FirefoxCustomProfileLauncher extends DestroyableRuntimeExecutingBrowserLauncher {
@@ -46,10 +47,15 @@ public class FirefoxCustomProfileLauncher extends DestroyableRuntimeExecutingBro
     public void launch(String url) {
         try {
             String profilePath = makeCustomProfile();
+            
             cmdarray = new String[] {commandPath, "-profile", profilePath, url};
-            //process = Runtime.getRuntime().exec(commandPath);
-            process = Runtime.getRuntime().exec(cmdarray);
-            //process = Runtime.getRuntime().exec("\"" + commandPath + "\" -profile \"" + profilePath + "\" " + url);
+            
+            AsyncExecute exe = new AsyncExecute();
+            exe.setCommandline(cmdarray);
+            exe.setEnvironment(new String[] {"MOZ_NO_REMOTE=1"});
+            // Set MOZ_NO_REMOTE in order to ensure we always get a new Firefox process
+            // http://blog.dojotoolkit.org/2005/12/01/running-multiple-versions-of-firefox-side-by-side
+            process = exe.asyncSpawn();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -150,5 +156,47 @@ public class FirefoxCustomProfileLauncher extends DestroyableRuntimeExecutingBro
         l.close();
         System.out.println("He's dead now, right?");
     }
+    
+    class AsyncExecute extends Execute {
+        File workingDirectory ;
+        Project project;
+        boolean useVMLauncher = true; 
+        
+        public AsyncExecute() {
+            project = new Project();
+        }
+        
+        // Copied from spawn, but actually returns the Process, instead of void
+        public Process asyncSpawn() throws IOException {
+            if (workingDirectory != null && !workingDirectory.exists()) {
+                throw new BuildException(workingDirectory + " doesn't exist.");
+            }
+            final Process process = launch(project, getCommandline(),
+                                           getEnvironment(), workingDirectory,
+                                           useVMLauncher);
+            if (Os.isFamily("windows")) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    project.log("interruption in the sleep after having spawned a process",
+                        Project.MSG_VERBOSE);
+                }
+            }
 
+            OutputStream dummyOut = new OutputStream() {
+                public void write(int b) throws IOException {
+                }
+            };
+
+            ExecuteStreamHandler streamHandler = new PumpStreamHandler(dummyOut);
+            streamHandler.setProcessErrorStream(process.getErrorStream());
+            streamHandler.setProcessOutputStream(process.getInputStream());
+            streamHandler.start();
+            process.getOutputStream().close();
+
+            project.log("spawned process " + process.toString(), Project.MSG_VERBOSE);
+            return process;
+        }
+    }
+    
 }
