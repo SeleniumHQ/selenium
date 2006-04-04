b8ff77e20352b69641b69984140b51f2bb948a45
Dan Fabulich <dan@fabulich.com>
2006-04-04 23:59:23 +0000

    Only run these tests on Windows, and make sure we run at least at least a minimal registry read test on all Windows operating systems.
    r2000

--- server/src/test/java/org/openqa/selenium/server/browserlaunchers/WindowsUtilsTest.java
+++ server/src/test/java/org/openqa/selenium/server/browserlaunchers/WindowsUtilsTest.java
@@ -23,9 +23,9 @@ import junit.framework.*;
 public class WindowsUtilsTest extends TestCase {
 
     public void testLoadEnvironment() {
-        if (WindowsUtils.thisIsWindows()) {
-            Map p =WindowsUtils.loadEnvironment();
-            assertFalse("Environment appears to be empty!", p.isEmpty());
+        if (!WindowsUtils.thisIsWindows()) return;
+        Map p =WindowsUtils.loadEnvironment();
+        assertFalse("Environment appears to be empty!", p.isEmpty());
 //            for (Iterator i = p.keySet().iterator(); i.hasNext();) {
 //                String key = (String) i.next();
 //                String value = (String) p.get(key);
@@ -33,20 +33,23 @@ public class WindowsUtilsTest extends TestCase {
 //                System.out.print('=');
 //                System.out.println(value);
 //            }
-            assertNotNull("SystemRoot env var apparently not set on Windows!", WindowsUtils.findSystemRoot());   
-        }
+        assertNotNull("SystemRoot env var apparently not set on Windows!", WindowsUtils.findSystemRoot());
     }
     public void testWMIC() {
+        if (!WindowsUtils.thisIsWindows()) return;
         assertTrue("wmic should be found", "wmic" != WindowsUtils.findWMIC());
     }
     public void testTaskKill() {
+        if (!WindowsUtils.thisIsWindows()) return;
         assertTrue("taskkill should be found", "taskkill" != WindowsUtils.findTaskKill());
     }
     public void testReadRegistryValue() {
+        if (!WindowsUtils.thisIsWindows()) return;
         boolean autoConfigURLExists = WindowsUtils.doesRegistryValueExist("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", "AutoConfigURL");
         System.out.println(autoConfigURLExists);
         if (autoConfigURLExists) {
             System.out.println(WindowsUtils.readStringRegistryValue("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", "AutoConfigURL"));
         }
+        System.out.println(WindowsUtils.readStringRegistryValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "CurrentVersion"));
     }
 }
