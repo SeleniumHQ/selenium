2f145411b8463d43d991beae656768396e39a565
Darrell DeBoer <darrell.deboer@gmail.com>
2006-04-13 02:10:12 +0000

    Deleted the intellij project in SVN Deleted the old "server" codebase
    r2045

--- server/src/main/java/org/openqa/selenium/server/htmlrunner/HTMLResultsListener.java
+++
@@ -1,9 +0,0 @@
-/*
- * Created on Feb 25, 2006
- *
- */
-package org.openqa.selenium.server.htmlrunner;
-
-public interface HTMLResultsListener {
-    public void processResults(HTMLTestResults results);
-}
