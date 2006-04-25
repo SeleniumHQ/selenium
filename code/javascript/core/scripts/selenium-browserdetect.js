500460d808bcc99715d326c0e3458826d21477a7
Darrell DeBoer <darrell.deboer@gmail.com>
2006-04-25 11:17:44 +0000

    - New browserdetect was broken in IE - revert
    r2145

--- code/javascript/core/scripts/selenium-browserdetect.js
+++ code/javascript/core/scripts/selenium-browserdetect.js
@@ -20,27 +20,14 @@
 // work around seem to make it necessary. Maybe as we learn more about what we need,
 // we can do this in a more "feature-centric" rather than "browser-centric" way.
 // TODO we should probably reuse an available browser-detection library
+var browserName=navigator.appName;
+var isOpera = (window.opera != null);
+var isIE = !isOpera && (browserName =="Microsoft Internet Explorer");
+var isKonqueror = (browserName == "Konqueror");
+var isSafari = (navigator.userAgent.indexOf('Safari') != -1);
+var isFirefox = (navigator.userAgent.indexOf('Firefox') != -1);
+var isNetscape = !isOpera && !isFirefox && (navigator.appName == "Netscape");
 
 // Get the Gecko version as an 8 digit date.
 var geckoResult = /^Mozilla\/5\.0 .*Gecko\/(\d{8}).*$/.exec(navigator.userAgent);
 var geckoVersion = geckoResult == null ? null : geckoResult[1];
-
-var detectedBrowser =
-{
-    browserName: navigator.appName,
-    opera: (window.opera != null),
-    ie: !isOpera && (browserName =="Microsoft Internet Explorer"),
-    konqueror: (browserName == "Konqueror"),
-    safari:  (navigator.userAgent.indexOf('Safari') != -1),
-    firefox: (navigator.userAgent.indexOf('Firefox') != -1),
-    netscape: !isOpera && !isFirefox && (navigator.appName == "Netscape")
-}
-
-// TODO - don't use global variables - pass the detected browser around
-var browserName = detectedBrowser.browserName;
-var isOpera = detectedBrowser.opera;
-var isIE = detectedBrowser.ie;
-var isKonqueror = detectedBrowser.konqueror;
-var isSafari = detectedBrowser.safari;
-var isFirefox = detectedBrowser.firefox;
-var isNetscape = detectedBrowser.netscape;
