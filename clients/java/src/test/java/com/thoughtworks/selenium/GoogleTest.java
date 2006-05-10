49e0305c9af580f2cf8bc1dc4ff7e70c2d4a1f2f
Dan Fabulich <dan@fabulich.com>
2006-05-10 20:13:57 +0000

    Adding a hard-coded GoogleTest that we can link in the documentation
    r2229

---
+++ clients/java/src/test/java/com/thoughtworks/selenium/GoogleTest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.selenium;
+
+import junit.framework.*;
+
+import org.openqa.selenium.server.*;
+
+public class GoogleTest extends TestCase
+{
+   private Selenium selenium;
+
+   public void setUp() throws Exception {
+        String url = "http://www.google.com";
+       selenium = new DefaultSelenium("localhost", SeleniumServer.DEFAULT_PORT, "*firefox", url);
+       selenium.start();
+    }
+   
+   protected void tearDown() throws Exception {
+       selenium.stop();
+   }
+   
+   public void testGoogleTestSearch() throws Throwable {
+		selenium.open("http://www.google.com");
+		assertEquals("Google", selenium.getTitle());
+		selenium.type("q", "Selenium OpenQA");
+		assertEquals("Selenium OpenQA", selenium.getValue("q"));
+		selenium.click("btnG");
+		selenium.waitForPageToLoad("5000");
+		selenium.assertTextPresent("openqa.org");
+		assertEquals("Selenium OpenQA - Google Search", selenium.getTitle());
+	}
+	
+}
