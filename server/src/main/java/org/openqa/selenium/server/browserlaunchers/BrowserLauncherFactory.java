786c4b06d1b6c721f13aa5e381fdac7baf619d70
Dan Fabulich <dan@fabulich.com>
2006-05-15 20:23:13 +0000

    Added preliminary IEHTA support
    r2272

--- server/src/main/java/org/openqa/selenium/server/browserlaunchers/BrowserLauncherFactory.java
+++ server/src/main/java/org/openqa/selenium/server/browserlaunchers/BrowserLauncherFactory.java
@@ -26,6 +26,7 @@ public class BrowserLauncherFactory {
     private static final Pattern FIREFOX_PATTERN = Pattern.compile("^\\*firefox( .*)?$");
     private static final Pattern IEXPLORE_PATTERN = Pattern.compile("^\\*iexplore( .*)?$");
     private static final Pattern SAFARI_PATTERN = Pattern.compile("^\\*safari( .*)?$");
+    private static final Pattern HTA_PATTERN = Pattern.compile("^\\*iehta( .*)?$");
     SeleniumServer server;
     
     public BrowserLauncherFactory(SeleniumServer server) {
@@ -38,6 +39,7 @@ public class BrowserLauncherFactory {
         Matcher FirefoxMatcher = FIREFOX_PATTERN.matcher(browser);
         Matcher IExploreMatcher = IEXPLORE_PATTERN.matcher(browser);
         Matcher SafariMatcher = SAFARI_PATTERN.matcher(browser);
+        Matcher HTAMatcher = HTA_PATTERN.matcher(browser);
         if (FirefoxMatcher.find()) {
             if (browser.equals("*firefox")) {
                 launcher = new FirefoxCustomProfileLauncher(server.getPort(), sessionId);
@@ -58,6 +60,13 @@ public class BrowserLauncherFactory {
             } else {
                 String browserStartCommand = SafariMatcher.group(1).substring(1);
                 launcher = new SafariCustomProfileLauncher(server.getPort(), sessionId, browserStartCommand);
+            }
+        } else if (HTAMatcher.find()) {
+            if (browser.equals("*iehta")) {
+                launcher = new HTABrowserLauncher(server.getPort(), sessionId);
+            } else {
+                String browserStartCommand = HTAMatcher.group(1).substring(1);
+                launcher = new HTABrowserLauncher(server.getPort(), sessionId, browserStartCommand);
             }
         } else {
             launcher = new DestroyableRuntimeExecutingBrowserLauncher(browser);
---
+++ server/src/main/java/org/openqa/selenium/server/browserlaunchers/HTABrowserLauncher.java
@@ -0,0 +1,104 @@
+/*
+ * Created on May 14, 2006
+ *
+ */
+package org.openqa.selenium.server.browserlaunchers;
+
+import java.io.*;
+import java.net.*;
+
+public class HTABrowserLauncher extends DestroyableRuntimeExecutingBrowserLauncher {
+
+    private int port;
+    private String sessionId;
+    private File dir;
+
+    public HTABrowserLauncher() {
+        super(findBrowserLaunchLocation());
+    }
+    
+    public HTABrowserLauncher(int port, String sessionId) {
+        super(findBrowserLaunchLocation());
+        this.port = port;
+        this.sessionId = sessionId;
+    }
+    
+    public HTABrowserLauncher(int port, String sessionId, String browserLaunchLocation) {
+        super(browserLaunchLocation);
+        this.port = port;
+        this.sessionId = sessionId;
+    }
+    
+    private static String findBrowserLaunchLocation() {
+        String defaultPath = System.getProperty("mshtaDefaultPath");
+        if (defaultPath == null) {
+            defaultPath = WindowsUtils.findSystemRoot() + "\\system32\\mshta.exe";
+        }
+        File defaultLocation = new File(defaultPath);
+        if (defaultLocation.exists()) {
+            return defaultLocation.getAbsolutePath();
+        }
+        File mshtaEXE = AsyncExecute.whichExec("mshta.exe");
+        if (mshtaEXE != null) return mshtaEXE.getAbsolutePath();
+        throw new RuntimeException("MSHTA.exe couldn't be found in the path!\n" +
+                "Please add the directory containing mshta.exe to your PATH environment\n" +
+                "variable, or explicitly specify a path to mshta.exe like this:\n" +
+                "*mshta c:\\blah\\mshta.exe");
+    }
+
+    public void launch(String url) {
+        String query = getQueryString(url);
+        if (null == query) {
+            query = "";
+        }
+        query += "&baseUrl=http://localhost:" + port + "/selenium-server/";
+        String hta = createHTAFile();
+        System.out.println("Launching Internet Explorer HTA...");
+        AsyncExecute exe = new AsyncExecute();
+        exe.setCommandline(new String[] {commandPath, hta, query});
+        try {
+            process = exe.asyncSpawn();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private String createHTAFile() {
+        dir = createCustomProfileDir(sessionId);
+        InputStream input = HTABrowserLauncher.class.getResourceAsStream("/core/SeleneseRunner.html");
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+        File hta = new File(dir, "SeleneseRunner.hta");
+        try {
+            FileWriter fw = new FileWriter(hta);
+            String line = br.readLine();
+            fw.write(line);
+            fw.write('\n');
+            fw.write("<base href=\"http://localhost:" + port + "/selenium-server/core/\">");
+            while ((line = br.readLine()) != null) {
+                fw.write(line);
+                fw.write('\n');
+            }
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return hta.getAbsolutePath();
+    }
+    
+    private String getQueryString(String url) {
+        try {
+            URL u = new URL(url);
+            String query = u.getQuery();
+            return query;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void close() {
+        super.close();
+        recursivelyDeleteDir(dir);
+    }
+
+}
