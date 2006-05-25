5dc8a7a4c61ad8d79280c8ae11764eaa31e4c21a
Dan Fabulich <dan@fabulich.com>
2006-05-25 01:47:45 +0000

    Putting the rest of the Opera profile in the custom profile directory; fixing various strings
    r2333

--- server/src/main/java/org/openqa/selenium/server/browserlaunchers/OperaCustomProfileLauncher.java
+++ server/src/main/java/org/openqa/selenium/server/browserlaunchers/OperaCustomProfileLauncher.java
@@ -16,14 +16,11 @@
  */
 package org.openqa.selenium.server.browserlaunchers;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.regex.Pattern;
+import java.io.*;
+import java.util.regex.*;
 
-import org.apache.tools.ant.taskdefs.condition.Os;
-import org.openqa.selenium.server.SeleniumServer;
+import org.apache.tools.ant.taskdefs.condition.*;
+import org.openqa.selenium.server.*;
 
 public class OperaCustomProfileLauncher implements BrowserLauncher {
 
@@ -76,7 +73,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
     }
     
     private static String findBrowserLaunchLocation() {
-        String defaultPath = System.getProperty("firefoxDefaultPath");
+        String defaultPath = System.getProperty("operaDefaultPath");
         if (defaultPath == null) {
             if (WindowsUtils.thisIsWindows()) {
                 defaultPath = WindowsUtils.getProgramFilesPath() + "\\Opera\\opera.exe";
@@ -92,8 +89,8 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
             	File operaEXE = AsyncExecute.whichExec("opera.exe");
             	if (operaEXE != null) return operaEXE.getAbsolutePath();
             	throw new RuntimeException("Opera couldn't be found in the path!\n" +
-            			"Please add the directory containing firefox.exe to your PATH environment\n" +
-            			"variable, or explicitly specify a path to Firefox like this:\n" +
+            			"Please add the directory containing opera.exe to your PATH environment\n" +
+            			"variable, or explicitly specify a path to Opera like this:\n" +
             			"*opera c:\\blah\\opera.exe");
         }
                 // On unix, prefer operaBin if it's on the path
@@ -109,6 +106,32 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
             
     static final Pattern JAVA_STYLE_UNC_URL = Pattern.compile("^file:////([^/]+/.*)$");
     static final Pattern JAVA_STYLE_LOCAL_URL = Pattern.compile("^file:/([A-Z]:/.*)$");
+    /**
+     * Generates an URL suitable for use in browsers, unlike Java's URLs, which choke
+     * on UNC paths.
+     * 
+     * <P>Java's URLs work in IE, but break in Mozilla.  Mozilla's team snobbily demanded
+     * that <I>all</I> file paths must have the empty authority (file:///), even for UNC file paths.
+     * On Mozilla \\socrates\build is therefore represented as file://///socrates/build.</P>  See
+     * Mozilla bug <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=66194">66194</A>. 
+     * @param path - the file path to convert to a browser URL
+     * @return a nice Mozilla-compatible file URL
+     */
+    private static String fileToBrowserURL(File f) {
+        if (f == null) return null;
+        String url = f.toURI().toString();
+        Matcher m = JAVA_STYLE_UNC_URL.matcher(url);
+        if (m.find()) {
+            url = "file://///";
+            url += m.group(1);
+        }
+        m = JAVA_STYLE_LOCAL_URL.matcher(url);
+        if (m.find()) {
+            url = "file:///";
+            url += m.group(1);
+        }
+        return url;
+    }
     
     public void launch(String url) {
         try {
@@ -139,6 +162,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
         
         // TODO Do we want to make these preferences configurable somehow?
         File opera6ini = new File(customProfileDir, "opera6.ini");
+        
         PrintStream out = new PrintStream(new FileOutputStream(opera6ini));
         // Configure us as the local proxy
         // TODO Proxy.pac file doesn't seem to want to work correctly
@@ -166,13 +190,23 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
         out.println("Run=0");
         
         out.println("[User Prefs]");
+        // Put our Opera profile in the custom profile dir
+        out.println("Opera Directory=" + customProfileDir.getAbsolutePath());
+        out.println("Shown First Time Setup=1");
+        out.println("One Time Page=");
         out.println("Show Setupdialog On Start=0");
         // Disable "do you want to remember this password?"
         out.println("Enable Wand=0");
         // Disable pop-up blocking
         out.println("Ignore Unrequested Popups=0");
-
-        // TODO Don't ask if we want to switch default browsers
+        // Don't ask if we want to switch default browsers
+        out.println("Show Default Browser Dialog=0");
+        // Don't open any pages on startup (except for command line URL)
+        out.println("Startup Type=4");
+        
+        out.println("[Install]");
+        out.println("HELLO=NO");
+        
         // TODO Disable security warnings
         
         out.close();
@@ -195,7 +229,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
         process.destroy();
         int exitValue = AsyncExecute.waitForProcessDeath(process, 10000);
         if (exitValue == 0) {
-            System.err.println("WARNING: Firefox seems to have ended on its own (did we kill the real browser???)");
+            System.err.println("WARNING: Opera seems to have ended on its own (did we kill the real browser???)");
         }
         try {
             waitForFileLockToGoAway(5*000, 500);
@@ -213,7 +247,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
                 System.err.print("Perhaps caused by: ");
                 if (taskKillException != null) taskKillException.printStackTrace();
                 if (fileLockException != null) fileLockException.printStackTrace();
-                throw new RuntimeException("Couldn't delete custom Firefox " +
+                throw new RuntimeException("Couldn't delete custom Opera " +
                         "profile directory, presumably because task kill failed; " +
                         "see stderr!", e);
             }
@@ -222,7 +256,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
         closed = true;
     }
     
-    /** Firefox knows it's running by using a "parent.lock" file in
+    /** Opera knows it's running by using a "parent.lock" file in
      * the profile directory.  Wait for this file to go away (and stay gone)
      * @param timeout max time to wait for the file to go away
      * @param timeToWait minimum time to wait to make sure the file is gone
@@ -237,7 +271,7 @@ public class OperaCustomProfileLauncher implements BrowserLauncher {
         if (lock.exists()) throw new FileLockRemainedException("Lock file still present! " + lock.getAbsolutePath());
     }
     
-    /** When initializing the profile, Firefox rapidly starts, stops, restarts and
+    /** When initializing the profile, Opera rapidly starts, stops, restarts and
      * stops again; we need to wait a bit to make sure the file lock is really gone.
      * @param lock the parent.lock file in the profile directory
      * @param timeToWait minimum time to wait to see if the file shows back 
