365dbf9cd1dea2d1251c6b25819db725a15001a1
Shinya Kasatani <shinya@example.com>
2006-06-03 15:32:27 +0000

    remove unused patches
    r2434

--- src/apply-patches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Applies patches to Selenium.
-#
-# You should run this script once after importing Selenium with Ant.
-#
-
-cat selenium-patches/clear-javascript-warnings.patch | (cd content/selenium && patch -p0)
