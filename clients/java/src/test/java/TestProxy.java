a2c63dc105aa5188909f9206c265bcb33b5cc7ec
Nelson Sproul <nsproul@bea.com>
2006-06-13 14:39:52 +0000

    use *custom launcher -- if we decide proxyInjection is worthwhile, we'll switch to a new *pi mode
    r2495

--- clients/java/src/test/java/TestProxy.java
+++ clients/java/src/test/java/TestProxy.java
@@ -12,4 +12,13 @@ public class TestProxy extends SeleneseTestCase
 		selenium.open("http://www.google.com");
 		assertTrue(selenium.isTextPresent("Advanced Search"));
 	}
+    
+    public void setUp() throws Exception {
+        // if/when a proxy injection browser launcher comes into being, its
+        // default URL will need to be injected w/ appropriate js; i.e., 
+        // 1. in jetty, load SeleneseRunner.html as a resource,
+        // 2. inject js into it        
+        //super.setUp("http://www.adyn.com", "*custom c:/PROGRA~1/MOZILL~1/firefox.exe");
+        super.setUp("http://www.adyn.com", "*custom c:/PROGRA~1/INTERN~1/iexplore.exe");
+    }
 }
