0171b4a2954ec627a71bd6e54ed81371fa881018
Nelson Sproul <nsproul@bea.com>
2006-06-23 18:50:20 +0000

    browsers ignore JavaScript injected after a frameset, so the usual strategy of just appending the js at EOF won't work then.  So instead use frameset's onload hook, being careful to record the AUT's onload setting, if there is one.
    r2591

--- server/src/main/java/org/openqa/selenium/server/InjectionHelper.java
+++ server/src/main/java/org/openqa/selenium/server/InjectionHelper.java
@@ -3,7 +3,10 @@ package org.openqa.selenium.server;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.mortbay.http.HttpResponse;
@@ -37,16 +40,31 @@ public class InjectionHelper {
     }
     
     public static void injectJavaScript(SeleniumServer seleniumServer, boolean isKnownToBeHtml, HttpResponse response, InputStream in, OutputStream out) throws IOException {
-        byte[] buf = new byte[1024];
+        byte[] buf = new byte[8192];
         int len = in.read(buf);
         if (len == -1) {
             return;
         }
+        String data = new String(buf, 0, len);
         if (!isKnownToBeHtml) {
-            String data = new String(buf);
-            Pattern regexp = Pattern.compile("<\\s*(html|head|body|table)", 
+            Pattern regexp = Pattern.compile("<\\s*(html|frameset|head|body|table)", 
                     Pattern.CASE_INSENSITIVE);  
-            isKnownToBeHtml = regexp.matcher(data).find(); 
+            isKnownToBeHtml = regexp.matcher(data).find();
+        }
+        boolean isFrameSet = false;
+        if (!isKnownToBeHtml) {
+            out.write(buf, 0, len);
+        }
+        else {
+            Pattern regexp = Pattern.compile("<\\s*frameset", 
+                     Pattern.CASE_INSENSITIVE);  
+            isFrameSet = regexp.matcher(data).find();
+            if (isFrameSet) {
+                // JavaScript inserted at the end of an HTML file is executed on load *unless*
+                // the file is a frame set.  In that case, only by means of the onload hook can 
+                // we execute.
+                data = usurpOnUnloadHook(data, "runTest");
+            }
         }
         String proxyHost = "localhost";
         int proxyPort = SeleniumServer.getPortDriversShouldContact();
@@ -65,25 +83,58 @@ public class InjectionHelper {
             .append(restoreJsStateInitializer(sessionId))
             .append("}\n");
             
-            out.write(makeJsChunk(moreJs.toString()));
             jsIn.close();
+            out.write(data.getBytes());
         }           
-        out.write(buf, 0, len);
         IO.copy(in, out);
             
-        if (isKnownToBeHtml) {
+        if (isKnownToBeHtml && !isFrameSet) {
             InputStream jsIn = new ClassPathResource("/core/scripts/injectionAtEOF.html").getInputStream();
             out.write(getJsWithSubstitutions(jsIn, proxyHost, proxyPort, sessionId));
             jsIn.close();
         }
     }
     
-    private static byte[] makeJsChunk(String js) {
-        StringBuffer sb = new StringBuffer("\n<script language=\"JavaScript\">\n");
-        sb.append(js)
-        .append("\n</script>\n");
-        return sb.toString().getBytes();
+    private static String usurpOnUnloadHook(String data, String string) {
+        Pattern framesetAreaRegexp = Pattern.compile("(<\\s*frameset.*?>)", Pattern.CASE_INSENSITIVE);
+        Matcher framesetMatcher = framesetAreaRegexp.matcher(data);
+        if (!framesetMatcher.find()) {
+            System.out.println("WARNING: looked like a frameset, but couldn't retrieve the frameset area");
+            return data;
+        }
+        String onloadRoutine = "piRunTest()";
+        String frameSetText = framesetMatcher.group(1);
+        Pattern onloadRegexp = Pattern.compile("onload='(.*?)'", Pattern.CASE_INSENSITIVE);
+        Matcher onloadMatcher = onloadRegexp.matcher(frameSetText);
+        if (!onloadMatcher.find()) {
+            onloadRegexp = Pattern.compile("onload=\"(.*?)\"", Pattern.CASE_INSENSITIVE); // try double quotes
+            onloadMatcher = onloadRegexp.matcher(frameSetText);
+        }
+        if (onloadMatcher.find()) {
+            String oldOnloadRoutine = onloadMatcher.group(1);
+            frameSetText = onloadMatcher.replaceFirst("");
+            String escapedOldOnloadRoutine = null;
+            try {
+                escapedOldOnloadRoutine = URLEncoder.encode(oldOnloadRoutine, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("could not handle " + oldOnloadRoutine + ": " + e);
+            }
+            onloadRoutine = "piRunTest(unescape('" + escapedOldOnloadRoutine  + "'))";
+        }
+        
+        // either there was no existing onload, or it's been stripped out
+        Pattern framesetTagRegexp = Pattern.compile("<\\s*frameset", Pattern.CASE_INSENSITIVE);
+        frameSetText = framesetTagRegexp.matcher(frameSetText).replaceFirst("<frameset onload=\"" + onloadRoutine + "\"");
+        data = framesetMatcher.replaceFirst(frameSetText);
+        return data;
     }
+
+//    private static byte[] makeJsChunk(String js) {
+//        StringBuffer sb = new StringBuffer("\n<script language=\"JavaScript\">\n");
+//        sb.append(js)
+//        .append("\n</script>\n");
+//        return sb.toString().getBytes();
+//    }
 
     private static byte[] getJsWithSubstitutions(InputStream jsIn, String proxyHost, int proxyPort, String sessionId) throws IOException {
         if (jsIn.available()==0) {
