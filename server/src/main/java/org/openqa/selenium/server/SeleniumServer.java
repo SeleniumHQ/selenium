b6de2adc45566acc934ab34d823b8e1d280748fb
Nelson Sproul <nsproul@bea.com>
2006-07-01 05:58:49 +0000

    restore change 1305
    r2662

--- server/src/main/java/org/openqa/selenium/server/SeleniumServer.java
+++ server/src/main/java/org/openqa/selenium/server/SeleniumServer.java
@@ -194,7 +194,7 @@ public class SeleniumServer {
                         "mode for all sessions, no matter what is passed to getNewBrowserSession.");
             }
             else if ("-port".equals(arg)) {
-                port = Integer.parseInt(args[++i]);
+                port = Integer.parseInt(getArg(args, ++i));
             }
             else if ("-proxyInjectionMode".equals(arg)) {
                 proxyInjectionModeArg = true;
@@ -202,16 +202,28 @@ public class SeleniumServer {
             else if ("-portDriversShouldContact".equals(arg)) {
                 // to facilitate tcptrace interception of interaction between 
                 // injected js and the selenium server
-                portDriversShouldContactArg = Integer.parseInt(args[++i]);
+                portDriversShouldContactArg = Integer.parseInt(getArg(args, ++i));
             }
             else if ("-debug".equals(arg)) {
                 SeleniumServer.setDebugMode(true);
             }
             else if ("-timeout".equals(arg)) {
-                timeout = Integer.parseInt(args[++i]);
+                timeout = Integer.parseInt(getArg(args, ++i));
+            }
+            else if ("-userJsInjection".equals(arg)) {
+                if (!InjectionHelper.addUserJsInjectionFile(getArg(args, ++i))) {
+                    usage(null);
+                    System.exit(1);
+                }
+            }
+            else if ("-userContentTransformation".equals(arg)) {
+                if (!InjectionHelper.addUserContentTransformation(getArg(args, ++i), getArg(args, ++i))) {
+                    usage(null);
+                    System.exit(1);
+                }
             }
             else if ("-userExtensions".equals(arg)) {
-                userExtensions = new File(args[++i]);
+                userExtensions = new File(getArg(args, ++i));
                 if (!userExtensions.exists()) {
                     System.err.println("User Extensions file doesn't exist: " + userExtensions.getAbsolutePath());
                     System.exit(1);
@@ -263,6 +275,14 @@ public class SeleniumServer {
         final SeleniumServer seleniumProxy = new SeleniumServer(port);
         seleniumProxy.setProxyInjectionMode(proxyInjectionModeArg);
         SeleniumServer.setPortDriversShouldContact(portDriversShouldContactArg);
+        
+        if (!isProxyInjectionMode() && 
+                (InjectionHelper.userContentTransformationsExist() ||
+                 InjectionHelper.userJsInjectionsExist())) {
+            usage("-userJsInjection and -userContentTransformation are only " +
+                    "valid in combination with -proxyInjectionMode");
+            System.exit(1);
+        }
         
         Thread jetty = new Thread(new Runnable() {
             public void run() {
@@ -363,6 +383,14 @@ public class SeleniumServer {
         
     }
 
+    private static String getArg(String[] args, int i) {
+        if (i >= args.length) {
+            usage("expected at least one more argument");
+            System.exit(-1);
+        }
+        return args[i];
+    }
+
     private static void proxyInjectionSpeech() {
         System.out.println("The selenium server will execute in proxyInjection mode.");
     }
@@ -382,14 +410,38 @@ public class SeleniumServer {
         if (msg!=null) {
             System.err.println(msg + ":");
         }
-        System.err.println("Usage: java -jar selenium-server.jar -debug [-port nnnn] [-timeout nnnn] [-interactive] [-defaultBrowserString browserString] [-htmlSuite browserString (e.g. \"*firefox\") startURL (e.g. \"http://www.google.com\") " +
+        System.err.println("Usage: java -jar selenium-server.jar -debug [-port nnnn] [-timeout nnnn] [-interactive]" +
+                " [-defaultBrowserString browserString] [-proxyInjectionMode [-userContentTransformation your-before-regexp-string your-after-string] [-userJsInjection your-js-filename]] [-htmlSuite browserString (e.g. \"*firefox\") startURL (e.g. \"http://www.google.com\") " +
                 "suiteFile (e.g. \"c:\\absolute\\path\\to\\my\\HTMLSuite.html\") resultFile (e.g. \"c:\\absolute\\path\\to\\my\\results.html\"]\n" +
                 "where:\n" +
                 "the argument for timeout is an integer number of seconds before we should give up\n" +
                 "the argument for port is the port number the selenium server should use (default 4444)" +
         "\n\t-interactive puts you into interactive mode.  See the tutorial for more details" +
         "\n\t-defaultBrowserString (e.g., *iexplore) sets the browser mode for all sessions, no matter what is passed to getNewBrowserSession" +
-        "\n\t-debug puts you into debug mode, with more trace information and diagnostics");
+        "\n\t-debug puts you into debug mode, with more trace information and diagnostics" +
+        "\n\t-proxyInjectionMode puts you into proxy injection mode, a mode where the selenium server acts as a proxy server " +
+        "\n\t\tfor all content going to the test application.  Under this mode, multiple domains can be visited, and the " +
+        "\n\t\tfollowing additional flags are supported:" +
+        "\n\t\t\tThe -userJsInjection flag allows you to point at a JavaScript file which will then be injected into all pages.  " +
+        "\n\t\t\tThe -userContentTransformation flag takes two arguments: the first is a regular expression which is matched " +
+        "\n\t\t\t\tagainst all test HTML content; the second is a string which will replace matches.  These flags can be used any " +
+        "\n\t\t\t\tnumber of times.  A simple example of how this could be useful: if you add" + 
+        "\n" + 
+        "\n\t\t\t\t   -userContentTransformation https http" + 
+        "\n" + 
+        "\n\t\t\t\tthen all \"https\" strings in the HTML of the test application will be changed to be \"http\".\n" +
+        "\n\t-debug puts you into debug mode, with more trace information and diagnostics" +
+        "\n\t-proxyInjectionMode puts you into proxy injection mode, a mode where the selenium server acts as a proxy server " +
+        "\n\t\tfor all content going to the test application.  Under this mode, multiple domains can be visited, and the " +
+        "\n\t\tfollowing additional flags are supported:" +
+        "\n\t\t\tThe -userJsInjection flag allows you to point at a JavaScript file which will then be injected into all pages.  " +
+        "\n\t\t\tThe -userContentTransformation flag takes two arguments: the first is a regular expression which is matched " +
+        "\n\t\t\t\tagainst all AUT content; the second is a string which will replace matches.  These flags can be used any " +
+        "\n\t\t\t\tnumber of times.  A simple example of how this could be useful: if you add" + 
+        "\n\n" + 
+        "   -userContentTransformation https http\r\n" + 
+        "\n\n" + 
+        "\n\t\t\t\tthen all \"https\" strings in the HTML of the test application will be changed to be \"http\".\n");
     }
 
     /** Prepares a Jetty server with its HTTP handlers.
