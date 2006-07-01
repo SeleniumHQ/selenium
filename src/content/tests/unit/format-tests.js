23c4186a1b3c77494c1e11f116c95854f339622e
Shinya Kasatani <shinya@example.com>
2006-07-01 13:39:17 +0000

    fixed tests
    r2664

--- src/content/tests/unit/format-tests.js
+++ src/content/tests/unit/format-tests.js
@@ -49,7 +49,7 @@ function testRubyRCFormat() {
 	assertEquals('assert !60.times{ break if (@selenium.is_text_present("test") rescue false); sleep 1 }', nextCommand());
 	assertEquals('assert !60.times{ break unless (@selenium.is_text_present("test") rescue true); sleep 1 }', nextCommand());
 	assertEquals('assert_equal "def", @selenium.get_text("abc")', nextCommand());
-	assertEquals('assert /^def(.|[\\r\\n])*$/ =~ @selenium.get_text("abc")', nextCommand());
+	assertEquals('assert /^def[\\s\\S]*$/ =~ @selenium.get_text("abc")', nextCommand());
 	assertEquals('assert_equal "abc", @selenium.get_location', nextCommand());
 	assertEquals('assert_not_equal "abc", @selenium.get_location', nextCommand());
 	assertEquals('@selenium.type "theText", @selenium.get_eval("\'abc\'")', nextCommand());
@@ -110,7 +110,7 @@ function testJavaRCFormat() {
 	assertTrue(nextCommand().indexOf('if (selenium.isTextPresent("test")) break; }') >= 0);
 	assertTrue(nextCommand().indexOf('if (!selenium.isTextPresent("test")) break; }') >= 0);
 	assertEquals('assertEquals("def", selenium.getText("abc"));', nextCommand());
-	assertEquals('assertTrue(selenium.getText("abc").matches("^def(.|[\\\\r\\\\n])*$"));', nextCommand());
+	assertEquals('assertTrue(selenium.getText("abc").matches("^def[\\\\s\\\\S]*$"));', nextCommand());
 	assertEquals('assertEquals("abc", selenium.getLocation());', nextCommand());
 	assertEquals('assertNotEquals("abc", selenium.getLocation());', nextCommand());
 	assertEquals('selenium.type("theText", selenium.getEval("\'abc\'"));', nextCommand());
