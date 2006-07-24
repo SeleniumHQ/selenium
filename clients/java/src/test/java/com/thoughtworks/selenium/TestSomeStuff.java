9ca44a838f074ee566eb26c440e02f7adc2cca11
Nelson Sproul <nsproul@bea.com>
2006-07-24 21:36:57 +0000

    add test case to reproduce the "retired" Selenese queue problem (that occurs under proxy injection for some users)
    r2820

--- clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.selenium;
 
 public class TestSomeStuff extends SeleneseTestCase {
-    public void testTextArea() throws Throwable {
+    public void xtestTextArea() throws Throwable {
         selenium.setContext("Test text area", "error");
         selenium.open("/selenium-server/tests/html/test_verifications.html");
         String s = selenium.getText("theTextarea");
@@ -9,5 +9,122 @@ public class TestSomeStuff extends SeleneseTestCase {
         || "Line 1 Line 2".equals(s);           // firefox
         assertTrue("text area", b);
         System.out.println(s);
+    }
+    public void xtestTypeHang() throws Throwable{
+        selenium.open("http://www.google.co.uk");
+        selenium.waitForPageToLoad("50000");
+        
+        selenium.type("q", "rabbits");
+        selenium.click("btnG");
+        selenium.waitForPageToLoad("50000");
+        
+        selenium.type("q", "selenium2");
+        selenium.click("btnG");
+        selenium.waitForPageToLoad("50000");
+        
+        selenium.type("q", "selenium");
+        selenium.click("btnG");
+        selenium.waitForPageToLoad("50000");
+    }    
+    public void testCallxtestTypeHang() throws Throwable {
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        /*
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();
+        xtestTypeHang();*/
     }
 }
