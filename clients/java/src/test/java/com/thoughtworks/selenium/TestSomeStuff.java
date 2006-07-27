dbae686b7bd2fdde05dda2c78e30fb8164d4b652
Nelson Sproul <nsproul@bea.com>
2006-07-27 16:19:52 +0000

    add some tests
    r2832

--- clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.selenium;
 
 public class TestSomeStuff extends SeleneseTestCase {
-    public void xtestTextArea() throws Throwable {
+    public void testTextArea() throws Throwable {
         selenium.setContext("Test text area", "error");
         selenium.open("/selenium-server/tests/html/test_verifications.html");
         String s = selenium.getText("theTextarea");
+        System.out.println("selenium.getElementHeight=" + selenium.getElementHeight("theTextarea"));
+        System.out.println("selenium.getElementWidth=" + selenium.getElementWidth("theTextarea"));
+        System.out.println("selenium.getElementPositionLeft=" + selenium.getElementPositionLeft("theTextarea"));
+        System.out.println("selenium.getElementPositionTop=" + selenium.getElementPositionTop("theTextarea"));
         boolean b = "Line 1\nLine 2".equals(s)  // IE
         || "Line 1 Line 2".equals(s);           // firefox
         assertTrue("text area", b);
@@ -26,7 +30,7 @@ public class TestSomeStuff extends SeleneseTestCase {
         selenium.click("btnG");
         selenium.waitForPageToLoad("50000");
     }    
-    public void testCallxtestTypeHang() throws Throwable {
+    public void xtestCallxtestTypeHang() throws Throwable {
         xtestTypeHang();
         xtestTypeHang();
         xtestTypeHang();
