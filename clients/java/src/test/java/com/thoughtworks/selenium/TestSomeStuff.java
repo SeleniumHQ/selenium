663ce43a40fe56568038c1423586c4b25d900280
Nelson Sproul <nsproul@bea.com>
2006-07-28 03:05:26 +0000

    fix up element locating code, which was broken for y-coord of pages spanning > 1 screenful
    r2835

--- clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/TestSomeStuff.java
@@ -1,7 +1,59 @@
 package com.thoughtworks.selenium;
 
 public class TestSomeStuff extends SeleneseTestCase {
-    public void testTextArea() throws Throwable {
+    public void setUp() throws Exception {
+        super.setUp("http://process-dev-vm2:8080");
+    }
+    public void testSlider_SliderStyles()
+    throws InterruptedException
+{
+    String idPrefix1 = "form1_";
+    selenium.open("/selenium-server/tests/html/Slider.faces.htm");
+    //basic vertical
+    selenium.dragdrop(idPrefix1  + "slider7e_Overlay", "+0,+80");
+String      valStr = selenium.getValue(idPrefix1 + "slider7e");
+    assertNotNull(valStr);
+    assertTrue(Double.parseDouble(valStr) > 0.0);
+    Thread.sleep(1000);
+
+    //scrollbar vertical
+    selenium.dragdrop(idPrefix1 + "slider7f_Overlay", "+0,+80");
+    valStr = selenium.getValue(idPrefix1 + "slider7f");
+    assertNotNull(valStr);
+    assertTrue(Double.parseDouble(valStr) > 0.0);
+    Thread.sleep(1000);
+
+    //volume vertical
+    selenium.dragdrop(idPrefix1 + "slider7g_Overlay", "+0,+80");
+    valStr = selenium.getValue(idPrefix1 + "slider7g");
+    assertNotNull(valStr);
+    assertTrue(Double.parseDouble(valStr) > 0.0);
+    Thread.sleep(1000);
+
+    //thermometer vertical
+    selenium.dragdrop(idPrefix1 + "slider7h_Overlay", "+0,+80");
+    valStr = selenium.getValue(idPrefix1 + "slider7h");
+    assertNotNull(valStr);
+    assertTrue(Double.parseDouble(valStr) > 0.0);
+    Thread.sleep(1000);
+}
+
+    public void xtestIdeaCreate() throws Throwable {
+        selenium.setContext("Test idea create", "error");
+        selenium.open("http://process-dev-vm2:8080/portal/server.pt");
+        selenium.type("in_tx_username", "hw_user");
+        selenium.click("in_bu_Login");
+        selenium.waitForPageToLoad("30000");
+        selenium.select("xpath=//select[@id='in_se_commnav']", "Process Community");  
+//        selenium.open("http://process-dev-vm2:8080/portal/server.pt?space=CommunityPage&cached=true&parentname=MyPage&parentid=1&in_hi_userid=251&control=SetCommunity&CommunityID=205&PageID=0");
+        selenium.click("link=Create Idea");
+        selenium.waitForPopUp("main", "30000");
+        selenium.type("field0", "here is an idea");
+        selenium.click("Ok");
+        selenium.waitForPageToLoad("30000");
+        System.out.println("ok?");
+    }
+    public void xtestTextArea() throws Throwable {
         selenium.setContext("Test text area", "error");
         selenium.open("/selenium-server/tests/html/test_verifications.html");
         String s = selenium.getText("theTextarea");
