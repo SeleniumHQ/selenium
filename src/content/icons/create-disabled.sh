26c57f921e0ee022dc5e17d43d9a76a5225292fb
Shinya Kasatani <shinya@example.com>
2006-08-05 14:30:27 +0000

    move images and css to skin directory
    r2883

--- src/content/icons/create-disabled.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Create disabled icons with ImageMagick
-#
-
-convert icons.png -fill white -colorize 60% icons_disabled.png
---
+++ src/script/create-disabled.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Create disabled icons with ImageMagick
+#
+
+convert skin/classic/icons.png -fill white -colorize 60% skin/classic/icons_disabled.png
