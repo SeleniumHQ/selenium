5c33f3ca3a8096848ff09c476b6151df158a0787
lhuang <lhuang@example.com>
2006-08-08 04:05:52 +0000

    refactored mock.js. Allowing expectation of same function with different parameters.
    r2892

--- code/javascript/unittest/jsmock/mock.js
+++ code/javascript/unittest/jsmock/mock.js
@@ -14,105 +14,94 @@
  *  limitations under the License.
  *
  */
- 
+
 // A simple mock library for Javascript
 //
 // Original code by Aslak Hellesoy and Ji Wang
 
 var Mock = function() {
-    this.expectedInvocations = {};
-    this.expectedArgs = {};
-    this.returnValues = {};
+    this.expectedMethodInvocations = [];
     this.attrs = [];
     this.expectedProperties = {};
-    
-    this.expects = function() {
-        functionName = arguments[0];
-        this.expectedArgs[functionName] = [];
-        for(i = 1; i < arguments.length; i++) {
-            this.expectedArgs[functionName][i-1] = arguments[i];
-        }
-        javascriptCode = "this." + functionName + " = function() {\n" +
-            "  // mark this function as \"executed\"\n" +
-            "  this.expectedInvocations[\"" + functionName + "\"] = true;\n" +
-            "  assertEquals(\"" + functionName + ": Wrong number of arguments.\", " + this.expectedArgs[functionName].length + ", arguments.length);\n" +
-            "  for(i = 0; i < arguments.length; i++) {\n" +
-            "    assertEquals(this.expectedArgs[\"" + functionName + "\"][i], arguments[i]);\n" +
-            "  };\n" +
-            "  var returnValue = this.returnValues[\"" + functionName + "\"];\n" +
-            "  if (returnValue && returnValue.isMockError) { throw returnValue };\n" +
-            "  return returnValue;\n" +
-            "}";
-        eval(javascriptCode);
-        // initially mark this function as "not yet executed"
-        this.expectedInvocations[functionName] = false;
-        this.attrs[this.attrs.length] = "dummy";
-        return new Returner(this, functionName);
-    };
-    
-    this.expectsProperty = function() {
-        var propertyName = arguments[0];
-        if(arguments.length == 2) {
-            expectedPropertyValue = arguments[1];
-            this.expectedProperties[propertyName] = expectedPropertyValue;
-            this.attrs[this.attrs.length] = "dummy";
-        } else {
-            return new PropertySetter(this, propertyName);
-        }
-    };
-
-    this.verify = function() {
-        // loop over all expected invocations and see if they were called
-        for(var functionName in this.expectedInvocations) {
-            var wasCalled = this.expectedInvocations[functionName];
-            if(!wasCalled) {
-                fail("Expected function not called:" + functionName);
-            }
-        }
-        var currentAttrs = [];
-        var currentAttrCount = 0;
-        
-        // verify that all expected properties are set
-//        for(var attr in this) {
-//            currentAttrs[currentAttrCount] = attr;
-//            currentAttrCount++;
-//        }
-//        if(this.attrs.length < currentAttrCount) {
-//            unexpectedAttr = currentAttrs[this.attrs.length]
-//            fail("Unexpected property was set: " + unexpectedAttr + "=" + eval("this." + unexpectedAttr))
-//        }
-        
-        // verify that all expected properties are set with the right value
-//        for(var attr in this.expectedProperties) {
-//            if(this.expectedProperties[attr] != eval("this." + attr)) {
-//                fail("Expected property was not set: " + attr + "=" + this.expectedProperties[attr])
-//            }
-//        }
-    };
 
     var attrCount = 0;
-    for(var attr in this) {
+    for (var attr in this) {
         this.attrs[attrCount] = attr;
         attrCount++;
     }
 };
 
-Returner = function(mock, functionName) {
-    this.mock = mock;
+Mock.prototype.expects = function() {
+    var functionName = arguments[0];
+    var expectedArgs = [];
+    for (var i = 1; i < arguments.length; i++) {
+        expectedArgs[i - 1] = arguments[i];
+    }
+    var methodInvocation = new MethodInvocation(functionName, expectedArgs);
+
+    eval("if (!this." + functionName + ") { this." + functionName + " = this._executeMethod; }");
+
+    this.expectedMethodInvocations.push(methodInvocation);
+    this.attrs[this.attrs.length] = "dummy";
+    return new Returner(methodInvocation);
+};
+
+Mock.prototype._executeMethod = function() {
+    var methodInvocation = this.expectedMethodInvocations.shift();
+    if (!methodInvocation) {
+        fail("No more expected method invocations.")
+    }
+    assertEquals(methodInvocation.functionName + ": Wrong number of arguments.", methodInvocation.expectedArgs.length, arguments.length);
+    for (var i = 0; i < arguments.length; i++) {
+        assertEquals(methodInvocation.expectedArgs[i], arguments[i]);
+    }
+    var returnValue = methodInvocation.returnValue;
+    if (returnValue && returnValue.isMockError) {
+        throw returnValue;
+    }
+    return returnValue;
+};
+
+Mock.prototype.expectsProperty = function() {
+    var propertyName = arguments[0];
+    if (arguments.length == 2) {
+        var expectedPropertyValue = arguments[1];
+        this.expectedProperties[propertyName] = expectedPropertyValue;
+        this.attrs[this.attrs.length] = "dummy";
+    } else {
+        return new PropertySetter(this, propertyName);
+    }
+};
+
+Mock.prototype.verify = function() {
+    // loop over all expected invocations and see if they were called
+    for (var i = 0; i < this.expectedMethodInvocations.length; i++) {
+        var methodInvocation = this.expectedMethodInvocations[i];
+        fail("Expected function not called:" + methodInvocation.functionName);
+    }
+};
+
+var MethodInvocation = function(functionName, expectedArgs) {
     this.functionName = functionName;
+    this.expectedArgs = expectedArgs;
+    this.returnValue = undefined;
+}
+
+var Returner = function(methodInvocation) {
+    this.methodInvocation = methodInvocation;
 };
 
 Returner.prototype.returns = function(returnValue) {
-    this.mock.returnValues[this.functionName] = returnValue;
+    this.methodInvocation.returnValue = returnValue;
 };
 
 Returner.prototype.andThrows = function(message) {
     var error = new Error(message);
     error.isMockError = true;
-    this.mock.returnValues[this.functionName] = error;
+    this.methodInvocation.returnValue = error;
 };
 
-PropertySetter = function(mock, propertyName) {
+var PropertySetter = function(mock, propertyName) {
     this.mock = mock;
     this.propertyName = propertyName;
 };
