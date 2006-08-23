7c129b256980c31ebed1ff19c4f67f08206e3249
Mike Williams <mdub@dogbiscuit.org>
2006-08-23 07:00:36 +0000

    Add separate targets for unit_tests and functional_tests. Avoid repetition of WEBrick port. Defer WEBrick startup until the browser-testing task actually runs.
    r2957

--- code/buildscript/javascripttest.rb
+++ code/buildscript/javascripttest.rb
@@ -9,33 +9,34 @@ require 'selenium_result'
 include Browser
 
 class JavaScriptTestTask < ::Rake::TaskLib
-  def initialize(name=:test)
+
+  def initialize(name)
     @name = name
     @tests = []
+    @mounts = {}
     @browsers = []
     @port = 4444
     
     @queue = Queue.new
     
-    
-    @server = WEBrick::HTTPServer.new(:Port => @port) # TODO: make port configurable
-    @server.mount_proc("/jsunitResults") do |req, res|
-      parser, log_file = [JsUnitResult.new(req), "logs/JsUnitResults.xml"]
-      html_report = handle_test_results(res, parser, log_file)    
-    end
-    
-    @server.mount_proc("/seleniumResults") do |req, res|
-      parser, log_file = [SeleniumResult.new(req), "logs/SeleniumResults.xml"]
-      html_report = handle_test_results(res, parser, log_file)      
-      File.open("logs/SeleniumResults.html", File::CREAT|File::RDWR) do |f|
-        f << html_report
-      end
-    end
-    
     yield self if block_given?
     define
   end
   
+  # test should be specified as a url
+  def run(test)
+    @tests << test
+  end
+  
+  def mount(path, dir=nil)
+    dir = Dir.pwd + path unless dir
+    @mounts[path] = dir
+  end
+  
+  def browser(browser)
+    @browsers << browser.new
+  end
+
   def handle_test_results(res, parser, log_file)
     html_report = parse_result(parser, log_file)
     res.body += html_report
@@ -46,20 +47,38 @@ class JavaScriptTestTask < ::Rake::TaskLib
   def parse_result(parser, log_file)
     xml = parser.to_xml()
     mkdir_p 'logs'
-    File.open(log_file, File::CREAT|File::RDWR) do |f|
+    File.open(log_file, "w") do |f|
       f << xml
     end
     parser.to_html()
   end
   
+  def create_server
+    @server = WEBrick::HTTPServer.new(:Port => @port)
+    @server.mount_proc("/jsunitResults") do |req, res|
+      parser, log_file = [JsUnitResult.new(req), "logs/JsUnitResults.xml"]
+      html_report = handle_test_results(res, parser, log_file)    
+    end
+    @server.mount_proc("/seleniumResults") do |req, res|
+      parser, log_file = [SeleniumResult.new(req), "logs/SeleniumResults.xml"]
+      html_report = handle_test_results(res, parser, log_file)      
+      File.open("logs/SeleniumResults.html", "w") do |f|
+        f << html_report
+      end
+    end
+    @mounts.each do |path,dir|
+      @server.mount(path, WEBrick::HTTPServlet::FileHandler, dir)
+    end
+  end
+
   def define
     task @name do
-      trap("INT") {
-        puts "INT! test-server shutdown"
-        @server.shutdown
-      }
+      create_server
       t = Thread.new { 
         puts "Starting test-server"
+        trap(:INT) {
+          @server.shutdown
+        }
         @server.start
       }
       
@@ -67,8 +86,10 @@ class JavaScriptTestTask < ::Rake::TaskLib
         if browser.supported?
           puts "Running tests with #{browser}"
           @tests.each do |test|
+            test_url = "ROOT" + test
+            test_url.gsub!(/ROOT/, "http://localhost:#{@port}")
             browser.setup
-            browser.visit("http://localhost:#{@port}#{test}")              
+            browser.visit(test_url)              
             passed = @queue.pop
             browser.teardown
             raise "TEST FAILED" unless passed
@@ -85,17 +106,4 @@ class JavaScriptTestTask < ::Rake::TaskLib
     end
   end
   
-  def mount(path, dir=nil)
-    dir = Dir.pwd + path unless dir
-    @server.mount(path, WEBrick::HTTPServlet::FileHandler, dir)
-  end
-  
-  # test should be specified as a url
-  def run(test)
-    @tests << test
-  end
-  
-  def browser(browser)
-    @browsers << browser.new
-  end
 end
