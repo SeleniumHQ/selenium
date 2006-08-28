527d30117c7aa9e19055995b9c68f66958be6955
Mike Williams <mdub@dogbiscuit.org>
2006-08-28 02:32:48 +0000

    handy util
    r2990

---
+++ code/buildscript/slow-server.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# Serve up the "javascript" directory from WEBrick, but inject a random
+# artificial delay between when serving each page.  This simulates running
+# Selenium from a slow web-server.
+
+require 'webrick'
+
+$port = 4444
+$min_delay = 0.5
+$max_delay = 1.5
+
+class RandomDelayedFileHandler < WEBrick::HTTPServlet::FileHandler
+
+  def initialize(server, dir, mindelay=0, maxdelay=nil)
+    @mindelay = mindelay
+    @maxdelay = maxdelay || @mindelay 
+    super(server, dir)
+  end
+
+  def random_delay
+    @mindelay + rand * (@maxdelay - @mindelay)
+  end
+
+  def exec_handler(req, res)
+    delay = random_delay
+    puts "delay=#{delay}"
+    sleep(delay)
+    super(req, res)
+  end
+
+end
+
+server = WEBrick::HTTPServer.new(:Port => $port)
+server.mount("/javascript", RandomDelayedFileHandler, "javascript", $min_delay, $max_delay)
+trap(:INT) {
+  puts "Shutting down test server"
+  server.shutdown
+}
+puts "Starting test-server"
+server.start
