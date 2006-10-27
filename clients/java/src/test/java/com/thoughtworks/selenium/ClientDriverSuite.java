5d20230ca0ae30b2774892b228662494ed4c6998
Nelson Sproul <nsproul@bea.com>
2006-10-27 14:43:26 +0000

    enable PI mode, but without running any tests, just to verify that there is no env poisoning going on
    r3410

--- clients/java/src/test/java/com/thoughtworks/selenium/ClientDriverSuite.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/ClientDriverSuite.java
@@ -43,7 +43,7 @@ public class ClientDriverSuite extends TestCase {
      */
     public static Test suite() {
         boolean isProxyInjectionMode = System.getProperty("selenium.proxyInjectionMode") != null
-                && System.getProperty("selenium.proxyInjectionMode").equals("true");
+        && System.getProperty("selenium.proxyInjectionMode").equals("true");
 
         String forcedBrowserMode = System.getProperty("selenium.forcedBrowserMode");
 
@@ -59,60 +59,61 @@ public class ClientDriverSuite extends TestCase {
             TestSuite supersuite = new TestSuite(ClientDriverSuite.class
                     .getName());
             TestSuite suite = new TestSuite(ClientDriverSuite.class.getName());
-            suite.addTestSuite(ApacheMyFacesSuggestTest.class);
-            if (isProxyInjectionMode) {
-                // once frames support is added to the main trunk, we will be
-                // able to run the following in non-proxy injection mode:
-                suite.addTestSuite(TestFramesClick.class);
-                suite.addTestSuite(TestFramesOpen.class);
-                suite.addTestSuite(TestFramesNested.class);
-            }
-            suite.addTest(I18nTest.suite());
-            suite.addTestSuite(RealDealIntegrationTest.class);
-            suite.addTestSuite(TestErrorChecking.class);
-            suite.addTestSuite(TestJavascriptParameters.class);
-            suite.addTestSuite(TestClick.class);
-            suite.addTestSuite(GoogleTestSearch.class);
-            suite.addTestSuite(GoogleTest.class);
-            suite.addTestSuite(TestCheckUncheck.class);
-            suite.addTestSuite(TestClick.class);
-            suite.addTestSuite(TestXPathLocators.class);
-            suite.addTestSuite(TestClickJavascriptHref.class);
-            suite.addTestSuite(TestCommandError.class);
-            suite.addTestSuite(TestComments.class);
-            suite.addTestSuite(TestFailingAssert.class);
-            suite.addTestSuite(TestFailingVerifications.class);
-            suite.addTestSuite(TestFocusOnBlur.class);
-            suite.addTestSuite(TestGoBack.class);
-            suite.addTestSuite(TestImplicitLocators.class);
-            suite.addTestSuite(TestLocators.class);
-            suite.addTestSuite(TestOpen.class);
-            suite.addTestSuite(TestPatternMatching.class);
-            suite.addTestSuite(TestPause.class);
             if (!isProxyInjectionMode) {
-                suite.addTestSuite(TestSelectWindow.class);
+                if (isProxyInjectionMode) {
+                    // once frames support is added to the main trunk, we will be
+                    // able to run the following in non-proxy injection mode:
+                    suite.addTestSuite(TestFramesClick.class);
+                    suite.addTestSuite(TestFramesOpen.class);
+                    suite.addTestSuite(TestFramesNested.class);
+                }
+                suite.addTestSuite(ApacheMyFacesSuggestTest.class);
+                suite.addTest(I18nTest.suite());
+                suite.addTestSuite(RealDealIntegrationTest.class);
+                suite.addTestSuite(TestErrorChecking.class);
+                suite.addTestSuite(TestJavascriptParameters.class);
+                suite.addTestSuite(TestClick.class);
+                suite.addTestSuite(GoogleTestSearch.class);
+                suite.addTestSuite(GoogleTest.class);
+                suite.addTestSuite(TestCheckUncheck.class);
+                suite.addTestSuite(TestXPathLocators.class);
+                suite.addTestSuite(TestClickJavascriptHref.class);
+                suite.addTestSuite(TestCommandError.class);
+                suite.addTestSuite(TestComments.class);
+                suite.addTestSuite(TestFailingAssert.class);
+                suite.addTestSuite(TestFailingVerifications.class);
+                suite.addTestSuite(TestFocusOnBlur.class);
+                suite.addTestSuite(TestGoBack.class);
+                suite.addTestSuite(TestImplicitLocators.class);
+                suite.addTestSuite(TestLocators.class);
+                suite.addTestSuite(TestOpen.class);
+                suite.addTestSuite(TestPatternMatching.class);
+                suite.addTestSuite(TestPause.class);
+                if (!isProxyInjectionMode) {
+                    suite.addTestSuite(TestSelectWindow.class);
+                }
+                suite.addTestSuite(TestStore.class);
+                suite.addTestSuite(TestSubmit.class);
+                suite.addTestSuite(TestType.class);
+                suite.addTestSuite(TestVerifications.class);
+                suite.addTestSuite(TestWait.class);
+                suite.addTestSuite(TestSelect.class);
+                suite.addTestSuite(TestEditable.class);
+                if (!isProxyInjectionMode
+                        || "*piiexplore".equals(forcedBrowserMode)) { // BUG in PI mode propogation of state
+                    suite.addTestSuite(TestPrompt.class);
+                    suite.addTestSuite(TestConfirmations.class);
+                    suite.addTestSuite(TestAlerts.class);
+                    suite.addTestSuite(TestRefresh.class);
+                }
+                suite.addTestSuite(TestVisibility.class);
+                suite.addTestSuite(TestMultiSelect.class);
+                if (!isProxyInjectionMode) {
+                    suite.addTestSuite(TestWaitInPopupWindow.class);
+                }
+                suite.addTestSuite(TestWaitFor.class);
+                suite.addTestSuite(TestWaitForNot.class);
             }
-            suite.addTestSuite(TestStore.class);
-            suite.addTestSuite(TestSubmit.class);
-            suite.addTestSuite(TestType.class);
-            suite.addTestSuite(TestVerifications.class);
-            suite.addTestSuite(TestWait.class);
-            suite.addTestSuite(TestSelect.class);
-            suite.addTestSuite(TestEditable.class);
-            if (!isProxyInjectionMode
-                    || "*piiexplore".equals(forcedBrowserMode)) { // BUG in PI mode propogation of state
-                suite.addTestSuite(TestPrompt.class);
-                suite.addTestSuite(TestConfirmations.class);
-                suite.addTestSuite(TestAlerts.class);
-                suite.addTestSuite(TestRefresh.class);
-            }
-            suite.addTestSuite(TestVisibility.class);
-            suite.addTestSuite(TestMultiSelect.class);
-            if (!isProxyInjectionMode) {
-                suite.addTestSuite(TestWaitInPopupWindow.class);
-            }
-            suite.addTestSuite(TestWaitFor.class);
-            suite.addTestSuite(TestWaitForNot.class);
             ClientDriverTestSetup setup = new ClientDriverTestSetup(suite);
             supersuite.addTest(setup);
             return supersuite;
