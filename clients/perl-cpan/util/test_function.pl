4a53df7d8026a50a013b4fdd376a37b4a0185a9a
lukec <lukec@example.com>
2006-10-31 06:45:51 +0000

    little helper for debugging selenium-server calls
    r3439

--- clients/perl-cpan/util/test_function.pl
+++ clients/perl-cpan/util/test_function.pl
@@ -7,8 +7,14 @@ use WWW::Selenium;
 my ($function, @args) = @ARGV;
 die 'no function' unless $function;
 
-my $sel = WWW::Selenium->new(browser_url => 'http://www.csc.uvic.ca', browser => '*chrome');
+my $sel = WWW::Selenium->new(browser_url => 'http://www.csc.uvic.ca', browser => '*chrome', verbose => 1);
 $sel->start;
-$sel->open('/~labspg/Reference/javascript/script4.4.html');
+$sel->open('http://www.csc.uvic.ca/~labspg/Reference/javascript/script4.9.html');
 my $content = $sel->get_body_text;
-die "No happy: ($content)" unless $content =~ /Happiness/;
+
+$sel->select('name=order', 'a box of cereal');
+$sel->add_selection('name=order', '100 lbs. blueberries');
+print "$function=(", join(',', $sel->$function(@args)), ")\n";
+
+
+$sel->close;
