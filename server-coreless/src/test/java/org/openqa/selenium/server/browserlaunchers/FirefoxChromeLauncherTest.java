b93980fbf5c8717f2519eb6659b220e827544c84
Dan Fabulich <dan@fabulich.com>
2006-12-13 00:14:46 +0000

    Oops, fixing this test
    r3737

--- server-coreless/src/test/java/org/openqa/selenium/server/browserlaunchers/FirefoxChromeLauncherTest.java
+++ server-coreless/src/test/java/org/openqa/selenium/server/browserlaunchers/FirefoxChromeLauncherTest.java
@@ -6,7 +6,7 @@ public class FirefoxChromeLauncherTest extends TestCase {
     public void testShouldAbleToCreateChromeUrlWithNormalUrl() throws Exception {
         String httpUrl = "http://www.my.com/folder/endname.html?a=aaa&b=bbb";
         String chromeUrl = new FirefoxChromeLauncher.ChromeUrlConvert().convert(httpUrl, 4444);
-        assertEquals("chrome://src/content/endname.html?a=aaa&b=bbb&baseUrl=http://localhost:4444/selenium-server/",
+        assertEquals("chrome://src/content/endname.html?a=aaa&b=bbb",
                 chromeUrl);
     }
 }
