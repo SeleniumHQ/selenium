8f672ee7806210ddd34ead25ff6904949c6e4de6
Mike Williams <mdub@dogbiscuit.org>
2007-01-18 10:51:34 +0000

    Get the "Continue" button working again.
    r3791

--- code/javascript/core/scripts/selenium-testrunner.js
+++ code/javascript/core/scripts/selenium-testrunner.js
@@ -342,7 +342,7 @@ objectExtend(HtmlTestRunnerControlPanel.prototype, {
     },
 
     reset: function() {
-        // this.runInterval = this.speedController.value;
+        this.runInterval = this.speedController.value;
         this._switchContinueButtonToPause();
     },
 
