f1ae594a1ad34f078129f5abbcedf0b52e12eb90
Simon M Stewart <simon.m.stewart@gmail.com>
2007-03-25 10:45:30 +0000

    SimonStewart: Firefox driver now passes the test to do with following meta redirects
    r3877

--- firefox/src/extension/content/webLoadingListener.js
+++ firefox/src/extension/content/webLoadingListener.js
@@ -1,11 +1,34 @@
 function WebLoadingListener(toCall) {
     var listener = this;
 
-    this.func = function(event) {
+//    this.func = function(event) {
+//        document.getElementById("appcontent").removeEventListener("DOMContentLoaded", listener.func, true);
+//        toCall(event);
+//    }
+
+
+     this.func = function(event) {
+        // Is there a meta refresh?
+        var result = Utils.getDocument().evaluate("/html/head/meta[@http-equiv]", Utils.getDocument(), null, Components.interfaces.nsIDOMXPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+        var element = result.iterateNext();
+        while (element) {
+            if ("refresh" == element.getAttribute("http-equiv").toLowerCase()) {
+                var content = element.getAttribute("content");
+                if (content) {
+                    var bits = content.split(';');
+                    dump("First bit of content: " + bits[0] + "\n");
+                    if (bits[0] - 0 === 0) {
+                        // We have an instant refresh, so return
+                        return;
+                    }
+                }
+            }
+            element = result.iterateNext();
+        }
+
         document.getElementById("appcontent").removeEventListener("DOMContentLoaded", listener.func, true);
         toCall(event);
     }
-
     document.getElementById("appcontent").addEventListener("DOMContentLoaded", this.func, true);
 }
 
