6c0a97668de101934719c7c8f55fad779fc637b5
Dan Fabulich <dan@fabulich.com>
2007-07-03 20:29:47 +0000

    Don't tell people to e-mail selenium-dev@openqa.org, because non-subscribers can't send e-mail there.  Instead, just send them to the forums.
    r4017

--- src/main/resources/core/scripts/selenium-executionloop.js
+++ src/main/resources/core/scripts/selenium-executionloop.js
@@ -119,7 +119,7 @@ TestLoop.prototype = {
     _handleCommandError : function(e) {
         if (!e.isSeleniumError) {
             LOG.exception(e);
-            var msg = "Selenium failure. Please report to selenium-dev@openqa.org, with error details from the log window.";
+            var msg = "Selenium failure. Please report to the Selenium Users forum at http://forums.openqa.org, with error details from the log window.";
             if (e.message) {
                 msg += "  The error message is: " + e.message;
             }
