8957c58e20a2458da4591d28760463d09b892bcd
Dan Fabulich <dan@fabulich.com>
2007-08-17 06:55:54 +0000

    Stupid Maven bug; suites only get run if they extend TestCase
    r4184

--- clients/java/src/test/java/com/thoughtworks/selenium/ClientUnitTestSuite.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/ClientUnitTestSuite.java
@@ -1,9 +1,10 @@
 package com.thoughtworks.selenium;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class ClientUnitTestSuite {
+public class ClientUnitTestSuite extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite(ClientUnitTestSuite.class.getName());
         suite.addTestSuite(CSVTest.class);
