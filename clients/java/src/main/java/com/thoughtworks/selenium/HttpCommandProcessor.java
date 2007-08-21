fd22ff97356978ed5a8c468ba245bfeac99188b6
Dan Fabulich <dan@fabulich.com>
2007-08-21 21:58:34 +0000

    SRC-194, spurious warning when getString returns a blank string
    r4208

--- clients/java/src/main/java/com/thoughtworks/selenium/HttpCommandProcessor.java
+++ clients/java/src/main/java/com/thoughtworks/selenium/HttpCommandProcessor.java
@@ -152,10 +152,10 @@ public class HttpCommandProcessor implements CommandProcessor {
 
     public String getString(String commandName, String[] args) {
         String result = doCommand(commandName, args);
-        if (result.length() > "OK,".length()) {
+        if (result.length() >= "OK,".length()) {
             return result.substring("OK,".length());
         }
-        System.out.println("WARNING: getString(" + commandName + ") saw a bad result " + result);
+        System.err.println("WARNING: getString(" + commandName + ") saw a bad result " + result);
         return "";
     }
 
