42241caec5424971284908168170d3f656c7d72c
Dan Fabulich <dan@fabulich.com>
2007-09-05 05:37:31 +0000

    Add a little info about the stack when we throw SeleniumError on IE
    r4261

--- src/main/resources/core/scripts/htmlutils.js
+++ src/main/resources/core/scripts/htmlutils.js
@@ -743,6 +743,17 @@ function AssertionFailedError(message) {
 
 function SeleniumError(message) {
     var error = new Error(message);
+    if (typeof(arguments.caller) != 'undefined') { // IE, not ECMA
+        var result = '';
+        for (var a = arguments.caller; a != null; a = a.caller) {
+            result += '> ' + a.callee.toString() + '\n';
+            if (a.caller == a) {
+                result += '*';
+                break;
+            }
+        }
+        error.stack = result;
+    }
     error.isSeleniumError = true;
     return error;
 }
