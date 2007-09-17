2ec2337a609433d2b73762ee0a66753b0d22492a
Dan Fabulich <dan@fabulich.com>
2007-09-17 18:20:52 +0000

    Create subprocess with CREATE_BREAKAWAY_FROM_JOB so "killableprocess killableprocess notepad" works, and so killableprocess will work under a Vista PCA job.  Added lots of error handling.  Wait for all processes to die in a separate thread and wait for newline in the current thread, instead of vice-versa.
    r4336

--- killableprocess/killableprocess.cpp
+++ killableprocess/killableprocess.cpp
@@ -16,22 +16,68 @@
  */
 #include "stdafx.h"
 
+//Manual unit tests:
+//
+//1) killableprocess notepad
+//Hit enter, notepad closes, KP dies
+//2) killableprocess notepad
+//Hit Ctrl-C, notepad closes, KP dies
+//3) killableprocess notepad
+//Close notepad, KP dies
+//4) killableprocess cmd /k cmd
+//Watching in Task Manager, two cmds are launched by KP.  Hit enter, both die.
+//5) killableprocess notepad
+//Kill KP using Task Manager; notepad closes.  (Process Explorer should NOT be running at this time)
+//6) killableprocess killableprocess notepad
+//Hit Enter, notepad dies.  (Process Explorer should NOT be running at this time)
+
 static HANDLE hJob;
+
+
+void ErrorExit(LPTSTR functionName) {
+    DWORD lastError = GetLastError();
+    _tprintf(L"%s failed with error %d\n", functionName, lastError);
+    ExitProcess(lastError);
+}
 
 // This function will get called when someone tries to stop this process
 BOOL CtrlHandler(DWORD ctrlType)
 {
+	//printf("CtrlHandler\n");
     // kill the job containing our spawned child and its descendents
-    TerminateJobObject(hJob, 79);
+    if (!TerminateJobObject(hJob, 79)) {
+        ErrorExit(L"TerminateJobObject");
+    }
     ExitProcess(0L);
     return TRUE;
 }
 
-// DGF Commit suicide if user hands us a line in stdout
+// DGF Commit suicide when all processes in a job are dead
 DWORD WINAPI ThreadProc( LPVOID lpParam )
 {
-    _TCHAR line[128];
-    _getts_s(line, 128);
+    JOBOBJECT_BASIC_PROCESS_ID_LIST pidList = { 0 };
+    if (!QueryInformationJobObject(hJob, JobObjectBasicProcessIdList, &pidList, sizeof(pidList), NULL)) {
+        ErrorExit(L"Initial QueryInformationJobObject");
+    }
+	//printf("%d processes\n", pidList.NumberOfProcessIdsInList);
+    while (pidList.NumberOfProcessIdsInList > 0) {
+        DWORD pid = pidList.ProcessIdList[0];
+        HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, pid);
+        if (!hProcess) {
+            ErrorExit(L"OpenProcess");
+        }
+        DWORD waitResult = WaitForSingleObject(hProcess, INFINITE);
+        if (waitResult == WAIT_FAILED) {
+            ErrorExit(L"WaitForSingleObject");
+        }
+        if (!CloseHandle (hProcess)) {
+            ErrorExit(L"CloseHandle (hProcess)");
+        }
+        if (!QueryInformationJobObject(hJob, JobObjectBasicProcessIdList, &pidList, sizeof(pidList), NULL)) {
+            ErrorExit(L"QueryInformationJobObject");
+        }
+    }
+    //printf("all processes dead\n");
     CtrlHandler(0L);
     return 0;
 }
@@ -41,7 +87,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
     // arg[0] is killableprocess.exe, arg[1] should be the executable
     if (argc < 2) {
-        printf("You must specify at least one argument");
+        printf("You must specify at least one argument\n");
         return 1;
     }
 
@@ -82,12 +128,18 @@ int _tmain(int argc, _TCHAR* argv[])
     ZeroMemory( &jeli, sizeof(jeli) );
 
     hJob = CreateJobObject(NULL, NULL);
+    if (!hJob) {
+        ErrorExit(L"CreateJobObject");
+    }
 
     // Hopefully, Windows will kill the job automatically if this process dies
     // But beware!  Process Explorer can break this by keeping open a handle to all jobs!
     // http://forum.sysinternals.com/forum_posts.asp?TID=4094
-    jeli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE;
-	SetInformationJobObject(hJob, JobObjectExtendedLimitInformation, &jeli, sizeof(jeli));
+    jeli.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+        | JOB_OBJECT_LIMIT_BREAKAWAY_OK; // so we can nest multiple killableprocesses if necessary
+    if (!SetInformationJobObject(hJob, JobObjectExtendedLimitInformation, &jeli, sizeof(jeli))) {
+        ErrorExit(L"SetinformationJobObject");
+    }
 
     // Start the child process. 
     if( !CreateProcess( NULL,   // No module name (use command line). 
@@ -95,23 +147,35 @@ int _tmain(int argc, _TCHAR* argv[])
         NULL,             // Process handle not inheritable. 
         NULL,             // Thread handle not inheritable. 
         FALSE,            // Set handle inheritance to FALSE. 
-        CREATE_SUSPENDED,                // Suspend so we can add to job
+        CREATE_SUSPENDED  // Suspend so we can add to job
+        |CREATE_BREAKAWAY_FROM_JOB, // Allow ourselves to breakaway from Vista's PCA if necessary
         NULL,             // Use parent's environment block. 
         NULL,             // Use parent's starting directory. 
         &si,              // Pointer to STARTUPINFO structure.
         &pi )             // Pointer to PROCESS_INFORMATION structure.
     ) 
     {
-        printf( "CreateProcess failed (%d).\n", GetLastError() );
-        return 1;
+        ErrorExit(L"CreateProcess");
     }
-    AssignProcessToJobObject(hJob, pi.hProcess);
-    ResumeThread(pi.hThread);
+    if (!AssignProcessToJobObject(hJob, pi.hProcess)) {
+        ErrorExit(L"AssignProcessToJobObject");
+    }
+    if (!ResumeThread(pi.hThread)) {
+        ErrorExit(L"ResumeThread");
+    }
     // Close process and thread handles. 
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
+    
+    if (!CloseHandle( pi.hThread )) {
+        ErrorExit(L"CloseHandle(pi.hThread)");
+    }
+    if (!CloseHandle( pi.hProcess )) {
+        ErrorExit(L"CloseHandle(pi.hProcess)");
+    }
+    
     // Handle Ctrl-C
-    SetConsoleCtrlHandler((PHANDLER_ROUTINE) CtrlHandler, TRUE);
+    if (!SetConsoleCtrlHandler((PHANDLER_ROUTINE) CtrlHandler, TRUE)) {
+        ErrorExit(L"SetConsoleCtrlHandler");
+    }
     
     // Create a thread to commit suicide on newline
     HANDLE hThread = CreateThread( 
@@ -121,19 +185,20 @@ int _tmain(int argc, _TCHAR* argv[])
             NULL,             // argument to thread function 
             0,                 // use default creation flags 
             NULL);   // returns the thread identifier 
-    CloseHandle(hThread);
-    
-    // Wait until all processes in the job are dead
-    JOBOBJECT_BASIC_PROCESS_ID_LIST pidList = { 0 };
-    QueryInformationJobObject(hJob, JobObjectBasicProcessIdList, &pidList, sizeof(pidList), NULL);
-    while (pidList.NumberOfProcessIdsInList > 0) {
-        DWORD pid = pidList.ProcessIdList[0];
-        HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, pid);
-        WaitForSingleObject(hProcess, INFINITE);
-        CloseHandle (hProcess);
-        QueryInformationJobObject(hJob, JobObjectBasicProcessIdList, &pidList, sizeof(pidList), NULL);
+    if (!hThread) {
+        ErrorExit(L"CreateThread");
+    }
+    if (!CloseHandle(hThread)) {
+        ErrorExit(L"CloseHandle(hThread)");
     }
 
+    // await a newline; if we get one, kill ourselves
+    char line[128];
+    gets_s(line, 127);
+    //printf("newline\n");
 
+    if (!TerminateJobObject(hJob, 666)) {
+        ErrorExit(L"TerminateJobObject");
+    }
     return 0;
 }
