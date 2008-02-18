4c4019ac36d5f59bee41ed549b9748c20f60f081
Jiayao Yu <Jiayao.Yu@test.com>
2008-02-18 13:13:57 +0000

    JiayaoYu: Changed hard coded i18n strings to externalized string
    r4803

--- common/test/java/com/googlecode/webdriver/I18nTest.java
+++ common/test/java/com/googlecode/webdriver/I18nTest.java
@@ -6,6 +6,6 @@ public class I18nTest extends AbstractDriverTestCase {
   @Ignore("ie, safari, htmlunit, firefox")
   public void testCn() {
       driver.get(chinesePage);
-      driver.findElement(By.linkText("中国之声")).click();
+      driver.findElement(By.linkText(Messages.getString("I18nTest.link1"))).click(); 
   }
 }
