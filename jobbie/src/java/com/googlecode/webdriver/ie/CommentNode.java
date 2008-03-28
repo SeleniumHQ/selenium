5bfd84aaecdaa627f4c0fcb1ccaebfe9e9ef6493
Simon M Stewart <simon.m.stewart@gmail.com>
2008-03-28 18:41:01 +0000

    SimonStewart: Adding a missing file
    r5005

---
+++ jobbie/src/java/com/googlecode/webdriver/ie/CommentNode.java
@@ -0,0 +1,11 @@
+package com.googlecode.webdriver.ie;
+
+public class CommentNode extends AbstractNode {
+	public CommentNode(long nodePointer) {
+		super(nodePointer);
+	}
+
+	public AttributeNode getFirstAttribute() {
+		return null;
+	}
+}
