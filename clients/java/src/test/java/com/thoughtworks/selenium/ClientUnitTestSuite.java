0538e9167ac8001fc640daecbc842774810b28a6
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-04-03 06:41:18 +0000

    SRC-416: DefaultSelenium.start() should not swallow exceptions
    r5032

--- clients/java/src/test/java/com/thoughtworks/selenium/ClientUnitTestSuite.java
+++ clients/java/src/test/java/com/thoughtworks/selenium/ClientUnitTestSuite.java
@@ -5,10 +5,12 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class ClientUnitTestSuite extends TestCase {
+
     public static Test suite() {
         TestSuite suite = new TestSuite(ClientUnitTestSuite.class.getName());
         suite.addTestSuite(CSVTest.class);
         suite.addTestSuite(WaitTest.class);
+        suite.addTestSuite(DefaultSeleniumStartErrorHandlingTest.class);
         return suite;
     }
 
