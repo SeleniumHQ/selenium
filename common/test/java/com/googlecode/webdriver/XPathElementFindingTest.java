e210fc236c1c9348b36c0c0fcc58e62698601231
Simon M Stewart <simon.m.stewart@gmail.com>
2008-04-11 13:02:56 +0000

    SimonStewart: Fixing broken build.
    r5095

--- common/test/java/com/googlecode/webdriver/XPathElementFindingTest.java
+++ common/test/java/com/googlecode/webdriver/XPathElementFindingTest.java
@@ -3,18 +3,7 @@ package com.googlecode.webdriver;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import java.io.FileInputStream;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.jaxen.JaxenException;
-import org.jaxen.Navigator;
-import org.jaxen.XPath;
-import org.jaxen.dom.DOMXPath;
-import org.w3c.dom.Node;
-import org.xml.sax.InputSource;
 
 public class XPathElementFindingTest extends AbstractDriverTestCase {
     public void testShouldThrowAnExceptionWhenThereIsNoLinkToClickAndItIsFoundWithXPath() {
@@ -70,27 +59,7 @@ public class XPathElementFindingTest extends AbstractDriverTestCase {
     }
     
     public void testShouldBeAbleToSearchForMultipleAttributes() throws Exception {
-//    	DocumentBuilderFactory factory 
-//        = DocumentBuilderFactory.newInstance();
-//       factory.setNamespaceAware(true);   
-//       DocumentBuilder builder = factory.newDocumentBuilder();    
-//       
-//       InputSource data = new InputSource(new FileInputStream("/work/webdriver/common/src/web/formPage.html"));
-//       Node doc = builder.parse(data);
-//       
-//       // There are different XPath classes in different packages
-//       // for the different APIs Jaxen supports
-//       XPath expression = new DOMXPath("//input[@type = 'submit' and @value = 'Click!']");
-////       Navigator navigator = expression.getNavigator();
-//    	
-//       Object singleNode = expression.selectSingleNode(doc);
-//       assertNotNull(singleNode);
-//       System.out.println(((Node) singleNode).getNodeValue());
-//       
     	driver.get(formPage);
-    	
-//    	driver.findElement(By.xpath("//form[@name='optional']"));
-//    	driver.findElement(By.xpath("//form[@name='optional']/input[@type='submit']"));
     	
     	try {
     		
