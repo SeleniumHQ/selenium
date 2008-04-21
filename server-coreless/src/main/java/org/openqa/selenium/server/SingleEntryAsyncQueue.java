834b86f381c6f67354e870e5d6e4ce61c1053c15
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-04-21 21:24:41 +0000

    Investigating Polling Deadlock: Test refactoring for SingleEntryAsyncQueue
    r5131

--- server-coreless/src/main/java/org/openqa/selenium/server/SingleEntryAsyncQueue.java
+++ server-coreless/src/main/java/org/openqa/selenium/server/SingleEntryAsyncQueue.java
@@ -26,7 +26,7 @@ import org.mortbay.log.LogFactory;
 
 /**
  * <p>Holds the command to be next run in the browser</p>
- *
+ * <p/>
  * This class uses reentrant locks in order to allow the same
  * thread to populate the queue as is waiting for it, which is
  * what currently happens on during browser startup.
@@ -37,89 +37,85 @@ import org.mortbay.log.LogFactory;
 public class SingleEntryAsyncQueue<T> {
     private static Log log = LogFactory.getLog(SingleEntryAsyncQueue.class);
     private final AtomicReference<T> poisonData;
-	private final int timeoutInSeconds;
+    private final int timeoutInSeconds;
     private final ArrayBlockingQueue<T> holder;
 
-	public SingleEntryAsyncQueue(int timeoutInSecs) {
-	  timeoutInSeconds = timeoutInSecs;
-	  holder = new ArrayBlockingQueue<T>(1);
-	  poisonData = new AtomicReference<T>();
-	}
-	
-	public int getTimeoutInSeconds() {
-	  return timeoutInSeconds;
-	}
-	
-	protected void setPoison(T poisonInstance) {
-	  poisonData.set(poisonInstance);
-	}
-	
-	protected boolean isPoison(T poisonSample) {
-	  boolean result = false;
-	  T poison = poisonData.get();
-	  if (null != poison && poison.equals(poisonSample)) {
-	    result = true;
-	  }
-	  return result;
-	}
+    public SingleEntryAsyncQueue(int timeoutInSecs) {
+        timeoutInSeconds = timeoutInSecs;
+        holder = new ArrayBlockingQueue<T>(1);
+        poisonData = new AtomicReference<T>();
+    }
 
-	protected T pollToGetContentUntilTimeout() {
-	  T result = holder.poll(); // in case it's already there
-	  if (null != result) {
-	    log.debug("data was waiting: " + result);
-	  }
-	  
-	  if (timeoutInSeconds > 0 && null == result) {
-	    long now = System.currentTimeMillis();
-		long deadline = now + (timeoutInSeconds * 1000L);
-		while (now < deadline) {
-    	  try {
-    	    log.debug("waiting for data for at most " + (deadline - now) + " more ms");
-    	    result = holder.poll(deadline - now, TimeUnit.MILLISECONDS);
-    	    log.debug("data from polling: " + result);
-    		now = deadline;
-    	  } catch (InterruptedException ie) {
-    	    now = System.currentTimeMillis();
-    	    log.debug("was interrupted; resuming wait");
-    	  }
-		}
-	  }
-	  return result;
-	}
+    public int getTimeoutInSeconds() {
+        return timeoutInSeconds;
+    }
 
-	protected boolean putContent(T thing) {
-      log.debug("putting command: " + thing);
-      boolean res = holder.offer(thing);
-      log.debug("..command put?: " + res);
-      return res;
-	}
+    protected void setPoison(T poisonInstance) {
+        poisonData.set(poisonInstance);
+    }
+
+    protected boolean isPoison(T poisonSample) {
+        boolean result = false;
+        T poison = poisonData.get();
+        if (null != poison && poison.equals(poisonSample)) {
+            result = true;
+        }
+        return result;
+    }
+
+    protected T pollToGetContentUntilTimeout() {
+        T result = holder.poll(); // in case it's already there
+        if (null != result) {
+            log.debug("data was waiting: " + result);
+        }
+
+        if (timeoutInSeconds > 0 && null == result) {
+            long now = System.currentTimeMillis();
+            long deadline = now + (timeoutInSeconds * 1000L);
+            while (now < deadline) {
+                try {
+                    log.debug("waiting for data for at most " + (deadline - now) + " more ms");
+                    result = holder.poll(deadline - now, TimeUnit.MILLISECONDS);
+                    log.debug("data from polling: " + result);
+                    now = deadline;
+                } catch (InterruptedException ie) {
+                    now = System.currentTimeMillis();
+                    log.debug("was interrupted; resuming wait");
+                }
+            }
+        }
+        return result;
+    }
+
+    protected boolean putContent(T thing) {
+        log.debug("putting command: " + thing);
+        boolean res = holder.offer(thing);
+        log.debug("..command put?: " + res);
+        return res;
+    }
 
     protected boolean isEmpty() {
-      return (0 == holder.size());
+        return (0 == holder.size());
     }
 
     protected T peek() {
-      return holder.peek();
+        return holder.peek();
     }
-    
+
     /**
-     * Clears the contents of the holder (if any) and also 
+     * Clears the contents of the holder (if any) and also
      * feeds 'poison' data a pending listener (if any);
-     * 
+     *
      * @return true if poison was set and sent to any listeners.
      */
     protected boolean poisonPollers() {
-      boolean result = true;
-      if (null != poisonData.get()) {
-        
+        if (null == poisonData.get()) {
+            holder.clear();
+            return false;
+        }
         // offer poison content.  If something is already there,
         // then the next listener will already have something to get.
         putContent(poisonData.get());
-        
-      } else {
-        result = false;
-        holder.clear();
-      }
-      return result;
+        return true;
     }
 }
--- server-coreless/src/test/java/org/openqa/selenium/server/SingleEntryAsyncQueueUnitTest.java
+++ server-coreless/src/test/java/org/openqa/selenium/server/SingleEntryAsyncQueueUnitTest.java
@@ -14,184 +14,199 @@ import java.util.logging.Logger;
 
 public class SingleEntryAsyncQueueUnitTest extends TestCase {
 
-  private static Log log = LogFactory.getLog(SingleEntryAsyncQueueUnitTest.class);
+    private static final Log logger = LogFactory.getLog(SingleEntryAsyncQueueUnitTest.class);
 
-  private static final String testCommand = "testCommand";
-  private static final String completeCommand = "testComplete";
-  private static final String poisonString = "POISON";
-  private static final int timeout = 3;
-  
-  private SingleEntryAsyncQueue<String> holder;
-  
-  @Override
-  public void setUp() throws Exception {
-    configureLogging();
-    holder = new SingleEntryAsyncQueue<String>(timeout);
-    holder.setPoison(poisonString);
-    log.info("Start test: " + getName());
-  }
-  
-  private void configureLogging() throws Exception {
-      SeleniumServer.setDebugMode(true);
-      SeleniumServer.configureLogging(new RemoteControlConfiguration());
-      Logger logger = Logger.getLogger("");
-      for (Handler handler : logger.getHandlers()) {
-          if (handler instanceof StdOutHandler) {
-              handler.setFormatter(new TerseFormatter(true));
-              break;
-          }
-      }
-  }
-  
-  @Override
-  public void tearDown() throws Exception {
-      SeleniumServer.setDebugMode(false);
-      SeleniumServer.configureLogging(new RemoteControlConfiguration());
-  }
+    private static final String testCommand = "testCommand";
+    private static final String completeCommand = "testComplete";
+    private static final String poisonString = "POISON";
+    private static final int timeout = 3;
+    private static final int MILLISECONDS = 1000;
 
-  public void testAssertStartingState() {
-    assertNull(holder.peek());
-  }
-  
-  public void testPollReturnsPoisonWhenPoisoned() throws Throwable {
-	  TrackableThread getter = launchGetter();
-	  holder.poisonPollers();
-	  expectContent(getter, poisonString);
-  }
+    private SingleEntryAsyncQueue<String> queue;
 
-	public void testPollReturnsNullAfterTimeout() {
-		long now = System.currentTimeMillis();
-		String nextRes = holder.pollToGetContentUntilTimeout();
-		long after = System.currentTimeMillis();
-		assertNull(nextRes);
-		long diff = after - now;
-		long expected = timeout*999;
-		assertTrue("diff was too short; was <" + diff +
-				"> expected at least <" + expected +
-				">", after - now >= expected); // at least retry seconds
-	}
-
-	public void testPutContentPuts() {
-		assertTrue(holder.isEmpty());
-    boolean res = holder.putContent(testCommand);
-		assertTrue(res);
-		assertFalse(holder.isEmpty());
-	}
-
-	public void testPutContentTwiceFails() {
-  	  assertTrue(holder.isEmpty());
-      boolean res = holder.putContent(testCommand);
-      assertTrue(res);
-      assertFalse(holder.isEmpty());
-      res = holder.putContent(completeCommand);
-      assertFalse(res);
-      assertEquals(testCommand, holder.peek());
-	}
-
-  public void testSimpleSingleThreaded() throws Throwable {
-    injectContent(testCommand, true);
-    expectContent(testCommand);
-    assertTrue(holder.isEmpty()); // command got picked up.
-  }
-
-  public void testRealSimple() throws Throwable {
-    TrackableThread sender = launchSender(testCommand);
-    assertNotNull(sender);
-    expectContent(testCommand);
-    assertNull(holder.peek());
-  }
-
-	/** Note that you can't do listener first in the same thread */
-  public void testListenerFirst() throws Throwable {
-    // try to get "something" in a different thread
-    TrackableThread getter = launchGetter();
-		// now send something in this thread
-		injectContent(testCommand, true);
-		// can't do the expect until after we send the command; it blocks in this thread
-    expectContent(getter, testCommand);
-		assertNull(holder.peek());
-  }
-
-  public void testTwoRoundsSingleThreaded() throws Throwable {
-    testSimpleSingleThreaded();
-    injectContent(completeCommand, true);
-    expectContent(completeCommand);
-  }
-
-  public void testRealTwoRounds() throws Throwable {
-    // send "something"
-    TrackableThread sender = launchSender(testCommand);
-    assertNotNull(sender);
-    // try to get "something"
-    TrackableThread getter = launchGetter();
-    expectContent(getter, testCommand);
-
-    // send a final "testComplete" in the current foreground thread
-		injectContent(completeCommand, true);
-		// create a new getter
-    TrackableThread getter2 = launchGetter();
-    expectContent(getter2, completeCommand);
-  }
-
-  private void injectContent(String content, boolean expected) throws Throwable {
-		boolean actual = holder.putContent(content);
-		assertEquals(content + "content got sent", expected, actual);
-  }
-
-  private void expectContent(TrackableThread commandGetter, String expected) throws Throwable {
-    String actual = (String) commandGetter.getResult();
-    assertEquals(expected + " content retrieved", expected, actual);
-  }
-
-  private void expectContent(String expected) throws Throwable {
-    String actual = holder.pollToGetContentUntilTimeout();
-    assertEquals(expected + " content retrieved", expected, actual);
-  }
-
-  /** start a thread to put the next command */
-  private TrackableThread launchSender(String content) {
-    String name = "launching sender";
-    TrackableThread t = new TrackableThread(new AsyncCommandSender(content), name);
-    t.start();
-    return t;
-  }
-
-  /** start a thread to wait for the next command */
-  private TrackableThread launchGetter() {
-    String name = "launching getter";
-    TrackableThread t = new TrackableThread(new AsyncCommandGetter(), name);
-    t.start();
-    return t;
-  }
-
-  /** Passes the specified command to command holder */
-  private class AsyncCommandSender extends TrackableRunnable {
-	private String content;
-    
-    public AsyncCommandSender(String content) {
-		this.content = content;
-    }
-    
     @Override
-    public Object go() throws Throwable {
-        boolean result = holder.putContent(content);
-        log.debug(Thread.currentThread().getName() + " got result: " + result);
-        return new Boolean(result);
+    public void setUp() throws Exception {
+        configureLogging();
+        queue = new SingleEntryAsyncQueue<String>(timeout);
+        logger.info("Start test: " + getName());
     }
-  }
 
-  /** Gets the command from the command holder */
-  private class AsyncCommandGetter extends TrackableRunnable {
-    
-    public AsyncCommandGetter() {
-    }
-    
     @Override
-    public Object go() throws Throwable {
-        String result = holder.pollToGetContentUntilTimeout();
-        log.debug(Thread.currentThread().getName() + " got result: " + result);
-        return result;
+    public void tearDown() throws Exception {
+        SeleniumServer.setDebugMode(false);
+        SeleniumServer.configureLogging(new RemoteControlConfiguration());
     }
-  }
-  
+
+    public void testPeekReturnsNullWhenEmpty() {
+        assertNull(queue.peek());
+    }
+
+    public void testPollReturnsNullAfterTimeout() {
+        final String nextRes;
+
+        nextRes = queue.pollToGetContentUntilTimeout();
+        assertNull(nextRes);
+    }
+
+    public void testPollDoesNotReturnBeforeTheTimeoutWhenTimingOut() {
+        final long duration;
+        final long after;
+        final long now;
+
+        now = System.currentTimeMillis();
+        queue.pollToGetContentUntilTimeout();
+        after = System.currentTimeMillis();
+        duration = after - now;
+        assertTrue("Returned too fast : " + duration + " ms", duration >= timeout * MILLISECONDS);
+    }
+
+    public void testPollReturnAtLeastWithinTwiceTheTimeoutValueWhenTimingOut() {
+        final long duration;
+        final long after;
+        final long now;
+
+        now = System.currentTimeMillis();
+        queue.pollToGetContentUntilTimeout();
+        after = System.currentTimeMillis();
+        duration = after - now;
+        assertTrue("Duration more than twice the timeout: " + duration + " ms",
+                duration / MILLISECONDS <= 2 * timeout);
+    }
+
+    public void testQueueIsEmptyWhenCreated() {
+        assertTrue(queue.isEmpty());
+    }
+
+    public void testQueueNotEmptyWhenContentIsPut() {
+        assertTrue(queue.putContent(testCommand));
+        assertFalse(queue.isEmpty());
+    }
+
+    public void testPutContentReturnsFalseWhenPuttingTwice() {
+        queue.putContent(testCommand);
+        assertFalse(queue.putContent(completeCommand));
+    }
+
+    public void testPutContentReturnsPreviousCommandWhenPuttingTwice() {
+        queue.putContent(testCommand);
+        queue.putContent(completeCommand);
+        assertEquals(testCommand, queue.peek());
+    }
+
+    public void testPollingContentReturnsCommandPreviouslyPut() throws Throwable {
+        queue.putContent(testCommand);
+        assertEquals(testCommand, queue.pollToGetContentUntilTimeout());
+    }
+
+    public void testCommandGotPickedUpAndQueueIsEmptyWhenPollingContentReturns() throws Throwable {
+        queue.putContent(testCommand);
+        queue.pollToGetContentUntilTimeout();
+        assertTrue(queue.isEmpty());
+    }
+
+    public void testCanPollTwice() throws Throwable {
+        queue.putContent(testCommand);
+        assertEquals(testCommand, queue.pollToGetContentUntilTimeout());
+        assertTrue(queue.isEmpty());
+
+        queue.putContent(completeCommand);
+        assertEquals(completeCommand, queue.pollToGetContentUntilTimeout());
+        assertTrue(queue.isEmpty());
+    }
+
+    public void testCanPollContentThatWhatPutByADifferentThread() throws Throwable {
+        new Thread(new AsyncCommandSender(testCommand), "launching sender").start();
+        assertEquals(testCommand, queue.pollToGetContentUntilTimeout());
+        assertNull(queue.peek());
+    }
+
+    public void testCanGetResultPostedByTheMainThreadFromAnotherThread() throws Throwable {
+        final TrackableThread getter;
+
+        /*
+         * Note that you can't do listener first in the same thread
+         */
+        getter = new TrackableThread(new AsyncCommandGetter(), "launching getter");
+        getter.start();
+        assertTrue(queue.putContent(testCommand));
+        assertEquals(testCommand, getter.getResult());
+        assertNull(queue.peek());
+    }
+
+    public void testCanPutAndGetResultsFromDifferentThreads() throws Throwable {
+        final TrackableThread firstGetter;
+        final TrackableThread secondGetter;
+
+        new TrackableThread(new AsyncCommandSender(testCommand), "launching sender").start();
+        firstGetter = new TrackableThread(new AsyncCommandGetter(), "launching firstGetter");
+        firstGetter.start();
+        assertEquals(testCommand, firstGetter.getResult());
+
+        assertTrue(queue.putContent(completeCommand));
+        secondGetter = new TrackableThread(new AsyncCommandGetter(), "launching firstGetter");
+        secondGetter.start();
+        assertEquals(completeCommand, secondGetter.getResult());
+    }
+
+    public void testPollReturnsPoisonOncePoisonedAndPoisonPollersIsCalled() throws Throwable {
+        queue.setPoison(poisonString);        
+        TrackableThread getter = new TrackableThread(new AsyncCommandGetter(), "launching getter");
+        getter.start();
+        assertTrue(queue.poisonPollers());
+        assertEquals(poisonString, getter.getResult());
+    }
+
+    public void testPoisonPollersClearContentWhenThereIsNoPoison() throws Throwable {
+        queue.putContent("some command");
+        assertFalse(queue.poisonPollers());
+        assertTrue(queue.isEmpty());
+    }
+
+
+    /**
+     * Passes the specified command to command holder
+     */
+    private class AsyncCommandSender extends TrackableRunnable {
+        private String content;
+
+        public AsyncCommandSender(String content) {
+            this.content = content;
+        }
+
+        @Override
+        public Object go() throws Throwable {
+            boolean result = queue.putContent(content);
+            logger.debug(Thread.currentThread().getName() + " got result: " + result);
+            return new Boolean(result);
+        }
+    }
+
+    /**
+     * Gets the command from the command holder
+     */
+    private class AsyncCommandGetter extends TrackableRunnable {
+
+        public AsyncCommandGetter() {
+        }
+
+        @Override
+        public Object go() throws Throwable {
+            String result = queue.pollToGetContentUntilTimeout();
+            logger.debug(Thread.currentThread().getName() + " got result: " + result);
+            return result;
+        }
+    }
+
+    private void configureLogging() throws Exception {
+        SeleniumServer.setDebugMode(true);
+        SeleniumServer.configureLogging(new RemoteControlConfiguration());
+        Logger logger = Logger.getLogger("");
+        for (Handler handler : logger.getHandlers()) {
+            if (handler instanceof StdOutHandler) {
+                handler.setFormatter(new TerseFormatter(true));
+                break;
+            }
+        }
+    }
+
 }
