4af153b2a5229a590d1100bee4d7af03c4f42db6
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-04-30 22:41:59 +0000

    Test: Fixing DollarRentACar
    r5152

--- tests/src/test/java/org/openqa/selenium/DollarRentACarTest.java
+++ tests/src/test/java/org/openqa/selenium/DollarRentACarTest.java
@@ -4,17 +4,18 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 
 public class DollarRentACarTest extends AbstractTest {
+    public static String TIMEOUT = "120000";
     @Test
     public void searchAndRent() throws InterruptedException {
         selenium.open("http://www.dollar.com/default.aspx?AspxAutoDetectCookieSupport=1");
         selenium.type("ctl05_ctl01_ResStartColumnLayout_LocationTime_PickupLocationTextBox", "PDX");
-        selenium.select("ctl05_ctl01_ResStartColumnLayout_LocationTime_CarTypeDropDownList", "label=Intermediate");
+        selenium.select("ctl05_ctl01_ResStartColumnLayout_LocationTime_CarTypeDropDownList", "label=Compact");
         selenium.click("ctl05_ctl01_ResStartColumnLayout_LocationTime_GetRatesButton");
-        selenium.waitForPageToLoad("30000");
-        selenium.click("ctl05_ctl03_VehicleInformationColumnLayout_GoButton");
-        selenium.waitForPageToLoad("30000");
-        selenium.select("ctl05_ctl03_PersonalInformationColumnLayout_OptionsColumnLayout_LoyaltyProgramList", "label=Virgin Atlantic Flying Club");
-        selenium.click("ctl05_ctl03_PersonalInformationColumnLayout_ReserveNowSitecoreImageButton");
+        selenium.waitForPageToLoad(TIMEOUT);
+        selenium.click("ctl04_ctl03_VehicleInformationColumnLayout_GoButton");
+        selenium.waitForPageToLoad(TIMEOUT);
+        selenium.select("ctl04_ctl03_PersonalInformationColumnLayout_OptionsColumnLayout_LoyaltyProgramList", "label=Virgin Atlantic Flying Club");
+        selenium.click("ctl04_ctl03_PersonalInformationColumnLayout_ReserveNowSitecoreImageButton");
         for (int second = 0; ; second++) {
             if (second >= 60) fail("timeout");
             try {
