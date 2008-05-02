86f04552731dbeeef2c46a4f1d78b531b30020f9
Paul Hammant <PaulHammant@gmail.com>
2008-05-02 14:48:38 +0000

    rename to be simpler again
    r5159

--- sample/src/ExampleSeleniumTest.java
+++ sample/src/ExampleSeleniumTest.java
@@ -10,12 +10,28 @@ public class ExampleSeleniumTest extends TestCase {
 	        browser.start();
 	    }
 
-	    public void testGoogle() {
+	    public void testGoogle() throws InterruptedException {
 	        browser.open("/webhp?hl=en");
-	        browser.type("q", "hello world");
+	        browser.type("name=q", "hello world");
 	        browser.click("btnG");
 	        browser.waitForPageToLoad("5000");
 	        assertEquals("hello world - Google Search", browser.getTitle());
+	        browser.type("name=q", "two");
+	        browser.click("btnG");
+	        browser.type("name=q", "three");
+	        browser.click("btnG");
+	        browser.type("name=q", "four");
+	        browser.click("btnG");
+	        browser.type("name=q", "five");
+	        browser.click("btnG");
+	        browser.type("name=q", "six");
+	        browser.click("btnG");
+	        browser.type("name=q", "seven");
+	        browser.click("btnG");
+	
+			Thread.sleep(10 * 1000);
+
+	
 	    }
 
 	    public void tearDown() {
