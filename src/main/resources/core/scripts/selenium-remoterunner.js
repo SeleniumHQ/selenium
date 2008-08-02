57ca32139d914dc7096df25ecec011c557b28c89
Paul Hammant <PaulHammant@gmail.com>
2008-08-02 21:30:03 +0000

    eight command in history now, no need to flush on open()
    r5492

--- src/main/resources/core/scripts/selenium-remoterunner.js
+++ src/main/resources/core/scripts/selenium-remoterunner.js
@@ -29,6 +29,14 @@ var relayToRC = null;
 var proxyInjectionMode = false;
 var uniqueId = 'sel_' + Math.round(100000 * Math.random());
 var seleniumSequenceNumber = 0;
+var cmd8 = "";
+var cmd7 = "";
+var cmd6 = "";
+var cmd5 = "";
+var cmd4 = "";
+var cmd3 = "";
+var cmd2 = "";
+var cmd1 = "";
 
 var RemoteRunnerOptions = classCreate();
 objectExtend(RemoteRunnerOptions.prototype, URLConfiguration.prototype);
@@ -207,13 +215,16 @@ objectExtend(RemoteRunner.prototype, {
         } else {
             cmdText += ")\n";
         }
+        cmd8 = cmd7;
+        cmd7 = cmd6;
+        cmd6 = cmd5;
+        cmd5 = cmd4;
+        cmd4 = cmd3;
+        cmd3 = cmd2;
+        cmd2 = cmd1;
+        cmd1 = cmdText;
         var commandList = document.commands.commandList;
-        // DGF flush commandList on "open"
-        if (command.command == "open") {
-            commandList.value = cmdText;
-        } else {
-            commandList.value += cmdText;
-        }
+        commandList.value = cmd8 + cmd7 + cmd6 + cmd5 + cmd4 + cmd3 + cmd2 + cmd1;
         commandList.scrollTop = commandList.scrollHeight;
 
     },
