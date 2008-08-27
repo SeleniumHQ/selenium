3630db7171dd165cb231b315133348335f32868a
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-08-27 06:13:29 +0000

    Ruby Client: Make it easy to just require the Rake tasks
    r5564

--- clients/ruby/lib/selenium/rake/tasks.rb
+++ clients/ruby/lib/selenium/rake/tasks.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../nautilus/shell')
+require File.expand_path(File.dirname(__FILE__) + '/../../tcp_socket_extension')
 require File.expand_path(File.dirname(__FILE__) + '/../remote_control/remote_control')
 require File.expand_path(File.dirname(__FILE__) + '/remote_control_start_task')
 require File.expand_path(File.dirname(__FILE__) + '/remote_control_stop_task')
