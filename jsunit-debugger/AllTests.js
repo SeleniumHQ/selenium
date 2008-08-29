2713698cb6dc4e93b9670a130443f0a5efa7b5cf
Dan Fabulich <dan@fabulich.com>
2008-08-29 19:24:29 +0000

    PageBotAccessorTest
    r5587

--- jsunit-debugger/AllTests.js
+++ jsunit-debugger/AllTests.js
@@ -27,9 +27,10 @@ load("src/test/resources/CommandFactoryTest.js");
 load("src/test/resources/CommandHandlerTest.js");
 load("src/test/resources/ConfirmHandlingTest.js");
 load("src/test/resources/ErrorCheckingCommandTest.js");
-load("src/test/resources/PatternMatcherTest.js");*/
+load("src/test/resources/PatternMatcherTest.js");
 load("src/test/resources/AssertTest.js");
-load("src/test/resources/OptionLocatorTest.js");
+load("src/test/resources/OptionLocatorTest.js");*/
+load("src/test/resources/PageBotAccessorTest.js");
 load("src/test/resources/SampleTest.js");
 
 var stringWriter = new StringWriter();
