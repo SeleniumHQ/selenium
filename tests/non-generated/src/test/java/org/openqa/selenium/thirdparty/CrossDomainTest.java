992d87a588832827378ddaa9492de1a47cca8cff
Dan Fabulich <dan@fabulich.com>
2008-09-06 23:19:54 +0000

    refactor non-generated tests to use SeleneseTestNgHelper
    r5642

--- tests/non-generated/src/test/java/org/openqa/selenium/thirdparty/CrossDomainTest.java
+++ tests/non-generated/src/test/java/org/openqa/selenium/thirdparty/CrossDomainTest.java
@@ -1,9 +1,7 @@
 package org.openqa.selenium.thirdparty;
 
-import org.testng.annotations.Test;
-import org.testng.SkipException;
 import org.openqa.selenium.AbstractTest;
-import org.openqa.selenium.utils.TestReporter;
+import org.testng.annotations.Test;
 
 public class CrossDomainTest extends AbstractTest {
     @Test
