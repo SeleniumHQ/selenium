2af9009e7fc4c3a57869f6fd542cd3bd8270fd96
Dan Fabulich <dan@fabulich.com>
2008-09-13 21:03:44 +0000

    attributes can be ints
    r5674

--- tests/generated/src/test/java/com/thoughtworks/selenium/TestSeleniumServerLauncher.java
+++ tests/generated/src/test/java/com/thoughtworks/selenium/TestSeleniumServerLauncher.java
@@ -3,6 +3,7 @@ package com.thoughtworks.selenium;
 import org.openqa.selenium.server.RemoteControlConfiguration;
 import org.openqa.selenium.server.SeleniumServer;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -11,10 +12,10 @@ public class TestSeleniumServerLauncher {
     SeleniumServer server;
     @BeforeSuite
     @Parameters({"selenium.host", "selenium.port"})
-    public void startServer(@Optional("localhost") String host, @Optional("4444") String port) throws Exception {
+    public void startServer(@Optional("localhost") String host, @Optional("4444") int port) throws Exception {
         if (!"localhost".equals(host)) return;
         RemoteControlConfiguration config = new RemoteControlConfiguration();
-        config.setPort(Integer.parseInt(port));
+        config.setPort(port);
         server = new SeleniumServer(config);
         server.start();
     }
