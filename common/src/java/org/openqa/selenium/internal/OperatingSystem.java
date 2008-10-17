ddaff3cb8ab6f8bee304e598d2af4935b5a08547
Simon M Stewart <simon.m.stewart@gmail.com>
2008-10-17 14:57:40 +0000

    SimonStewart: Working on fixing issue #99
    r5766

--- common/src/java/org/openqa/selenium/internal/OperatingSystem.java
+++ common/src/java/org/openqa/selenium/internal/OperatingSystem.java
@@ -9,7 +9,7 @@ public enum OperatingSystem {
 			return "\r\n";
 		}
 	},
-	MAC("mac") {
+	MAC("mac", "darwin") {
 		@Override
 		public String getLineEnding() {
 			return "\r";
@@ -21,7 +21,7 @@ public enum OperatingSystem {
 			return "\n";
 		}
         },
-        ANY(null) {
+        ANY("") {
                @Override
                public String getLineEnding() {
                    throw new UnsupportedOperationException("getLineEnding");
@@ -29,9 +29,9 @@ public enum OperatingSystem {
         };
 
 	private static OperatingSystem currentOs;
-	private final String partOfOsName;
+	private final String[] partOfOsName;
 
-	private OperatingSystem(String partOfOsName) {
+	private OperatingSystem(String... partOfOsName) {
 		this.partOfOsName = partOfOsName;
 	}
 
@@ -39,23 +39,38 @@ public enum OperatingSystem {
 		if (currentOs != null) {
 			return currentOs;
 		}
-		
-		for (OperatingSystem os : OperatingSystem.values()) {
-			if (os.isCurrentPlatform()) {
-				currentOs = os;
-				return os;
+
+                OperatingSystem mostLikely = UNIX;
+                for (OperatingSystem os : OperatingSystem.values()) {
+                  for (String matcher : os.partOfOsName) {
+                        if ("".equals(matcher))
+                          continue;
+
+                        if (os.isExactMatch(matcher)) {
+                          currentOs = os;
+                          return os;
+                        }
+                        if (os.isCurrentPlatform(matcher)) {
+			  mostLikely = os;
 			}
-		}
+                  }
+                }
 
 		// Default to assuming we're on a unix variant (including linux)
-		currentOs = UNIX;
-		return UNIX;
+		currentOs = mostLikely;
+		return mostLikely;
 	}
 
-	public abstract String getLineEnding();
+        public abstract String getLineEnding();
 
-	private boolean isCurrentPlatform() {
+	private boolean isCurrentPlatform(String matchAgainst) {
 		String osName = System.getProperty("os.name").toLowerCase();
-		return osName.indexOf(partOfOsName) != -1;
+		return osName.indexOf(matchAgainst) != -1;
 	}
+
+        private boolean isExactMatch(String matchAgainst) {
+              String osName = System.getProperty("os.name").toLowerCase();
+              return matchAgainst.equals(osName);
+        }
+
 }
