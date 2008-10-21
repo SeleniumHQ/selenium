0565d7f79569f59f50203e8ce9536e3525be55c0
Michael Tamm <michael.tamm2@gmail.com>
2008-10-21 12:31:32 +0000

    MichaelTamm: Added more helpful message to the RuntimeException thrown by the Response constructor
    r5776

--- firefox/src/java/org/openqa/selenium/firefox/Response.java
+++ firefox/src/java/org/openqa/selenium/firefox/Response.java
@@ -29,7 +29,7 @@ public class Response {
 
             isError = (Boolean) result.get("isError");
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Could not parse \"" + json.replace("\"", "\\\"").replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t") + "\".", e);
         }
     }
 
