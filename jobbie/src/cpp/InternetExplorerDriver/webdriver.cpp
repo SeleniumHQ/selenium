9edcdca0136f2d83591c587210a9cfc30d628c0b
Simon M Stewart <simon.m.stewart@gmail.com>
2008-11-30 12:01:14 +0000

    SimonStewart: Exposing more functionality through the C library, and switching the calling convention to stdcall.
    r5874

--- jobbie/src/cpp/InternetExplorerDriver/webdriver.cpp
+++ jobbie/src/cpp/InternetExplorerDriver/webdriver.cpp
@@ -1,19 +1,97 @@
 #include "stdafx.h"
 #include "webdriver.h"
+#include "finder.h"
 #include "InternetExplorerDriver.h"
 #include "utils.h"
 #include <stdio.h>
+#include <vector>
 
 struct WebDriver {
-       InternetExplorerDriver *ie;
+    InternetExplorerDriver *ie;
 };
 
 struct WebElement {
-		ElementWrapper *element;
+	ElementWrapper *element;
+};
+
+struct StringWrapper {
+	wchar_t *text;
+};
+
+struct ElementCollection {
+	std::vector<ElementWrapper*> elements;
 };
 
 extern "C"
 {
+
+// String manipulation functions
+int wdStringLength(StringWrapper* string, int* length)
+{
+	if (!string) {
+		cerr << "No string to get length of" << endl;
+		*length = -1;
+		return -1;
+	}
+	if (!string->text) {
+		cerr << "No underlying string to get length of" << endl;
+		*length = -1;
+		return -2;
+	}
+	size_t len = wcslen(string->text);
+	*length = (int) len + 1;
+
+	return 0;
+}
+
+int wdFreeString(StringWrapper* string)
+{
+	if (!string) {
+		return  -1;
+	}
+
+	if (string->text) delete[] string->text;
+	delete string;
+	return 0;
+}
+
+int wdCopyString(StringWrapper* source, int size, wchar_t* dest)
+{
+	if (!source) {
+		cerr << "No source wrapper" << endl;
+		return -1;
+	}
+
+	if (!source->text) {
+		cerr << "No source text" << endl;
+		return -2;
+	}
+
+	wcscpy_s(dest, size, source->text);
+	return 0;
+}
+
+// Element manipulation functions
+int wdFreeElement(WebElement* element)
+{
+	if (!element)
+		return -1;
+
+	if (element->element) delete element->element;
+	delete element;
+	return 0;
+}
+
+// Driver manipulation functions
+int wdFreeDriver(WebDriver* driver)
+{
+	if (!driver || !driver->ie) return -1;
+	driver->ie->close();
+    delete driver->ie;
+    delete driver;
+	return 0;
+}
+
 WebDriver* webdriver_newDriverInstance()
 {
 //	startCom();
@@ -25,25 +103,24 @@ WebDriver* webdriver_newDriverInstance()
     return driver;
 }
 
-void webdriver_deleteDriverInstance(WebDriver* driver)
+int webdriver_get(WebDriver* driver, wchar_t* url)
 {
-	driver->ie->close();
-    delete driver->ie;
-    delete driver;
-}
-
-void webdriver_get(WebDriver* driver, wchar_t* url)
-{
+	if (!driver || !driver->ie) return -1;
 	driver->ie->get(url);
+	return 0;
 }
 
-void webdriver_close(WebDriver* driver)
+int webdriver_close(WebDriver* driver)
 {
+	if (!driver || !driver->ie) return -1;
 	driver->ie->close();
+	return 0;
 }
 
-const wchar_t* webdriver_getCurrentUrl(WebDriver* driver)
+int webdriver_getCurrentUrl(WebDriver* driver, StringWrapper** result)
 {
+	if (!driver || !driver->ie) return -1;
+
 	driver->ie->getCurrentUrl();
 
 	const std::wstring originalString(driver->ie->getCurrentUrl());
@@ -51,7 +128,50 @@ const wchar_t* webdriver_getCurrentUrl(WebDriver* driver)
 	wchar_t* toReturn = new wchar_t[length];
 
 	wcscpy_s(toReturn, length, originalString.c_str());
-	return toReturn;
+
+	StringWrapper* res = new StringWrapper();
+	res->text = toReturn;
+	
+	*result = res;
+
+	return 0;
 }
 
+int wdeSendKeys(WebElement* element, const wchar_t* text)
+{
+	if (!element || !element->element) {
+		return -1;
+	}
+
+	element->element->sendKeys(text);
+
+	return 0;
+}
+
+int wdFindElementByName(WebDriver* driver, WebElement* element, const wchar_t* name, WebElement** result)
+{
+	CComPtr<IHTMLDOMNode> res;
+	InternetExplorerDriver* ie = driver->ie;
+	CComPtr<IHTMLElement> elem;
+	if (element && element->element) {
+		elem = element->element->getWrappedElement();
+	}
+
+	ElementWrapper* wrapper = NULL;
+	try {
+		wrapper = ie->selectElementByName(elem, name);
+	} catch (std::wstring& ) {
+		return -1;
+	}
+
+	if (!wrapper) {
+		return -2;
+	}
+
+	WebElement* toReturn = new WebElement();
+	toReturn->element = wrapper;
+
+	*result = toReturn;
+	return 0;
+}
 }
--- jobbie/src/cpp/InternetExplorerDriver/webdriver.h
+++ jobbie/src/cpp/InternetExplorerDriver/webdriver.h
@@ -14,13 +14,37 @@ typedef struct WebDriver WebDriver;
 struct WebElement;
 typedef struct WebElement WebElement;
 
+struct StringWrapper;
+typedef struct StringWrapper StringWrapper;
+
+struct ElementCollection;
+typedef struct ElementCollection ElementCollection;
+
+// String manipulation functions
+EXPORT int wdStringLength(StringWrapper* string, int* length);
+EXPORT int wdFreeString(StringWrapper* string);
+EXPORT int wdCopyString(StringWrapper* source, int length, wchar_t* dest);
+
+// Element manipulation functions
+EXPORT int wdFreeElement(WebElement* element);
+
+// Driver manipulation functions
+EXPORT int wdFreeDriver(WebDriver* driver);
+
+// WebDriver functions
 EXPORT WebDriver* webdriver_newDriverInstance();
-EXPORT void webdriver_deleteDriverInstance(WebDriver* handle);
+EXPORT int webdriver_deleteDriverInstance(WebDriver* handle);
 
-EXPORT void webdriver_get(WebDriver* driver, wchar_t* url);
-EXPORT void webdriver_close(WebDriver* driver);
+EXPORT int webdriver_get(WebDriver* driver, wchar_t* url);
+EXPORT int webdriver_close(WebDriver* driver);
 
-EXPORT const wchar_t* webdriver_getCurrentUrl(WebDriver* driver);
+EXPORT int webdriver_getCurrentUrl(WebDriver* driver, StringWrapper** result);
+
+// Element functions
+EXPORT int wdeSendKeys(WebElement* element, const wchar_t* text);
+
+// Element locating functions
+EXPORT int wdFindElementByName(WebDriver* driver, WebElement* element, const wchar_t* name, WebElement** result);
 
 #ifdef __cplusplus
 }
