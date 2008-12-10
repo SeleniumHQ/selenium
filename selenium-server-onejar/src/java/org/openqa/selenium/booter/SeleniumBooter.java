6689000698baf5387ada86a2c4dc1bb9abd0d79d
Paul Hammant <PaulHammant@gmail.com>
2008-12-10 23:33:40 +0000

    move main RC classes closer to entry point
    r5919

--- selenium-server-onejar/src/java/org/openqa/selenium/booter/SeleniumBooter.java
+++
@@ -1,9 +0,0 @@
-package org.openqa.selenium.booter;
-
-import org.openqa.selenium.server.SeleniumServer;
-
-public class SeleniumBooter {
-	public static void main(String[] args) throws Throwable {
-		SeleniumServer.main(args);
-	}
-}
