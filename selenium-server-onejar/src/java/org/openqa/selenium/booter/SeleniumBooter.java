926b9f0e86880944e8eebdd3ca3b257cafd5323d
Paul Hammant <PaulHammant@gmail.com>
2008-12-10 22:51:34 +0000

    start of one jar solution for selenium server
    r5917

---
+++ selenium-server-onejar/src/java/org/openqa/selenium/booter/SeleniumBooter.java
@@ -0,0 +1,9 @@
+package org.openqa.selenium.booter;
+
+import org.openqa.selenium.server.SeleniumServer;
+
+public class SeleniumBooter {
+	public static void main(String[] args) throws Throwable {
+		SeleniumServer.main(args);
+	}
+}
