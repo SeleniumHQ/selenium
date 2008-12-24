efd98d769ac020d952be06b6aa188cedd9682fc8
Paul Hammant <PaulHammant@gmail.com>
2008-12-24 22:11:03 +0000

    fixing a test, demonstrating JUnitConditionRunner
    r5993

--- sample/src/ExampleUsingConditionsTest.java
+++ sample/src/ExampleUsingConditionsTest.java
@@ -1,7 +1,9 @@
 import junit.framework.TestCase;
-import com.thoughtworks.selenium.*;
-import com.thoughtworks.selenium.condition.*;
 
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.condition.JUnitConditionRunner;
+import com.thoughtworks.selenium.condition.Text;
 
 public class ExampleUsingConditionsTest extends TestCase {
 
@@ -9,27 +11,26 @@ public class ExampleUsingConditionsTest extends TestCase {
     private JUnitConditionRunner conditionRunner;
 
     public void setUp() {
-        browser = new DefaultSelenium("localhost",
-                4444, "*firefox", "http://www.ajax.org");
+        browser = new DefaultSelenium("localhost", 4444, "*firefox",
+                "http://www.francisshanahan.com");
         browser.start();
         /*
-                 Conditional Runner can be used to wait for conditions
-                 to be true before continuing on. In the example below,
-                 the runner is waiting for text to be present from the AJAX control
-                 on the given webpage.
-             */
+         * Conditional Runner can be used to wait for conditions to be true
+         * before continuing on. In the example below, the runner is waiting for
+         * text to be present from the AJAX control on the given webpage.
+         */
         conditionRunner = new JUnitConditionRunner(browser);
     }
 
     public void testWaitForAJAXToLoad() throws InterruptedException {
         browser.showContextualBanner();
-        browser.open("/"); // relative URL is correct idiom for open()
-
-        assertTrue(browser.isVisible("loadscreen"));
-        conditionRunner.waitFor(new Text("Ajax stands for Asynchronous Javascript And XML"));
-        assertFalse(browser.isVisible("loadscreen"));
+        // relative URL is correct idiom for open()
+        browser.open("/zuggest.aspx");
+        browser.waitForPageToLoad("5000");
+        browser.type("txtKeywords", "Neal Ford");
+        browser.select("idx", "Books");
+        conditionRunner.waitFor(new Text("The"));
     }
-
 
     public void tearDown() {
         browser.stop();
