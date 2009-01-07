e57fe5773265d97c6331b492e684deb4eb41aae8
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2009-01-07 09:16:25 +0000

    Fix Firefox default browser location on latest Ubuntu. Made it dynamic.
    r6052

--- server-coreless/src/main/java/org/openqa/selenium/server/browserlaunchers/locators/Firefox3Locator.java
+++ server-coreless/src/main/java/org/openqa/selenium/server/browserlaunchers/locators/Firefox3Locator.java
@@ -2,15 +2,17 @@ package org.openqa.selenium.server.browserlaunchers.locators;
 
 import org.openqa.selenium.server.browserlaunchers.WindowsUtils;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 /**
  * Discovers a valid Firefox 2.x or 3.x installation on local system. Preference is given to 2.x installs.
  */
 public class Firefox3Locator extends FirefoxLocator {
 
-    private static final String[] USUAL_UNIX_LAUNCHER_LOCATIONS = {
+    private static final String[] USUAL_OS_X_LAUNCHER_LOCATIONS = {
             "/Applications/Firefox-3.app/Contents/MacOS",
             "/Applications/Firefox.app/Contents/MacOS",
-            "/usr/lib/firefox-3.0.3", /* Ubuntu 8.04 default location */
     };
 
     private static final String[] USUAL_WINDOWS_LAUNCHER_LOCATIONS = {
@@ -18,6 +20,10 @@ public class Firefox3Locator extends FirefoxLocator {
             WindowsUtils.getProgramFilesPath() + "\\Mozilla Firefox",
             WindowsUtils.getProgramFilesPath() + "\\Firefox",
     };
+
+
+    private String[] usualLauncherLocations;
+
 
     protected String browserName() {
         return "Firefox 3";
@@ -28,15 +34,49 @@ public class Firefox3Locator extends FirefoxLocator {
     }
 
     protected String[] standardlauncherFilenames() {
-        if (WindowsUtils.thisIsWindows()) {
+        if (runningOnWindows()) {
             return new String[]{"firefox.exe"};
         } else {
             return new String[]{"firefox-bin", "firefox"};
         }
     }
-    
-    protected String[] usualLauncherLocations() {
-        return WindowsUtils.thisIsWindows() ? USUAL_WINDOWS_LAUNCHER_LOCATIONS : USUAL_UNIX_LAUNCHER_LOCATIONS;
+
+    protected synchronized String[] usualLauncherLocations() {
+        if (null == usualLauncherLocations) {
+            usualLauncherLocations = runningOnWindows() ? USUAL_WINDOWS_LAUNCHER_LOCATIONS : usualUnixLauncherLocations();
+        }
+
+        return usualLauncherLocations;
     }
 
+    protected String[] usualUnixLauncherLocations() {
+        final String[] ubuntuLocations;
+
+        ubuntuLocations = firefoxDefaultLocationsOnUbuntu();
+        return ubuntuLocations.length == 0 ? USUAL_OS_X_LAUNCHER_LOCATIONS : ubuntuLocations;
+    }
+
+
+    /**
+     * Dynamic because the directory version number keep changing. 
+     */
+    protected String[] firefoxDefaultLocationsOnUbuntu() {
+        final File dir;
+
+        dir = new File("/usr/lib");
+
+        if (!dir.exists() && dir.isDirectory()) {
+            return new String[] {};
+        }
+        return dir.list(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith("firefox-");
+            }
+        });
+    }
+
+    protected boolean runningOnWindows() {
+        return WindowsUtils.thisIsWindows();
+    }
+    
 }
