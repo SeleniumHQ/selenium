bb51e0d86752d852c083c194a069046e7ff4b120
Jiayao Yu <Jiayao.Yu@test.com>
2009-02-09 12:06:08 +0000

    JiayaoYu:convert tabs to whitespaces in the py binding api_examples
    r6224

--- bindings/py/tests/api_examples.py
+++ bindings/py/tests/api_examples.py
@@ -11,67 +11,67 @@ from webdriver.firefox.firefoxlauncher import FirefoxLauncher
 
 class ApiExampleTest (unittest.TestCase):
     def setUp(self):
-	self.driver = WebDriver()
+        self.driver = WebDriver()
 
     def tearDown(self):
-	pass
+        pass
 
     def testGetTitle(self):
-	self._loadSimplePage()
-	title = self.driver.get_title()
-	self.assertEquals("Hello WebDriver", title)
+        self._loadSimplePage()
+        title = self.driver.get_title()
+        self.assertEquals("Hello WebDriver", title)
 
     def testGetCurrentUrl(self):
-	self._loadSimplePage()
-	url = self.driver.get_current_url()
-	self.assertEquals("http://localhost:8000/simpleTest.html", url)
+        self._loadSimplePage()
+        url = self.driver.get_current_url()
+        self.assertEquals("http://localhost:8000/simpleTest.html", url)
 
     def testFindElementsByXPath(self):
-	self._loadSimplePage()
-	elem = self.driver.find_element_by_xpath("//h1")
-	self.assertEquals("Heading", elem.get_text())
+        self._loadSimplePage()
+        elem = self.driver.find_element_by_xpath("//h1")
+        self.assertEquals("Heading", elem.get_text())
 
     def testFindElementsByXpath(self):
-	self._loadPage("nestedElements")
-	elems = self.driver.find_elements_by_xpath("//option")
-	self.assertEquals(48, len(elems))
-	self.assertEquals("One", elems[0].get_value())
+        self._loadPage("nestedElements")
+        elems = self.driver.find_elements_by_xpath("//option")
+        self.assertEquals(48, len(elems))
+        self.assertEquals("One", elems[0].get_value())
 
     def testFindElementsByName(self):
-	self._loadPage("xhtmlTest")
-	elem = self.driver.find_element_by_name("windowOne")
-	self.assertEquals("Open new window", elem.get_text())
+        self._loadPage("xhtmlTest")
+        elem = self.driver.find_element_by_name("windowOne")
+        self.assertEquals("Open new window", elem.get_text())
 
     def testShouldBeAbleToEnterDataIntoFormFields(self):
-	self._loadPage("xhtmlTest")
-	elem = self.driver.find_element_by_xpath("//form[@name='someForm']/input[@id='username']")
-	elem.clear()
-	elem.send_keys("some text")
-	elem = self.driver.find_element_by_xpath("//form[@name='someForm']/input[@id='username']")
-	self.assertEquals("some text", elem.get_value())
+        self._loadPage("xhtmlTest")
+        elem = self.driver.find_element_by_xpath("//form[@name='someForm']/input[@id='username']")
+        elem.clear()
+        elem.send_keys("some text")
+        elem = self.driver.find_element_by_xpath("//form[@name='someForm']/input[@id='username']")
+        self.assertEquals("some text", elem.get_value())
 
     def testSwitchToWindow(self):
-	title_1 = "XHTML Test Page"
-	title_2 = "We Arrive Here"
-	self._loadPage("xhtmlTest")
-	self.driver.find_element_by_link_text("Open new window").click()
-	self.assertEquals(title_1, self.driver.get_title())
-	try:
-	    self.driver.SwitchToWindow("result")
-	except:
-	  # This may fail because the window is not loading fast enough, so try again
-	  time.sleep(1)
-	  self.driver.switch_to_window("result")
-	self.assertEquals(title_2, self.driver.get_title())
+        title_1 = "XHTML Test Page"
+        title_2 = "We Arrive Here"
+        self._loadPage("xhtmlTest")
+        self.driver.find_element_by_link_text("Open new window").click()
+        self.assertEquals(title_1, self.driver.get_title())
+        try:
+            self.driver.SwitchToWindow("result")
+        except:
+            # This may fail because the window is not loading fast enough, so try again
+            time.sleep(1)
+            self.driver.switch_to_window("result")
+        self.assertEquals(title_2, self.driver.get_title())
 
     def testSwitchToFrameByIndex(self):
-	self._loadPage("frameset")
-	self.driver.switch_to_frame(2)
-	self.driver.switch_to_frame(0)
-	self.driver.switch_to_frame(2)
-	checkbox = self.driver.find_element_by_id("checky")
-	checkbox.toggle()
-	checkbox.submit()
+        self._loadPage("frameset")
+        self.driver.switch_to_frame(2)
+        self.driver.switch_to_frame(0)
+        self.driver.switch_to_frame(2)
+        checkbox = self.driver.find_element_by_id("checky")
+        checkbox.toggle()
+        checkbox.submit()
   
     def testSwitchFrameByName(self):
         self._loadPage("frameset")
