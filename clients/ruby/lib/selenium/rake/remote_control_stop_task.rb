063eaa510f60696310dc4ed9ab0428a0001bb704
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2009-02-20 17:00:04 +0000

    Ruby Client: Incorporating a github patch by Aaron Tinio: wait for complete shutdown when stopping Selenium RC
    r6297

--- clients/ruby/lib/selenium/rake/remote_control_stop_task.rb
+++ clients/ruby/lib/selenium/rake/remote_control_stop_task.rb
@@ -1,14 +1,15 @@
 module Selenium
   module Rake
-  
+
     class RemoteControlStopTask
-      attr_accessor :host, :port, :timeout_in_seconds
+      attr_accessor :host, :port, :timeout_in_seconds, :wait_until_stopped
 
       def initialize(name = :'selenium:rc:stop')
         @host = "localhost"
         @name = name
         @port = 4444
         @timeout_in_seconds = 5
+        @wait_until_stopped = true
         yield self if block_given?
         define
       end
@@ -19,6 +20,9 @@ module Selenium
           puts "Stopping Selenium Remote Control running at #{@host}:#{@port}..."
           remote_control = Selenium::RemoteControl::RemoteControl.new(@host, @port, @timeout_in_seconds)
           remote_control.stop
+          if @wait_until_stopped
+            TCPSocket.wait_for_service_termination :host => @host, :port => @port
+          end
           puts "Stopped Selenium Remote Control running at #{@host}:#{@port}"
         end
       end
--- clients/ruby/lib/tcp_socket_extension.rb
+++ clients/ruby/lib/tcp_socket_extension.rb
@@ -4,20 +4,28 @@ require 'socket'
 class TCPSocket
   
   def self.wait_for_service(options)
-    socket = nil
-    Timeout::timeout(options[:timeout] || 20) do
-      loop do
-        begin
-          socket = TCPSocket.new(options[:host], options[:port])
-          return
-        rescue Errno::ECONNREFUSED
-          puts ".\n"
-          sleep 2
-        end
-      end
-    end
-  ensure
-    socket.close unless socket.nil?
+    verbose_wait until listening_service?(options)
   end
   
+  def self.wait_for_service_termination(options)
+    verbose_wait while listening_service?(options)
+  end
+
+  def self.listening_service?(options)
+    Timeout::timeout(options[:timeout] || 20) do
+      begin
+        socket = TCPSocket.new(options[:host], options[:port])
+        socket.close unless socket.nil?
+        true
+      rescue Errno::ECONNREFUSED
+        false
+      end
+    end
+  end
+
+  def self.verbose_wait
+    puts ".\n"
+    sleep 2
+  end
+
 end
