54e23de051f440eceab40a1b381f6be928b92c15
Simon M Stewart <simon.m.stewart@gmail.com>
2009-11-20 18:44:17 +0000

    SimonStewart: Adding a link back to the qdox project
    r7667

--- common/src/java/org/openqa/selenium/internal/Base64Encoder.java
+++ common/src/java/org/openqa/selenium/internal/Base64Encoder.java
@@ -26,6 +26,8 @@ import java.io.StringReader;
  * <p>This meets the standard as described in RFC 1521, section 5.2 <http://www.freesoft.org/CIE/RFC/1521/7.htm>, allowing
  * other Base64 tools to manipulate the data.</p>
  *
+ * Original code from <a href="http://qdox.codehaus.org/">qdox</a>
+ *
  * @author Joe Walnes
  */
 public class Base64Encoder {
