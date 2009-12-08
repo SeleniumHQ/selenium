6f0edb017f1b3ac019106d23869475f381c73698
Simon M Stewart <simon.m.stewart@gmail.com>
2009-12-08 18:25:28 +0000

    SimonStewart: Only falling back to the prebuilt binaries if we can't actually build them.
    r7793

--- rake-tasks/dotnet.rb
+++ rake-tasks/dotnet.rb
@@ -2,11 +2,11 @@
 class DotNet < BaseGenerator
   def library(args)
     task args[:name].to_sym => FileList[args[:srcs]] do
-#      if msbuild?
-#        sh "devenv #{args[:solution]} /project #{args[:project]} Release /rebuild", :verbose => false
-#      else
+      if msbuild?
+        sh "devenv #{args[:solution]} /project #{args[:project]} Release /rebuild", :verbose => false
+      else
         copy_prebuilt(args[:prebuilt], args[:name])
-#      end
+      end
     end
 
     task args[:name].to_sym => args[:solution]
