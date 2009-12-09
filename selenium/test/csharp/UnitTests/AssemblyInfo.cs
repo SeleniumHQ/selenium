cb3af73afc6c9fc255d4a531ccd14695b6bdec20
Simon M Stewart <simon.m.stewart@gmail.com>
2009-12-09 16:27:32 +0000

    SimonStewart: Merging the dotnet bindings from selenium rc into the merge branch.
    r7804

---
+++ selenium/test/csharp/UnitTests/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly : AssemblyTitle("Selenium Unit Tests")]
+[assembly : AssemblyDescription("")]
+[assembly : AssemblyConfiguration("")]
