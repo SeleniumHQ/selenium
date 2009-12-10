56e23cbdfb95a55111b84378591a1b8c488adcb5
Patrick Lightbody <plightbo@gmail.com>
2009-12-10 15:43:10 +0000

    PatrickLightbody simple version/revision mapping for the alpha release (still should make this be automatically determined during the build)
    r7830

--- common/src/js/core/scripts/selenium-version.js
+++ common/src/js/core/scripts/selenium-version.js
@@ -1,5 +1,5 @@
-Selenium.version = "@VERSION@";
-Selenium.revision = "@REVISION@";
+Selenium.version = "2.0";
+Selenium.revision = "alpha 1";
 
 window.top.document.title += " v" + Selenium.version + " [" + Selenium.revision + "]";
 
