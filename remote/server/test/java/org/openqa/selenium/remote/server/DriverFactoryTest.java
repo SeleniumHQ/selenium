83e0e3f692700b2bc0449411554542d40e55cfed
Jason Lebya <jmleyba@gmail.com>
2009-12-17 01:55:44 +0000

    JasonLeyba:  DriverFactoryTest should be located under the test/ directory, not src/
    r7893

--- remote/server/test/java/org/openqa/selenium/remote/server/DriverFactoryTest.java
+++ remote/server/test/java/org/openqa/selenium/remote/server/DriverFactoryTest.java
@@ -17,12 +17,12 @@ limitations under the License.
 
 package org.openqa.selenium.remote.server;
 
-import junit.framework.TestCase;
-
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import junit.framework.TestCase;
 
 public class DriverFactoryTest extends TestCase {
   private DriverFactory factory;
