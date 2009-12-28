e5a138850b35d1ef50096862586407b0c2b88e37
Patrick Lightbody <plightbo@gmail.com>
2009-12-28 04:32:05 +0000

    PatrickLightbody Give the Timer thread a more useful name when looking at stack dumps.
    r7952

--- selenium/src/java/org/openqa/selenium/internal/seleniumemulation/Timer.java
+++ selenium/src/java/org/openqa/selenium/internal/seleniumemulation/Timer.java
@@ -22,13 +22,18 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import com.thoughtworks.selenium.SeleniumException;
 
 public class Timer {
-  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+  private final ExecutorService executor = Executors.newSingleThreadExecutor(new ThreadFactory() {
+      public Thread newThread(Runnable r) {
+          return new Thread(r, "Selenium Timer Thread");
+      }
+  });
   private long timeout;
 
   public Timer(long timeout) {
