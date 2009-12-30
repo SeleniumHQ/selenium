f000ecb001a0b0da10c3c4be1ecc27f0a97b1e32
Patrick Lightbody <plightbo@gmail.com>
2009-12-30 18:32:45 +0000

    PatrickLightbody Preferring the isTextPresent logic from Se 1.0 by using the same getTextContent() behavior found in htmlutils.js. Doing this because the getText() function on the body tag tends to cause crazy page scrolling behavior that slows things down, causes rendering quirks, and otherwise doesn't perfectly match the original Selenium 1.0 behavior.
    r7965

--- selenium/src/java/org/openqa/selenium/internal/seleniumemulation/IsTextPresent.java
+++ selenium/src/java/org/openqa/selenium/internal/seleniumemulation/IsTextPresent.java
@@ -23,6 +23,7 @@ import java.util.regex.Pattern;
 
 import com.google.common.collect.Maps;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class IsTextPresent extends SeleneseCommand<Boolean> {
@@ -35,7 +36,44 @@ public class IsTextPresent extends SeleneseCommand<Boolean> {
 
   @Override
   protected Boolean handleSeleneseCommand(WebDriver driver, String pattern, String ignored) {
-    String text = driver.findElement(By.xpath("/html/body")).getText();
+    String text;
+    if (driver instanceof JavascriptExecutor) {
+      // let's use the same getTextContent function provided by Selenium 1.0 in htmlutils.js if we can
+      JavascriptExecutor js = (JavascriptExecutor) driver;
+      String script = "if (!window.__seleniumCompat__getTextContent) {\n"
+                      + "  window.__seleniumCompat__getTextContent = function(element, preformatted) {\n"
+                      + "    if (element.style && (element.style.visibility == 'hidden' || element.style.display == 'none')) return '';\n"
+                      + "    if (element.nodeType == 3 /*Node.TEXT_NODE*/) {\n"
+                      + "      var text = element.data;\n"
+                      + "      if (!preformatted) {\n"
+                      + "        text = text.replace(/\\n|\\r|\\t/g, \" \");\n"
+                      + "      }\n"
+                      + "      \n"
+                      + "      return text;\n"
+                      + "    }\n"
+                      + "    if (element.nodeType == 1 /*Node.ELEMENT_NODE*/ && element.nodeName != 'SCRIPT') {\n"
+                      + "      var childrenPreformatted = preformatted || (element.tagName == \"PRE\");\n"
+                      + "      var text = \"\";\n"
+                      + "      for (var i = 0; i < element.childNodes.length; i++) {\n"
+                      + "        var child = element.childNodes.item(i);\n"
+                      + "        text += window.__seleniumCompat__getTextContent(child, childrenPreformatted);\n"
+                      + "        if (element.tagName == \"P\" || element.tagName == \"BR\" || element.tagName == \"HR\" || element.tagName == \"DIV\") {\n"
+                      + "          text += \"\\n\";\n"
+                      + "        }\n"
+                      + "      }\n"
+                      + "      return text;\n"
+                      + "    }\n"
+                      + "    return '';\n"
+                      + "  }\n"
+                      + "};\n"
+                      + "return window.__seleniumCompat__getTextContent(document.body);\n";
+      text = js.executeScript(script).toString();
+    } else {
+      // bummer - no javascript support available. We can do it this way, but be warned: this can
+      // be slow and cause some weird visual artifacts due to crazy scrolling as WebDriver determines
+      // if some text is visible or not
+      text = driver.findElement(By.xpath("/html/body")).getText();
+    }
     text = text.trim();
 
     String strategyName = "implicit";
