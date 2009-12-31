d62aba823087eb9e9d1f4c27da3f25c28cfccce7
Adam Goucher <adam_goucher@hotmail.com>
2009-12-31 01:08:25 +0000

    this script will copy the necessary bits in order to run se-ide through a proxy extension. in your extensions dir, create a file called {a6fd85ed-e919-4a43-a5af-8da18bda539f} and in it put in the path to your selenium-ide/trunk/src/build dir (the build dir is actually created as part of the script)
    i'll create the equiv. bash script in a week or so when i get on a unix box again
    
    
    r7968

---
+++ src/proxy.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem This script will create a 'local' copy of the se-ide extension which you can
+rem then point to using the proxy trick described at http://wiki.openqa.org/display/SIDE/Building+Selenium+IDE
+rem But, before you run it, you need to do a 'mvn clean install' to get the
+rem external dependencies moved into the right spots
+
+setlocal
+
+set ROOT_DIR=%CD%
+set TMP_DIR="build"
+
+rem remove any left-over files from previous build
+del /S /Q /F %TMP_DIR%
+
+mkdir %TMP_DIR%\content
+
+robocopy content %TMP_DIR%\content /E /XD .svn
+robocopy locale %TMP_DIR%\locale /E /XD .svn
+robocopy skin %TMP_DIR%\skin /E /XD .svn
+copy install.rdf %TMP_DIR%
+copy chrome.manifest %TMP_DIR%
+
+endlocal
