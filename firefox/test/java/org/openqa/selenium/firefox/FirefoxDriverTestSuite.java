924a308ad452c758ca72208459288ab83dfe65ae
Daniel Wagner-Hall <dawagner@gmail.com>
2010-01-03 03:04:04 +0000

    DanielWagnerHall: Oops, let's not disable the JSAPI tests...
    r7981

--- firefox/test/java/org/openqa/selenium/firefox/FirefoxDriverTestSuite.java
+++ firefox/test/java/org/openqa/selenium/firefox/FirefoxDriverTestSuite.java
@@ -43,7 +43,7 @@ public class FirefoxDriverTestSuite extends TestCase {
         .exclude(FIREFOX)
         .keepDriverInstance()
         .includeJavascriptTests()
-        //.includeJsApiTests()
+        .includeJsApiTests()
         .create();
   }
 
