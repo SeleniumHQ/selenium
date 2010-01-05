c116551ec5a3ba1579e6575d8bec1f9b48b4bc9a
Adam Goucher <adam_goucher@hotmail.com>
2010-01-05 01:46:26 +0000

    tabs to spaces (forgot to save before the last commit apparently)
    r7995

--- src/content/api.js
+++ src/content/api.js
@@ -1,20 +1,20 @@
 function API() {
-	this.verison = 0.1;	
-	this.preferences = SeleniumIDE.Preferences;
+    this.verison = 0.1;	
+    this.preferences = SeleniumIDE.Preferences;
 };
 
 // add the provided chrome url to the list of user extensions provided through plugins
 // -- or not if it already exists
 API.prototype.addPluginProvidedUserExtension = function(url) {
-	var options = {};
-	current = this.preferences.getString("pluginProvidedUserExtensions");
-	if (typeof current == 'undefined'){
- 		options["pluginProvidedUserExtensions"] = url;
-		this.preferences.save(options, "pluginProvidedUserExtensions");
-	} else {
- 		if (current.search(url) == -1) {
- 			options["pluginProvidedUserExtensions"] = current + ' ' + url;
-			this.preferences.save(options, "pluginProvidedUserExtensions");
-		}
-	}
+    var options = {};
+    current = this.preferences.getString("pluginProvidedUserExtensions");
+    if (typeof current == 'undefined'){
+        options["pluginProvidedUserExtensions"] = url;
+        this.preferences.save(options, "pluginProvidedUserExtensions");
+    } else {
+        if (current.search(url) == -1) {
+            options["pluginProvidedUserExtensions"] = current + ' ' + url;
+            this.preferences.save(options, "pluginProvidedUserExtensions");
+        }
+    }
 };
