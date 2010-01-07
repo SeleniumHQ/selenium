a0b0eac79db0aaef8e1629fc91077576c666a944
Adam Goucher <adam_goucher@hotmail.com>
2010-01-07 20:01:19 +0000

    unix proxy script. gives a bunch of output from the find command when getting rid of the .svn dirs, but just ignore those
    r8024

---
+++ src/proxy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script will create a 'local' copy of the se-ide extension which you can
+# then point to using the proxy trick described at http://wiki.openqa.org/display/SIDE/Building+Selenium+IDE
+# But, before you run it, you need to do a 'mvn clean install' to get the
+# external dependencies moved into the right spots
+
+TMP_DIR="build"
+
+# remove any left-over files from previous build
+rm -rf $TMP_DIR
+
+mkdir -p $TMP_DIR/content
+
+cp -R content $TMP_DIR/content
+rm -rf $TMP_DIR/content/selenium-tests
+rm -rf $TMP_DIR/content/tests
+cp -R locale $TMP_DIR/locale
+cp -R skin $TMP_DIR/skin
+cp -R components $TMP_DIR/components
+cp install.rdf $TMP_DIR
+cp chrome.manifest $TMP_DIR
+find $TMP_DIR -name ".svn" -type d -exec rm -rf {} \; 
