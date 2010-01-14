85abee86a27544cb2fb7b16285a633a729f5600b
Adam Goucher <adam_goucher@hotmail.com>
2010-01-14 21:26:51 +0000

    AdamGoucher - fixed the way plugin provided user extensions are loaded. and added an observer that clears the preferences added by the api when the browser is closed or restart. This make development of plugins easier as each plugin does not need an observer (but certainly can it if wants)
    r8083

--- src/content/api.js
+++ src/content/api.js
@@ -9,7 +9,7 @@ API.prototype.addPluginProvidedUserExtension = function(url) {
     var options = {};
     
     var current = this.preferences.getString("pluginProvidedUserExtensions");
-    if (typeof current == 'undefined'){
+    if (!current || current.length == 0){
         options["pluginProvidedUserExtensions"] = url;
         this.preferences.save(options, "pluginProvidedUserExtensions");
     } else {
@@ -25,7 +25,7 @@ API.prototype.addPluginProvidedFormatter = function(id, name, url) {
     var options = {}
     
     var current = this.preferences.getString("pluginProvidedFormatters");
-    if (typeof current == "undefined")  {
+    if (!current || current.length == 0)  {
         options["pluginProvidedFormatters"] = id + ";" + name + ";" + url;
         this.preferences.save(options, "pluginProvidedFormatters");
     } else {
@@ -35,3 +35,33 @@ API.prototype.addPluginProvidedFormatter = function(id, name, url) {
         }
     }
 };
+
+// cleanup after outselves
+//
+// this works because plugins re-register every time the browser restarts
+function initializeSeIDEAPIObserver() {
+    seIDEAPIObserver.register();
+}
+
+var seIDEAPIObserver = {
+    _uninstall : false,
+    observe : function(subject, topic, data) {
+        if (topic == "quit-application-granted") {
+            var branch = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.selenium-ide.");
+branch.setCharPref("pluginProvidedUserExtensions", "");
+            branch.setCharPref("pluginProvidedUserExtensions", "");
+            branch.setCharPref("pluginProvidedFormatters", "");
+            this.unregister();
+        }
+    },
+    register : function() {
+        var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+        observerService.addObserver(this, "quit-application-granted", false);
+    },
+    unregister : function() {
+        var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+        observerService.removeObserver(this, "quit-application-granted");
+    }
+}
+
+window.addEventListener("load", initializeSeIDEAPIObserver, false);
