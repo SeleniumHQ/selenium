34415a1c8d1770122323f4d97cc1e5bf6d8f0c2c
Davis Burns <david.burns@theautomatedtester.co.uk>
2010-02-19 00:08:21 +0000

    DavidBurns: Updated ExecuteScript comments to same as IJavaScriptExecutor.
    r8387

--- jobbie/src/csharp/webdriver-ie/IE/InternetExplorerDriver.cs
+++ jobbie/src/csharp/webdriver-ie/IE/InternetExplorerDriver.cs
@@ -235,6 +235,34 @@ namespace OpenQA.Selenium.IE
         /// <param name="script">The JavaScript code to execute.</param>
         /// <param name="args">The arguments to the script.</param>
         /// <returns>The value returned by the script.</returns>
+        /// <remarks>
+        /// <para>
+        /// The <see cref="ExecuteScript"/>method executes JavaScript in the context of 
+        /// the currently selected frame or window. This means that "document" will refer 
+        /// to the current document. If the script has a return value, then the following 
+        /// steps will be taken:
+        /// </para>
+        /// <para>
+        /// <list type="bullet">
+        /// <item><description>For an HTML element, this method returns a <see cref="IWebElement"/></description></item>
+        /// <item><description>For a number, a <see cref="System.Int64"/> is returned</description></item>
+        /// <item><description>For a boolean, a <see cref="System.Boolean"/> is returned</description></item>
+        /// <item><description>For all other cases a <see cref="System.String"/> is returned.</description></item>
+        /// <item><description>For an array,we check the first element, and attempt to return a 
+        /// <see cref="List{T}"/> of that type, following the rules above. Nested lists are not
+        /// supported.</description></item>
+        /// <item><description>If the value is null or there is no return value,
+        /// <see langword="null"/> is returned.</description></item>
+        /// </list>
+        /// </para>
+        /// <para>
+        /// Arguments must be a number (which will be converted to a <see cref="System.Int64"/>),
+        /// a <see cref="System.Boolean"/>, a <see cref="System.String"/> or a <see cref="IWebElement"/>.
+        /// An exception will be thrown if the arguments do not meet these criteria. 
+        /// The arguments will be made available to the JavaScript via the "arguments" magic 
+        /// variable, as if the function were called via "Function.apply" 
+        /// </para>
+        /// </remarks>
         public object ExecuteScript(string script, params object[] args)
         {
             object toReturn = null;
