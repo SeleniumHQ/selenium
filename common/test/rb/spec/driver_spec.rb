a0e775ee04b69bcd2772aaa4bd4514ab397def54
Jari Bakken <jari.bakken@gmail.com>
2010-03-15 16:18:29 +0000

    JariBakken: Move Firefox-specific spec
    r8511

--- common/test/rb/spec/driver_spec.rb
+++ common/test/rb/spec/driver_spec.rb
@@ -212,18 +212,5 @@ describe "Driver" do
     end
   end
 
-  compliant_on :driver => :firefox do
-    describe ".new" do
-      it "should take a Firefox::Profile instance as argument" do
-        begin
-          profile = Selenium::WebDriver::Firefox::Profile.new
-          driver = Selenium::WebDriver.for :firefox, :profile => profile
-        ensure
-          driver.quit if driver
-        end
-      end
-    end
-  end
-
 end
 
