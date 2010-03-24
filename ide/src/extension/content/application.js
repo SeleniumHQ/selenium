b5260cdd99636bd889c4b4acccbe3e017a371ba1
Dave Hunt <dave.hunt@gmail.com>
2010-03-24 14:45:27 +0000

    Dave Hunt: After discussion with Adam Goucher and Jérémy Hérault I have removed the line that previously linked changing the display source format with the clipboard format.
    r8567

--- ide/src/extension/content/application.js
+++ ide/src/extension/content/application.js
@@ -96,7 +96,6 @@ Application.prototype = {
         this.currentFormat = format;
         this.options.selectedFormat = format.id;
         Preferences.save(this.options, 'selectedFormat');
-        //this.setClipboardFormat(format);
         this.notify("currentFormatChanged", format);
     },
 
