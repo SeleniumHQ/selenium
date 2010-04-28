db50411d140c03ef33a6acbe2da0cf2b8d84070e
Simon M Stewart <simon.m.stewart@gmail.com>
2010-04-28 14:12:43 +0000

    SimonStewart: Fixing style, no logical changes
    r8770

--- remote/server/src/java/org/openqa/selenium/server/BrowserConfigurationOptions.java
+++ remote/server/src/java/org/openqa/selenium/server/BrowserConfigurationOptions.java
@@ -10,152 +10,163 @@ import org.openqa.selenium.remote.ProxyPac;
 
 public class BrowserConfigurationOptions {
 
-    private Map<String,String> options = new HashMap<String,String>();
-    private boolean hasOptions = false;
-    
-    public BrowserConfigurationOptions(String browserConfiguration) {
-        //"name=value;name=value"
-        String[] optionsPairList = browserConfiguration.split(";");
-        for (int i = 0; i < optionsPairList.length; i++) {
-            String[] option = optionsPairList[i].split("=", 2);
-            if (2 == option.length) {
-              String optionsName = option[0].trim();
-              String optionValue = option[1].trim();
-              options.put(optionsName, optionValue);
-              hasOptions = true;
-            }
-        }
-    }
-    
-    public BrowserConfigurationOptions() {}
-    
-    public String serialize() {
-        //"profile:XXXXXXXXXX"
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        for (String key : options.keySet()) {
-            if (first) {
-                first = false;
-            } else {
-                sb.append(';');
-            }
-            sb.append(key).append(':').append(options.get(key));
-        }
-        return sb.toString();
-    }
+  private Map<String, String> options = new HashMap<String, String>();
+  private boolean hasOptions = false;
 
-    public String getProfile() {
-        return options.get("profile");
-    }
-    
-    public boolean hasOptions() {
-      return hasOptions;
-    }
-    
-    public boolean isSingleWindow() {
-        return is("singleWindow");
-    }
-    
-    public void setSingleWindow(Boolean singleWindow) {
-        options.put("singleWindow", singleWindow.toString());
+  public BrowserConfigurationOptions(String browserConfiguration) {
+    //"name=value;name=value"
+    String[] optionsPairList = browserConfiguration.split(";");
+    for (int i = 0; i < optionsPairList.length; i++) {
+      String[] option = optionsPairList[i].split("=", 2);
+      if (2 == option.length) {
+        String optionsName = option[0].trim();
+        String optionValue = option[1].trim();
+        options.put(optionsName, optionValue);
         hasOptions = true;
-    }
-    
-    public String getExecutablePath() {
-        return options.get("executablePath");
-    }
-    
-    public void setExecutablePath(String executablePath) {
-        options.put("executablePath", executablePath);
-        hasOptions = true;
-    }
-    
-    public boolean isTimeoutSet() {
-      String value = options.get("timeoutInSeconds");
-      if (value == null)
-        return false;
-      else 
-        return true;
-    }
-
-    public boolean isCommandLineFlagsSet() {
-        String value = options.get("commandLineFlags");
-        if (value == null) 
-            return false;
-        else 
-            return true;
-    }
-    
-    public String getCommandLineFlags() {
-        return options.get("commandLineFlags");
-    }
-
-    public int getTimeoutInSeconds() {
-        String value = options.get("timeoutInSeconds");
-        if (value == null) return RemoteControlConfiguration.DEFAULT_TIMEOUT_IN_SECONDS;
-        return Integer.parseInt(value);
-    }
-
-    public boolean isAvoidingProxy() {
-      return is("avoidProxy");
-    }
-
-    public void setAvoidProxy(boolean avoidProxy) {
-      set("avoidProxy", avoidProxy);
-    }
-
-    public ProxyPac getProxyConfig() {
-      String raw = get("proxy");
-      if (raw == null) {
-        return null;
-      }
-
-      try {
-        return new JsonToBeanConverter().convert(ProxyPac.class, raw);
-      } catch (Exception e) {
-        throw new SeleniumException("Unable to retrieve proxy configuration", e);
       }
     }
+  }
 
-    public boolean isEnsuringCleanSession() {
-      return is("ensureCleanSession");
+  public BrowserConfigurationOptions() {
+  }
+
+  public String serialize() {
+    //"profile:XXXXXXXXXX"
+    StringBuilder sb = new StringBuilder();
+    boolean first = true;
+    for (String key : options.keySet()) {
+      if (first) {
+        first = false;
+      } else {
+        sb.append(';');
+      }
+      sb.append(key).append(':').append(options.get(key));
+    }
+    return sb.toString();
+  }
+
+  public String getProfile() {
+    return options.get("profile");
+  }
+
+  public boolean hasOptions() {
+    return hasOptions;
+  }
+
+  public boolean isSingleWindow() {
+    return is("singleWindow");
+  }
+
+  public void setSingleWindow(Boolean singleWindow) {
+    options.put("singleWindow", singleWindow.toString());
+    hasOptions = true;
+  }
+
+  public String getExecutablePath() {
+    return options.get("executablePath");
+  }
+
+  public void setExecutablePath(String executablePath) {
+    options.put("executablePath", executablePath);
+    hasOptions = true;
+  }
+
+  public boolean isTimeoutSet() {
+    String value = options.get("timeoutInSeconds");
+    if (value == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public boolean isCommandLineFlagsSet() {
+    String value = options.get("commandLineFlags");
+    if (value == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public String getCommandLineFlags() {
+    return options.get("commandLineFlags");
+  }
+
+  public int getTimeoutInSeconds() {
+    String value = options.get("timeoutInSeconds");
+    if (value == null) {
+      return RemoteControlConfiguration.DEFAULT_TIMEOUT_IN_SECONDS;
+    }
+    return Integer.parseInt(value);
+  }
+
+  public boolean isAvoidingProxy() {
+    return is("avoidProxy");
+  }
+
+  public void setAvoidProxy(boolean avoidProxy) {
+    set("avoidProxy", avoidProxy);
+  }
+
+  public ProxyPac getProxyConfig() {
+    String raw = get("proxy");
+    if (raw == null) {
+      return null;
     }
 
-    public boolean is(String key) {
-        String value = options.get(key);
-        if (value == null) return false;
-        return Boolean.parseBoolean(value);
+    try {
+      return new JsonToBeanConverter().convert(ProxyPac.class, raw);
+    } catch (Exception e) {
+      throw new SeleniumException("Unable to retrieve proxy configuration", e);
     }
-    
-    public String get(String key) {
-        return options.get(key);
+  }
+
+  public boolean isEnsuringCleanSession() {
+    return is("ensureCleanSession");
+  }
+
+  public boolean is(String key) {
+    String value = options.get(key);
+    if (value == null) {
+      return false;
     }
-    
-    public File getFile(String key) {
-        String value = options.get(key);
-        if (value == null) return null;
-        return new File(value);
+    return Boolean.parseBoolean(value);
+  }
+
+  public String get(String key) {
+    return options.get(key);
+  }
+
+  public File getFile(String key) {
+    String value = options.get(key);
+    if (value == null) {
+      return null;
     }
-    
-    public void set(String key, Object value) {
-        if (value == null) {
-            options.put(key, null);
-        } else {
-            options.put(key, value.toString());
-        }
+    return new File(value);
+  }
+
+  public void set(String key, Object value) {
+    if (value == null) {
+      options.put(key, null);
+    } else {
+      options.put(key, value.toString());
     }
-    
-    public void setSafely(String key, Object value) {
-        if (value == null) return;
-        set(key, value);
+  }
+
+  public void setSafely(String key, Object value) {
+    if (value == null) {
+      return;
     }
-    
-    /**
-     * Returns the serialization of this object, as defined by the serialize()
-     * method.
-     */
-    @Override
-    public String toString() {
-        return serialize();
-    }
+    set(key, value);
+  }
+
+  /**
+   * Returns the serialization of this object, as defined by the serialize()
+   * method.
+   */
+  @Override
+  public String toString() {
+    return serialize();
+  }
 }
