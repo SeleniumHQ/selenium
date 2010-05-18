b35f46685a208f8cd2e4f8dc290ff4a9c090d3cd
Simon M Stewart <simon.m.stewart@gmail.com>
2010-05-18 11:34:42 +0000

    SimonStewart: Formatting changes.
    r8935

--- remote/server/test/java/org/openqa/selenium/server/BrowserConfigurationOptionsTest.java
+++ remote/server/test/java/org/openqa/selenium/server/BrowserConfigurationOptionsTest.java
@@ -5,43 +5,48 @@ import junit.framework.TestCase;
 
 public class BrowserConfigurationOptionsTest extends TestCase {
 
-    public void testInitializationWithNoOptions() {
-      new BrowserConfigurationOptions("");
-    }
-    
-    public void testInitializationWithGoodSingleOption() {
-      BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile=foo");
-      assertEquals("foo", options.getProfile());
-      assertTrue(options.hasOptions());
-    }
-    
-    public void testInitializationWithGoodSingleOptionAndWhitespace() {
-      BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile= foo bar");
-      assertEquals("foo bar", options.getProfile());
-      assertTrue(options.hasOptions());
-    }
-    
-    public void testInitializationWithBadSingleOption() {
-      BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile_foo");
-      assertNull(options.getProfile());
-      assertFalse(options.hasOptions());
-    }
-    
-    public void testInitializationWithGoodOptionsAndWhitespace() {
-      BrowserConfigurationOptions options = 
+  public void testInitializationWithNoOptions() {
+    new BrowserConfigurationOptions("");
+  }
+
+  public void testInitializationWithGoodSingleOption() {
+    BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile=foo");
+    assertEquals("foo", options.getProfile());
+    assertTrue(options.hasOptions());
+  }
+
+  public void testInitializationWithGoodSingleOptionAndWhitespace() {
+    BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile= foo bar");
+    assertEquals("foo bar", options.getProfile());
+    assertTrue(options.hasOptions());
+  }
+
+  public void testInitializationWithBadSingleOption() {
+    BrowserConfigurationOptions options = new BrowserConfigurationOptions("profile_foo");
+    assertNull(options.getProfile());
+    assertFalse(options.hasOptions());
+  }
+
+  public void testInitializationWithGoodOptionsAndWhitespace() {
+    BrowserConfigurationOptions options =
         new BrowserConfigurationOptions("profile=foo ; unknown=bar");
-      assertEquals("foo", options.getProfile());
-      assertTrue(options.hasOptions());
+    assertEquals("foo", options.getProfile());
+    assertTrue(options.hasOptions());
+  }
+
+  public void testToStringEquivalentToSerialize() {
+    String[] tests = {
+        "",
+        "foo",
+        "foo bar",
+        null
+    };
+
+    BrowserConfigurationOptions options = new BrowserConfigurationOptions();
+
+    for (String test : tests) {
+      options.set("profile", test);
+      assertEquals(options.serialize(), options.toString());
     }
-    
-    public void testToStringEquivalentToSerialize() {
-        String[] tests = { "", "foo", "foo bar", null };
-        
-        BrowserConfigurationOptions options = new BrowserConfigurationOptions();
-        
-        for (String test : tests) {
-            options.set("profile", test);
-            assertEquals(options.serialize(), options.toString());
-        }
-    }
+  }
 }
