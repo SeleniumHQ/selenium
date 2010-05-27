c3eb014947b4e11ef4dec1bbcf38c36d039aa85c
Jari Bakken <jari.bakken@gmail.com>
2010-05-27 09:41:15 +0000

    JariBakken: Add rubydocs() and rubygem() mappings.
    r9007

--- rake-tasks/ruby.rb
+++ rake-tasks/ruby.rb
@@ -7,8 +7,8 @@ class RubyMappings
     fun.add_mapping "ruby_test", MRITest.new
     fun.add_mapping "ruby_test", AddDependencies.new
 
-    # TODO: fun.add_mapping("rubygem", RubyGem.new)
-    # TODO: fun.add_mapping("rubydocs", RubyDocs.new)
+    fun.add_mapping "rubydocs", RubyDocs.new
+    fun.add_mapping "rubygem", RubyGem.new
   end
 
   class RubyTasks < Tasks
@@ -63,10 +63,10 @@ class RubyMappings
         ENV['WD_SPEC_DRIVER'] = args[:driver_name] # TODO: get rid of ENV
 
         jruby :include     => args[:include],
-              :require     => req,
-              :command     => args[:command],
-              :files       => args[:srcs],
-              :objectspace => dir.include?("jobbie") # hack
+        :require     => req,
+        :command     => args[:command],
+        :files       => args[:srcs],
+        :objectspace => dir.include?("jobbie") # hack
       end
 
     end
@@ -80,13 +80,115 @@ class RubyMappings
         ENV['WD_SPEC_DRIVER'] = args[:driver_name] # TODO: get rid of ENV
 
         ruby :include => args[:include],
-             :require => args[:require],
-             :command => args[:command],
-             :files   => args[:srcs]
+        :require => args[:require],
+        :command => args[:command],
+        :files   => args[:srcs]
       end
     end
   end
 
+  class RubyDocs
+    def handle(fun, dir, args)
+      if have_yard?
+        define_task(args)
+      else
+        define_noop
+      end
+    end
+
+    def define_task(args)
+      files      = args[:srcs] || raise("no :srcs specified for rubydocs")
+      output_dir = args[:output_dir] || raise("no :output_dir specified for rubydocs")
+
+      files  = Array(files).map { |glob| Dir[glob] }.flatten
+
+      YARD::Rake::YardocTask.new("ruby:docs") do |t|
+        t.files = files
+        t.options << "--verbose"
+        t.options << "--readme" << args[:readme] if args.has_key?(:readme)
+        t.options << "--output-dir" << output_dir
+      end
+    end
+
+    def have_yard?
+      require 'yard'
+      true
+    rescue LoadError
+      false
+    end
+
+    def define_noop
+      task :rubydocs do
+        abort "YARD is not available."
+      end
+    end
+  end # RubyDocs
+
+  class RubyGem
+    def handle(fun, dir, args)
+      raise "no :srcs for rubygem" unless args[:srcs]
+      raise "no :version for rubygem" unless args[:version]
+
+      if has_gem_task?
+        define_gem_tasks(args)
+      else
+        define_noop
+      end
+    end
+
+    def has_gem_task?
+      require "rubygems"
+      require "rake/gempackagetask"
+
+      true
+    rescue LoadError
+      false
+    end
+
+    def define_gem_tasks(args)
+      namespace(:ruby) {
+        namespace(:gem) {
+          gemspec = spec(args)
+
+          Rake::GemPackageTask.new(gemspec) do |pkg|
+            pkg.package_dir = args[:output_dir]
+          end
+
+          task :clean do
+            rm_rf args[:output_dir]
+          end
+
+          desc 'Build and release the ruby gem to Gemcutter'
+          task :release => [:clean, :gem] do
+            sh "gem push #{args[:output_dir]}/#{gemspec.name}-#{gemspec.version}.gem"
+          end
+        }
+      }
+    end
+
+    def spec(args)
+      Gem::Specification.new do |s|
+        s.name          = 'selenium-webdriver'
+        s.version       = args[:version]
+        s.summary       = "The next generation developer focused tool for automated testing of webapps"
+        s.description   = "WebDriver is a tool for writing automated tests of websites. It aims to mimic the behaviour of a real user, and as such interacts with the HTML of the application."
+        s.authors       = ["Jari Bakken"]
+        s.email         = "jari.bakken@gmail.com"
+        s.homepage      = "http://selenium.googlecode.com"
+
+        s.add_dependency "json_pure"
+        s.add_dependency "ffi"
+
+        if s.respond_to? :add_development_dependency
+          s.add_development_dependency "rspec"
+          s.add_development_dependency "rack"
+        end
+
+        s.require_paths = args[:require_paths] if args.has_key?(:require_paths)
+        s.files         = args[:srcs].map { |e| Dir[e] }.flatten
+      end
+    end
+  end # RubyGem
 end # RubyMappings
 
 class RubyRunner
@@ -135,105 +237,3 @@ def ruby(opts)
 end
 
 
-
-#
-# docs
-#
-
-begin
-  require 'yard'
-  YARD::Rake::YardocTask.new(:rubydocs) do |t|
-    t.files   += Dir['chrome/src/rb/lib/**/*.rb']
-    t.files   += Dir['common/src/rb/lib/**/*.rb']
-    t.files   += Dir['firefox/src/rb/lib/**/*.rb']
-    t.files   += Dir['jobbie/src/rb/lib/**/*.rb']
-    t.files   += Dir['remote/client/src/rb/lib/**/*.rb']
-    t.options += %w[--verbose --readme common/src/rb/README --output-dir build/rubydocs]
-
-    if ENV['minimal']
-      t.options << "--no-private"
-    end
-  end
-rescue LoadError
-  task :rubydocs do
-    abort "YARD is not available. In order to run yardoc, you must: sudo gem install yard"
-  end
-end
-
-
-#
-# gem
-#
-
-begin
-  require "rubygems"
-  require "rake/gempackagetask"
-
-  PKG_DIR     = "build/gem"
-  GEM_VERSION = ENV['VERSION'] ||= '0.0.0'
-  GEM_SPEC    = Gem::Specification.new do |s|
-   s.name          = 'selenium-webdriver'
-   s.version       = GEM_VERSION
-   s.summary       = "The next generation developer focused tool for automated testing of webapps"
-   s.description   = "WebDriver is a tool for writing automated tests of websites. It aims to mimic the behaviour of a real user, and as such interacts with the HTML of the application."
-   s.authors       = ["Jari Bakken"]
-   s.email         = "jari.bakken@gmail.com"
-   s.homepage      = "http://selenium.googlecode.com"
-
-   s.add_dependency "json_pure"
-   s.add_dependency "ffi"
-
-   if s.respond_to? :add_development_dependency
-     s.add_development_dependency "rspec"
-     s.add_development_dependency "rack"
-   end
-
-   s.require_paths = []
-
-   s.files         += FileList['COPYING']
-
-   # Common
-   s.require_paths << 'common/src/rb/lib'
-   s.files         += FileList['common/src/rb/**/*']
-   s.files         += FileList['common/src/js/**/*']
-
-   # Firefox
-   s.require_paths << 'firefox/src/rb/lib'
-   s.files         += FileList['firefox/src/rb/**/*']
-   s.files         += FileList['firefox/src/extension/**/*']
-   s.files         += FileList['firefox/prebuilt/**/*']
-
-   # Chrome
-   s.require_paths << "chrome/src/rb/lib"
-   s.files         += FileList['chrome/src/rb/**/*']
-   s.files         += FileList['chrome/src/extension/**/*']
-   s.files         += FileList['chrome/prebuilt/**/*.dll']
-
-   # IE
-   s.require_paths << 'jobbie/src/rb/lib'
-   s.files         += FileList['jobbie/src/rb/**/*']
-   s.files         += FileList['jobbie/prebuilt/**/InternetExplorerDriver.dll']
-
-   # Remote
-   s.require_paths << 'remote/client/src/rb/lib'
-   s.files         += FileList['remote/client/src/rb/**/*']
-  end
-
-  namespace :gem do
-    Rake::GemPackageTask.new(GEM_SPEC) do |pkg|
-      pkg.package_dir = PKG_DIR
-    end
-
-    task :clean do
-      rm_rf PKG_DIR
-    end
-
-    desc 'Build and release the ruby gem to Gemcutter'
-    task :release => [:clean, :gem] do
-      sh "gem push #{PKG_DIR}/#{GEM_SPEC.name}-#{GEM_SPEC.version}.gem"
-    end
-  end
-
-rescue LoadError
-  # $stderr.puts "rubygems not installed - gem tasks unavailable"
-end
