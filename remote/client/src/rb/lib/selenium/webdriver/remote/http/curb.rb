2b18d83e2f07d75fbafa9d61fe143225990f0be9
Jari Bakken <jari.bakken@gmail.com>
2010-06-15 14:41:57 +0000

    JariBakken: Workaround for http://github.com/taf2/curb/issues/issue/33
    r9147

--- remote/client/src/rb/lib/selenium/webdriver/remote/http/curb.rb
+++ remote/client/src/rb/lib/selenium/webdriver/remote/http/curb.rb
@@ -4,20 +4,20 @@ module Selenium
   module WebDriver
     module Remote
       module Http
-        
+
         #
-        # An alternative to the default Net::HTTP client. 
-        # 
+        # An alternative to the default Net::HTTP client.
+        #
         # This can be used for the Firefox and Remote drivers if you have Curb
         # installed.
-        # 
+        #
         # @example Using Curb
-        # 
+        #   require 'selenium/webdriver/remote/http/curb'
         #   include Selenium
-        # 
+        #
         #   driver = WebDriver.for :firefox, :http_client => WebDriver::Remote::Http::Curb
-        # 
-        
+        #
+
         class Curb < Common
 
           private
@@ -25,6 +25,10 @@ module Selenium
           def request(verb, url, headers, payload)
             client.url     = url.to_s
             client.headers = headers
+
+            # http://github.com/taf2/curb/issues/issue/33
+            client.head   = false
+            client.delete = false
 
             case verb
             when :get
@@ -37,6 +41,8 @@ module Selenium
               client.http_put
             when :delete
               client.http_delete
+            when :head
+              client.http_head
             else
               raise Error::WebDriverError, "unknown HTTP verb: #{verb.inspect}"
             end
