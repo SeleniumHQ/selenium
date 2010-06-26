cee2ecedf8230b256c52fcf51c829497259a74b1
Jari Bakken <jari.bakken@gmail.com>
2010-06-26 11:36:15 +0000

    JariBakken: Fix scoping issue on Ruby 1.8 and make sure dirs exist when unzipping.
    r9225

--- common/src/rb/lib/selenium/webdriver/zip_helper.rb
+++ common/src/rb/lib/selenium/webdriver/zip_helper.rb
@@ -13,7 +13,10 @@ module Selenium
 
         Zip::ZipFile.open(path) do |zip|
           zip.each do |entry|
-            zip.extract(entry, File.join(destination, entry.name))
+            to = File.join(destination, entry.name)
+            FileUtils.mkdir_p File.dirname(to)
+
+            zip.extract(entry, to)
           end
         end
 
