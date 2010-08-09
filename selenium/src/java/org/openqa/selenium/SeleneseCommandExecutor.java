eaced521ad89701098fcbc101cddedffd9cf4cd9
Simon M Stewart <simon.m.stewart@gmail.com>
2010-08-09 16:45:14 +0000

    SimonStewart: Making it easier to modify the SeleneseCommandExecutor without subclassing it
    r9439

--- selenium/src/java/org/openqa/selenium/SeleneseCommandExecutor.java
+++ selenium/src/java/org/openqa/selenium/SeleneseCommandExecutor.java
@@ -17,54 +17,53 @@ limitations under the License.
 
 package org.openqa.selenium;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
 import com.thoughtworks.selenium.CommandProcessor;
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.HttpCommandProcessor;
 import com.thoughtworks.selenium.Selenium;
 import com.thoughtworks.selenium.SeleniumException;
-import com.google.common.collect.ImmutableMap;
-
 import org.openqa.selenium.firefox.internal.Executable;
-import org.openqa.selenium.internal.selenesedriver.SeleneseFunction;
 import org.openqa.selenium.internal.selenesedriver.ClearElement;
 import org.openqa.selenium.internal.selenesedriver.ClickElement;
+import org.openqa.selenium.internal.selenesedriver.ExecuteScript;
 import org.openqa.selenium.internal.selenesedriver.FindElement;
 import org.openqa.selenium.internal.selenesedriver.GetCurrentUrl;
+import org.openqa.selenium.internal.selenesedriver.GetElementAttribute;
 import org.openqa.selenium.internal.selenesedriver.GetElementText;
 import org.openqa.selenium.internal.selenesedriver.GetElementValue;
+import org.openqa.selenium.internal.selenesedriver.GetPageSource;
 import org.openqa.selenium.internal.selenesedriver.GetTagName;
 import org.openqa.selenium.internal.selenesedriver.GetTitle;
 import org.openqa.selenium.internal.selenesedriver.GetUrl;
-import org.openqa.selenium.internal.selenesedriver.NewSession;
-import org.openqa.selenium.internal.selenesedriver.QuitSelenium;
-import org.openqa.selenium.internal.selenesedriver.GetElementAttribute;
-import org.openqa.selenium.internal.selenesedriver.GetPageSource;
 import org.openqa.selenium.internal.selenesedriver.IsElementDisplayed;
 import org.openqa.selenium.internal.selenesedriver.IsElementEnabled;
 import org.openqa.selenium.internal.selenesedriver.IsElementSelected;
+import org.openqa.selenium.internal.selenesedriver.NewSession;
+import org.openqa.selenium.internal.selenesedriver.QuitSelenium;
+import org.openqa.selenium.internal.selenesedriver.SeleneseFunction;
 import org.openqa.selenium.internal.selenesedriver.SendKeys;
 import org.openqa.selenium.internal.selenesedriver.SetElementSelected;
 import org.openqa.selenium.internal.selenesedriver.SubmitElement;
 import org.openqa.selenium.internal.selenesedriver.ToggleElement;
-import org.openqa.selenium.internal.selenesedriver.ExecuteScript;
 import org.openqa.selenium.remote.BeanToJsonConverter;
 import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.CommandExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.ErrorCodes;
 import org.openqa.selenium.remote.JsonToBeanConverter;
 import org.openqa.selenium.remote.Response;
-import org.openqa.selenium.remote.DriverCommand;
-import org.openqa.selenium.remote.ErrorCodes;
-import static org.openqa.selenium.remote.DriverCommand.*;
 
-import java.io.File;
-import java.net.URL;
-import java.util.Map;
+import static org.openqa.selenium.remote.DriverCommand.*;
 
 public class SeleneseCommandExecutor implements CommandExecutor {
   private final ErrorCodes errorCodes;
   private final Selenium instance;
-  private Map<String, SeleneseFunction> functions;
+  private Map<String, SeleneseFunction> functions = Maps.newHashMap();
 
   public SeleneseCommandExecutor(URL seleniumServer, URL remoteAddress, Capabilities capabilities) {
     this(new HttpCommandProcessor(
@@ -73,7 +72,11 @@ public class SeleneseCommandExecutor implements CommandExecutor {
   }
 
   public SeleneseCommandExecutor(CommandProcessor processor) {
-    instance = new DefaultSelenium(processor);
+    this(new DefaultSelenium(processor));
+  }
+
+  public SeleneseCommandExecutor(Selenium instance) {
+    this.instance = instance;
     errorCodes = new ErrorCodes();
 
     prepareCommands();
@@ -113,33 +116,35 @@ public class SeleneseCommandExecutor implements CommandExecutor {
     return response;
   }
 
+  public void addCommand(String methodName, SeleneseFunction command) {
+    functions.put(methodName, command);
+  }
+
   private void prepareCommands() {
     FindElement findElement = new FindElement();
-    
-    functions = ImmutableMap.<String, SeleneseFunction>builder()
-        .put(CLEAR_ELEMENT, new ClearElement())
-        .put(CLICK_ELEMENT, new ClickElement())
-        .put(GET_CURRENT_URL, new GetCurrentUrl())
-        .put(EXECUTE_SCRIPT, new ExecuteScript())
-        .put(FIND_ELEMENT, findElement)
-        .put(IMPLICITLY_WAIT, findElement.implicitlyWait())
-        .put(GET, new GetUrl())
-        .put(GET_ELEMENT_ATTRIBUTE, new GetElementAttribute())
-        .put(GET_ELEMENT_TEXT, new GetElementText())
-        .put(GET_ELEMENT_VALUE, new GetElementValue())
-        .put(GET_ELEMENT_TAG_NAME, new GetTagName())
-        .put(GET_TITLE, new GetTitle())
-        .put(IS_ELEMENT_DISPLAYED, new IsElementDisplayed())
-        .put(IS_ELEMENT_ENABLED, new IsElementEnabled())
-        .put(IS_ELEMENT_SELECTED, new IsElementSelected())
-        .put(NEW_SESSION, new NewSession())
-        .put(GET_PAGE_SOURCE, new GetPageSource())
-        .put(SEND_KEYS_TO_ELEMENT, new SendKeys())
-        .put(SET_ELEMENT_SELECTED, new SetElementSelected())
-        .put(SUBMIT_ELEMENT, new SubmitElement())
-        .put(TOGGLE_ELEMENT, new ToggleElement())
-        .put(QUIT, new QuitSelenium())
-        .build();
+
+    addCommand(CLEAR_ELEMENT, new ClearElement());
+    addCommand(CLICK_ELEMENT, new ClickElement());
+    addCommand(GET_CURRENT_URL, new GetCurrentUrl());
+    addCommand(EXECUTE_SCRIPT, new ExecuteScript());
+    addCommand(FIND_ELEMENT, findElement);
+    addCommand(IMPLICITLY_WAIT, findElement.implicitlyWait());
+    addCommand(GET, new GetUrl());
+    addCommand(GET_ELEMENT_ATTRIBUTE, new GetElementAttribute());
+    addCommand(GET_ELEMENT_TEXT, new GetElementText());
+    addCommand(GET_ELEMENT_VALUE, new GetElementValue());
+    addCommand(GET_ELEMENT_TAG_NAME, new GetTagName());
+    addCommand(GET_TITLE, new GetTitle());
+    addCommand(IS_ELEMENT_DISPLAYED, new IsElementDisplayed());
+    addCommand(IS_ELEMENT_ENABLED, new IsElementEnabled());
+    addCommand(IS_ELEMENT_SELECTED, new IsElementSelected());
+    addCommand(NEW_SESSION, new NewSession());
+    addCommand(GET_PAGE_SOURCE, new GetPageSource());
+    addCommand(SEND_KEYS_TO_ELEMENT, new SendKeys());
+    addCommand(SET_ELEMENT_SELECTED, new SetElementSelected());
+    addCommand(SUBMIT_ELEMENT, new SubmitElement());
+    addCommand(TOGGLE_ELEMENT, new ToggleElement());
+    addCommand(QUIT, new QuitSelenium());
   }
 
   private static String startCommand(Capabilities capabilities) {
