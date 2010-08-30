67e58c6dd85a4fd84255606d196486aab075fcd6
Jari Bakken <jari.bakken@gmail.com>
2010-08-30 11:19:44 +0000

    JariBakken: Failing test for issue #768.
    r9558

---
+++ remote/client/test/rb/spec/http/default_spec.rb
@@ -0,0 +1,23 @@
+module Selenium
+  module WebDriver
+    module Remote
+      module Http
+
+        describe Default do
+
+          after { Default.timeout = nil }
+
+          it "uses the timeout set on the class" do
+            Default.timeout = 10
+
+            http = Default.new(URI.parse("http://example.com")).send :http
+            http.open_timeout.should == 10
+            http.read_timeout.should == 10
+          end
+        end
+
+      end # Http
+    end # Remote
+  end # WebDriver
+end # Selenium
+
