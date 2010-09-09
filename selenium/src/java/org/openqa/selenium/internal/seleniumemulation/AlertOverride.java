44ed9c097e896b12450ea400990c8cebeb9a0fb7
Simon M Stewart <simon.m.stewart@gmail.com>
2010-09-09 17:00:01 +0000

    SimonStewart: Implementing some of the confirmation functionality. Note that this won't return the correct results once the page has reloaded, but it's better than nothing.
    r9621

--- selenium/src/java/org/openqa/selenium/internal/seleniumemulation/AlertOverride.java
+++ selenium/src/java/org/openqa/selenium/internal/seleniumemulation/AlertOverride.java
@@ -17,30 +17,65 @@ limitations under the License.
 
 package org.openqa.selenium.internal.seleniumemulation;
 
+import com.thoughtworks.selenium.SeleniumException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.internal.Holder;
 
 public class AlertOverride {
   public void replaceAlertMethod(WebDriver driver) {
     ((JavascriptExecutor) driver).executeScript(
       "if (window.__webdriverAlerts) { return; } " +
       "window.__webdriverAlerts = []; " +
-      "window.alert = function(msg) { window.__webdriverAlerts.push(msg); }"
+      "window.alert = function(msg) { window.__webdriverAlerts.push(msg); }; " +
+      "window.__webdriverConfirms = []; " +
+      "window.__webdriverNextConfirm = true; " +
+      "window.confirm = function(msg) { " +
+      "  window.__webdriverConfirms.push(msg); " +
+      "  var res = window.__webdriverNextConfirm; " +
+      "  window.__webdriverNextConfirm = true; " +
+      "  return res; " +
+      "};"
     );
   }
 
   public String getNextAlert(WebDriver driver) {
-    return (String) ((JavascriptExecutor) driver).executeScript(
+    String result = (String) ((JavascriptExecutor) driver).executeScript(
       "if (!window.__webdriverAlerts) { return null }; " +
       "var t = window.__webdriverAlerts.shift();" +
       "if (t) { t = t.replace(/\\n/g, ' '); } " +
       "return t;"
     );
+
+    if (result == null) {
+      throw new SeleniumException("There were no alerts");
+    }
+
+    return result;
   }
 
   public boolean isAlertPresent(WebDriver driver) {
     return Boolean.TRUE.equals(((JavascriptExecutor) driver).executeScript(
       "return window.__webdriverAlerts && window.__webdriverAlerts.length > 0;"
+    ));
+  }
+
+  public String getNextConfirmation(WebDriver driver) {
+    String result = (String) ((JavascriptExecutor) driver).executeScript(
+        "if (!window.__webdriverConfirms) { return null; } " + 
+        "return window.__webdriverConfirms.shift();"
+    );
+
+    if (result == null) {
+      throw new SeleniumException("There were no confirmations");
+    }
+
+    return result;
+  }
+
+  public boolean isConfirmationPresent(WebDriver driver) {
+    return Boolean.TRUE.equals(((JavascriptExecutor) driver).executeScript(
+      "return window.__webdriverConfirms && window.__webdriverConfirms.length > 0;"
     ));
   }
 }
---
+++ selenium/src/java/org/openqa/selenium/internal/seleniumemulation/GetConfirmation.java
@@ -0,0 +1,21 @@
+// Copyright 2010 Google Inc.  All Rights Reserved.
+
+package org.openqa.selenium.internal.seleniumemulation;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author simonstewart@google.com (Simon Stewart)
+ */
+public class GetConfirmation extends SeleneseCommand {
+  private final AlertOverride alertOverride;
+
+  public GetConfirmation(AlertOverride alertOverride) {
+    this.alertOverride = alertOverride;
+  }
+
+  @Override
+  protected Object handleSeleneseCommand(WebDriver driver, String locator, String value) {
+    return alertOverride.getNextConfirmation(driver);
+  }
+}
