da846d19c12318bc82238a206953b49ac84f8eef
Jason Lebya <jmleyba@gmail.com>
2010-09-12 05:20:43 +0000

    JasonLeyba: Whitespace cleanup.
    r9627

--- common/src/java/org/openqa/selenium/Cookie.java
+++ common/src/java/org/openqa/selenium/Cookie.java
@@ -21,24 +21,24 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class Cookie {
-    private final String name;
-    private final String value;
-    private final String path;
-    private final String domain;
-    private final Date expiry;
+  private final String name;
+  private final String value;
+  private final String path;
+  private final String domain;
+  private final Date expiry;
 
-    /**
-     * Creates a cookie. If the path is left blank or set to null it will be
-     * set to "/"
-     *
-     * @param name name cannot be null or empty string
-     * @param value value can be an empty string but not be null
-     * @param path The path to use. Will default to "/"
-     * @param expiry expiry can be null
-     */
-    public Cookie(String name, String value, String path, Date expiry) {
-      this(name, value, null, path, expiry);
-    }
+  /**
+   * Creates a cookie. If the path is left blank or set to null it will be
+   * set to "/"
+   *
+   * @param name   name cannot be null or empty string
+   * @param value  value can be an empty string but not be null
+   * @param path   The path to use. Will default to "/"
+   * @param expiry expiry can be null
+   */
+  public Cookie(String name, String value, String path, Date expiry) {
+    this(name, value, null, path, expiry);
+  }
 
   public Cookie(String name, String value, String domain, String path, Date expiry) {
     this.name = name;
@@ -60,91 +60,95 @@ public class Cookie {
    * Create a cookie for the default path with the given name and value with
    * no expiry set.
    *
-   * @param name The cookie's name
+   * @param name  The cookie's name
    * @param value The cookie's value
    */
-    public Cookie(String name, String value) {
-        this(name, value, "/", null);
-    }
+  public Cookie(String name, String value) {
+    this(name, value, "/", null);
+  }
 
   /**
    * Create a cookie.
    *
-   * @param name The cookie's name
+   * @param name  The cookie's name
    * @param value The cookie's value
-   * @param path The path the cookie is for
+   * @param path  The path the cookie is for
    */
-    public Cookie(String name, String value, String path) {
-        this(name, value, path, null);
+  public Cookie(String name, String value, String path) {
+    this(name, value, path, null);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public String getDomain() {
+    return domain;
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public boolean isSecure() {
+    return false;
+  }
+
+  public Date getExpiry() {
+    return expiry;
+  }
+
+  protected void validate() {
+    if (name == null || "".equals(name) || value == null || path == null) {
+      throw new IllegalArgumentException("Required attributes are not set or " +
+                                         "any non-null attribute set to null");
     }
 
-    public String getName() {
-        return name;
+    if (name.indexOf(';') != -1) {
+      throw new IllegalArgumentException(
+          "Cookie names cannot contain a ';': " + name);
     }
+  }
 
-    public String getValue() {
-        return value;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public boolean isSecure() {
-        return false;
-    }
-
-    public Date getExpiry() {
-        return expiry;
-    }
-
-    protected void validate() {
-        if (name == null || "".equals(name) || value == null || path == null)
-            throw new IllegalArgumentException("Required attributes are not set or " +
-                    "any non-null attribute set to null");
-
-        if (name.indexOf(';') != -1)
-            throw new IllegalArgumentException(
-                    "Cookie names cannot contain a ';': " + name);
-    }
-
-    @Override
-    public String toString() {
-        return name + "=" + value 
-                + (expiry == null ? "" : "; expires=" + new SimpleDateFormat("EEE, dd-MM-yyyy hh:mm:ss z").format(expiry))
-                + ("".equals(path) ? "" : "; path=" + path)
-                + (domain == null ? "" : "; domain=" + domain);
+  @Override
+  public String toString() {
+    return name + "=" + value
+           + (expiry == null ? ""
+                             : "; expires=" + new SimpleDateFormat("EEE, dd-MM-yyyy hh:mm:ss z")
+                                 .format(expiry))
+           + ("".equals(path) ? "" : "; path=" + path)
+           + (domain == null ? "" : "; domain=" + domain);
 //                + (isSecure ? ";secure;" : "");
+  }
+
+  /**
+   * Two cookies are equal if the name and value match
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Cookie)) {
+      return false;
     }
 
-    /**
-     *  Two cookies are equal if the name and value match
-     */
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (!(o instanceof Cookie)) {
-        return false;
-      }
+    Cookie cookie = (Cookie) o;
 
-      Cookie cookie = (Cookie) o;
-
-      if (!name.equals(cookie.name)) {
-        return false;
-      }
-      return !(value != null ? !value.equals(cookie.value) : cookie.value != null);
+    if (!name.equals(cookie.name)) {
+      return false;
     }
+    return !(value != null ? !value.equals(cookie.value) : cookie.value != null);
+  }
 
-    @Override
-    public int hashCode() {
-      return name.hashCode();
-    }
+  @Override
+  public int hashCode() {
+    return name.hashCode();
+  }
 
   public static class Builder {
 
