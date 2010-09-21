f05f6887113d515f70a4fdc5bb0a6116570f60e1
Simon M Stewart <simon.m.stewart@gmail.com>
2010-09-21 14:56:52 +0000

    SimonStewart: ignoring failing test for the IE driver.
    r9680

--- common/test/java/org/openqa/selenium/FrameSwitchingTest.java
+++ common/test/java/org/openqa/selenium/FrameSwitchingTest.java
@@ -299,26 +299,25 @@ public class FrameSwitchingTest extends AbstractDriverTestCase {
     assertThat(driver.getCurrentUrl(), equalTo(url));
   }
   
-  @Ignore(SELENESE)
-  public void testShouldBeAbleToCarryOnWorkingIfTheFrameIsDeletedFromUnderUs()
-        {
-            driver.get(pages.deletingFrame);
-            
-            driver.switchTo().frame("iframe1");
-            
-            WebElement killIframe = driver.findElement(By.id("killIframe"));
-            killIframe.click();
+  @Ignore({SELENESE, IE})
+  public void testShouldBeAbleToCarryOnWorkingIfTheFrameIsDeletedFromUnderUs() {
+    driver.get(pages.deletingFrame);
 
-            driver.switchTo().frame(0);
-            WebElement addIFrame = driver.findElement(By.id("addBackFrame"));
-            addIFrame.click();
+    driver.switchTo().frame("iframe1");
 
-            driver.switchTo().frame("iframe1");
-            try {
-                driver.findElement(By.id("checkbox"));
-            }
-            catch (WebDriverException web) {
-                fail("Could not find element after switching frame");
-            }
-        }
+    WebElement killIframe = driver.findElement(By.id("killIframe"));
+    killIframe.click();
+
+    driver.switchTo().frame(0);
+    WebElement addIFrame = driver.findElement(By.id("addBackFrame"));
+    addIFrame.click();
+
+    driver.switchTo().frame("iframe1");
+    try {
+      driver.findElement(By.id("checkbox"));
+    }
+    catch (WebDriverException web) {
+      fail("Could not find element after switching frame");
+    }
+  }
 }
