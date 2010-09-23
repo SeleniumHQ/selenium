f54cd231149a2621690d3847c42a2a6667f8e3d3
Jason Lebya <jmleyba@gmail.com>
2010-09-23 16:15:10 +0000

    JasonLeyba: it would help if r9709 had a test that actually passed.
    r9710

--- selenium/test/java/org/openqa/selenium/internal/seleniumemulation/TimerTest.java
+++ selenium/test/java/org/openqa/selenium/internal/seleniumemulation/TimerTest.java
@@ -3,16 +3,24 @@ package org.openqa.selenium.internal.seleniumemulation;
 import junit.framework.TestCase;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.RejectedExecutionException;
 
 public class TimerTest extends TestCase {
 
   public void testCannotExecuteCommandsAfterStoppingTheTimer() {
     Timer timer = new Timer(250);
     timer.stop();
-    timer.run(new Callable<Void>() {
-      public Void call() throws Exception {
-        return null;
-      }
-    });
+    try {
+      timer.run(new Callable<Void>() {
+        public Void call() throws Exception {
+          return null;
+        }
+      });
+      fail();
+    } catch (RuntimeException ex) {
+      Throwable cause = ex.getCause();
+      assertNotNull(cause);
+      assertTrue(cause instanceof RejectedExecutionException);
+    }
   }
 }
