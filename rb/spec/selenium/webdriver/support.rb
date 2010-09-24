9f987d73e5e26d9b280b1ea8133537a72768c3b4
Jari Bakken <jari.bakken@gmail.com>
2010-09-24 16:08:37 +0000

    JariBakken: Fix the spec run on MRI
    r9721

---
+++ rb/spec/selenium/webdriver/support.rb
@@ -0,0 +1,13 @@
+require "selenium/webdriver/support/remote_server"
+require "selenium/webdriver/support/test_environment"
+require "selenium/webdriver/support/jruby_test_environment"
+require "selenium/webdriver/support/guards"
+require "selenium/webdriver/support/helpers"
+
+module Selenium
+  module WebDriver
+    module SpecSupport
+      autoload :RackServer, "selenium/webdriver/support/rack_server"
+    end
+  end
+end
--- rb/spec/spec_helper.rb
+++ rb/spec/spec_helper.rb
@@ -1,18 +1,6 @@
 require "spec"
 require "selenium-webdriver"
-require "selenium/webdriver/support/remote_server"
-require "selenium/webdriver/support/test_environment"
-require "selenium/webdriver/support/jruby_test_environment"
-require "selenium/webdriver/support/guards"
-require "selenium/webdriver/support/helpers"
-
-module Selenium
-  module WebDriver
-    module SpecSupport
-      autoload :RackServer, "selenium/webdriver/spec_support/rack_server"
-    end
-  end
-end
+require "selenium/webdriver/support"
 
 include Selenium
 
