ceac423ee5909929a6c390325e0efff34ee22524
Jari Bakken <jari.bakken@gmail.com>
2010-10-27 10:58:14 +0000

    JariBakken: Removing old se-client test dirs - it's all under rb/spec/ now.
    r10080

--- rb/test/integration/api/start_stop_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-describe "Session Management" do
-
-	it "Can call stop even when session was not started" do
-    3.times { selenium_driver.close_current_browser_session }
-  end
-
-end
--- rb/test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'test/unit'
-require 'selenium-client'
