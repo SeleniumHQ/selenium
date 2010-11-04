a4c48ef83463ec0fd4ff9fec284d6b49d310e763
Jari Bakken <jari.bakken@gmail.com>
2010-11-04 16:25:14 +0000

    JariBakken: Clean up requires.
    r10159

--- rb/lib/selenium-client.rb
+++ rb/lib/selenium-client.rb
@@ -1,4 +1,2 @@
-# Original code by Aslak Hellesoy and Darren Hobbs
-
 require "selenium/client"
 
--- rb/lib/selenium/client.rb
+++ rb/lib/selenium/client.rb
@@ -8,21 +8,21 @@ require 'cgi'
 require "digest/md5"
 require 'fileutils'
 require 'tmpdir'
-require File.expand_path(File.dirname(__FILE__) + '/client/tcp_socket_extension')
-require File.expand_path(File.dirname(__FILE__) + '/client/errors')
-require File.expand_path(File.dirname(__FILE__) + '/client/protocol')
-require File.expand_path(File.dirname(__FILE__) + '/client/legacy_driver')
-require File.expand_path(File.dirname(__FILE__) + '/client/javascript_expression_builder')
-require File.expand_path(File.dirname(__FILE__) + '/client/javascript_frameworks/prototype')
-require File.expand_path(File.dirname(__FILE__) + '/client/javascript_frameworks/jquery')
-require File.expand_path(File.dirname(__FILE__) + '/client/extensions')
-require File.expand_path(File.dirname(__FILE__) + '/client/idiomatic')
-require File.expand_path(File.dirname(__FILE__) + '/client/base')
-require File.expand_path(File.dirname(__FILE__) + '/client/driver')
-require File.expand_path(File.dirname(__FILE__) + '/client/selenium_helper')
-require File.expand_path(File.dirname(__FILE__) + '/client/server_control')
-require File.expand_path(File.dirname(__FILE__) + '/rake/selenium_server_start_task')
-require File.expand_path(File.dirname(__FILE__) + '/rake/selenium_server_stop_task')
+
+require 'selenium/client/tcp_socket_extension'
+require 'selenium/client/errors'
+require 'selenium/client/protocol'
+require 'selenium/client/legacy_driver'
+require 'selenium/client/javascript_expression_builder'
+require 'selenium/client/javascript_frameworks/prototype'
+require 'selenium/client/javascript_frameworks/jquery'
+require 'selenium/client/extensions'
+require 'selenium/client/idiomatic'
+require 'selenium/client/base'
+require 'selenium/client/driver'
+require 'selenium/client/selenium_helper'
+require 'selenium/server'
+require 'selenium/rake/server_task'
 
 # Backward compatibility
 
