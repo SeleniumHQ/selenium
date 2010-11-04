0b77fe2914dd63c80becf467aab1f27f29ff6122
Jari Bakken <jari.bakken@gmail.com>
2010-11-04 16:09:21 +0000

    JariBakken: Delete the old rake tasks.
    r10158

--- rb/lib/selenium/rake/tasks.rb
+++
@@ -1,6 +0,0 @@
-require "net/http"
-require File.expand_path(File.dirname(__FILE__) + '/../../nautilus/shell')
-require File.expand_path(File.dirname(__FILE__) + '/../../tcp_socket_extension')
-require File.expand_path(File.dirname(__FILE__) + '/../server_control/server_control')
-require File.expand_path(File.dirname(__FILE__) + '/selenium_server_start_task')
-require File.expand_path(File.dirname(__FILE__) + '/selenium_server_stop_task')
