696731591bf2384754a98be7aaa4f102cdf916d9
Jason Lebya <jmleyba@gmail.com>
2010-11-07 20:05:20 +0000

    JasonLeyba:  Whitespace cleanup.
    r10203

--- common/src/java/org/openqa/selenium/JavascriptExecutor.java
+++ common/src/java/org/openqa/selenium/JavascriptExecutor.java
@@ -21,34 +21,34 @@ package org.openqa.selenium;
  * Indicates that a driver can execute Javascript, providing access to the mechanism to do so.
  */
 public interface JavascriptExecutor {
-    /**
-     * Execute javascript in the context of the currently selected frame or
-     * window. This means that "document" will refer to the current document.
-     * If the script has a return value, then the following steps will be taken:
-     *
-     * <ul> <li>For an HTML element, this method returns a WebElement</li>
-     * <li>For a number, a Long is returned</li>
-     * <li>For a boolean, a Boolean is returned</li>
-     * <li>For all other cases, a String is returned.</li>
-     * <li>For an array, return a List&lt;Object&gt; with each object
-     * following the rules above.  We support nested lists.</li>
-     * <li>Unless the value is null or there is no return value,
-     * in which null is returned</li> </ul>
-     *
-     * <p>Arguments must be a number, a boolean, a String, WebElement,
-     * or a List of any combination of the above. An exception will be
-     * thrown if the arguments do not meet these criteria. The arguments
-     * will be made available to the javascript via the "arguments" magic
-     * variable, as if the function were called via "Function.apply"
-     *
-     * @param script The javascript to execute
-     * @param args The arguments to the script. May be empty
-     * @return One of Boolean, Long, String, List or WebElement. Or null.
-     */
-    Object executeScript(String script, Object... args);
+  /**
+   * Execute javascript in the context of the currently selected frame or
+   * window. This means that "document" will refer to the current document.
+   * If the script has a return value, then the following steps will be taken:
+   *
+   * <ul> <li>For an HTML element, this method returns a WebElement</li>
+   * <li>For a number, a Long is returned</li>
+   * <li>For a boolean, a Boolean is returned</li>
+   * <li>For all other cases, a String is returned.</li>
+   * <li>For an array, return a List&lt;Object&gt; with each object
+   * following the rules above.  We support nested lists.</li>
+   * <li>Unless the value is null or there is no return value,
+   * in which null is returned</li> </ul>
+   *
+   * <p>Arguments must be a number, a boolean, a String, WebElement,
+   * or a List of any combination of the above. An exception will be
+   * thrown if the arguments do not meet these criteria. The arguments
+   * will be made available to the javascript via the "arguments" magic
+   * variable, as if the function were called via "Function.apply"
+   *
+   * @param script The javascript to execute
+   * @param args The arguments to the script. May be empty
+   * @return One of Boolean, Long, String, List or WebElement. Or null.
+   */
+  Object executeScript(String script, Object... args);
 
-    /**
-     * It's not enough to simply support javascript, it also needs to be enabled too.
-     */
-      boolean isJavascriptEnabled();
+  /**
+   * It's not enough to simply support javascript, it also needs to be enabled too.
+   */
+  boolean isJavascriptEnabled();
 }
