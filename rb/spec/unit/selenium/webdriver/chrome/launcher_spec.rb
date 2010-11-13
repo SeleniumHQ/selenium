bc8b470534aeaa488b0ae0da55c077768fe193ee
Jari Bakken <jari.bakken@gmail.com>
2010-11-13 10:55:54 +0000

    JariBakken: Fixing the Chrome::Launcher spec
    r10314

--- rb/spec/unit/selenium/webdriver/chrome/launcher_spec.rb
+++ rb/spec/unit/selenium/webdriver/chrome/launcher_spec.rb
@@ -3,21 +3,25 @@ require File.expand_path("../../spec_helper", __FILE__)
 module Selenium
   module WebDriver
     module Chrome
+
       describe Launcher do
         let(:mock_process) do
           mock("ChildProcess", :io => mock.as_null_object, :start => true)
         end
 
         it "uses the user-provided path if set" do
-          Chrome.path = Gem.ruby
+          Platform.stub!(:assert_executable).with("/some/path")
+          Chrome.path = "/some/path"
+
           ChildProcess.should_receive(:build).
-                       with { |exec| exec == Gem.ruby }.
+                       with { |*args| args.first.should == "/some/path" }.
                        and_return(mock_process)
 
           Launcher.launcher.launch(nil)
         end
       end
-    end # Firefox
+
+    end # Chrome
   end # WebDriver
 end # Selenium
 
