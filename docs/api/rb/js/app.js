af829b1fdd50af6e1fa010c0202ed28cd4044ae7
Jari Bakken <jari.bakken@gmail.com>
2010-11-29 13:14:22 +0000

    JariBakken: Update the Ruby API docs.
    r10442

--- docs/api/rb/js/app.js
+++ docs/api/rb/js/app.js
@@ -152,7 +152,7 @@ function generateTOC() {
     show = true;
     var thisTag = parseInt(this.tagName[1]);
     if (this.id.length == 0) {
-      var proposedId = $(this).text().replace(/[^a-z0-9:'"\.()=-]/ig, '_');
+      var proposedId = $(this).text().replace(/[^a-z0-9:\.()=-]/ig, '_');
       if ($('#' + proposedId).length > 0) proposedId += counter++;
       this.id = proposedId;
     }
