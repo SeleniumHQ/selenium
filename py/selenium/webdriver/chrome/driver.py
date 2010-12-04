4669fc2698732b82adc650ee30af8994374ea46a
Davis Burns <david.burns@theautomatedtester.co.uk>
2010-12-04 18:23:27 +0000

    DavidBurns with AnthonyLong fixes for # 1077, # 912 and #997
    r10515

--- py/selenium/webdriver/chrome/driver.py
+++ py/selenium/webdriver/chrome/driver.py
@@ -1,5 +1,5 @@
-# Copyright 2008-2009 WebDriver committers
-# Copyright 2008-2009 Google Inc.
+# Copyright 2008-2010 WebDriver committers
+# Copyright 2008-2010 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,29 +12,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from __future__ import with_statement
 import os
 from selenium.webdriver.common.exceptions import RemoteDriverServerException
 from selenium.webdriver.remote import utils
-from subprocess import Popen
+import subprocess
 import httplib
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from threading import Thread
-from Queue import Queue
+import BaseHTTPServer
+import SimpleHTTPServer
+import threading
+import Queue
+import time
+import urllib
+import sys
+import platform
+import tempfile
+import shutil
+import signal
 try:
     import json
 except ImportError:
     import simplejson as json
-from time import sleep, time
-from urllib import urlopen
-from os.path import expanduser, join, dirname, abspath, isdir, exists
-from sys import platform
-from tempfile import mkdtemp
-import shutil
-#from shutil import shutil.copytree, shutil.rmtree, copy
-from os import environ
 
 INITIAL_HTML = '''
 <html>
@@ -52,7 +50,7 @@ INITIAL_HTML = '''
     </body>
 </html>'''
 
-class RequestHandler(SimpleHTTPRequestHandler):
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.respond(INITIAL_HTML, "text/html")
     # From my (Miki Tebeka) understanding, the driver works by sending a POST http
@@ -78,11 +76,9 @@ class RequestHandler(SimpleHTTPRequestHandler):
             if line.strip() == "EOResponse":
                 break
             lines.append(line)
-
         data = "".join(lines).strip()
         if not data:
             return
-
         self.server.result_queue.put(json.loads(data))
 
     def respond(self, data, content_type):
@@ -103,54 +99,57 @@ def _find_chrome_in_registry():
     try:
         key = OpenKey(HKEY_CURRENT_USER, path)
         install_dir = QueryValue(key, "InstallLocation")
-    except WindowsError:
+    except OSError.WindowsError:
         return ""
 
-    return join(install_dir, "chrome.exe")
+    return os.path.join(install_dir, "chrome.exe")
 
 def _is_win7():
-    import sys
     return sys.getwindowsversion()[0] == 6
 
 def _default_windows_location():
     if _is_win7():
-        appdata = environ["LOCALAPPDATA"]
+        appdata = os.environ["LOCALAPPDATA"]
     else:
-        home = expanduser("~")
-        appdata = join(home, "Local Settings", "Application Data")
+        home = os.path.expanduser("~")
+        appdata = os.path.join(home, "Local Settings", "Application Data")
 
-    return join(appdata, "Google", "Chrome", "Application", "chrome.exe")
+    return os.path.join(appdata, "Google", "Chrome", "Application", "chrome.exe")
 
 def _windows_chrome():
     return _find_chrome_in_registry() or _default_windows_location()
 
 def _linux_chrome():
-    locations = ["/usr/bin/google-chrome","/usr/bin/chromium","/usr/bin/chromium-browser"]
+    locations = [
+        "/usr/bin/google-chrome",
+        "/usr/bin/chromium",
+        "/usr/bin/chromium-browser",
+    ]
     return _start_cmd(locations)
 
 def _mac_chrome():
     locations = ["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"]
     import getpass
-    locations.append(join("/Users/", getpass.getuser(), "Applications", "Google Chrome.app", "Contents", "MacOS", "Google Chrome"))
+    locations.append(os.path.join("/Users/", getpass.getuser(), "Applications", "Google Chrome.app", "Contents", "MacOS", "Google Chrome"))
     return _start_cmd(locations)
 
 def _start_cmd(locations):
     cmd = ""
     for cmd_path in locations:
-      if exists(cmd_path):
-        cmd = cmd_path
-    if cmd=="":
-      raise RuntimeError("Unable to find browser")
+        if os.path.exists(cmd_path):
+            cmd = cmd_path
+    if cmd == "":
+        raise RuntimeError("Unable to find browser")
     return cmd
 
 _BINARY = {
-    "win32" : _windows_chrome,
-    "darwin" : _mac_chrome,
-    "linux2" : _linux_chrome 
+    "win32": _windows_chrome,
+    "darwin": _mac_chrome,
+    "linux2": _linux_chrome,
 }
 
 def chrome_exe():
-    return _BINARY[platform]()
+    return _BINARY[sys.platform]()
 
 def touch(filename):
     with open(filename, "ab"):
@@ -164,32 +163,36 @@ def create_extension_dir():
     extension_dir = _copy_zipped_extension("chrome-extension.zip")
     if extension_dir:
         return extension_dir
-
-    extension_dir = join(dirname(abspath(__file__)), "chrome-extension.zip")
+    extension_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "chrome-extension.zip")
     extension_dir = _copy_zipped_extension(extension_dir)
     if extension_dir:
         return extension_dir
-
-    path = mkdtemp()
-
+    path = tempfile.mkdtemp()
     # FIXME: Copied manually
-    extdir = join(dirname(abspath(__file__)), "extension")
-    if not isdir(extdir):
-        extdir = join(dirname(dirname(abspath(__file__))), "extension")
-        assert isdir(extdir), "can't find extension"
+    extdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "extension")
+    if not os.path.isdir(extdir):
+        extdir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "extension")
+        assert os.path.isdir(extdir), "can't find extension"
     # shutil.copytree need to create the directory
     shutil.rmtree(path)
     shutil.copytree(extdir, path)
     return path
 
 def create_profile_dir():
-    path = mkdtemp()
+    path = tempfile.mkdtemp()
     return path
 
-# FIXME: Find a free one dinamically
-PORT = 33292
+PORT = 0
 
-def run_chrome(extension_dir, profile_dir, port,untrusted_certificates, custom_args):
+def on_error(func, path, exc_info):
+    import stat
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        pass
+
+def run_chrome(extension_dir, profile_dir, port, untrusted_certificates, custom_args):
     command = [
         chrome_exe(),
         "--load-extension=%s" % extension_dir,
@@ -203,31 +206,30 @@ def run_chrome(extension_dir, profile_dir, port,untrusted_certificates, custom_a
         "--no-default-browser-check",
         "http://localhost:%s/chromeCommandExecutor" % port,
         untrusted_certificates,
-        custom_args]
-    return Popen(command)
+        custom_args,
+    ]
+    return subprocess.Popen(command)
 
 def run_server(timeout=10):
-    server = HTTPServer(("", 0), RequestHandler)
-    server.command_queue = Queue()
-    server.result_queue = Queue()
-    t = Thread(target=server.serve_forever)
+    server = BaseHTTPServer.HTTPServer(("", 0), RequestHandler)
+    server.command_queue = Queue.Queue()
+    server.result_queue = Queue.Queue()
+    t = threading.Thread(target=server.serve_forever)
     t.daemon = True
     t.start()
-
-    start = time()
-    while time() - start < timeout:
+    start = time.time()
+    while time.time() - start < timeout:
         try:
-            urlopen("http://localhost:%s" % server.server_port)
+            urllib.urlopen("http://localhost:%s" % server.server_port)
             break
         except IOError:
-            sleep(0.1)
+            time.sleep(0.1)
     else:
         raise RemoteDriverServerException("Can't open server after %s seconds" % timeout)
-
     return server
 
 class ChromeDriver:
-    def __init__(self,custom_profile=None, untrusted_certificates=False, custom_args=""):
+    def __init__(self, custom_profile=None, untrusted_certificates=False, custom_args=""):
         self._server = None
         self._profile_dir = custom_profile
         self._extension_dir = None
@@ -237,38 +239,35 @@ class ChromeDriver:
 
     def start(self):
         self._extension_dir = create_extension_dir()
-        self._profile_dir = create_profile_dir() if self._profile_dir else self._profile_dir
+        self._profile_dir = create_profile_dir() if self._profile_dir is None else self._profile_dir
         self._server = run_server()
         self._chrome = run_chrome(self._extension_dir, self._profile_dir,
                                   self._server.server_port, self._untrusted_certificates,
                                   self._custom_args)
 
     def stop(self):
-        if self._chrome:
-            try:
-                self._chrome.kill()
-                self._chrome.wait()
-                self._chrome = None
-            except AttributeError:
-                # POpen.kill is a python 2.6 API...
-                pass
-
+        try:
+            os.kill(self._chrome.pid, signal.SIGTERM)
+        except AttributeError:
+            if platform.version() == "Windows":
+                import ctypes
+                PROCESS_TERMINATE = 1
+                handle = ctypes.windll.kernel32.OpenProcess(PROCESS_TERMINATE, False, self._chrome.pid)
+                ctypes.windll.kernel32.TerminateProcess(handle, -1)
+                ctypes.windll.kernel32.CloseHandle(handle)
+        self._chrome = None
         if self._server:
             self._server.server_close()
             self._server = None
-
         for path in (self._profile_dir, self._extension_dir):
-            print path
-            if not path:
-                continue
-            for i in xrange(5):
-                if os.path.exists(path):
-                    shutil.rmtree(path)
-            if os.path.exists(path):
-                return "Couldn't delete profile. Please try manually."
+            try:
+                shutil.rmtree(path, ignore_errors=False, onerror=on_error)
+            except OSError:
+                pass
 
     def execute(self, command, params):
         to_send = params.copy()
         to_send["request"] = command
         self._server.command_queue.put(to_send)
         return self._server.result_queue.get()
+    
