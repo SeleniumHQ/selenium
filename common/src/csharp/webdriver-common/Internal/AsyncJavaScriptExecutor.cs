d774cc4ac53b7f5365f4ba5bf848161acf4d0184
Jim Evans <james.h.evans.jr@gmail.com>
2010-12-20 18:04:19 +0000

    JimEvans: Adding unmerged .NET files from branch for asynchronous javascript execution.
    r10705

---
+++ common/src/csharp/webdriver-common/Internal/AsyncJavaScriptExecutor.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace OpenQA.Selenium.Internal
+{
+    /// <summary>
+    /// Utility class used to execute "asynchronous" scripts. This class should
+    /// only be used by browsers that do not natively support asynchronous
+    /// script execution.
+    /// 
+    /// <para>Warning: this class is intended for internal use
+    /// only. This class will be removed without warning after all
+    /// native asynchronous implemenations have been completed.
+    /// </para>
+    /// </summary>
+    public class AsyncJavaScriptExecutor
+    {
+        private IJavaScriptExecutor executor;
+        private TimeSpan timeout = TimeSpan.FromMilliseconds(0);
+
+        private const string AsyncScriptTemplate = @"document.__$webdriverPageId = '{0}';
+var timeoutId = window.setTimeout(function() {{
+  window.setTimeout(function() {{
+    document.__$webdriverAsyncTimeout = 1;
+  }}, 0);
+}}, {1});
+document.__$webdriverAsyncTimeout = 0;
+var callback = function(value) {{
+  document.__$webdriverAsyncTimeout = 0;
+  document.__$webdriverAsyncScriptResult = value;
+  window.clearTimeout(timeoutId);
+}};
+var argsArray = Array.prototype.slice.call(arguments);
+argsArray.push(callback);
+if (document.__$webdriverAsyncScriptResult !== undefined) {{
+  delete document.__$webdriverAsyncScriptResult;
+}}
+(function() {{
+{2}
+}}).apply(null, argsArray);";
+
+        private const string PollingScriptTemplate = @"var pendingId = '{0}';
+if (document.__$webdriverPageId != '{1}') {{
+  return [pendingId, -1];
+}} else if ('__$webdriverAsyncScriptResult' in document) {{
+  var value = document.__$webdriverAsyncScriptResult;
+  delete document.__$webdriverAsyncScriptResult;
+  return value;
+}} else {{
+  return [pendingId, document.__$webdriverAsyncTimeout];
+}}
+";
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncJavaScriptExecutor"/> class.
+        /// </summary>
+        /// <param name="executor">An <see cref="IJavaScriptExecutor"/> object capable of executing JavaScript.</param>
+        public AsyncJavaScriptExecutor(IJavaScriptExecutor executor)
+        {
+            this.executor = executor;
+        }
+
+        /// <summary>
+        /// Gets or sets the timeout for the script executor.
+        /// </summary>
+        public TimeSpan Timeout
+        {
+            get { return timeout; }
+            set { timeout = value; }
+        }
+
+        /// <summary>
+        /// Executes a JavaScript script asynchronously.
+        /// </summary>
+        /// <param name="script">The script to execute.</param>
+        /// <param name="args">An array of objects used as arguments in the script.</param>
+        /// <returns>The object which is the return value of the script.</returns>
+        /// <exception cref="InvalidOperationException">if the object executing the function doesn't support JavaScript.</exception>
+        /// <exception cref="WebDriverException">if the page reloads during the JavaScript execution.</exception>
+        /// <exception cref="TimeoutException">if the timeout expires during the JavaScript execution.</exception>
+        public object ExecuteScript(string script, object[] args)
+        {
+            if (!executor.IsJavaScriptEnabled)
+            {
+                throw new InvalidOperationException(
+                    "The underlying JavascriptExecutor must have JavaScript enabled");
+            }
+
+            // Injected into the page along with the user's script. Used to detect when a new page is
+            // loaded while waiting for the script result.
+            string pageId = Guid.NewGuid().ToString();
+
+            string asyncScript = string.Format(AsyncScriptTemplate, pageId, timeout.TotalMilliseconds, script);
+
+            // This is used by our polling function to return a result that indicates the script has
+            // neither finished nor timed out yet.
+            string pendingId = Guid.NewGuid().ToString();
+
+            string pollFunction = string.Format(PollingScriptTemplate, pendingId, pageId);
+
+            // Execute the async script.
+            DateTime startTime = DateTime.Now;
+            executor.ExecuteScript(asyncScript, args);
+
+            // Finally, enter a loop running the poll function. This loop will run until one of the
+            // following occurs:
+            // - The async script invokes the callback with its result.
+            // - The poll function detects that the script has timed out.
+            // We rely on the polling function to detect timeouts so we stay in sync with the browser's
+            // javascript event loop.
+            while (true)
+            {
+                object result = executor.ExecuteScript(pollFunction);
+                ReadOnlyCollection<object> resultList = result as ReadOnlyCollection<object>;
+                if (resultList != null && resultList.Count == 2 && pendingId == resultList[0].ToString())
+                {
+                    long timeoutFlag = (long)resultList[1];
+                    if (timeoutFlag < 0)
+                    {
+                        throw new WebDriverException(
+                            "Detected a new page load while waiting for async script result."
+                            + "\nScript: " + script);
+                    }
+
+                    TimeSpan elapsedTime = DateTime.Now - startTime;
+                    if (timeoutFlag > 0)
+                    {
+                        throw new TimeoutException("Timed out waiting for async script callback."
+                            + "\nElapsed time: " + elapsedTime.Milliseconds + "ms"
+                            + "\nScript: " + script);
+                    }
+                }
+                else
+                {
+                    return result;
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ common/test/csharp/webdriver-common-test/ExecutingAsyncJavascriptTest.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+
+namespace OpenQA.Selenium
+{
+    [TestFixture]
+    public class ExecutingAsyncJavascriptTest : DriverTestFixture
+    {
+        private IJavaScriptExecutor executor;
+
+        [SetUp]
+        public void SetUpEnvironment()
+        {
+            if (driver is IJavaScriptExecutor)
+            {
+                executor = (IJavaScriptExecutor)driver;
+            }
+
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromMilliseconds(0));
+        }
+
+        [Test]
+        public void ShouldNotTimeoutIfCallbackInvokedImmediately()
+        {
+            driver.Url = ajaxyPage;
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1](123);");
+            Assert.IsInstanceOfType(typeof(long), result);
+            Assert.AreEqual(123, (long)result);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnJavascriptPrimitivesFromAsyncScripts()
+        {
+            driver.Url = ajaxyPage;
+            Assert.IsNull(executor.ExecuteAsyncScript("arguments[arguments.length - 1](null);"));
+            Assert.IsNull(executor.ExecuteAsyncScript("arguments[arguments.length - 1]();"));
+            Assert.AreEqual(123, (long)executor.ExecuteAsyncScript("arguments[arguments.length - 1](123);"));
+            Assert.AreEqual("abc", executor.ExecuteAsyncScript("arguments[arguments.length - 1]('abc');").ToString());
+            Assert.IsFalse((bool)executor.ExecuteAsyncScript("arguments[arguments.length - 1](false);"));
+            Assert.IsTrue((bool)executor.ExecuteAsyncScript("arguments[arguments.length - 1](true);"));
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnAnArrayLiteralFromAnAsyncScript()
+        {
+            driver.Url = ajaxyPage;
+
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1]([]);");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(typeof(ReadOnlyCollection<object>), result);
+            Assert.AreEqual(0, ((ReadOnlyCollection<object>)result).Count);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnAnArrayObjectFromAnAsyncScript()
+        {
+            driver.Url = ajaxyPage;
+
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1](new Array());");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(typeof(ReadOnlyCollection<object>), result);
+            Assert.AreEqual(0, ((ReadOnlyCollection<object>)result).Count);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnArraysOfPrimitivesFromAsyncScripts()
+        {
+            driver.Url = ajaxyPage;
+
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1]([null, 123, 'abc', true, false]);");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(typeof(ReadOnlyCollection<object>), result);
+            ReadOnlyCollection<object> resultList = result as ReadOnlyCollection<object>;
+            Assert.AreEqual(5, resultList.Count);
+            Assert.IsNull(resultList[0]);
+            Assert.AreEqual(123, (long)resultList[1]);
+            Assert.AreEqual("abc", resultList[2].ToString());
+            Assert.IsTrue((bool)resultList[3]);
+            Assert.IsFalse((bool)resultList[4]);
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnWebElementsFromAsyncScripts()
+        {
+            driver.Url = ajaxyPage;
+
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1](document.body);");
+            Assert.IsInstanceOfType(typeof(IWebElement), result);
+            Assert.AreEqual("body", ((IWebElement)result).TagName.ToLower());
+        }
+
+        [Test]
+        public void ShouldBeAbleToReturnArraysOfWebElementsFromAsyncScripts()
+        {
+            driver.Url = ajaxyPage;
+
+            object result = executor.ExecuteAsyncScript("arguments[arguments.length - 1]([document.body, document.body]);");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(typeof(ReadOnlyCollection<IWebElement>), result);
+            ReadOnlyCollection<IWebElement> resultsList = (ReadOnlyCollection<IWebElement>)result;
+            Assert.AreEqual(2, resultsList.Count);
+            Assert.IsInstanceOfType(typeof(IWebElement), resultsList[0]);
+            Assert.IsInstanceOfType(typeof(IWebElement), resultsList[1]);
+            Assert.AreEqual("body", ((IWebElement)resultsList[0]).TagName.ToLower());
+            Assert.AreEqual(((IWebElement)resultsList[0]), ((IWebElement)resultsList[1]));
+        }
+
+        [Test]
+        [ExpectedException(typeof(TimeoutException))]
+        public void ShouldTimeoutIfScriptDoesNotInvokeCallback()
+        {
+            driver.Url = ajaxyPage;
+            executor.ExecuteAsyncScript("return 1 + 2;");
+        }
+
+        [Test]
+        [ExpectedException(typeof(TimeoutException))]
+        public void shouldTimeoutIfScriptDoesNotInvokeCallbackWithAZeroTimeout()
+        {
+            driver.Url = ajaxyPage;
+            executor.ExecuteAsyncScript("window.setTimeout(function() {}, 0);");
+        }
+
+        [Test]
+        public void ShouldNotTimeoutIfScriptCallsbackInsideAZeroTimeout()
+        {
+            driver.Url = ajaxyPage;
+            executor.ExecuteAsyncScript(
+                "var callback = arguments[arguments.length - 1];" +
+                "window.setTimeout(function() { callback(123); }, 0)");
+        }
+
+        [Test]
+        [ExpectedException(typeof(TimeoutException))]
+        public void ShouldTimeoutIfScriptDoesNotInvokeCallbackWithLongTimeout()
+        {
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromMilliseconds(500));
+            driver.Url = ajaxyPage;
+            executor.ExecuteAsyncScript(
+                "var callback = arguments[arguments.length - 1];" +
+                "window.setTimeout(callback, 1500);");
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ShouldDetectPageLoadsWhileWaitingOnAnAsyncScriptAndReturnAnError()
+        {
+            driver.Url = ajaxyPage;
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromMilliseconds(100));
+            executor.ExecuteAsyncScript("window.location = '" + dynamicPage + "';");
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ShouldCatchErrorsWhenExecutingInitialScript()
+        {
+            driver.Url = ajaxyPage;
+            executor.ExecuteAsyncScript("throw Error('you should catch this!');");
+        }
+
+        [Test]
+        public void ShouldBeAbleToExecuteAsynchronousScripts()
+        {
+            driver.Url = ajaxyPage;
+
+            IWebElement typer = driver.FindElement(By.Name("typer"));
+            typer.SendKeys("bob");
+            Assert.AreEqual("bob", typer.Value);
+
+            driver.FindElement(By.Id("red")).Click();
+            driver.FindElement(By.Name("submit")).Click();
+
+            Assert.AreEqual(1, GetNumberOfDivElements(), "There should only be 1 DIV at this point, which is used for the butter message");
+
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(10));
+            string text = (string)executor.ExecuteAsyncScript(
+                "var callback = arguments[arguments.length - 1];"
+                + "window.registerListener(arguments[arguments.length - 1]);");
+            Assert.AreEqual("bob", text);
+            Assert.AreEqual("", typer.Value);
+
+            Assert.AreEqual(2, GetNumberOfDivElements(), "There should be 1 DIV (for the butter message) + 1 DIV (for the new label)");
+        }
+
+        [Test]
+        public void ShouldBeAbleToPassMultipleArgumentsToAsyncScripts()
+        {
+            driver.Url = ajaxyPage;
+            long result = (long)executor.ExecuteAsyncScript("arguments[arguments.length - 1](arguments[0] + arguments[1]);", 1, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        //[Test]
+        public void ShouldBeAbleToMakeXMLHttpRequestsAndWaitForTheResponse()
+        {
+            string script =
+                "var url = arguments[0];" +
+                "var callback = arguments[arguments.length - 1];" +
+                // Adapted from http://www.quirksmode.org/js/xmlhttp.html
+                "var XMLHttpFactories = [" +
+                "  function () {return new XMLHttpRequest()}," +
+                "  function () {return new ActiveXObject('Msxml2.XMLHTTP')}," +
+                "  function () {return new ActiveXObject('Msxml3.XMLHTTP')}," +
+                "  function () {return new ActiveXObject('Microsoft.XMLHTTP')}" +
+                "];" +
+                "var xhr = false;" +
+                "while (!xhr && XMLHttpFactories.length) {" +
+                "  try {" +
+                "    xhr = XMLHttpFactories.shift().call();" +
+                "  } catch (e) {}" +
+                "}" +
+                "if (!xhr) throw Error('unable to create XHR object');" +
+                "xhr.open('GET', url, true);" +
+                "xhr.onreadystatechange = function() {" +
+                "  if (xhr.readyState == 4) callback(xhr.responseText);" +
+                "};" +
+                "xhr.send();";
+
+            driver.Url = ajaxyPage;
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(3));
+            string response = (string)executor.ExecuteAsyncScript(script, sleepingPage + "?time=2");
+            Assert.AreEqual("<html><head><title>Done</title></head><body>Slept for 2s</body></html>", response.Trim());
+        }
+
+        private long GetNumberOfDivElements()
+        {
+            IJavaScriptExecutor jsExecutor = driver as IJavaScriptExecutor;
+            // Selenium does not support "findElements" yet, so we have to do this through a script.
+            return (long)jsExecutor.ExecuteScript("return document.getElementsByTagName('div').length;");
+        }
+    }
+}
