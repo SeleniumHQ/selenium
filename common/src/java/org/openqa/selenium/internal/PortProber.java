8b3a7cf155c608d3fbabb902922d7e7421f14c3d
Simon M Stewart <simon.m.stewart@gmail.com>
2010-12-20 13:05:04 +0000

    SimonStewart: Adding classes moved during the ie driver merge
    r10689

---
+++ common/src/java/org/openqa/selenium/internal/PortProber.java
@@ -0,0 +1,107 @@
+/*
+Copyright 2010 WebDriver committers
+Copyright 2010 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.openqa.selenium.internal;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@SuppressWarnings({"UtilityClass"})
+public class PortProber {
+  private static final Random random = new Random();
+
+  private PortProber() {
+  }
+
+  public static int findFreePort() {
+    for (int i = 0; i < 5; i++) {
+      int seedPort = createAcceptablePort();
+      int suggestedPort = checkPortIsFree(seedPort);
+      if (suggestedPort != -1) {
+        return suggestedPort;
+      }
+    }
+    throw new RuntimeException("Unable to find a free port");
+  }
+
+  public static Callable<Integer> freeLocalPort(final int port) {
+    return new Callable<Integer>() {
+
+      public Integer call() throws Exception {
+        if (checkPortIsFree(port) != -1) {
+          return port;
+        }
+        return null;
+      }
+    };
+  }
+
+  private static int createAcceptablePort() {
+    synchronized (random) {
+      int seed = random.nextInt();
+      // avoid protected ports
+      final int FIRST_PORT = 1025;
+      final int LAST_PORT = 65534;
+      final int randomInt = Math.abs(random.nextInt());
+      seed = (randomInt % (LAST_PORT - FIRST_PORT + 1)) + FIRST_PORT;
+      return seed;
+    }
+  }
+
+  private static int checkPortIsFree(int port) {
+    ServerSocket socket = null;
+    try {
+      socket = new ServerSocket(port);
+      int localPort = socket.getLocalPort();
+      socket.close();
+      return localPort;
+    } catch (IOException e) {
+      return -1;
+    }
+  }
+
+  public static boolean pollPort(int port) {
+    return pollPort(port, 15, SECONDS);
+  }
+
+  public static boolean pollPort(int port, int timeout, TimeUnit unit) {
+    long end = System.currentTimeMillis() + unit.toMillis(timeout);
+    while (System.currentTimeMillis() < end) {
+      try {
+        Socket socket = new Socket("localhost", port);
+        socket.close();
+        return true;
+      } catch (ConnectException e) {
+        // Ignore this
+      } catch (UnknownHostException e) {
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return false;
+  }
+}
---
+++ common/src/java/org/openqa/selenium/internal/ProcessUtils.java
@@ -0,0 +1,160 @@
+package org.openqa.selenium.internal;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.internal.IOUtils;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class ProcessUtils {
+  /**
+   * Waits the specified timeout for the process to die
+   *
+   * @param p The process to kill.
+   * @param timeout How long to wait in milliseconds.
+   * @return The exit code of the given process.
+   */
+  public static int waitForProcessDeath(Process p, long timeout) {
+    ProcessWaiter pw = new ProcessWaiter(p);
+    Thread waiter = new Thread(pw);
+    waiter.start();
+    try {
+      waiter.join(timeout);
+    } catch (InterruptedException e) {
+      throw new RuntimeException("Bug? Main interrupted while waiting for process", e);
+    }
+    if (waiter.isAlive()) {
+      waiter.interrupt();
+    }
+    try {
+      waiter.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException("Bug? Main interrupted while waiting for dead process waiter", e);
+    }
+    InterruptedException ie = pw.getException();
+    if (ie != null) {
+      throw new ProcessStillAliveException("Timeout waiting for process to die", ie);
+    }
+    return p.exitValue();
+
+  }
+
+  /**
+   * Forcibly kills a process, using OS tools like "kill" as a last resort
+   *
+   * @param process The process to kill.
+   * @return The exit value of the process.
+   */
+  public static int killProcess(Process process) {
+    int exitValue;
+
+    // first, wait a second to see if the process will die on it's own (we will likely have asked
+    // the process to kill itself just before calling this method
+    try {
+      exitValue = waitForProcessDeath(process, 1000);
+      if (exitValue == 0) {
+        return exitValue;
+      }
+    } catch (Exception e) {
+      // no? ok, no biggie, now let's force kill it...
+    }
+
+    process.destroy();
+    try {
+      exitValue = waitForProcessDeath(process, 10000);
+    } catch (ProcessStillAliveException ex) {
+      if (Platform.getCurrent().is(Platform.WINDOWS)) {
+        throw ex;
+      }
+      try {
+        System.out.println("Process didn't die after 10 seconds");
+        kill9(process);
+        exitValue = waitForProcessDeath(process, 10000);
+      } catch (Exception e) {
+        System.out.println("Process refused to die after 10 seconds, and couldn't kill9 it");
+        e.printStackTrace();
+        throw new RuntimeException(
+            "Process refused to die after 10 seconds, and couldn't kill9 it: " + e.getMessage(),
+            ex);
+      }
+    }
+    return exitValue;
+  }
+
+  /**
+   * @param p The process to gather the PID of.
+   * @return The process's PID
+   */
+  public static int getProcessId(Process p) {
+    if (Platform.getCurrent().is(Platform.WINDOWS)) {
+      throw new IllegalStateException("UnixUtils may not be used on Windows");
+    }
+    try {
+      Field f = p.getClass().getDeclaredField("pid");
+      f.setAccessible(true);
+      Integer pid = (Integer) f.get(p);
+      return pid;
+    } catch (Exception e) {
+      throw new RuntimeException("Couldn't detect pid", e);
+    }
+  }
+
+  /**
+   * Runs "kill -9" on the specified pid
+   *
+   * @param pid The PID of the process to kill.
+   * @throws IOException If unable to kill the process.
+   * @throws InterruptedException If unable to kill the process.
+   */
+  public static void kill9(Integer pid) throws IOException, InterruptedException {
+    ProcessBuilder pb = new ProcessBuilder("kill", "-9", pid.toString());
+    pb.redirectErrorStream();
+    Process p = pb.start();
+    int code = p.waitFor();
+
+    if (code != 0) {
+      String output = IOUtils.readFully(p.getInputStream());
+      throw new RuntimeException("kill return code " + code + ": " + output);
+    }
+  }
+
+  /**
+   * Runs "kill -9" on the specified process
+   *
+   * @param p The process to kill.
+   * @throws IOException If unable to kill the process.
+   * @throws InterruptedException If unable to kill the process.
+   */
+  public static void kill9(Process p) throws IOException, InterruptedException {
+    kill9(getProcessId(p));
+  }
+
+  private static class ProcessWaiter implements Runnable {
+
+    private volatile InterruptedException t;
+    private final Process p;
+
+    public InterruptedException getException() {
+      return t;
+    }
+
+    public ProcessWaiter(Process p) {
+      this.p = p;
+    }
+
+    public void run() {
+      try {
+        p.waitFor();
+      } catch (InterruptedException e) {
+        this.t = e;
+      }
+    }
+  }
+
+  public static class ProcessStillAliveException extends RuntimeException {
+
+    public ProcessStillAliveException(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
+}
