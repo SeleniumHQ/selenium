eedd71ce7c0b06620d6c9ff810a35399af127fab
Davis Burns <david.burns@theautomatedtester.co.uk>
2011-01-06 14:13:33 +0000

    DavidBurns Select Element Handling tests and child element fixes found by teststest/selenium/webdriver/firefox/test_ff_select_element_handling.py
    r10856

--- py/selenium/webdriver/remote/webelement.py
+++ py/selenium/webdriver/remote/webelement.py
@@ -15,7 +15,7 @@
 
 """WebElement implementation."""
 from command import Command
-
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.exceptions import NoSuchAttributeException
 
 
@@ -87,52 +87,60 @@ class WebElement(object):
 
     def find_element_by_id(self, id_):
         """Finds element by id."""
-        return self._get_elem_by("id", id_)
+        return self.find_element(by=By.ID, value=id_)
 
     def find_elements_by_id(self, id_):
-        return self._get_elems_by("id", id_)
+        return self.find_elements(by=By.ID, value=id_)
 
     def find_element_by_name(self, name):
         """Find element by name."""
-        return self._get_elem_by("name", name)
+        return self.find_element(by=By.NAME, value=name)
 
     def find_elements_by_name(self, name):
-        return self._get_elems_by("name", name)
+        return self.find_elements(by=By.NAME, value=name)
 
     def find_element_by_link_text(self, link_text):
         """Finds element by link text."""
-        return self._get_elem_by("link text", link_text)
+        return self.find_element(by=By.LINK_TEXT, value=link_text)
 
     def find_elements_by_link_text(self, link_text):
-        return self._get_elems_by("link text", link_text)
+        return self.find_elements(by=By.LINK_TEXT, value=link_text)
 
     def find_element_by_partial_link_text(self, link_text):
-        return self._get_elem_by("partial link text", link_text)
+        return self.find_element(by=By.PARTIAL_LINK_TEXT, value=link_text)
 
     def find_elements_by_partial_link_text(self, link_text):
-        return self._get_elems_by("partial link text", link_text)
+        return self.find_elements(by=By.PARTIAL_LINK_TEXT, value=link_text)
 
     def find_element_by_tag_name(self, name):
-        return self._get_elem_by("tag name", name)
+        return self.find_element(by=By.TAG_NAME, value=name)
 
     def find_elements_by_tag_name(self, name):
-        return self._get_elems_by("tag name", name)
+        return self.find_elements(by=By.TAG_NAME, value=name)
 
     def find_element_by_xpath(self, xpath):
         """Finds element by xpath."""
-        return self._get_elem_by("xpath", xpath)
+        return self.find_element(by=By.XPATH, value=xpath)
 
     def find_elements_by_xpath(self, xpath):
         """Finds elements within the elements by xpath."""
-        return self._get_elems_by("xpath", xpath)
-
+        return self.find_elements(by=By.XPATH, value=xpath)
+    
     def find_element_by_class_name(self, name):
         """Finds an element by their class name."""
-        return self._get_elem_by("class name", name)
+        return self.find_element(by=By.CLASS_NAME, value=name)
 
     def find_elements_by_class_name(self, name):
         """Finds elements by their class name."""
-        return self._get_elems_by("class name", name)
+        return self.find_elements(by=By.CLASS_NAME, value=name)
+
+    def find_element_by_css_selector(self, css_selector):
+        """Find and return an element by CSS selector."""
+        return self.find_element(by=By.CSS_SELECTOR, value=css_selector)
+    
+    def find_elements_by_css_selector(self, css_selector):
+        """Find and return list of multiple elements by CSS selector."""
+        return self.find_elements(by=By.CSS_SELECTOR, value=css_selector)
 
     def send_keys(self, *value):
         """Simulates typing into the element."""
@@ -177,10 +185,10 @@ class WebElement(object):
         params['id'] = self._id
         return self._parent._execute(command, params)
 
-    def _get_elem_by(self, by, value):
+    def find_element(self, by=By.ID, value=None):
         return self._execute(Command.FIND_CHILD_ELEMENT,
                              {"using": by, "value": value})['value']
 
-    def _get_elems_by(self, by, value):
+    def find_elements(self, by=By.ID, value=None):
         return self._execute(Command.FIND_CHILD_ELEMENTS,
                              {"using": by, "value": value})['value']
---
+++ py/test/selenium/webdriver/common/select_element_handling_tests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# Copyright 2008-2009 WebDriver committers
+# Copyright 2008-2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import re
+import tempfile
+import time
+import shutil
+import unittest
+from selenium.webdriver.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+class SelectElementHandlingTests(unittest.TestCase):
+  
+    def testShouldBeAbleToChangeTheSelectedOptionInASelect(self):
+        self._loadPage("formPage")
+        selectBox = self.driver.find_element(by=By.XPATH, value="//select[@name='selectomatic']")
+        options = selectBox.find_elements(by=By.TAG_NAME, value="option")
+        one = options[0]
+        two = options[1]
+        self.assertTrue(one.is_selected())
+        self.assertFalse(two.is_selected())
+
+        two.select()
+        self.assertFalse(one.is_selected())
+        self.assertTrue(two.is_selected())
+
+    def testShouldBeAbleToSelectMoreThanOneOptionFromASelectWhichAllowsMultipleChoices(self):
+        self._loadPage("formPage")
+
+        multiSelect = self.driver.find_element(by=By.ID, value="multi")
+        options = multiSelect.find_elements(by=By.TAG_NAME, value="option")
+        for option in options:
+            option.select()
+
+        for i in range(len(options)):
+            option = options[i]
+            self.assertTrue(option.is_selected(), 
+            "Option at index is not selected but should be: " + str(i))
+
+    def _pageURL(self, name):
+        return "http://localhost:%d/%s.html" % (self.webserver.port, name)
+
+    def _loadSimplePage(self):
+        self._loadPage("simpleTest")
+
+    def _loadPage(self, name):
+        self.driver.get(self._pageURL(name))
