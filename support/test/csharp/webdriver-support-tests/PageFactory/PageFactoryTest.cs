04d392aec73b67b00f042e7608eb6807ba30f948
Daniel Wagner-Hall <dawagner@gmail.com>
2011-01-09 21:36:57 +0000

    DanielWagnerHall: Tidying up tests, making them less browser dependent to speed them up
    r10913

--- support/test/csharp/webdriver-support-tests/PageFactory/PageFactoryTest.cs
+++ support/test/csharp/webdriver-support-tests/PageFactory/PageFactoryTest.cs
@@ -1,21 +1,22 @@
-﻿using NMock2;
+﻿using System;
+using NMock2;
 using NUnit.Framework;
-using OpenQA.Selenium.Firefox;
 
 namespace OpenQA.Selenium.Support.PageFactory
 {
     [TestFixture]
-    //TODO: Extend DriverTestFixture
     public class PageFactoryTest
     {
         private Mockery mocks;
-        private readonly IWebDriver driver = new FirefoxDriver();
-        private const string xhtmlPage = @"file:///C:\svn\selenium-trunk\common\src\web\xhtmlTest.html";
+        private IWebDriver mockDriver;
+        private IWebElement mockElement;
 
         [SetUp]
         public void SetUp()
         {
             mocks = new Mockery();
+            mockDriver = mocks.NewMock<IWebDriver>();
+            mockElement = mocks.NewMock<IWebElement>();
         }
         
         [TearDown]
@@ -23,169 +24,86 @@ namespace OpenQA.Selenium.Support.PageFactory
         {
             mocks.VerifyAllExpectationsHaveBeenMet();
         }
-        
-        [TestFixtureTearDown]
-        public void FixtureTearDown()
-        {
-            driver.Quit();
-        }
-        
+
         [Test]
         public void ElementShouldBeNullUntilInitElementsCalled()
         {
-            driver.Url = xhtmlPage;
             var page = new Page();
+            
             Assert.Null(page.formElement);
 
-            PageFactory.InitElements(driver, page);
-
-            Assert.NotNull(page.formElement);
-        }
-
-        [Test]
-        public void DoesNotSearchForUnusedElements()
-        {
-            var mockDriver = mocks.NewMock<IWebDriver>();
-            Expect.Never.On(mockDriver).Method("FindElement");
-            Expect.Never.On(mockDriver).Method("FindElements");
-
-            var page = new Page();
             PageFactory.InitElements(mockDriver, page);
+            Assert.NotNull(page.formElement);
         }
         
         [Test]
         public void FindsElement()
         {
-            driver.Url = xhtmlPage;
             var page = new Page();
-
-            PageFactory.InitElements(driver, page);
-
-            AssertElementFound(page.formElement);
+            AssertFindsElementByExactlyOneLookup(page, () => page.formElement);
         }
         
         [Test]
         public void FindsElementEachAccess()
         {
-            var mockElement = mocks.NewMock<IWebElement>();
-            Expect.Exactly(1).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
-        
-            var mockDriver = mocks.NewMock<IWebDriver>();
-            Expect.Exactly(1).On(mockDriver).Method("FindElement").With(By.Name("someForm")).Will(Return.Value(mockElement));
-
             var page = new Page();
-
-            PageFactory.InitElements(mockDriver, page);
-
-            AssertElementFound(page.formElement);
-
+            
+            AssertFindsElementByExactlyOneLookup(page, () => page.formElement);
             mocks.VerifyAllExpectationsHaveBeenMet();
 
-            Expect.Exactly(1).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
-            Expect.Exactly(1).On(mockDriver).Method("FindElement").With(By.Name("someForm")).Will(Return.Value(mockElement));
-            
-            AssertElementFound(page.formElement);
+            ExpectOneLookup();
+            AssertFoundElement(page.formElement);
         }
 
         [Test]
         public void FindsPrivateElement()
         {
-            driver.Url = xhtmlPage;
             var page = new PrivatePage();
-
-            PageFactory.InitElements(driver, page);
-
-            AssertElementFound(page.GetField());
+            AssertFindsElementByExactlyOneLookup(page, page.GetField);
         }
 
         [Test]
         public void FindsPropertyElement()
         {
-            driver.Url = xhtmlPage;
             var page = new ElementAsPropertyPage();
-
-            PageFactory.InitElements(driver, page);
-
-            AssertElementFound(page.formElement);
-        }
-
-        [Test]
-        public void FindsRenderedElement()
-        {
-            driver.Url = xhtmlPage;
-            var page = new RenderedPage();
-
-            PageFactory.InitElements(driver, page);
-
-            Assert.True(page.formElement.Displayed);
+            AssertFindsElementByExactlyOneLookup(page, () => page.FormElement);
         }
 
         [Test]
         public void FindsParentAndChildElement()
         {
-            driver.Url = xhtmlPage;
+            ExpectOneLookup();
+            var mockElement2 = mocks.NewMock<IWebElement>();
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.TagName("body")).Will(Return.Value(mockElement2));
+            Expect.Once.On(mockElement2).GetProperty("TagName").Will(Return.Value("body"));
+        
             var page = new ChildPage();
 
-            PageFactory.InitElements(driver, page);
-
-            AssertElementFound(page.formElement);
-            AssertElementFound(page.childFormElement);
-        }
-        
-        [Test]
-        public void ElementEqualityWorks()
-        {
-            driver.Url = xhtmlPage;
-            var page = new Page();
-
-            PageFactory.InitElements(driver, page);
-
-            var expectedElement = driver.FindElement(By.Name("someForm"));
-
-            Assert.True(page.formElement.Equals(expectedElement));
-            Assert.True(expectedElement.Equals(page.formElement));
-            Assert.AreEqual(expectedElement.GetHashCode(), page.formElement.GetHashCode());
-        }
-        
-        [Test]
-        public void UsesElementAsScriptArgument()
-        {
-            driver.Url = xhtmlPage;
-            var page = new Page();
-
-            PageFactory.InitElements(driver, page);
-
-            var tagName = (string)((IJavaScriptExecutor)driver).ExecuteScript("return arguments[0].tagName", page.formElement);
-
-            Assert.AreEqual("form", tagName.ToLower());
+            AssertFindsElement(page, () => page.formElement);
+            AssertFoundElement(page.childElement, "body");
         }
         
         [Test]
         public void FallsBackOnOtherLocatorsOnFailure()
         {
-            var mockElement = mocks.NewMock<IWebElement>();
-            Expect.Once.On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
-        
-            var mockDriver = mocks.NewMock<IWebDriver>();
-            Expect.AtLeastOnce.On(mockDriver).Method("FindElement").With(By.Name("notthisname")).Will(Throw.Exception(new NoSuchElementException()));
-            Expect.AtLeastOnce.On(mockDriver).Method("FindElement").With(By.TagName("form")).Will(Return.Value(mockElement));
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.Name("notthisname")).Will(Throw.Exception(new NoSuchElementException()));
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.TagName("form")).Will(Return.Value(mockElement));
             Expect.Never.On(mockDriver).Method("FindElement").With(By.Id("notthiseither")).Will(Return.Value(mockElement));
+
+            Expect.Once.On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
 
             var page = new FallsbackPage();
             PageFactory.InitElements(mockDriver, page);
 
-            AssertElementFound(page.formElement);
-
-            mocks.VerifyAllExpectationsHaveBeenMet();
+            AssertFoundElement(page.formElement);
         }
 
         [Test]
         public void ThrowsIfAllLocatorsFail()
         {
-            var mockDriver = mocks.NewMock<IWebDriver>();
-            Expect.AtLeastOnce.On(mockDriver).Method("FindElement").With(By.Name("notthisname")).Will(Throw.Exception(new NoSuchElementException()));
-            Expect.AtLeastOnce.On(mockDriver).Method("FindElement").With(By.TagName("notthiseither")).Will(Throw.Exception(new NoSuchElementException()));
-            Expect.AtLeastOnce.On(mockDriver).Method("FindElement").With(By.Id("stillnotthis")).Will(Throw.Exception(new NoSuchElementException()));
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.Name("notthisname")).Will(Throw.Exception(new NoSuchElementException()));
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.TagName("notthiseither")).Will(Throw.Exception(new NoSuchElementException()));
+            Expect.Once.On(mockDriver).Method("FindElement").With(By.Id("stillnotthis")).Will(Throw.Exception(new NoSuchElementException()));
 
             var page = new FailsToFallbackPage();
             PageFactory.InitElements(mockDriver, page);
@@ -196,46 +114,76 @@ namespace OpenQA.Selenium.Support.PageFactory
         [Test]
         public void CachesElement()
         {
-            var mockElement = mocks.NewMock<IWebElement>();
-            Expect.Exactly(2).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
-
-            var mockDriver = mocks.NewMock<IWebDriver>();
             Expect.Once.On(mockDriver).Method("FindElement").With(By.Name("someForm")).Will(Return.Value(mockElement));
+            Expect.Exactly(2).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
 
             var page = new CachedPage();
 
-            PageFactory.InitElements(mockDriver, page);
-
-            AssertElementFound(page.formElement);
-            AssertElementFound(page.formElement);
+            AssertFindsElement(page, () => page.formElement);
+            AssertFoundElement(page.formElement);
         }
 
         [Test]
         public void CachesIfClassMarkedCachedElement()
         {
-            var mockElement = mocks.NewMock<IWebElement>();
-            Expect.Exactly(2).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
-
-            var mockDriver = mocks.NewMock<IWebDriver>();
             Expect.Once.On(mockDriver).Method("FindElement").With(By.Name("someForm")).Will(Return.Value(mockElement));
+            Expect.Exactly(2).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
 
             var page = new CachedClassPage();
 
+            AssertFindsElement(page, () => page.formElement);
+            AssertFoundElement(page.formElement);
+        }
+
+        #region Test helper methods
+
+        private void ExpectOneLookup()
+        {
+            Expect.Exactly(1).On(mockDriver).Method("FindElement").With(By.Name("someForm")).Will(Return.Value(mockElement));
+            Expect.Exactly(1).On(mockElement).GetProperty("TagName").Will(Return.Value("form"));
+        }
+
+        /// <summary>
+        /// Asserts that getElement yields an element from page which can be interacted with, by exactly one element lookup
+        /// </summary>
+        private void AssertFindsElementByExactlyOneLookup(object page, Func<IWebElement> getElement)
+        {
+            ExpectOneLookup();
+            AssertFindsElement(page, getElement);
+        }
+
+        /// <summary>
+        /// Asserts that getElement yields an element which can be interacted with, with no constraints on element lookups
+        /// </summary>
+        private void AssertFindsElement(object page, Func<IWebElement> getElement)
+        {
             PageFactory.InitElements(mockDriver, page);
 
-            AssertElementFound(page.formElement);
-            AssertElementFound(page.formElement);
-        }
-
-        private static void AssertElementFound(IWebElement element)
-        {
-            Assert.AreEqual("form", element.TagName.ToLower());
+            AssertFoundElement(getElement());
         }
         
-        #region Page classes for tests
-        #pragma warning disable 649 //We set this through reflection, so expect an always-null warning
+        /// <summary>
+        /// Asserts that the element has been found and can be interacted with
+        /// </summary>
+        private static void AssertFoundElement(IWebElement element)
+        {
+            AssertFoundElement(element, "form");
+        }
 
-        private class Page
+        /// <summary>
+        /// Asserts that the element has been found and has the tagName passed
+        /// </summary>
+        private static void AssertFoundElement(IWebElement element, string tagName)
+        {
+            Assert.AreEqual(tagName, element.TagName.ToLower());
+        }
+        
+        #endregion
+        
+        #region Page classes for tests
+        #pragma warning disable 649 //We set fields through reflection, so expect an always-null warning
+
+        internal class Page
         {
             [FindsBy(Name = "someForm")]
             public IWebElement formElement;
@@ -254,14 +202,8 @@ namespace OpenQA.Selenium.Support.PageFactory
 
         private class ChildPage : Page
         {
-            [FindsBy(Name = "someForm")]
-            public IWebElement childFormElement;
-        }
-
-        private class RenderedPage
-        {
-            [FindsBy(Name = "someForm")]
-            public IRenderedWebElement formElement;
+            [FindsBy(TagName = "body")]
+            public IWebElement childElement;
         }
 
         private class FallsbackPage
@@ -279,7 +221,7 @@ namespace OpenQA.Selenium.Support.PageFactory
         private class ElementAsPropertyPage
         {
             [FindsBy(Name = "someForm")]
-            public IWebElement formElement { get; set; }
+            public IWebElement FormElement { get; set; }
         }
 
         private class CachedPage
