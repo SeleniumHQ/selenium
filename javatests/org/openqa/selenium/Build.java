79b5c09fca1f3ca9e758ca4cc15fc566ae572e16
Michael Tamm <michael.tamm2@gmail.com>
2011-01-22 22:37:25 +0000

    MichaelTamm: Added BuildWatcher to Build class
    r11072

--- javatests/org/openqa/selenium/Build.java
+++ javatests/org/openqa/selenium/Build.java
@@ -2,6 +2,9 @@
 
 package org.openqa.selenium;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,36 +17,72 @@ import static org.openqa.selenium.Platform.WINDOWS;
 public class Build {
   private List<String> targets = new ArrayList<String>();
 
-  public Build() {
-    String command = Platform.getCurrent().is(WINDOWS) ? "go.bat" : "./go";
-    targets.add(command);
-  }
-
   public Build of(String... targets) {
     this.targets.addAll(Arrays.asList(targets));
-
     return this;
   }
 
   public void go() {
-    System.out.println("Running " + targets);
-
-    ProcessBuilder builder = new ProcessBuilder(targets);
-    builder.directory(InProject.locate("Rakefile").getParentFile());
+    if (targets.isEmpty()) {
+      throw new IllegalStateException("No targets specified");
+    }
+    System.out.println("\nBuilding " + targets + " ...");
+    ProcessBuilder builder = prepareBuild();
     try {
       executeBuild(builder);
     } catch (Exception e) {
-      e.printStackTrace();
+      e.printStackTrace(System.err);
       fail("Cannot build");
     }
   }
 
+  private ProcessBuilder prepareBuild() {
+    List<String> command = new ArrayList<String>();
+    command.add(Platform.getCurrent().is(WINDOWS) ? "go.bat" : "./go");
+    command.addAll(targets);
+    ProcessBuilder builder = new ProcessBuilder(command);
+    builder.directory(InProject.locate("Rakefile").getParentFile());
+    builder.redirectErrorStream(true);
+    return builder;
+  }
+
   private void executeBuild(ProcessBuilder builder) throws Exception {
     Process process = builder.start();
-
+    BuildWatcher buildWatcher = new BuildWatcher(process);
+    buildWatcher.start();
     int exitValue = process.waitFor();
     if (exitValue != 0) {
       fail("Unable to build artifacts");
+    }
+  }
+
+  private static class BuildWatcher extends Thread {
+    private Process buildProcess;
+
+    private BuildWatcher(Process buildProcess) {
+      super("BuildWatcher");
+      this.buildProcess = buildProcess;
+    }
+
+    public void run() {
+      BufferedReader buildOutput = null;
+      try {
+        buildOutput = new BufferedReader(new InputStreamReader(buildProcess.getInputStream()), 8192);
+        for (String s = buildOutput.readLine(); s != null && !interrupted(); s = buildOutput.readLine()) {
+          try {
+            System.out.println(">>> " + s);
+          } catch (Throwable ignored) {}
+        }
+      } catch (Throwable e) {
+        System.err.print("ERROR: Could not read from stdout of " + buildProcess + ":");
+        e.printStackTrace(System.err);
+      } finally {
+        if (buildOutput != null) {
+          try {
+            buildOutput.close();
+          } catch (Throwable ignored) {}
+        }
+      }
     }
   }
 }
