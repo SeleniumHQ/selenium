1d0bb3b2e2b40a6c700fb1ef3b29915f09c7ea1f
Daniel Wagner-Hall <dawagner@gmail.com>
2011-01-22 14:13:26 +0000

    DanielWagnerHall: Re-enabling element equality tests in IE as they pass with the IE Driver rewrite (issue 251)
    r11064

--- javatests/org/openqa/selenium/ElementEqualityTest.java
+++ javatests/org/openqa/selenium/ElementEqualityTest.java
@@ -2,14 +2,11 @@ package org.openqa.selenium;
 
 import java.util.List;
 
-import static org.openqa.selenium.Ignore.Driver.IE;
 import static org.openqa.selenium.Ignore.Driver.IPHONE;
-import static org.openqa.selenium.Ignore.Driver.REMOTE;
 import static org.openqa.selenium.Ignore.Driver.SELENESE;
 
-@Ignore(IPHONE)
+@Ignore({IPHONE, SELENESE})
 public class ElementEqualityTest extends AbstractDriverTestCase {
-  @Ignore(SELENESE)
   public void testElementEqualityShouldWork() {
     driver.get(pages.simpleTestPage);
     
@@ -19,7 +16,6 @@ public class ElementEqualityTest extends AbstractDriverTestCase {
     assertEquals(body, xbody);
   }
 
-  @Ignore(SELENESE)
   public void testElementInequalityShouldWork() {
     driver.get(pages.simpleTestPage);
     
@@ -28,7 +24,6 @@ public class ElementEqualityTest extends AbstractDriverTestCase {
     assertFalse(ps.get(0).equals(ps.get(1)));
   }
 
-  @Ignore({IE, REMOTE, SELENESE})
   public void testFindElementHashCodeShouldMatchEquality() {
     driver.get(pages.simpleTestPage);
     WebElement body = driver.findElement(By.tagName("body"));
@@ -37,7 +32,6 @@ public class ElementEqualityTest extends AbstractDriverTestCase {
     assertEquals(body.hashCode(), xbody.hashCode());
   }
   
-  @Ignore({IE, REMOTE, SELENESE})
   public void testFindElementsHashCodeShouldMatchEquality() {
     driver.get(pages.simpleTestPage);
     List<WebElement> body = driver.findElements(By.tagName("body"));
