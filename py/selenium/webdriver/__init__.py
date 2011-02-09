629049663a58615d143fa5cdd782e2c079fd881f
Davis Burns <david.burns@theautomatedtester.co.uk>
2011-02-09 13:03:27 +0000

    DavidBurns adding missing Profile to __init__ to help with imports
    r11347

--- py/selenium/webdriver/__init__.py
+++ py/selenium/webdriver/__init__.py
@@ -16,6 +16,7 @@
 # limitations under the License.
 
 from firefox.webdriver import WebDriver as Firefox
+from firefox.firefox_profile import FirefoxProfile
 from chrome.webdriver import WebDriver as Chrome
 from ie.webdriver import WebDriver as Ie
 from remote.webdriver import WebDriver as Remote
