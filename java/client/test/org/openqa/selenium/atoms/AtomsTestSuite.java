8c17db880208261fc7cc464e21e7f10034aec7fb
Jason Lebya <jmleyba@gmail.com>
2011-03-05 05:48:45 +0000

    JasonLeyba: Another atom scope test.
    r11590

--- java/client/test/org/openqa/selenium/atoms/AtomsTestSuite.java
+++ java/client/test/org/openqa/selenium/atoms/AtomsTestSuite.java
@@ -1,13 +1,18 @@
 package org.openqa.selenium.atoms;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import net.sourceforge.htmlunit.corejs.javascript.Context;
 import net.sourceforge.htmlunit.corejs.javascript.ContextAction;
 import net.sourceforge.htmlunit.corejs.javascript.ContextFactory;
+import net.sourceforge.htmlunit.corejs.javascript.Function;
+import net.sourceforge.htmlunit.corejs.javascript.NativeObject;
 import net.sourceforge.htmlunit.corejs.javascript.ScriptableObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -70,6 +75,35 @@ public class AtomsTestSuite {
         assertEquals(1234, context.evaluateString(global, "_", "", 1, null));
 
         context.evaluateString(global, "(" + fragment + ").call({})", FRAGMENT_PATH, 1, null);
+        assertEquals(1234, context.evaluateString(global, "_", "", 1, null));
+        return null;
+      }
+    });
+  }
+
+  @Test
+  public void nestedFragmentsShouldNotLeakVariables() {
+    ContextFactory.getGlobal().call(new ContextAction() {
+      public Object run(Context context) {
+        // executeScript atom recursing on itself to execute "return 1+2".
+        // Should result in {status:0,value:{status:0,value:3}}
+        // Global scope should not be modified.
+        String nestedScript = String.format("(%s).call(this, %s, ['return 1+2;'], false)",
+            fragment, fragment);
+
+        NativeObject result = (NativeObject) context.evaluateString(global, nestedScript,
+            FRAGMENT_PATH, 1, null);
+
+        assertThat(result.get("status"), instanceOf(Number.class));
+        assertEquals(0, ((Number) result.get("status")).intValue());
+        assertThat(result.get("value"), instanceOf(NativeObject.class));
+
+        result = (NativeObject) result.get("value");
+        assertThat(result.get("status"), instanceOf(Number.class));
+        assertEquals(0, ((Number) result.get("status")).intValue());
+        assertThat(result.get("value"), instanceOf(Number.class));
+        assertEquals(3, ((Number) result.get("value")).intValue());
+
         assertEquals(1234, context.evaluateString(global, "_", "", 1, null));
         return null;
       }
