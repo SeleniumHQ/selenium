630de2bc382613c2dcfdf3003093bf48515a4962
Jari Bakken <jari.bakken@gmail.com>
2011-03-10 14:35:45 +0000

    JariBakken: Re-enable screenshot specs for IE (fixed by r11652).
    r11655

--- rb/spec/integration/selenium/webdriver/driver_spec.rb
+++ rb/spec/integration/selenium/webdriver/driver_spec.rb
@@ -21,27 +21,25 @@ describe "Driver" do
     end
   end
 
-  not_compliant_on :browser => :ie do
-    it "should save a screenshot" do
-      driver.navigate.to url_for("xhtmlTest.html")
-      path = "screenshot_tmp.png"
+  it "should save a screenshot" do
+    driver.navigate.to url_for("xhtmlTest.html")
+    path = "screenshot_tmp.png"
 
-      begin
-        driver.save_screenshot path
-        File.exist?(path).should be_true # sic
-        File.size(path).should > 0
-      ensure
-        File.delete(path) if File.exist?(path)
-      end
+    begin
+      driver.save_screenshot path
+      File.exist?(path).should be_true # sic
+      File.size(path).should > 0
+    ensure
+      File.delete(path) if File.exist?(path)
     end
+  end
 
-    it "should return a screenshot in the specified format" do
-      driver.navigate.to url_for("xhtmlTest.html")
+  it "should return a screenshot in the specified format" do
+    driver.navigate.to url_for("xhtmlTest.html")
 
-      ss = driver.screenshot_as(:png)
-      ss.should be_kind_of(String)
-      ss.size.should > 0
-    end
+    ss = driver.screenshot_as(:png)
+    ss.should be_kind_of(String)
+    ss.size.should > 0
   end
 
   it "raises an error when given an unknown format" do
