972b81577397215bf5c49f63ffeea8ea08a37e6a
Dounia Berrada <berrada@google.com>
2011-03-29 18:01:17 +0000

    DouniaBerrada: Updating R.java for Android
    r11830

--- android/gen/org/openqa/selenium/android/app/R.java
+++ android/gen/org/openqa/selenium/android/app/R.java
@@ -26,7 +26,8 @@ public final class R {
         public static final int single_session_layout=0x7f030000;
     }
     public static final class raw {
-        public static final int javascript_xpath=0x7f050000;
+        public static final int is_displayed_android=0x7f050000;
+        public static final int javascript_xpath=0x7f050001;
     }
     public static final class string {
         public static final int ctx_menu_navigate=0x7f060001;
