75cbe33bdecd7548c322592b35e25af30e16d1e3
Davis Burns <david.burns@theautomatedtester.co.uk>
2011-04-03 22:25:20 +0000

    DavidBurns deleting the extension folder if its there before we try copy to it
    r11869

--- py/selenium/webdriver/firefox/firefox_profile.py
+++ py/selenium/webdriver/firefox/firefox_profile.py
@@ -204,7 +204,8 @@ class FirefoxProfile(object):
             installrdfpath = os.path.join(tempdir,"install.rdf")
             ext_dir = os.path.join(
                 self.extensionsDir, self._read_id_from_install_rdf(installrdfpath))
-
+        if os.path.exists(ext_dir):
+            shutil.rmtree(ext_dir)
         shutil.copytree(tempdir, ext_dir)
 
     def _read_id_from_install_rdf(self, installrdfpath):
