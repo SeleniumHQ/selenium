e231d2ef4a3c2505544576a57f72b65f52a2977f
Jari Bakken <jari.bakken@gmail.com>
2011-04-19 16:41:00 +0000

    JariBakken: Making navigation_spec more robust with native events (IE).
    r12046

--- rb/spec/integration/selenium/webdriver/navigation_spec.rb
+++ rb/spec/integration/selenium/webdriver/navigation_spec.rb
@@ -1,27 +1,43 @@
 require File.expand_path("../spec_helper", __FILE__)
 
 describe "Navigation" do
+  let(:wait) { Selenium::WebDriver::Wait.new }
+
   it "should navigate back and forward" do
-    driver.navigate.to url_for("formPage.html")
-    driver.current_url.should == url_for("formPage.html")
+    form_title   = "We Leave From Here"
+    result_title = "We Arrive Here"
+    form_url     = url_for "formPage.html"
+    result_url   = url_for "resultPage.html"
+
+    driver.navigate.to form_url
+    driver.title.should == form_title
 
     driver.find_element(:id, 'imageButton').submit
-    driver.title.should == "We Arrive Here"
+    wait.until { driver.title != form_title }
+
+    driver.current_url.should include(result_url)
+    driver.title.should == result_title
 
     driver.navigate.back
-    driver.current_url.should == url_for("formPage.html")
-    driver.title.should == "We Leave From Here"
+
+    driver.current_url.should include(form_url)
+    driver.title.should == form_title
 
     driver.navigate.forward
-    driver.title.should == "We Arrive Here"
+    driver.current_url.should include(result_url)
+    driver.title.should == result_title
   end
 
   it "should refresh the page" do
+    changed_title = "Changed"
+
     driver.navigate.to url_for("javascriptPage.html")
-    driver.find_element(:xpath, '//a[text() = "Change the page title!"]').click
-    driver.title.should == "Changed"
+    driver.find_element(:link_text, "Change the page title!").click
+    driver.title.should == changed_title
 
     driver.navigate.refresh
+    wait.until { driver.title != changed_title }
+
     driver.title.should == "Testing Javascript"
   end
 end
