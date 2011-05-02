f3fe357cc1dcde102ce86798a316e52ef793d69d
Davis Burns <david.burns@theautomatedtester.co.uk>
2011-05-02 09:41:21 +0000

    DavidBurns adding save_screenshot to Chrome. Will error till exposed
    r12095

--- py/selenium/webdriver/chrome/webdriver.py
+++ py/selenium/webdriver/chrome/webdriver.py
@@ -51,3 +51,19 @@ class WebDriver(RemoteWebDriver):
             pass
         finally:
             self.service.stop()
+
+    def save_screenshot(self, filename):
+        """
+        Gets the screenshot of the current window. Returns False if there is
+        any IOError, else returns True. Use full paths in your filename.
+        """
+        png = self._execute(Command.SCREENSHOT)['value']
+        try:
+            f = open(filename, 'wb')
+            f.write(base64.decodestring(png))
+            f.close()
+        except IOError:
+            return False
+        finally:
+            del png
+        return True
