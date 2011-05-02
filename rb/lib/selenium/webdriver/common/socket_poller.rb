a473ab902aa70f8b3d2cd15b180264e49857cd39
Jari Bakken <jari.bakken@gmail.com>
2011-05-02 21:01:59 +0000

    JariBakken: Bump the connect timeout in SocketPoller's select() call.
    This appears to be necessary on JRuby.
    
    r12103

--- rb/lib/selenium/webdriver/common/socket_poller.rb
+++ rb/lib/selenium/webdriver/common/socket_poller.rb
@@ -36,6 +36,8 @@ module Selenium
 
       private
 
+      CONNECT_TIMEOUT = 5
+
       NOT_CONNECTED_ERRORS = [Errno::ECONNREFUSED, Errno::ENOTCONN, SocketError]
       NOT_CONNECTED_ERRORS << Errno::EPERM if Platform.cygwin?
 
@@ -50,7 +52,7 @@ module Selenium
         begin
           sock.connect_nonblock sockaddr
         rescue Errno::EINPROGRESS
-          if IO.select(nil, [sock], nil, 1)
+          if IO.select(nil, [sock], nil, CONNECT_TIMEOUT)
             retry
           else
             raise Errno::ECONNREFUSED
