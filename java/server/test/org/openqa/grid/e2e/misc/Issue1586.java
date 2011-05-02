030e7152ec506004ea4178f1de1e896013a043dd
Francois Reynaud <francois.reynaud@gmail.com>
2011-05-02 17:19:23 +0000

    FrancoisReynaud: adding a disabled test showing issue1586.
    r12097

---
+++ java/server/test/org/openqa/grid/e2e/misc/Issue1586.java
@@ -0,0 +1,59 @@
+package org.openqa.grid.e2e.misc;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.grid.e2e.utils.GridConfigurationMock;
+import org.openqa.grid.internal.Registry;
+import org.openqa.grid.selenium.SelfRegisteringRemote;
+import org.openqa.grid.web.Hub;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.net.PortProber;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+// see http://code.google.com/p/selenium/issues/detail?id=1586
+public class Issue1586 {
+
+	private Hub hub = Hub.getNewInstanceForTest(PortProber.findFreePort(), Registry.getNewInstanceForTestOnly());
+	private URL hubURL = hub.getUrl();
+
+	@BeforeClass(alwaysRun = true)
+	public void prepare() throws Exception {
+
+		hub.start();
+		hubURL = new URL("http://" + hub.getHost() + ":" + hub.getPort());
+		SelfRegisteringRemote remote = SelfRegisteringRemote.create(GridConfigurationMock.webdriverConfig(hub.getRegistrationURL()));
+		remote.addFirefoxSupport();
+		remote.launchRemoteServer();
+		remote.registerToHub();
+
+	}
+
+	@Test(enabled=false)
+	public void test() throws MalformedURLException, InterruptedException {
+		DesiredCapabilities ff = DesiredCapabilities.firefox();
+		WebDriver driver = new RemoteWebDriver(new URL(hubURL + "/grid/driver"), ff);
+		for (int i = 0; i < 20; i++) {
+			driver.get("http://code.google.com/p/selenium/");
+			WebElement keywordInput = driver.findElement(By.name("q"));
+			keywordInput.clear();
+			keywordInput.sendKeys("test");
+			WebElement submitButton = driver.findElement(By.name("projectsearch"));
+			submitButton.click();
+			driver.getCurrentUrl(); // fails here
+		}
+		driver.quit();
+
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void stop() throws Exception {
+		hub.stop();
+	}
+}
