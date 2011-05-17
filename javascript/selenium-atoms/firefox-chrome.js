35b62772e5b12799194663ef1812838fed3f2d4d
Samit.Badle@googlemail.com <Samit.Badle@gmail.com>
2011-05-17 15:39:48 +0000

    SamitBadle: Fixing Issue 1463 with Simon's code and small modifications to exclude unwrapping on Firefox versions earlier than 4.0
    r12202

--- javascript/selenium-atoms/firefox-chrome.js
+++ javascript/selenium-atoms/firefox-chrome.js
@@ -1,5 +1,24 @@
-
 goog.provide('core.firefox');
+
+/**
+ * @returns {boolean} Whether the firefox instance needs elements to be
+ * unwrapped.
+ */
+core.firefox.isUsingUnwrapping_ = function() {
+    try {
+        var appInfo = Components.classes['@mozilla.org/xre/app-info;1'].
+                getService(Components.interfaces.nsIXULAppInfo);
+        var versionChecker = Components.classes['@mozilla.org/xpcom/version-comparator;1'].
+                getService(Components.interfaces.nsIVersionComparator);
+
+        return (versionChecker.compare(appInfo.version, '4.0') >= 0);
+    } catch(e) {
+        // like when its not Firefox
+        return false;
+    }
+};
+
+core.firefox.isUsingUnwrapping_ = core.firefox.isUsingUnwrapping_();
 
 // This method closely follows unwrapNode() from mozmill-tests
 /**
@@ -9,34 +28,38 @@ goog.provide('core.firefox');
  * @returns {!Object} The object, unwrapped if possible.
  */
 core.firefox.unwrap = function(thing) {
-  if (!goog.isDefAndNotNull(thing)) {
-    return thing;
-  }
-
-  // If we've already unwrapped the object, don't unwrap it again.
-  // TODO(simon): see whether XPCWrapper->IsSecurityWrapper is available in JS
-  if (thing.__fxdriver_unwrapped) {
-    return thing;
-  }
-
-  if (thing['wrappedJSObject']) {
-    thing.wrappedJSObject.__fxdriver_unwrapped = true;
-    return thing.wrappedJSObject;
-  }
-
-  // unwrap is not available on older branches (3.5 and 3.6) - Bug 533596
-  try {
-    var isWrapper = thing == XPCNativeWrapper(thing);
-    if (isWrapper) {
-      var unwrapped = XPCNativeWrapper.unwrap(thing);
-      var toReturn = !!unwrapped ? unwrapped : thing;
-      toReturn.__fxdriver_unwrapped = true;
-      return toReturn;
+    if (!core.firefox.isUsingUnwrapping_) {
+        return thing;
     }
-  } catch(e) {
-    // Unwrapping will fail for JS literals - numbers, for example. Catch
-    // the exception and proceed, it will eventually be returned as-is.
-  }
 
-  return thing;
+    if (!goog.isDefAndNotNull(thing)) {
+        return thing;
+    }
+
+    // If we've already unwrapped the object, don't unwrap it again.
+    // TODO(simon): see whether XPCWrapper->IsSecurityWrapper is available in JS
+    if (thing.__fxdriver_unwrapped) {
+        return thing;
+    }
+
+    if (thing['wrappedJSObject']) {
+        thing.wrappedJSObject.__fxdriver_unwrapped = true;
+        return thing.wrappedJSObject;
+    }
+
+    // unwrap is not available on older branches (3.5 and 3.6) - Bug 533596
+    try {
+        var isWrapper = thing == XPCNativeWrapper(thing);
+        if (isWrapper) {
+            var unwrapped = XPCNativeWrapper.unwrap(thing);
+            var toReturn = !!unwrapped ? unwrapped : thing;
+            toReturn.__fxdriver_unwrapped = true;
+            return toReturn;
+        }
+    } catch(e) {
+        // Unwrapping will fail for JS literals - numbers, for example. Catch
+        // the exception and proceed, it will eventually be returned as-is.
+    }
+
+    return thing;
 };
