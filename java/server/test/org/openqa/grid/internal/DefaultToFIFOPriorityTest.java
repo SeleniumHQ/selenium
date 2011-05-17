a5ec52317ddb1b45c3d751af2416b02442eb0e76
Francois Reynaud <francois.reynaud@gmail.com>
2011-05-17 15:09:20 +0000

    FrancoisReynaud : converting from testNG toJUnit.
    r12200

--- java/server/test/org/openqa/grid/internal/DefaultToFIFOPriorityTest.java
+++ java/server/test/org/openqa/grid/internal/DefaultToFIFOPriorityTest.java
@@ -7,44 +7,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import junit.framework.Assert;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.grid.internal.listeners.Prioritizer;
 import org.openqa.grid.internal.mock.MockedNewSessionRequestHandler;
 import org.openqa.grid.internal.mock.MockedRequestHandler;
 import org.openqa.grid.web.servlet.handler.RequestType;
-import org.testng.Assert;
-import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
 
 // TODO freynaud copy paste from PriorityTestLoad ....
-@Test(timeOut=10000)
+
 public class DefaultToFIFOPriorityTest {
 
 	private final static int MAX = 50;
-	
-	private Registry registry = Registry.getNewInstanceForTestOnly();
+
+	private static Registry registry = Registry.getNewInstanceForTestOnly();
 
 	// priority rule : nothing defined = FIFO
-	private Prioritizer fifo = null;
+	private static Prioritizer fifo = null;
 
-	Map<String, Object> ff = new HashMap<String, Object>();
-	RemoteProxy p1;
-	List<MockedRequestHandler> requests = new ArrayList<MockedRequestHandler>();
+	private static Map<String, Object> ff = new HashMap<String, Object>();
+	private static RemoteProxy p1;
+	private static List<MockedRequestHandler> requests = new ArrayList<MockedRequestHandler>();
+	private static TestSession session = null;
 
 	/**
 	 * create a hub with 1 FF
+	 * 
+	 * @throws InterruptedException
 	 */
-	@BeforeClass(alwaysRun = true)
-	public void setup() {
+	@BeforeClass
+	public static void setup() throws InterruptedException {
 
 		registry.setPrioritizer(fifo);
 		ff.put(APP, "FF");
 		p1 = RemoteProxyFactory.getNewBasicRemoteProxy(ff, "http://machine1:4444");
 		registry.add(p1);
 
-		
 		for (int i = 1; i <= MAX; i++) {
 			Map<String, Object> cap = new HashMap<String, Object>();
 			cap.put(APP, "FF");
@@ -52,23 +53,15 @@ public class DefaultToFIFOPriorityTest {
 			MockedNewSessionRequestHandler req = new MockedNewSessionRequestHandler(registry, cap);
 			requests.add(req);
 		}
-	}
 
-	TestSession session = null;
-
-	// use all the spots ( so 1 ) of the grid so that a queue buils up
-	@Test
-	public void useAllProxies() {
+		// use all the spots ( so 1 ) of the grid so that a queue buils up
 		MockedRequestHandler newSessionRequest = new MockedRequestHandler(registry);
 		newSessionRequest.setRequestType(RequestType.START_SESSION);
 		newSessionRequest.setDesiredCapabilities(ff);
 		newSessionRequest.process();
 		session = newSessionRequest.getTestSession();
-	}
 
-	// fill the queue with MAX requests.
-	@Test(dependsOnMethods = "useAllProxies")
-	public void queueSomeMore() {
+		// fill the queue with MAX requests.
 		for (MockedRequestHandler h : requests) {
 			final MockedRequestHandler req = h;
 			new Thread(new Runnable() {
@@ -77,40 +70,28 @@ public class DefaultToFIFOPriorityTest {
 				}
 			}).start();
 		}
-	}
+		
+		System.out.println(">" + requests);
 
-	// free the grid : the queue is consumed, and the test with the highest
-	// priority should be processed.
-	@Test(dependsOnMethods = "queueSomeMore")
-	public void releaseTheSessionBlockingTheGrid() throws InterruptedException {
-		while(requests.size()!=MAX){
+		// free the grid : the queue is consumed, and the test with the highest
+		// priority should be processed.
+		while (requests.size() != MAX) {
 			Thread.sleep(250);
 		}
 		session.terminateSyncronousFOR_TEST_ONLY();
-
 	}
 
 	// validate that the one with priority 5 has been assigned a proxy
-	@Test(dependsOnMethods = "releaseTheSessionBlockingTheGrid")
-	public void validate() throws InterruptedException {
-		int cpt=0;
-		while(cpt <10){
-			Thread.sleep(250);
-			cpt++;
-			try {
-				requests.get(0).getTestSession();
-			}catch (IllegalAccessError iae){
-				//ignore
-			}
-		}
-		System.out.println(">"+requests);
-		Assert.assertNotNull(requests.get(0).getTestSession());	
-		Assert.assertEquals(requests.get(0).getDesiredCapabilities().get("_priority"), 1);
+	@Test
+	public void validateRequestAreHandledFIFO() throws InterruptedException {
+		System.out.println(">" + requests);
+		Thread.sleep(1000);
+		Assert.assertNotNull(requests.get(0).getTestSession());
+		Assert.assertEquals(1, requests.get(0).getDesiredCapabilities().get("_priority"));
 	}
-	
-	
-	@AfterClass(alwaysRun=true)
-	public void teardown(){
+
+	@AfterClass
+	public static void teardown() {
 		registry.stop();
 	}
 
