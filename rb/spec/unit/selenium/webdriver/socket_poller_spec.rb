7c6ec4d030b99992aa19a065f724a35bfd00f83b
Jari Bakken <jari.bakken@gmail.com>
2011-05-18 18:17:18 +0000

    JariBakken: Make sure the SocketPoller spec passes on JRuby.
    r12210

--- rb/spec/unit/selenium/webdriver/socket_poller_spec.rb
+++ rb/spec/unit/selenium/webdriver/socket_poller_spec.rb
@@ -1,51 +1,68 @@
 require File.expand_path("../spec_helper", __FILE__)
 
-describe Selenium::WebDriver::SocketPoller do
-  let(:poller)         { Selenium::WebDriver::SocketPoller.new("localhost", 1234, 5, 0.05)  }
-  let(:socket)         { mock Socket, :close => true}
+module Selenium
+  module WebDriver
+    describe SocketPoller do
+      let(:poller)         { Selenium::WebDriver::SocketPoller.new("localhost", 1234, 5, 0.05)  }
+      let(:socket)         { mock Socket, :close => true}
 
-  before do
-    Socket.should_receive(:new).any_number_of_times.and_return socket
+      def setup_connect(*states)
+        # TODO(jari): find a cleaner way to solve the platform-specific collaborators
+        if Platform.jruby?
+          states.each { |state|
+            if state
+              TCPSocket.should_receive(:new).and_return socket
+            else
+              TCPSocket.should_receive(:new).and_raise Errno::ECONNREFUSED
+            end
+          }
+        else
+          Socket.should_receive(:new).any_number_of_times.and_return socket
+          states.each { |state|
+            socket.should_receive(:connect_nonblock).
+                   and_raise(state ? Errno::EISCONN : Errno::ECONNREFUSED)
+          }
+        end
+      end
+
+      describe "#connected?" do
+        it "returns true when the socket is listening" do
+          setup_connect false, true
+          poller.should be_connected
+        end
+
+        it "returns false if the socket is not listening after the given timeout" do
+          setup_connect false
+
+          start = Time.parse("2010-01-01 00:00:00")
+          wait  = Time.parse("2010-01-01 00:00:04")
+          stop  = Time.parse("2010-01-01 00:00:06")
+
+          Time.should_receive(:now).and_return(start, wait, stop)
+          poller.should_not be_connected
+        end
+      end
+
+      describe "#closed?" do
+        it "returns true when the socket is closed" do
+          setup_connect true, true, false
+          
+          poller.should be_closed
+        end
+
+        it "returns false if the socket is still listening after the given timeout" do
+          setup_connect true
+          
+          start = Time.parse("2010-01-01 00:00:00")
+          wait  = Time.parse("2010-01-01 00:00:04")
+          stop  = Time.parse("2010-01-01 00:00:06")
+
+          Time.should_receive(:now).and_return(start, wait, stop)
+
+          poller.should_not be_closed
+        end
+      end
+
+    end
   end
-
-  describe "#connected?" do
-    it "returns true when the socket is listening" do
-      socket.should_receive(:connect_nonblock).and_raise(Errno::ECONNREFUSED)
-      socket.should_receive(:connect_nonblock).and_raise(Errno::EISCONN)
-
-      poller.should be_connected
-    end
-
-    it "returns false if the socket is not listening after the given timeout" do
-      start = Time.parse("2010-01-01 00:00:00")
-      wait  = Time.parse("2010-01-01 00:00:04")
-      stop  = Time.parse("2010-01-01 00:00:06")
-
-      Time.should_receive(:now).and_return(start, wait, stop)
-      socket.should_receive(:connect_nonblock).and_raise(Errno::ECONNREFUSED)
-
-      poller.should_not be_connected
-    end
-  end
-
-  describe "#closed?" do
-    it "returns true when the socket is closed" do
-      socket.should_receive(:connect_nonblock).twice.and_raise Errno::EISCONN
-      socket.should_receive(:connect_nonblock).and_raise Errno::ECONNREFUSED
-
-      poller.should be_closed
-    end
-
-    it "returns false if the socket is still listening after the given timeout" do
-      start = Time.parse("2010-01-01 00:00:00")
-      wait  = Time.parse("2010-01-01 00:00:04")
-      stop  = Time.parse("2010-01-01 00:00:06")
-
-      Time.should_receive(:now).and_return(start, wait, stop)
-      socket.should_receive(:connect_nonblock).and_raise Errno::EISCONN
-
-      poller.should_not be_closed
-    end
-  end
-
 end
