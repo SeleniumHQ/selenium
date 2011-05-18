ba7b82a28302e2e866bcb123ad8b257ab6916e6c
Jari Bakken <jari.bakken@gmail.com>
2011-05-18 16:53:47 +0000

    JariBakken: Make JS compilation less chatty again.
    r12204

--- rake-tasks/crazy_fun/mappings/javascript.rb
+++ rake-tasks/crazy_fun/mappings/javascript.rb
@@ -107,9 +107,9 @@ module Javascript
 
       # ignore stdout --- the commands we use log to stderr
       # this also causes the command to actually execute
-      puts out.read
+
       output = err.read
-      puts output
+
       if output =~ /ERROR/m
         puts output
         exit(2)
