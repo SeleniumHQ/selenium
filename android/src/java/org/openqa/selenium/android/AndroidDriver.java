7f3a82a87e15f9a3df76fe128bea3fc24ab18b71
Eran Mes <eran.mes@gmail.com>
2011-05-26 20:49:21 +0000

    EranMes: Reverting r12261 as it is breaking the build on Mac and Windows.
    r12265

--- android/src/java/org/openqa/selenium/android/AndroidDriver.java
+++ android/src/java/org/openqa/selenium/android/AndroidDriver.java
@@ -43,8 +43,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.android.util.JsUtil;
 import org.openqa.selenium.android.util.SimpleTimer;
 import org.openqa.selenium.html5.BrowserConnection;
-import org.openqa.selenium.html5.Location;
-import org.openqa.selenium.html5.LocationContext;
 import org.openqa.selenium.internal.Base64Encoder;
 import org.openqa.selenium.internal.FindsById;
 import org.openqa.selenium.internal.FindsByLinkText;
@@ -53,11 +51,13 @@ import org.openqa.selenium.internal.FindsByTagName;
 import org.openqa.selenium.internal.FindsByXPath;
 
 import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
 import android.util.Log;
 
 public class AndroidDriver implements WebDriver, SearchContext, FindsByTagName, JavascriptExecutor,
     FindsById, FindsByLinkText, FindsByName, FindsByXPath, TakesScreenshot,
-    Rotatable, BrowserConnection, LocationContext {
+    Rotatable, BrowserConnection {
 
   public static final String LOG_TAG = AndroidDriver.class.getName();
   
@@ -575,10 +575,15 @@ public class AndroidDriver implements WebDriver, SearchContext, FindsByTagName, 
   }
 
   public boolean isOnline() {
-    return controller.isConnected();
+    return Settings.System.getInt(getContext().getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON, 0) == 0;
   }
 
   public void setOnline(boolean online) throws WebDriverException {
-    controller.setConnected(online);
+    Settings.System.putInt(getContext().getContentResolver(),
+        Settings.System.AIRPLANE_MODE_ON, online ? 0 : 1);
+    Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+    intent.putExtra("state", !online);
+    getContext().sendBroadcast(intent);
   }
 }
