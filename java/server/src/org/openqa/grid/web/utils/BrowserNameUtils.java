1d465d488b41629824cb6ef2fcd5ea1a8bf7d8f9
Francois Reynaud <francois.reynaud@gmail.com>
2011-06-04 15:05:45 +0000

    FrancoisReynaud: addign a helper method to get the browser icons for the grid consoles.
    r12364

--- java/server/src/org/openqa/grid/web/utils/BrowserNameUtils.java
+++ java/server/src/org/openqa/grid/web/utils/BrowserNameUtils.java
@@ -12,9 +12,11 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 
 package org.openqa.grid.web.utils;
+
+import static org.openqa.grid.common.RegistrationRequest.BROWSER;
 
 import com.google.common.collect.Maps;
 import org.openqa.grid.common.RegistrationRequest;
@@ -27,50 +29,56 @@ import java.util.Map;
  * Utilities for dealing with browser names.
  */
 public class BrowserNameUtils {
-    public static String lookupGrid1Environment(String browserString) {
-        String translatedBrowserString = Hub.getGrid1Mapping().get(browserString);
+	public static String lookupGrid1Environment(String browserString) {
+		String translatedBrowserString = Hub.getGrid1Mapping().get(browserString);
 
-        return (translatedBrowserString == null) ? browserString : translatedBrowserString;
-    }
+		return (translatedBrowserString == null) ? browserString : translatedBrowserString;
+	}
 
-    public static Map<String, Object> parseGrid2Environment(String environment) {
-        Map<String, Object> ret = Maps.newHashMap();
+	public static Map<String, Object> parseGrid2Environment(String environment) {
+		Map<String, Object> ret = Maps.newHashMap();
 
-        String[] details = environment.split(" ");
-        if (details.length == 1) {
-            // simple browser string
-            ret.put(RegistrationRequest.BROWSER, details[0]);
-        } else {
-            // more complex. Only case handled so far = X on Y
-            // where X is the browser string, Y the OS
-            ret.put(RegistrationRequest.BROWSER, details[0]);
-            if (details.length==3){
-                ret.put(RegistrationRequest.PLATFORM, Platform.extractFromSysProperty(details[2]));
-            }
-        }
+		String[] details = environment.split(" ");
+		if (details.length == 1) {
+			// simple browser string
+			ret.put(RegistrationRequest.BROWSER, details[0]);
+		} else {
+			// more complex. Only case handled so far = X on Y
+			// where X is the browser string, Y the OS
+			ret.put(RegistrationRequest.BROWSER, details[0]);
+			if (details.length == 3) {
+				ret.put(RegistrationRequest.PLATFORM, Platform.extractFromSysProperty(details[2]));
+			}
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
-    public static String consoleIconName(String browserString) {
-        String ret = browserString;
+	public static String consoleIconName(String browserString) {
+		String ret = browserString;
 
-        // Take care of any Grid 1.0 named environment translation.
-        if (browserString.charAt(0) != '*') {
-            browserString = lookupGrid1Environment(browserString);
-        }
+		// Take care of any Grid 1.0 named environment translation.
+		if (browserString.charAt(0) != '*') {
+			browserString = lookupGrid1Environment(browserString);
+		}
 
-        // Map browser environments to icon names.
-        if (browserString.contains("iexplore") || browserString.startsWith("*iehta")) {
+		// Map browser environments to icon names.
+		if (browserString.contains("iexplore") || browserString.startsWith("*iehta")) {
 			ret = "internet explorer";
 		} else if (browserString.contains("firefox") || browserString.startsWith("*chrome")) {
 			ret = "firefox";
 		} else if (browserString.startsWith("*safari")) {
 			ret = "safari";
 		} else if (browserString.startsWith("*googlechrome")) {
-            ret = "chrome";
-        }
+			ret = "chrome";
+		}
 
-        return ret;
-    }
+		return ret;
+	}
+
+	public static String getConsoleIconPath(String browserString) {
+		String name = consoleIconName(browserString);
+		return "/grid/resources/images/" + name + ".png";
+	}
+
 }
