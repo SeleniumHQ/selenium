a36e2c0551fdb3f23438d8503770ed2364ab1632
Jim Evans <james.h.evans.jr@gmail.com>
2011-06-08 20:53:20 +0000

    JimEvans: Updating .NET AssemblyInfo.cs files
    r12417

--- dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// <copyright file="AssemblyInfo.cs" company="WebDriver Committers">
+// Copyright 2007-2011 WebDriver committers
+// Copyright 2007-2011 Google Inc.
+// Portions copyright 2007 ThoughtWorks, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -6,12 +24,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Selenium")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Selenium WebDriver WebDriverBackedSelenium")]
+[assembly: AssemblyDescription("Selenium WebDriver .NET Bindings")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("QALAB")]
+[assembly: AssemblyCompany("Selenium Committers")]
 [assembly: AssemblyProduct("Selenium")]
-[assembly: AssemblyCopyright("Copyright © Selenium Contributors 2010")]
+[assembly: AssemblyCopyright("Copyright © Selenium Committers 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- dotnet/src/WebDriver/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver/Properties/AssemblyInfo.cs
@@ -24,12 +24,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebDriver")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Selenium WebDriver")]
+[assembly: AssemblyDescription("Selenium WebDriver .NET Bindings")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("WebDriver Committers")]
-[assembly: AssemblyProduct("WebDriver")]
-[assembly: AssemblyCopyright("Copyright © WebDriver Committers 2011")]
+[assembly: AssemblyCompany("Selenium Committers")]
+[assembly: AssemblyProduct("Selenium")]
+[assembly: AssemblyCopyright("Copyright © Selenium Committers 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,10 +37,12 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f5381bd7-128a-4fa5-80e5-4dab4c37c1ae")]
+
+// Mark the assembly as CLSCompliant for use with all CLS languages
+[assembly: CLSCompliant(true)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -48,9 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
