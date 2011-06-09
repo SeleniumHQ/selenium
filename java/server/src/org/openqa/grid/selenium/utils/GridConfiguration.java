ea672248c471bb1d0ca8b0859509cfb966577a57
Francois Reynaud <francois.reynaud@gmail.com>
2011-06-09 10:59:07 +0000

    FrancoisReynaud: #1803
    r12426

--- java/server/src/org/openqa/grid/selenium/utils/GridConfiguration.java
+++ java/server/src/org/openqa/grid/selenium/utils/GridConfiguration.java
@@ -264,15 +264,17 @@ public class GridConfiguration {
 			if (registrationURL == null) {
 				throw new InvalidParameterException("registration url cannot be null");
 			}
+			// TODO freyanud : validation should also check that the selenium server
+			// param passed to the node do not contain anything that doesn't make
+			// sense in a grid environement.For instance launching a node with
+			// -interactive.
+			if (getNodeRemoteControlConfiguration().isInteractive() == true) {
+				throw new InvalidParameterException("no point launching the node in interactive mode");
+			}
 		}
 
-		// TODO freyanud : validation should also check that the selenium server
-		// param passed to the node do not contain anything that doesn't make
-		// sense in a grid environement.For instance launching a node with
-		// -interactive.
-		if (getNodeRemoteControlConfiguration().isInteractive() == true) {
-			throw new InvalidParameterException("no point launching the node in interactive mode");
-		}
+		
+		
 
 	}
 
