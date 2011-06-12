9fb9ace2444c429590ebca4abe4bd45f12f2764e
Simon M Stewart <simon.m.stewart@gmail.com>
2011-06-12 21:12:20 +0000

    SimonStewart: tests can also be found in packages beginning with .com
    r12460

--- java/client/test/org/openqa/selenium/TestSuiteBuilder.java
+++ java/client/test/org/openqa/selenium/TestSuiteBuilder.java
@@ -17,38 +17,42 @@ limitations under the License.
 
 package org.openqa.selenium;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.openqa.selenium.internal.InProject;
 
 import java.io.File;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertTrue;
 
-import org.openqa.selenium.internal.InProject;
-
 public class TestSuiteBuilder {
 
-  private Set<File> sourceDirs = new HashSet<File>();
-  private Set<Ignore.Driver> ignored = new HashSet<Ignore.Driver>();
+  private Set<File> sourceDirs = Sets.newHashSet();
+  private Set<Ignore.Driver> ignored = Sets.newHashSet();
   private Class<? extends WebDriver> driverClass;
   private boolean keepDriver;
   private boolean includeJavascript;
   private boolean withDriver = true;
   private boolean withEnvironment = true;
-  private List<String> onlyRun = new ArrayList<String>();
-  private Set<String> patterns = new HashSet<String>();
-  private Set<String> excludePatterns = new HashSet<String>();
-  private Set<String> testMethodNames = new HashSet<String>();
-  private Set<String> decorators = new LinkedHashSet<String>();
-  private Set<Package> packages = new LinkedHashSet<Package>();
+  private List<String> onlyRun = Lists.newArrayList();
+  private Set<String> patterns = Sets.newHashSet();
+  private Set<String> excludePatterns = Sets.newHashSet();
+  private Set<String> testMethodNames = Sets.newHashSet();
+  private Set<String> decorators = Sets.newLinkedHashSet();
+  private Set<Package> packages = Sets.newLinkedHashSet();
   private boolean outputTestNames = false;
   private File baseDir;
 
@@ -262,10 +266,13 @@ public class TestSuiteBuilder {
       return null;
     }
 
-    // Assume that all classes are under a "org" package
+    // Assume that all classes are under a "org" or "com" package
     int index = path.indexOf("/org/");
     if (index == -1) {
-      return null;
+      index = path.indexOf("/com/");
+      if (index == -1) {
+        return null;
+      }
     }
 
     path = path.substring(index + 1, path.length() - ".java".length());
