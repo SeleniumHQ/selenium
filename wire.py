4bd901a49a9a392d30109da8594e0dc10aebdd2b
Jason Lebya <jmleyba@gmail.com>
2011-06-20 20:34:44 +0000

    JasonLeyba: Adding more detail to the /status command's documentation.
    r12543

--- wire.py
+++ wire.py
@@ -200,9 +200,30 @@ def main():
   resources = []
  
   resources.append(Resource('/status').
-      Get('Gets the server\'s status').
-      SetReturnType(None, 'A status of 0 is returned if the server is ready \
-(HTTP 200 OK response)'))
+      Get('''
+Query the server\'s current status.  The server should respond with a general \
+"HTTP 200 OK" response if it is alive and accepting commands. The response \
+body should be a JSON object describing the state of the server. All server \
+implementations should return two basic objects describing the server's \
+current platform and when the server was built. All fields are optional; \
+if omitted, the client should assume the value is uknown. Furthermore, \
+server implementations may include additional fields not listed here.
+
+|| *Key* || *Type* || *Description* ||
+|| build || object || ||
+|| build.version || string || A generic release label (i.e. "2.0rc3") ||
+|| build.revision || string || The revision of the local source control client \
+from which the server was built ||
+|| build.time || string || A timestamp from when the server was built. ||
+|| os || object || ||
+|| os.arch || string || The current system architecture. ||
+|| os.name || string || The name of the operating system the server is \
+currently running on: "windows", "linux", etc. ||
+|| os.version || string || The operating system version. ||
+
+''').
+      SetReturnType('{object}',
+                    'An object describing the general status of the server.'))
  
   resources.append(
       Resource('/session').
