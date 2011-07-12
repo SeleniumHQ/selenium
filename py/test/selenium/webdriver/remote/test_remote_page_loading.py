405fa2916092e7ba8d3637c6827fb8454ca97cd5
David Burns <david.burns@theautomatedtester.co.uk>
2011-07-12 18:08:30 +0000

    DavidBurns refactoring remote tests moving the server start/stop to a central place
    r12903

--- py/test/selenium/webdriver/remote/test_remote_page_loading.py
+++ py/test/selenium/webdriver/remote/test_remote_page_loading.py
@@ -13,48 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-import socket
-import subprocess
-import time
-import urllib
-import signal
 from selenium.test.selenium.webdriver.common.webserver import SimpleWebServer
 from selenium.test.selenium.webdriver.common import page_loading_tests 
 from selenium import webdriver
-
-SERVER_ADDR = "localhost"
-DEFAULT_PORT = 4444
-
-
-def wait_for_server(url, timeout):
-    start = time.time()
-    while time.time() - start < timeout:
-        try:
-            urllib.urlopen(url)
-            return 1
-        except IOError:
-            time.sleep(0.2)
-
-    return 0
+from selenium.test.selenium.common import utils
 
 
 def setup_module(module):
-    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    url = "http://%s:%d/wd/hub" % (SERVER_ADDR, DEFAULT_PORT)
-    try:
-        _socket.connect((SERVER_ADDR, DEFAULT_PORT))
-        print ("The remote driver server is already running or something else"
-               "is using port %d, continuing..." % DEFAULT_PORT)
-    except:
-        print ("Starting the remote driver server")
-        RemotePageLoadingTests.server_proc = subprocess.Popen(
-            "java -jar build/java/server/src/org/openqa/selenium/server/server-standalone.jar",
-            shell=True)
-
-        assert wait_for_server(url, 10), "can't connect"
-        print "Server should be online"
-
+    utils.start_server(module)    
     webserver = SimpleWebServer()
     webserver.start()
     RemotePageLoadingTests.webserver = webserver
@@ -74,9 +40,4 @@ def teardown_module(module):
         RemotePageLoadingTests.webserver.stop()
     except AttributeError:
         pass    
-    # FIXME: This does not seem to work, the server process lingers
-    try:
-        os.kill(RemotePageLoadingTests.server_proc.pid, signal.SIGTERM)
-        time.sleep(5)
-    except:
-        pass
+    utils.stop_server(module)
