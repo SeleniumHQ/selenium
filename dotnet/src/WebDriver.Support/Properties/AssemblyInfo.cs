9e5e706764a733e7fd16116caf98f59483ca8717
Jim Evans <james.h.evans.jr@gmail.com>
2011-07-18 17:30:12 +0000

    JimEvans: Bumping .NET version numbers for development.
    r12985

--- dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
