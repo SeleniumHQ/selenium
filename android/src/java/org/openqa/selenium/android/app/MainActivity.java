3fdb13b69762f6558bb97c4fdee6c8a1cab4e6ee
Dounia Berrada <berrada@google.com>
2011-07-19 00:04:52 +0000

    DouniaBerrada: Removing unecessary imports from MainActivity.java
    r12989

--- android/src/java/org/openqa/selenium/android/app/MainActivity.java
+++ android/src/java/org/openqa/selenium/android/app/MainActivity.java
@@ -51,7 +51,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.Window;
 import android.webkit.CookieManager;
