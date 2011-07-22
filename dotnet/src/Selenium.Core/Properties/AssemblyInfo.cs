f17ec3f45d05dcdbcee42c3575f836a76e08d2c6
Jim Evans <james.h.evans.jr@gmail.com>
2011-07-22 09:59:52 +0000

    JimEvans: 2.2 .NET version number bump
    r13042

--- dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
