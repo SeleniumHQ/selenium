c760e25d147e8a58e2dda9d651811f50f19b0300
Jari Bakken <jari.bakken@gmail.com>
2011-07-28 13:06:51 +0000

    JariBakken: Update docs for TakesScreenshot, Rotatable
    r13114

--- rb/lib/selenium/webdriver/common/driver_extensions/rotatable.rb
+++ rb/lib/selenium/webdriver/common/driver_extensions/rotatable.rb
@@ -10,6 +10,14 @@ module Selenium
 
         ORIENTATIONS = [:landscape, :portrait]
 
+        #
+        # Change the screen orientation
+        #
+        # @param [:landscape, :portrait] Orientation
+        #
+        # @api public
+        #
+
         def rotate(orientation)
           unless ORIENTATIONS.include?(orientation)
             raise ArgumentError, "expected #{ORIENTATIONS.inspect}, got #{orientation.inspect}"
@@ -17,6 +25,14 @@ module Selenium
 
           bridge.setScreenOrientation(orientation.to_s.upcase)
         end
+
+        #
+        # Get the current screen orientation
+        #
+        # @return [:landscape, :portrait] orientation
+        #
+        # @api public
+        #
 
         def orientation
           bridge.getScreenOrientation.to_sym.downcase
--- rb/lib/selenium/webdriver/common/driver_extensions/takes_screenshot.rb
+++ rb/lib/selenium/webdriver/common/driver_extensions/takes_screenshot.rb
@@ -8,9 +8,23 @@ module Selenium
     module DriverExtensions
       module TakesScreenshot
 
+        #
+        # Save a PNG screenshot to the given path
+        #
+        # @api public
+        #
+
         def save_screenshot(png_path)
           File.open(png_path, 'wb') { |f| f << screenshot_as(:png) }
         end
+
+        #
+        # Return a PNG screenshot in the given format as a string
+        #
+        # @param [:base64, :png] format
+        # @return String screenshot
+        #
+        # @api public
 
         def screenshot_as(format)
           case format
