301ba010e5ba4aad5b9705e37068226a64067197
Simon M Stewart <simon.m.stewart@gmail.com>
2011-07-28 17:25:05 +0000

    SimonStewart: Moving towards using the closure library's logging infrastructure in the firefox driver
    r13125

--- javascript/firefox-driver/js/logging.js
+++ javascript/firefox-driver/js/logging.js
@@ -16,108 +16,165 @@
  */
 
 goog.provide('Logger');
+goog.provide('webdriver.debug');
+goog.provide('webdriver.debug.ConsoleFormatter');
+
+goog.require('goog.debug.Formatter');
+goog.require('goog.debug.Logger');
+goog.require('goog.debug.LogRecord');
+goog.require('goog.debug.TextFormatter');
 
 /**
- * Declare the namespace
+ * @private
  */
-var Logger = {
-  initialized: false
+webdriver.debug.initialized_ = false;
+
+/**
+ * Formatter that returns formatted text for display on Firefox's error console.
+ *
+ * @constructor
+ * @extends {goog.debug.Formatter}
+ */
+webdriver.debug.ConsoleFormatter = function() {
+  goog.debug.Formatter.call(this);
+};
+goog.inherits(webdriver.debug.ConsoleFormatter, goog.debug.Formatter);
+
+/**
+ * Formats a record as text.
+ *
+ * @param {goog.debug.LogRecord} logRecord the logRecord to format.
+ * @return {string} The formatted string.
+ */
+webdriver.debug.ConsoleFormatter.prototype.formatRecord = function(logRecord) {
+  // Build message html
+  var sb = [];
+
+  if (this.showSeverityLevel) {
+    sb.push('[', logRecord.getLevel().name, '] ');
+  }
+
+  var stack = Components.stack;
+  // Trial and error says that we're 7 levels deep in the stack when called.
+  for (var i = 0; i < 7 && stack; i++) {
+    stack = stack['caller'];
+  }
+
+  var filename = stack.filename.replace(/.*\//, '');
+  sb.push(filename + ':' + stack.lineNumber + ' ');
+
+  sb.push(logRecord.getMessage(), '\n');
+  if (this.showExceptionText && logRecord.getException()) {
+    sb.push(logRecord.getExceptionText(), '\n');
+  }
+
+  return sb.join('');
 };
 
-Logger._init = function() {
-  if (Logger.initialized == true) {
+/**
+ * Add a log handler to the logger that will record messages to Firefox's Error Console.
+ *
+ * @param {!goog.debug.Logger} logger The logger to use.
+ * @private
+ */
+webdriver.debug.addErrorConsoleLogger_ = function(logger) {
+  if (!Components) {
     return;
   }
 
-  Logger.initialized = true;
-  var CC = Components.classes;
-  var CI = Components.interfaces;
+  var formatter = new webdriver.debug.ConsoleFormatter();
+  formatter.showSeverityLevel = true;
 
-  var consoleService = CC["@mozilla.org/consoleservice;1"]
-      .getService(CI["nsIConsoleService"]);
+  var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
+      .getService(Components.interfaces["nsIConsoleService"]);
 
-  var prefs = CC["@mozilla.org/preferences-service;1"]
-      .getService(CI["nsIPrefBranch"]);
-  var logToConsole = prefs.prefHasUserValue("webdriver_log_to_console") &&
-      prefs.getBoolPref("webdriver_log_to_console");
-  var logToFile = prefs.prefHasUserValue("webdriver.log.file") &&
+  logger.addHandler(function(logRecord) {
+    var text = formatter.formatRecord(logRecord);
+    consoleService.logStringMessage(text);
+  })
+};
+
+
+/**
+ * Add a log handler to the logger that will record to a log file.
+ *
+ * @param {!goog.debug.Logger} logger The logger to use.
+ * @param {string?} opt_path The path to the file to use for logging.
+ * @private
+ */
+webdriver.debug.addFileLogger_ = function(logger, opt_path) {
+  if (!opt_path) {
+    return;
+  }
+
+  // Make sure that file exists
+  var file = Components.classes['@mozilla.org/file/local;1']
+      .createInstance(Components.interfaces['nsILocalFile']);
+  file.initWithPath(logToFile);
+  file.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0666);
+  var fileName = file.path;
+
+  var formatter = new goog.debug.TextFormatter('webdriver');
+
+  logger.addHandler(function(logRecord) {
+    var file = Components.classes['@mozilla.org/file/local;1']
+        .createInstance(Components.interfaces['nsILocalFile']);
+    file.initWithPath(fileName);
+
+    var ostream = Components.classes['@mozilla.org/network/file-output-stream;1']
+        .createInstance(Components.interfaces['nsIFileOutputStream']);
+    // Append to file
+    ostream.init(file, 0x02 | 0x10, 0666, 0);
+
+    var converter = Components.classes['@mozilla.org/intl/converter-output-stream;1']
+        .createInstance(Components.interfaces['nsIConverterOutputStream']);
+    converter.init(ostream, 'UTF-8', 0, 0);
+
+    var text = formatter.formatRecord(logRecord);
+    converter.writeString(text);
+
+    converter.close();
+  });
+};
+
+/**
+ * Initialize the goog.debug logging infrastructure with the pieces needed to
+ * log on Firefox.
+ */
+webdriver.debug.initialize = function() {
+  if (webdriver.debug.initialized_) {
+    return;
+  }
+
+  var rootLogger = goog.debug.Logger.getLogger('');
+  webdriver.debug.addErrorConsoleLogger_(rootLogger);
+
+  var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+     .getService(Components.interfaces["nsIPrefBranch"]);
+
+  var file = prefs.prefHasUserValue("webdriver.log.file") &&
       prefs.getCharPref("webdriver.log.file");
+  webdriver.debug.addFileLogger_(rootLogger, file);
 
-  if (logToConsole) {
-    var consoleLogger = {
-      observe: function(message) {
-        dump(message.message);
-      },
-      QueryInterface: function(iid) {
-        if (!iid.equals(CI['nsIConsoleListener']) &&
-            !idd.equals(CI['nsISupports'])) {
-          throw Components.results.NS_ERROR_NO_INTERFACE;
-        }
-
-        return this;
-      }
-    };
-
-    consoleService.registerListener(consoleLogger);
-  }
-
-  if (!!logToFile) {
-    // Make sure that file exists
-    var file = CC['@mozilla.org/file/local;1']
-        .createInstance(CI['nsILocalFile']);
-    file.initWithPath(logToFile);
-    file.createUnique(CI.nsIFile.NORMAL_FILE_TYPE, 0666);
-    var fileName = file.path;
-    consoleService.logStringMessage("Also logging to file: " + fileName);
-
-    var fileLogger = {
-      observe: function(message) {
-        var file = CC['@mozilla.org/file/local;1']
-            .createInstance(CI['nsILocalFile']);
-        file.initWithPath(fileName);
-
-        var ostream = CC['@mozilla.org/network/file-output-stream;1']
-        .createInstance(CI['nsIFileOutputStream']);
-        // Append to file
-        ostream.init(file, 0x02 | 0x10, 0666, 0);
-
-        var converter = CC['@mozilla.org/intl/converter-output-stream;1']
-            .createInstance(CI['nsIConverterOutputStream']);
-        converter.init(ostream, 'UTF-8', 0, 0);
-
-        converter.writeString(message.message);
-
-        converter.close();
-      },
-      QueryInterface: function(iid) {
-        if (!iid.equals(CI['nsIConsoleListener']) &&
-            !idd.equals(CI['nsISupports'])) {
-          throw Components.results.NS_ERROR_NO_INTERFACE;
-        }
-
-        return this;
-      }
-    };
-
-    consoleService.registerListener(fileLogger)
-  }
-
-  Logger.log_ = function(message) {
-    consoleService.logStringMessage(message);
-  };
+  webdriver.debug.initialized_ = true;
 };
 
 
 Logger.dumpn = function(text) {
-  Logger._init();
-  var stack = Components.stack.caller;
-  var filename = stack.filename.replace(/.*\//, '');
-  Logger.log_(filename + ":" + stack.lineNumber  + " - " + text + "\n");
+  webdriver.debug.initialize();
+
+  var logger = goog.debug.Logger.getLogger('webdriver');
+  logger.info(text);
 };
 
 
 Logger.dump = function(element) {
-  Logger._init();
+  // no-op
+};
+
+
+Logger.dump = function(element) {
+  webdriver.debug.initialize();
   var dump = "=============\n";
 
   var rows = [];
@@ -135,22 +192,23 @@ Logger.dump = function(element) {
 
   try {
     Logger.dumpProperties_(element, rows);
-  } catch (e) {
-    Logger.log_("caught an exception: " + e);
+  } catch (ignored) {
   }
 
   rows.sort();
-  for (var i in rows) {
-    dump += rows[i] + "\n";
+  for (var j in rows) {
+    dump += rows[j] + "\n";
   }
 
   dump += "=============\n\n\n";
-  Logger.log_(dump);
+
+  var logger = goog.debug.Logger.getLogger('webdriver');
+  logger.warning(dump);
 };
 
 
 Logger.dumpProperties_ = function(view, rows) {
-  Logger._init();
+  webdriver.debug.initialize();
   for (var i in view) {
     var value = "\t" + i + ": ";
     try {
@@ -169,7 +227,7 @@ Logger.dumpProperties_ = function(view, rows) {
 
 
 Logger.stackTrace = function() {
-  Logger._init();
+  webdriver.debug.initialize();
   var stack = Components.stack;
   var i = 5;
   var dump = "";
