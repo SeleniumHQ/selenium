3abec597541c90fd4ceb8ccc9b83a7fdbcfb8d7f
Simon M Stewart <simon.m.stewart@gmail.com>
2011-08-08 10:24:48 +0000

    SimonStewart: Version number bump to 2.4
    r13274

--- Rakefile
+++ Rakefile
@@ -35,7 +35,7 @@ require 'rake-tasks/ie_code_generator'
 require 'rake-tasks/gecko_sdks'
 
 def version
-  "2.3.0"
+  "2.4.0"
 end
 ide_version = "1.0.12"
 
--- javascript/selenium-core/scripts/selenium-version.js
+++ javascript/selenium-core/scripts/selenium-version.js
@@ -1,4 +1,4 @@
-Selenium.version = "2.3";
+Selenium.version = "2.4";
 Selenium.revision = ".0";
 
 window.top.document.title += " v" + Selenium.version + Selenium.revision;
