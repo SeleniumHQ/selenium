f8a58b27b51a9893b86250c8ed96b665b145e0e9
Jim Evans <james.h.evans.jr@gmail.com>
2011-08-11 16:17:01 +0000

    JimEvans: Bumping .NET assembly versions in preparation for release.
    r13321

--- dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
