cd1699f14ce1f92f77d1ff674c59aa5652058ddc
Jim Evans <james.h.evans.jr@gmail.com>
2011-08-15 01:04:17 +0000

    JimEvans: Updating .NET assembly versions for development
    r13366

--- dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.2.0")]
+[assembly: AssemblyFileVersion("2.4.2.0")]
