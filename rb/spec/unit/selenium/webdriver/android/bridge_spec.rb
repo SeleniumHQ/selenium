e2db8c89c7d17f9f3f8925fbfddf4fe7e50b238e
Jari Bakken <jari.bakken@gmail.com>
2011-08-17 20:10:58 +0000

    JariBakken: Fix Android::Bridge when no :http_client is passed.
    r13421

--- rb/spec/unit/selenium/webdriver/android/bridge_spec.rb
+++ rb/spec/unit/selenium/webdriver/android/bridge_spec.rb
@@ -18,6 +18,11 @@ module Selenium
           http.should_receive(:server_url=).with URI.parse("http://example.com")
           Bridge.new(:http_client => http, :url => "http://example.com")
         end
+
+        it "uses the default HTTP client when none is specified" do
+          Remote::Http::Default.should_receive(:new).and_return http
+          Bridge.new
+        end
       end
 
     end # IPhone
