9f4c1f322c144730fbaa9b91df37219b665d92f8
Jari Bakken <jari.bakken@gmail.com>
2011-09-01 02:11:10 +0000

    JariBakken: Fix spec for last change.
    r13679

--- rb/spec/unit/selenium/webdriver/remote/http/common_spec.rb
+++ rb/spec/unit/selenium/webdriver/remote/http/common_spec.rb
@@ -6,13 +6,13 @@ module Selenium
       module Http
         describe Common do
 
-          it "sends Content-Length=0 header for POST requests without a command in the body" do
+          it "sends non-empty body header for POST requests without command data" do
             common = Common.new
             common.server_url = URI.parse("http://server")
 
             common.should_receive(:request).
                   with(:post, URI.parse("http://server/clear"),
-                        hash_including("Content-Length" => "0"), nil)
+                        hash_including("Content-Length" => "2"), "{}")
 
             common.call(:post, "clear", nil)
           end
