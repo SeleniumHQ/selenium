f4feb2b0913d216443904ac34a928924d904a271
Jari Bakken <jari.bakken@gmail.com>
2011-09-13 20:12:05 +0000

    JariBakken: We do want the error reported.
    r13848

--- rb/lib/selenium/webdriver/common/error.rb
+++ rb/lib/selenium/webdriver/common/error.rb
@@ -74,7 +74,8 @@ module Selenium
 
       class << self
         def for_code(code)
-          return if code.to_i == 0
+          return if code == 0
+          return WebDriverError if code.nil?
 
           Errors[code - 1] || WebDriverError
         end
