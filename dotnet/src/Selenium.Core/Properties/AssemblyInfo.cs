3f405509cb5233ea9ea4988dae3dbb143c819af4
Jim Evans <james.h.evans.jr@gmail.com>
2011-09-21 14:19:07 +0000

    JimEvans: Bumping .NET version numbers in preparation for impending release.
    r13885

--- dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
--- dotnet/src/WebDriver/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]
