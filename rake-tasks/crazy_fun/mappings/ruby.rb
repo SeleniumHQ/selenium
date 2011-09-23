1d1a47638e170a4348910a9c07dd4577c9975885
Andreas Tolf Tolfsen <andreastt@opera.com>
2011-09-23 10:55:31 +0000

    AndreasTolfTolfsen: Command-line option  for RSpec tests is now forwarded from Crazy Fun, enabling filtered running
    r13917

--- rake-tasks/crazy_fun/mappings/ruby.rb
+++ rake-tasks/crazy_fun/mappings/ruby.rb
@@ -105,7 +105,7 @@ class RubyMappings
         ruby :include => args[:include],
              :require => args[:require],
              :command => args[:command],
-             :args    => %w[--format CI::Reporter::RSpec --format s --color],
+             :args    => %w[--format CI::Reporter::RSpec --format s --color] + (!!ENV['example'] ? ['--example', ENV['example']] : []),
              :debug   => !!ENV['log'],
              :files   => args[:srcs]
       end
