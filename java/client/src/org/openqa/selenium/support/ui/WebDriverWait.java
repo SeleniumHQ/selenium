42bb713177062b1565c2bb68bc7f3937275dab78
Paul Hammant <PaulHammant@gmail.com>
2011-09-28 18:48:38 +0000

    add javadoc
    r13983

--- java/client/src/org/openqa/selenium/support/ui/WebDriverWait.java
+++ java/client/src/org/openqa/selenium/support/ui/WebDriverWait.java
@@ -29,17 +29,27 @@ public class WebDriverWait extends FluentWait<WebDriver> {
   public final static long DEFAULT_SLEEP_TIMEOUT = 500;
 
   /**
+   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
+   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
+   * list by calling ignoring(exceptions to add).
+   *
    * @param driver The WebDriver instance to pass to the expected conditions
    * @param timeOutInSeconds The timeout in seconds when an expectation is called
+   * @see WebDriverWait#ignoring(Class[]) equals
    */
   public WebDriverWait(WebDriver driver, long timeOutInSeconds) {
     this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, DEFAULT_SLEEP_TIMEOUT);
   }
 
   /**
+   * Wait will ignore instances of NotFoundException that are encountered (thrown) by default in
+   * the 'until' condition, and immediately propagate all others.  You can add more to the ignore
+   * list by calling ignoring(exceptions to add).
+   *
    * @param driver The WebDriver instance to pass to the expected conditions
    * @param timeOutInSeconds The timeout in seconds when an expectation is called
    * @param sleepInMillis The duration in milliseconds to sleep between polls.
+   * @see WebDriverWait#ignoring(Class[]) equals
    */
   public WebDriverWait(WebDriver driver, long timeOutInSeconds, long sleepInMillis) {
     this(driver, new SystemClock(), Sleeper.SYSTEM_SLEEPER, timeOutInSeconds, sleepInMillis);
