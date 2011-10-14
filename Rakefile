60f379e9e84c067d5cc3fcf63fb786caeb724dde
Simon M Stewart <simon.m.stewart@gmail.com>
2011-10-14 10:02:06 +0000

    SimonStewart: Bumping version numbers for the 2.9 release
    r14162

--- Rakefile
+++ Rakefile
@@ -35,7 +35,7 @@ require 'rake-tasks/ie_code_generator'
 require 'rake-tasks/gecko_sdks'
 
 def version
-  "2.8.0"
+  "2.9.0"
 end
 ide_version = "1.0.12"
 
--- javascript/selenium-core/scripts/selenium-version.js
+++ javascript/selenium-core/scripts/selenium-version.js
@@ -1,4 +1,4 @@
-Selenium.version = "2.8";
+Selenium.version = "2.9";
 Selenium.revision = ".0";
 
 window.top.document.title += " v" + Selenium.version + Selenium.revision;
