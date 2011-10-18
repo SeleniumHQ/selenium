53f3ab3f83f3ff5aa824ae9debe5a55cba8f24d4
Simon M Stewart <simon.m.stewart@gmail.com>
2011-10-18 22:32:37 +0000

    SimonStewart: Modifying the internal selenese test base to copy required files around so that we can test with webdriver-backed seleniums from the comfort of IntelliJ
    r14228

--- java/client/test/com/thoughtworks/selenium/InternalSelenseTestBase.java
+++ java/client/test/com/thoughtworks/selenium/InternalSelenseTestBase.java
@@ -1,12 +1,17 @@
 package com.thoughtworks.selenium;
 
 import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import com.google.common.io.Resources;
 
+import org.openqa.selenium.Build;
+import org.openqa.selenium.DevMode;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.environment.GlobalTestEnvironment;
+import org.openqa.selenium.internal.InProject;
 import org.openqa.selenium.internal.WrapsDriver;
 import org.openqa.selenium.v1.SeleniumTestEnvironment;
 
@@ -15,10 +20,55 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.testng.Assert;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class InternalSelenseTestBase extends SeleneseTestBase {
+  private static final Logger log = Logger.getLogger(InternalSelenseTestBase.class.getName());
+
+  @BeforeClass
+  public static void buildJavascriptLibraries() throws IOException {
+    if (!DevMode.isInDevMode()) {
+      return;
+    }
+
+    log.info("In dev mode. Copying required files in case we're using a WebDriver-backed Selenium");
+
+    try {
+      new Build().of(
+          "//java/client/src/org/openqa/selenium/internal/seleniumemulation",
+          "//third_party/js/sizzle"
+      ).go();
+
+      File buildDir = InProject.locate("out/production/selenium/org/openqa/selenium/internal/seleniumemulation");
+      File atomsDir = InProject.locate("build/javascript/selenium-atoms");
+
+      for (File file : atomsDir.listFiles()) {
+        if (file.getName().endsWith(".js")) {
+          File dest = new File(buildDir, file.getName());
+          Files.copy(file, dest);
+        }
+      }
+
+      File sizzle = InProject.locate("third_party/js/sizzle/sizzle.js");
+      Files.copy(sizzle, new File(buildDir, "sizzle.js"));
+
+      File seDir = InProject.locate("java/client/test/com/thoughtworks/selenium");
+      File destDir = InProject.locate("out/production/selenium/com/thoughtworks/selenium");
+      for (File file : seDir.listFiles()) {
+        if (file.getName().endsWith(".js")) {
+          File dest = new File(destDir, file.getName());
+          Files.copy(file, dest);
+        }
+      }
+
+    } catch (WebDriverException e) {
+      System.err.println("Cannot build javascript libraries for selenium emulation: " + e.getMessage());
+    }
+  }
+
   @BeforeClass
   public static void initializeServer() {
     GlobalTestEnvironment.get(SeleniumTestEnvironment.class);
