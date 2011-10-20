44fc4889ab9c232d5126880ebc377031e3f6cd7a
Dounia Berrada <berrada@google.com>
2011-10-20 18:43:17 +0000

    DouniaBerrada: Adding better error messages and changing the flicking distance for testCanFlickHorizontally
    r14275

--- java/client/test/org/openqa/selenium/interactions/touch/TouchFlickTest.java
+++ java/client/test/org/openqa/selenium/interactions/touch/TouchFlickTest.java
@@ -97,13 +97,13 @@ public class TouchFlickTest extends AbstractDriverTestCase {
     int x = link.getLocation().x;
     // The element is located at the right of the page,
     // so it is not initially visible on the screen.
-    assertTrue(x > 2000);
+    assertTrue("Expected x > 2000, but got x = " + x, x > 1500);
 
-    Action flick = getBuilder(driver).flick(750, 0).build();
+    Action flick = getBuilder(driver).flick(1000, 0).build();
     flick.perform();
     x = link.getLocation().x;
     // After flicking, the element should now be visible on the screen.
-    assertTrue("Got: " + x, x < 1500);
+    assertTrue("Expected x < 1500, but got x = " + x, x < 1500);
   }
 
   @Ignore(value = {CHROME, FIREFOX, HTMLUNIT, IE, IPHONE, OPERA, SELENESE, ANDROID},
