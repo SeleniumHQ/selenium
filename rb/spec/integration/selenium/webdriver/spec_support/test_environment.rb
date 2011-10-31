02a41604c1ec99e192dea3ff06009ee48f6ba69c
Jari Bakken <jari.bakken@gmail.com>
2011-10-31 12:53:07 +0000

    JariBakken: Hack to make Ruby specs run on Android.
    r14463

--- rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
@@ -85,7 +85,10 @@ module Selenium
         end
 
         def url_for(filename)
-          app_server.where_is filename
+          url = app_server.where_is filename
+          url.sub!("127.0.0.1", "10.0.2.2") if browser == :android
+
+          url
         end
 
         private
