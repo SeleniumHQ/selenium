2bc5d41fde3b9c529f19192b1e1e1ac115b14ba2
Jari Bakken <jari.bakken@gmail.com>
2011-11-03 11:25:29 +0000

    JariBakken: Clean up the command list.
    r14507

--- rb/lib/selenium/webdriver/remote/commands.rb
+++ rb/lib/selenium/webdriver/remote/commands.rb
@@ -7,34 +7,82 @@ class Selenium::WebDriver::Remote::Bridge
 
   command :newSession,                             :post,    "session"
   command :getCapabilities,                        :get,     "session/:session_id"
-  command :quit,                                   :delete,  "session/:session_id"
-  command :setImplicitWaitTimeout,                 :post,    "session/:session_id/timeouts/implicit_wait"
-  command :setScriptTimeout,                       :post,    "session/:session_id/timeouts/async_script"
-  command :getCurrentWindowHandle,                 :get,     "session/:session_id/window_handle"
-  command :getWindowHandles,                       :get,     "session/:session_id/window_handles"
+
+  #
+  # basic driver
+  #
+
   command :getCurrentUrl,                          :get,     "session/:session_id/url"
   command :get,                                    :post,    "session/:session_id/url"
-  command :dismissAlert,                           :post,    "session/:session_id/dismiss_alert"
-  command :acceptAlert,                            :post,    "session/:session_id/accept_alert"
-  command :getAlertText,                           :get,     "session/:session_id/alert_text"
-  command :setAlertValue,                          :post,    "session/:session_id/alert_text"
   command :goForward,                              :post,    "session/:session_id/forward"
   command :goBack,                                 :post,    "session/:session_id/back"
   command :refresh,                                :post,    "session/:session_id/refresh"
-  command :executeScript,                          :post,    "session/:session_id/execute"
-  command :executeAsyncScript,                     :post,    "session/:session_id/execute_async"
-  command :screenshot,                             :get,     "session/:session_id/screenshot"
-  command :switchToFrame,                          :post,    "session/:session_id/frame"
-  command :switchToWindow,                         :post,    "session/:session_id/window"
-  command :getAllCookies,                          :get,     "session/:session_id/cookie"
-  command :addCookie,                              :post,    "session/:session_id/cookie"
-  command :deleteAllCookies,                       :delete,  "session/:session_id/cookie"
-  command :deleteCookieNamed,                      :delete,  "session/:session_id/cookie/:name"
+  command :quit,                                   :delete,  "session/:session_id"
+  command :close,                                  :delete,  "session/:session_id/window"
+  command :getVisible,                             :get,     "session/:session_id/visible"
+  command :setVisible,                             :post,    "session/:session_id/visible"
   command :getPageSource,                          :get,     "session/:session_id/source"
   command :getTitle,                               :get,     "session/:session_id/title"
   command :findElement,                            :post,    "session/:session_id/element"
   command :findElements,                           :post,    "session/:session_id/elements"
   command :getActiveElement,                       :post,    "session/:session_id/element/active"
+
+  #
+  # window handling
+  #
+
+  command :getCurrentWindowHandle,                 :get,     "session/:session_id/window_handle"
+  command :getWindowHandles,                       :get,     "session/:session_id/window_handles"
+
+  #
+  # script execution
+  #
+
+  command :executeScript,                          :post,    "session/:session_id/execute"
+  command :executeAsyncScript,                     :post,    "session/:session_id/execute_async"
+
+  #
+  # screenshot
+  #
+
+  command :screenshot,                             :get,     "session/:session_id/screenshot"
+
+  #
+  # alerts
+  #
+
+  command :dismissAlert,                           :post,    "session/:session_id/dismiss_alert"
+  command :acceptAlert,                            :post,    "session/:session_id/accept_alert"
+  command :getAlertText,                           :get,     "session/:session_id/alert_text"
+  command :setAlertValue,                          :post,    "session/:session_id/alert_text"
+
+  #
+  # target locator
+  #
+
+  command :switchToFrame,                          :post,    "session/:session_id/frame"
+  command :switchToWindow,                         :post,    "session/:session_id/window"
+
+  #
+  # options
+  #
+
+  command :getAllCookies,                          :get,     "session/:session_id/cookie"
+  command :addCookie,                              :post,    "session/:session_id/cookie"
+  command :deleteAllCookies,                       :delete,  "session/:session_id/cookie"
+  command :deleteCookieNamed,                      :delete,  "session/:session_id/cookie/:name"
+
+  #
+  # timeouts
+  #
+
+  command :setImplicitWaitTimeout,                 :post,    "session/:session_id/timeouts/implicit_wait"
+  command :setScriptTimeout,                       :post,    "session/:session_id/timeouts/async_script"
+
+  #
+  # element
+  #
+
   command :describeElement,                        :get,     "session/:session_id/element/:id"
   command :findChildElement,                       :post,    "session/:session_id/element/:id/element"
   command :findChildElements,                      :post,    "session/:session_id/element/:id/elements"
@@ -42,6 +90,7 @@ class Selenium::WebDriver::Remote::Bridge
   command :submitElement,                          :post,    "session/:session_id/element/:id/submit"
   command :getElementValue,                        :get,     "session/:session_id/element/:id/value"
   command :sendKeysToElement,                      :post,    "session/:session_id/element/:id/value"
+  command :uploadFile,                             :post,    "session/:session_id/file"
   command :getElementTagName,                      :get,     "session/:session_id/element/:id/name"
   command :clearElement,                           :post,    "session/:session_id/element/:id/clear"
   command :isElementSelected,                      :get,     "session/:session_id/element/:id/selected"
@@ -55,15 +104,19 @@ class Selenium::WebDriver::Remote::Bridge
   command :getElementSize,                         :get,     "session/:session_id/element/:id/size"
   command :dragElement,                            :post,    "session/:session_id/element/:id/drag"
   command :getElementValueOfCssProperty,           :get,     "session/:session_id/element/:id/css/:property_name"
-  command :close,                                  :delete,  "session/:session_id/window"
   command :getElementText,                         :get,     "session/:session_id/element/:id/text"
-  command :getVisible,                             :get,     "session/:session_id/visible"
-  command :setVisible,                             :post,    "session/:session_id/visible"
+
+  #
+  # rotatable
+  #
+
   command :getScreenOrientation,                   :get,     "session/:session_id/orientation"
   command :setScreenOrientation,                   :post,    "session/:session_id/orientation"
-  command :uploadFile,                             :post,    "session/:session_id/file"
 
+  #
   # interactions API
+  #
+
   command :click,                                  :post,    "session/:session_id/click"
   command :doubleClick,                            :post,    "session/:session_id/doubleclick"
   command :mouseDown,                              :post,    "session/:session_id/buttondown"
@@ -72,7 +125,10 @@ class Selenium::WebDriver::Remote::Bridge
   command :sendModifierKeyToActiveElement,         :post,    "session/:session_id/modifier"
   command :sendKeysToActiveElement,                :post,    "session/:session_id/keys"
 
+  #
   # html 5
+  #
+
   command :executeSql,                             :post,   "/session/:session_id/execute_sql"
   command :getLocation,                            :get,    "/session/:session_id/location"
   command :setLocation,                            :post,   "/session/:session_id/location"
@@ -96,20 +152,26 @@ class Selenium::WebDriver::Remote::Bridge
   command :clearSessionStorage,                    :delete, "/session/:session_id/session_storage"
   command :getSessionStorageSize,                  :get,    "/session/:session_id/session_storage/size"
 
+  #
   # ime
+  #
+
   command :imeGetAvailableEngines,                 :get,    "/session/:session_id/ime/available_engines"
   command :imeGetActiveEngine,                     :get,    "/session/:session_id/ime/active_engine"
   command :imeIsActivated,                         :get,    "/session/:session_id/ime/activated"
   command :imeDeactivate,                          :post,   "/session/:session_id/ime/deactivate"
   command :imeActivateEngine,                      :post,   "/session/:session_id/ime/activate"
 
+  #
   # touch
-  command :touchSingleTap,                         :post,   "/session/:sessionId/touch/click"
-  command :touchDown,                              :post,   "/session/:sessionId/touch/down"
-  command :touchUp,                                :post,   "/session/:sessionId/touch/up"
-  command :touchMove,                              :post,   "/session/:sessionId/touch/move"
-  command :touchScroll,                            :post,   "/session/:sessionId/touch/scroll"
-  command :touchDoubleTap,                         :post,   "/session/:sessionId/touch/doubleclick"
-  command :touchLongPress,                         :post,   "/session/:sessionId/touch/longclick"
-  command :touchFlick,                             :post,   "/session/:sessionId/touch/flick"
+  #
+
+  command :touchSingleTap,                         :post,   "/session/:session_id/touch/click"
+  command :touchDown,                              :post,   "/session/:session_id/touch/down"
+  command :touchUp,                                :post,   "/session/:session_id/touch/up"
+  command :touchMove,                              :post,   "/session/:session_id/touch/move"
+  command :touchScroll,                            :post,   "/session/:session_id/touch/scroll"
+  command :touchDoubleTap,                         :post,   "/session/:session_id/touch/doubleclick"
+  command :touchLongPress,                         :post,   "/session/:session_id/touch/longclick"
+  command :touchFlick,                             :post,   "/session/:session_id/touch/flick"
 end
