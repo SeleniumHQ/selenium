b3c0b055987ba9f27238dd897b12746ee1c867e5
Simon M Stewart <simon.m.stewart@gmail.com>
2011-11-04 12:31:57 +0000

    SimonStewart: Attempting to make it easier to test changes in the iphone driver
    r14554

---
+++ java/client/test/org/openqa/selenium/iphone/AlreadyRunningIPhoneDriverTestSuite.java
@@ -0,0 +1,34 @@
+package org.openqa.selenium.iphone;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.openqa.selenium.TestSuiteBuilder;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.openqa.selenium.Ignore.Driver.IPHONE;
+import static org.openqa.selenium.Ignore.Driver.REMOTE;
+
+public class AlreadyRunningIPhoneDriverTestSuite extends TestCase {
+  public static Test suite() throws Exception {
+    return new TestSuiteBuilder()
+        .addSourceDir("java/client/test")
+        .usingDriver(AlreadyRunningIPhoneDriver.class)
+        .exclude(IPHONE)
+        .exclude(REMOTE)
+        .keepDriverInstance()
+        .includeJavascriptTests()
+        .create();
+  }
+
+  public static class AlreadyRunningIPhoneDriver extends RemoteWebDriver {
+    public AlreadyRunningIPhoneDriver() throws MalformedURLException {
+      super(new URL("http://localhost:3001/hub"), DesiredCapabilities.iphone());
+    }
+  }
+
+}
