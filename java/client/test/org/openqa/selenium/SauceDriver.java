714c6e0c20c4ff37299efd25e1f873b85c998e94
Daniel Wagner-Hall <dawagner@gmail.com>
2011-11-18 13:35:43 +0000

    DanielWagnerHall: Making the SauceDriver more configurable
    r14795

--- java/client/test/org/openqa/selenium/SauceDriver.java
+++ java/client/test/org/openqa/selenium/SauceDriver.java
@@ -10,25 +10,44 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 
 public class SauceDriver extends RemoteWebDriver {
-  private static final String SELENIUM_VERSION_ENV_NAME = "SELENIUM_VERSION";
+  private static final String SELENIUM_VERSION_ENV_NAME = "SAUCE_SELENIUM_VERSION";
   private static final String SAUCE_APIKEY_ENV_NAME = "SAUCE_APIKEY";
   private static final String SAUCE_USERNAME_ENV_NAME = "SAUCE_USERNAME";
+  private static final String DESIRED_BROWSER_VERSION_ENV_NAME = "SAUCE_BROWSER_VERSION";
+  
+  // Should be one of the values listed for Platform, e.g. xp, win7, android, ...
+  private static final String DESIRED_OS_ENV_NAME = "SAUCE_OS";
 
   public SauceDriver(DesiredCapabilities desiredCapabilities) {
-    super(getSauceEndpoint(), munge(desiredCapabilities, getSeleniumVersion()));
+    super(getSauceEndpoint(),
+      munge(
+        desiredCapabilities,
+        getSeleniumVersion(),
+        getDesiredBrowserVersion(),
+        getDesiredOS()));
+  }
+
+  private static String getDesiredBrowserVersion() {
+    return getNonNullEnv(DESIRED_BROWSER_VERSION_ENV_NAME);
+  }
+
+  private static String getDesiredOS() {
+    return getNonNullEnv(DESIRED_OS_ENV_NAME);
   }
 
   private static String getSeleniumVersion() {
-    String seleniumVersion = System.getenv(SELENIUM_VERSION_ENV_NAME);
-    Preconditions.checkNotNull(seleniumVersion);
-    return seleniumVersion;
+    return getNonNullEnv(SELENIUM_VERSION_ENV_NAME);
+  }
+  
+  private static String getNonNullEnv(String propertyName) {
+    String value = System.getenv(propertyName);
+    Preconditions.checkNotNull(value);
+    return value;
   }
 
   private static URL getSauceEndpoint() {
-    String sauceUsername = System.getenv(SAUCE_USERNAME_ENV_NAME);
-    String sauceKey = System.getenv(SAUCE_APIKEY_ENV_NAME);
-    Preconditions.checkNotNull(sauceUsername);
-    Preconditions.checkNotNull(sauceKey);
+    String sauceUsername = getNonNullEnv(SAUCE_USERNAME_ENV_NAME);
+    String sauceKey = getNonNullEnv(SAUCE_APIKEY_ENV_NAME);
 
     try {
       return new URL(String.format("http://%s:%s@ondemand.saucelabs.com:80/wd/hub", sauceUsername, sauceKey));
@@ -38,9 +57,11 @@ public class SauceDriver extends RemoteWebDriver {
     throw new IllegalStateException("Should have returned or thrown");
   }
 
-  private static Capabilities munge(DesiredCapabilities desiredCapabilities, String seleniumVersion) {
+  private static Capabilities munge(DesiredCapabilities desiredCapabilities, String seleniumVersion, String browserVersion, String os) {
     DesiredCapabilities mungedCapabilities = new DesiredCapabilities(desiredCapabilities);
     mungedCapabilities.setCapability("selenium-version", seleniumVersion);
+    mungedCapabilities.setVersion(browserVersion);
+    mungedCapabilities.setPlatform(Platform.extractFromSysProperty(os));
     return mungedCapabilities;
   }
 }
