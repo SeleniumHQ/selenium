81b157af1d7fd0ffb29437f515b08412c57c1a83
Jari Bakken <jari.bakken@gmail.com>
2011-11-28 00:00:04 +0000

    JariBakken: Add Select support class for Ruby.
    Thanks to Samit Badle for the initial work on this.
    
    r14910

--- rb/lib/selenium/webdriver/support.rb
+++ rb/lib/selenium/webdriver/support.rb
@@ -1,3 +1,4 @@
 require 'selenium/webdriver/support/event_firing_bridge'
 require 'selenium/webdriver/support/abstract_event_listener'
 require 'selenium/webdriver/support/block_event_listener'
+require 'selenium/webdriver/support/select'
