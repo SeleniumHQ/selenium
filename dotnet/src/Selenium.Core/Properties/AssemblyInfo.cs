9dbc1a4ddda8b8fde9645ee13f76dea012ad8c82
Jim Evans <james.h.evans.jr@gmail.com>
2011-11-28 15:33:07 +0000

    JimEvans: Bumping .NET assembly version numbers to 2.14 in preparation for release.
    r14934

--- dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.13.0.0")]
-[assembly: AssemblyFileVersion("2.13.0.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
--- dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.13.0.0")]
-[assembly: AssemblyFileVersion("2.13.0.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
