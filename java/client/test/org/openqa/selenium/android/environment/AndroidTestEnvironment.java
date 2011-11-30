c2b526d5cb32c70ee44cbd9e6c8aeb911a5e08d4
Eran Mes <eran.mes@gmail.com>
2011-11-30 16:51:31 +0000

    EranMes: Allow running the Android tests on a real device by specifying the external IP of the in-process test server.
    r14991

--- java/client/test/org/openqa/selenium/android/environment/AndroidTestEnvironment.java
+++ java/client/test/org/openqa/selenium/android/environment/AndroidTestEnvironment.java
@@ -24,9 +24,17 @@ import org.openqa.selenium.environment.webserver.Jetty7AppServer;
 
 public class AndroidTestEnvironment implements TestEnvironment {
   private AppServer appServer;
+  // In order to run the tests on a real device, the device and the host
+  // running the tests must be on the same network. This variable should
+  // contain the externally-visible IP of the host.
+  private static String ANDROID_HOST_IP = "ANDROID_HTTP_HOST";
 
   public AndroidTestEnvironment() {
-    appServer = new Jetty7AppServer("10.0.2.2");
+    String servingHost = System.getenv(ANDROID_HOST_IP);
+    if (servingHost == null) {
+      servingHost = "10.0.2.2";
+    }
+    appServer = new Jetty7AppServer(servingHost);
     appServer.start();
   }
 
