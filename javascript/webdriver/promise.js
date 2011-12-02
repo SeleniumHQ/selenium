6160df60f84499916416bf7d01ea467732d47535
Jason Lebya <jmleyba@gmail.com>
2011-12-02 23:04:50 +0000

    JasonLeyba: Update copyright headers.
    r15029

--- javascript/webdriver/promise.js
+++ javascript/webdriver/promise.js
@@ -1,7 +1,4 @@
-// Copyright (c) 2005-2011, The Dojo Foundation
-// All rights reserved.
-//
-// Modifications Copyright 2011 WebDriver committers.
+// Copyright 2011 Software Freedom Conservatory. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
