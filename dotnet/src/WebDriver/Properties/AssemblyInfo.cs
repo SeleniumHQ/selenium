f7712b3b921a2eb917acef036977b7ac5adfcf27
Jim Evans <james.h.evans.jr@gmail.com>
2011-12-07 15:32:22 +0000

    JimEvans: Bumping .NET assembly version numbers to 2.15
    r15096

--- dotnet/src/WebDriver/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.14.0.0")]
-[assembly: AssemblyFileVersion("2.14.0.0")]
+[assembly: AssemblyVersion("2.15.0.0")]
+[assembly: AssemblyFileVersion("2.15.0.0")]
