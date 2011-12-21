3683bb5925539dea7138c67620d941b372d9c6e1
Jim Evans <james.h.evans.jr@gmail.com>
2011-12-21 15:44:58 +0000

    JimEvans: Removing unnecessary Console.WriteLine() statements from .NET tests.
    r15249

--- dotnet/test/WebDriver.Common.Tests/WindowTest.cs
+++ dotnet/test/WebDriver.Common.Tests/WindowTest.cs
@@ -18,7 +18,6 @@ namespace OpenQA.Selenium
         public void ShouldBeAbleToGetTheSizeOfTheCurrentWindow()
         {
             Size size = driver.Manage().Window.Size;
-            Console.WriteLine("Window size: {0}, {1}", size.Width, size.Height);
             Assert.Greater(size.Width, 0);
             Assert.Greater(size.Height, 0);
         }
@@ -33,14 +32,12 @@ namespace OpenQA.Selenium
         {
             IWindow window = driver.Manage().Window;
             Size size = window.Size;
-            Console.WriteLine("Window size: {0}, {1}", size.Width, size.Height);
 
             // resize relative to the initial size, since we don't know what it is
             Size targetSize = new Size(size.Width - 20, size.Height - 20);
             window.Size = targetSize;
 
             Size newSize = window.Size;
-            Console.WriteLine("Window size: {0}, {1}", newSize.Width, newSize.Height);
             Assert.AreEqual(targetSize.Width, newSize.Width);
             Assert.AreEqual(targetSize.Height, newSize.Height);
         }
@@ -54,7 +51,6 @@ namespace OpenQA.Selenium
         public void ShouldBeAbleToGetThePositionOfTheCurrentWindow()
         {
             Point position = driver.Manage().Window.Position;
-            Console.WriteLine("Window postion: {0}, {1}", position.X, position.Y);
             Assert.Greater(position.X, 0);
             Assert.Greater(position.Y, 0);
         }
@@ -69,13 +65,11 @@ namespace OpenQA.Selenium
         {
             IWindow window = driver.Manage().Window;
             Point position = window.Position;
-            Console.WriteLine("Window postion: {0}, {1}", position.X, position.Y);
            
             Point targetPosition = new Point(position.X + 10, position.Y + 10);
             window.Position = targetPosition;
 
             Point newLocation = window.Position;
-            Console.WriteLine("Window postion: {0}, {1}", newLocation.X, newLocation.Y);
 
             Assert.AreEqual(targetPosition.X, newLocation.X);
             Assert.AreEqual(targetPosition.Y, newLocation.Y);
