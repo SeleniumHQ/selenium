377b345747bde7ffea2f1afeb605b1fd11daf401
Luke Semerau <luke.semerau@gmail.com>
2011-12-29 06:47:13 +0000

    LukeIS: fixing Copyright for all the recently touched files in iOS
    r15295

--- iphone/src/objc/SessionRoot.m
+++ iphone/src/objc/SessionRoot.m
@@ -3,7 +3,7 @@
 //  iWebDriver
 //
 //  Copyright 2010 Google Inc.
-//  Copyright 2011 Software Freedom Convervancy.
+//  Copyright 2011 Software Freedom Conservancy.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
