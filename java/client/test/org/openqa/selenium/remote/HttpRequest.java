e5a2592a460505ad594a0d9135244237ca98dac3
Simon M Stewart <simon.m.stewart@gmail.com>
2011-12-29 09:01:18 +0000

    SimonStewart: Modify the test httprequest class to support more methods
    r15298

--- java/client/test/org/openqa/selenium/remote/HttpRequest.java
+++ java/client/test/org/openqa/selenium/remote/HttpRequest.java
@@ -17,34 +17,40 @@ limitations under the License.
 
 package org.openqa.selenium.remote;
 
+import com.google.common.base.Throwables;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 import org.openqa.selenium.remote.internal.HttpClientFactory;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 
 public class HttpRequest {
+
   private String response;
 
-  public HttpRequest(Method method, String url, Object payload) throws Exception {
-    if (method == Method.POST) {
-      HttpPost post = new HttpPost(url);
-      post.addHeader("Accept", "application/json");
-      post.addHeader("Accept-Charset", "utf-8");
-      post.addHeader("Content-Type", "application/json; charset=utf8");
+  public HttpRequest(Method method, String url, Object payload) {
+    HttpUriRequest request = method.prepare(url, payload);
+    execute(request);
+  }
 
-      String content = new BeanToJsonConverter().convert(payload);
-
-      post.setEntity(new StringEntity(content, "UTF-8"));
-
+  private void execute(HttpUriRequest method) {
+    try {
       HttpClientFactory httpClientFactory = new HttpClientFactory();
       final HttpClient defaultHttpClient = httpClientFactory.getHttpClient();
       HttpEntity httpEntity = null;
       try {
-        HttpResponse res = defaultHttpClient.execute(post);
+        HttpResponse res = defaultHttpClient.execute(method);
         httpEntity = res.getEntity();
         if (httpEntity != null) {
           this.response = EntityUtils.toString(httpEntity);
@@ -53,11 +59,11 @@ public class HttpRequest {
         if (httpEntity != null) {
           EntityUtils.consume(httpEntity);
         }
-        post.abort();
+        method.abort();
         httpClientFactory.close();
       }
-    } else {
-      throw new UnsupportedOperationException("Unsupported method: " + method);
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
     }
   }
 
@@ -65,7 +71,50 @@ public class HttpRequest {
     return response;
   }
 
+  // I don't know why I like enums with methods, but I do.
   public static enum Method {
-    POST
+    DELETE {
+      @Override
+      public HttpUriRequest prepare(String url, Object payload) {
+        return new HttpDelete(url);
+      }
+    },
+    GET {
+      @Override
+      public HttpUriRequest prepare(String url, Object payload) {
+        HttpGet get = new HttpGet(url);
+        get.addHeader("Accept", "application/json, */*");
+        get.addHeader("Accept-Charset", "utf-8");
+        get.addHeader("Content-Type", "application/json; charset=utf8");
+        return get;
+      }
+    },
+    POST {
+      @Override
+      public HttpUriRequest prepare(String url, Object payload) {
+        HttpPost post = new HttpPost(url);
+        post.addHeader("Accept", "application/json, */*");
+        post.addHeader("Accept-Charset", "utf-8");
+        post.addHeader("Content-Type", "application/json; charset=utf8");
+
+        if (payload != null) {
+          String content = new BeanToJsonConverter().convert(payload);
+          try {
+            post.setEntity(new StringEntity(content, "UTF-8"));
+          } catch (UnsupportedEncodingException e) {
+            throw Throwables.propagate(e);
+          }
+        }
+        return post;
+      }
+    },
+    PUT {
+      @Override
+      public HttpUriRequest prepare(String url, Object payload) {
+        return new HttpPut(url);
+      }
+    };
+
+    public abstract HttpUriRequest prepare(String url, Object payload);
   }
 }
