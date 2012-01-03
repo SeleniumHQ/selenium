1ef030a542b78557e5a48f788f8ce86b5c80257b
Ajay Kemparaj <ajaykemparaj@gmail.com>
2012-01-03 12:50:39 +0000

    AjayKemparaj:Made Grid Help look cleaner , previously it was ugly , i am guilty for  code duplication of RemoteControlLauncher.printWrappedErrorLine being copied to the class
    r15340

--- java/server/src/org/openqa/grid/common/GridDocHelper.java
+++ java/server/src/org/openqa/grid/common/GridDocHelper.java
@@ -1,27 +1,23 @@
 /*
-Copyright 2011 WebDriver committers
-Copyright 2011 Software Freedom Conservancy
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011 WebDriver committers Copyright 2011 Software Freedom Conservancy
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 package org.openqa.grid.common;
-
-import org.apache.commons.lang.WordUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 
 public class GridDocHelper {
   private static Properties gridProperties = load("defaults/GridParameters.properties");
@@ -53,20 +49,44 @@ public class GridDocHelper {
   }
 
   private static void printHelpInConsole(Properties p, String msg, boolean error) {
+    String indent = "  ";
+    String indent2x = indent + indent;
     if (msg != null) {
       if (error) {
         System.out.println("Error building the config :" + msg);
       } else {
         System.out.println(msg);
       }
-
     }
 
     System.out.println("Usage :");
     for (Object key : p.keySet()) {
-      System.out.println("-" + key + ":\n\t"
-          + WordUtils.wrap(getParam(p, key.toString()), 80, "\n\t", false));
+      System.out.println(indent + "-" + key + ":\t");
+      printWrappedErrorLine(indent2x, getParam(p, key.toString()), true);
+      System.out.println("");
     }
+  }
+
+
+  public static void printWrappedErrorLine(String prefix, String msg, boolean first) {
+    System.err.print(prefix);
+    if (!first) {
+      System.err.print("  ");
+    }
+    int defaultWrap = 70;
+    int wrap = defaultWrap - prefix.length();
+    if (wrap > msg.length()) {
+      System.err.println(msg);
+      return;
+    }
+    String lineRaw = msg.substring(0, wrap);
+    int spaceIndex = lineRaw.lastIndexOf(' ');
+    if (spaceIndex == -1) {
+      spaceIndex = lineRaw.length();
+    }
+    String line = lineRaw.substring(0, spaceIndex);
+    System.err.println(line);
+    printWrappedErrorLine(prefix, msg.substring(spaceIndex + 1), false);
   }
 
   private static Properties load(String resource) {
