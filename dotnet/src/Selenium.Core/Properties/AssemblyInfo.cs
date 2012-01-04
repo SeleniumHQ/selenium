bcfc39dd0fa554008cd0892bcfe6375c3c859eef
Jim Evans <james.h.evans.jr@gmail.com>
2012-01-04 18:34:18 +0000

    JimEvans: Updating version numbers of .NET assemblies for 2.16
    r15381

--- dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.15.0.0")]
-[assembly: AssemblyFileVersion("2.15.0.0")]
+[assembly: AssemblyVersion("2.16.0.0")]
+[assembly: AssemblyFileVersion("2.16.0.0")]
