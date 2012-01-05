f69fa0af30d79711bb1dd381bf33c3063940b82e
Daniel Wagner-Hall <dawagner@gmail.com>
2012-01-05 12:18:14 +0000

    DanielWagnerHall: Version bump and docs update for 2.16.1
    r15404

--- Rakefile
+++ Rakefile
@@ -37,7 +37,7 @@ require 'rake-tasks/ie_code_generator'
 require 'rake-tasks/gecko_sdks'
 
 def version
-  "2.16.0"
+  "2.16.1"
 end
 ide_version = "1.0.12"
 
--- javascript/selenium-core/scripts/selenium-version.js
+++ javascript/selenium-core/scripts/selenium-version.js
@@ -1,5 +1,5 @@
 Selenium.version = "2.16";
-Selenium.revision = ".0";
+Selenium.revision = ".1";
 
 window.top.document.title += " v" + Selenium.version + Selenium.revision;
 
