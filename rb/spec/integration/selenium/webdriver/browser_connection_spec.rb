4d603b3a705d0ad0c9afc97427e0050060f09751
Jari Bakken <jari.bakken@gmail.com>
2012-01-27 23:57:30 +0000

    JariBakken: Enable browser connection spec on Android.
    r15725

--- rb/spec/integration/selenium/webdriver/browser_connection_spec.rb
+++ rb/spec/integration/selenium/webdriver/browser_connection_spec.rb
@@ -3,16 +3,16 @@ require File.expand_path("../spec_helper", __FILE__)
 describe "Driver" do
   context "browser connection" do
 
-    compliant_on :browser => nil do
-      it "can set the browser offline and online"  do
+    compliant_on :browser => :android do
+      it "can set the browser offline and online" do
         driver.navigate.to url_for("html5Page.html")
         driver.should be_online
 
         driver.online = false
-        driver.should_not be_online
+        wait.until { not driver.online? }
 
         driver.online = true
-        driver.should be_online
+        wait.until { driver.online? }
       end
     end
 
