072e9c54d9b73e81e9bcc243ff3a545076a85107
Simon M Stewart <simon.m.stewart@gmail.com>
2012-02-01 16:12:55 +0000

    SimonStewart: Deleting the half-written emulation code from the python bindings. They're no longer needed.
    r15786

--- py/selenium/webdriver/emulation/base.py
+++
@@ -1,9 +0,0 @@
-
-class BaseCommand(object):
-  def __init__(self, driver, baseUrl):
-    self.driver = driver
-    if baseUrl.endswith('/'):
-      self.baseUrl = baseUrl[:-1]
-    else:
-      self.baseUrl = baseUrl
-    
