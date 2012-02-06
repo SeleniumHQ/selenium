a8460be60d27f56efd4b6159ec1c2fe090ba4c20
Simon M Stewart <simon.m.stewart@gmail.com>
2012-02-06 13:23:02 +0000

    SimonStewart: Version number bump for 2.19
    r15838

--- Rakefile
+++ Rakefile
@@ -37,7 +37,7 @@ require 'rake-tasks/ie_code_generator'
 require 'rake-tasks/gecko_sdks'
 
 def version
-  "2.18.0"
+  "2.19.0"
 end
 ide_version = "1.0.12"
 
--- javascript/selenium-core/scripts/selenium-version.js
+++ javascript/selenium-core/scripts/selenium-version.js
@@ -1,4 +1,4 @@
-Selenium.version = "2.18";
+Selenium.version = "2.19";
 Selenium.revision = ".0";
 
 window.top.document.title += " v" + Selenium.version + Selenium.revision;
