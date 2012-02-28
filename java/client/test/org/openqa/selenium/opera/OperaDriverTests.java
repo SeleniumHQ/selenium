3142d970dc9919f3ea3af799cb10d6d2cf471868
Simon M Stewart <simon.m.stewart@gmail.com>
2012-02-28 16:47:19 +0000

    SimonStewart: Move the Opera tests to junit4
    r16030

--- java/client/test/org/openqa/selenium/opera/OperaDriverTests.java
+++ java/client/test/org/openqa/selenium/opera/OperaDriverTests.java
@@ -3,11 +3,15 @@ package org.openqa.selenium.opera;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.junit.internal.runners.SuiteMethod;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.TestSuiteBuilder;
 
 import static org.openqa.selenium.testing.drivers.Browser.opera;
 
-public class OperaDriverTestSuite extends TestSuite {
+
+@RunWith(SuiteMethod.class)
+public class OperaDriverTests extends TestSuite {
 
   public static Test suite() throws Exception {
     return new TestSuiteBuilder()
