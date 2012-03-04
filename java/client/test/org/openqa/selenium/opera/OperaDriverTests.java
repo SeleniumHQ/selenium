ad46da13c4699285209c281676979092b4dff105
Simon M Stewart <simon.m.stewart@gmail.com>
2012-03-04 22:41:58 +0000

    SimonStewart: The opera driver has nothing in the legacy test suite any more
    r16114

--- java/client/test/org/openqa/selenium/opera/OperaDriverTests.java
+++ java/client/test/org/openqa/selenium/opera/OperaDriverTests.java
@@ -1,33 +1,12 @@
 package org.openqa.selenium.opera;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
-import org.junit.internal.runners.SuiteMethod;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.openqa.selenium.StandardSeleniumTests;
-import org.openqa.selenium.TestSuiteBuilder;
-
-import static org.openqa.selenium.testing.drivers.Browser.opera;
-
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    StandardSeleniumTests.class,
-    OperaDriverTests.LegacyTests.class
+    StandardSeleniumTests.class
 })
 public class OperaDriverTests {
-
-  @RunWith(SuiteMethod.class)
-  public static class LegacyTests extends TestSuite {
-  public static Test suite() throws Exception {
-    return new TestSuiteBuilder()
-        .addSourceDir("java/client/test")
-        .using(opera)
-        .keepDriverInstance()
-        .includeJavascriptTests()
-        .create();
-  }
-  }
 }
