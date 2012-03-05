f37d9e90787576f0c9efd3da0fef57c7bcc281ec
Simon M Stewart <simon.m.stewart@gmail.com>
2012-03-05 11:09:23 +0000

    SimonStewart: The executable test relies on the system running the test having firefox installed. Checking this assumption before running the test as this is not true on the CI server.
    r16121

--- java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
+++ java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.os.CommandLine;
 import org.openqa.selenium.testing.NeedsLocalEnvironment;
 import org.openqa.selenium.testing.SeleniumTestRunner;
 
@@ -12,9 +14,16 @@ import java.io.File;
 
 public class ExecutableTest {
 
+  private String executableName = Platform.getCurrent().is(Platform.MAC) ?
+      "/Applications/Firefox.app/Contents/Resources/firefox" : "firefox";
+
   @Test
-  @NeedsLocalEnvironment(reason = "Requires local browser launching environment")
   public void testEnvironmentDiscovery() {
+    if (CommandLine.findExecutable(executableName) == null) {
+      System.out.println("Skipping test: no firefox present");
+      return;
+    }
+
     Executable env = new Executable(null);
     File exe = env.getFile();
     assertNotNull(exe);
