f23227e883d051f5545df20be2ccd3e788460f6d
Daniel Wagner-Hall <dawagner@gmail.com>
2012-03-05 15:06:11 +0000

    DanielWagnerHall: Goodbye trailing space
    r16142

--- javascript/webdriver/test/test_bootstrap.js
+++ javascript/webdriver/test/test_bootstrap.js
@@ -53,7 +53,7 @@
   // All of the files to load. Files are specified in the order they must be
   // loaded, NOT alphabetical order.
   var files = [
-    '../../../third_party/closure/goog/base.js',  
+    '../../../third_party/closure/goog/base.js',
     '../../deps.js'
   ];
 
