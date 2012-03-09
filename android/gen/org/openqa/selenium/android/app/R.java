ec4d05222afbdd759e1f7bb70912810914acd2ff
Simon M Stewart <simon.m.stewart@gmail.com>
2012-03-09 16:05:42 +0000

    SimonStewart: First stab at getting android support in idea working
    r16214

--- android/gen/org/openqa/selenium/android/app/R.java
+++ android/gen/org/openqa/selenium/android/app/R.java
@@ -26,6 +26,7 @@ public final class R {
         public static final int single_session_layout=0x7f030000;
     }
     public static final class string {
+        public static final int app_name=0x7f050009;
         public static final int ctx_menu_navigate=0x7f050001;
         public static final int default_session_status=0x7f050002;
         public static final int jetty_already_started=0x7f050005;
