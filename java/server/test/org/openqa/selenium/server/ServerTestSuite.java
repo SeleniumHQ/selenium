e676f915c2a564d1f2451bdd4bef022e74e0f9cd
Simon M Stewart <simon.m.stewart@gmail.com>
2012-03-09 14:00:30 +0000

    SimonStewart: Move another server-specific test into the server tree
    r16204

--- java/server/test/org/openqa/selenium/server/ServerTestSuite.java
+++ java/server/test/org/openqa/selenium/server/ServerTestSuite.java
@@ -2,14 +2,12 @@
  * Created on May 18, 2006
  *
  */
-package org.openqa.selenium;
+package org.openqa.selenium.server;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.openqa.selenium.os.WindowsUtils;
-import org.openqa.selenium.server.LinuxHTMLRunnerFunctionalTest;
-import org.openqa.selenium.server.WindowsHTMLRunnerFunctionalTest;
 
 
 public class ServerTestSuite extends TestSuite {
