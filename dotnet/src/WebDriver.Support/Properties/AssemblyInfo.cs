212a88f3fe4ea44f3a186dcb3e48cd53e0888148
Jim Evans <james.h.evans.jr@gmail.com>
2012-03-30 15:36:46 +0000

    JimEvans: Updating .NET assembly version information to 2.21 for impending release
    r16418

--- dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
+++ dotnet/src/WebDriver.Support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.20.0.0")]
-[assembly: AssemblyFileVersion("2.20.0.0")]
+[assembly: AssemblyVersion("2.21.0.0")]
+[assembly: AssemblyFileVersion("2.21.0.0")]
