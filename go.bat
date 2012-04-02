c0a7780941007e6d797455fa6162c8daad573238
Kristian Rosenvold <kristian.rosenvold@gmail.com>
2012-04-02 15:45:07 +0000

    KristianRosenvold: Upped MaxPermSize to 384
    It seems like 64 bit builds are running on the edge on 256
    
    r16430

--- go.bat
+++ go.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -Xmx1024m -XX:MaxPermSize=256m -XX:ReservedCodeCacheSize=128m -client -jar third_party\jruby\jruby-complete.jar -X-C -S rake %*
+java -Xmx1024m -XX:MaxPermSize=284m -XX:ReservedCodeCacheSize=128m -client -jar third_party\jruby\jruby-complete.jar -X-C -S rake %*
