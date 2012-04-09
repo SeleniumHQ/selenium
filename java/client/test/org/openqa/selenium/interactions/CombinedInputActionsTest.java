5598dbf230c6ea368c18faaa93fb0f798500b11c
Kristian Rosenvold <kristian.rosenvold@gmail.com>
2012-04-09 14:57:43 +0000

    KristianRosenvold: Ignored test for chrome
    https://groups.google.com/group/selenium-developers/browse_thread/thread/d50c895dde1944b9#
    
    r16495

--- java/client/test/org/openqa/selenium/interactions/CombinedInputActionsTest.java
+++ java/client/test/org/openqa/selenium/interactions/CombinedInputActionsTest.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.TestWaiter.waitFor;
 import static org.openqa.selenium.WaitingConditions.elementToExist;
 import static org.openqa.selenium.testing.Ignore.Driver.ANDROID;
+import static org.openqa.selenium.testing.Ignore.Driver.CHROME;
 import static org.openqa.selenium.testing.Ignore.Driver.HTMLUNIT;
 import static org.openqa.selenium.testing.Ignore.Driver.IE;
 import static org.openqa.selenium.testing.Ignore.Driver.IPHONE;
@@ -47,7 +48,7 @@ import static org.openqa.selenium.testing.TestUtilities.isNativeEventsEnabled;
 /**
  * Tests combined input actions.
  */
-@Ignore(ANDROID)
+@Ignore(value =  {ANDROID,CHROME})
 public class CombinedInputActionsTest extends JUnit4TestBase {
 
   // TODO: Check if this could work in any browser without native events.
