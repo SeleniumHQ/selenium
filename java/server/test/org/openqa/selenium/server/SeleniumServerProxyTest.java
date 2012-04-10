de7a3b169a5974fb9b858aaae0821861c2f207d5
Daniel Wagner-Hall <dawagner@gmail.com>
2012-04-10 12:53:24 +0000

    DanielWagnerHall: Removing commons-io dependency, organising imports
    r16512

--- java/server/test/org/openqa/selenium/server/SeleniumServerProxyTest.java
+++ java/server/test/org/openqa/selenium/server/SeleniumServerProxyTest.java
@@ -1,23 +1,24 @@
 package org.openqa.selenium.server;
 
-import static org.junit.Assert.assertTrue;
 import static org.openqa.selenium.net.PortProber.pollPort;
+
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.base.Throwables;
+import com.google.common.io.ByteStreams;
+
+import org.openqa.selenium.Build;
+import org.openqa.selenium.net.PortProber;
+import org.openqa.selenium.os.CommandLine;
+import org.openqa.selenium.testing.InProject;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.openqa.selenium.Build;
-import org.openqa.selenium.net.PortProber;
-import org.openqa.selenium.os.CommandLine;
-import org.openqa.selenium.server.TrustEverythingSSLTrustManager;
-
-import junit.framework.TestCase;
-import org.apache.commons.io.IOUtils;
-import org.openqa.selenium.testing.InProject;
 
 import java.io.File;
-import java.io.StringWriter;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
@@ -56,19 +57,22 @@ public class SeleniumServerProxyTest {
     URLConnection client =
         target.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", port)));
 
-    StringWriter responseWriter = new StringWriter();
-    IOUtils.copy(client.getInputStream(), responseWriter);
-    String response = responseWriter.toString();
-    assertTrue(response.contains("<body>"));
+    assertTrue(getResponseAsString(client).contains("<body>"));
 
     target = new URL("https://www.google.com/selenium-server/core/Blank.html");
     client =
         target.openConnection(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", port)));
     TrustEverythingSSLTrustManager.trustAllSSLCertificates((HttpsURLConnection) client);
 
-    responseWriter = new StringWriter();
-    IOUtils.copy(client.getInputStream(), responseWriter);
-    response = responseWriter.toString();
-    assertTrue(response.contains("<body>"));
+    assertTrue(getResponseAsString(client).contains("<body>"));
+  }
+
+  private String getResponseAsString(URLConnection client) {
+    try {
+      byte[] bytes = ByteStreams.toByteArray(client.getInputStream());
+      return new String(bytes);
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
   }
 }
