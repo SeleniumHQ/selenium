6eff786e64b787f3a880179ecaed71f7b1cd5cce
Daniel Wagner-Hall <dawagner@gmail.com>
2012-04-17 13:23:16 +0000

    DanielWagnerHall: Fixing ignored test printer to work with junit 4
    r16639

--- java/client/test/org/openqa/selenium/internal/IgnoredTestPrinter.java
+++ java/client/test/org/openqa/selenium/internal/IgnoredTestPrinter.java
@@ -1,32 +1,98 @@
 package org.openqa.selenium.internal;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.io.Files;
 
-import org.openqa.selenium.TestSuiteBuilder;
+import org.openqa.selenium.StandardSeleniumTests;
+import org.openqa.selenium.testing.Ignore;
 import org.openqa.selenium.testing.drivers.Browser;
+import org.openqa.selenium.testing.drivers.TestIgnorance;
+
+import org.junit.internal.runners.model.ReflectiveCallable;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+
+import org.junit.runners.model.InitializationError;
+
+import org.junit.runners.Suite;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 public class IgnoredTestPrinter {
-  public static void main(String[] args) throws Exception {
-    IgnoreCollector collector = new IgnoreCollector();
+  public IgnoreCollector ignoreCollector = new IgnoreCollector();
+  private final Class<StandardSeleniumTests> testClass;
 
-    for (Browser browser : Browser.values()) {
-      if (browser == Browser.none) {
-        continue;
+  public static void main(String[] args) throws Throwable {
+    new IgnoredTestPrinter(StandardSeleniumTests.class).print();
+  }
+  
+  public IgnoredTestPrinter(Class<StandardSeleniumTests> testClass) {
+    this.testClass = testClass;
+  }
+  
+  public void print() throws Throwable {
+    Set<Class<?>> testClasses = getTestClasses(testClass);
+    for (Class<?> testClass : testClasses) {
+      for (Browser browser : Browser.values()) {
+        if (browser == Browser.none) {
+          continue;
+        }
+        System.setProperty("selenium.browser", browser.toString());
+        new SeleniumIgnoreCollectingTestRunner(testClass).run(new RunNotifier());
       }
-      new TestSuiteBuilder()
-          .addSourceDir("java/client/test")
-          .using(browser)
-          .withIgnoredTestCallback(collector)
-          .create()
-      ;
     }
-
-
+    
     final File out = new File("ignores.json");
-    Files.write(collector.toJson().getBytes(), out);
+    Files.write(ignoreCollector.toJson().getBytes(), out);
     System.out.println("Wrote ignores to " + out.getAbsolutePath());
   }
 
+  private Set<Class<?>> getTestClasses(Class<?> suite) {
+    Suite.SuiteClasses suiteClasses = suite.getAnnotation(Suite.SuiteClasses.class);
+    if (suiteClasses == null) {
+      return ImmutableSet.<Class<?>>of(suite);
+    }
+    
+    Set<Class<?>> testClasses = new HashSet<Class<?>>();
+    for (Class<?> suiteClass : suiteClasses.value()) {
+      testClasses.addAll(getTestClasses(suiteClass));
+    }
+    return testClasses;
+  }
+
+  public class SeleniumIgnoreCollectingTestRunner extends BlockJUnit4ClassRunner {
+
+    private TestIgnorance ignorance;
+
+    public SeleniumIgnoreCollectingTestRunner(Class<?> klass) throws InitializationError {
+      super(klass);
+      Browser browser = Browser.detect();
+      ignorance = new TestIgnorance(browser);
+    }
+
+    @Override
+    protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
+      Object test = null;
+      Description description = describeChild(method);
+      try {
+        test = new ReflectiveCallable() {
+          @Override
+          protected Object runReflectiveCall() throws Throwable {
+            return createTest();
+          }
+        }.run();
+      } catch (Throwable e) {
+        runLeaf(new Fail(e), description, notifier);
+      }
+
+      if (ignorance.isIgnored(method, test)) {
+        ignoreCollector.callback(test.getClass(), method.getName(), test.getClass().getAnnotation(Ignore.class));
+      }
+    }
+  }
 }
