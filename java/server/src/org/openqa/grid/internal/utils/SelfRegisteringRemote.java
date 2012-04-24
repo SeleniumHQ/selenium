57efa43d29b8fb4d9cd7f69684143f38b5083b13
Francois Reynaud <francois.reynaud@gmail.com>
2012-04-24 22:15:59 +0000

    FrancoisReynaud: add some validation on the default node that remove invalid capabilities ( IE on non Win platform and Opera not installed )
    r16719

--- java/server/src/org/openqa/grid/internal/utils/SelfRegisteringRemote.java
+++ java/server/src/org/openqa/grid/internal/utils/SelfRegisteringRemote.java
@@ -38,12 +38,17 @@ import org.openqa.selenium.server.RemoteControlConfiguration;
 import org.openqa.selenium.server.SeleniumServer;
 import org.openqa.selenium.server.log.LoggingManager;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Node;
+import com.opera.core.systems.OperaDriver;
+import com.opera.core.systems.OperaPaths;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -63,9 +68,36 @@ public class SelfRegisteringRemote {
 
   public SelfRegisteringRemote(RegistrationRequest config) {
     this.nodeConfig = config;
+    removeInvalidCapabilities();
     this.httpClientFactory = new HttpClientFactory();
   }
 
+  private void removeInvalidCapabilities(){
+    List<DesiredCapabilities> toRemove = new ArrayList<DesiredCapabilities>();
+    
+    for (DesiredCapabilities cap : nodeConfig.getCapabilities()){
+      
+      // IE only on windows.
+      if (isIE(cap) && ! Platform.getCurrent().is(Platform.WINDOWS)){
+        toRemove.add(cap);
+      }
+      // opera not installed.
+      if (isOpera(cap) && OperaPaths.operaPath()==null){
+        toRemove.add(cap);
+      }
+    }
+    for (DesiredCapabilities rm : toRemove){
+      nodeConfig.getCapabilities().remove(rm);
+    }
+  }
+  
+  private boolean isOpera(DesiredCapabilities cap){
+    return DesiredCapabilities.opera().getBrowserName().equals(cap.getBrowserName());
+  }
+  private boolean isIE(DesiredCapabilities cap){
+    return DesiredCapabilities.internetExplorer().getBrowserName().equals(cap.getBrowserName());
+  }
+  
   public URL getRemoteURL() {
     String host = (String) nodeConfig.getConfiguration().get(RegistrationRequest.HOST);
     String port = (String) nodeConfig.getConfiguration().get(RegistrationRequest.PORT);
