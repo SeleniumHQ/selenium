792357eee75a879504173000fb1df301f2862a8e
Jason Lebya <jmleyba@gmail.com>
2012-04-26 00:25:28 +0000

    JasonLeyba: Extract webdriver.Key to its own file.
    r16731

--- javascript/webdriver/webdriver.js
+++ javascript/webdriver/webdriver.js
@@ -16,7 +16,6 @@
  * @fileoverview The heart of the WebDriver JavaScript API.
  */
 
-goog.provide('webdriver.Key');
 goog.provide('webdriver.WebDriver');
 goog.provide('webdriver.WebDriver.Navigation');
 goog.provide('webdriver.WebDriver.Options');
@@ -31,6 +30,7 @@ goog.require('goog.array');
 goog.require('goog.object');
 goog.require('webdriver.Command');
 goog.require('webdriver.CommandName');
+goog.require('webdriver.Key');
 goog.require('webdriver.Locator');
 goog.require('webdriver.Session');
 goog.require('webdriver.error');
@@ -1269,87 +1269,6 @@ webdriver.WebDriver.TargetLocator.prototype.window = function(nameOrHandle) {
       new webdriver.Command(webdriver.CommandName.SWITCH_TO_WINDOW).
           setParameter('name', nameOrHandle),
       'WebDriver.switchTo().window(' + nameOrHandle + ')');
-};
-
-
-//////////////////////////////////////////////////////////////////////////////
-//
-//  webdriver.Key
-//
-//////////////////////////////////////////////////////////////////////////////
-
-
-/**
- * Representations of pressable keys that aren't text.  These are stored in
- * the Unicode PUA (Private Use Area) code points, 0xE000-0xF8FF.  Refer to
- * http://www.google.com.au/search?&q=unicode+pua&btnG=Search
- *
- * @enum {string}
- */
-webdriver.Key = {
-  NULL:         '\uE000',
-  CANCEL:       '\uE001',  // ^break
-  HELP:         '\uE002',
-  BACK_SPACE:   '\uE003',
-  TAB:          '\uE004',
-  CLEAR:        '\uE005',
-  RETURN:       '\uE006',
-  ENTER:        '\uE007',
-  SHIFT:        '\uE008',
-  CONTROL:      '\uE009',
-  ALT:          '\uE00A',
-  PAUSE:        '\uE00B',
-  ESCAPE:       '\uE00C',
-  SPACE:        '\uE00D',
-  PAGE_UP:      '\uE00E',
-  PAGE_DOWN:    '\uE00F',
-  END:          '\uE010',
-  HOME:         '\uE011',
-  ARROW_LEFT:   '\uE012',
-  LEFT:         '\uE012',
-  ARROW_UP:     '\uE013',
-  UP:           '\uE013',
-  ARROW_RIGHT:  '\uE014',
-  RIGHT:        '\uE014',
-  ARROW_DOWN:   '\uE015',
-  DOWN:         '\uE015',
-  INSERT:       '\uE016',
-  DELETE:       '\uE017',
-  SEMICOLON:    '\uE018',
-  EQUALS:       '\uE019',
-
-  NUMPAD0:      '\uE01A',  // number pad keys
-  NUMPAD1:      '\uE01B',
-  NUMPAD2:      '\uE01C',
-  NUMPAD3:      '\uE01D',
-  NUMPAD4:      '\uE01E',
-  NUMPAD5:      '\uE01F',
-  NUMPAD6:      '\uE020',
-  NUMPAD7:      '\uE021',
-  NUMPAD8:      '\uE022',
-  NUMPAD9:      '\uE023',
-  MULTIPLY:     '\uE024',
-  ADD:          '\uE025',
-  SEPARATOR:    '\uE026',
-  SUBTRACT:     '\uE027',
-  DECIMAL:      '\uE028',
-  DIVIDE:       '\uE029',
-
-  F1:           '\uE031',  // function keys
-  F2:           '\uE032',
-  F3:           '\uE033',
-  F4:           '\uE034',
-  F5:           '\uE035',
-  F6:           '\uE036',
-  F7:           '\uE037',
-  F8:           '\uE038',
-  F9:           '\uE039',
-  F10:          '\uE03A',
-  F11:          '\uE03B',
-  F12:          '\uE03C',
-
-  COMMAND:      '\uE03D',  // Apple command key
-  META:         '\uE03D'   // alias for Windows key
 };
 
 
