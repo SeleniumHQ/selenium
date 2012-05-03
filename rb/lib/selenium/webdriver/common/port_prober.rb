10fea424a91136980c676a72fb2eaecdfac6a10e
Jari Bakken <jari.bakken@gmail.com>
2012-05-03 01:01:35 +0000

    JariBakken: Make sure Ruby's PortProber.above checks all network interfaces.
    r16769

--- rb/lib/selenium/webdriver/common/port_prober.rb
+++ rb/lib/selenium/webdriver/common/port_prober.rb
@@ -5,21 +5,25 @@ module Selenium
         port += 1 until free? port
         port
       end
-      
+
       def self.random
         server = TCPServer.new(Platform.localhost, 0)
         port   = server.addr[1]
         server.close
-        
+
         port
       end
-      
+
       def self.free?(port)
-        TCPServer.new(Platform.localhost, port).close
+        interfaces = Socket.getaddrinfo("localhost", port).map { |e| e[2] }
+        interfaces += ["0.0.0.0", Platform.ip]
+        interfaces.each { |address| TCPServer.new(address, port).close }
+
         true
       rescue SocketError, Errno::EADDRINUSE
         false
       end
+
     end # PortProber
   end # WebDriver
 end # Selenium
