4e887ef999f28e29c42a9eaa9d6b172f799b28ec
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-05-04 12:13:24 +0000

    AndreasTolfTolfsen: Unignoring some clear tests for Opera
    r16795

--- java/client/test/org/openqa/selenium/ClearTest.java
+++ java/client/test/org/openqa/selenium/ClearTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2012 WebDriver committers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package org.openqa.selenium;
 
 import org.junit.Test;
@@ -13,8 +29,7 @@ import static org.openqa.selenium.testing.Ignore.Driver.IPHONE;
 import static org.openqa.selenium.testing.Ignore.Driver.OPERA;
 import static org.openqa.selenium.testing.Ignore.Driver.SELENESE;
 
-
-@Ignore({CHROME, OPERA, SELENESE, ANDROID})
+@Ignore({CHROME, SELENESE, ANDROID})
 public class ClearTest extends JUnit4TestBase {
 
   @Test
@@ -39,6 +54,7 @@ public class ClearTest extends JUnit4TestBase {
   }
 
   @Test
+  @Ignore(OPERA)
   public void testTextInputShouldNotClearWhenReadOnly() {
     try {
       driver.get(pages.readOnlyPage);
@@ -71,6 +87,7 @@ public class ClearTest extends JUnit4TestBase {
   }
 
   @Test
+  @Ignore(OPERA)
   public void testTextAreaShouldNotClearWhenReadOnly() {
     try {
       driver.get(pages.readOnlyPage);
