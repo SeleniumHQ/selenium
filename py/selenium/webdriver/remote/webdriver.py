6c3aabb7578e77bafd61299d1d32d2dba1fe70dc
David Burns <david.burns@theautomatedtester.co.uk>
2012-05-11 20:43:17 +0000

    DavidBurns correcting syntax error, not sure how this hasnt been hit
    r16880

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -50,7 +50,7 @@ class WebDriver(object):
         """
         if desired_capabilities is None:
             raise WebDriverException("Desired Capabilities can't be None")
-        if not isinstance(desired_capabilities, dict)
+        if not isinstance(desired_capabilities, dict):
             raise WebDriverException("Desired Capabilities must be a dictionary")
         self.command_executor = command_executor
         if type(self.command_executor) is str or type(self.command_executor) is unicode:
