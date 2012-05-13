007c7fe9a60c328ae00f2f0e1c7d8f6a857edb0f
Simon M Stewart <simon.m.stewart@gmail.com>
2012-05-13 21:43:40 +0000

    SimonStewart: Minor tweaks to copyright headers
    r16903

--- java/client/test/org/openqa/selenium/TextPagesTest.java
+++ java/client/test/org/openqa/selenium/TextPagesTest.java
@@ -1,6 +1,6 @@
 /*
 Copyright 2007-2009 WebDriver committers
-Copyright 2007-2009 Google Inc.
+Copyright 2007-2009 Software Freedom Conservancy
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,9 +13,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- */
-
-// Copyright 2008 Google Inc.  All Rights Reserved.
+*/
 
 package org.openqa.selenium;
 
