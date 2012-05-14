898ab9c5f56323dba277b8bef6835a766ec30b7f
Simon M Stewart <simon.m.stewart@gmail.com>
2012-05-14 14:40:37 +0000

    SimonStewart: Minor formatting changes to copyright headers. No logical change.
    r16914

--- java/server/src/org/openqa/grid/web/servlet/handler/LegacySeleniumRequest.java
+++ java/server/src/org/openqa/grid/web/servlet/handler/LegacySeleniumRequest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2011 WebDriver committers Copyright 2011 Software Freedom Conservancy
+ * Copyright 2011 WebDriver committers
+ * Copyright 2011 Software Freedom Conservancy
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
