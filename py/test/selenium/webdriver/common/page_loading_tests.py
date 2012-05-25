9acbe5f53b9fc5d4c59c1162d806be92bd41083b
David Burns <david.burns@theautomatedtester.co.uk>
2012-05-25 14:56:14 +0000

    DavidBurns updated page loading tests for python
    r17019

--- py/test/selenium/webdriver/common/page_loading_tests.py
+++ py/test/selenium/webdriver/common/page_loading_tests.py
@@ -13,13 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import re
-import tempfile
-import time
-import shutil
 import unittest
-from selenium.common.exceptions import NoSuchElementException
+import pytest
+
 from selenium.webdriver.common.by import By
 
 class PageLoadingTests(unittest.TestCase):
@@ -45,13 +41,15 @@ class PageLoadingTests(unittest.TestCase):
         self.driver.get(location + "#text")
         self.driver.find_element(by=By.ID, value="id1")
 
+    @pytest.mark.ignore_safari
     def testShouldReturnWhenGettingAUrlThatDoesNotResolve(self):
         try:
           # Of course, we're up the creek if this ever does get registered
           self.driver.get("http://www.thisurldoesnotexist.comx/")
-        except IllegalStateException, e:
+        except IllegalStateException:
             pass
-
+    
+    @pytest.mark.ignore_safari
     def testShouldReturnWhenGettingAUrlThatDoesNotConnect(self):
         # Here's hoping that there's nothing here. There shouldn't be
         self.driver.get("http://localhost:3001")
@@ -108,6 +106,12 @@ class PageLoadingTests(unittest.TestCase):
 
         self.driver.forward()
         self.assertEqual(self.driver.title, "We Arrive Here")
+
+    @pytest.mark.ignore_ie
+    def testShouldNotHangifDocumentOpenCallIsNeverFollowedByDocumentCloseCall(self):
+        ''' See http://code.google.com/p/selenium/issues/detail?id=208 '''
+        self._loadPage("document_write_in_onload")
+        self.driver.find_element(By.XPATH, "//body")
 
     def testShouldBeAbleToRefreshAPage(self):
         self._loadPage("xhtmlTest")
