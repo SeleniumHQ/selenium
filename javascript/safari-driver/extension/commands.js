c2ea90c8bd550db7b0a0feab0c872f6d3a87fbd1
Jason Lebya <jmleyba@gmail.com>
2012-05-26 15:42:35 +0000

    JasonLeyba: Copyright header cleanup.
    r17028

--- javascript/safari-driver/extension/commands.js
+++ javascript/safari-driver/extension/commands.js
@@ -1,4 +1,5 @@
-// Copyright 2012 Software Freedom Conservancy. All Rights Reserved.
+// Copyright 2012 Selenium committers
+// Copyright 2012 Software Freedom Conservancy
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
