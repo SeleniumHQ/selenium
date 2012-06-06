5ac57622876203239749d61072b8ccd50c57f71e
Jim Evans <james.h.evans.jr@gmail.com>
2012-06-06 18:30:25 +0000

    JimEvans: Bumping .NET assembly version numbers to 2.23.0 for impending release.
    r17134

--- dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.22.1.0")]
-[assembly: AssemblyFileVersion("2.22.1.0")]
+[assembly: AssemblyVersion("2.23.0.0")]
+[assembly: AssemblyFileVersion("2.23.0.0")]
