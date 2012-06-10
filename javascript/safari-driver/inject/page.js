90b0bc6abd49151f5df177b678bd5e48f9268bfa
Jason Lebya <jmleyba@gmail.com>
2012-06-10 04:48:20 +0000

    JasonLeyba: Add missing type annotation.
    r17154

--- javascript/safari-driver/inject/page.js
+++ javascript/safari-driver/inject/page.js
@@ -99,6 +99,7 @@ goog.exportSymbol('init', safaridriver.inject.page.init);
 /**
  * Handles command messages from the injected script.
  * @param {!safaridriver.message.Command} message The command message.
+ * @param {!MessageEvent} e The original message event.
  * @throws {Error} If the command is not supported by this script.
  * @private
  */
