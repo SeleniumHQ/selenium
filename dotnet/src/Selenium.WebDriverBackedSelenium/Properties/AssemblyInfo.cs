87170635e9a00670fc284d0144d01a44ce09dd36
Jim Evans <james.h.evans.jr@gmail.com>
2012-06-19 15:17:19 +0000

    JimEvans: Updating .NET version to 2.24.0 for impending release.
    r17201

--- dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
+++ dotnet/src/Selenium.WebDriverBackedSelenium/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.23.0.0")]
-[assembly: AssemblyFileVersion("2.23.0.0")]
+[assembly: AssemblyVersion("2.24.0.0")]
+[assembly: AssemblyFileVersion("2.24.0.0")]
