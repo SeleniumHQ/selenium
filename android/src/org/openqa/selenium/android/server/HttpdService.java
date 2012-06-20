6f3bb7897b5cabc852c6ae1ae6323da5c83335a0
Simon M Stewart <simon.m.stewart@gmail.com>
2012-06-20 16:10:40 +0000

    SimonStewart: Move the webbit server off the UI thread in the android driver. I suspect it's not quite right. Only tested on ICS x86 emulator.
    r17232

--- android/src/org/openqa/selenium/android/server/HttpdService.java
+++ android/src/org/openqa/selenium/android/server/HttpdService.java
@@ -17,37 +17,40 @@ limitations under the License.
 
 package org.openqa.selenium.android.server;
 
-import org.openqa.selenium.android.library.Logger;
-import org.openqa.selenium.android.Platform;
-import org.openqa.selenium.android.app.R;
-import org.webbitserver.WebServer;
-import org.webbitserver.WebServers;
+import com.google.common.base.Throwables;
 
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.widget.Toast;
+
+import org.openqa.selenium.android.Platform;
+import org.openqa.selenium.android.app.R;
+import org.openqa.selenium.android.library.Logger;
+import org.webbitserver.WebServer;
+import org.webbitserver.WebServers;
 
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
 public class HttpdService extends Service {
   private static final String LOG_TAG = HttpdService.class.getName();
+  private static final int PORT = 8080;
+
   private NotificationManager notificationManager;
-  private WebServer server;
-  private int port = 8080;
-
   private PowerManager.WakeLock wakeLock;
-
   private IBinder binder;
-  
+  private HttpdThread serverThread;
+
   @Override
   public void onCreate() {
     binder = new WebDriverBinder(this);
-    startServer();
+    startHttpd();
+
     super.onCreate();
   }
 
@@ -79,18 +82,18 @@ public class HttpdService extends Service {
         wakeLock = null;
       }
 
-      if (server != null) {
-        stopJetty();
+      if (serverThread != null) {
+        stopHttpd();
         // Cancel the persistent notification.
         notificationManager.cancel(R.string.httpd_started);
         // Tell the user we stopped.
         Toast.makeText(this, getText(R.string.httpd_stopped), Toast.LENGTH_SHORT).show();
-        Logger.log(Level.INFO, LOG_TAG, "onDestroy", "Jetty stopped");
+        Logger.log(Level.INFO, LOG_TAG, "onDestroy", "Httpd stopped");
       } else {
         Toast.makeText(HttpdService.this, R.string.httpd_not_running, Toast.LENGTH_SHORT).show();
       }
     } catch (Exception e) {
-      Logger.log(Level.INFO, LOG_TAG, "onDestroy", "Error stopping jetty" + e.getMessage());
+      Logger.log(Level.INFO, LOG_TAG, "onDestroy", "Error stopping httpd: " + e.getMessage());
       Toast.makeText(this, getText(R.string.httpd_not_stopped), Toast.LENGTH_SHORT).show();
     }
   }
@@ -105,15 +108,7 @@ public class HttpdService extends Service {
    * Get a reference to the Webbit Server instance
    */
   public WebServer getServer() {
-    return server;
-  }
-
-  protected void workAroundFroyoBug(WebServer server) {
-    // Workaround a Froyo bug
-    // http://code.google.com/p/android/issues/detail?id=9431
-    if (Platform.FROYO == Platform.sdk()) {
-      System.setProperty("java.net.preferIPv6Addresses", "false");
-    }
+    return serverThread.getServer();
   }
 
   protected void configureHandlers(WebServer server) {
@@ -121,39 +116,102 @@ public class HttpdService extends Service {
     server.add("/wd/hub/.*", new AndroidDriverServlet(Logger.getLogger(), "/wd/hub"));
   }
 
-  public void startServer() {
-    if (server != null) {
+  public void startHttpd() {
+    if (serverThread != null && serverThread.isAlive()) {
       Toast.makeText(HttpdService.this, R.string.httpd_already_started, Toast.LENGTH_SHORT).show();
       return;
     }
 
-    try {
-      // Get a wake lock to stop the cpu going to sleep
-      PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-      wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "IJetty");
-      wakeLock.acquire();
-
-      server = WebServers.createWebServer(Executors.newCachedThreadPool(), port);
-
-      workAroundFroyoBug(server);
-      configureHandlers(server);
-
-      server.start();
-
-      notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-      Toast.makeText(HttpdService.this, R.string.httpd_started, Toast.LENGTH_SHORT).show();
-
-      Logger.log(Level.INFO, LOG_TAG, "startServer", "Httpd started");
-    } catch (Exception e) {
-      Logger.log(Level.WARNING, LOG_TAG, "startServer", "Error starting httpd " + e);
-      Toast.makeText(this, getText(R.string.httpd_not_started), Toast.LENGTH_SHORT).show();
-      throw new RuntimeException("Httpd failed to start!");
+    if (serverThread != null) {
+      Logger.log(Level.INFO, LOG_TAG, "startHttpd", "Stopping extant httpd.");
+      stopHttpd();
     }
+
+    serverThread = new HttpdThread(this, PORT);
+    serverThread.start();
   }
 
-  protected void stopJetty() throws Exception {
+  protected void stopHttpd() {
+    if (serverThread == null) {
+      return;
+    }
+    if (!serverThread.isAlive()) {
+      serverThread = null;
+      return;
+    }
+
     Logger.log(Level.FINE, LOG_TAG, "stopServer", "Httpd stopping");
-    server.stop();
-    server = null;
+    serverThread.stopLooping();
+    serverThread.interrupt();
+    try {
+      serverThread.join();
+    } catch (InterruptedException e) {
+      throw Throwables.propagate(e);
+    }
+    serverThread = null;
+  }
+
+  private class HttpdThread extends Thread {
+
+    private final WebServer server;
+    private final Context context;
+    private Looper looper;
+
+    public HttpdThread(Context context, int port) {
+      this.context = context;
+
+      // Create the server but absolutely do not start it here
+      server = WebServers.createWebServer(Executors.newCachedThreadPool(), port);
+
+      workAroundFroyoBug();
+      configureHandlers(server);
+    }
+
+    @Override
+    public void run() {
+      Looper.prepare();
+      looper = Looper.myLooper();
+      startServer();
+      Looper.loop();
+    }
+
+    public WebServer getServer() {
+      return server;
+    }
+
+    private void startServer() {
+      try {
+        // Get a wake lock to stop the cpu going to sleep
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "WebDriver");
+        wakeLock.acquire();
+
+        server.start();
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Toast.makeText(HttpdService.this, R.string.httpd_started, Toast.LENGTH_SHORT).show();
+
+        Logger.log(Level.INFO, LOG_TAG, "startHttpd", "Httpd started");
+      } catch (Exception e) {
+        Logger.log(Level.WARNING, LOG_TAG, "startHttpd", "Error starting httpd " + e);
+        Toast.makeText(context, getText(R.string.httpd_not_started), Toast.LENGTH_SHORT).show();
+        throw new RuntimeException("Httpd failed to start!");
+      }
+    }
+
+    private void workAroundFroyoBug() {
+      // Workaround a Froyo bug
+      // http://code.google.com/p/android/issues/detail?id=9431
+      if (Platform.FROYO == Platform.sdk()) {
+        System.setProperty("java.net.preferIPv6Addresses", "false");
+      }
+    }
+
+    public void stopLooping() {
+      if (looper == null) {
+        return;
+      }
+      looper.quit();
+    }
   }
 }
