71b133ab27a8ec55288b6e3de22c3998ff6fcb20
Jari Bakken <jari.bakken@gmail.com>
2012-06-21 15:24:22 +0000

    Revert "JariBakken: Fix CSS color expectation."
    This reverts commit 35f5d8706ff2488e4bdaf00811ee8e1b29f833b4.
    
    r17248

--- rb/spec/integration/selenium/webdriver/element_spec.rb
+++ rb/spec/integration/selenium/webdriver/element_spec.rb
@@ -142,10 +142,8 @@ describe "Element" do
       style1 = element.css_value("background-color")
       style2 = element.style("background-color") # backwards compatibility
 
-      expected = 'rgba(0, 128, 0, 1)'
-
-      style1.should == expected
-      style2.should == expected
+      acceptable = ["rgb(0, 128, 0)", "#008000", 'rgba(0,128,0,1)']
+      acceptable.should include(style1, style2)
     end
   end
 
