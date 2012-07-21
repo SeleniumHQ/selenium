dc87d8974284b305784ceebe9c148cbabf5cc9b1
Alexei Barantsev <barancev@gmail.com>
2012-07-21 21:04:08 +0000

    AlexeiBarantsev: Updating imports for Hamcrest 1.3
    r17554

--- java/server/test/org/openqa/selenium/remote/server/CapabilitiesComparatorTest.java
+++ java/server/test/org/openqa/selenium/remote/server/CapabilitiesComparatorTest.java
@@ -19,8 +19,8 @@ package org.openqa.selenium.remote.server;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.number.OrderingComparisons.greaterThan;
-import static org.hamcrest.number.OrderingComparisons.lessThan;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.hamcrest.number.OrderingComparison.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.openqa.selenium.remote.server.CapabilitiesComparator.getBestMatch;
 
