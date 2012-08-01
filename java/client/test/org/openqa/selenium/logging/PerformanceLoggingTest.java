8f87b01d1b109d8739fa55b412b24e44e54c0f69
Eran Mes <eran.mes@gmail.com>
2012-08-01 10:50:10 +0000

    EranMes on behalf of EmmaSoderberg: Fix performance logging test for Firefox.
    r17619

--- java/client/test/org/openqa/selenium/logging/PerformanceLoggingTest.java
+++ java/client/test/org/openqa/selenium/logging/PerformanceLoggingTest.java
@@ -17,66 +17,87 @@ limitations under the License.
 package org.openqa.selenium.logging;
 
 import static org.hamcrest.Matchers.greaterThan;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import static org.openqa.selenium.remote.CapabilityType.ENABLE_PROFILING_CAPABILITY;
+import static org.openqa.selenium.testing.Ignore.Driver.ANDROID;
+import static org.openqa.selenium.testing.Ignore.Driver.CHROME;
+import static org.openqa.selenium.testing.Ignore.Driver.HTMLUNIT;
+import static org.openqa.selenium.testing.Ignore.Driver.IE;
+import static org.openqa.selenium.testing.Ignore.Driver.IPHONE;
+import static org.openqa.selenium.testing.Ignore.Driver.OPERA;
+import static org.openqa.selenium.testing.Ignore.Driver.SAFARI;
+import static org.openqa.selenium.testing.Ignore.Driver.SELENESE;
 
 import org.junit.After;
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.logging.profiler.EventType;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.testing.drivers.SynthesizedFirefoxDriver;
+import org.openqa.selenium.testing.Ignore;
+import org.openqa.selenium.testing.JUnit4TestBase;
+import org.openqa.selenium.testing.drivers.WebDriverBuilder;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 
-public class PerformanceLoggingTest {
-  private WebDriver driver;
+@Ignore({ANDROID,CHROME,HTMLUNIT,IE,IPHONE,OPERA,SAFARI,SELENESE})
+public class PerformanceLoggingTest extends JUnit4TestBase {
+  
+  private WebDriver localDriver;
 
   @After
   public void quitDriver() {
-    if (driver != null) {
-      driver.quit();
-      driver = null;
+    if (localDriver != null) {
+      localDriver.quit();
+      localDriver = null;
     }
   }
 
   @Test
   public void testDisabledProfilingDoesNotLog() {
-    driver = new SynthesizedFirefoxDriver();
-    driver.get("https://www.google.com");
-    assertEquals(0, getProfilerEntries().getAll().size());
+    driver.get(pages.simpleTestPage);
+    assertTrue("Profiler should not log when disabled", 
+        getProfilerEntries(driver).getAll().size() == 0);
   }
 
   @Test
   public void testLogsSingleHttpCommand() {
     startLoggingDriver();
     // Expect start of newSession, end of newSession, start of getLogs, end of getLogs
-    assertEquals(4, getProfilerEntriesOfType(EventType.HTTP_COMMAND).size());
+    // Expect start of newSession, end of newSession, start of getLogs, end of getLogs
+    assertEquals(4, getProfilerEntriesOfType(getProfilerEntries(localDriver), 
+        EventType.HTTP_COMMAND).size());
   }
 
   @Test
   public void testGetsYieldToPageLoadLogEntries() throws Exception {
     startLoggingDriver();
-    driver.get("https://www.google.com/imghp");
-    driver.findElement(By.name("q")).sendKeys("Monkeys");
-    driver.findElement(By.name("btnG")).click();
-    assertThat(getProfilerEntriesOfType(EventType.YIELD_TO_PAGE_LOAD).size(), greaterThan(0));
+    localDriver.get(pages.formPage);
+    localDriver.findElement(By.id("submitButton")).click();
+    assertThat(getProfilerEntriesOfType(getProfilerEntries(localDriver), 
+        EventType.YIELD_TO_PAGE_LOAD).size(), greaterThan(0));
   }
-
+  
   private void startLoggingDriver() {
-    driver = new SynthesizedFirefoxDriver(getProfilerEnabledCapabilities());
+    WebDriverBuilder builder = new WebDriverBuilder().setDesiredCapabilities(
+        getCapabilitiesWithProfilerOn(true));
+    localDriver = builder.get();    
   }
 
-  private LogEntries getProfilerEntries() {
+  private LogEntries getProfilerEntries(WebDriver driver) {
     return driver.manage().logs().get(LogType.PROFILER);
   }
 
-  private ImmutableList<LogEntry> getProfilerEntriesOfType(final EventType eventType) {
-    return ImmutableList.copyOf(Iterables.filter(getProfilerEntries(), new Predicate<LogEntry>() {
+  private ImmutableList<LogEntry> getProfilerEntriesOfType(final LogEntries entries, 
+      final EventType eventType) {
+    return ImmutableList.copyOf(Iterables.filter(entries, new Predicate<LogEntry>() {
       @Override
       public boolean apply(LogEntry entry) {
         return entry.getMessage().contains(eventType.toString());
@@ -84,9 +105,9 @@ public class PerformanceLoggingTest {
     }));
   }
 
-  private static DesiredCapabilities getProfilerEnabledCapabilities() {
+  private static DesiredCapabilities getCapabilitiesWithProfilerOn(boolean enabled) {
     DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-    capabilities.setCapability(CapabilityType.ENABLE_PROFILING_CAPABILITY, true);
+    capabilities.setCapability(ENABLE_PROFILING_CAPABILITY, enabled);
     return capabilities;
   }
 }
