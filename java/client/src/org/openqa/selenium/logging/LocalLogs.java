84f71aa8f54609ac8f8a9ef6bfb77880f49aa2be
Andreas Tolf Tolfsen <andreastt@opera.com>
2012-08-17 14:49:23 +0000

    AndreasTolfTolfsen: Return list of available log types in this instance of LocalLogs
    r17691

--- java/client/src/org/openqa/selenium/logging/LocalLogs.java
+++ java/client/src/org/openqa/selenium/logging/LocalLogs.java
@@ -16,13 +16,13 @@ limitations under the License.
 
 package org.openqa.selenium.logging;
 
-import java.util.HashMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 
 /**
  * Stores and retrieves logs in-process (i.e. without any RPCs).
@@ -33,16 +33,15 @@ public class LocalLogs implements Logs {
    * Logger which doesn't do anything.
    */
   public static final LocalLogs NULL_LOGGER = new LocalLogs() {
-    @Override
     public void addEntry(String logType, LogEntry entry) {
     }
   };
-  
-  private Map<String, List<LogEntry>> localLogs = new HashMap<String, List<LogEntry>>();
+
+  private final Map<String, List<LogEntry>> localLogs = Maps.newHashMap();
   private final Set<String> logTypesToIgnore;
 
   public LocalLogs() {
-    this.logTypesToIgnore = ImmutableSet.<String>of();
+    logTypesToIgnore = ImmutableSet.of();
   }
 
   public LocalLogs(Set<String> logTypesToIgnore) {
@@ -50,6 +49,7 @@ public class LocalLogs implements Logs {
   }
 
   public LogEntries get(String logType) {
+    // TODO(andreastt): Should presumably clear the logs of that type to reduce memory consumption
     return new LogEntries(getLocalLogs(logType));
   }
 
@@ -57,13 +57,21 @@ public class LocalLogs implements Logs {
     if (localLogs.containsKey(logType)) {
       return localLogs.get(logType);
     }
+
     return Lists.newArrayList();
   }
 
+  /**
+   * Add a new log entry to the local storage.
+   *
+   * @param logType the log type to store
+   * @param entry   the entry to store
+   */
   public void addEntry(String logType, LogEntry entry) {
     if (logTypesToIgnore.contains(logType)) {
       return;
     }
+
     if (!localLogs.containsKey(logType)) {
       localLogs.put(logType, Lists.newArrayList(entry));
     } else {
@@ -72,6 +80,7 @@ public class LocalLogs implements Logs {
   }
 
   public Set<String> getAvailableLogTypes() {
-    return ImmutableSet.<String>of(LogType.CLIENT);
+    return localLogs.keySet();
   }
+
 }
