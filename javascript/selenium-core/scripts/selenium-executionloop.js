4cfff6886690427d6b80c4e54879c399d227f575
Simon M Stewart <simon.m.stewart@gmail.com>
2012-09-26 21:34:46 +0000

    SimonStewart: Take out something I added for debugging. It returns too much detail in the common case
    r17874

--- javascript/selenium-core/scripts/selenium-executionloop.js
+++ javascript/selenium-core/scripts/selenium-executionloop.js
@@ -121,9 +121,6 @@ TestLoop.prototype = {
             LOG.exception(e);
             var msg = "Command execution failure. Please search the user group at https://groups.google.com/forum/#!forum/selenium-users for error details from the log window.";
             msg += "  The error message is: " + extractExceptionMessage(e);
-            if (e.stack) {
-                msg += '\n' + e.stack;
-            }
             return this.commandError(msg);
         } else {
             LOG.error(e.message);
@@ -159,9 +156,6 @@ TestLoop.prototype = {
             this.result = {};
             this.result.failed = true;
             this.result.failureMessage = extractExceptionMessage(e);
-            if (e.stack) {
-                this.result.failureMessage += '\n' + e.stack;
-            }
             this.commandComplete(this.result);
             this.continueTest();
         }
