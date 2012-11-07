f0125906ce854c7018e948b8e848aaa0674937ab
Alexei Barantsev <barancev@gmail.com>
2012-11-07 12:03:43 +0000

    AlexeiBarantsev: Removing precondition because it blocks this test on windows systems entirely.
    r18088

--- java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
+++ java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
@@ -22,23 +22,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.os.CommandLine;
 
 import java.io.File;
 
 public class ExecutableTest {
 
-  private String executableName = Platform.getCurrent().is(Platform.MAC) ?
-      "/Applications/Firefox.app/Contents/Resources/firefox" : "firefox";
-
   @Test
   public void testEnvironmentDiscovery() {
-    if (CommandLine.find(executableName) == null) {
-      System.out.println("Skipping test: no firefox present");
-      return;
-    }
-
     Executable env = new Executable(null);
     File exe = env.getFile();
     assertNotNull(exe);
