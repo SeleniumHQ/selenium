bd933640555317238aab227b8d6113493c83a167
Daniel Wagner-Hall <dawagner@gmail.com>
2013-01-08 11:23:49 +0000

    DanielWagnerHall: Adding equals and hashCode to ChromeOptions
    r18409

--- java/client/src/org/openqa/selenium/chrome/ChromeOptions.java
+++ java/client/src/org/openqa/selenium/chrome/ChromeOptions.java
@@ -20,6 +20,7 @@ package org.openqa.selenium.chrome;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -199,5 +200,22 @@ public class ChromeOptions {
     }
 
     return capabilities;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof ChromeOptions)) {
+      return false;
+    }
+    ChromeOptions that = (ChromeOptions) other;
+    return Objects.equal(this.binary, that.binary)
+        && Objects.equal(this.args, that.args)
+        && Objects.equal(this.extensionFiles, that.extensionFiles)
+        && Objects.equal(this.experimentalOptions, that.experimentalOptions);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(this.binary, this.args, this.extensionFiles, this.experimentalOptions);
   }
 }
