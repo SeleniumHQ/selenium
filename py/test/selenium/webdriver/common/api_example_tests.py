dd96eab6cd50e46a60bb4dcefdf3c44bb58f278a
Daniel Fackrell <dfackrell@bluehost.com>
2013-02-12 21:53:41 +0000

    More print() changes for python3

--- py/test/selenium/webdriver/common/api_example_tests.py
+++ py/test/selenium/webdriver/common/api_example_tests.py
@@ -25,7 +25,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 def not_available_on_remote(func):
     def testMethod(self):
-        print self.driver
+        print(self.driver)
         if type(self.driver) == 'remote':
             return lambda x: None
         else:
