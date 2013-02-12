cf50cfe0e31d1512cf4566b900ea99208c78c0c7
Daniel Fackrell <dfackrell@bluehost.com>
2013-02-12 17:07:55 +0000

    Use "as" for raised exceptions

--- py/test/selenium/webdriver/common/implicit_waits_tests.py
+++ py/test/selenium/webdriver/common/implicit_waits_tests.py
@@ -44,9 +44,9 @@ class ImplicitWaitTest(unittest.TestCase):
         try:
             self.driver.find_element_by_id("box0")
             self.fail("Expected NoSuchElementException to have been thrown")
-        except NoSuchElementException, e:
+        except NoSuchElementException as e:
             pass
-        except Exception, e:
+        except Exception as e:
             self.fail("Expected NoSuchElementException but got " + str(e))
 
     def testShouldReturnAfterFirstAttemptToFindOneAfterDisablingImplicitWaits(self):
@@ -56,9 +56,9 @@ class ImplicitWaitTest(unittest.TestCase):
         try:
             self.driver.find_element_by_id("box0")
             self.fail("Expected NoSuchElementException to have been thrown")
-        except NoSuchElementException, e:
+        except NoSuchElementException as e:
             pass
-        except Exception, e:
+        except Exception as e:
             self.fail("Expected NoSuchElementException but got " + str(e))
     
     def testShouldImplicitlyWaitUntilAtLeastOneElementIsFoundWhenSearchingForMany(self):
