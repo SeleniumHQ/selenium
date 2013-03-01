b5480499d8237d449fccc34bde9ea9521de55c2b
Alexei Barantsev <barancev@gmail.com>
2013-03-02 01:05:20 +0400

    Splitting a long test scenario into shorter ones

--- java/client/test/com/thoughtworks/selenium/corebased/TestOpen.java
+++ java/client/test/com/thoughtworks/selenium/corebased/TestOpen.java
@@ -22,24 +22,27 @@ import com.thoughtworks.selenium.InternalSelenseTestBase;
 
 import org.junit.Test;
 
-import java.util.regex.Pattern;
-
 public class TestOpen extends InternalSelenseTestBase {
   @Test
   public void testOpen() throws Exception {
     selenium.open("../tests/html/test_open.html");
     verifyTrue(selenium.getLocation().matches("^[\\s\\S]*/tests/html/test_open\\.html$"));
-    // Should really split these verifications into their own test file.
-    verifyTrue(Pattern.compile(".*/tests/html/[Tt]est_open.html").matcher(selenium.getLocation())
-        .find());
-    verifyFalse(selenium.getLocation().matches("^[\\s\\S]*/foo\\.html$"));
-    verifyTrue(selenium.isTextPresent("glob:This is a test of the open command."));
+  }
+  
+  @Test
+  public void testIsTextPresentCanDoExactAndRegexChecks() {
+    selenium.open("../tests/html/test_open.html");
     verifyTrue(selenium.isTextPresent("This is a test of the open command."));
+    verifyTrue(selenium.isTextPresent("glob:This is a test of the open command."));
     verifyTrue(selenium.isTextPresent("exact:This is a test of"));
     verifyTrue(selenium.isTextPresent("regexp:This is a test of"));
     verifyTrue(selenium.isTextPresent("regexp:T*his is a test of"));
     verifyFalse(selenium.isTextPresent("exact:XXXXThis is a test of"));
     verifyFalse(selenium.isTextPresent("regexp:ThXXXXXXXXXis is a test of"));
+  }
+
+@Test
+  public void testCanOpenSlowLoadingPage() {
     selenium.open("../tests/html/test_page.slow.html");
     verifyTrue(selenium.getLocation().matches("^[\\s\\S]*/tests/html/test_page\\.slow\\.html$"));
     verifyEquals(selenium.getTitle(), "Slow Loading Page");
@@ -48,4 +51,5 @@ public class TestOpen extends InternalSelenseTestBase {
     selenium.open("../tests/html/test_open.html");
     selenium.open("../tests/html/test_open.html");
   }
+
 }
