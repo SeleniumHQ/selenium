5c0f4a0378b001556236e9178d36930f273244d0
Jim Evans <james.h.evans.jr@gmail.com>
2013-04-11 20:59:36 -0400

    Corrected logic error in reading TabProcGrowth registry value.
    The TabProcGrowth registry value can be a DWORD or a string. If it is a
    string, it can be a numeric string (e.g., "0"), and still be treated by
    Internet Explorer as if it were an actual numeric value. This change to
    BrowserFactory::GetRegistryValue() correctly handles both cases,
    coercing the value to a string type for comparison.

--- cpp/IEDriver/BrowserFactory.cpp
+++ cpp/IEDriver/BrowserFactory.cpp
@@ -686,6 +686,7 @@ bool BrowserFactory::GetRegistryValue(const HKEY root_key,
 
   bool value_retrieved = false;
   DWORD required_buffer_size;
+  DWORD value_type;
   HKEY key_handle;
   long registry_call_result = ::RegOpenKeyEx(root_key,
                                              subkey.c_str(),
@@ -696,22 +697,47 @@ bool BrowserFactory::GetRegistryValue(const HKEY root_key,
     registry_call_result = ::RegQueryValueEx(key_handle,
                                              value_name.c_str(),
                                              NULL,
-                                             NULL,
+                                             &value_type,
                                              NULL,
                                              &required_buffer_size);
     if (ERROR_SUCCESS == registry_call_result) {
-      std::vector<TCHAR> value_buffer(required_buffer_size);
-      DWORD value_type(0);
-      registry_call_result = ::RegQueryValueEx(key_handle,
-                                               value_name.c_str(),
-                                               NULL,
-                                               &value_type,
-                                               reinterpret_cast<LPBYTE>(&value_buffer[0]),
-                                               &required_buffer_size);
-      if (ERROR_SUCCESS == registry_call_result) {
-        *value = &value_buffer[0];
-        value_retrieved = true;
+      if (value_type == REG_SZ || value_type == REG_EXPAND_SZ || value_type == REG_MULTI_SZ) {
+        std::vector<wchar_t> value_buffer(required_buffer_size);
+        registry_call_result = ::RegQueryValueEx(key_handle,
+                                                value_name.c_str(),
+                                                NULL,
+                                                &value_type,
+                                                reinterpret_cast<LPBYTE>(&value_buffer[0]),
+                                                &required_buffer_size);
+        if (ERROR_SUCCESS == registry_call_result) {
+          *value = &value_buffer[0];
+          value_retrieved = true;
+        }
+      } else if (value_type == REG_DWORD) {
+        DWORD numeric_value = 0;
+        registry_call_result = ::RegQueryValueEx(key_handle,
+                                                 value_name.c_str(),
+                                                 NULL,
+                                                 &value_type,
+                                                 reinterpret_cast<LPBYTE>(&numeric_value),
+                                                 &required_buffer_size);
+        if (ERROR_SUCCESS == registry_call_result) {
+          // Coerce the numeric value to a string to return back.
+          // Assume 10 characters will be enough to hold the size
+          // of the value.
+          std::vector<wchar_t> numeric_value_buffer(10);
+          _ltow_s(numeric_value, &numeric_value_buffer[0], numeric_value_buffer.size(), 10);
+          *value = &numeric_value_buffer[0];
+          value_retrieved = true;
+        }
       } else {
+        LOG(WARN) << "Unexpected value type of " << value_type
+                  << " for RegQueryValueEx was found for value with name "
+                  << LOGWSTRING(value_name) << " in subkey "
+                  << LOGWSTRING(subkey) << " in hive "
+                  << root_key_description;
+      }
+      if (ERROR_SUCCESS != registry_call_result) {
         LOG(WARN) << "RegQueryValueEx failed with error code "
                   << registry_call_result << " retrieving value with name "
                   << LOGWSTRING(value_name) << " in subkey "
