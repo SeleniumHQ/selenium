f122b5ee932c5f012f5c7817f578f7597957a7e6
Alexei Barantsev <barancev@gmail.com>
2013-05-28 15:26:35 +0400

    Updating TestNG to 6.8.5

--- rake-tasks/crazy_fun/mappings/java.rb
+++ rake-tasks/crazy_fun/mappings/java.rb
@@ -67,7 +67,7 @@ module CrazyFunJava
       :classpath => 'third_party/java/jarjar/jarjar-1.4.jar'
       ant.taskdef :resource  => 'testngtasks' do |t|
         t.classpath do |cp|
-          cp.pathelement :location => 'third_party/java/testng/testng-6.8.jar'
+          cp.pathelement :location => 'third_party/java/testng/testng-6.8.5.jar'
           cp.pathelement :location => 'third_party/java/jcommander/jcommander-1.29.jar'
           cp.pathelement :location => 'third_party/java/beanshell/bsh-1.3.0.jar'
         end
