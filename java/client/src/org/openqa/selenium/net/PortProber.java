812152e7951621ce72439003ca79f5149bfd5d32
Piotr Witoslawski <pwitoslawski@opera.com>
2013-06-04 16:16:27 +0200

    Allow PortProber to look for free ports in a given range
    Signed-off-by: Andreas Tolf Tolfsen <andreastt@opera.com>

--- java/client/src/org/openqa/selenium/net/PortProber.java
+++ java/client/src/org/openqa/selenium/net/PortProber.java
@@ -1,5 +1,6 @@
 /*
-Copyright 2010 Selenium committers
+Copyright 2013 Software Freedom Conservancy
+Copyright 2010-2013 Selenium committers
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,7 +13,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- */
+*/
 
 package org.openqa.selenium.net;
 
@@ -33,6 +34,8 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 @SuppressWarnings({"UtilityClass"})
 public class PortProber {
 
+  public static final int FIND_FREE_PORT_RETRIES = 5;
+
   private static final Random random = new Random();
   private static final EphemeralPortRangeDetector ephemeralRangeDetector;
 
@@ -40,17 +43,34 @@ public class PortProber {
     final Platform current = Platform.getCurrent();
 
     if (current.is(Platform.LINUX)) {
-       ephemeralRangeDetector = LinuxEphemeralPortRangeDetector.getInstance();
-     } else if (current.is(Platform.XP)){
-       ephemeralRangeDetector = new OlderWindowsVersionEphemeralPortDetector();
+      ephemeralRangeDetector = LinuxEphemeralPortRangeDetector.getInstance();
+    } else if (current.is(Platform.XP)) {
+      ephemeralRangeDetector = new OlderWindowsVersionEphemeralPortDetector();
     } else {
-       ephemeralRangeDetector = new FixedIANAPortRange();
+      ephemeralRangeDetector = new FixedIANAPortRange();
     }
   }
 
+  /**
+   * Find a free port that is within a probable free range.
+   *
+   * @return a free port number
+   * @throws RuntimeException if unable to find a free port
+   */
   public static int findFreePort() {
-    for (int i = 0; i < 5; i++) {
-      int seedPort = createAcceptablePort();
+    return findFreePort(getNonEphemeralRange());
+  }
+
+  /**
+   * Find a free port that is within the given port range.
+   *
+   * @param range the range of ports (inclusively) to search for free ports on
+   * @return a free port number
+   * @throws RuntimeException if unable to find a free port
+   */
+  public static int findFreePort(PortRange range) {
+    for (int i = 0; i < FIND_FREE_PORT_RETRIES; i++) {
+      int seedPort = getRandomPortFromRange(range);
       int suggestedPort = checkPortIsFree(seedPort);
       if (suggestedPort != -1) {
         return suggestedPort;
@@ -61,7 +81,6 @@ public class PortProber {
 
   public static Callable<Integer> freeLocalPort(final int port) {
     return new Callable<Integer>() {
-
       public Integer call()
           throws Exception {
         if (checkPortIsFree(port) != -1) {
@@ -73,43 +92,46 @@ public class PortProber {
   }
 
   /**
-   * Returns a port that is within a probable free range. <p/> Based on the ports in
-   * http://en.wikipedia.org/wiki/Ephemeral_ports, this method stays away from all well-known
-   * ephemeral port ranges, since they can arbitrarily race with the operating system in
-   * allocations. Due to the port-greedy nature of selenium this happens fairly frequently.
-   * Staying within the known safe range increases the probability tests will run green quite
-   * significantly.
+   * Returns a port that is within a probable free range.
+   *
+   * <p /> Based on the ports in http://en.wikipedia.org/wiki/Ephemeral_ports, this method stays
+   * away from all well-known ephemeral port ranges, since they can arbitrarily race with the
+   * operating system in allocations.  Due to the port-greedy nature of selenium this happens fairly
+   * frequently. Staying within the known safe range increases the probability tests will run green
+   * quite significantly.
    *
    * @return a random port number
    */
-  private static int createAcceptablePort() {
-      synchronized (random) {
+  private static PortRange getNonEphemeralRange() {
+    final int firstPort;
+    final int lastPort;
 
+    int freeAbove = 65535 - ephemeralRangeDetector.getHighestEphemeralPort();
+    int freeBelow = Math.max(0, ephemeralRangeDetector.getLowestEphemeralPort() - 1024);
 
-          final int FIRST_PORT;
-          final int LAST_PORT;
+    if (freeAbove > freeBelow) {
+      firstPort = ephemeralRangeDetector.getHighestEphemeralPort();
+      lastPort = 65535;
+    } else {
+      firstPort = 1024;
+      lastPort = ephemeralRangeDetector.getLowestEphemeralPort();
+    }
 
-          int free_above = 65535 - ephemeralRangeDetector.getHighestEphemeralPort();
-          int free_below = Math.max(0, ephemeralRangeDetector.getLowestEphemeralPort() - 1024);
+    if (firstPort == lastPort) {
+      return new PortRange(firstPort, firstPort);
+    }
+    if (firstPort > lastPort) {
+      throw new UnsupportedOperationException("Could not find ephemeral port to use");
+    }
+    return new PortRange(firstPort, lastPort);
+  }
 
-          if (free_above > free_below) {
-              FIRST_PORT = ephemeralRangeDetector.getHighestEphemeralPort();
-              LAST_PORT = 65535;
-          } else {
-              FIRST_PORT = 1024;
-              LAST_PORT = ephemeralRangeDetector.getLowestEphemeralPort();
-          }
-
-          if (FIRST_PORT == LAST_PORT) {
-              return FIRST_PORT;
-          }
-          if (FIRST_PORT > LAST_PORT) {
-              throw new UnsupportedOperationException("Could not find ephemeral port to use");
-          }
-          final int randomInt = random.nextInt();
-          final int portWithoutOffset = Math.abs(randomInt % (LAST_PORT - FIRST_PORT + 1));
-          return portWithoutOffset + FIRST_PORT;
-      }
+  private static int getRandomPortFromRange(PortRange range) {
+    synchronized (random) {
+      final int randomInt = random.nextInt();
+      final int offset = Math.abs(randomInt % (range.getEnd() - range.getStart() + 1));
+      return randomInt + offset;
+    }
   }
 
   private static int checkPortIsFree(int port) {
@@ -140,7 +162,7 @@ public class PortProber {
         socket.close();
         return true;
       } catch (ConnectException e) {
-        // Ignore this
+        // ignore this
       } catch (UnknownHostException e) {
         throw new RuntimeException(e);
       } catch (IOException e) {
@@ -150,4 +172,26 @@ public class PortProber {
 
     return false;
   }
+
+  /**
+   * Represents a port range, where the last port is inclusive.
+   */
+  public static class PortRange {
+    private int start;
+    private int end;
+
+    public int getStart() {
+      return start;
+    }
+
+    public int getEnd() {
+      return end;
+    }
+
+    public PortRange(int start, int end) {
+      this.start = start;
+      this.end = end;
+    }
+  }
+
 }
