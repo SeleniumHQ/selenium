08a55ce06c965f0bedf881f718f1d4cb7abf871e
Alexander Bayandin <a.bayandin@gmail.com>
2013-07-25 02:38:31 -0700

    Clear _actions after performing action_chains
    This will make possible to reuse `ActionChains` object after perform  for other actions wihout creating a new object.
    
    case:
    ```
    ac = ActionChains(driver)
    ac.move_to_element(some_element).click().perform()
    # ac._actions contains moving and clicking
    ac.send_keys(some_keys).perform()
    # ac._actions contains moving, clicking and sending keys, but I expect, that it contain only sending keys action.
    ```
    
    Another solution is to add a new method `clear` witch will be clear self._actions list.

--- py/selenium/webdriver/common/action_chains.py
+++ py/selenium/webdriver/common/action_chains.py
@@ -42,6 +42,7 @@ class ActionChains(object):
         """
         for action in self._actions:
             action()
+        self._actions = []
 
     def click(self, on_element=None):
         """
