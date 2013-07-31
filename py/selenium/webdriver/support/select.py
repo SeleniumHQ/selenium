26dbaa260db1827c28cb900644e03eaf41f64521
Alexandr Savchuk <a.u.savchuk@gmail.com>
2013-07-31 08:29:29 +0400

    Fixes misprint in condition in Select.select_by_index method.
    It will fix case when selection is performed by index at multiple select element.

--- py/selenium/webdriver/support/select.py
+++ py/selenium/webdriver/support/select.py
@@ -90,13 +90,14 @@ class Select:
             - index - The option at this index will be selected 
            """
         match = str(index)
+        matched = False
         for opt in self.options:
             if opt.get_attribute("index") == match:
                 self._setSelected(opt)
                 if not self.is_multiple:
                     return
-                match = True
-        if match != True:
+                matched = True
+        if not matched:
             raise NoSuchElementException("Could not locate element with index %d" % index)
 
     def select_by_visible_text(self, text):
