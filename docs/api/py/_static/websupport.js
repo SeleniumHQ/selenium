c7afeacf43122340881f98dc3deca81fefd8030e
Luke Inman-Semerau <luke.semerau@gmail.com>
2013-08-07 13:23:13 -0700

    updating pydocs for 2.34

--- docs/api/py/_static/websupport.js
+++ docs/api/py/_static/websupport.js
@@ -4,7 +4,7 @@
  *
  * sphinx.websupport utilties for all documentation.
  *
- * :copyright: Copyright 2007-2011 by the Sphinx team, see AUTHORS.
+ * :copyright: Copyright 2007-2013 by the Sphinx team, see AUTHORS.
  * :license: BSD, see LICENSE for details.
  *
  */
