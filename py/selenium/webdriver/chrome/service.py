aa67ba6b0cb9856a24f4ae223c4d91566e2e415c
Luke Inman-Semerau <luke.semerau@gmail.com>
2013-08-29 10:43:30 -0700

    chromedriver download is not in the selenium GC project. Updating python exception message

--- py/selenium/webdriver/chrome/service.py
+++ py/selenium/webdriver/chrome/service.py
@@ -64,7 +64,7 @@ class Service(object):
         except:
             raise WebDriverException(
                 "ChromeDriver executable needs to be available in the path. \
-                Please download from http://code.google.com/p/selenium/downloads/list\
+                Please download from http://code.google.com/p/chromedriver/downloads/list\
                 and read up at http://code.google.com/p/selenium/wiki/ChromeDriver")
         count = 0
         while not utils.is_connectable(self.port):
