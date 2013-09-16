bb0f61e2a0ba69d9357068b769b6553b4def3205
Alexei Barantsev <barancev@gmail.com>
2013-09-16 14:45:08 +0400

    Adding a new test (that fails in Firefox)

--- java/client/test/org/openqa/selenium/ClickScrollingTest.java
+++ java/client/test/org/openqa/selenium/ClickScrollingTest.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.fail;
 import static org.openqa.selenium.TestWaiter.waitFor;
 import static org.openqa.selenium.testing.Ignore.Driver.ANDROID;
 import static org.openqa.selenium.testing.Ignore.Driver.CHROME;
+import static org.openqa.selenium.testing.Ignore.Driver.FIREFOX;
 import static org.openqa.selenium.testing.Ignore.Driver.HTMLUNIT;
 import static org.openqa.selenium.testing.Ignore.Driver.IE;
 import static org.openqa.selenium.testing.Ignore.Driver.IPHONE;
@@ -257,5 +258,20 @@ public class ClickScrollingTest extends JUnit4TestBase {
   private long getScrollTop() {
     return (Long)((JavascriptExecutor)driver).executeScript("return document.body.scrollTop;");
   }
-  
+
+  @Test
+  @Ignore(value = {FIREFOX, OPERA, IPHONE, SAFARI, MARIONETTE},
+          reason = "Firefox: fails, others: not tested")
+  public void testShouldBeAbleToClickElementInATallFrame() {
+    try {
+      driver.get(appServer.whereIs("scrolling_tests/page_with_tall_frame.html"));
+      driver.switchTo().frame("tall_frame");
+      WebElement element = driver.findElement(By.name("checkbox"));
+      element.click();
+      assertTrue(element.isSelected());
+    } finally {
+      driver.switchTo().defaultContent();
+    }
+  }
+
 }
