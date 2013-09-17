07d5fcf72f39821c19412b568779a1f6acbf449f
Alexei Barantsev <barancev@gmail.com>
2013-09-17 18:37:48 +0400

    Cleaning imports

--- java/client/test/org/openqa/selenium/ClickScrollingTest.java
+++ java/client/test/org/openqa/selenium/ClickScrollingTest.java
@@ -33,7 +33,6 @@ import static org.junit.Assert.fail;
 import static org.openqa.selenium.TestWaiter.waitFor;
 import static org.openqa.selenium.testing.Ignore.Driver.ANDROID;
 import static org.openqa.selenium.testing.Ignore.Driver.CHROME;
-import static org.openqa.selenium.testing.Ignore.Driver.FIREFOX;
 import static org.openqa.selenium.testing.Ignore.Driver.HTMLUNIT;
 import static org.openqa.selenium.testing.Ignore.Driver.IE;
 import static org.openqa.selenium.testing.Ignore.Driver.IPHONE;
