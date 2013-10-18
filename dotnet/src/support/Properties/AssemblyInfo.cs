2730abde37c8ebc02dfa7fb3249a0be5db9419c9
Jim Evans <james.h.evans.jr@gmail.com>
2013-10-18 12:25:33 -0400

    Updating .NET assembly version numbers and CHANGELOG for 2.37.0 release

--- dotnet/src/support/Properties/AssemblyInfo.cs
+++ dotnet/src/support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.36.0.0")]
-[assembly: AssemblyFileVersion("2.36.0.0")]
+[assembly: AssemblyVersion("2.37.0.0")]
+[assembly: AssemblyFileVersion("2.37.0.0")]
