7e101409161b58b6192dec3dde6acf459dc7d825
Jari Bakken <jari.bakken@gmail.com>
2013-10-21 16:05:49 +0200

    Expose the logging API (beta, subject to change) in the Ruby client
        driver.manage.logs #=> Selenium::WebDriver::Logs

--- rb/spec/integration/selenium/webdriver/options_spec.rb
+++ rb/spec/integration/selenium/webdriver/options_spec.rb
@@ -1,46 +1,77 @@
 require File.expand_path("../spec_helper", __FILE__)
+require 'pry'
 
-describe "Options" do
-  describe "cookie management" do
-    it "should get all" do
-      driver.navigate.to url_for("xhtmlTest.html")
-      driver.manage.add_cookie :name => "foo", :value => "bar"
+module Selenium
+  module WebDriver
+    describe Options do
 
-      cookies = driver.manage.all_cookies
+      describe 'logs' do
+        compliant_on :driver => [:firefox] do
+          it 'can fetch available log types' do
+            driver.manage.logs.available_types.should == [:browser, :driver]
+          end
 
-      cookies.should have(1).things
-      cookies.first[:name].should == "foo"
-      cookies.first[:value].should == "bar"
-    end
+          it 'can get the browser log' do
+            driver.navigate.to url_for("simpleTest.html")
 
-    it "should delete one" do
-      driver.navigate.to url_for("xhtmlTest.html")
+            entries = driver.manage.logs.get(:browser)
+            entries.should_not be_empty
+            entries.first.should be_kind_of(LogEntry)
+          end
 
-      driver.manage.add_cookie :name => "foo", :value => "bar"
-      driver.manage.delete_cookie("foo")
-    end
+          it 'can get the driver log' do
+            driver.navigate.to url_for("simpleTest.html")
 
-    it "should delete all" do
-      driver.navigate.to url_for("xhtmlTest.html")
-
-      driver.manage.add_cookie :name => "foo", :value => "bar"
-      driver.manage.delete_all_cookies
-      driver.manage.all_cookies.should be_empty
-    end
-
-    not_compliant_on :browser => [:ie, :android, :iphone, :safari] do
-      it "should use DateTime for expires" do
-        driver.navigate.to url_for("xhtmlTest.html")
-
-        expected = DateTime.new(2039)
-        driver.manage.add_cookie :name    => "foo",
-        :value   => "bar",
-        :expires => expected
-
-        actual = driver.manage.cookie_named("foo")[:expires]
-        actual.should be_kind_of(DateTime)
-        actual.should == expected
+            entries = driver.manage.logs.get(:driver)
+            entries.should_not be_empty
+            entries.first.should be_kind_of(LogEntry)
+          end
+        end
       end
+
+      describe "cookie management" do
+        it "should get all" do
+          driver.navigate.to url_for("xhtmlTest.html")
+          driver.manage.add_cookie :name => "foo", :value => "bar"
+
+          cookies = driver.manage.all_cookies
+
+          cookies.should have(1).things
+          cookies.first[:name].should == "foo"
+          cookies.first[:value].should == "bar"
+        end
+
+        it "should delete one" do
+          driver.navigate.to url_for("xhtmlTest.html")
+
+          driver.manage.add_cookie :name => "foo", :value => "bar"
+          driver.manage.delete_cookie("foo")
+        end
+
+        it "should delete all" do
+          driver.navigate.to url_for("xhtmlTest.html")
+
+          driver.manage.add_cookie :name => "foo", :value => "bar"
+          driver.manage.delete_all_cookies
+          driver.manage.all_cookies.should be_empty
+        end
+
+        not_compliant_on :browser => [:ie, :android, :iphone, :safari] do
+          it "should use DateTime for expires" do
+            driver.navigate.to url_for("xhtmlTest.html")
+
+            expected = DateTime.new(2039)
+            driver.manage.add_cookie :name => "foo",
+                                     :value   => "bar",
+                                     :expires => expected
+
+            actual = driver.manage.cookie_named("foo")[:expires]
+            actual.should be_kind_of(DateTime)
+            actual.should == expected
+          end
+        end
+      end
+
     end
   end
 end
---
+++ rb/spec/unit/selenium/webdriver/log_entry_spec.rb
@@ -0,0 +1,11 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+module Selenium
+  module WebDriver
+    describe LogEntry do
+      it 'has specs' do
+        pending
+      end
+    end
+  end
+end
