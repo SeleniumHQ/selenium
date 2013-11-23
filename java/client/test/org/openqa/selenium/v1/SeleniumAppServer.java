bf7ea9d3574d3b4f3e689c3582309ee232466f9b
Jason Leyba <jmleyba@gmail.com>
2013-11-22 21:48:58 -0800

    Fix SeleniumAppServer

--- java/client/test/org/openqa/selenium/v1/SeleniumAppServer.java
+++ java/client/test/org/openqa/selenium/v1/SeleniumAppServer.java
@@ -20,6 +20,7 @@ import java.io.File;
 
 import org.openqa.selenium.environment.webserver.Jetty7AppServer;
 import org.openqa.selenium.testing.InProject;
+import org.seleniumhq.jetty7.servlet.ServletContextHandler;
 import org.seleniumhq.jetty7.webapp.WebAppContext;
 
 public class SeleniumAppServer extends Jetty7AppServer {
@@ -28,8 +29,8 @@ public class SeleniumAppServer extends Jetty7AppServer {
 
   public SeleniumAppServer() {
     super();
-    WebAppContext rcWebApp = addWebApplication(RC_CONTEXT_PATH, findRootOfRcTestPages());
-    addServlet(rcWebApp, "Cached Content", "/cachedContentTest", CachedContentServlet.class);
+    ServletContextHandler context = addResourceHandler(RC_CONTEXT_PATH, findRootOfRcTestPages());
+    addServlet(context, "/cachedContentTest", CachedContentServlet.class);
   }
 
   protected File findRootOfRcTestPages() {
