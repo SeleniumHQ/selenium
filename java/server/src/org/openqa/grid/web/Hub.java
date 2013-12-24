71c5e231f442e782196157c8c4677593b0912389
Alexei Barantsev <barancev@gmail.com>
2013-12-24 23:41:33 +0400

    Restricting the host where grid hub is listening if -host option is specified. Fixes issue 4589

--- java/server/src/org/openqa/grid/web/Hub.java
+++ java/server/src/org/openqa/grid/web/Hub.java
@@ -54,6 +54,7 @@ public class Hub {
 
   private final int port;
   private final String host;
+  private final boolean isHostRestricted;
   private final Registry registry;
   private final Map<String, Class<? extends Servlet>> extraServlet = Maps.newHashMap();
 
@@ -77,9 +78,11 @@ public class Hub {
 
     if (config.getHost() != null) {
       host = config.getHost();
+      isHostRestricted = true;
     } else {
       NetworkUtils utils = new NetworkUtils();
       host = utils.getIp4NonLoopbackAddressOfThisMachine().getHostAddress();
+      isHostRestricted = false;
     }
     this.port = config.getPort();
 
@@ -101,6 +104,9 @@ public class Hub {
       server = new Server();
       SocketConnector socketListener = new SocketConnector();
       socketListener.setMaxIdleTime(60000);
+      if (isHostRestricted) {
+        socketListener.setHost(host);
+      }
       socketListener.setPort(port);
       server.addConnector(socketListener);
 
