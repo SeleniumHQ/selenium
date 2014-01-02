878cd6263850fd2239134556317ce049c0c3f175
David Lai <david@wiredrive.com>
2014-01-02 14:20:33 -0800

    Undo changes.  As per discussion, we’ll leave as is and to update documentation instead.

--- py/selenium/webdriver/common/desired_capabilities.py
+++ py/selenium/webdriver/common/desired_capabilities.py
@@ -17,10 +17,6 @@
 The Desired Capabilities implementation.
 """
 
-class __ClassProperty__(property):
-    def __get__(self, cls, owner):
-        return self.fget.__get__(None, owner)()
-
 class DesiredCapabilities(object):
     """
     Set of supported desired capabilities.
@@ -47,138 +43,79 @@ class DesiredCapabilities(object):
 
     """
 
-    __FIREFOX__ = {
+    FIREFOX = {
         "browserName": "firefox",
         "version": "",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def FIREFOX(cls):
-        return cls.__FIREFOX__.copy()
-
-
-    __INTERNETEXPLORER__ = {
+    INTERNETEXPLORER = {
         "browserName": "internet explorer",
         "version": "",
         "platform": "WINDOWS",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def INTERNETEXPLORER(cls):
-        return cls.__FIREFOX__.copy()
-
-
-    __CHROME__ = {
+    CHROME = {
         "browserName": "chrome",
         "version": "",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def CHROME(cls):
-        return cls.__CHROME__.copy()
-
-    __OPERA__ = {
+    OPERA = {
         "browserName": "opera",
         "version": "",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def OPERA(cls):
-        return cls.__OPERA__.copy()
-
-    __SAFARI__ = {
+    SAFARI = {
         "browserName": "safari",
         "version": "",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def SAFARI(cls):
-        return cls.__SAFARI__.copy()
-
-    __HTMLUNIT__ = {
+    HTMLUNIT = {
         "browserName": "htmlunit",
         "version": "",
         "platform": "ANY",
     }
 
-    @__ClassProperty__
-    @classmethod
-    def HTMLUNIT(cls):
-        return cls.__HTMLUNIT__.copy()
-
-    __HTMLUNITWITHJS__ = {
+    HTMLUNITWITHJS = {
         "browserName": "htmlunit",
         "version": "firefox",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def HTMLUNITWITHJS(cls):
-        return cls.__HTMLUNITWITHJS__.copy()
-
-    __IPHONE__ = {
+    IPHONE = {
         "browserName": "iPhone",
         "version": "",
         "platform": "MAC",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def IPHONE(cls):
-        return cls.__IPHONE__.copy()
-
-    __IPAD__ = {
+    IPAD = {
         "browserName": "iPad",
         "version": "",
         "platform": "MAC",
         "javascriptEnabled": True,
     }
 
-    @__ClassProperty__
-    @classmethod
-    def IPAD(cls):
-        return cls.__IPAD__.copy()
-
-    __ANDROID__ = {
+    ANDROID = {
         "browserName": "android",
         "version": "",
         "platform": "ANDROID",
         "javascriptEnabled": True,
     }
-
-    @__ClassProperty__
-    @classmethod
-    def ANDROID(cls):
-        return cls.__ANDROID__.copy()
-
-    __PHANTOMJS__ = {
+    
+    PHANTOMJS = {
         "browserName":"phantomjs",
         "version": "",
         "platform": "ANY",
         "javascriptEnabled": True,
     }
-
-    @__ClassProperty__
-    @classmethod
-    def PHANTOMJS(cls):
-        return cls.__PHANTOMJS__.copy()
-
-
 
