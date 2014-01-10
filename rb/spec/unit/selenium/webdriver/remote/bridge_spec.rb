0067bdf66553aa6ddbf10cd27ef0ae8fe4d2089b
Dylan Lacey <github@dylanlacey.com>
2014-01-09 21:39:43 -0800

    Throw a real error when uploading non-file things in Ruby.

--- rb/spec/unit/selenium/webdriver/remote/bridge_spec.rb
+++ rb/spec/unit/selenium/webdriver/remote/bridge_spec.rb
@@ -8,6 +8,16 @@ module Selenium
         it "raises ArgumentError if passed invalid options" do
           lambda { Bridge.new(:foo => 'bar') }.should raise_error(ArgumentError)
         end
+
+        it "raises WebDriverError if uploading non-files" do
+          request_body = {:sessionId => '11123', :value => {}}.to_json
+          headers = {'Content-Type' => 'application/json'}
+          stub_request(:post, "http://127.0.0.1:4444/wd/hub/session").to_return(
+            :status => 200, :body => request_body, :headers => headers)
+
+          bridge = Bridge.new
+          lambda { bridge.upload("NotAFile")}.should raise_error(Error::WebDriverError)
+        end
       end
 
     end # Remote
