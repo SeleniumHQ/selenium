d8410660efc3628a67fa27064aaa57bdb7db56ea
richardrb <bustamante@chromium.org>
2014-03-03 12:06:42 -0800

    Updating Copyright Information

--- py/selenium/webdriver/chrome/remote_connection.py
+++ py/selenium/webdriver/chrome/remote_connection.py
@@ -1,5 +1,4 @@
-# Copyright 2014 WebDriver committers
-# Copyright 2014 Google Inc.
+# Copyright 2014 Software Freedom Conservancy
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
