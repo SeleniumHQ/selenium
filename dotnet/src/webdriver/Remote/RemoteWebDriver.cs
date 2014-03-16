4c9c197b55db1fee8dc28ab5ec3f88325b0afbd8
martincostello <martin.costello1@gmail.com>
2014-03-16 18:04:56 +0000

    Changed RemoteWebDriver.GetScreenshot() to be virtual.

--- dotnet/src/webdriver/Remote/RemoteWebDriver.cs
+++ dotnet/src/webdriver/Remote/RemoteWebDriver.cs
@@ -709,7 +709,7 @@ namespace OpenQA.Selenium.Remote
         /// Gets a <see cref="Screenshot"/> object representing the image of the page on the screen.
         /// </summary>
         /// <returns>A <see cref="Screenshot"/> object containing the image.</returns>
-        public Screenshot GetScreenshot()
+        public virtual Screenshot GetScreenshot()
         {
             // Get the screenshot as base64.
             Response screenshotResponse = Execute(DriverCommand.Screenshot, null);
