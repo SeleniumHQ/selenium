3df0acdd2cde1ab39d2c3f1d0e174d6496b038ae
Alex Henrie <alexhenrie24@gmail.com>
2014-07-30 12:30:11 -0600

    Remove unused import subprocess.PIPE

--- py/selenium/webdriver/opera/service.py
+++ py/selenium/webdriver/opera/service.py
@@ -15,7 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import subprocess
-from subprocess import PIPE
 import time
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common import utils
