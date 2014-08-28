SeleniumHQ/selenium

commit 6a7bb83aaea4a69d6de03caddb81c9a7eb11c4b7

On Behalf of Jason Morrison (jasonm): when using desired capabilities and chrome options together, attempt to merge rather than squash.
Fixes Issue #7389

py/selenium/webdriver/chrome/webdriver.py
@@ -45,14 +45,16 @@ class WebDriver(RemoteWebDriver):
          - chrome_options: this takes an instance of ChromeOptions
         """
         if chrome_options is None:
-            options = Options()
+            if desired_capabilities is None:
+                desired_capabilities = Options().to_capabilities()
+            else:
+                # desired_capabilities stays as passed
+                pass
         else:
-            options = chrome_options
-
-        if desired_capabilities is not None:
-          desired_capabilities.update(options.to_capabilities())
-        else:
-          desired_capabilities = options.to_capabilities()
+            if desired_capabilities is None:
+                desired_capabilities = options.to_capabilities()
+            else:
+                desired_capabilities.update(options.to_capabilities())
 
         self.service = Service(executable_path, port=port,
             service_args=service_args, log_path=service_log_path)

commit df140408a58ac6d45fe56ab207c03ab4ef86174a

Fixing NameError: global name 'options' is not defined.
if chrome_options is not None:

...
py/selenium/webdriver/chrome/webdriver.py", line 55, in __init__
   desired_capabilities.update(options.to_capabilities())
NameError: global name 'options' is not defined

Regression was introduced in 6a7bb83aaea4a69d6de03caddb81c9a7eb11c4b7

py/selenium/webdriver/chrome/webdriver.py
@@ -50,9 +50,9 @@ class WebDriver(RemoteWebDriver):
                 desired_capabilities = Options().to_capabilities()
         else:
             if desired_capabilities is None:
-                desired_capabilities = options.to_capabilities()
+                desired_capabilities = chrome_options.to_capabilities()
             else:
-                desired_capabilities.update(options.to_capabilities())
+                desired_capabilities.update(chrome_options.to_capabilities())
 
         self.service = Service(executable_path, port=port,
             service_args=service_args, log_path=service_log_path)