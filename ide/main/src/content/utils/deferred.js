50039c601c8a7f8338f6db35d799851ac1660eee
Samit Badle <Samit.Badle@gmail.com>
2014-09-14 17:19:00 +0200

    Making deferred.js available globally and adding a new http library also available globally

--- ide/main/src/content/utils/deferred.js
+++ ide/main/src/content/utils/deferred.js
@@ -1,4 +1,23 @@
-//Loosely modeled on jQuery's Deferred and
+/*
+ * Copyright 2014 Samit Badle
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Partial implementation of promises
+ * Loosely modeled on jQuery's Deferred
+ */
 function Deferred(ctor) {
   var that = this, state = Deferred.PENDING, done = [], fail = [], argsValue = null;
 
