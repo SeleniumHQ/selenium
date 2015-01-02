ef9dc8d32422c90228fea0dcd46424909a9b62f6
Jason Leyba <jmleyba@gmail.com>
2015-01-02 11:05:10 -0800

    Fix tests by using the proper accessor for the environment's current browser.

--- javascript/node/selenium-webdriver/test/window_test.js
+++ javascript/node/selenium-webdriver/test/window_test.js
@@ -52,7 +52,7 @@ test.suite(function(env) {
       driver.manage().window().setPosition(position.x + 10, position.y + 10);
 
       // For phantomjs, setPosition is a no-op and the "window" stays at (0, 0)
-      if (env.browser === Browser.PHANTOM_JS) {
+      if (env.currentBrowser() === Browser.PHANTOM_JS) {
         driver.manage().window().getPosition().then(function(position) {
           assert(position.x).equalTo(0);
           assert(position.y).equalTo(0);
@@ -71,7 +71,7 @@ test.suite(function(env) {
       driver.manage().window().setPosition(position.x + 10, position.y + 10);
 
       // For phantomjs, setPosition is a no-op and the "window" stays at (0, 0)
-      if (env.browser === Browser.PHANTOM_JS) {
+      if (env.currentBrowser() === Browser.PHANTOM_JS) {
         driver.manage().window().getPosition().then(function(position) {
           assert(position.x).equalTo(0);
           assert(position.y).equalTo(0);
