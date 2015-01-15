2a8d4830b51cdbc6bd5ffd5926ed8a2d7d9284bb
timmartin19 <tim.martin@vertical-knowledge.com>
2015-01-15 12:00:24 -0500

    Cleaned up setup.py
    Also changed how version is declared to make it cleaner and accessible
    throughout the program

--- py/setup.py
+++ py/setup.py
@@ -23,61 +23,69 @@ from distutils.command.install import INSTALL_SCHEMES
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
 
-setup_args = {
-    'cmdclass': {'install': install},
-    'name': 'selenium',
-    'version': "2.44.0",
-    'description': 'Python bindings for Selenium',
-    'long_description': open(join(abspath(dirname(__file__)), "py", "README")).read(),
-    'url': 'http://code.google.com/p/selenium/',
-    'classifiers': ['Development Status :: 5 - Production/Stable',
-                    'Intended Audience :: Developers',
-                    'License :: OSI Approved :: Apache Software License',
-                    'Operating System :: POSIX',
-                    'Operating System :: Microsoft :: Windows',
-                    'Operating System :: MacOS :: MacOS X',
-                    'Topic :: Software Development :: Testing',
-                    'Topic :: Software Development :: Libraries',
-                    'Programming Language :: Python',
-                    'Programming Language :: Python :: 2.6',
-                    'Programming Language :: Python :: 2.7',
-                    'Programming Language :: Python :: 3.2',
-                    'Programming Language :: Python :: 3.3'],
-    'package_dir': {
-        'selenium': 'py/selenium',
-        'selenium.common': 'py/selenium/common',
-        'selenium.test': 'py/test',
-        'selenium.test.selenium': 'py/test/selenium',
-        'selenium.webdriver': 'py/selenium/webdriver',
+here = abspath(dirname(__file__))
+
+with open(join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+setup(
+    cmdclass={'install': install},
+    name='selenium',
+    version=version,
+    description='Python bindings for Selenium',
+    long_description=open(join(abspath(dirname(__file__)), "README")).read(),
+    url='http://code.google.com/p/selenium/',
+    install_requires=['six'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3'
+    ],
+    package_dir={
+        'selenium': 'selenium',
+        'selenium.common': 'selenium/common',
+        'selenium.test': 'test',
+        'selenium.test.selenium': 'test/selenium',
+        'selenium.webdriver': 'selenium/webdriver',
     },
-    'packages': ['selenium',
-                 'selenium.common',
-                 'selenium.test',
-                 'selenium.test.selenium',
-                 'selenium.test.selenium.common',
-                 'selenium.test.selenium.webdriver',
-                 'selenium.test.selenium.webdriver.common',
-                 'selenium.test.selenium.webdriver.ie',
-                 'selenium.test.selenium.webdriver.support',
-                 'selenium.webdriver',
-                 'selenium.webdriver.android',
-                 'selenium.webdriver.chrome',
-                 'selenium.webdriver.common',
-                 'selenium.webdriver.common.html5',
-                 'selenium.webdriver.support',
-                 'selenium.webdriver.firefox',
-                 'selenium.webdriver.ie',
-                 'selenium.webdriver.opera',
-                 'selenium.webdriver.phantomjs',
-                 'selenium.webdriver.remote',
-                 'selenium.webdriver.support', ],
-    'package_data': {
+    packages=[
+        'selenium',
+        'selenium.common',
+        'selenium.test',
+        'selenium.test.selenium',
+        'selenium.test.selenium.common',
+        'selenium.test.selenium.webdriver',
+        'selenium.test.selenium.webdriver.common',
+        'selenium.test.selenium.webdriver.ie',
+        'selenium.test.selenium.webdriver.support',
+        'selenium.webdriver',
+        'selenium.webdriver.android',
+        'selenium.webdriver.chrome',
+        'selenium.webdriver.common',
+        'selenium.webdriver.common.html5',
+        'selenium.webdriver.support',
+        'selenium.webdriver.firefox',
+        'selenium.webdriver.ie',
+        'selenium.webdriver.opera',
+        'selenium.webdriver.phantomjs',
+        'selenium.webdriver.remote',
+        'selenium.webdriver.support'
+    ],
+    package_data={
         'selenium.webdriver.firefox': ['*.xpi', 'webdriver_prefs.json'],
     },
-    'data_files': [('selenium/webdriver/firefox/x86', ['py/selenium/webdriver/firefox/x86/x_ignore_nofocus.so']),
-                   ('selenium/webdriver/firefox/amd64', ['py/selenium/webdriver/firefox/amd64/x_ignore_nofocus.so'])],
-    'include_package_data': True,
-    'zip_safe': False
-}
-
-setup(**setup_args)
+    data_files=[('selenium/webdriver/firefox/x86', ['selenium/webdriver/firefox/x86/x_ignore_nofocus.so']),
+                ('selenium/webdriver/firefox/amd64', ['selenium/webdriver/firefox/amd64/x_ignore_nofocus.so'])],
+    include_package_data=True,
+    zip_safe=False
+)
