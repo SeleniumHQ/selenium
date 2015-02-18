9dbf2e8e771c52f8d18594cf5694f9b60ef05734
Pat Tullmann <pt@bebop.co>
2015-02-18 13:09:24 -0800

    webdriverjs mocha: wait for 'done' callbacks to be invoked
    Old code was passing the promise reject method as the test done callback.
    The promise representing the 'it' was always completed immediately.  This
    change makes it so if a test function is expecting a done callback, the
    callback is given control over resolving the promise.
    
    Add tests for checking the "done" callback on an asynchronous 'it'
    invocation causes the system to wait correctly when using the webdriverjs
    wrappers for the mocha 'it'; and tests for the interaction of 'done' and
    control flow.
    
    Several tests were passing the httpserver "start" method as a "before"
    callback.  Since this method takes an optional port number, it has a
    formal parameter, and so the updated 'it' infrastructure assumed these
    functions were async and expected a "done" callback to be passed in.  That
    didn't work.
    
    Plus a smattering of documentation cleanups around the JavaScript code.
    
    Signed-off-by: Jason Leyba <jmleyba@gmail.com>

--- javascript/node/selenium-webdriver/http/index.js
+++ javascript/node/selenium-webdriver/http/index.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Defines a the {@code webdriver.http.Client} for use with
+ * @fileoverview Defines the {@code webdriver.http.Client} for use with
  * NodeJS.
  */
 
