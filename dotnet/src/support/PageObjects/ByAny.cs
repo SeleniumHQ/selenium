e8bd25c201c594f516d331d7500eae1808afd29a
Immanuel Hayden <immanuel.hayden@gmail.com>
2015-03-13 09:29:18 +0100

    support for ByAll (find elements matching all locators) and ByAny (find elements matching any of these locators)

---
+++ dotnet/src/support/PageObjects/ByAny.cs
@@ -0,0 +1,75 @@
+﻿// Copyright 2007-2012 Google Inc.
+// Portions copyright 2012 Software Freedom Conservancy
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace OpenQA.Selenium.Support.PageObjects
+{
+    /// <summary>Mechanism used to locate elements within a document using a series of  lookups. This class will
+    /// find all DOM elements that matches any of the locators in sequence, e.g.</summary>
+    ///  <example><code>driver.findElements(new ByAny(by1, by2))</code>
+    ///  will find all elements that match by1 and then all elements that match by2.
+    ///  This means that the list of elements returned may not be in document order.</example>>
+    public class ByAny : By
+    {
+        private readonly By[] _bys;
+
+        public ByAny(By[] bys)
+        {
+            _bys = bys;
+        }
+
+        public override IWebElement FindElement(ISearchContext context)
+        {
+            var elements = FindElements(context);
+            if (elements.Count == 0)
+            {
+                throw new NoSuchElementException("Cannot locate an element using " + ToString());
+            }
+            return elements[0];
+        }
+
+        public override ReadOnlyCollection<IWebElement> FindElements(ISearchContext context)
+        {
+            var elems = new List<IWebElement>();
+            foreach (var @by in _bys)
+            {
+                elems.AddRange(@by.FindElements(context));
+            }
+
+            return elems.Distinct().ToList().AsReadOnly();
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder("By.all(");
+            stringBuilder.Append("{");
+
+            var first = true;
+            foreach (var by in _bys)
+            {
+                stringBuilder.Append((first ? "" : ",")).Append(by);
+                first = false;
+            }
+            stringBuilder.Append("})");
+            return stringBuilder.ToString();
+        }
+
+    }
+}
