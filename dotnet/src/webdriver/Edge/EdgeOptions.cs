67bfdb74df3f538e218414a43567fa97b444e63b
petruc <petruc@microsoft.com>
2015-05-28 11:59:14 -0700

    Edge browser bindings derived from Chrome bindings

---
+++ dotnet/src/webdriver/Edge/EdgeOptions.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="EdgeOptions.cs" company="Microsoft">
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using OpenQA.Selenium.Remote;
+
+namespace OpenQA.Selenium.Edge
+{
+    /// <summary>
+    /// Class to manage options specific to <see cref="EdgeDriver"/>
+    /// </summary>
+    public class EdgeOptions
+    {
+        /// <summary>
+        /// This is temporary until the MicrosoftWebDriver capabilities are defined
+        /// Returns DesiredCapabilities for Chrome with these options included as
+        /// capabilities. This does not copy the options. Further changes will be
+        /// reflected in the returned capabilities.
+        /// </summary>
+        /// <returns>The DesiredCapabilities for Chrome with these options.</returns>
+        public ICapabilities ToCapabilities()
+        {
+            DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            return capabilities;
+        }
+    }
+}
