dc4b39da9a400f0164444f4b0d8b22bfed72f021
alex.collins <alex.e.c@gmail.com>
2015-06-02 23:25:03 +0100

    Modify SearchContext.findElements to allow implementing classes to return a list of any class to sub-classes WebElement (e.g. MobileElement).

---
+++ java/client/test/org/openqa/selenium/SearchContextTest.java
@@ -0,0 +1,29 @@
+package org.openqa.selenium;
+
+import java.util.List;
+
+// this class just tests we can compile the code
+public class SearchContextTest {
+
+  private interface TestWebElement extends WebElement {
+
+  }
+
+  private interface SubClassingSearchContext extends SearchContext {
+
+    @Override
+    List<WebElement> findElements(By by);
+
+    @Override
+    TestWebElement findElement(By by);
+  }
+
+  private interface SameClassSearchContext extends SearchContext {
+
+    @Override
+    List<WebElement> findElements(By by);
+
+    @Override
+    WebElement findElement(By by);
+  }
+}
