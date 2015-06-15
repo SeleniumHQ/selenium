4499d96d9910d8866bd45f6e5dd47cdc2720f0dc
Alexei Barantsev <barancev@gmail.com>
2015-06-15 19:39:27 +0300

    firefox: Moving WebDriverError from utils.js to error.js

--- javascript/firefox-driver/js/utils.js
+++ javascript/firefox-driver/js/utils.js
@@ -16,7 +16,6 @@
 // under the License.
 
 goog.provide('Utils');
-goog.provide('WebDriverError');
 
 goog.require('WebLoadingListener');
 goog.require('bot.ErrorCode');
@@ -31,62 +30,6 @@ goog.require('goog.log');
 goog.require('goog.string');
 goog.require('goog.style');
 
-
-/**
- * A WebDriver error.
- * @param {!number} code The error code.
- * @param {!string|Error} messageOrError The error message, or another Error to
- *     propagate.
- * @param {!Object=} additional Additional fields bearing useful information.
- * @constructor
- */
-WebDriverError = function(code, messageOrError, additional) {
-  var message;
-  var stack;
-  if (messageOrError instanceof Error) {
-    message = messageOrError.message;
-    stack = messageOrError.stack;
-  } else {
-    message = messageOrError.toString();
-    stack = Error(message).stack.split('\n');
-    stack.shift();
-    stack = stack.join('\n');
-  }
-
-  this.additionalFields = [];
-
-  if (!!additional) {
-    for (var field in additional) {
-      this.additionalFields.push(field);
-      this[field] = additional[field];
-    }
-  }
-
-  /**
-   * This error's status code.
-   * @type {!number}
-   */
-  this.code = code;
-
-  /**
-   * This error's message.
-   * @type {string}
-   */
-  this.message = message;
-
-  /**
-   * Captures a stack trace for when this error was thrown.
-   * @type {string}
-   */
-  this.stack = stack;
-
-  /**
-   * Used to identify this class since instanceof will not work across
-   * component boundaries.
-   * @type {!boolean}
-   */
-  this.isWebDriverError = true;
-};
 
 function notifyOfCloseWindow(windowId) {
   windowId = windowId || 0;
