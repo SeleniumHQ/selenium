ce728ab54b7874337c86b36a7be1e79b08bc417e
Seva Lotoshnikov <vlotoshnikov@gmail.com>
2015-06-17 19:35:32 -0700

    Remove unused imports after 09601a484c5

--- java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
+++ java/client/test/org/openqa/selenium/firefox/internal/ExecutableTest.java
@@ -23,8 +23,6 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Assume;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.openqa.selenium.testing.NeedsLocalEnvironment;
 import org.openqa.selenium.testing.drivers.SauceDriver;
 
