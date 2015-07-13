932b740bfc90f1c6d4d53e1956f65b36a0a90491
petruc <petruc@microsoft.com>
2015-07-13 15:13:27 -0700

    Fix Comment in EdgeOptions for Java bindings

--- java/client/src/org/openqa/selenium/edge/EdgeOptions.java
+++ java/client/src/org/openqa/selenium/edge/EdgeOptions.java
@@ -53,28 +53,28 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class EdgeOptions {
 	
     /**
-	* Key used to store a set of EdgeOptions in a {@link DesiredCapabilities}
-	* object.
-	*/
+     * Key used to store a set of EdgeOptions in a {@link DesiredCapabilities}
+	 * object.
+	 */
 	public static final String CAPABILITY = "edgeOptions";
 	
 	private String pageLoadStrategy;
 	
 	/**
-	* Sets the page load strategy for  Edge
-	* 
-	* Supported values are "normal", "eager" and "none"
-	*/ 
+	 * Sets the page load strategy for  Edge
+	 * 
+	 * Supported values are "normal", "eager" and "none"
+	 */ 
 	public void setPageLoadStrategy(String strategy) {
       this.pageLoadStrategy = checkNotNull(strategy);
 	}
     
 	/**
-	* Converts this instance to its JSON representation.
-	*
-	* @return The JSON representation of the options.
-	* @throws IOException If an error occurred while reading the Safari extension files.
-	*/
+	 * Converts this instance to its JSON representation.
+	 *
+	 * @return The JSON representation of the options.
+	 * @throws IOException If an error occurred while reading the Edge extension files.
+	 */
 	public JsonObject toJson() throws IOException {
 	  JsonObject options = new JsonObject();
 	  if (this.pageLoadStrategy != null)
@@ -101,5 +101,4 @@ public class EdgeOptions {
 	  
       return capabilities;
     }
-
 }
