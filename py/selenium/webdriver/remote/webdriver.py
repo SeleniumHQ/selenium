e605fcda6f85b0000c85d40fd0458ac79a5173c9
AutomatedTester <dburns@mozilla.com>
2015-07-28 15:59:12 +0100

    Update timeout methods to work with W3C dialect of JSON Protocol

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -641,7 +641,11 @@ class WebDriver(object):
         :Usage:
             driver.implicitly_wait(30)
         """
-        self.execute(Command.IMPLICIT_WAIT, {'ms': float(time_to_wait) * 1000})
+        if self.capabilities["marionette"] == True:
+            self.execute(Command.SET_TIMEOUTS,
+                         {'ms': float(time_to_wait) * 1000, 'type':'implicit'})
+        else:
+            self.execute(Command.IMPLICIT_WAIT, {'ms': float(time_to_wait) * 1000})
 
     def set_script_timeout(self, time_to_wait):
         """
@@ -654,8 +658,12 @@ class WebDriver(object):
         :Usage:
             driver.set_script_timeout(30)
         """
-        self.execute(Command.SET_SCRIPT_TIMEOUT,
-            {'ms': float(time_to_wait) * 1000})
+        if self.capabilities["marionette"] == True:
+            self.execute(Command.SET_TIMEOUTS,
+                         {'ms': float(time_to_wait) * 1000, 'type':'script'})
+        else:
+            self.execute(Command.SET_SCRIPT_TIMEOUT,
+                         {'ms': float(time_to_wait) * 1000})
 
     def set_page_load_timeout(self, time_to_wait):
         """
