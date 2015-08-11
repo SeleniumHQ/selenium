47ec81fda714ad39521c48bba0caf6c9d9771935
Jim Evans <james.h.evans.jr@gmail.com>
2015-08-11 10:25:13 -0700

    Adding additional support for Microsoft Edge in .NET bindings tests

---
+++ dotnet/test/edge/EdgeSpecificTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+using OpenQA.Selenium.Environment;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+
+namespace OpenQA.Selenium.Edge
+{
+    [TestFixture]
+    public class EdgeSpecificTests : DriverTestFixture
+    {
+    }
+}
