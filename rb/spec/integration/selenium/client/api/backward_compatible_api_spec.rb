1545015252bd646d6f48bcd15b966dcb9a21d8f0
Alex Rodionov <p0deje@gmail.com>
2015-09-17 10:07:41 +0600

    rb: Remove \t from source. Are we barbarians?

--- rb/spec/integration/selenium/client/api/backward_compatible_api_spec.rb
+++ rb/spec/integration/selenium/client/api/backward_compatible_api_spec.rb
@@ -21,21 +21,21 @@ require File.expand_path(__FILE__ + '/../../spec_helper')
 
 describe "Backward Compatible API" do
   it "provides legacy driver methods" do
-		page.open "http://localhost:4444/selenium-server/org/openqa/selenium/tests/html/test_click_page1.html"
+    page.open "http://localhost:4444/selenium-server/org/openqa/selenium/tests/html/test_click_page1.html"
 
     expect(page.get_title).to eql("Click Page 1")
-		expect(page.get_text("link").index("Click here for next page")).not_to be_nil
+    expect(page.get_text("link").index("Click here for next page")).not_to be_nil
 
-		links = page.get_all_links
-		expect(links.length).to be > 3
-		expect(links[3]).to eql("linkToAnchorOnThisPage")
+    links = page.get_all_links
+    expect(links.length).to be > 3
+    expect(links[3]).to eql("linkToAnchorOnThisPage")
 
-		page.click "link"
-		page.wait_for_page_to_load 5000
-		expect(page.get_location).to match(%r"/selenium-server/org/openqa/selenium/tests/html/test_click_page2.html")
+    page.click "link"
+    page.wait_for_page_to_load 5000
+    expect(page.get_location).to match(%r"/selenium-server/org/openqa/selenium/tests/html/test_click_page2.html")
 
-		page.click "previousPage"
-		page.wait_for_page_to_load 5000
-		expect(page.get_location).to match(%r"/selenium-server/org/openqa/selenium/tests/html/test_click_page1.html")
+    page.click "previousPage"
+    page.wait_for_page_to_load 5000
+    expect(page.get_location).to match(%r"/selenium-server/org/openqa/selenium/tests/html/test_click_page1.html")
   end
 end
