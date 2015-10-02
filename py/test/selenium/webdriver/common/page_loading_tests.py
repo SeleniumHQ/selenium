6ce324157539b3eff9c706a580cf721f28caaa41
AutomatedTester <dburns@mozilla.com>
2015-10-02 21:06:49 +0100

    Disable tests that are not working

--- py/test/selenium/webdriver/common/page_loading_tests.py
+++ py/test/selenium/webdriver/common/page_loading_tests.py
@@ -20,18 +20,19 @@ import pytest
 
 from selenium.webdriver.common.by import By
 
+@pytest.mark.ignore_marionette
 class PageLoadingTests(unittest.TestCase):
 
     def testShouldWaitForDocumentToBeLoaded(self):
         self._loadSimplePage()
-        
+
         self.assertEqual(self.driver.title, "Hello WebDriver")
-  
+
     # Disabled till Java WebServer is used
     #def testShouldFollowRedirectsSentInTheHttpResponseHeaders(self):
     #    self.driver.get(pages.redirectPage);
     #    self.assertEqual(self.driver.title, "We Arrive Here")
-  
+
     # Disabled till the Java WebServer is used
     #def testShouldFollowMetaRedirects(self):
     #    self._loadPage("metaRedirect")
@@ -50,7 +51,7 @@ class PageLoadingTests(unittest.TestCase):
           self.driver.get("http://www.thisurldoesnotexist.comx/")
         except ValueError:
             pass
-    
+
     @pytest.mark.ignore_safari
     def testShouldReturnWhenGettingAUrlThatDoesNotConnect(self):
         # Here's hoping that there's nothing here. There shouldn't be
@@ -59,7 +60,7 @@ class PageLoadingTests(unittest.TestCase):
       #@Ignore({IE, IPHONE, SELENESE})
       #def testShouldBeAbleToLoadAPageWithFramesetsAndWaitUntilAllFramesAreLoaded() {
       #  self.driver.get(pages.framesetPage);
-    
+
       #  self.driver.switchTo().frame(0);
       #  WebElement pageNumber = self.driver.findElement(By.xpath("#span[@id='pageNumber']"));
       #  self.assertEqual((pageNumber.getText().trim(), equalTo("1"));
@@ -129,4 +130,3 @@ class PageLoadingTests(unittest.TestCase):
 
     def _loadPage(self, name):
         self.driver.get(self._pageURL(name))
-
