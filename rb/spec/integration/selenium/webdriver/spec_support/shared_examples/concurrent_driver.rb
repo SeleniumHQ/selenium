5b69f9d810414dab53ec2709cee603c223ebae51
Alex Rodionov <p0deje@gmail.com>
2015-10-15 12:10:20 +0600

    rb: Regression spec for parallel Safari startup

--- rb/spec/integration/selenium/webdriver/spec_support/shared_examples/concurrent_driver.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/shared_examples/concurrent_driver.rb
@@ -1,23 +1,25 @@
 shared_examples_for "driver that can be started concurrently" do |driver_name|
   it "is started sequentially" do
     expect {
-      # start 5 drivers concurrently
-      threads, drivers = [], []
+      Timeout.timeout(45) do
+        # start 5 drivers concurrently
+        threads, drivers = [], []
 
-      5.times do
-        threads << Thread.new do
-          drivers << Selenium::WebDriver.for(driver_name)
+        5.times do
+          threads << Thread.new do
+            drivers << Selenium::WebDriver.for(driver_name)
+          end
         end
-      end
 
-      threads.each do |thread|
-        thread.abort_on_exception = true
-        thread.join
-      end
+        threads.each do |thread|
+          thread.abort_on_exception = true
+          thread.join
+        end
 
-      drivers.each do |driver|
-        driver.title # make any wire call
-        driver.quit
+        drivers.each do |driver|
+          driver.title # make any wire call
+          driver.quit
+        end
       end
     }.not_to raise_error
   end
