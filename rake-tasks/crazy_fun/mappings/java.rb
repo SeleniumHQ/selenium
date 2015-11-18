1f90589af3d4df023cb08d1f7013e4c048ba6ac9
Alexei Barantsev <barancev@gmail.com>
2015-11-18 16:44:49 +0300

    Updating TestNG. Fixes issue #1263

--- rake-tasks/crazy_fun/mappings/java.rb
+++ rake-tasks/crazy_fun/mappings/java.rb
@@ -67,7 +67,7 @@ module CrazyFunJava
       :classpath => 'third_party/java/jarjar/jarjar-1.4.jar'
       ant.taskdef :resource  => 'testngtasks' do |t|
         t.classpath do |cp|
-          cp.pathelement :location => 'third_party/java/testng/testng-6.9.6.jar'
+          cp.pathelement :location => 'third_party/java/testng/testng-6.9.9.jar'
           cp.pathelement :location => 'third_party/java/jcommander/jcommander-1.29.jar'
           cp.pathelement :location => 'third_party/java/beanshell/bsh-2.0b4.jar'
         end
