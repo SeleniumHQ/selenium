c1154cc9bf701aa577ccb4d588670e75f8830ddc
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-01-18 21:23:30 +0000

    Bump python version to 2.49.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ for scheme in INSTALL_SCHEMES.values():
 setup_args = {
     'cmdclass': {'install': install},
     'name': 'selenium',
-    'version': "2.49.0",
+    'version': "2.49.1",
     'description': 'Python bindings for Selenium',
     'long_description': open(join(abspath(dirname(__file__)), "py", "README")).read(),
     'url': 'https://github.com/SeleniumHQ/selenium/',
