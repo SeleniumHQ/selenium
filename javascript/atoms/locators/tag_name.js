c4312488632ad52cb4afa200adacdf89e8b97630
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-01-20 20:42:47 +0000

    Finding tagName with empty string should throw InvalidSelectorException
    We have been returning a NoSuchElementException in single and [] in many
    but "" leads to invalid CSS Selector which will be the standard way to
    search for elements in the future.

--- javascript/atoms/locators/tag_name.js
+++ javascript/atoms/locators/tag_name.js
@@ -17,6 +17,8 @@
 
 goog.provide('bot.locators.tagName');
 
+goog.require('bot.Error');
+goog.require('bot.ErrorCode');
 goog.require('goog.array');
 
 
@@ -29,6 +31,10 @@ goog.require('goog.array');
  *     such element could be found.
  */
 bot.locators.tagName.single = function(target, root) {
+  if (target === "") {
+    throw new bot.Error(bot.ErrorCode.INVALID_SELECTOR_ERROR,
+        'Unable to locate an element with the tagName ""');
+  }
   return root.getElementsByTagName(target)[0] || null;
 };
 
@@ -41,5 +47,9 @@ bot.locators.tagName.single = function(target, root) {
  * @return {goog.array.ArrayLike} All matching elements, or an empty list.
  */
 bot.locators.tagName.many = function(target, root) {
+  if (target === "") {
+    throw new bot.Error(bot.ErrorCode.INVALID_SELECTOR_ERROR,
+        'Unable to locate an element with the tagName ""');
+  }
   return root.getElementsByTagName(target);
 };
