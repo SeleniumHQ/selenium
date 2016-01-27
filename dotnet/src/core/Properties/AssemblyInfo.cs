150d4cc48f20fcefd5a7a17a01685181cddb7a37
Jim Evans <james.h.evans.jr@gmail.com>
2016-01-27 14:54:30 -0800

    Updating .NET version resources and change log for 2.50 release

--- dotnet/src/core/Properties/AssemblyInfo.cs
+++ dotnet/src/core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.49.0.0")]
-[assembly: AssemblyFileVersion("2.49.0.0")]
+[assembly: AssemblyVersion("2.50.0.0")]
+[assembly: AssemblyFileVersion("2.50.0.0")]
--- dotnet/src/support/Properties/AssemblyInfo.cs
+++ dotnet/src/support/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.49.0.0")]
-[assembly: AssemblyFileVersion("2.49.0.0")]
+[assembly: AssemblyVersion("2.50.0.0")]
+[assembly: AssemblyFileVersion("2.50.0.0")]
