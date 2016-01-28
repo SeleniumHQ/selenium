9078ebaf5becd17e6aa60b0d4ee53bb326f7416b
Jim <james.evans@salesforce.com>
2016-01-28 14:18:00 -0500

    Fixing API mismatch in .NET ChromeOptions and rereleasing as 2.50.1

--- dotnet/src/core/Properties/AssemblyInfo.cs
+++ dotnet/src/core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.50.0.0")]
-[assembly: AssemblyFileVersion("2.50.0.0")]
+[assembly: AssemblyVersion("2.50.1.0")]
+[assembly: AssemblyFileVersion("2.50.1.0")]
--- dotnet/src/webdriver/Properties/AssemblyInfo.cs
+++ dotnet/src/webdriver/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.50.0.0")]
-[assembly: AssemblyFileVersion("2.50.0.0")]
+[assembly: AssemblyVersion("2.50.1.0")]
+[assembly: AssemblyFileVersion("2.50.1.0")]
