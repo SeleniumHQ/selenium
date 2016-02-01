ab99cb6cac80bb26a1b36675db2fb8fe04685d29
Alexei Barantsev <barancev@gmail.com>
2016-02-01 11:25:54 +0300

    Python: Fixing error message handling. Fixes issue #1507

--- py/selenium/webdriver/remote/errorhandler.py
+++ py/selenium/webdriver/remote/errorhandler.py
@@ -160,7 +160,7 @@ class ErrorHandler(object):
             if exception_class == ErrorInResponseException:
                 raise exception_class(response, value)
             raise exception_class(value)
-        if message != "" and 'message' in value:
+        if message == "" and 'message' in value:
             message = value['message']
 
         screen = None
