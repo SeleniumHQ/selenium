ee4eb06dd240da69b728465c082dc78bee9b5f72
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-02-01 13:57:24 +0000

    Bump python client versions to 2.50.1

--- py/selenium/__init__.py
+++ py/selenium/__init__.py
@@ -18,4 +18,4 @@
 from selenium import selenium
 
 
-__version__ = "2.50.0"
+__version__ = "2.50.1"
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ for scheme in INSTALL_SCHEMES.values():
 setup_args = {
     'cmdclass': {'install': install},
     'name': 'selenium',
-    'version': "2.50.0",
+    'version': "2.50.1",
     'description': 'Python bindings for Selenium',
     'long_description': open(join(abspath(dirname(__file__)), "py", "README")).read(),
     'url': 'https://github.com/SeleniumHQ/selenium/',
