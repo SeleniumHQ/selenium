c68ace0e522bd05b5051c9d5a609c26be1b96f6e
Alexei Barantsev <barancev@gmail.com>
2016-02-01 13:19:27 +0300

    Python: Fixing error message handling. Fixes issue #1497

--- py/selenium/webdriver/remote/errorhandler.py
+++ py/selenium/webdriver/remote/errorhandler.py
@@ -100,18 +100,20 @@ class ErrorHandler(object):
             if value_json and isinstance(value_json, basestring):
                 import json
                 try:
-                  value = json.loads(value_json)
-                  status = value.get('error', None)
-                  if status is None:
-                      status = value["status"]
-                      try:
-                          message = value["value"]["message"]
-                      except TypeError:
-                          message = None
-                  else:
-                      message = value.get('message', None)
+                    value = json.loads(value_json)
+                    status = value.get('error', None)
+                    if status is None:
+                        status = value["status"]
+                        message = value["value"]
+                        if not isinstance(message, basestring):
+                            try:
+                                message = message['message']
+                            except TypeError:
+                                message = None
+                    else:
+                        message = value.get('message', None)
                 except ValueError:
-                  pass
+                    pass
 
         exception_class = ErrorInResponseException
         if status in ErrorCode.NO_SUCH_ELEMENT:
@@ -191,4 +193,4 @@ class ErrorHandler(object):
         raise exception_class(message, screen, stacktrace)
 
     def _value_or_default(self, obj, key, default):
-      return obj[key] if key in obj else default
+        return obj[key] if key in obj else default
