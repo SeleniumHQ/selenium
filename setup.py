64b76a454e30bae274ddd0e006506d1009ac2ebe
Alexei Barantsev <barancev@gmail.com>
2016-02-03 21:26:41 +0300

    Python: Fixing README file name in setup script

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup_args = {
     'name': 'selenium',
     'version': "2.50.1",
     'description': 'Python bindings for Selenium',
-    'long_description': open(join(abspath(dirname(__file__)), "py", "README")).read(),
+    'long_description': open(join(abspath(dirname(__file__)), "py", "README.rst")).read(),
     'url': 'https://github.com/SeleniumHQ/selenium/',
     'classifiers': ['Development Status :: 5 - Production/Stable',
                    'Intended Audience :: Developers',
