0f1b10b8a55541657125cb6e36145dcf0f1bc83f
Luke Inman-Semerau <luke.semerau@gmail.com>
2016-02-04 09:39:03 -0800

    stop gap to help with elementFromPoint not being completely reliable

--- javascript/firefox-driver/js/utils.js
+++ javascript/firefox-driver/js/utils.js
@@ -723,6 +723,16 @@ Utils.getClickablePoint = function(element) {
       }
       parentElemIter = parentElemIter.parentNode;
     }
+
+    // elementFromPoint does not appear to be reliable. This will catch
+    // other cases where the parent element is found instead.
+    parentElemIter = element.parentNode;
+    while (parentElemIter) {
+      if (parentElemIter == elementAtPoint) {
+        return true;
+      }
+      parentElemIter = parentElemIter.parentNode;
+    }
   };
 
   var rectPointRelativeToView = function(x, y, r) {
