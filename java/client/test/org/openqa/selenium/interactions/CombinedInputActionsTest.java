bb4ba4d0309ee06d8930d1b5bae610296461e3fd
Alexei Barantsev <barancev@gmail.com>
2016-03-03 21:57:47 +0300

    Disabling HtmlUnit tests broken in 2.20

--- java/client/test/org/openqa/selenium/interactions/CombinedInputActionsTest.java
+++ java/client/test/org/openqa/selenium/interactions/CombinedInputActionsTest.java
@@ -353,6 +353,7 @@ public class CombinedInputActionsTest extends JUnit4TestBase {
   @JavascriptEnabled
   @Test
   @Ignore(value = {SAFARI, MARIONETTE}, issues = {4136})
+  @NotYetImplemented(HTMLUNIT) // broken in 2.20
   public void canClickOnASuckerFishStyleMenu() throws InterruptedException {
     driver.get(pages.javascriptPage);
 
@@ -380,6 +381,7 @@ public class CombinedInputActionsTest extends JUnit4TestBase {
   @JavascriptEnabled
   @Test
   @Ignore(value = {SAFARI, MARIONETTE}, issues = {4136})
+  @NotYetImplemented(HTMLUNIT) // broken in 2.20
   public void testCanClickOnSuckerFishMenuItem() throws Exception {
     driver.get(pages.javascriptPage);
 
