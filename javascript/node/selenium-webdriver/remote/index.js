9bcba27be1066be727743bd544df4f4b221b2c12
Jason Leyba <jmleyba@gmail.com>
2016-03-15 14:01:00 -0700

    [js] For consistency with java, the file detector should ignore directory paths.
    Fixes #1814
    
    Signed-off-by: Jason Leyba <jmleyba@gmail.com>

--- javascript/node/selenium-webdriver/remote/index.js
+++ javascript/node/selenium-webdriver/remote/index.js
@@ -381,25 +381,34 @@ SeleniumServer.Options;
  * @final
  */
 class FileDetector extends input.FileDetector {
-  /** @override */
-  handleFile(driver, filePath) {
-    return promise.checkedNodeCall(fs.stat, filePath).then(function(stats) {
+  /**
+   * Prepares a `file` for use with the remote browser. If the provided path
+   * does not reference a normal file (i.e. it does not exist or is a
+   * directory), then the promise returned by this method will be resolved with
+   * the original file path. Otherwise, this method will upload the file to the
+   * remote server, which will return the file's path on the remote system so
+   * it may be referenced in subsequent commands.
+   *
+   * @override
+   */
+  handleFile(driver, file) {
+    return promise.checkedNodeCall(fs.stat, file).then(function(stats) {
       if (stats.isDirectory()) {
-        throw TypeError('Uploading directories is not supported: ' + filePath);
+        return file;  // Not a valid file, return original input.
       }
 
       var zip = new AdmZip();
-      zip.addLocalFile(filePath);
+      zip.addLocalFile(file);
       // Stored compression, see https://en.wikipedia.org/wiki/Zip_(file_format)
       zip.getEntries()[0].header.method = 0;
 
       var command = new cmd.Command(cmd.Name.UPLOAD_FILE)
           .setParameter('file', zip.toBuffer().toString('base64'));
       return driver.schedule(command,
-          'remote.FileDetector.handleFile(' + filePath + ')');
+          'remote.FileDetector.handleFile(' + file + ')');
     }, function(err) {
       if (err.code === 'ENOENT') {
-        return filePath;  // Not a file; return original input.
+        return file;  // Not a file; return original input.
       }
       throw err;
     });
