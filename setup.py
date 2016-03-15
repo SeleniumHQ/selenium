aa404f6baebe1615ef488f3091566bb70dddb5e9
AutomatedTester <david.burns@theautomatedtester.co.uk>
2016-03-15 22:46:16 +0000

    Drop support for Python 3.2

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ setup_args = {
                     'Programming Language :: Python',
                     'Programming Language :: Python :: 2.6',
                     'Programming Language :: Python :: 2.7',
-                    'Programming Language :: Python :: 3.2',
                     'Programming Language :: Python :: 3.3',
-                    'Programming Language :: Python :: 3.4'],
+                    'Programming Language :: Python :: 3.4',
+                    'Programming Language :: Python :: 3.5'],
     'package_dir': {
         'selenium': 'py/selenium',
         'selenium.common': 'py/selenium/common',
