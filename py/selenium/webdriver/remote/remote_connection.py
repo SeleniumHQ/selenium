6aa6e059a45c4ecc0a07a9cf57ece07d4f19d287
Jason Juang <juangj@google.com>
2016-03-28 16:49:55 -0700

    Resolve IPv6 addresses in Python remote WebDriver.
    The RemoteConnection resolves the hostname in the remote server's
    address to an IP in order to avoid the cost of looking up the IP on
    every request. This change enables IPv6 resolution; IPv4 is still
    preferred, so an IPv6 address is only returned in cases where the host
    is not available over IPv4.

--- py/selenium/webdriver/remote/remote_connection.py
+++ py/selenium/webdriver/remote/remote_connection.py
@@ -36,6 +36,52 @@ from . import utils
 LOGGER = logging.getLogger(__name__)
 
 
+def _resolve_ip(host):
+  """Resolve a hostname to an IP, preferring IPv4 addresses.
+
+  We prefer IPv4 so that we don't change behavior from previous IPv4-only
+  implementations, and because some drivers (e.g., FirefoxDriver) do not support
+  IPv6 connections.
+
+  :Args:
+  - host - A hostname.
+
+  :Returns:
+  A single IP address, as a string. If any IPv4 address is found, one is
+  returned. Otherwise, if any IPv6 address is found, one is returned. If
+  neither, then None is returned.
+
+  """
+  try:
+      addrinfos = socket.getaddrinfo(host, None)
+  except socket.gaierror:
+      return None
+
+  ip = None
+  for family, _, _, _, sockaddr in addrinfos:
+      if family == socket.AF_INET:
+          return sockaddr[0]
+      if not ip and family == socket.AF_INET6:
+          ip = sockaddr[0]
+  return ip
+
+
+def _join_host_port(host, port):
+  """Joins a hostname and port together.
+
+  This is a minimal implementation intended to cope with IPv6 literals. For
+  example, _join_host_port('::1', 80) == '[::1]:80'.
+
+  :Args:
+  - host - A hostname.
+  - port - An integer port.
+
+  """
+  if ':' in host and not host.startswith('['):
+    return '[%s]:%d' % (host, port)
+  return '%s:%d' % (host, port)
+
+
 class Request(url_request.Request):
     """
     Extends the url_request.Request to support all HTTP request types.
@@ -167,11 +213,12 @@ class RemoteConnection(object):
         parsed_url = parse.urlparse(remote_server_addr)
         addr = ""
         if parsed_url.hostname and resolve_ip:
-            try:
-                netloc = socket.gethostbyname(parsed_url.hostname)
+            ip = _resolve_ip(parsed_url.hostname)
+            if ip:
+                netloc = ip
                 addr = netloc
                 if parsed_url.port:
-                    netloc += ':%d' % parsed_url.port
+                    netloc = _join_host_port(netloc, parsed_url.port)
                 if parsed_url.username:
                     auth = parsed_url.username
                     if parsed_url.password:
@@ -180,8 +227,9 @@ class RemoteConnection(object):
                 remote_server_addr = parse.urlunparse(
                     (parsed_url.scheme, netloc, parsed_url.path,
                      parsed_url.params, parsed_url.query, parsed_url.fragment))
-            except socket.gaierror:
-                LOGGER.info('Could not get IP address for host: %s' % parsed_url.hostname)
+            else:
+                LOGGER.info('Could not get IP address for host: %s' %
+                            parsed_url.hostname)
 
         self._url = remote_server_addr
         if keep_alive:
