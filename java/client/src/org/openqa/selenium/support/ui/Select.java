3fb65b74e784ca703c354b4d9b919a9aeb978554
Yevgeniia Tymoshchuk <tymoschuk.jane@gmail.com>
2016-05-13 15:45:56 +0300

    Interface for Select component extracted
    This interface will allow to create custom realisation of like-select components, which actually do not have select tag inside.

--- java/client/src/org/openqa/selenium/support/ui/Select.java
+++ java/client/src/org/openqa/selenium/support/ui/Select.java
@@ -28,7 +28,7 @@ import java.util.StringTokenizer;
 /**
  * Models a SELECT tag, providing helper methods to select and deselect options.
  */
-public class Select {
+public class Select implements ISelect{
 
   private final WebElement element;
   private final boolean isMulti;
@@ -59,6 +59,7 @@ public class Select {
    * @return Whether this select element support selecting multiple options at the same time? This
    *         is done by checking the value of the "multiple" attribute.
    */
+  @Override
   public boolean isMultiple() {
     return isMulti;
   }
@@ -66,6 +67,7 @@ public class Select {
   /**
    * @return All options belonging to this select tag
    */
+  @Override
   public List<WebElement> getOptions() {
     return element.findElements(By.tagName("option"));
   }
@@ -73,6 +75,7 @@ public class Select {
   /**
    * @return All selected options belonging to this select tag
    */
+  @Override
   public List<WebElement> getAllSelectedOptions() {
     List<WebElement> toReturn = new ArrayList<>();
 
@@ -90,6 +93,7 @@ public class Select {
    *         normal select)
    * @throws NoSuchElementException If no option is selected
    */
+  @Override
   public WebElement getFirstSelectedOption() {
     for (WebElement option : getOptions()) {
       if (option.isSelected()) {
@@ -109,10 +113,11 @@ public class Select {
    * @param text The visible text to match against
    * @throws NoSuchElementException If no matching option elements are found
    */
+  @Override
   public void selectByVisibleText(String text) {
     // try to find the option via XPATH ...
     List<WebElement> options =
-        element.findElements(By.xpath(".//option[normalize-space(.) = " + Quotes.escape(text) + "]"));
+      element.findElements(By.xpath(".//option[normalize-space(.) = " + Quotes.escape(text) + "]"));
 
     boolean matched = false;
     for (WebElement option : options) {
@@ -132,8 +137,8 @@ public class Select {
       } else {
         // get candidates via XPATH ...
         candidates =
-            element.findElements(By.xpath(".//option[contains(., " +
-                Quotes.escape(subStringWithoutSpace) + ")]"));
+          element.findElements(By.xpath(".//option[contains(., " +
+                                        Quotes.escape(subStringWithoutSpace) + ")]"));
       }
       for (WebElement option : candidates) {
         if (text.equals(option.getText())) {
@@ -170,6 +175,7 @@ public class Select {
    * @param index The option at this index will be selected
    * @throws NoSuchElementException If no matching option elements are found
    */
+  @Override
   public void selectByIndex(int index) {
     String match = String.valueOf(index);
 
@@ -191,9 +197,10 @@ public class Select {
    * @param value The value to match against
    * @throws NoSuchElementException If no matching option elements are found
    */
+  @Override
   public void selectByValue(String value) {
     List<WebElement> options = element.findElements(By.xpath(
-        ".//option[@value = " + Quotes.escape(value) + "]"));
+      ".//option[@value = " + Quotes.escape(value) + "]"));
 
     boolean matched = false;
     for (WebElement option : options) {
@@ -214,10 +221,11 @@ public class Select {
    *
    * @throws UnsupportedOperationException If the SELECT does not support multiple selections
    */
+  @Override
   public void deselectAll() {
     if (!isMultiple()) {
       throw new UnsupportedOperationException(
-          "You may only deselect all options of a multi-select");
+        "You may only deselect all options of a multi-select");
     }
 
     for (WebElement option : getOptions()) {
@@ -235,14 +243,15 @@ public class Select {
    * @throws NoSuchElementException If no matching option elements are found
    * @throws UnsupportedOperationException If the SELECT does not support multiple selections
    */
+  @Override
   public void deselectByValue(String value) {
     if (!isMultiple()) {
       throw new UnsupportedOperationException(
-          "You may only deselect options of a multi-select");
+        "You may only deselect options of a multi-select");
     }
-    
+
     List<WebElement> options = element.findElements(By.xpath(
-        ".//option[@value = " + Quotes.escape(value) + "]"));
+      ".//option[@value = " + Quotes.escape(value) + "]"));
     boolean matched = false;
     for (WebElement option : options) {
       setSelected(option, false);
@@ -261,14 +270,15 @@ public class Select {
    * @throws NoSuchElementException If no matching option elements are found
    * @throws UnsupportedOperationException If the SELECT does not support multiple selections
    */
+  @Override
   public void deselectByIndex(int index) {
     if (!isMultiple()) {
       throw new UnsupportedOperationException(
-          "You may only deselect options of a multi-select");
+        "You may only deselect options of a multi-select");
     }
-    
+
     String match = String.valueOf(index);
-    
+
     for (WebElement option : getOptions()) {
       if (match.equals(option.getAttribute("index"))) {
         setSelected(option, false);
@@ -288,14 +298,15 @@ public class Select {
    * @throws NoSuchElementException If no matching option elements are found
    * @throws UnsupportedOperationException If the SELECT does not support multiple selections
    */
+  @Override
   public void deselectByVisibleText(String text) {
     if (!isMultiple()) {
       throw new UnsupportedOperationException(
-          "You may only deselect options of a multi-select");
+        "You may only deselect options of a multi-select");
     }
-    
+
     List<WebElement> options = element.findElements(By.xpath(
-        ".//option[normalize-space(.) = " + Quotes.escape(text) + "]"));
+      ".//option[normalize-space(.) = " + Quotes.escape(text) + "]"));
 
     boolean matched = false;
     for (WebElement option : options) {
