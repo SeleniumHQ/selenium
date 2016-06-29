15fe5fcbdac61c87c91cbd0352825be0a119ca05
Jim Evans <james.h.evans.jr@gmail.com>
2016-06-29 06:14:50 -0700

    .NET bindings version 2.53.1 to work with Firefox 47.0.1

--- dotnet/src/core/Properties/AssemblyInfo.cs
+++ dotnet/src/core/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Selenium Committers")]
 [assembly: AssemblyProduct("Selenium")]
-[assembly: AssemblyCopyright("Copyright © Software Freedom Conservancy 2014")]
+[assembly: AssemblyCopyright("Copyright © Software Freedom Conservancy 2016")]
 
 // Mark the assembly as CLSCompliant for use with all CLS languages
 [assembly: CLSCompliant(true)]
@@ -21,5 +21,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.52.0.0")]
-[assembly: AssemblyFileVersion("2.52.0.0")]
+[assembly: AssemblyVersion("2.53.1.0")]
+[assembly: AssemblyFileVersion("2.53.1.0")]
--- dotnet/src/webdriver/Properties/AssemblyInfo.cs
+++ dotnet/src/webdriver/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Selenium Committers")]
 [assembly: AssemblyProduct("Selenium")]
-[assembly: AssemblyCopyright("Copyright © Software Freedom Conservancy 2014")]
+[assembly: AssemblyCopyright("Copyright © Software Freedom Conservancy 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.52.0.0")]
-[assembly: AssemblyFileVersion("2.52.0.0")]
+[assembly: AssemblyVersion("2.53.1.0")]
+[assembly: AssemblyFileVersion("2.53.1.0")]
