93332c3e873388f8a57790bf31d2a98dce0551c7
Luke Inman-Semerau <luke.semerau@gmail.com>
2016-07-13 14:42:25 -0700

    updating copyright

--- py/test/selenium/webdriver/common/frame_switching_tests.py
+++ py/test/selenium/webdriver/common/frame_switching_tests.py
@@ -3,10 +3,10 @@
 # distributed with this work for additional information
 # regarding copyright ownership.  The SFC licenses this file
 # to you under the Apache License, Version 2.0 (the
-# "License") you may not use this file except in compliance
+# "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#   http:#www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
