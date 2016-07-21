352e5da9875886353f9b0e17807250b91d0ca246
Rafael Chavez <rafchavez3@gmail.com>
2016-07-21 15:04:46 -0700

    DeviceRotation response can return long. (#2504)

--- java/client/src/org/openqa/selenium/DeviceRotation.java
+++ java/client/src/org/openqa/selenium/DeviceRotation.java
@@ -40,10 +40,10 @@ public class DeviceRotation {
 	 * @param z
 	 */
 	public DeviceRotation(int x, int y, int z) {
-		this.validateParameters(x, y, z);
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.validateParameters(this.x, this.y, this.z);		
 	}
 	
 	/**
@@ -54,19 +54,21 @@ public class DeviceRotation {
 	 * z : zVal
 	 * @param map
 	 */
-	public DeviceRotation(Map<String, Integer> map) {
+	public DeviceRotation(Map<String, Number> map) {
 		if (map == null || !map.containsKey("x") || !map.containsKey("y") || !map.containsKey("z")) {
 			throw new IllegalArgumentException("Could not initialize DeviceRotation with map given: " + map.toString());
 		}
-		this.validateParameters(map.get("x"), map.get("y"), map.get("z"));
-		this.x = map.get("x");
-		this.y = map.get("y");
-		this.z = map.get("z");
+		this.x = map.get("x").intValue();
+		this.y = map.get("y").intValue();
+		this.z = map.get("z").intValue();
+		this.validateParameters(x, y, z);
 	}
 	
 	private void validateParameters(int x, int y, int z) {
 		if (x < 0 || y < 0 || z < 0) {
 			throw new IllegalArgumentException("DeviceRotation requires positive axis values: \nx = " + x + "\ny = " + y + "\nz = " + z);
+		} else if (x >= 360 || y >= 360 || z >= 360) {
+			throw new IllegalArgumentException("DeviceRotation requires positive axis values under 360: \nx = " + x + "\ny = " + y + "\nz = " + z);
 		}
 	}
 	
@@ -97,6 +99,21 @@ public class DeviceRotation {
 	public ImmutableMap<String,Integer> parameters() {
 		return ImmutableMap.of("x", this.x, "y", this.y, "z", this.z);
 	}
-
 	
+	@Override
+	public boolean equals(Object o) 
+	{
+		if (!(o instanceof DeviceRotation)) {
+			return false;
+		}
+		if (o == this) {
+			return true;
+		}
+		
+		DeviceRotation obj = (DeviceRotation)o;
+		if (obj.getX() != this.getX() || obj.getY() != this.getY() || obj.getZ() != this.getZ()) {
+			return false;
+		}
+		return true;
+	}
 }
