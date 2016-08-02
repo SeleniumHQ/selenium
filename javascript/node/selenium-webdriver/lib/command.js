9b6bf5e6e9fa1c3a85b37e718a8cb7bbabeb501d
Sammy Jelin <sjelin@gmail.com>
2016-08-02 14:15:56 -0700

    allow defining custom commands on a deferred executor in node

--- javascript/node/selenium-webdriver/lib/command.js
+++ javascript/node/selenium-webdriver/lib/command.js
@@ -229,6 +229,21 @@ class Executor {
    *     result.
    */
   execute(command) {}
+
+  /**
+   * Defines a new command for use with this executor. When a command is sent,
+   * the {@code path} will be preprocessed using the command's parameters; any
+   * path segments prefixed with ":" will be replaced by the parameter of the
+   * same name. For example, given "/person/:name" and the parameters
+   * "{name: 'Bob'}", the final command path will be "/person/Bob".
+   *
+   * @param {string} name The command name.
+   * @param {string} method The HTTP method to use when sending this command.
+   * @param {string} path The path to send the command to, relative to
+   *     the WebDriver server's command root and of the form
+   *     "/path/:variable/segment".
+   */
+  defineCommand(name, method, path) {}
 }
 
 
@@ -248,6 +263,11 @@ class DeferredExecutor {
     this.execute = function(command) {
       return delegate.then(executor => executor.execute(command));
     };
+
+    /** @override */
+    this.defineCommand(name, method, path) {
+      delegate.then(executor => executor.defineCommand(name, method, path));
+    }
   }
 }
 
