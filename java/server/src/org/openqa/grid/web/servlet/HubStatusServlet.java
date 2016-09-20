978350b0e8ce9b707669080f5d70dc57b1e28af4
tommywo <tommywo@users.noreply.github.com>
2016-09-20 21:13:03 +0200

    Allow using query string in a GET request to HubStatusServlet (#2771)

--- java/server/src/org/openqa/grid/web/servlet/HubStatusServlet.java
+++ java/server/src/org/openqa/grid/web/servlet/HubStatusServlet.java
@@ -33,6 +33,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -55,9 +56,9 @@ import javax.servlet.http.HttpServletResponse;
  *      ]
  * }
  *
- * if no param is specified, all params known to the hub are returned.
+ * alternatively you can use a query string ?configuration=timeout,servlets
  *
- * {"configuration": []  }
+ * if no param is specified, all params known to the hub are returned.
  *
  */
 public class HubStatusServlet extends RegistryBasedServlet {
@@ -101,7 +102,10 @@ public class HubStatusServlet extends RegistryBasedServlet {
       if (request.getInputStream() != null) {
         JsonObject requestJSON = getRequestJSON(request);
         List<String> keysToReturn = null;
-        if (requestJSON != null && requestJSON.has("configuration")) {
+
+        if (request.getParameter("configuration") != null && !"".equals(request.getParameter("configuration"))) {
+          keysToReturn = Arrays.asList(request.getParameter("configuration").split(","));
+        } else if (requestJSON != null && requestJSON.has("configuration")) {
           keysToReturn = new Gson().fromJson(requestJSON.getAsJsonArray("configuration"), ArrayList.class);
         }
 
