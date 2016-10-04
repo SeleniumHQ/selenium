49cc3aec5cf2ffcc247cefac9b653ed311a6529c
Simon Stewart <simon.m.stewart@gmail.com>
2016-10-04 21:54:53 +0100

    Avoid using singletons in tests.
    I've seen test failures because of the use of statics in the
    grid tests, and this contains a test that's flaky on travis.

--- java/server/test/org/openqa/grid/internal/listener/CommandListenerTest.java
+++ java/server/test/org/openqa/grid/internal/listener/CommandListenerTest.java
@@ -30,7 +30,7 @@ import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -134,11 +134,11 @@ public class CommandListenerTest {
     }
   }
 
-  static RegistrationRequest req = null;
-  static Map<String, Object> app1 = new HashMap<>();
+  private RegistrationRequest req = null;
+  private Map<String, Object> app1 = new HashMap<>();
 
-  @BeforeClass
-  public static void prepare() {
+  @Before
+  public void prepare() {
     app1.put(CapabilityType.APPLICATION_NAME, "app1");
     GridNodeConfiguration config = new GridNodeConfiguration();
     config.capabilities.add(new DesiredCapabilities(app1));
