9706bb7525ff9937c52010db2ae71bc71c1fabb2
Andreas Tolfsen <ato@mozilla.com>
2016-10-10 20:17:25 +0100

    py: new FirefoxDriver ctor precedence logic and moz:firefoxOptions support (#2882)
    This change tries to make sense of the keyword arguments accepted by
    FirefoxDriver's constructor in the transition phase between the Firefox
    add-on driver and the Mozilla supported geckodriver HTTP service.
    
    Prior to this patch, which arguments were consumed and respected varied
    greatly on whether the `capabilities["marionette"]` key was set to true.
    Following this change, the options passed on to geckodriver will
    consistently come out of firefox.Options.
    
    The patch implements precedence logic for `capabilities`,
    `firefox_options`, and `firefox_binary`/`firefox_profile`.  They will
    populate the underlying data model in that order, which means a binary-
    or profile location set in `firefox_options` will override one set in the
    `capabilities` dictionary, and that `firefox_binary` and `firefox_profile`
    will override both of these.
    
    The goal is to make it predictable what key is chosen, and to make the
    underlying data model somewhat easier to understand.  The latter is
    particularly important because the extension connection used for the old
    Firefox add-on does not accept a binary, profile, or arguments through
    capabilities since it is the Python bindings themselves that manage the
    lifetime of the browser.
    
    In the case of geckodriver, which is an external HTTP service, it
    manages the lifetime and accepts a `moz:firefoxOptions` dictionary that
    is not unlike `chromeOptions` as of version 0.11.0.  Versions prior to
    geckodriver 0.11.0 accepted `firefox_binary`, `firefox_profile`, and
    `firefox_args` as top-level entries on the capabilities JSON Object.
    This is no longer supported behaviour by either the Python bindings
    or geckodriver.

--- py/selenium/webdriver/firefox/webdriver.py
+++ py/selenium/webdriver/firefox/webdriver.py
@@ -28,13 +28,14 @@ except NameError:  # Python 3.x
 import shutil
 import socket
 import sys
+import types
 
+from .extension_connection import ExtensionConnection
 from .firefox_binary import FirefoxBinary
+from .firefox_profile import FirefoxProfile
 from .options import Options
 from .remote_connection import FirefoxRemoteConnection
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.extension_connection import ExtensionConnection
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from .service import Service
 from .webelement import FirefoxWebElement
@@ -47,25 +48,93 @@ class WebDriver(RemoteWebDriver):
 
     _web_element_cls = FirefoxWebElement
 
-    def __init__(self, firefox_profile=None, firefox_binary=None, timeout=30,
-                 capabilities=None, proxy=None, executable_path="geckodriver", firefox_options=None, log_path="geckodriver.log"):
-        capabilities = capabilities or DesiredCapabilities.FIREFOX.copy()
+    def __init__(self, firefox_profile=None, firefox_binary=None,
+                 timeout=30, capabilities=None, proxy=None,
+                 executable_path="geckodriver", firefox_options=None,
+                 log_path="geckodriver.log"):
+        """Starts a new local session of Firefox.
 
-        self.profile = firefox_profile or FirefoxProfile()
-        self.profile.native_events_enabled = (
-            self.NATIVE_EVENTS_ALLOWED and self.profile.native_events_enabled)
+        Based on the combination and specificity of the various keyword
+        arguments, a capabilities dictionary will be constructed that
+        is passed to the remote end.
 
-        self.binary = firefox_binary or capabilities.get("binary", FirefoxBinary())
+        The keyword arguments given to this constructor are helpers to
+        more easily allow Firefox WebDriver sessions to be customised
+        with different options.  They are mapped on to a capabilities
+        dictionary that is passed on to the remote end.
 
-        self.options = firefox_options or Options()
-        self.options.binary_location = self.binary if isinstance(self.binary, basestring) else self.binary._start_cmd
-        self.options.profile = self.profile
-        capabilities.update(self.options.to_capabilities())
+        As some of the options, such as `firefox_profile` and
+        `firefox_options.profile` are mutually exclusive, precedence is
+        given from how specific the setting is.  `capabilities` is the
+        least specific keyword argument, followed by `firefox_options`,
+        followed by `firefox_binary` and `firefox_profile`.
 
-        # marionette
+        In practice this means that if `firefox_profile` and
+        `firefox_options.profile` are both set, the selected profile
+        instance will always come from the most specific variable.
+        In this case that would be `firefox_profile`.  This will result in
+        `firefox_options.profile` to be ignored because it is considered
+        a less specific setting than the top-level `firefox_profile`
+        keyword argument.  Similarily, if you had specified a
+        `capabilities["firefoxOptions"]["profile"]` Base64 string,
+        this would rank below `firefox_options.profile`.
+
+        :param firefox_profile: Instance of ``FirefoxProfile`` object
+            or a string.  If undefined, a fresh profile will be created
+            in a temporary location on the system.
+        :param firefox_binary: Instance of ``FirefoxBinary`` or full
+            path to the Firefox binary.  If undefined, the system default
+            Firefox installation will  be used.
+        :param timeout: Time to wait for Firefox to launch when using
+            the extension connection.
+        :param capabilities: Dictionary of desired capabilities.
+        :param proxy: The proxy settings to us when communicating with
+            Firefox via the extension connection.
+        :param executable_path: Full path to override which geckodriver
+            binary to use for Firefox 47.0.1 and greater, which
+            defaults to picking up the binary from the system path.
+        :param firefox_options: Instance of ``options.Options``.
+        :param log_path: Where to log information from the driver.
+
+        """
+        self.binary = None
+        self.profile = None
+
+        if capabilities is None:
+            capabilities = DesiredCapabilities.FIREFOX.copy()
+        if firefox_options is None:
+            firefox_options = Options()
+
+        if capabilities.get("binary"):
+            self.binary = capabilities["binary"]
+
+        # firefox_options overrides capabilities
+        if firefox_options is not None:
+            if firefox_options.binary is not None:
+                self.binary = firefox_options.binary
+            if firefox_options.profile is not None:
+                self.profile = firefox_options.profile
+
+        # firefox_binary and firefox_profile
+        # override firefox_options
+        if firefox_binary is not None:
+            if isinstance(firefox_binary, types.StringTypes):
+                firefox_binary = FirefoxBinary(firefox_binary)
+            self.binary = firefox_binary
+            firefox_options.binary = firefox_binary
+        if firefox_profile is not None:
+            if isinstance(firefox_profile, types.StringTypes):
+                firefox_profile = FirefoxProfile(firefox_profile)
+            self.profile = firefox_profile
+            firefox_options.profile = firefox_profile
+
+        # W3C remote
+        # TODO(ato): Perform conformance negotiation
         if capabilities.get("marionette"):
-            self.service = Service(executable_path, firefox_binary=self.options.binary_location, log_path=log_path)
+            self.service = Service(executable_path, log_path=log_path)
             self.service.start()
+
+            capabilities.update(firefox_options.to_capabilities())
 
             executor = FirefoxRemoteConnection(
                 remote_server_addr=self.service.service_url)
@@ -74,8 +143,18 @@ class WebDriver(RemoteWebDriver):
                 command_executor=executor,
                 desired_capabilities=capabilities,
                 keep_alive=True)
+
+        # Selenium remote
         else:
-            # Oh well... sometimes the old way is the best way.
+            if self.binary is None:
+                self.binary = FirefoxBinary()
+            if self.profile is None:
+                self.profile = FirefoxProfile()
+
+            # disable native events if globally disabled
+            self.profile.native_events_enabled = (
+                self.NATIVE_EVENTS_ALLOWED and self.profile.native_events_enabled)
+
             if proxy is not None:
                 proxy.add_to_capabilities(capabilities)
 
@@ -111,6 +190,8 @@ class WebDriver(RemoteWebDriver):
     @property
     def firefox_profile(self):
         return self.profile
+
+    # Extension commands:
 
     def set_context(self, context):
         self.execute("SET_CONTEXT", {"context": context})
--- py/test/selenium/webdriver/marionette/mn_options_tests.py
+++ py/test/selenium/webdriver/marionette/mn_options_tests.py
@@ -15,25 +15,96 @@
 # specific language governing permissions and limitations
 # under the License.
 
-
 import pytest
+import shutil
+import tempfile
+import types
 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Log, Options
 from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture
 def driver(capabilities):
-    options = Options()
-    driver = Firefox(
-        capabilities=capabilities,
-        firefox_options=options)
+    driver = Firefox(firefox_options=Options(), capabilities=capabilities)
     yield driver
     driver.quit()
 
 
-class TestOptions(object):
-
+class TestIntegration(object):
     def test_we_can_pass_options(self, driver, pages):
-        pages.load('formPage.html')
+        pages.load("formPage.html")
         driver.find_element_by_id("cheese")
+
+
+class TestUnit(object):
+    def test_ctor(self):
+        opts = Options()
+        assert opts._binary is None
+        assert opts._profile is None
+        assert opts._arguments == []
+        assert isinstance(opts.log, Log)
+
+    def test_binary(self):
+        opts = Options()
+        assert opts.binary is None
+
+        other_binary = FirefoxBinary()
+        assert other_binary != opts.binary
+        opts.binary = other_binary
+        assert other_binary == opts.binary
+
+        path = "/path/to/binary"
+        opts.binary = path
+        assert isinstance(opts.binary, FirefoxBinary)
+        assert opts.binary._start_cmd == path
+
+    def test_profile(self, tmpdir_factory):
+        opts = Options()
+        assert opts.profile is None
+
+        other_profile = FirefoxProfile()
+        assert other_profile != opts.profile
+        opts.profile = other_profile
+        assert other_profile == opts.profile
+
+        opts.profile = str(tmpdir_factory.mktemp("profile"))
+        assert isinstance(opts.profile, FirefoxProfile)
+
+    def test_arguments(self):
+        opts = Options()
+        assert len(opts.arguments) == 0
+
+        opts.add_argument("--foo")
+        assert len(opts.arguments) == 1
+        opts.arguments.append("--bar")
+        assert len(opts.arguments) == 2
+        assert opts.arguments == ["--foo", "--bar"]
+
+    def test_to_capabilities(self):
+        opts = Options()
+        assert opts.to_capabilities() == {}
+
+        profile = FirefoxProfile()
+        opts.profile = profile
+        caps = opts.to_capabilities()
+        assert "moz:firefoxOptions" in caps
+        assert "profile" in caps["moz:firefoxOptions"]
+        assert isinstance(caps["moz:firefoxOptions"]["profile"], types.StringTypes)
+        assert caps["moz:firefoxOptions"]["profile"] == profile.encoded
+
+        opts.add_argument("--foo")
+        caps = opts.to_capabilities()
+        assert "moz:firefoxOptions" in caps
+        assert "args" in caps["moz:firefoxOptions"]
+        assert caps["moz:firefoxOptions"]["args"] == ["--foo"]
+
+        binary = FirefoxBinary()
+        opts.binary = binary
+        caps = opts.to_capabilities()
+        assert "moz:firefoxOptions" in caps
+        assert "binary" in caps["moz:firefoxOptions"]
+        assert isinstance(caps["moz:firefoxOptions"]["binary"], types.StringTypes)
+        assert caps["moz:firefoxOptions"]["binary"] == binary._start_cmd
