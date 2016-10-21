9157c7071f9900c2608f5ca40ae4f518ed373b96
Dave Hunt <dave.hunt@gmail.com>
2016-10-21 10:49:46 +0100

    [py] Only attempt to remove the profile path if one was specified

--- py/selenium/webdriver/firefox/webdriver.py
+++ py/selenium/webdriver/firefox/webdriver.py
@@ -186,12 +186,13 @@ class WebDriver(RemoteWebDriver):
             self.service.stop()
         else:
             self.binary.kill()
-        try:
-            shutil.rmtree(self.profile.path)
-            if self.profile.tempfolder is not None:
-                shutil.rmtree(self.profile.tempfolder)
-        except Exception as e:
-            print(str(e))
+        if self.profile is not None:
+            try:
+                shutil.rmtree(self.profile.path)
+                if self.profile.tempfolder is not None:
+                    shutil.rmtree(self.profile.tempfolder)
+            except Exception as e:
+                print(str(e))
 
     @property
     def firefox_profile(self):
