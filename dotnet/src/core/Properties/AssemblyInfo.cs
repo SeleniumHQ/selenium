b8f34769498d4233833a1d9aaf943f67340384b0
Jim Evans <james.h.evans.jr@gmail.com>
2016-11-19 09:52:32 -0800

    Bumping .NET version number to 3.0.1

--- dotnet/src/core/Properties/AssemblyInfo.cs
+++ dotnet/src/core/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
