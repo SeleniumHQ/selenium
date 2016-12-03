0c67ad33490bc2807683a62484e3198e1de8a417
Jason Juang <juangj@google.com>
2016-12-02 18:20:05 -0800

    If the selectionStart attribute is null, then selection APIs do not apply to that element.

--- javascript/atoms/keyboard.js
+++ javascript/atoms/keyboard.js
@@ -812,7 +812,9 @@ bot.Keyboard.prototype.updateOnHomeOrEnd_ = function(key) {
  */
 bot.Keyboard.checkCanUpdateSelection_ = function(element) {
   try {
-    element.selectionStart;
+    if (typeof element.selectionStart == 'number') {
+      return;
+    }
   } catch (ex) {
     // The native error message is actually pretty informative, just add a
     // reference to the relevant Chrome bug to provide more context.
@@ -823,6 +825,7 @@ bot.Keyboard.checkCanUpdateSelection_ = function(element) {
     }
     throw ex;
   }
+  throw Error('Element does not support selection');
 };
 
 
