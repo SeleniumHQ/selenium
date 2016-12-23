f8c903b49f084605ec8bbacc34b1077938278ee3
Alexei Barantsev <barancev@gmail.com>
2016-12-24 00:51:43 +0300

    Inlining

--- java/client/test/org/openqa/selenium/environment/webserver/ManifestServlet.java
+++ java/client/test/org/openqa/selenium/environment/webserver/ManifestServlet.java
@@ -28,8 +28,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.openqa.selenium.io.IOUtils;
-
 public class ManifestServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
--- java/client/test/org/openqa/selenium/environment/webserver/Utf8Servlet.java
+++ java/client/test/org/openqa/selenium/environment/webserver/Utf8Servlet.java
@@ -24,8 +24,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.openqa.selenium.io.IOUtils;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
