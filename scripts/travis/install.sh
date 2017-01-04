0873bc02ffa8104e6bb4fd3cd1944e1bbafe9572
Alexei Barantsev <barancev@gmail.com>
2017-01-04 18:10:56 +0300

    Updating geckodriver version

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -13,7 +13,7 @@ if [[ ! -z $CHROME ]]; then
 fi
 
 if [[ ! -z $MARIONETTE ]]; then
-  export GECKODRIVER_VERSION=v0.11.1
+  export GECKODRIVER_VERSION=v0.12.0
   curl -L -o geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/${GECKODRIVER_VERSION}/geckodriver-${GECKODRIVER_VERSION}-linux64.tar.gz
   gunzip -c geckodriver.tar.gz | tar xopf -
   chmod +x geckodriver && sudo mv geckodriver /usr/local/bin
