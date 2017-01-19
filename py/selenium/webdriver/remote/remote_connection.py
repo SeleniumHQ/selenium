b7a066871d947ba64147dc1480324e7862b3297a
Leo Laskin <llaskin@Sebs-MacBook-Pro.local>
2017-01-19 12:16:33 -0600

    fixing if statement

--- py/selenium/webdriver/remote/remote_connection.py
+++ py/selenium/webdriver/remote/remote_connection.py
@@ -471,7 +471,7 @@ class RemoteConnection(object):
             request.add_header('Accept', 'application/json')
             request.add_header('Content-Type', 'application/json;charset=UTF-8')
 
-            if((parsed_url.username != NULL) && (parsed_url.password != NULL))
+            if parsed_url.username
                 base64string = base64.b64encode('%s:%s' % (parsed_url.username, parsed_url.password))
                 request.add_header("Authorization", "Basic %s" % base64string)
 
