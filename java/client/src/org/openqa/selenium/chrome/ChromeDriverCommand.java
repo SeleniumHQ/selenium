0e7d7c471f6c8d6dfbff540091dd6643126bc3d1
ragePowered <Pass!Word456>
2017-02-07 15:22:32 +0200

    [3450] Added bindings for custom ChromeDriver commands (GET_NETWORK_CONDITIONS, SET_NETWORK_CONDITIONS and DELETE_NETWORK_CONDITIONS) that allows client code to utilize built-in throttling functionality.

--- java/client/src/org/openqa/selenium/chrome/ChromeDriverCommand.java
+++ java/client/src/org/openqa/selenium/chrome/ChromeDriverCommand.java
@@ -24,4 +24,7 @@ final class ChromeDriverCommand {
   private ChromeDriverCommand() {}
 
   static final String LAUNCH_APP = "launchApp";
+  static final String GET_NETWORK_CONDITIONS = "getNetworkConditions";
+  static final String SET_NETWORK_CONDITIONS = "setNetworkConditions";
+  static final String DELETE_NETWORK_CONDITIONS = "deleteNetworkConditions";
 }
