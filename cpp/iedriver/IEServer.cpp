da7f84a407edd429c1f518d1165a9aa9a0fb818a
Jim Evans <james.h.evans.jr@gmail.com>
2017-02-22 07:12:09 -0800

    Removing Microsoft IE driver implmentation
    The Micorosft IE driver implementation has been abandoned in favor of the
    Edge driver implementation. It has received no updates in over two years,
    and will likely not be updated in the future. The open-source
    implementation is still supported and will continue to be used.

--- cpp/iedriver/IEServer.cpp
+++ cpp/iedriver/IEServer.cpp
@@ -28,12 +28,10 @@ IEServer::IEServer(int port,
                    const std::string& log_level,
                    const std::string& log_file,
                    const std::string& version,
-                   const std::string& driver_implementation,
                    const std::string& acl) : Server(port, host, log_level, log_file, acl) {
   LOG(TRACE) << "Entering IEServer::IEServer";
   LOG(INFO) << "Driver version: " << version;
   this->version_ = version;
-  this->driver_implementation_ = driver_implementation;
 }
 
 IEServer::~IEServer(void) {
@@ -44,7 +42,6 @@ SessionHandle IEServer::InitializeSession() {
   SessionHandle session_handle(new IESession());
   SessionParameters params;
   params.port = this->port();
-  params.implementation = IESession::ConvertDriverEngine(this->driver_implementation_);
   session_handle->Initialize(reinterpret_cast<void*>(&params));
   return session_handle;
 }
