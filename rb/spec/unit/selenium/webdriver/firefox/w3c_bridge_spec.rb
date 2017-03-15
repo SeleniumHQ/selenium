dd486216e7d3168eea2665f60e7d1c853bbb0111
Lucas Tierney <lucast1533@gmail.com>
2017-03-15 10:09:28 -0500

    [rb] update unit tests for w3c responses wrapped in 'value'

--- rb/spec/unit/selenium/webdriver/firefox/w3c_bridge_spec.rb
+++ rb/spec/unit/selenium/webdriver/firefox/w3c_bridge_spec.rb
@@ -24,7 +24,7 @@ module Selenium
     module Firefox
       describe W3CBridge do
         let(:launcher) { double(Launcher, launch: nil, quit: nil, url: 'http://localhost:4444/wd/hub') }
-        let(:resp) { {'sessionId' => 'foo', 'value' => @default_capabilities} }
+        let(:resp) { { 'value' => {'sessionId' => 'foo', 'value' => @default_capabilities} } }
         let(:http) { double(Remote::Http::Default, call: resp).as_null_object }
         let(:caps) { Remote::Capabilities.chrome }
 
