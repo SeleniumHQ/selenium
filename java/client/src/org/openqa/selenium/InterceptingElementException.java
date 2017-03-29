fae4300ff67aae22b3c345c26022d28e4a71b3e3
Jarob22 <james@jamesreed.name>
2017-03-30 00:18:48 +0100

    subclassing ElementNotInteractable instead

--- java/client/src/org/openqa/selenium/InterceptingElementException.java
+++ java/client/src/org/openqa/selenium/InterceptingElementException.java
@@ -4,7 +4,7 @@ package org.openqa.selenium;
  * Thrown to indicate that a click was attempted on an element but was intercepted by another
  * element on top of it
  */
-public class InterceptingElementException extends InvalidElementStateException {
+public class InterceptingElementException extends ElementNotInteractableException {
 
   public InterceptingElementException(String message) {
     super(message);
