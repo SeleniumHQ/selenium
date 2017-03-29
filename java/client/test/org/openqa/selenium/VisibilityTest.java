50d1ff862d03c654eefc04d8abfd488541479490
Alexei Barantsev <barancev@gmail.com>
2017-03-29 16:48:43 +0300

    Ignoring a couple of tests failing due to a geckodriver bug

--- java/client/test/org/openqa/selenium/VisibilityTest.java
+++ java/client/test/org/openqa/selenium/VisibilityTest.java
@@ -102,6 +102,7 @@ public class VisibilityTest extends JUnit4TestBase {
   }
 
   @Test
+  @Ignore(value = MARIONETTE, reason = "https://github.com/mozilla/geckodriver/issues/579")
   public void testShouldNotBeAbleToClickOnAnElementThatIsNotDisplayed() {
     driver.get(pages.javascriptPage);
     WebElement element = driver.findElement(By.id("unclickable"));
@@ -115,6 +116,7 @@ public class VisibilityTest extends JUnit4TestBase {
   }
 
   @Test
+  @Ignore(value = MARIONETTE, reason = "https://github.com/mozilla/geckodriver/issues/579")
   public void testShouldNotBeAbleToTypeToAnElementThatIsNotDisplayed() {
     driver.get(pages.javascriptPage);
     WebElement element = driver.findElement(By.id("unclickable"));
