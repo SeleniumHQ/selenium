a709f452b12413123e865d99e2ce936569d818ff
Simon Stewart <simon.m.stewart@gmail.com>
2017-04-25 14:13:28 +0100

    Explain why ExpectedCondition continues to extend guava's Function
    It's to avoid having users rewrite code. We're so
    dependent on the library it doesn't make sense to
    remove a single call site.

--- java/client/src/org/openqa/selenium/support/ui/ExpectedCondition.java
+++ java/client/src/org/openqa/selenium/support/ui/ExpectedCondition.java
@@ -32,4 +32,7 @@ import org.openqa.selenium.WebDriver;
  *
  * @param <T> The return type
  */
+// NB: this originally extended Guava's Function interface since Java didn't have one. To avoid code
+// such as "com.google.common.base.Function condition = ExpectedConditions.elementFound(By);"
+// breaking at compile time, we continue to extend Guava's Function interface.
 public interface ExpectedCondition<T> extends Function<WebDriver, T> {}
