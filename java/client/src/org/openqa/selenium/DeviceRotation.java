08570cc976d3fc9f5fd0e40719e4b6982830c171
Marc Fisher <fisherii@google.com>
2017-05-10 16:31:00 -0700

    Add hashCode to DeviceRotation class. (#3999)

--- java/client/src/org/openqa/selenium/DeviceRotation.java
+++ java/client/src/org/openqa/selenium/DeviceRotation.java
@@ -20,105 +20,109 @@ package org.openqa.selenium;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
- * Defines an object which represents the three dimensional plane and how a device can be rotated about it.
- * Each of the axis is in positive degrees on the real number scale (0 &lt;= deg &lt;= 360).
+ * Defines an object which represents the three dimensional plane and how a device can be rotated
+ * about it. Each of the axis is in positive degrees on the real number scale (0 &lt;= deg &lt;=
+ * 360).
  *
- * Example Instantiation to rotate device to "Landscape Right":
- * DeviceRotation(0, 0, 90);
+ * <p>Example Instantiation to rotate device to "Landscape Right": DeviceRotation(0, 0, 90);
  */
 public class DeviceRotation {
-	//Default orientation is portrait
-	private int x = 0;
-	private int y = 0;
-	private int z = 0;
+  //Default orientation is portrait
+  private int x = 0;
+  private int y = 0;
+  private int z = 0;
 
-	/**
-	 * Instantiate a DeviceRotation object based on three integers.
-	 * @param x
-	 * @param y
-	 * @param z
-	 */
-	public DeviceRotation(int x, int y, int z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.validateParameters(this.x, this.y, this.z);
-	}
+  /**
+   * Instantiate a DeviceRotation object based on three integers.
+   *
+   * @param x
+   * @param y
+   * @param z
+   */
+  public DeviceRotation(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.validateParameters(this.x, this.y, this.z);
+  }
 
-	/**
-	 * Instantiate a DeviceRotation object based on a
-	 * HashMap object where the keys are the axis x, y, and z respectively:
-	 * x : xVal
-	 * y : yVal
-	 * z : zVal
-	 * @param map
-	 */
-	public DeviceRotation(Map<String, Number> map) {
-		if (map == null || !map.containsKey("x") || !map.containsKey("y") || !map.containsKey("z")) {
-			throw new IllegalArgumentException("Could not initialize DeviceRotation with map given: " + map.toString());
-		}
-		this.x = map.get("x").intValue();
-		this.y = map.get("y").intValue();
-		this.z = map.get("z").intValue();
-		this.validateParameters(x, y, z);
-	}
+  /**
+   * Instantiate a DeviceRotation object based on a HashMap object where the keys are the axis x, y,
+   * and z respectively: x : xVal y : yVal z : zVal
+   *
+   * @param map
+   */
+  public DeviceRotation(Map<String, Number> map) {
+    if (map == null || !map.containsKey("x") || !map.containsKey("y") || !map.containsKey("z")) {
+      throw new IllegalArgumentException(
+          "Could not initialize DeviceRotation with map given: " + map.toString());
+    }
+    this.x = map.get("x").intValue();
+    this.y = map.get("y").intValue();
+    this.z = map.get("z").intValue();
+    this.validateParameters(x, y, z);
+  }
 
-	private void validateParameters(int x, int y, int z) {
-		if (x < 0 || y < 0 || z < 0) {
-			throw new IllegalArgumentException("DeviceRotation requires positive axis values: \nx = " + x + "\ny = " + y + "\nz = " + z);
-		} else if (x >= 360 || y >= 360 || z >= 360) {
-			throw new IllegalArgumentException("DeviceRotation requires positive axis values under 360: \nx = " + x + "\ny = " + y + "\nz = " + z);
-		}
-	}
+  private void validateParameters(int x, int y, int z) {
+    if (x < 0 || y < 0 || z < 0) {
+      throw new IllegalArgumentException(
+          "DeviceRotation requires positive axis values: \nx = " + x + "\ny = " + y + "\nz = " + z);
+    } else if (x >= 360 || y >= 360 || z >= 360) {
+      throw new IllegalArgumentException(
+          "DeviceRotation requires positive axis values under 360: \nx = "
+              + x
+              + "\ny = "
+              + y
+              + "\nz = "
+              + z);
+    }
+  }
 
-	/**
-	 * @return the x
-	 */
-	public int getX() {
-		return x;
-	}
+  /** @return the x */
+  public int getX() {
+    return x;
+  }
 
-	/**
-	 * @return the y
-	 */
-	public int getY() {
-		return y;
-	}
+  /** @return the y */
+  public int getY() {
+    return y;
+  }
 
-	/**
-	 * @return the z
-	 */
-	public int getZ() {
-		return z;
-	}
+  /** @return the z */
+  public int getZ() {
+    return z;
+  }
 
-	/**
-	 * @return returns all axis mapped to a Map
-	 */
-	public Map<String,Integer> parameters() {
+  /** @return returns all axis mapped to a Map */
+  public Map<String, Integer> parameters() {
     HashMap<String, Integer> values = new HashMap<>();
     values.put("x", this.x);
     values.put("y", this.y);
     values.put("z", this.z);
     return Collections.unmodifiableMap(values);
-	}
+  }
 
-	@Override
-	public boolean equals(Object o)
-	{
-		if (!(o instanceof DeviceRotation)) {
-			return false;
-		}
-		if (o == this) {
-			return true;
-		}
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof DeviceRotation)) {
+      return false;
+    }
+    if (o == this) {
+      return true;
+    }
 
-		DeviceRotation obj = (DeviceRotation)o;
-		if (obj.getX() != this.getX() || obj.getY() != this.getY() || obj.getZ() != this.getZ()) {
-			return false;
-		}
-		return true;
-	}
+    DeviceRotation obj = (DeviceRotation) o;
+    if (obj.getX() != this.getX() || obj.getY() != this.getY() || obj.getZ() != this.getZ()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getX(), getY(), getZ());
+  }
 }
