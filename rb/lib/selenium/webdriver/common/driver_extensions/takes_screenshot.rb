bf9882412f117a28fb9a61278fa270ec7187cf4e
Andrei Botalov <botalov.andrey@gmail.com>
2017-06-10 22:02:49 +0300

    rb - warn on incorrectly named screenshot
    Signed-off-by: Alex Rodionov <p0deje@gmail.com>

--- rb/lib/selenium/webdriver/common/driver_extensions/takes_screenshot.rb
+++ rb/lib/selenium/webdriver/common/driver_extensions/takes_screenshot.rb
@@ -32,6 +32,11 @@ module Selenium
         #
 
         def save_screenshot(png_path)
+          extension = File.extname(png_path).downcase
+          if extension != '.png'
+            WebDriver.logger.warn "name used for saved screenshot does match file type. "\
+                                  "It should end with .png extension"
+          end
           File.open(png_path, 'wb') { |f| f << screenshot_as(:png) }
         end
 
