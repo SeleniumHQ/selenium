86419e922cbfafaf7b2471248a8ff92901984005
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2017-06-18 10:42:27 +0530

    Propagate altered DC to Node
    Currently the grid logic is such that if a particular
    test’s capabilities are altered via an overloaded
    version of
    
    * DefaultRemoteProxy#getNewSession
    * DefaultRemoteProxy#beforeSession
    
    the expectation is that the altered capabilities
    are what gets forwarded to the actual node prior to
    an actual session creation on the node. But
    this doesn’t happen because the altered desired
    capabilities are not being considered.
    Fixed this.
    
    Signed-off-by: Simon Stewart <simon.m.stewart@gmail.com>

--- java/server/src/org/openqa/grid/web/servlet/handler/WebDriverRequest.java
+++ java/server/src/org/openqa/grid/web/servlet/handler/WebDriverRequest.java
@@ -18,12 +18,15 @@
 
 package org.openqa.grid.web.servlet.handler;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import org.openqa.grid.common.exception.GridException;
 import org.openqa.grid.internal.ExternalSessionKey;
 import org.openqa.grid.internal.Registry;
+import org.openqa.selenium.remote.BeanToJsonConverter;
 import org.openqa.selenium.remote.JsonToBeanConverter;
 
 import java.util.Map;
@@ -31,6 +34,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 public class WebDriverRequest extends SeleniumBasedRequest {
+
+  private static final String CAPABILITIES = "capabilities";
+  private static final String DESIRED_CAPABILITIES = "desiredCapabilities";
 
   public WebDriverRequest(HttpServletRequest httpServletRequest, Registry registry) {
     super(httpServletRequest, registry);
@@ -65,17 +71,41 @@ public class WebDriverRequest extends SeleniumBasedRequest {
       JsonObject map = new JsonParser().parse(json).getAsJsonObject();
       // Current W3C has required / desired capabilities wrapped in a 'capabilities' object.
       // This will need to be updated if/when https://github.com/w3c/webdriver/pull/327 gets merged
-      if (map.has("capabilities")) {
-        JsonObject outerCapabilities = map.getAsJsonObject("capabilities");
-        if (outerCapabilities.has("desiredCapabilities")) {
-          JsonObject desiredCapabilities = outerCapabilities.getAsJsonObject("desiredCapabilities");
+      if (map.has(CAPABILITIES)) {
+        JsonObject outerCapabilities = map.getAsJsonObject(CAPABILITIES);
+        if (outerCapabilities.has(DESIRED_CAPABILITIES)) {
+          JsonObject desiredCapabilities = outerCapabilities.getAsJsonObject(DESIRED_CAPABILITIES);
           return new JsonToBeanConverter().convert(Map.class, desiredCapabilities);
         }
       }
-      JsonObject dc = map.get("desiredCapabilities").getAsJsonObject();
+      JsonObject dc = map.get(DESIRED_CAPABILITIES).getAsJsonObject();
       return new JsonToBeanConverter().convert(Map.class, dc);
+
     } catch (Exception e) {
       throw new GridException("Cannot extract a capabilities from the request: " + json, e);
     }
   }
+
+  @Override
+  public String getBody() {
+    String json =  super.getBody();
+    try {
+      Map<String, Object> capsMap = getDesiredCapabilities();
+      if (capsMap == null) {
+        return json;
+      }
+      JsonObject map = new JsonParser().parse(json).getAsJsonObject();
+      JsonElement dc = new BeanToJsonConverter().convertObject(capsMap);
+      if (map.has(CAPABILITIES)) {
+          map.getAsJsonObject(CAPABILITIES)
+              .add(DESIRED_CAPABILITIES, new BeanToJsonConverter().convertObject(dc));
+      } else {
+        map.add(DESIRED_CAPABILITIES, dc);
+      }
+      return new Gson().toJson(map);
+    } catch (Exception e) {
+      return json;
+    }
+  }
+
 }
