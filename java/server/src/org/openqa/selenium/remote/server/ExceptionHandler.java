8aca73793b62c29c38e6ed4d1afc5f3322158471
Simon Stewart <simon.m.stewart@gmail.com>
2017-07-21 11:04:39 +0100

    Make the new ErrorHandler flesh out responses properly
    It omits the status code, but at least the fields are
    correct now.

--- java/server/src/org/openqa/selenium/remote/server/ExceptionHandler.java
+++ java/server/src/org/openqa/selenium/remote/server/ExceptionHandler.java
@@ -22,13 +22,21 @@ import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.remote.BeanToJsonConverter;
+import org.openqa.selenium.remote.ErrorCodes;
 import org.openqa.selenium.remote.http.HttpRequest;
 import org.openqa.selenium.remote.http.HttpResponse;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
 
 /**
  * Takes an exception and formats it for a local end that speaks either the OSS or W3C dialect of
@@ -36,26 +44,52 @@ import java.util.Map;
  */
 class ExceptionHandler implements CommandHandler {
 
-  private final Exception exception;
+  private final static ErrorCodes ERRORS = new ErrorCodes();
+  private final static BeanToJsonConverter toJson = new BeanToJsonConverter();
 
-  public ExceptionHandler(Exception e) {
+  private final Throwable exception;
+
+  public ExceptionHandler(Throwable e) {
+    if (e == null) {
+      e = new WebDriverException("Unknown error");
+    }
+    if (e instanceof ExecutionException && e.getCause() != null) {
+      e = e.getCause();
+    }
     this.exception = e;
-    System.err.println(e);
   }
 
   @Override
   public void execute(HttpRequest req, HttpResponse resp) {
+    int code = ERRORS.toStatusCode(exception);
+    String status = ERRORS.toState(code);
 
+    Map<String, Object> toSerialise = new HashMap<>();
 
+    // W3C
     Map<String, Object> value = new HashMap<>();
     value.put("message", exception.getMessage());
     value.put("stacktrace", Throwables.getStackTraceAsString(exception));
-    value.put("error", "unknown error");
+    value.put("error", status);
 
-    Map<String, Object> toSerialise = new HashMap<>();
+    // JSON Wire Protocol
+    toSerialise.put("status", code);
+    value.put(
+        "stackTrace",
+        Stream.of(exception.getStackTrace())
+            .map(ste -> {
+              JsonObject line = new JsonObject();
+              line.add("fileName", new JsonPrimitive(ste.getFileName()));
+              line.add("lineNumber", new JsonPrimitive(ste.getLineNumber()));
+              line.add("className", new JsonPrimitive(ste.getClassName()));
+              line.add("methodName", new JsonPrimitive(ste.getMethodName()));
+              return ste;
+            })
+            .collect(ImmutableList.toImmutableList()));
+
     toSerialise.put("value", value);
 
-    byte[] bytes = new Gson().toJson(toSerialise).getBytes(UTF_8);
+    byte[] bytes = toJson.convert(toSerialise).getBytes(UTF_8);
     resp.setStatus(HTTP_INTERNAL_ERROR);
 
     resp.setHeader("Content-Type", JSON_UTF_8.toString());
