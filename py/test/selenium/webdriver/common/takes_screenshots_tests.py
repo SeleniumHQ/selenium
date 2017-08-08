0aca9a31823114e2ec8eafa6964c8a926e08cbff
Alexei Barantsev <barancev@gmail.com>
2017-08-08 11:52:47 +0300

    [py] Unignoring green marionette tests

--- py/test/selenium/webdriver/common/takes_screenshots_tests.py
+++ py/test/selenium/webdriver/common/takes_screenshots_tests.py
@@ -35,7 +35,6 @@ def test_get_screenshot_as_png(driver, pages):
 
 @pytest.mark.xfail_chrome
 @pytest.mark.xfail_firefox
-@pytest.mark.xfail_marionette
 def test_get_element_screenshot(driver, pages):
     pages.load("simpleTest.html")
     element = driver.find_element_by_id("multiline")
