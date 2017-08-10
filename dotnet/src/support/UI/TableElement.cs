b5f26d7f4e20cbae32234224e17988f329599386
Marc Siegmund <marc.siegmund@six-group.com>
2017-08-10 11:25:53 +0200

    added type-check on creation and throw exception if necessary

--- dotnet/src/support/UI/TableElement.cs
+++ dotnet/src/support/UI/TableElement.cs
@@ -22,69 +22,67 @@ using System.Linq;
 
 namespace OpenQA.Selenium.Support.UI
 {
+  /// <summary>
+  ///   Wraps an IWebElement into a user-friendly table-element
+  /// </summary>
+  public class TableElement
+  {
+    private readonly IWebElement table;
+
     /// <summary>
-    ///     Wraps an IWebElement into a user-friendly table-element
+    ///   Initializes a new instance of the <see cref="TableElement" /> class.
     /// </summary>
-    public class TableElement
+    /// <param name="table">The IWebElement that is wrapped</param>
+    public TableElement(IWebElement table)
     {
-        private const string TagName = "table";
-        private readonly IWebElement table;
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="TableElement" /> class.
-        /// </summary>
-        /// <param name="table">The IWebElement that is wrapped as TableElement</param>
-        public TableElement(IWebElement table)
-        {
-            this.CheckType(table);
-            this.table = table;
-        }
-
-        /// <summary>
-        ///     Gets the caption of the table
-        /// </summary>
-        public IWebElement Caption => this.Section("caption");
-
-        /// <summary>
-        ///     Gets the table rows in the header section
-        /// </summary>
-        public IList<TableRowElement> HeaderRows => this.TheRows(this.Section("thead"));
-
-        /// <summary>
-        ///     Gets the table rows in the footer section
-        /// </summary>
-        public IList<TableRowElement> FooterRows => this.TheRows(this.Section("tfoot"));
-
-        /// <summary>
-        ///     Gets the table rows in the body section
-        /// </summary>
-        public IList<TableRowElement> BodyRows => this.TheRows(this.Section("tbody"));
-
-        /// <summary>
-        ///     Gets the table rows in the entire table (any section)
-        /// </summary>
-        public IList<TableRowElement> Rows => this.TheRows(this.table);
-
-        private IWebElement Section(string tagName)
-        {
-            return this.table.FindElement(By.TagName(tagName));
-        }
-
-        private void CheckType(IWebElement element)
-        {
-            if (string.IsNullOrEmpty(element.TagName) ||
-                (string.Compare(element.TagName, TagName, StringComparison.OrdinalIgnoreCase) != 0))
-            {
-                throw new UnexpectedTagNameException(TagName, element.TagName);
-            }
-        }
-
-        private IList<TableRowElement> TheRows(IWebElement parentElement)
-        {
-            var webElements = parentElement.FindElements(By.TagName("tr"));
-            var list = new List<TableRowElement>(webElements.Count);
-            list.AddRange(webElements.Select(webElement => new TableRowElement(webElement)));
-            return list;
-        }
+      CheckType(table);
+      this.table = table;
     }
+
+    /// <summary>
+    ///   The caption of the table
+    /// </summary>
+    public IWebElement Caption => this.Section("caption");
+
+    /// <summary>
+    ///   The table rows in the header section
+    /// </summary>
+    public IList<TableRowElement> HeaderRows => TheRows(this.Section("thead"));
+
+    /// <summary>
+    ///   The table rows in the footer section
+    /// </summary>
+    public IList<TableRowElement> FooterRows => TheRows(this.Section("tfoot"));
+
+    /// <summary>
+    ///   The table rows in the body section
+    /// </summary>
+    public IList<TableRowElement> BodyRows => TheRows(this.Section("tbody"));
+
+    /// <summary>
+    ///   The table rows in the entire table (any section)
+    /// </summary>
+    public IList<TableRowElement> Rows => TheRows(this.table);
+
+    private IWebElement Section(string tagName)
+    {
+      return this.table.FindElement(By.TagName(tagName));
+    }
+
+    private static void CheckType(IWebElement webElement)
+    {
+      if (!webElement.TagName.Equals("table"))
+      {
+        throw new NotTableElementException(webElement.TagName);
+      }
+    }
+
+    private static IList<TableRowElement> TheRows(IWebElement parentElement)
+    {
+      var webElements = parentElement.FindElements(By.TagName("tr"));
+      var list = new List<TableRowElement>(webElements.Count);
+      list.AddRange(webElements.Select(webElement => new TableRowElement(webElement)));
+      return list;
+    }
+  }
 }
