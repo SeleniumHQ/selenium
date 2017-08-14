08954de1db7ddeb8f03cf855f504866da162c627
Jim Evans <james.h.evans.jr@gmail.com>
2017-08-14 13:05:30 -0700

    Adding ability to run Edge via remote in .NET tests

--- dotnet/test/common/Environment/RemoteSeleniumServer.cs
+++ dotnet/test/common/Environment/RemoteSeleniumServer.cs
@@ -36,9 +36,14 @@ namespace OpenQA.Selenium.Environment
                 string ieDriverExe = System.IO.Path.Combine(currentDirectory, "IEDriverServer.exe");
                 string chromeDriverExe = System.IO.Path.Combine(currentDirectory, "chromedriver.exe");
                 string geckoDriverExe = System.IO.Path.Combine(currentDirectory, "geckodriver.exe");
+                string edgeDriverExe = System.IO.Path.Combine(currentDirectory, "MicrosoftWebDriver.exe");
                 webserverProcess = new Process();
                 webserverProcess.StartInfo.FileName = "java.exe";
-                webserverProcess.StartInfo.Arguments = "-Dwebdriver.ie.driver=" + ieDriverExe + " -Dwebdriver.gecko.driver=" + geckoDriverExe + " -Dwebdriver.chrome.driver=" + chromeDriverExe + " -jar " + serverJarName + " -port 6000 -enablePassThrough";
+                webserverProcess.StartInfo.Arguments = "-Dwebdriver.ie.driver=" + ieDriverExe
+                                                     + " -Dwebdriver.gecko.driver=" + geckoDriverExe
+                                                     + " -Dwebdriver.chrome.driver=" + chromeDriverExe
+                                                     + " -Dwebdriver.edge.driver=" + edgeDriverExe
+                                                     + " -jar " + serverJarName + " -port 6000";
                 webserverProcess.StartInfo.WorkingDirectory = projectRootPath;
                 webserverProcess.Start();
                 DateTime timeout = DateTime.Now.Add(TimeSpan.FromSeconds(30));
