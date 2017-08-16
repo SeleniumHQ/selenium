3ebee3f7b177a66a5303f40980d505ec9ec96789
Jim Evans <james.h.evans.jr@gmail.com>
2017-08-16 08:31:15 -0700

    Updating .NET FirefoxOptions to not force specifying binary location
    In the Marionette/Geckodriver case, the service executable can locate the
    Firefox executable on its own when looking in default installation
    locations. There is no need for the language bindings to go spelunking
    through the registry in that case any longer because the bindings do not
    have to directly launch the Firefox executable.

--- dotnet/src/webdriver/Firefox/FirefoxOptions.cs
+++ dotnet/src/webdriver/Firefox/FirefoxOptions.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="FirefoxOptions.cs" company="WebDriver Committers">
+// <copyright file="FirefoxOptions.cs" company="WebDriver Committers">
 // Licensed to the Software Freedom Conservancy (SFC) under one
 // or more contributor license agreements. See the NOTICE file
 // distributed with this work for additional information
@@ -355,12 +355,15 @@ namespace OpenQA.Selenium.Firefox
             }
             else
             {
-                using (FirefoxBinary executablePathBinary = new FirefoxBinary())
+                if (!this.isMarionette)
                 {
-                    string executablePath = executablePathBinary.BinaryExecutable.ExecutablePath;
-                    if (!string.IsNullOrEmpty(executablePath))
+                    using (FirefoxBinary executablePathBinary = new FirefoxBinary())
                     {
-                        firefoxOptions[FirefoxBinaryCapability] = executablePath;
+                        string executablePath = executablePathBinary.BinaryExecutable.ExecutablePath;
+                        if (!string.IsNullOrEmpty(executablePath))
+                        {
+                            firefoxOptions[FirefoxBinaryCapability] = executablePath;
+                        }
                     }
                 }
             }
