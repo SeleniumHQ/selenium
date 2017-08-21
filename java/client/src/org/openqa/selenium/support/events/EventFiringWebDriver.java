ee78fd6049d39282c34a8625cb2320b6079162bd
Joshua Bruning <joshbruning@google.com>
2017-08-21 11:16:47 -0700

    Missed an Entry -> Map.Entry.

--- java/client/src/org/openqa/selenium/support/events/EventFiringWebDriver.java
+++ java/client/src/org/openqa/selenium/support/events/EventFiringWebDriver.java
@@ -274,7 +274,7 @@ public class EventFiringWebDriver implements WebDriver, JavascriptExecutor, Take
       return ((List) result).stream().map(this::wrapResult).collect(Collectors.toList());
     }
     if (result instanceof Map) {
-      Set<Entry<String, Object>> entrySet = ((Map<String, Object>) result).entrySet();
+      Set<Map.Entry<String, Object>> entrySet = ((Map<String, Object>) result).entrySet();
       Map<String, Object> resultMap = new HashMap<>();
       for (Map.Entry<String, Object> entry : entrySet) {
         resultMap.put(entry.getKey(), wrapResult(entry.getValue()));
