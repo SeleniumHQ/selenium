e861986a344c1236f2e5ec8891c35141d16aa84f
Lucas Tierney <lucast1533@gmail.com>
2017-08-22 07:27:25 -0500

    [py] allow IE WebDriver to accept IE Options

--- py/selenium/webdriver/ie/webdriver.py
+++ py/selenium/webdriver/ie/webdriver.py
@@ -19,6 +19,7 @@ from selenium.webdriver.common import utils
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from .service import Service
+from .options import Options
 
 DEFAULT_TIMEOUT = 30
 DEFAULT_PORT = 0
@@ -28,16 +29,40 @@ DEFAULT_LOG_FILE = None
 
 
 class WebDriver(RemoteWebDriver):
+    """ Controls the IEServerDriver and allows you to drive Internet Explorer """
 
     def __init__(self, executable_path='IEDriverServer.exe', capabilities=None,
                  port=DEFAULT_PORT, timeout=DEFAULT_TIMEOUT, host=DEFAULT_HOST,
-                 log_level=DEFAULT_LOG_LEVEL, log_file=DEFAULT_LOG_FILE):
+                 log_level=DEFAULT_LOG_LEVEL, log_file=DEFAULT_LOG_FILE, ie_options=None):
+        """
+        Creates a new instance of the chrome driver.
+
+        Starts the service and then creates new instance of chrome driver.
+
+        :Args:
+         - executable_path - path to the executable. If the default is used it assumes the executable is in the $PATH
+         - capabilities: capabilities Dictionary object
+         - port - port you would like the service to run, if left as 0, a free port will be found.
+         - log_level - log level you would like the service to run.
+         - log_file - log file you would like the service to log to.
+         - ie_options: IE Options instance, providing additional IE options
+        """
         self.port = port
         if self.port == 0:
             self.port = utils.free_port()
         self.host = host
         self.log_level = log_level
         self.log_file = log_file
+
+        if ie_options is None:
+            # desired_capabilities stays as passed in
+            if capabilities is None:
+                capabilities = self.create_options().to_capabilities()
+        else:
+            if capabilities is None:
+                capabilities = ie_options.to_capabilities()
+            else:
+                capabilities.update(ie_options.to_capabilities())
 
         self.iedriver = Service(
             executable_path,
@@ -60,3 +85,6 @@ class WebDriver(RemoteWebDriver):
     def quit(self):
         RemoteWebDriver.quit(self)
         self.iedriver.stop()
+
+    def create_options(self):
+        return Options()
