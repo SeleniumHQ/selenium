9d26333c95dbfadfbf28f0957bda1f77dd9b9687
Alexei Barantsev <barancev@gmail.com>
2017-08-28 18:35:07 +0300

    Fixing a grid test: two new status servlets were added recently

--- java/server/test/org/openqa/grid/internal/utils/SelfRegisteringRemoteTest.java
+++ java/server/test/org/openqa/grid/internal/utils/SelfRegisteringRemoteTest.java
@@ -87,7 +87,7 @@ public class SelfRegisteringRemoteTest {
 
     // there should be three servlets on the remote's map -- The resource servlet, the
     // help servlet, and the one we added above.
-    assertEquals(3, remote.getNodeServlets().size());
+    assertEquals(5, remote.getNodeServlets().size());
     assertEquals(ResourceServlet.class, remote.getNodeServlets().get("/resources/*"));
     assertEquals(DisplayHelpServlet.class,
                  remote.getNodeServlets().get("/extra/DisplayHelpServlet/*"));
