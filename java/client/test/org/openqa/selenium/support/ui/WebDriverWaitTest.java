2494e69275c3e82b63f8413a506856ac200954ec
Tom Trumper <tom-trumper@hotmail.co.uk>
2017-09-12 15:07:49 +0100

    Improved TimeoutException for wrapped WebDrivers
    Modified WebDriverWait TimeoutException to include session ID and
    capabilities for WebDrivers that are wrapped e.g. in an
    EventFiringWebDriver instance.
    
    Fixes #4408.
    
    Signed-off-by: Alexei Barantsev <barancev@gmail.com>

--- java/client/test/org/openqa/selenium/support/ui/WebDriverWaitTest.java
+++ java/client/test/org/openqa/selenium/support/ui/WebDriverWaitTest.java
@@ -17,10 +17,17 @@
 
 package org.openqa.selenium.support.ui;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+import static org.openqa.selenium.testing.TestUtilities.catchThrowable;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -28,12 +35,22 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsDriver;
+import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.CommandExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.Response;
+import org.openqa.selenium.remote.SessionId;
+
+import java.io.IOException;
 
 @RunWith(JUnit4.class)
 public class WebDriverWaitTest {
@@ -44,6 +61,28 @@ public class WebDriverWaitTest {
   @Before
   public void createMocks() {
     MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void shouldIncludeRemoteInfoForWrappedDriverTimeout() throws IOException {
+    Capabilities caps = new MutableCapabilities();
+    Response response = new Response(new SessionId("foo"));
+    response.setValue(caps.asMap());
+    CommandExecutor executor = mock(CommandExecutor.class);
+    when(executor.execute(any(Command.class))).thenReturn(response);
+
+    RemoteWebDriver driver = new RemoteWebDriver(executor, caps);
+    WebDriver testDriver = mock(WebDriver.class, withSettings().extraInterfaces(WrapsDriver.class));
+    when(((WrapsDriver) testDriver).getWrappedDriver()).thenReturn(driver);
+
+    TickingClock clock = new TickingClock(200);
+    WebDriverWait wait = new WebDriverWait(testDriver, clock, clock, 1, 200);
+
+    Throwable ex = catchThrowable(() -> wait.until((d) -> false));
+    assertNotNull(ex);
+    assertThat(ex, instanceOf(TimeoutException.class));
+    assertThat(ex.getMessage(), containsString("Capabilities [{javascriptEnabled=true, platformName=ANY, platform=ANY}]"));
+    assertThat(ex.getMessage(), containsString("Session ID: foo"));
   }
 
   @Test
@@ -105,4 +144,3 @@ public class WebDriverWaitTest {
     }
   }
 }
-
