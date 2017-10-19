85ab1d162c94ea57856478bb366aabf7aa780bea
Jayson Smith <github@nes.33mail.com>
2017-10-19 07:35:08 -0700

    Rebased from Master

--- rb/lib/selenium/webdriver/common/error.rb
+++ rb/lib/selenium/webdriver/common/error.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+#
 # Licensed to the Software Freedom Conservancy (SFC) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -19,96 +21,103 @@ module Selenium
   module WebDriver
     module Error
       class WebDriverError < StandardError; end
-      class UnsupportedOperationError < WebDriverError; end
-
-      class IndexOutOfBoundsError < WebDriverError; end          # 1
-      class NoCollectionError < WebDriverError; end              # 2
-      class NoStringError < WebDriverError; end                  # 3
-      class NoStringLengthError < WebDriverError; end            # 4
-      class NoStringWrapperError < WebDriverError; end           # 5
-      class NoSuchDriverError < WebDriverError; end              # 6
 
       #
-      # An element could not be located on the page using the given search
-      # parameters.
+      # Indicates that a command that should have executed properly cannot be supported for some
+      # reason.
+      #
+
+      class UnsupportedOperationError < WebDriverError; end
+
+      class IndexOutOfBoundsError < WebDriverError; end # 1
+      class NoCollectionError < WebDriverError; end # 2
+      class NoStringError < WebDriverError; end # 3
+      class NoStringLengthError < WebDriverError; end # 4
+      class NoStringWrapperError < WebDriverError; end # 5
+      class NoSuchDriverError < WebDriverError; end # 6
+
+      #
+      # An element could not be located on the page using the given search parameters.
       #
 
       class NoSuchElementError < WebDriverError; end # 7
 
       #
-      # A request to switch to a frame could not be satisfied because the
-      # frame could not be found.
+      # A command to switch to a frame could not be satisfied because the frame could not be found.
       #
 
-      class NoSuchFrameError < WebDriverError; end               # 8
-      class UnknownCommandError < WebDriverError; end            # 9
+      class NoSuchFrameError < WebDriverError; end # 8
 
       #
-      # Indicates that a reference to an element is now "stale" - the element
-      # no longer appears in the DOM of the page.
+      # A command could not be executed because the remote end is not aware of it.
+      #
+
+      class UnknownCommandError < WebDriverError; end # 9
+
+      #
+      # A command failed because the referenced element is no longer attached to the DOM.
       #
 
       class StaleElementReferenceError < WebDriverError; end # 10
 
       #
-      # Raised to indicate that although an element is present on the DOM,
-      # it is not visible, and so is not able to be interacted with.
+      # Raised to indicate that although an element is present on the DOM, it is not visible, and
+      # so is not able to be interacted with.
       #
 
       class ElementNotVisibleError < WebDriverError; end # 11
 
       #
-      # Raised when an interaction could not be performed because the element
-      # is in an invalid state (e.g. attempting to click a disabled element).
+      # The target element is in an invalid state, rendering it impossible to interact with, for
+      # example if you click a disabled element.
       #
 
       class InvalidElementStateError < WebDriverError; end # 12
 
       #
-      # An unknown server-side error occurred while processing the command.
+      # An unknown error occurred in the remote end while processing the command.
       #
 
-      class UnknownError < WebDriverError; end                   # 13
-      class ExpectedError < WebDriverError; end                  # 14
+      class UnknownError < WebDriverError; end # 13
+      class ExpectedError < WebDriverError; end # 14
 
       #
       # An attempt was made to select an element that cannot be selected.
       #
 
-      class ElementNotSelectableError < WebDriverError; end      # 15
-      class NoSuchDocumentError < WebDriverError; end            # 16
+      class ElementNotSelectableError < WebDriverError; end # 15
+      class NoSuchDocumentError < WebDriverError; end # 16
 
       #
-      # An error occurred while executing user supplied JavaScript.
+      # An error occurred while executing JavaScript supplied by the user.
       #
 
-      class JavascriptError < WebDriverError; end                # 17
-      class NoScriptResultError < WebDriverError; end            # 18
+      class JavascriptError < WebDriverError; end # 17
+      class NoScriptResultError < WebDriverError; end # 18
 
       #
       # An error occurred while searching for an element by XPath.
       #
 
-      class XPathLookupError < WebDriverError; end               # 19
-      class NoSuchCollectionError < WebDriverError; end          # 20
+      class XPathLookupError < WebDriverError; end # 19
+      class NoSuchCollectionError < WebDriverError; end # 20
 
       #
-      # Raised when a command does not complete in enough time.
+      # An operation did not complete before its timeout expired.
       #
 
-      class TimeOutError < WebDriverError; end                   # 21
-      class NullPointerError < WebDriverError; end               # 22
-      class NoSuchWindowError < WebDriverError; end              # 23
+      class TimeOutError < WebDriverError; end # 21
+      class NullPointerError < WebDriverError; end # 22
+      class NoSuchWindowError < WebDriverError; end # 23
 
       #
-      # Raised when attempting to add a cookie under a different domain than
-      # the current URL.
+      # An illegal attempt was made to set a cookie under a different domain than the current page.
       #
 
       class InvalidCookieDomainError < WebDriverError; end # 24
 
       #
-      # Raised when a driver fails to set a cookie.
+      # A command to set a cookie’s value could not be satisfied.
       #
 
       class UnableToSetCookieError < WebDriverError; end # 25
@@ -137,9 +146,8 @@ module Selenium
       class InvalidElementCoordinatesError < WebDriverError; end # 29
 
       #
-      # Indicates that IME support is not available. This exception is rasied
-      # for every IME-related method call if IME support is not available on
-      # the machine.
+      # Indicates that IME support is not available. This exception is rasied for every IME-related
+      # method call if IME support is not available on the machine.
       #
 
       class IMENotAvailableError < WebDriverError; end # 30
@@ -151,7 +159,7 @@ module Selenium
       class IMEEngineActivationFailedError < WebDriverError; end # 31
 
       #
-      # Argument was an invalid selector (e.g. XPath/CSS).
+      # Argument was an invalid selector.
       #
 
       class InvalidSelectorError < WebDriverError; end # 32
@@ -163,54 +171,134 @@ module Selenium
       class SessionNotCreatedError < WebDriverError; end # 33
 
       #
-      # Indicates that the target provided to the actions #move method is
-      # invalid, e.g. outside of the bounds of the window.
+      # The target for mouse interaction is not in the browser’s viewport and cannot be brought
+      # into that viewport.
       #
 
       class MoveTargetOutOfBoundsError < WebDriverError; end # 34
 
+      #
+      # Indicates that the XPath selector is invalid
+      #
+
+      class InvalidXpathSelectorError < WebDriverError; end # 51
+      class InvalidXpathSelectorReturnTyperError < WebDriverError; end # 52
+
+      #
+      # A command could not be completed because the element is not pointer or keyboard
+      # interactable.
+      #
+
+      class ElementNotInteractableError < WebDriverError; end # 60
+
+      #
+      # The arguments passed to a command are either invalid or malformed.
+      #
+
+      class InvalidArgumentError < WebDriverError; end # 61
+
+      #
+      # No cookie matching the given path name was found amongst the associated cookies of the
+      # current browsing context’s active document.
+      #
+
+      class NoSuchCookieError < WebDriverError; end # 62
+
+      #
+      # A screen capture was made impossible.
+      #
+
+      class UnableToCaptureScreenError < WebDriverError; end # 63
+
+      #
+      # Occurs if the given session id is not in the list of active sessions, meaning the session
+      # either does not exist or that it’s not active.
+      #
+
+      class InvalidSessionIdError < WebDriverError; end
+
+      #
+      # An attempt was made to operate on a modal dialog when one was not open.
+      #
+
+      class NoSuchAlertError < WebDriverError; end
+
+      #
+      # A modal dialog was open, blocking this operation.
+      #
+
+      class UnexpectedAlertOpenError < WebDriverError; end
+
+      #
+      # The requested command matched a known URL but did not match an method for that URL.
+      #
+
+      class UnknownMethodError < WebDriverError; end
+
+      #
+      # The Element Click command could not be completed because the element receiving the events
+      # is obscuring the element that was requested clicked.
+      #
+
+      class ElementClickInterceptedError < WebDriverError; end
+
+      # aliased for backwards compatibility
+      NoAlertPresentError       = NoSuchAlertError
+      ScriptTimeOutError        = ScriptTimeoutError
+      ObsoleteElementError      = StaleElementReferenceError
+      UnhandledError            = UnknownError
+      UnexpectedJavascriptError = JavascriptError
+      NoAlertOpenError          = NoAlertPresentError
+      ElementNotDisplayedError  = ElementNotVisibleError
+
       # @api private
-      ERRORS = [
-        IndexOutOfBoundsError,          # 1
-        NoCollectionError,              # 2
-        NoStringError,                  # 3
-        NoStringLengthError,            # 4
-        NoStringWrapperError,           # 5
-        NoSuchDriverError,              # 6
-        NoSuchElementError,             # 7
-        NoSuchFrameError,               # 8
-        UnknownCommandError,            # 9
-        StaleElementReferenceError,     # 10
-        ElementNotVisibleError,         # 11
-        InvalidElementStateError,       # 12
-        UnknownError,                   # 13
-        ExpectedError,                  # 14
-        ElementNotSelectableError,      # 15
-        NoSuchDocumentError,            # 16
-        JavascriptError,                # 17
-        NoScriptResultError,            # 18
-        XPathLookupError,               # 19
-        NoSuchCollectionError,          # 20
-        TimeOutError,                   # 21
-        NullPointerError,               # 22
-        NoSuchWindowError,              # 23
-        InvalidCookieDomainError,       # 24
-        UnableToSetCookieError,         # 25
-        UnhandledAlertError,            # 26
-        NoAlertPresentError,            # 27
-        ScriptTimeOutError,             # 28
-        InvalidElementCoordinatesError, # 29
-        IMENotAvailableError,           # 30
-        IMEEngineActivationFailedError, # 31
-        InvalidSelectorError,           # 32
-        SessionNotCreatedError,         # 33
-        MoveTargetOutOfBoundsError      # 34
-      ].freeze
+      ERRORS = {
+        1 => IndexOutOfBoundsError,
+        2 => NoCollectionError,
+        3 => NoStringError,
+        4 => NoStringLengthError,
+        5 => NoStringWrapperError,
+        6 => NoSuchDriverError,
+        7 => NoSuchElementError,
+        8 => NoSuchFrameError,
+        9 => UnknownCommandError,
+        10 => StaleElementReferenceError,
+        11 => ElementNotVisibleError,
+        12 => InvalidElementStateError,
+        13 => UnknownError,
+        14 => ExpectedError,
+        15 => ElementNotSelectableError,
+        16 => NoSuchDocumentError,
+        17 => JavascriptError,
+        18 => NoScriptResultError,
+        19 => XPathLookupError,
+        20 => NoSuchCollectionError,
+        21 => TimeOutError,
+        22 => NullPointerError,
+        23 => NoSuchWindowError,
+        24 => InvalidCookieDomainError,
+        25 => UnableToSetCookieError,
+        26 => UnhandledAlertError,
+        27 => NoAlertPresentError,
+        28 => ScriptTimeOutError,
+        29 => InvalidElementCoordinatesError,
+        30 => IMENotAvailableError,
+        31 => IMEEngineActivationFailedError,
+        32 => InvalidSelectorError,
+        33 => SessionNotCreatedError,
+        34 => MoveTargetOutOfBoundsError,
+        51 => InvalidXpathSelectorError,
+        52 => InvalidXpathSelectorReturnTyperError,
+        60 => ElementNotInteractableError,
+        61 => InvalidArgumentError,
+        62 => NoSuchCookieError,
+        63 => UnableToCaptureScreenError
+      }.freeze
 
       class << self
         def for_code(code)
           return if [nil, 0].include? code
-          return ERRORS[code - 1] if code.is_a? Integer
+          return ERRORS.fetch(code) if code.is_a? Integer
 
           klass_name = code.split(' ').map(&:capitalize).join
           Error.const_get("#{klass_name.gsub('Error', '')}Error")
