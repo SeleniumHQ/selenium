de5c81fd86a3228195d2f6d5d9526bbc4b3c3534
Simon Stewart <simon.m.stewart@gmail.com>
2017-11-15 04:23:41 -0800

    Remove PhantomJS from the project

--- java/client/test/org/openqa/selenium/testing/drivers/WebDriverBuilder.java
+++ java/client/test/org/openqa/selenium/testing/drivers/WebDriverBuilder.java
@@ -84,7 +84,6 @@ public class WebDriverBuilder implements Supplier<WebDriver> {
     suppliers.add(new SauceBackedDriverSupplier(desiredCaps));
     suppliers.add(new GridSupplier(desiredCaps));
     suppliers.add(new RemoteSupplier(desiredCaps));
-    suppliers.add(new PhantomJSDriverSupplier(desiredCaps));
     suppliers.add(new TestInternetExplorerSupplier(desiredCaps));
     suppliers.add(new ReflectionBackedDriverSupplier(desiredCaps));
     suppliers.add(new DefaultDriverSupplier(desiredCaps));
