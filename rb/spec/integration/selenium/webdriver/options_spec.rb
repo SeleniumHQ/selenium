7452a085bbed749a46003b6193632d1fb923f8ef
Titus Fortner <titusfortner@gmail.com>
2017-11-17 11:26:13 -0600

    [rb] fix logging specs

--- rb/spec/integration/selenium/webdriver/options_spec.rb
+++ rb/spec/integration/selenium/webdriver/options_spec.rb
@@ -20,7 +20,7 @@ require_relative 'spec_helper'
 module Selenium
   module WebDriver
     describe Options do
-      describe 'logs', except: {browser: %i[firefox ie edge safari safari_preview]} do
+      describe 'logs', only: {browser: %i[ff_esr chrome]} do
         it 'can fetch remote log types', only: {driver: :remote} do
           expect(driver.manage.logs.available_types).to include(:server, :browser, :driver)
         end
@@ -30,9 +30,9 @@ module Selenium
           expect(driver.manage.logs.available_types).not_to include(:server)
         end
 
-        # TODO (Alex): Investigate why this spec passes on macOS but fails on Linux.
-        it 'can get the browser log', only: {browser: %i[firefox ff_esr]}, except: {browser: :chrome, platform: :linux} do
-          driver.navigate.to url_for('simpleTest.html')
+        it 'can get the browser log' do
+          driver.navigate.to url_for('errors.html')
+          driver.find_element(tag_name: 'input').click
 
           entries = driver.manage.logs.get(:browser)
           expect(entries).not_to be_empty
