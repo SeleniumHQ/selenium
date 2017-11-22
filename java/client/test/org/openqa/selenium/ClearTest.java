0233d3b4619528b127e087e4a994c8bb3cc5df58
Alexei Barantsev <barancev@gmail.com>
2017-11-22 12:23:53 +0300

    Adding tests for clear operation for all types of inputs.

--- java/client/test/org/openqa/selenium/ClearTest.java
+++ java/client/test/org/openqa/selenium/ClearTest.java
@@ -20,10 +20,16 @@ package org.openqa.selenium;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.openqa.selenium.testing.Driver.CHROME;
+import static org.openqa.selenium.testing.Driver.FIREFOX;
+import static org.openqa.selenium.testing.Driver.HTMLUNIT;
+import static org.openqa.selenium.testing.Driver.IE;
+import static org.openqa.selenium.testing.Driver.MARIONETTE;
 import static org.openqa.selenium.testing.TestUtilities.catchThrowable;
 
 import org.junit.Test;
 import org.openqa.selenium.testing.JUnit4TestBase;
+import org.openqa.selenium.testing.NotYetImplemented;
 
 public class ClearTest extends JUnit4TestBase {
 
@@ -83,4 +89,124 @@ public class ClearTest extends JUnit4TestBase {
     element.clear();
     assertEquals("", element.getText());
   }
+
+  @Test
+  public void shouldBeAbleToClearNoTypeInput() {
+    shouldBeAbleToClearInput(By.name("no_type"), "input with no type");
+  }
+
+  @Test
+  public void shouldBeAbleToClearNumberInput() {
+    shouldBeAbleToClearInput(By.name("number_input"), "42");
+  }
+
+  @Test
+  public void shouldBeAbleToClearEmailInput() {
+    shouldBeAbleToClearInput(By.name("email_input"), "admin@localhost");
+  }
+
+  @Test
+  public void shouldBeAbleToClearPasswordInput() {
+    shouldBeAbleToClearInput(By.name("password_input"), "qwerty");
+  }
+
+  @Test
+  public void shouldBeAbleToClearSearchInput() {
+    shouldBeAbleToClearInput(By.name("search_input"), "search");
+  }
+
+  @Test
+  public void shouldBeAbleToClearTelInput() {
+    shouldBeAbleToClearInput(By.name("tel_input"), "911");
+  }
+
+  @Test
+  public void shouldBeAbleToClearTextInput() {
+    shouldBeAbleToClearInput(By.name("text_input"), "text input");
+  }
+
+  @Test
+  public void shouldBeAbleToClearUrlInput() {
+    shouldBeAbleToClearInput(By.name("url_input"), "http://seleniumhq.org/");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(FIREFOX)
+  @NotYetImplemented(MARIONETTE)
+  @NotYetImplemented(IE)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearRangeInput() {
+    shouldBeAbleToClearInput(By.name("range_input"), "42");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(FIREFOX)
+  @NotYetImplemented(MARIONETTE)
+  @NotYetImplemented(IE)
+  public void shouldBeAbleToClearCheckboxInput() {
+    shouldBeAbleToClearInput(By.name("checkbox_input"), "Checkbox");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(FIREFOX)
+  @NotYetImplemented(MARIONETTE)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearColorInput() {
+    shouldBeAbleToClearInput(By.name("color_input"), "#00ffff");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(MARIONETTE)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearDateInput() {
+    shouldBeAbleToClearInput(By.name("date_input"), "2017-11-22");
+  }
+
+  @Test
+  public void shouldBeAbleToClearDatetimeInput() {
+    shouldBeAbleToClearInput(By.name("datetime_input"), "2017-11-22T11:22");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(HTMLUNIT)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearDatetimeLocalInput() {
+    shouldBeAbleToClearInput(By.name("datetime_local_input"), "2017-11-22T11:22");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(MARIONETTE)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearTimeInput() {
+    shouldBeAbleToClearInput(By.name("time_input"), "11:22");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearMonthInput() {
+    shouldBeAbleToClearInput(By.name("month_input"), "2017-11");
+  }
+
+  @Test
+  @NotYetImplemented(CHROME)
+  @NotYetImplemented(HTMLUNIT)
+  public void shouldBeAbleToClearWeekInput() {
+    shouldBeAbleToClearInput(By.name("week_input"), "2017-W47");
+  }
+
+  private void shouldBeAbleToClearInput(By locator, String oldValue) {
+    driver.get(appServer.whereIs("inputs.html"));
+    WebElement element = driver.findElement(locator);
+    assertEquals(oldValue, element.getAttribute("value"));
+    element.clear();
+    assertEquals("", element.getAttribute("value"));
+  }
+
 }
