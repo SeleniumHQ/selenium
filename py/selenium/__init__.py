c5e4a585f1e3c4b3468ec93ab50d3668d47aef1b
lmtierney <lucast1533@gmail.com>
2018-01-04 13:06:33 -0600

    [py] update CHANGES and bump version to 3.8.1

--- py/selenium/__init__.py
+++ py/selenium/__init__.py
@@ -16,4 +16,4 @@
 # under the License.
 
 
-__version__ = "3.8.0"
+__version__ = "3.8.1"
--- py/selenium/webdriver/__init__.py
+++ py/selenium/webdriver/__init__.py
@@ -35,4 +35,4 @@ from .common.action_chains import ActionChains  # noqa
 from .common.touch_actions import TouchActions  # noqa
 from .common.proxy import Proxy  # noqa
 
-__version__ = '3.8.0'
+__version__ = '3.8.1'
