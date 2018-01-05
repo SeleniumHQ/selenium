432daf00fed09ad3e2fefb1dda13f2984a8c7711
Graham Russell <graham@ham1.co.uk>
2018-01-06 01:01:35 +0800

    Remove * imports

--- java/client/src/org/openqa/selenium/opera/OperaOptions.java
+++ java/client/src/org/openqa/selenium/opera/OperaOptions.java
@@ -27,7 +27,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.CapabilityType;
 
 import java.io.File;
