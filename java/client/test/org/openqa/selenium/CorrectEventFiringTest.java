0a9b576c011a837aaed01f76b56fcf526d47329e
Alexei Barantsev <barancev@gmail.com>
2018-01-15 15:45:37 +0300

    Ignoring and unignoring tests to match current state of Marionette (Nightly)

--- java/client/test/org/openqa/selenium/CorrectEventFiringTest.java
+++ java/client/test/org/openqa/selenium/CorrectEventFiringTest.java
@@ -277,6 +277,7 @@ public class CorrectEventFiringTest extends JUnit4TestBase {
   }
 
   @Test
+  @NotYetImplemented(value = MARIONETTE, reason = "https://bugzilla.mozilla.org/show_bug.cgi?id=1430571")
   public void testClearingAnElementShouldCauseTheOnChangeHandlerToFire() {
     driver.get(pages.javascriptPage);
 
