6fcca10e899d7e72e229a21801db29c4ba48c263
Simon Stewart <simon.m.stewart@gmail.com>
2018-01-20 16:35:26 +0000

    [travis] Let's find out if X is working

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,16 +1,6 @@
 set -ex
 
-sh /etc/init.d/xvfb start &
-
-for i in 1 2 3 4 5 6 7 8 9
-do
-    xdpyinfo >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    sleep 0.5
-done
-
+sh /etc/init.d/xvfb start
 
 if [[ ! -z $TOXENV ]]; then
   . jdk_switcher.sh && jdk_switcher use oraclejdk8
