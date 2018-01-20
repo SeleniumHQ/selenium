2235dc832f02cb5fa95c268a40d14a916a2bda93
Simon Stewart <simon.m.stewart@gmail.com>
2018-01-20 16:25:23 +0000

    [travis] more tweaking

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,6 +1,16 @@
 set -ex
 
-sh /etc/init.d/xvfb start
+sh /etc/init.d/xvfb start &
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+    xdpyinfo >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 0.5
+done
+
 
 if [[ ! -z $TOXENV ]]; then
   . jdk_switcher.sh && jdk_switcher use oraclejdk8
