eb1788d37808142f9e6325fd80e5e430ffcbb484
Alexei Barantsev <barancev@gmail.com>
2018-02-27 20:25:52 +0300

    Oops... Enabling standard tests back in Safari

--- java/client/test/org/openqa/selenium/safari/SafariDriverTests.java
+++ java/client/test/org/openqa/selenium/safari/SafariDriverTests.java
@@ -28,7 +28,7 @@ import org.openqa.selenium.testing.TestUtilities;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    //StandardSeleniumTests.class,
+    StandardSeleniumTests.class,
     //AlertsTest.class,
     CleanSessionTest.class,
     CrossDomainTest.class,
