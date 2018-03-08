35e97c8db62efe80228a6c8e3bdeb748c7f545c2
morrishoresh <morrish@perfectomobile.com>
2018-03-08 18:13:42 +0200

    Revert "Revert "Changing server-specific capabilities prefix. Fixes #5502""
    This reverts commit e2c80576bf47dcb30df098f2dafd0d0d1becaabd.

--- java/server/src/org/openqa/grid/selenium/node/FirefoxMutator.java
+++ java/server/src/org/openqa/grid/selenium/node/FirefoxMutator.java
@@ -31,7 +31,7 @@ import java.util.function.Function;
 
 public class FirefoxMutator implements Function<Capabilities, Capabilities> {
 
-  private static final String CONFIG_UUID_CAPABILITY = "se:CONFIG_UUID";
+  private static final String CONFIG_UUID_CAPABILITY = "server:CONFIG_UUID";
 
   private final Capabilities config;
 
