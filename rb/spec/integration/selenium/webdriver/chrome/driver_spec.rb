f8a68689b6a741d1ec204c0490c877e1e3939320
Pulkit Sharma <avastpulkit@gmail.com>
2018-03-08 23:46:47 +0530

    [rb] Fixing headless specs, Adding download_test.html file instead of using data URLs.

--- rb/spec/integration/selenium/webdriver/chrome/driver_spec.rb
+++ rb/spec/integration/selenium/webdriver/chrome/driver_spec.rb
@@ -52,45 +52,53 @@ module Selenium
             expect(driver.title).to eq('Color Page')
           end
 
-          describe('#set_download_path') do
-            let(:download_path) { Dir.mktmpdir }
-            let(:file_to_download) { "blank.html" }
-            let(:final_path_to_downloaded_file) { download_path + "/" + file_to_download }
-            let(:page_to_download_from) do
-              'data:text/html,'\
-              '<!DOCTYPE html>'\
-                '<div>'\
-                  '<a download="" href="' + url_for(file_to_download) + '">Go!</a>'\
-                '</div>'\
-              '</html>'
+          describe('#download_path=') do
+            before(:all) do
+              @page_to_download_from = "file://" + File.expand_path('../download_test.html', __FILE__)
+              @download_dir = Dir.mktmpdir
+              @downloaded_file_path = @download_dir + "/sample.crx"
             end
 
-            before(:each) do
-              File.delete(final_path_to_downloaded_file) if File.exist?(final_path_to_downloaded_file)
+            after(:all) do
+              FileUtils.rm_r(@download_dir)
             end
 
+            after(:each) do
+              File.delete(@downloaded_file_path) if File.exist?(@downloaded_file_path)
+            end
+
+            # Works since Chrome 65 / chromedriver 2.36
             it 'can download files in headless mode' do
               options = Selenium::WebDriver::Chrome::Options.new
               options.headless!
-              local_driver = WebDriver::Driver.for(:chrome, options: options)
-              local_driver.download_path = download_path
-              local_driver.get(page_to_download_from)
 
-              local_driver.find_element(css: 'a').click
+              create_driver!(options: options) do |driver|
+                driver.download_path = @download_dir
+                driver.get(@page_to_download_from)
+                driver.find_element(css: 'a').click
 
-              sleep 3
-              local_driver.quit
-              expect(File).to exist(final_path_to_downloaded_file)
+                # Wait for file to download
+                5.times do
+                  break if File.exist?(@downloaded_file_path)
+                  sleep 1
+                end
+              end
+
+              expect(File).to exist(@downloaded_file_path)
             end
 
             it 'can download files in non-headless mode' do
-              driver.download_path = download_path
-              driver.get(page_to_download_from)
-
+              driver.download_path = @download_dir
+              driver.get(@page_to_download_from)
               driver.find_element(css: 'a').click
 
-              sleep 3
-              expect(File).to exist(final_path_to_downloaded_file)
+              # Wait for file to download
+              5.times do
+                break if File.exist?(@downloaded_file_path)
+                sleep 1
+              end
+
+              expect(File).to exist(@downloaded_file_path)
             end
           end
         end
