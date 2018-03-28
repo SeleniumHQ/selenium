2e85a4dbca9e3debfa260bfd22c855f2b70032af
Phillip Haydon <phillip.haydon@gmail.com>
2018-03-28 17:11:52 +0800

    Updated docs from ChromeDriver to FirefoxDriver

--- dotnet/src/webdriver/Firefox/FirefoxDriver.cs
+++ dotnet/src/webdriver/Firefox/FirefoxDriver.cs
@@ -175,7 +175,7 @@ namespace OpenQA.Selenium.Firefox
 
         /// <summary>
         /// Initializes a new instance of the <see cref="FirefoxDriver"/> class using the specified path
-        /// to the directory containing ChromeDriver.exe.
+        /// to the directory containing geckodriver.exe.
         /// </summary>
         /// <param name="geckoDriverDirectory">The full path to the directory containing geckodriver.exe.</param>
         public FirefoxDriver(string geckoDriverDirectory)
@@ -184,22 +184,22 @@ namespace OpenQA.Selenium.Firefox
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ChromeDriver"/> class using the specified path
-        /// to the directory containing ChromeDriver.exe and options.
+        /// Initializes a new instance of the <see cref="FirefoxDriver"/> class using the specified path
+        /// to the directory containing geckodriver.exe and options.
         /// </summary>
-        /// <param name="geckoDriverDirectory">The full path to the directory containing ChromeDriver.exe.</param>
-        /// <param name="options">The <see cref="ChromeOptions"/> to be used with the Chrome driver.</param>
+        /// <param name="geckoDriverDirectory">The full path to the directory containing geckodriver.exe.</param>
+        /// <param name="options">The <see cref="FirefoxOptions"/> to be used with the Firefox driver.</param>
         public FirefoxDriver(string geckoDriverDirectory, FirefoxOptions options)
             : this(geckoDriverDirectory, options, RemoteWebDriver.DefaultCommandTimeout)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ChromeDriver"/> class using the specified path
-        /// to the directory containing ChromeDriver.exe, options, and command timeout.
+        /// Initializes a new instance of the <see cref="FirefoxDriver"/> class using the specified path
+        /// to the directory containing geckodriver.exe, options, and command timeout.
         /// </summary>
-        /// <param name="geckoDriverDirectory">The full path to the directory containing ChromeDriver.exe.</param>
-        /// <param name="options">The <see cref="ChromeOptions"/> to be used with the Chrome driver.</param>
+        /// <param name="geckoDriverDirectory">The full path to the directory containing geckodriver.exe.</param>
+        /// <param name="options">The <see cref="FirefoxOptions"/> to be used with the Firefox driver.</param>
         /// <param name="commandTimeout">The maximum amount of time to wait for each command.</param>
         public FirefoxDriver(string geckoDriverDirectory, FirefoxOptions options, TimeSpan commandTimeout)
             : this(FirefoxDriverService.CreateDefaultService(geckoDriverDirectory), options, commandTimeout)
