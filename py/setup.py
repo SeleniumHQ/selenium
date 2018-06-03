fd26a9b00e34794e1c0d9d1bcdd74fc01be1f2c1
wiggin15 <arnony@infinidat.com>
2018-06-03 17:14:28 +0300

    Remove package_data and data_files options from setup.py
    With `include_package_data` option set to `True` in `setup.py`, package data that is specified in MANIFEST.in is installed into the target directory when installing selenium. This means that we don't need to specify `package_data` and `data_files` in setup.py too. With the options set as before, files were included twice in wheel distributions (once in their original place and once in the ".data" directory) and that triggered a bug that caused setuptools to fail to install the wheel distribution of selenium.

--- py/setup.py
+++ py/setup.py
@@ -67,14 +67,6 @@ setup_args = {
                  'selenium.webdriver.phantomjs',
                  'selenium.webdriver.remote',
                  'selenium.webdriver.support', ],
-    'package_data': {
-        'selenium.webdriver.firefox': ['*.xpi', 'webdriver_prefs.json'],
-        'selenium.webdriver.remote': ['getAttribute.js', 'isDisplayed.js'],
-    },
-    'data_files': [('selenium/webdriver/firefox/x86', ['selenium/webdriver/firefox/x86/x_ignore_nofocus.so']),
-                   ('selenium/webdriver/firefox/amd64', ['selenium/webdriver/firefox/amd64/x_ignore_nofocus.so']),
-                   ('selenium/webdriver/remote', ['selenium/webdriver/remote/getAttribute.js']),
-                   ('selenium/webdriver/remote', ['selenium/webdriver/remote/isDisplayed.js'])],
     'include_package_data': True,
     'zip_safe': False
 }
