4c1eca1853d9e8c6c10d1a9cf14491b3d89be068
Jim Evans <james.h.evans.jr@gmail.com>
2018-06-04 17:44:32 -0700

    Fixing null pointer exception for .NET on Safari with mouse movements

--- dotnet/src/webdriver/Remote/RemoteMouse.cs
+++ dotnet/src/webdriver/Remote/RemoteMouse.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="RemoteMouse.cs" company="WebDriver Committers">
+// <copyright file="RemoteMouse.cs" company="WebDriver Committers">
 // Licensed to the Software Freedom Conservancy (SFC) under one
 // or more contributor license agreements. See the NOTICE file
 // distributed with this work for additional information
@@ -110,7 +110,7 @@ namespace OpenQA.Selenium.Remote
             }
             else
             {
-                parameters.Add("element", null);
+                parameters.Add("element", string.Empty);
             }
 
             parameters.Add("xoffset", offsetX);
