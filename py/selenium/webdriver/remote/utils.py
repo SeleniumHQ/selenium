02f793dd3779444de1232417ea2135a553517aa5
AutomatedTester <david.burns@theautomatedtester.co.uk>
2018-06-29 15:48:40 +0100

    [py] Remove unused util methods from remote

--- py/selenium/webdriver/remote/utils.py
+++ py/selenium/webdriver/remote/utils.py
@@ -38,28 +38,6 @@ def load_json(s):
     return json.loads(s)
 
 
-def handle_find_element_exception(e):
-    if ("Unable to find" in e.response["value"]["message"] or "Unable to locate" in e.response["value"]["message"]):
-        raise NoSuchElementException("Unable to locate element:")
-    else:
-        raise e
-
-
-def return_value_if_exists(resp):
-    if resp and "value" in resp:
-        return resp["value"]
-
-
-def get_root_parent(elem):
-    parent = elem.parent
-    while True:
-        try:
-            parent.parent
-            parent = parent.parent
-        except AttributeError:
-            return parent
-
-
 def unzip_to_temp_dir(zip_file_name):
     """Unzip zipfile to a temporary directory.
 
