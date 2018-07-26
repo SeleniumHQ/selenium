a3d2f41cd392c9842e3b37be673ec0e287c58ce8
Alex Rodionov <p0deje@gmail.com>
2018-07-26 14:49:59 +0700

    Allow to use multiple files with file detector

--- rb/spec/integration/selenium/webdriver/remote/element_spec.rb
+++ rb/spec/integration/selenium/webdriver/remote/element_spec.rb
@@ -19,25 +19,41 @@ require_relative '../spec_helper'
 
 module Selenium
   module WebDriver
-    describe Element, only: {driver: :remote, browser: %i[chrome ff_esr firefox]} do
+    describe Element, only: {driver: :remote} do
       before do
-        driver.file_detector = ->(_str) { __FILE__ }
+        driver.file_detector = ->(filename) { File.join(__dir__, filename) }
       end
 
       after do
         driver.file_detector = nil
       end
 
-      it 'uses the file detector' do
-        driver.navigate.to url_for('upload.html')
+      context 'when uploading one file', only: {browser: %i[chrome ff_esr firefox ie]} do
+        it 'uses the file detector' do
+          driver.navigate.to url_for('upload.html')
 
-        driver.find_element(id: 'upload').send_keys('random string')
-        driver.find_element(id: 'go').submit
-        wait.until { driver.find_element(id: 'upload_label').displayed? }
+          driver.find_element(id: 'upload').send_keys('element_spec.rb')
+          driver.find_element(id: 'go').submit
+          wait.until { driver.find_element(id: 'upload_label').displayed? }
 
-        driver.switch_to.frame('upload_target')
-        body = driver.find_element(xpath: '//body')
-        expect(body.text).to include('uses the set file detector')
+          driver.switch_to.frame('upload_target')
+          body = driver.find_element(xpath: '//body')
+          expect(body.text.scan('Licensed to the Software Freedom Conservancy').count).to eq(3)
+        end
+      end
+
+      context 'when uploading multiple files', only: {browser: %i[chrome ie]} do
+        it 'uses the file detector' do
+          driver.navigate.to url_for('upload_multiple.html')
+
+          driver.find_element(id: 'upload').send_keys("driver_spec.rb\nelement_spec.rb")
+          driver.find_element(id: 'go').submit
+          wait.until { driver.find_element(id: 'upload_label').displayed? }
+
+          driver.switch_to.frame('upload_target')
+          body = driver.find_element(xpath: '//body')
+          expect(body.text.scan('Licensed to the Software Freedom Conservancy').count).to eq(4)
+        end
       end
     end
   end # WebDriver
