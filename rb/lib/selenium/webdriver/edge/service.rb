4e0d1babd9ada2489dcfc29620b471b845e85d12
Lucas Tierney <lucast1533@gmail.com>
2018-08-20 14:10:25 -0500

    [rb] replace erroneous em dashes with regular dash

--- rb/lib/selenium/webdriver/edge/service.rb
+++ rb/lib/selenium/webdriver/edge/service.rb
@@ -44,8 +44,8 @@ module Selenium
 
         def extract_service_args(driver_opts)
           driver_args = super
-          driver_args << "–host=#{driver_opts[:host]}" if driver_opts.key? :host
-          driver_args << "–package=#{driver_opts[:package]}" if driver_opts.key? :package
+          driver_args << "--host=#{driver_opts[:host]}" if driver_opts.key? :host
+          driver_args << "--package=#{driver_opts[:package]}" if driver_opts.key? :package
           driver_args << "-verbose" if driver_opts[:verbose] == true
           driver_args
         end
