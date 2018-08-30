64078e1777807a7ead988f380b4efe98558a38eb
Alexei Barantsev <barancev@gmail.com>
2018-08-30 11:06:17 +0300

    [java] Ignoring a test because of Firefox regression, see https://bugzilla.mozilla.org/show_bug.cgi?id=1477977#c1

--- java/client/test/org/openqa/selenium/AlertsTest.java
+++ java/client/test/org/openqa/selenium/AlertsTest.java
@@ -352,6 +352,7 @@ public class AlertsTest extends JUnit4TestBase {
   }
 
   @Test
+  @NotYetImplemented(value = MARIONETTE, reason = "https://bugzilla.mozilla.org/show_bug.cgi?id=1477977")
   public void testHandlesTwoAlertsFromOneInteraction() {
     driver.get(appServer.create(new Page()
         .withScripts(
