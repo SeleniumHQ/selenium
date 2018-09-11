b6bb5d924ce94758600e19c20a938e3e68a32de9
Alexei Barantsev <barancev@gmail.com>
2018-09-11 10:20:06 +0300

    [py] Ignoring/unignoring some tests according to the current state of marionette

--- py/test/selenium/webdriver/common/interactions_tests.py
+++ py/test/selenium/webdriver/common/interactions_tests.py
@@ -143,10 +143,7 @@ def testCannotMoveToANullLocator(driver, pages):
         move.perform()
 
 
-@pytest.mark.xfail_marionette(
-    reason='https://bugzilla.mozilla.org/show_bug.cgi?id=1292178')
 @pytest.mark.xfail_firefox
-@pytest.mark.xfail_remote
 def testClickingOnFormElements(driver, pages):
     """Copied from org.openqa.selenium.interactions.CombinedInputActionsTest."""
     pages.load("formSelectionPage.html")
