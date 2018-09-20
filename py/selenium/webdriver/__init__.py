c513b18b7acdf7d1793fd2dce5f656157063ad7c
Lucas Tierney <lucast1533@gmail.com>
2018-09-20 13:22:46 -0500

    [py] update CHANGELOG and versions for 3.14.1

--- py/selenium/webdriver/__init__.py
+++ py/selenium/webdriver/__init__.py
@@ -36,4 +36,4 @@ from .common.action_chains import ActionChains  # noqa
 from .common.touch_actions import TouchActions  # noqa
 from .common.proxy import Proxy  # noqa
 
-__version__ = '3.14.0'
+__version__ = '3.14.1'
