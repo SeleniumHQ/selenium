65750ad9dd52191aa9fea3d0abbb9759f273cce5
Grigory Mischenko <gmischenko.external@epo.org>
2018-09-20 16:09:22 +0200

    Adding tests to verify that timeout is passing to createDriverService for:   ChromeDriverService.Builder   EdgeDriverService.Builder   GeckoDriverService.Builder   XpiDriverService.Builder   InternetExplorerDriverService.Builder   OperaDriverService.Builder

---
+++ java/client/test/org/openqa/selenium/ie/InternetExplorerDriverServiceTest.java
@@ -0,0 +1,34 @@
+package org.openqa.selenium.ie;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.time.Duration;
+
+public class InternetExplorerDriverServiceTest {
+
+  @Test
+  public void builderPassesTimeoutToDriverService() {
+    File exe = new File("someFile");
+    Duration defaultTimeout = Duration.ofSeconds(20);
+    Duration customTimeout = Duration.ofSeconds(60);
+
+    InternetExplorerDriverService.Builder builderMock =
+        spy(InternetExplorerDriverService.Builder.class);
+    doReturn(exe).when(builderMock).findDefaultExecutable();
+    builderMock.build();
+
+    verify(builderMock).createDriverService(any(), anyInt(), eq(defaultTimeout), any(), any());
+
+    builderMock.withTimeout(customTimeout);
+    builderMock.build();
+    verify(builderMock).createDriverService(any(), anyInt(), eq(customTimeout), any(), any());
+  }
+}
