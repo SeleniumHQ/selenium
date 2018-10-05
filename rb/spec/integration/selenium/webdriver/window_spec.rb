9337dbd540ab2cea4a26eb55f563989633c914ad
Alex Rodionov <p0deje@gmail.com>
2018-10-05 14:18:29 +0600

    Make RuboCop happy again

--- rb/spec/integration/selenium/webdriver/window_spec.rb
+++ rb/spec/integration/selenium/webdriver/window_spec.rb
@@ -116,7 +116,8 @@ module Selenium
 
       # Edge: Not Yet - https://dev.windows.com/en-us/microsoft-edge/platform/status/webdriver/details/
       # https://github.com/mozilla/geckodriver/issues/1281
-      it 'can make window full screen', only: {window_manager: true, browser: [:ie, :firefox]}, exclude: {driver: :remote, browser: :firefox, platform: :linux} do
+      it 'can make window full screen', only: {window_manager: true, browser: %i[ie firefox]},
+                                        exclude: {driver: :remote, browser: :firefox, platform: :linux} do
         window.size = old_size = Dimension.new(200, 200)
 
         wait.until { window.size != old_size }
@@ -128,7 +129,8 @@ module Selenium
 
       # Edge: Not Yet - https://dev.windows.com/en-us/microsoft-edge/platform/status/webdriver/details/
       # https://github.com/mozilla/geckodriver/issues/1281
-      it 'can minimize the window', only: {window_manager: true, browser: [:ie, :firefox]}, exclude: {driver: :remote, browser: :firefox, platform: :linux} do
+      it 'can minimize the window', only: {window_manager: true, browser: %i[ie firefox]},
+                                    exclude: {driver: :remote, browser: :firefox, platform: :linux} do
         window.minimize
         expect(driver.execute_script('return document.hidden;')).to be true
       end
