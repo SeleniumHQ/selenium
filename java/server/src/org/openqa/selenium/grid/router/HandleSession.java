86facb30fca72c06e2a005959c4b878a2710e4a0
Simon Stewart <simon.m.stewart@gmail.com>
2018-10-13 16:56:52 +0100

    Add a router to tie everything together

---
+++ java/server/src/org/openqa/selenium/grid/router/HandleSession.java
@@ -0,0 +1,86 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium.grid.router;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import org.openqa.selenium.UnsupportedCommandException;
+import org.openqa.selenium.grid.data.Session;
+import org.openqa.selenium.grid.sessionmap.SessionMap;
+import org.openqa.selenium.grid.web.CommandHandler;
+import org.openqa.selenium.grid.web.ReverseProxyHandler;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.remote.http.HttpRequest;
+import org.openqa.selenium.remote.http.HttpResponse;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Predicate;
+
+class HandleSession implements Predicate<HttpRequest>, CommandHandler {
+
+  private final LoadingCache<SessionId, CommandHandler> knownSessions;
+
+  public HandleSession(SessionMap sessions) {
+    Objects.requireNonNull(sessions);
+
+    this.knownSessions = CacheBuilder.newBuilder()
+        .expireAfterAccess(Duration.ofMinutes(1))
+        .build(new CacheLoader<SessionId, CommandHandler>() {
+          @Override
+          public CommandHandler load(SessionId id) throws Exception {
+            Session session = sessions.get(id);
+            if (session instanceof CommandHandler) {
+              return (CommandHandler) session;
+            }
+            return new ReverseProxyHandler(session.getUri().toURL());
+          }
+        });
+  }
+
+  @Override
+  public boolean test(HttpRequest req) {
+    return req.getUri().startsWith("/session/");
+  }
+
+  @Override
+  public void execute(HttpRequest req, HttpResponse resp) throws IOException {
+    if (!test(req)) {
+      throw new UnsupportedCommandException(
+          String.format("(%s) %s", req.getMethod(), req.getUri()));
+    }
+
+    String[] split = req.getUri().split("/", 4);
+    SessionId id = new SessionId(split[2]);
+
+    try {
+      knownSessions.get(id).execute(req, resp);
+    } catch (ExecutionException e) {
+      Throwable cause = e.getCause();
+      if (cause instanceof RuntimeException) {
+        throw (RuntimeException) cause;
+      }
+      throw new RuntimeException(cause);
+    }
+
+  }
+}
---
+++ java/server/test/org/openqa/selenium/grid/router/EndToEndTest.java
@@ -0,0 +1,146 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium.grid.router;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.ImmutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.grid.config.MapConfig;
+import org.openqa.selenium.grid.data.Session;
+import org.openqa.selenium.grid.distributor.Distributor;
+import org.openqa.selenium.grid.distributor.local.LocalDistributor;
+import org.openqa.selenium.grid.distributor.remote.RemoteDistributor;
+import org.openqa.selenium.grid.node.local.LocalNode;
+import org.openqa.selenium.grid.server.BaseServer;
+import org.openqa.selenium.grid.server.BaseServerOptions;
+import org.openqa.selenium.grid.server.Server;
+import org.openqa.selenium.grid.sessionmap.SessionMap;
+import org.openqa.selenium.grid.sessionmap.local.LocalSessionMap;
+import org.openqa.selenium.grid.sessionmap.remote.RemoteSessionMap;
+import org.openqa.selenium.grid.web.CommandHandler;
+import org.openqa.selenium.net.PortProber;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+import org.openqa.selenium.remote.http.HttpClient;
+import org.openqa.selenium.remote.http.HttpRequest;
+import org.openqa.selenium.remote.http.HttpResponse;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.util.function.Function;
+
+public class EndToEndTest {
+
+  private final Capabilities driverCaps = new ImmutableCapabilities("browserName", "cheese");
+
+  @Test
+  public void inMemory() throws URISyntaxException {
+
+    SessionMap sessions = new LocalSessionMap();
+    Distributor distributor = new LocalDistributor();
+    URI nodeUri = new URI("http://localhost:4444");
+    LocalNode node = LocalNode.builder(nodeUri, sessions)
+        .add(driverCaps, createFactory(nodeUri))
+        .build();
+    distributor.add(node);
+
+    Router router = new Router(sessions, distributor);
+
+    Server<?> server = createServer();
+    server.addHandler(router, (inj, req) -> router);
+    server.start();
+
+    exerciseDriver(server);
+  }
+
+  private void exerciseDriver(Server<?> server) {
+    WebDriver driver = new RemoteWebDriver(
+        server.getUrl(),
+        new ImmutableCapabilities("browserName", "cheese", "type", "cheddar"));
+    driver.get("http://www.google.com");
+    driver.quit();
+  }
+
+  @Test
+  public void withServers() throws URISyntaxException {
+
+    LocalSessionMap localSessions = new LocalSessionMap();
+    Server<?> sessionServer = createServer();
+    sessionServer.addHandler(localSessions, (inj, req) -> localSessions);
+    sessionServer.start();
+
+    SessionMap sessions = new RemoteSessionMap(getClient(sessionServer));
+
+    LocalDistributor localDistributor = new LocalDistributor();
+    Server<?> distributorServer = createServer();
+    distributorServer.addHandler(localDistributor, (inj, req) -> localDistributor);
+    distributorServer.start();
+
+    Distributor distributor = new RemoteDistributor(getClient(distributorServer));
+
+    int port = PortProber.findFreePort();
+    URI nodeUri = new URI("http://localhost:" + port);
+    LocalNode localNode = LocalNode.builder(nodeUri, sessions)
+        .add(driverCaps, createFactory(nodeUri))
+        .build();
+    Server<?> nodeServer = new BaseServer<>(new BaseServerOptions(
+        new MapConfig(ImmutableMap.of("server", ImmutableMap.of("port", port)))));
+    nodeServer.addHandler(localNode, (inj, req) -> localNode);
+    nodeServer.start();
+
+    distributor.add(localNode);
+
+    Router router = new Router(sessions, distributor);
+    Server<?> routerServer = createServer();
+    routerServer.addHandler(router, (inj, req) -> router);
+    routerServer.start();
+
+    exerciseDriver(routerServer);
+  }
+
+  private HttpClient getClient(Server<?> server) {
+    return HttpClient.Factory.createDefault().createClient(server.getUrl());
+  }
+
+  private Server<?> createServer() {
+    int port = PortProber.findFreePort();
+    return new BaseServer<>(new BaseServerOptions(
+        new MapConfig(ImmutableMap.of("server", ImmutableMap.of("port", port)))));
+  }
+
+  private Function<Capabilities, Session> createFactory(URI serverUri) {
+    class SpoofSession extends Session implements CommandHandler {
+
+      private SpoofSession(Capabilities capabilities) {
+        super(new SessionId(UUID.randomUUID()), serverUri, capabilities);
+      }
+
+      @Override
+      public void execute(HttpRequest req, HttpResponse resp) {
+
+      }
+    }
+
+    return caps -> new SpoofSession(caps);
+  }
+
+}
