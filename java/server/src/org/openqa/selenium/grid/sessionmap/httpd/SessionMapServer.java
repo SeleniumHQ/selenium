c15c94ef4d2fb0ec38a59cb056bce2de26f2b2e2
Simon Stewart <simon.m.stewart@gmail.com>
2018-10-14 22:32:28 +0100

    Add a server for the session map

---
+++ java/server/src/org/openqa/selenium/grid/sessionmap/httpd/SessionMapServer.java
@@ -0,0 +1,92 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium.grid.sessionmap.httpd;
+
+import com.google.auto.service.AutoService;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+import org.openqa.selenium.cli.CliCommand;
+import org.openqa.selenium.grid.config.AnnotatedConfig;
+import org.openqa.selenium.grid.config.CompoundConfig;
+import org.openqa.selenium.grid.config.ConcatenatingConfig;
+import org.openqa.selenium.grid.config.Config;
+import org.openqa.selenium.grid.config.EnvConfig;
+import org.openqa.selenium.grid.server.BaseServer;
+import org.openqa.selenium.grid.server.BaseServerFlags;
+import org.openqa.selenium.grid.server.BaseServerOptions;
+import org.openqa.selenium.grid.server.HelpFlags;
+import org.openqa.selenium.grid.server.Server;
+import org.openqa.selenium.grid.sessionmap.SessionMap;
+import org.openqa.selenium.grid.sessionmap.local.LocalSessionMap;
+
+@AutoService(CliCommand.class)
+public class SessionMapServer implements CliCommand {
+
+  @Override
+  public String getName() {
+    return "sessions";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Adds this server as the session map in a selenium grid.";
+  }
+
+  @Override
+  public Executable configure(String... args) {
+
+    HelpFlags help = new HelpFlags();
+    BaseServerFlags serverFlags = new BaseServerFlags(5556);
+
+    JCommander commander = JCommander.newBuilder()
+        .programName(getName())
+        .addObject(help)
+        .addObject(serverFlags)
+        .build();
+
+    return () -> {
+      try {
+        commander.parse(args);
+      } catch (ParameterException e) {
+        System.err.println(e.getMessage());
+        commander.usage();
+        return;
+      }
+
+      if (help.displayHelp(commander, System.out)) {
+        return;
+      }
+
+      Config config = new CompoundConfig(
+          new AnnotatedConfig(help),
+          new AnnotatedConfig(serverFlags),
+          new EnvConfig(),
+          new ConcatenatingConfig("sessions", '.', System.getProperties()));
+
+      SessionMap sessions = new LocalSessionMap();
+
+      BaseServerOptions serverOptions = new BaseServerOptions(config);
+
+      Server<?> server = new BaseServer<>(serverOptions);
+      server.addHandler(sessions, (inj, req) -> sessions);
+      server.start();
+    };
+  }
+}
