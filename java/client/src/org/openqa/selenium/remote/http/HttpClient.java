3d89deb7e78122bad89b30561d3a627893ec540d
bozdemir84 <burak.bbm@gmail.com>
2018-10-29 14:50:19 +0100

    Added Locale.US for USER_AGENT toLowerCase (#6491)
    USER_AGENT toLowerCase without locale is causing problems with some languages, in may case Turkish. 
    It maps WINDOWS to wındows which is resulting an exception later when it is used because of 'ı' char instead of 'i'. 
    Calling toLowerCase with Locale.US solves this problem.

--- java/client/src/org/openqa/selenium/remote/http/HttpClient.java
+++ java/client/src/org/openqa/selenium/remote/http/HttpClient.java
@@ -29,6 +29,7 @@ import java.net.Proxy;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.Locale;
 
 /**
  * Defines a simple client for making HTTP requests.
@@ -39,8 +40,8 @@ public interface HttpClient {
       "selenium/%s (java %s)",
       new BuildInfo().getReleaseLabel(),
       (Platform.getCurrent().family() == null ?
-       Platform.getCurrent().toString().toLowerCase() :
-       Platform.getCurrent().family().toString().toLowerCase()));
+       Platform.getCurrent().toString().toLowerCase(Locale.US) :
+       Platform.getCurrent().family().toString().toLowerCase(Locale.US)));
 
   /**
    * Executes the given request, following any redirects if necessary.
