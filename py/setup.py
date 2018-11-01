b03095da9921f16cbfabb0c8352a0c8e372d6bc7
AutomatedTester <david.burns@theautomatedtester.co.uk>
2018-11-01 08:58:13 +0000

    Bump python version

--- py/setup.py
+++ py/setup.py
@@ -29,7 +29,7 @@ for scheme in INSTALL_SCHEMES.values():
 setup_args = {
     'cmdclass': {'install': install},
     'name': 'selenium',
-    'version': "3.14.1",
+    'version': "3.141.0",
     'license': 'Apache 2.0',
     'description': 'Python bindings for Selenium',
     'long_description': open(join(abspath(dirname(__file__)), "README.rst")).read(),
