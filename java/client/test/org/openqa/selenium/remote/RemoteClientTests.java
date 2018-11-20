428988998c815b481263e826f990a7451859936f
Alexei Barantsev <barancev@gmail.com>
2018-11-20 20:42:48 +0300

    [java] Moving a test to a more appropriate package where the target class is located

--- java/client/test/org/openqa/selenium/remote/RemoteClientTests.java
+++ java/client/test/org/openqa/selenium/remote/RemoteClientTests.java
@@ -19,13 +19,11 @@ package org.openqa.selenium.remote;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.openqa.selenium.remote.internal.CircularOutputStreamTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     AugmenterTest.class,
     ErrorHandlerTest.class,
-    CircularOutputStreamTest.class,
     JsonWireProtocolResponseTest.class,
     ProtocolHandshakeTest.class,
     RemoteLogsTest.class,
