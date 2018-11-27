50027f085ca39a40fb30c3b75f2e5235f8c22f6d
Simon Stewart <simon.m.stewart@gmail.com>
2018-11-27 11:29:57 +0000

    Undo some WIP that should not have checked in

--- java/server/src/org/openqa/selenium/grid/server/LoggingOptions.java
+++ java/server/src/org/openqa/selenium/grid/server/LoggingOptions.java
@@ -3,8 +3,6 @@ package org.openqa.selenium.grid.server;
 import org.openqa.selenium.grid.config.Config;
 import org.openqa.selenium.remote.tracing.DistributedTracer;
 
-import io.jaegertracing.Configuration;
-
 import java.util.Objects;
 
 public class LoggingOptions {
@@ -16,15 +14,7 @@ public class LoggingOptions {
   }
 
   public DistributedTracer getTracer() {
-    Configuration.SamplerConfiguration samplerConfig =
-        Configuration.SamplerConfiguration.fromEnv().withType("const").withParam(1);
-    Configuration.ReporterConfiguration reporterConfig =
-        Configuration.ReporterConfiguration.fromEnv().withLogSpans(true);
-    Configuration config = new Configuration("selenium")
-        .withSampler(samplerConfig)
-        .withReporter(reporterConfig);
-
-    return DistributedTracer.builder().use(config.getTracer()).build();
+    return DistributedTracer.builder().detect().build();
   }
 
 }
