dfba236ce00d7e589dc2280bde00b2c083d66282
Jim Evans <james.h.evans.jr@gmail.com>
2018-12-28 14:06:15 -0800

    Removing isFocusable atom from sendKeys command in IE
    The isFocusable atom does not provide the correct information as to
    whether an element is focusable according to the W3C and WHATWG DOM
    specifications. This change removes the use of that atom, and relies
    instead on the abililty to set focus to the element before sending
    the keystrokes.

--- cpp/iedriver/Element.cpp
+++ cpp/iedriver/Element.cpp
@@ -237,8 +237,6 @@ bool Element::IsInteractable() {
 bool Element::IsFocusable() {
   LOG(TRACE) << "Entering Element::IsFocusable";
 
-  bool result = false;
-
   CComPtr<IHTMLBodyElement> body;
   HRESULT hr = this->element_->QueryInterface<IHTMLBodyElement>(&body);
   if (SUCCEEDED(hr) && body) {
@@ -259,26 +257,7 @@ bool Element::IsFocusable() {
       return true;
     }
   }
-
-  // The atom is just the definition of an anonymous
-  // function: "function() {...}"; Wrap it in another function so we can
-  // invoke it with our arguments without polluting the current namespace.
-  std::wstring script_source(L"(function() { return (");
-  script_source += atoms::asString(atoms::IS_FOCUSABLE);
-  script_source += L")})();";
-
-  
-  Script script_wrapper(doc, script_source, 1);
-  script_wrapper.AddArgument(this->element_);
-  int status_code = script_wrapper.Execute();
-
-  if (status_code == WD_SUCCESS) {
-    result = script_wrapper.result().boolVal == VARIANT_TRUE;
-  } else {
-    LOG(WARN) << "Failed to determine is element enabled";
-  }
-
-  return result;
+  return false;
 }
 
 bool Element::IsObscured(LocationInfo* click_location,
