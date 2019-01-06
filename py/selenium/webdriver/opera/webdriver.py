d656778270a502f93daee02cc72c50fe979bf05d
Corey Goldberg <cgoldberg@gmail.com>
2019-01-06 13:24:17 -0500

    Update webdriver.py

--- py/selenium/webdriver/opera/webdriver.py
+++ py/selenium/webdriver/opera/webdriver.py
@@ -14,8 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import warnings
-
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromiumDriver
 from .options import Options
 
