535eb7e4fb82c3e45457dc7c789f2bc6add820c0
Alexei Barantsev <barancev@gmail.com>
2019-01-11 18:38:58 +0300

    Adding more info about skipped tests to notifications

--- java/client/test/org/openqa/selenium/testing/NotificationRule.java
+++ java/client/test/org/openqa/selenium/testing/NotificationRule.java
@@ -106,6 +106,8 @@ public class NotificationRule extends TestWatcher {
       map.put("testcase", description.getMethodName());
       map.put("job_id", jobId);
       map.put("result", "skipped");
+      map.put("exception", e.getClass().getName());
+      map.put("message", e.getMessage());
       map.put("stacktrace", stacktraceToString(e));
       map.put("started_at", startedAt);
       map.put("finished_at", finishedAt);
