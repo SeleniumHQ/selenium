25d96c0bb0d5ca5d209049b36074883c3b80a6de
Johannes Eckstein <johannes.eckstein@nucos.de>
2019-02-07 12:05:37 +0100

    separate file

--- py/selenium/webdriver/support/wait.py
+++ py/selenium/webdriver/support/wait.py
@@ -16,7 +16,6 @@
 # under the License.
 
 import time
-import asyncio as aio
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 
@@ -105,44 +104,6 @@ class WebDriverWait(object):
             except self._ignored_exceptions:
                 return True
             time.sleep(self._poll)
-            if time.time() > end_time:
-                break
-        raise TimeoutException(message)
-
-class AsyncWebDriverWait(WebDriverWait):
-
-    async def until(self, method, message=''):
-        """Calls the method provided with the driver as an argument until the \
-        return value is not False."""
-        screen = None
-        stacktrace = None
-
-        end_time = time.time() + self._timeout
-        while True:
-            try:
-                value = method(self._driver)
-                if value:
-                    return value
-            except self._ignored_exceptions as exc:
-                screen = getattr(exc, 'screen', None)
-                stacktrace = getattr(exc, 'stacktrace', None)
-            await aio.sleep(self._poll)
-            if time.time() > end_time:
-                break
-        raise TimeoutException(message, screen, stacktrace)
-
-    async def until_not(self, method, message=''):
-        """Calls the method provided with the driver as an argument until the \
-        return value is False."""
-        end_time = time.time() + self._timeout
-        while True:
-            try:
-                value = method(self._driver)
-                if not value:
-                    return value
-            except self._ignored_exceptions:
-                return True
-            await aio.sleep(self._poll)
             if time.time() > end_time:
                 break
         raise TimeoutException(message)
---
+++ py/selenium/webdriver/support/wait_async.py
@@ -0,0 +1,41 @@
+import asyncio as aio
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class AsyncWebDriverWait(WebDriverWait):
+
+    async def until(self, method, message=''):
+        """Calls the method provided with the driver as an argument until the \
+        return value is not False."""
+        screen = None
+        stacktrace = None
+
+        end_time = time.time() + self._timeout
+        while True:
+            try:
+                value = method(self._driver)
+                if value:
+                    return value
+            except self._ignored_exceptions as exc:
+                screen = getattr(exc, 'screen', None)
+                stacktrace = getattr(exc, 'stacktrace', None)
+            await aio.sleep(self._poll)
+            if time.time() > end_time:
+                break
+        raise TimeoutException(message, screen, stacktrace)
+
+    async def until_not(self, method, message=''):
+        """Calls the method provided with the driver as an argument until the \
+        return value is False."""
+        end_time = time.time() + self._timeout
+        while True:
+            try:
+                value = method(self._driver)
+                if not value:
+                    return value
+            except self._ignored_exceptions:
+                return True
+            await aio.sleep(self._poll)
+            if time.time() > end_time:
+                break
+        raise TimeoutException(message)
