1674e148bdbbbd225db19f1543ab79a4fff60b82
Corey Goldberg <cgoldberg@gmail.com>
2019-02-17 17:45:56 -0500

    [Ruby] remove --disable-gpu option for headless Chrome

--- rb/lib/selenium/webdriver/chrome/options.rb
+++ rb/lib/selenium/webdriver/chrome/options.rb
@@ -134,9 +134,6 @@ module Selenium
 
         def headless!
           add_argument '--headless'
-
-          # https://bugs.chromium.org/p/chromium/issues/detail?id=737678#c1
-          add_argument '--disable-gpu' if WebDriver::Platform.windows?
         end
 
         #
