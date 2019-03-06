d5412ae2de57d754de493fcbb6d7a48904e0f335
Thomas Walpole <twalpole@gmail.com>
2019-03-05 18:57:41 -0800

    rebase from master and fix rubocop warnings

--- rb/lib/selenium/webdriver/safari/service.rb
+++ rb/lib/selenium/webdriver/safari/service.rb
@@ -26,7 +26,7 @@ module Selenium
 
       class Service < WebDriver::Service
         DEFAULT_PORT = 7050
-        @executable = 'safaridriver'.freeze
+        @executable = 'safaridriver'
         @missing_text = <<-ERROR.gsub(/\n +| {2,}/, ' ').freeze
           Unable to find Apple's safaridriver which comes with Safari 10.
           More info at https://webkit.org/blog/6900/webdriver-support-in-safari-10/
