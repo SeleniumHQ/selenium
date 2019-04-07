7e0e0cc32c7602d368c32547c16adea2707aae6f
Alexei Barantsev <barancev@gmail.com>
2019-04-07 21:33:01 +0300

    [java] Deleting unused imports

--- java/server/src/org/openqa/selenium/remote/server/KnownElements.java
+++ java/server/src/org/openqa/selenium/remote/server/KnownElements.java
@@ -26,7 +26,6 @@ import org.openqa.selenium.interactions.Locatable;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class KnownElements {
