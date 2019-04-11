2528532d0f8d64bbb9ac1bb0a760124d0eb7010e
Jim Evans <james.h.evans.jr@gmail.com>
2019-04-11 16:49:10 -0700

    Moving .NET IActionExecutor interface from internal namespace

--- dotnet/src/webdriver/IActionExecutor.cs
+++ dotnet/src/webdriver/IActionExecutor.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IActionExecutor.cs" company="WebDriver Committers">
+// <copyright file="IActionExecutor.cs" company="WebDriver Committers">
 // Licensed to the Software Freedom Conservancy (SFC) under one
 // or more contributor license agreements. See the NOTICE file
 // distributed with this work for additional information
@@ -22,7 +22,7 @@ using System.Linq;
 using System.Text;
 using OpenQA.Selenium.Interactions;
 
-namespace OpenQA.Selenium.Internal
+namespace OpenQA.Selenium
 {
     /// <summary>
     /// Interface allowing execution of W3C Specification-compliant actions.
