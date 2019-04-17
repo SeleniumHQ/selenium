902d27d9a8babef8141964f9658a226dd1a2d532
AutomatedTester <david.burns@theautomatedtester.co.uk>
2019-04-17 10:48:07 +0100

    [py] Bump python bindings to 4.0.0a1

--- py/setup.py
+++ py/setup.py
@@ -44,7 +44,7 @@ for scheme in INSTALL_SCHEMES.values():
 setup_args = {
     'cmdclass': {'install': install},
     'name': 'selenium',
-    'version': "3.141.0",
+    'version': "4.0.0a1",
     'license': 'Apache 2.0',
     'description': 'Python bindings for Selenium',
     'long_description': open(join(abspath(dirname(__file__)), "README.rst")).read(),
