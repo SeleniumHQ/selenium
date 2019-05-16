d3218e51da96b410065714771f4c656c2ac3ea68
Yahav N. Hoffman <ynhoffman@gmail.com>
2019-05-16 16:35:29 +0300

    adding inheritance to test pom with page factory poc

--- py/test/selenium/webdriver/support/page_facroty_pom_test/results_page.py
+++ py/test/selenium/webdriver/support/page_facroty_pom_test/results_page.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.support.page_facroty import cacheable, callable_find_by as find_by
 from selenium.webdriver.common.by import By
+from test.selenium.webdriver.support.page_facroty_pom_test.search_page import GoogleSearchPage
 
-class GoogleResultsPage(object):
+class GoogleResultsPage(GoogleSearchPage):
 
+    _search_box = find_by(how=By.CSS_SELECTOR, using="div.a4bIc > input[name='q']", cacheable=True)
     _urls = find_by(how=By.CSS_SELECTOR, using="cite.iUh30", multiple=True, cacheable=True)
 
     def __init__(self, driver):
-        self._driver = driver
+        super(GoogleResultsPage, self).__init__(driver=driver)
         self.url = self._driver.current_url
         cacheable(lookup=self)
         print("\nPage:", self, "\nLocation:", self.url)
@@ -20,3 +22,6 @@ class GoogleResultsPage(object):
 
     def enter_result(self, idx):
         self._urls()[idx].click()
+
+    def search_word_submit(self, word):
+        super(GoogleResultsPage, self).search_word_submit(word=word)
