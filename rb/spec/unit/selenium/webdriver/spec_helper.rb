20f33189de994e9b2c35552350782dc8bdb17e95
RustyNail <takonoyawarakaage@yahoo.co.jp>
2019-05-30 08:48:50 +0900

    Detele settings in spec_helper.rb

--- rb/spec/unit/selenium/webdriver/spec_helper.rb
+++ rb/spec/unit/selenium/webdriver/spec_helper.rb
@@ -42,8 +42,4 @@ RSpec.configure do |c|
   c.include Selenium::WebDriver::UnitSpecHelper
 
   c.filter_run focus: true if ENV['focus']
-
-  c.define_derived_metadata do |meta|
-    meta[:aggregate_failures] = true
-  end
 end
