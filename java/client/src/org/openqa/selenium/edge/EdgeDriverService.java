515b81c1e98dde24e5c229baf2d10ea13efff703
Simon Stewart <simon.m.stewart@gmail.com>
2019-06-03 12:44:39 +0100

    Fix Edge tests

--- java/client/src/org/openqa/selenium/edge/EdgeDriverService.java
+++ java/client/src/org/openqa/selenium/edge/EdgeDriverService.java
@@ -18,7 +18,6 @@ package org.openqa.selenium.edge;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-
 import org.openqa.selenium.remote.service.DriverService;
 
 import java.io.File;
