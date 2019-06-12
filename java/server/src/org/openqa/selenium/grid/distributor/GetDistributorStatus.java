f17525ca655579838b6f69c77e1117a383f32bcc
Simon Stewart <simon.m.stewart@gmail.com>
2019-06-12 15:25:47 +0100

    Move most command handlers in distributor to http handlers

--- java/server/src/org/openqa/selenium/grid/distributor/GetDistributorStatus.java
+++ java/server/src/org/openqa/selenium/grid/distributor/GetDistributorStatus.java
@@ -24,12 +24,13 @@ import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.grid.data.DistributorStatus;
 import org.openqa.selenium.grid.web.CommandHandler;
 import org.openqa.selenium.json.Json;
+import org.openqa.selenium.remote.http.HttpHandler;
 import org.openqa.selenium.remote.http.HttpRequest;
 import org.openqa.selenium.remote.http.HttpResponse;
 
 import java.util.Objects;
 
-class GetDistributorStatus implements CommandHandler {
+class GetDistributorStatus implements HttpHandler {
 
   private final Json json;
   private final Distributor distributor;
@@ -40,9 +41,9 @@ class GetDistributorStatus implements CommandHandler {
   }
 
   @Override
-  public void execute(HttpRequest req, HttpResponse resp) {
+  public HttpResponse execute(HttpRequest req) {
     DistributorStatus status = distributor.getStatus();
 
-    resp.setContent(utf8String(json.toJson(ImmutableMap.of("value", status))));
+    return new HttpResponse().setContent(utf8String(json.toJson(ImmutableMap.of("value", status))));
   }
 }
