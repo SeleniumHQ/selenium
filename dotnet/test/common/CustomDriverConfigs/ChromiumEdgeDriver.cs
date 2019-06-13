ea975d2ac5fb48e5219a9386783c535fd5071475
Jim Evans <james.h.evans.jr@gmail.com>
2019-06-13 10:33:14 -0700

    Enabling tests for Chromium-based Edge in .NET

--- dotnet/test/common/CustomDriverConfigs/ChromiumEdgeDriver.cs
+++ dotnet/test/common/CustomDriverConfigs/ChromiumEdgeDriver.cs
@@ -8,6 +8,8 @@ namespace OpenQA.Selenium.Edge
 {
     public class ChromiumEdgeDriver : EdgeDriver
     {
+        private static string servicePath = string.Empty;
+
         public ChromiumEdgeDriver()
             : this(DefaultService, DefaultOptions)
         {
@@ -20,16 +22,22 @@ namespace OpenQA.Selenium.Edge
 
         public static EdgeOptions DefaultOptions
         {
-            get { return new EdgeOptions(false) { UseSpecCompliantProtocol = true }; }
+            get { return new EdgeOptions(false) { BinaryLocation = @"C:\Program Files (x86)\Microsoft\Edge Dev\Application\msedge.exe" }; }
         }
 
         public static EdgeDriverService DefaultService
         {
             get
             {
-                EdgeDriverService service = EdgeDriverService.CreateDefaultService(false);
+                EdgeDriverService service = EdgeDriverService.CreateDefaultService(ServicePath, false);
                 return service;
             }
+        }
+
+        public static string ServicePath
+        {
+            get { return servicePath; }
+            set { servicePath = value; }
         }
     }
 }
