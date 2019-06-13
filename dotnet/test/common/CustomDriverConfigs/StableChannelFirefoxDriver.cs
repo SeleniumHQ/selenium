e4d3f605d08f6daf7b2057c783bcbee6ecee1814
Jim Evans <james.h.evans.jr@gmail.com>
2019-06-13 11:20:24 -0700

    Reorganizing .NET tests for specific driver configurations

--- dotnet/test/common/CustomDriverConfigs/StableChannelFirefoxDriver.cs
+++ dotnet/test/common/CustomDriverConfigs/StableChannelFirefoxDriver.cs
@@ -5,14 +5,14 @@ namespace OpenQA.Selenium.Firefox
     // This is a simple wrapper class to create a FirefoxDriver that
     // uses the Marionette implementation and has no parameters in the
     // constructor.
-    public class ReleaseFirefoxWebDriver : FirefoxDriver
+    public class StableChannelFirefoxDriver : FirefoxDriver
     {
-        public ReleaseFirefoxWebDriver(FirefoxDriverService service)
+        public StableChannelFirefoxDriver(FirefoxDriverService service)
             : this(service, DefaultOptions)
         {
         }
 
-        public ReleaseFirefoxWebDriver(FirefoxDriverService service, FirefoxOptions options)
+        public StableChannelFirefoxDriver(FirefoxDriverService service, FirefoxOptions options)
             : base(service, options, RemoteWebDriver.DefaultCommandTimeout)
         {
         }
