567a81e738db0138b12af1bc62259cb89f57c82e
Titus Fortner <titusfortner@gmail.com>
2019-06-20 17:00:12 +0200

    [rb] remove unnecessary methods from driver classes

--- rb/lib/selenium/webdriver/firefox/driver.rb
+++ rb/lib/selenium/webdriver/firefox/driver.rb
@@ -32,7 +32,7 @@ module Selenium
         include DriverExtensions::TakesScreenshot
 
         def initialize(opts = {})
-          opts[:desired_capabilities] = create_capabilities(opts)
+          opts[:desired_capabilities] ||= Remote::Capabilities.firefox
 
           opts[:url] ||= service_url(opts)
 
@@ -54,17 +54,6 @@ module Selenium
           super
         ensure
           @service&.stop
-        end
-
-        private
-
-        def create_capabilities(opts)
-          caps = opts.delete(:desired_capabilities) { Remote::Capabilities.firefox }
-          options = opts.delete(:options) { Options.new }
-          options = options.as_json
-          caps.merge!(options) unless options.empty?
-
-          caps
         end
       end # Driver
     end # Firefox
--- rb/spec/integration/selenium/webdriver/edge_chrome/profile_spec.rb
+++ rb/spec/integration/selenium/webdriver/edge_chrome/profile_spec.rb
@@ -61,8 +61,8 @@ module Selenium
 
           expect(profile).to receive(:layout_on_disk).and_return 'ignored'
 
-          expect(profile.as_json).to eq(directory: 'ignored',
-                                        extensions: [Base64.strict_encode64('test')])
+          expect(profile.as_json).to eq('directory' => 'ignored',
+                                        'extensions' => [Base64.strict_encode64('test')])
         end
 
         it "raises an error if the extension doesn't exist" do
