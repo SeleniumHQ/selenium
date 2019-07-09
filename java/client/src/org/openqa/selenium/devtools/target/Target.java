547238f9cf340ae7cac446cce9b286333e8f2805
Shay Dratler <Shay_Dratler@intuit.com>
2019-07-09 14:16:33 +0300

    Fix JavaDoc

--- java/client/src/org/openqa/selenium/devtools/target/Target.java
+++ java/client/src/org/openqa/selenium/devtools/target/Target.java
@@ -78,7 +78,7 @@ public class Target {
    * Inject object to the target's main frame that provides a communication channel with browser
    * target. Injected object will be available as window[bindingName]. The object has the follwing
    * API: binding.send(json) - a method to send messages over the remote debugging protocol
-   * binding.onmessage = json => handleMessage(json) - a callback that will be called for the
+   * binding.onmessage equals to json then bing to handleMessage(json) - a callback that will be called for the
    * protocol notifications and command responses. EXPERIMENTAL
    */
   @Beta
