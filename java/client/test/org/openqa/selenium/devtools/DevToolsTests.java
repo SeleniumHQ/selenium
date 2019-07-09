b872677f43b4f2be1d87941b11f5129d60d25a7a
Shay Dratler <Shay_Dratler@intuit.com>
2019-07-09 12:14:20 +0300

    Dummy commit

--- java/client/test/org/openqa/selenium/devtools/DevToolsTests.java
+++ java/client/test/org/openqa/selenium/devtools/DevToolsTests.java
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 package org.openqa.selenium.devtools;
 
 import org.junit.runner.RunWith;
