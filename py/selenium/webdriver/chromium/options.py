25ad82a40a5c961a023f2ee1bbcef8a5ef385a7c
Lucas Tierney <lucast1533@gmail.com>
2019-08-01 22:52:35 -0500

    [py] fix chrome options import and unit test

--- py/selenium/webdriver/chromium/options.py
+++ py/selenium/webdriver/chromium/options.py
@@ -18,7 +18,7 @@
 import base64
 import os
 
-from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.options import ArgOptions
 
 
