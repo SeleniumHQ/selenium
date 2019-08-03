ea30eb2bb4eb2f0784ed1c9a49d3bb1cc27a17a8
Marcus Merrell <mmerrell@gmail.com>
2019-08-03 17:05:13 -0500

    Fixing typo in ByFriend(l)yLocator class name

--- java/client/src/org/openqa/selenium/support/friendly/ByFriendlyLocator.java
+++ java/client/src/org/openqa/selenium/support/friendly/ByFriendlyLocator.java
@@ -23,7 +23,7 @@ import java.util.Objects;
 import static com.google.common.base.Preconditions.checkArgument;
 import static org.openqa.selenium.json.Json.MAP_TYPE;
 
-public class ByFriendyLocator {
+public class ByFriendlyLocator {
 
   private static final Json JSON = new Json();
   private static final String FIND_ELEMENTS;
@@ -31,10 +31,10 @@ public class ByFriendyLocator {
     try {
       String location = String.format(
         "/%s/%s",
-        ByFriendyLocator.class.getPackage().getName().replace(".", "/"),
+        ByFriendlyLocator.class.getPackage().getName().replace(".", "/"),
         "findElements.js");
 
-      URL url = ByFriendyLocator.class.getResource(location);
+      URL url = ByFriendlyLocator.class.getResource(location);
 
       String rawFunction = Resources.toString(url, StandardCharsets.UTF_8);
       FIND_ELEMENTS = String.format("return (%s).apply(null, arguments);", rawFunction);
