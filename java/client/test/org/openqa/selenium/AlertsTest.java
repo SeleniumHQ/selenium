f768b3281f200eb3d05a8fabddd1cb0ef3baf050
Alexei Barantsev <barancev@gmail.com>
2019-08-27 22:47:53 +0300

    [java] Unignoring a couple of tests fixed in Chrome 76

--- java/client/test/org/openqa/selenium/AlertsTest.java
+++ java/client/test/org/openqa/selenium/AlertsTest.java
@@ -494,8 +494,6 @@ public class AlertsTest extends JUnit4TestBase {
   }
 
   @Test
-  @NotYetImplemented(CHROME)
-  @NotYetImplemented(CHROMIUMEDGE)
   @Ignore(value = HTMLUNIT, reason = "https://github.com/SeleniumHQ/htmlunit-driver/issues/57")
   @NotYetImplemented(value = MARIONETTE,
       reason = "https://bugzilla.mozilla.org/show_bug.cgi?id=1279211")
