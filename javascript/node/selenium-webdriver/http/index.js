f25d11707c86495161336dcd7da56402b73b816b
Julian Didier <did.julian@gmail.com>
2019-09-10 12:53:16 +0200

    Use the proxy protocol when a webdriver proxy is configured (#7511)
    When the http protocol is different between the remote selenium
    server and the webdriver proxy, an EPROTO error is encountered.
    
    See #7509 for more information and a reproductible test case.

--- javascript/node/selenium-webdriver/http/index.js
+++ javascript/node/selenium-webdriver/http/index.js
@@ -187,6 +187,10 @@ function sendRequest(options, onOk, onError, opt_data, opt_proxy, opt_retries) {
     options.hostname = proxy.hostname;
     options.port = proxy.port;
 
+    // Update the protocol to avoid EPROTO errors when the webdriver proxy
+    // uses a different protocol from the remote selenium server.
+    options.protocol = opt_proxy.protocol;
+
     if (proxy.auth) {
       options.headers['Proxy-Authorization'] =
           'Basic ' + new Buffer(proxy.auth).toString('base64');
