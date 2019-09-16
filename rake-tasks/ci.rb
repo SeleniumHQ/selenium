e4bda2aa29e86bbde334c5be5fc0c06c1455e271
Luke Hill <lukehill_uk@hotmail.com>
2019-09-16 14:38:40 +0200

    More movement of files and methods where appropriate, plus fixing up their caller locations
    Currently no physical files have been removed, and not all top level files have been fixed up

--- rake-tasks/ci.rb
+++ rake-tasks/ci.rb
@@ -18,42 +18,10 @@ def net_http
   end
 end
 
-namespace :ci do
-  task :upload_to_sauce do
-    upload_path = ENV['UPLOAD_PATH']
-    username = ENV['SAUCE_USERNAME']
-    apikey = ENV['SAUCE_APIKEY']
-    upload_filename = Pathname.new(upload_path).basename
-    upload_url = "http://saucelabs.com/rest/v1/storage/#{username}/#{upload_filename}"
-    body = nil
-
-    File.open(find_file(upload_path), 'r') do |infile|
-      body = infile.read
-    end
-
-    6.times do
-      uri = URI.parse(upload_url)
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.basic_auth(username, apikey)
-      request['Content-Type'] = 'application/octet-stream'
-      request.body = body
-      http = net_http.new(uri.host, uri.port)
-      http.read_timeout = 60 * 5 # 5 min
-      begin
-        response = http.request(request)
-      rescue StandardError => e
-        puts "issue uploading file: #{e}"
-        next
-      end
-      metadata = JSON.parse(response.body)
-      local_digest = Digest::MD5.hexdigest(body)
-      if metadata['md5'] == local_digest
-        puts "file successfully uploaded: #{metadata['filename']}"
-      else
-        puts "issues uploading file: #{response.code} - #{response.body}"
-      end
-      break
-    end
-    raise 'failed to upload to saucelabs after numerous retries'
-  end
-end
+# Luke - Sep 2019
+# This method doesn't appear to have any caller locations and there is a duplicate much more complex
+# method inside the /rb directory proper
+#
+# Removing this "should" in theory be ok. But will tackle it at a later date once other
+# simplifications have been made!
+#
