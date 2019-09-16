5573706feaacb069e207c26e0535a4edb20f0050
Alexei Barantsev <barancev@gmail.com>
2019-09-16 10:20:57 +0300

    Oops, forgot about sudo

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,10 +1,10 @@
 set -ex
 
 if [[ ! -z $CHROME ]]; then
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-  apt-get update -qqy
-  apt-get -qqy install google-chrome-stable
+  sudo apt-get update -qqy
+  sudo apt-get -qqy install google-chrome-stable
   rm /etc/apt/sources.list.d/google-chrome.list
   export CHROMEDRIVER_VERSION=`curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE`
   curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
