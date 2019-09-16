3a7a86d6331ecdd8311a6cedc30f723fe2e53934
Alexei Barantsev <barancev@gmail.com>
2019-09-16 10:03:35 +0300

    An attempt to install the proper chrome version by install script

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,6 +1,11 @@
 set -ex
 
 if [[ ! -z $CHROME ]]; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+  apt-get update -qqy
+  apt-get -qqy install google-chrome-stable
+  rm /etc/apt/sources.list.d/google-chrome.list
   export CHROMEDRIVER_VERSION=`curl -s http://chromedriver.storage.googleapis.com/LATEST_RELEASE`
   curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
   unzip chromedriver_linux64.zip && chmod +x chromedriver && sudo mv chromedriver /usr/local/bin
