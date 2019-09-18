5f38a0da9b8360af1ea1276565499adef0dd4f0c
Jan Trejbal <jan.trejbal@scalesoft.cz>
2019-09-18 19:03:20 +0200

    Fixing the .NET WebSocket message handler
    This change fixes how responses are mapped to the request and propagated
    back to the caller, as well as correcting the use of Try* methods to
    check for return values.
    
    Signed-off-by: Jim Evans <james.h.evans.jr@gmail.com>

--- dotnet/src/webdriver/DevTools/AutoGenerated/DevToolsSession.cs
+++ dotnet/src/webdriver/DevTools/AutoGenerated/DevToolsSession.cs
@@ -1,3 +1,4 @@
+
 namespace OpenQA.Selenium.DevTools
 {
     using System;
@@ -159,31 +160,52 @@ namespace OpenQA.Selenium.DevTools
             var responseWasReceived = await Task.Run(() => message.SyncEvent.Wait(millisecondsTimeout.Value, cancellationToken));
 
             if (!responseWasReceived && throwExceptionIfResponseNotReceived)
-                throw new InvalidOperationException($"A command response was not received: {commandName}");
-
-            DevToolsCommandData modified;
-            m_pendingCommands.TryRemove(message.CommandId, out modified);
-            if (modified.IsError)
             {
-                var errorMessage = modified.Result.Value<string>("message");
-                var errorData = modified.Result.Value<string>("data");
-
-                var exceptionMessage = $"{commandName}: {errorMessage}";
-                if (!String.IsNullOrWhiteSpace(errorData))
-                    exceptionMessage = $"{exceptionMessage} - {errorData}";
-
-                LogTrace("Recieved Error Response {0}: {1} {2}", modified.CommandId, message, errorData);
-                throw new CommandResponseException(exceptionMessage)
-                {
-                    Code = modified.Result.Value<long>("code")
-                };
+                throw new InvalidOperationException($"A command response was not received: {commandName}");
             }
 
-            return modified.Result;
+            if (m_pendingCommands.TryRemove(message.CommandId, out var modified))
+            {
+                if (modified.IsError)
+                {
+                    var errorMessage = modified.Result.Value<string>("message");
+                    var errorData = modified.Result.Value<string>("data");
+
+                    var exceptionMessage = $"{commandName}: {errorMessage}";
+                    if (!String.IsNullOrWhiteSpace(errorData))
+                        exceptionMessage = $"{exceptionMessage} - {errorData}";
+
+                    LogTrace("Recieved Error Response {0}: {1} {2}", modified.CommandId, message, errorData);
+                    throw new CommandResponseException(exceptionMessage)
+                    {
+                        Code = modified.Result.Value<long>("code")
+                    };
+                }
+
+                return modified.Result;
+            }
+
+            return null;
         }
 
         private async Task OpenSessionConnection(CancellationToken cancellationToken)
         {
+            if (m_sessionSocket == null)
+            {
+                return;
+            }
+
+            // Try to prevent "System.InvalidOperationException: The WebSocket has already been started."
+            while (m_sessionSocket.State == WebSocketState.Connecting)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                await Task.Delay(10);
+            }
+
             if (m_sessionSocket.State != WebSocketState.Open)
             {
                 await m_sessionSocket.ConnectAsync(new Uri(m_endpointAddress), cancellationToken);
@@ -247,26 +269,35 @@ namespace OpenQA.Selenium.DevTools
         {
             var messageObject = JObject.Parse(message);
 
-            long commandId;
-            if (messageObject.TryGetValue("id", out JToken idProperty) && (commandId = idProperty.Value<long>()) == m_currentCommandId)
+            if (messageObject.TryGetValue("id", out var idProperty))
             {
+                var commandId = idProperty.Value<long>();
+
                 DevToolsCommandData commandInfo;
-                m_pendingCommands.TryGetValue(commandId, out commandInfo);
-                if (messageObject.TryGetValue("error", out JToken errorProperty))
+                if (m_pendingCommands.TryGetValue(commandId, out commandInfo))
                 {
-                    commandInfo.IsError = true;
-                    commandInfo.Result = errorProperty;
+                    if (messageObject.TryGetValue("error", out var errorProperty))
+                    {
+                        commandInfo.IsError = true;
+                        commandInfo.Result = errorProperty;
+                    }
+                    else
+                    {
+                        commandInfo.Result = messageObject["result"];
+                        LogTrace("Recieved Response {0}: {1}", commandId, commandInfo.Result.ToString());
+                    }
+
                     commandInfo.SyncEvent.Set();
-                    return;
+                }
+                else
+                {
+                    LogError("Recieved Unknown Response {0}: {1}", commandId, message);
                 }
 
-                commandInfo.Result = messageObject["result"];
-                LogTrace("Recieved Response {0}: {1}", commandId, commandInfo.Result.ToString());
-                commandInfo.SyncEvent.Set();
                 return;
             }
 
-            if (messageObject.TryGetValue("method", out JToken methodProperty))
+            if (messageObject.TryGetValue("method", out var methodProperty))
             {
                 var method = methodProperty.Value<string>();
                 var methodParts = method.Split(new char[] { '.' }, 2);
