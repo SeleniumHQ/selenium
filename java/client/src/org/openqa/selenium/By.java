7108f3abff15313202a1df4ab216a4295534db50
Alexei Barantsev <barancev@gmail.com>
2019-09-29 15:08:06 +0300

    [java] Deleting unused imports

--- java/client/src/org/openqa/selenium/By.java
+++ java/client/src/org/openqa/selenium/By.java
@@ -30,8 +30,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
