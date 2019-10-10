9bca9da3f8ffc69123ece9930ef08a362346f0c9
Luke Hill <lukehill_uk@hotmail.com>
2019-10-10 15:01:03 +0200

    Dry up checks to only retain the checker methods we use

--- rake_tasks/selenium_rake/checks.rb
+++ rake_tasks/selenium_rake/checks.rb
@@ -5,38 +5,20 @@ require 'rbconfig'
 module SeleniumRake
   class Checks
     class << self
-      # y
+      PRESENT_CACHE = {}.freeze
+
       def windows?
         (RbConfig::CONFIG['host_os'] =~ /mswin|msys|mingw32/) != nil
       end
 
-      # y
       def mac?
         (RbConfig::CONFIG['host_os'] =~ /darwin|mac os/) != nil
       end
 
-      def linux?
-        (RbConfig::CONFIG['host_os'] =~ /linux/) != nil
-      end
-
-      def cygwin?
-        RUBY_PLATFORM.downcase.include?('cygwin')
-      end
-
-      # y
       def dir_separator
         File::ALT_SEPARATOR || File::SEPARATOR
       end
 
-      def env_separator
-        File::PATH_SEPARATOR
-      end
-
-      def jruby?
-        RUBY_PLATFORM =~ /java/
-      end
-
-      # y
       def path_for(path)
         windows? ? path.gsub('/', dir_separator) : path
       end
@@ -49,45 +31,33 @@ module SeleniumRake
         end
       end
 
-      PRESENT_CACHE = {}.freeze
-
-      # y
       def chrome?
         present?('chromedriver') || present?('chromedriver.exe')
       end
 
-      # y
       def edge?
         present?('msedgedriver') || present?('msedgedriver.exe')
       end
 
-      # Think of the confusion if we called this "g++"
-      def gcc?
-        linux? && present?('g++')
-      end
-
-      def msbuild_installed?
-        windows? && present?('msbuild.exe')
-      end
-
-      # y
       def opera?
         present?('opera') || present?('Opera')
       end
 
-      # y
       def python?
         present?('python') || present?('python.exe')
       end
 
       private
 
-      # y used
+      def cygwin?
+        RUBY_PLATFORM.downcase.include?('cygwin')
+      end
+
       def present?(arg)
         return PRESENT_CACHE[arg] if PRESENT_CACHE.key?(arg)
 
         bool = prefixes.any? do |prefix|
-          File.exist?(prefix + File::SEPARATOR + arg)
+          File.exist?("#{prefix}#{File::SEPARATOR}#{arg}")
         end
 
         bool = File.exist?("/Applications/#{arg}.app") if !bool && mac?
