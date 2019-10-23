473a136e639a62bc35af9e5815a8a8b60f852499
Luke Hill <lukehill_uk@hotmail.com>
2019-10-23 13:47:20 +0200

    Namespace ExportMappings correctly and mark it as potentially obsolete

--- rake_tasks/crazy_fun/mappings/export_mappings.rb
+++ rake_tasks/crazy_fun/mappings/export_mappings.rb
@@ -1,7 +1,13 @@
-class ExportMappings
-  def add_all(fun)
-    fun.add_mapping("export_file", Export::CheckPreconditions.new)
-    fun.add_mapping("export_file", Export::CreateTask.new)
-    fun.add_mapping("export_file", Export::AddDependencies.new)
+module CrazyFun
+  module Mappings
+    class ExportMappings
+      # TODO: LH - Check the usage of this. It seems it isn't consumed
+      # If so all downstream fun mappings (Export::), can be removed
+      def add_all(fun)
+        fun.add_mapping("export_file", Export::CheckPreconditions.new)
+        fun.add_mapping("export_file", Export::CreateTask.new)
+        fun.add_mapping("export_file", Export::AddDependencies.new)
+      end
+    end
   end
 end
