a2e6d4b2640db4ac09b22f23c79f652a8ff2ac7c
Luke Hill <lukehill_uk@hotmail.com>
2019-10-25 11:00:31 +0200

    Remove python top level class method which is never included or extended Alter rakefile order, remove uncalled requires and add comments about each section of requires

--- rake_tasks/crazy_fun/mappings/python.rb
+++ rake_tasks/crazy_fun/mappings/python.rb
@@ -1,7 +1,1 @@
 require 'rake_tasks/selenium_rake/checks'
-
-module Python
-  def self.lib_dir
-     Dir.glob('build/lib*')[0] || 'build/lib'
-  end
-end
--- rake_tasks/crazy_fun/mappings/rename/check_preconditions.rb
+++
@@ -1,9 +0,0 @@
-module Folder
-  class CheckPreconditions < Tasks
-    def handle(fun, dir, args)
-      raise StandardError, ":name must be set" if args[:name].nil?
-      raise StandardError, ":srcs or :deps must be set" if args[:srcs].nil? and args[:deps].nil?
-      raise StandardError, ":out must be set" if args[:out].nil?
-    end
-  end
-end
