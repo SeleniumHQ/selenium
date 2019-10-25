341dc6e9689909bfde792e7e028c4c50cf63e157
Luke Hill <lukehill_uk@hotmail.com>
2019-10-25 10:55:52 +0200

    Small tidy to Rakefile and moving requires around to avoid duplication

--- rake_tasks/crazy_fun/mappings/python.rb
+++ rake_tasks/crazy_fun/mappings/python.rb
@@ -1,8 +1,5 @@
 require 'rake_tasks/selenium_rake/checks'
 
-require_relative 'python/add_normal_dependencies'
-require_relative 'python/py_task'
-
 module Python
   def self.lib_dir
      Dir.glob('build/lib*')[0] || 'build/lib'
--- rake_tasks/crazy_fun/mappings/python/py_task.rb
+++
@@ -1,9 +0,0 @@
-module Python
-  class PyTask < Tasks
-    def get_resources(_browser, args)
-      resources = []
-      resources.concat(args[:resources]) if args[:resources]
-      resources
-    end
-  end
-end
