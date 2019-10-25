46a3c907f7e87d36731e438fd3aa572e980cdf1d
Luke Hill <lukehill_uk@hotmail.com>
2019-10-25 10:15:18 +0200

    Commented out a load more top level mappings files
    If these aren't consumed. We can remove nearly 1000 LOC

---
+++ rake_tasks/crazy_fun/mappings/gcc_mappings.rb
@@ -0,0 +1,7 @@
+# module CrazyFun
+#   module Mappings
+#     class GccMappings
+#
+#     end
+#   end
+# end
--- rake_tasks/crazy_fun/mappings/python_mappings.rb
+++ rake_tasks/crazy_fun/mappings/python_mappings.rb
@@ -1,6 +1,6 @@
-class PythonMappings
-  def add_all(fun)
-    fun.add_mapping("py_test", Python::AddDependencies.new)
-    fun.add_mapping("py_test", Python::RunTests.new)
-  end
-end
+# class PythonMappings
+#   def add_all(fun)
+#     fun.add_mapping("py_test", Python::AddDependencies.new)
+#     fun.add_mapping("py_test", Python::RunTests.new)
+#   end
+# end
