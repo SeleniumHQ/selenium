95bb2fbebcc5c6b26dd9863e798d748c5c798ba8
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2019-11-05 12:05:04 +0000

    CrazyFun: Remaining Namespace work (Aside from the .rl items) (#7736)
    Namespace all of RubyMappings classes, tasks (and make inheritance chain correct), and RakeMappings (in the same vein as RubyMappings)

--- rake_tasks/crazy_fun/mappings/ruby_mappings.rb
+++ rake_tasks/crazy_fun/mappings/ruby_mappings.rb
@@ -8,20 +8,24 @@ require_relative 'ruby_mappings/ruby_library'
 require_relative 'ruby_mappings/ruby_linter'
 require_relative 'ruby_mappings/ruby_test'
 
-class RubyMappings
-  def add_all(fun)
-    fun.add_mapping "ruby_library", RubyLibrary.new
+module CrazyFun
+  module Mappings
+    class RubyMappings
+      def add_all(fun)
+        fun.add_mapping "ruby_library", RubyLibrary.new
 
-    fun.add_mapping "ruby_test", CheckTestArgs.new
-    fun.add_mapping "ruby_test", AddTestDefaults.new
-    fun.add_mapping "ruby_test", ExpandSourceFiles.new
-    fun.add_mapping "ruby_test", RubyTest.new
-    fun.add_mapping "ruby_test", AddTestDependencies.new
+        fun.add_mapping "ruby_test", CheckTestArgs.new
+        fun.add_mapping "ruby_test", AddTestDefaults.new
+        fun.add_mapping "ruby_test", ExpandSourceFiles.new
+        fun.add_mapping "ruby_test", RubyTest.new
+        fun.add_mapping "ruby_test", AddTestDependencies.new
 
-    fun.add_mapping "ruby_lint", ExpandSourceFiles.new
-    fun.add_mapping "ruby_lint", RubyLinter.new
+        fun.add_mapping "ruby_lint", ExpandSourceFiles.new
+        fun.add_mapping "ruby_lint", RubyLinter.new
 
-    fun.add_mapping "rubydocs", RubyDocs.new
-    fun.add_mapping "rubygem", RubyGem.new
+        fun.add_mapping "rubydocs", RubyDocs.new
+        fun.add_mapping "rubygem", RubyGem.new
+      end
+    end
   end
 end
