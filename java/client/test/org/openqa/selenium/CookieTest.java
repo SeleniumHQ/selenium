b7381d6a3440c7289b62682d6cbe36d6c33cbc66
Gerry Gao <gerryg@google.com>
2019-12-10 13:06:40 -0800

    Add `SameSite` attribute to `Cookie`
    Cookie object in Selenium Java doesn't support setting `SameSite`
    attribute, described in the spce:
    https://tools.ietf.org/html/draft-ietf-httpbis-rfc6265bis-03#section-5.3.7
    
    ChromeDriver already supports it. So improving Selenium Java API to
    support it as well.
    
    Partially fix #7798

--- java/client/test/org/openqa/selenium/CookieTest.java
+++ java/client/test/org/openqa/selenium/CookieTest.java
@@ -75,10 +75,19 @@ public class CookieTest {
   }
 
   @Test
+  public void testCookiesShouldAllowSameSiteToBeSet() {
+    Cookie cookie = new Cookie("name", "value", "", "/", new Date(), false, true, "Lax");
+    assertThat(cookie.getSameSite()).isEqualTo("Lax");
+
+    Cookie builderCookie = new Cookie.Builder("name", "value").sameSite("Lax").build();
+    assertThat(builderCookie.getSameSite()).isEqualTo("Lax");
+  }
+
+  @Test
   public void testCookieSerializes() throws IOException, ClassNotFoundException {
     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-    Cookie cookieToSerialize = new Cookie("Fish", "cod", "", "", null, false);
+    Cookie cookieToSerialize = new Cookie("Fish", "cod", "", "", null, false, true, "Lax");
 
     objectOutputStream.writeObject(cookieToSerialize);
     byte[] serializedCookie = byteArrayOutputStream.toByteArray();
