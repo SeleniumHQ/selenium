2a0652c1d3164c56447ac69b90afd63d561dbe9e
Gerry Gao <gerryg@google.com>
2019-12-17 16:46:04 -0800

    Update "sameSite" JSON object key to "samesite".
    This is to align with W3C WebDriver Spec.
    https://w3c.github.io/webdriver/#dfn-table-for-cookie-conversion

--- java/client/test/org/openqa/selenium/remote/RemoteWebDriverUnitTest.java
+++ java/client/test/org/openqa/selenium/remote/RemoteWebDriverUnitTest.java
@@ -623,7 +623,7 @@ public class RemoteWebDriverUnitTest {
     CommandExecutor executor = prepareExecutorMock(
         echoCapabilities,
         valueResponder(ImmutableList.of(
-            ImmutableMap.of("name", "cookie1", "value", "value1", "sameSite", "Lax"),
+            ImmutableMap.of("name", "cookie1", "value", "value1", "samesite", "Lax"),
             ImmutableMap.of("name", "cookie2", "value", "value2"))));
 
     RemoteWebDriver driver = new RemoteWebDriver(executor, new ImmutableCapabilities());
