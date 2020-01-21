68312eb6fe27c8cb4115ed2f67f52b3e4fe8f707
Adam Goucher <adam@mobilexco.com>
2020-01-20 23:14:59 -0500

    properly comparing string in java

--- java/server/src/org/openqa/selenium/grid/distributor/local/LocalDistributor.java
+++ java/server/src/org/openqa/selenium/grid/distributor/local/LocalDistributor.java
@@ -281,7 +281,7 @@ public class LocalDistributor extends Distributor {
     LOG.fine("Refreshing: " + status.getUri());
 
     // check registrationSecret and stop processing if it doesn't match
-    if (status.getRegistrationSecret() != registrationSecret) {
+    if (!status.getRegistrationSecret().equals(registrationSecret)) {
       LOG.severe(String.format("Node at %s failed to send correct registration secret.", status.getUri()));
       return;
     }
