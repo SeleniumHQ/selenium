f068070bf2bd4f0cf567636234bf8b8ed6126f25
Sri Harsha <sri_harsha509@hotmail.com>
2020-03-12 20:28:26 +0530

    Add: Adding sameSite cookie attribute to python bindings
    Signed-off-by: Sri Harsha <sri_harsha509@hotmail.com>

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -947,15 +947,22 @@ class WebDriver(object):
 
         :Args:
          - cookie_dict: A dictionary object, with required keys - "name" and "value";
-            optional keys - "path", "domain", "secure", "expiry"
+            optional keys - "path", "domain", "secure", "expiry", "sameSite"
 
         Usage:
             driver.add_cookie({'name' : 'foo', 'value' : 'bar'})
             driver.add_cookie({'name' : 'foo', 'value' : 'bar', 'path' : '/'})
             driver.add_cookie({'name' : 'foo', 'value' : 'bar', 'path' : '/', 'secure':True})
+            driver.add_cookie({'name': 'foo', 'value': 'bar', 'sameSite': 'Strict'})
 
         """
-        self.execute(Command.ADD_COOKIE, {'cookie': cookie_dict})
+        if 'sameSite' in cookie_dict:
+          if cookie_dict['sameSite'] =='Strict' or cookie_dict['sameSite'] == 'Lax':
+            self.execute(Command.ADD_COOKIE, {'cookie': cookie_dict})
+          else:
+            raise NoSuchCookieException("sameSite should be either 'Strict' or 'Lax'")
+        else:
+           self.execute(Command.ADD_COOKIE, {'cookie': cookie_dict})
 
     # Timeouts
     def implicitly_wait(self, time_to_wait):
