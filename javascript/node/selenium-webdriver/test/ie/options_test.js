4a6d7be84ff3de73d11b62b4c8ec3aa145f2c077
Sri Harsha <Harsha509@users.noreply.github.com>
2020-04-03 14:11:24 +0530

    Add: Added Missing 'fileUploadDialogTimeout' capability in JS bindings (#8054)
    * Add: Added Missing 'fileUploadDialogTimeout' option in JS bindings
    
    * Add:[JS] Adding IE test in JS bindings to test fileUploadDialogTimeout
    
    Signed-off-by: Sri Harsha <sri_harsha509@hotmail.com>
    
    Co-authored-by: David Burns <david.burns@theautomatedtester.co.uk>

---
+++ javascript/node/selenium-webdriver/test/ie/options_test.js
@@ -0,0 +1,45 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+'use strict';
+
+const assert = require('assert');
+const ie = require('../../ie');
+const test = require('../../lib/test');
+const {Browser} = require('../../');
+
+test.suite(function(env) {
+  let driver;
+
+  describe('Internet Explorer options', function() {
+    test.ignore(env.browsers(Browser.SAFARI, Browser.FIREFOX, Browser.CHROME, Browser.EDGE)).
+    it('can set fileUploadDialogTimeout', async function() {
+      let timeOut = 10000;
+      let options = new ie.Options().
+          fileUploadDialogTimeout(timeOut);
+
+      driver = await env.builder()
+          .setIeOptions(options)
+          .build();
+
+      let caps = await driver.getCapabilities();
+       caps = caps.map_.get(ie.VENDOR_COMMAND_PREFIX)[ie.Key.FILE_UPLOAD_DIALOG_TIMEOUT];
+      assert.equal(caps, timeOut);
+      await driver.quit();
+    });
+  });
+});
