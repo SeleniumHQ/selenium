ae4aee9a901a781332bc872f32469c420dc3c963
Chris NeJame <SalmonMode@users.noreply.github.com>
2020-04-03 17:42:19 -0400

    Put default in class to speed up lookup time

--- py/selenium/webdriver/safari/webdriver.py
+++ py/selenium/webdriver/safari/webdriver.py
@@ -27,9 +27,6 @@ from .service import Service
 from .remote_connection import SafariRemoteConnection
 
 
-DEFAULT_DRIVER_PATH = "/usr/bin/safaridriver"
-
-
 class WebDriver(RemoteWebDriver):
     """
     Controls the SafariDriver and allows you to drive the browser.
@@ -40,6 +37,8 @@ class WebDriver(RemoteWebDriver):
     setting the driver_path attribute of the class. The executable_path argument
     will be prioritized over the class's driver_path attribute, if it's set.
     """
+    
+    DEFAULT_DRIVER_PATH = "/usr/bin/safaridriver"
 
     driver_path = DEFAULT_DRIVER_PATH
 
