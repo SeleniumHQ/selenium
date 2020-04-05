5c0bbe12dd50a1779e6eaf4b46f1860eaedd4611
Alexei Barantsev <barancev@gmail.com>
2020-04-05 15:56:51 +0300

    [grid] Adding tests for driver server service factory

---
+++ java/server/test/org/openqa/selenium/grid/node/config/DriverServiceSessionFactoryTest.java
@@ -0,0 +1,104 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium.grid.node.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.ImmutableCapabilities;
+import org.openqa.selenium.grid.data.CreateSessionRequest;
+import org.openqa.selenium.grid.node.ActiveSession;
+import org.openqa.selenium.remote.Dialect;
+import org.openqa.selenium.remote.http.HttpClient;
+import org.openqa.selenium.remote.service.DriverService;
+
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.trace.Tracer;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class DriverServiceSessionFactoryTest {
+
+  private Tracer tracer;
+  private HttpClient.Factory clientFactory;
+  private DriverService.Builder builder;
+  private DriverService driverService;
+
+  @Before
+  public void setUp() {
+    tracer = OpenTelemetry.getTracerProvider().get("default");
+    clientFactory = HttpClient.Factory.createDefault();
+    builder = mock(DriverService.Builder.class);
+    driverService = mock(DriverService.class);
+    when(builder.build()).thenReturn(driverService);
+  }
+
+  @Test
+  public void itDelegatesCapabilitiesTestingToPredicate() {
+    DriverServiceSessionFactory factory = factoryFor("chrome", builder);
+
+    assertThat(factory.test(toPayload("chrome"))).isTrue();
+    assertThat(factory.test(toPayload("firefox"))).isFalse();
+
+    verifyNoInteractions(builder);
+  }
+
+  @Test
+  public void shouldNotInstantiateSessionIfNoDialectSpecifiedInARequest() {
+    DriverServiceSessionFactory factory = factoryFor("chrome", builder);
+
+    Optional<ActiveSession> session = factory.apply(new CreateSessionRequest(
+        ImmutableSet.of(), toPayload("chrome"), ImmutableMap.of()));
+
+    assertThat(session).isEmpty();
+    verifyNoInteractions(builder);
+  }
+
+  @Test
+  public void shouldNotInstantiateSessionIfCapabilitiesDoNotMatch() {
+    DriverServiceSessionFactory factory = factoryFor("chrome", builder);
+
+    Optional<ActiveSession> session = factory.apply(new CreateSessionRequest(
+        ImmutableSet.of(Dialect.W3C), toPayload("firefox"), ImmutableMap.of()));
+
+    assertThat(session).isEmpty();
+    verify(builder, times(1)).build();
+    verifyNoMoreInteractions(builder);
+  }
+
+  private DriverServiceSessionFactory factoryFor(String browser, DriverService.Builder builder) {
+    Predicate<Capabilities> predicate = c -> c.getBrowserName().equals(browser);
+    return new DriverServiceSessionFactory(tracer, clientFactory, predicate, builder);
+  }
+
+  private Capabilities toPayload(String browserName) {
+    return new ImmutableCapabilities("browserName", browserName);
+  }
+}
---
+++ java/server/test/org/openqa/selenium/grid/node/config/NodeOptionsTest.java
@@ -0,0 +1,86 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.openqa.selenium.grid.node.config;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.events.EventBus;
+import org.openqa.selenium.events.local.GuavaEventBus;
+import org.openqa.selenium.grid.config.Config;
+import org.openqa.selenium.grid.config.MapConfig;
+import org.openqa.selenium.grid.node.SessionFactory;
+import org.openqa.selenium.grid.node.local.LocalNode;
+import org.openqa.selenium.remote.http.HttpClient;
+
+import io.opentelemetry.OpenTelemetry;
+import io.opentelemetry.trace.Tracer;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class NodeOptionsTest {
+
+  private Tracer tracer;
+  private HttpClient.Factory clientFactory;
+  private LocalNode.Builder builderSpy;
+
+  @Before
+  public void setUp() throws URISyntaxException {
+    tracer = OpenTelemetry.getTracerProvider().get("default");
+    EventBus bus = new GuavaEventBus();
+    clientFactory = HttpClient.Factory.createDefault();
+    URI uri = new URI("http://localhost:1234");
+    LocalNode.Builder builder = LocalNode.builder(tracer, bus, clientFactory, uri, null);
+    builderSpy = spy(builder);
+  }
+
+  @Test
+  public void canCreateNodeOptionsWithDriverDetection() {
+    Config config = new MapConfig(ImmutableMap.of(
+        "node", ImmutableMap.of("detect-drivers", "true")));
+    new NodeOptions(config).configure(tracer, clientFactory, builderSpy);
+
+    verify(builderSpy, atLeastOnce()).add(any(Capabilities.class), any(SessionFactory.class));
+  }
+
+  @Test
+  public void canCreateNodeOptionsWithoutDriverDetection() {
+    Config config = new MapConfig(ImmutableMap.of(
+        "node", ImmutableMap.of("detect-drivers", "false")));
+    new NodeOptions(config).configure(tracer, clientFactory, builderSpy);
+
+    verifyNoInteractions(builderSpy);
+  }
+
+  @Test
+  public void nodeOptionsDoNotDetectDriversByDefault() {
+    Config config = new MapConfig(ImmutableMap.of());
+    new NodeOptions(config).configure(tracer, clientFactory, builderSpy);
+
+    verifyNoInteractions(builderSpy);
+  }
+}
