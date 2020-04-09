eb236e5e37138b88f3def486f973b242abb38c7c
Alexei Barantsev <barancev@gmail.com>
2020-04-10 01:05:42 +0300

    [java] Deleting unused imports

--- java/client/src/org/openqa/selenium/tools/javadoc/JavadocJarMaker.java
+++ java/client/src/org/openqa/selenium/tools/javadoc/JavadocJarMaker.java
@@ -31,7 +31,6 @@ import java.io.OutputStream;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
