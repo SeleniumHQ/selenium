139c1e2e12c171c3c815875c272ed252466fba90
Alexei Barantsev <barancev@gmail.com>
2020-04-24 00:44:48 +0300

    [java] Getting rid of unnecessary generics

--- java/client/test/org/openqa/selenium/atoms/InputAtomsTest.java
+++ java/client/test/org/openqa/selenium/atoms/InputAtomsTest.java
@@ -49,10 +49,10 @@ public class InputAtomsTest {
         global = context.initStandardObjects();
 
         // Check assumptions abut the global context, which the atoms assumes is a DOM window.
-        assertThat((Object) eval(context, "this.window=this;")).isEqualTo(global);
-        assertThat((Object) eval(context, "this")).isEqualTo(global);
-        assertThat((Object) eval(context, "window")).isEqualTo(global);
-        assertThat((Object) eval(context, "this === window")).isEqualTo(true);
+        assertThat(eval(context, "this.window=this;")).isEqualTo(global);
+        assertThat(eval(context, "this")).isEqualTo(global);
+        assertThat(eval(context, "window")).isEqualTo(global);
+        assertThat(eval(context, "this === window")).isEqualTo(true);
 
         eval(context, source, RESOURCE_PATH);
 
@@ -68,17 +68,15 @@ public class InputAtomsTest {
       }
 
       private void assertFunction(Context context, String property) {
-        assertThat((Object) eval(context, "typeof " + property)).describedAs(property).isEqualTo("function");
+        assertThat(eval(context, "typeof " + property)).describedAs(property).isEqualTo("function");
       }
 
-      @SuppressWarnings({"unchecked"})
-      private <T> T eval(Context context, String script) {
-        return (T) eval(context, script, "");
+      private Object eval(Context context, String script) {
+        return eval(context, script, "");
       }
 
-      @SuppressWarnings({"unchecked"})
-      private <T> T eval(Context context, String script, String src) {
-        return (T) context.evaluateString(global, script, src, 1, null);
+      private Object eval(Context context, String script, String src) {
+        return context.evaluateString(global, script, src, 1, null);
       }
     });
   }
