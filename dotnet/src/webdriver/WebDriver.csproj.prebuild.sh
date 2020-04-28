b70332040f2831963df148ad8f29f4710b63ee05
Jim Evans <james.h.evans.jr@gmail.com>
2020-04-28 18:07:34 -0400

    Updating .NET non-Windows build files
    When converting script files for building on non-Windows OSes, the
    paths weren't properly converted on some lines (backslash to slash).

--- dotnet/src/webdriver/WebDriver.csproj.prebuild.sh
+++ dotnet/src/webdriver/WebDriver.csproj.prebuild.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [[ ! -f "$1..\..\..\bazel-bin\javascript\webdriver\atoms\get-attribute.js" ]]
+if [[ ! -f "$1../../../bazel-bin/javascript/webdriver/atoms/get-attribute.js" ]]
 then
   echo "Building getAttribute atom"
   bazel build //javascript/webdriver/atoms:get-attribute.js
 fi
 
-if [[ ! -f "$1..\..\..\bazel-bin\javascript\atoms\fragments\is-displayed.js" ]]
+if [[ ! -f "$1../../../bazel-bin/javascript/atoms/fragments/is-displayed.js" ]]
 then
   echo "Building isDisplayed atom"
   bazel build //javascript/atoms/fragments:is-displayed.js
 fi
 
-if if [[ ! -f "$1..\..\..\bazel-bin\javascript\atoms\fragments\find-elements.js" ]]
+if [[ ! -f "$1../../../bazel-bin/javascript/atoms/fragments/find-elements.js" ]]
 then
   echo "Building findElements atom"
   bazel build //javascript/atoms/fragments:find-elements.js
