4a42ca674abe36d8497de758e7dd187843655cf9
Simon Stewart <simon.m.stewart@gmail.com>
2020-05-12 17:45:51 +0100

    Extend Browser enum to provide some useful information

--- java/client/test/org/openqa/selenium/testing/drivers/Browser.java
+++ java/client/test/org/openqa/selenium/testing/drivers/Browser.java
@@ -17,22 +17,41 @@
 
 package org.openqa.selenium.testing.drivers;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.ImmutableCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariOptions;
+
 import java.util.logging.Logger;
 
+import static org.openqa.selenium.remote.CapabilityType.BROWSER_NAME;
+
 public enum Browser {
-  ALL,
-  CHROME,
-  EDGE,
-  CHROMIUMEDGE,
-  HTMLUNIT,
-  FIREFOX,
-  IE,
-  MARIONETTE,
-  OPERA,
-  OPERABLINK,
-  SAFARI;
+  ALL(new ImmutableCapabilities(), false),
+  CHROME(new ChromeOptions(), true),
+  EDGE(new EdgeOptions(), false),
+  CHROMIUMEDGE(new EdgeOptions(), true),
+  HTMLUNIT(new ImmutableCapabilities(BROWSER_NAME, BrowserType.HTMLUNIT), false),
+  FIREFOX(new FirefoxOptions(), false),
+  IE(new InternetExplorerOptions(), false),
+  MARIONETTE(new FirefoxOptions(), false),
+  OPERA(new OperaOptions(), false),
+  OPERABLINK(new OperaOptions(), false),
+  SAFARI(new SafariOptions(), false);
 
   private static final Logger log = Logger.getLogger(Browser.class.getName());
+  private final Capabilities canonicalCapabilities;
+  private final boolean supportsCdp;
+
+  private Browser(Capabilities canonicalCapabilities, boolean supportsCdp) {
+    this.canonicalCapabilities = ImmutableCapabilities.copyOf(canonicalCapabilities);
+    this.supportsCdp = supportsCdp;
+  }
 
   public static Browser detect() {
     String browserName = System.getProperty("selenium.browser");
@@ -63,5 +82,13 @@ public enum Browser {
     }
 
     throw new RuntimeException(String.format("Cannot determine driver from name %s", browserName));
+  }
+
+  public boolean supportsCdp() {
+    return supportsCdp;
+  }
+
+  public Capabilities getCapabilities() {
+    return canonicalCapabilities;
   }
 }
