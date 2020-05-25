33e6b0785a868df5005c85c281410955d232c280
Alexei Barantsev <barancev@gmail.com>
2020-05-25 23:14:42 +0300

    [java] Merging AbstractCapabilitiees into MutableCapabilities

--- java/client/src/org/openqa/selenium/MutableCapabilities.java
+++ java/client/src/org/openqa/selenium/MutableCapabilities.java
@@ -17,13 +17,24 @@
 
 package org.openqa.selenium;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import org.openqa.selenium.internal.Require;
+import org.openqa.selenium.logging.LogLevelMapping;
+import org.openqa.selenium.logging.LoggingPreferences;
 
-public class MutableCapabilities extends AbstractCapabilities implements Serializable {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MutableCapabilities implements Capabilities, Serializable {
 
   private static final long serialVersionUID = -112816287184979465L;
 
@@ -39,6 +50,8 @@ public class MutableCapabilities extends AbstractCapabilities implements Seriali
     keys.add("safari.options");
     OPTION_KEYS = Collections.unmodifiableSet(keys);
   }
+
+  private final Map<String, Object> caps = new TreeMap<>();
 
   public MutableCapabilities() {
     // no-arg constructor
@@ -87,8 +100,9 @@ public class MutableCapabilities extends AbstractCapabilities implements Seriali
     setCapability(capabilityName, (Object) value);
   }
 
-  @Override
   public void setCapability(String key, Object value) {
+    Require.nonNull("Capability name", key);
+
     // We have to special-case some keys and values because of the popular idiom of calling
     // something like "capabilities.setCapability(SafariOptions.CAPABILITY, new SafariOptions());"
     // and this is no longer needed as options are capabilities. There will be a large amount of
@@ -98,6 +112,130 @@ public class MutableCapabilities extends AbstractCapabilities implements Seriali
       return;
     }
 
-    super.setCapability(key, value);
+    if (value == null) {
+      caps.remove(key);
+      return;
+    }
+
+    if ("loggingPrefs".equals(key) && value instanceof Map) {
+      LoggingPreferences prefs = new LoggingPreferences();
+      @SuppressWarnings("unchecked") Map<String, String> prefsMap = (Map<String, String>) value;
+
+      for (String logType : prefsMap.keySet()) {
+        prefs.enable(logType, LogLevelMapping.toLevel(prefsMap.get(logType)));
+      }
+      caps.put(key, prefs);
+      return;
+    }
+
+    if ("platform".equals(key) && value instanceof String) {
+      try {
+        caps.put(key, Platform.fromString((String) value));
+      } catch (WebDriverException e) {
+        caps.put(key, value);
+      }
+      return;
+    }
+
+    if ("unexpectedAlertBehaviour".equals(key)) {
+      caps.put("unexpectedAlertBehaviour", value);
+      caps.put("unhandledPromptBehavior", value);
+      return;
+    }
+
+    caps.put(key, value);
+  }
+
+  @Override
+  public Map<String, Object> asMap() {
+    return Collections.unmodifiableMap(caps);
+  }
+
+  @Override
+  public Object getCapability(String capabilityName) {
+    return caps.get(capabilityName);
+  }
+
+  @Override
+  public Set<String> getCapabilityNames() {
+    return Collections.unmodifiableSet(caps.keySet());
+  }
+
+  public Map<String, Object> toJson() {
+    return asMap();
+  }
+
+  /**
+   * Subclasses can use this to add information that isn't always in the capabilities map.
+   */
+  protected int amendHashCode() {
+    return 0;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(amendHashCode(), caps);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Capabilities)) {
+      return false;
+    }
+
+    Capabilities that = (Capabilities) o;
+
+    return asMap().equals(that.asMap());
+  }
+
+  @Override
+  public String toString() {
+    Map<Object, String> seen = new IdentityHashMap<>();
+    return "Capabilities " + abbreviate(seen, caps);
+  }
+
+  private String abbreviate(Map<Object, String> seen, Object stringify) {
+    if (stringify == null) {
+      return "null";
+    }
+
+    StringBuilder value = new StringBuilder();
+
+    if (stringify.getClass().isArray()) {
+      value.append("[");
+      value.append(
+          Stream.of((Object[]) stringify)
+              .map(item -> abbreviate(seen, item))
+              .collect(Collectors.joining(", ")));
+      value.append("]");
+    } else if (stringify instanceof Collection) {
+      value.append("[");
+      value.append(
+          ((Collection<?>) stringify).stream()
+              .map(item -> abbreviate(seen, item))
+              .collect(Collectors.joining(", ")));
+      value.append("]");
+    } else if (stringify instanceof Map) {
+      value.append("{");
+      value.append(
+          ((Map<?, ?>) stringify).entrySet().stream()
+              .sorted(Comparator.comparing(entry -> String.valueOf(entry.getKey())))
+              .map(entry -> String.valueOf(entry.getKey()) + ": " + abbreviate(seen, entry.getValue()))
+              .collect(Collectors.joining(", ")));
+      value.append("}");
+    } else {
+      String s = String.valueOf(stringify);
+      if (s.length() > 30) {
+        value.append(s, 0, 27).append("...");
+      } else {
+        value.append(s);
+      }
+    }
+
+    seen.put(stringify, value.toString());
+    return value.toString();
   }
 }
