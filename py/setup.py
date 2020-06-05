80a5b192a3c3035df674aea3a38c2e5400dc6def
AutomatedTester <david.burns@theautomatedtester.co.uk>
2020-06-05 15:31:08 +0100

    [py] bump python bindings to post2

--- py/setup.py
+++ py/setup.py
@@ -27,7 +27,7 @@ for scheme in INSTALL_SCHEMES.values():
 setup_args = {
     'cmdclass': {'install': install},
     'name': 'selenium',
-    'version': "4.0.0a6post1",
+    'version': "4.0.0a6post2",
     'license': 'Apache 2.0',
     'description': 'Python bindings for Selenium',
     'long_description': open(join(abspath(dirname(__file__)), "README.rst")).read(),
