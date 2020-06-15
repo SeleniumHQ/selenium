ae500032f7a6eb2a0a8092c33b113b478f4cc4a4
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-06-15 13:14:25 +0530

    Add getClass method

--- java/server/src/org/openqa/selenium/grid/config/Config.java
+++ java/server/src/org/openqa/selenium/grid/config/Config.java
@@ -17,6 +17,8 @@
 
 package org.openqa.selenium.grid.config;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -39,5 +41,31 @@ public interface Config {
 
   default Optional<Boolean> getBool(String section, String option) {
     return get(section, option).map(Boolean::parseBoolean);
+  }
+
+  default <X> Optional<Class<X>> getClass(String section, String option, Class<X> typeOfClass, String defaultClazz) {
+    String clazz = get(section, option).orElse(defaultClazz);
+
+    try {
+      Class<?> ClassClazz = Class.forName(clazz);
+      Method create = ClassClazz.getMethod("create", Config.class);
+
+      if (!Modifier.isStatic(create.getModifiers())) {
+        throw new IllegalArgumentException(String.format(
+            "Class %s's `create(Config)` method must be static", clazz));
+      }
+
+      if (!typeOfClass.isAssignableFrom(create.getReturnType())) {
+        throw new IllegalArgumentException(String.format(
+            "Class %s's `create(Config)` method must be static", clazz));
+      }
+
+      return (Optional<Class<X>>) create.invoke(null, this);
+    } catch (NoSuchMethodException e) {
+      throw new IllegalArgumentException(String.format(
+          "Class %s must have a static `create(Config)` method", clazz));
+    } catch (ReflectiveOperationException e) {
+      throw new IllegalArgumentException("Unable to find class: " + clazz, e);
+    }
   }
 }
