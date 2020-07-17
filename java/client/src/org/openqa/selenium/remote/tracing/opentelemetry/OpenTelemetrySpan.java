a4e7fe0bd447f18c4c97350c3109f3e49ae320f1
Puja Jagani <puja.jagani93@gmail.com>
2020-07-17 14:39:59 +0530

    [tracing] Match client and server spans with OpenTelemetry specification (#8541)
    Co-authored-by: David Burns <david.burns@theautomatedtester.co.uk>

--- java/client/src/org/openqa/selenium/remote/tracing/opentelemetry/OpenTelemetrySpan.java
+++ java/client/src/org/openqa/selenium/remote/tracing/opentelemetry/OpenTelemetrySpan.java
@@ -127,6 +127,15 @@ class OpenTelemetrySpan extends OpenTelemetryContext implements AutoCloseable, S
       .put(Status.Kind.OK, io.opentelemetry.trace.Status.OK)
       .put(Status.Kind.RESOURCE_EXHAUSTED, io.opentelemetry.trace.Status.RESOURCE_EXHAUSTED)
       .put(Status.Kind.UNKNOWN, io.opentelemetry.trace.Status.UNKNOWN)
+      .put(Status.Kind.INVALID_ARGUMENT,io.opentelemetry.trace.Status.INVALID_ARGUMENT)
+      .put(Status.Kind.DEADLINE_EXCEEDED,io.opentelemetry.trace.Status.DEADLINE_EXCEEDED)
+      .put(Status.Kind.ALREADY_EXISTS,io.opentelemetry.trace.Status.ALREADY_EXISTS)
+      .put(Status.Kind.PERMISSION_DENIED,io.opentelemetry.trace.Status.PERMISSION_DENIED)
+      .put(Status.Kind.OUT_OF_RANGE,io.opentelemetry.trace.Status.OUT_OF_RANGE)
+      .put(Status.Kind.UNIMPLEMENTED,io.opentelemetry.trace.Status.UNIMPLEMENTED)
+      .put(Status.Kind.INTERNAL,io.opentelemetry.trace.Status.INTERNAL)
+      .put(Status.Kind.UNAVAILABLE,io.opentelemetry.trace.Status.UNAVAILABLE)
+      .put(Status.Kind.UNAUTHENTICATED,io.opentelemetry.trace.Status.UNAUTHENTICATED)
       .build();
 
   @Override
