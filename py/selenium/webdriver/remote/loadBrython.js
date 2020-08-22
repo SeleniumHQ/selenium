697f6927e5ecd15fe3afd3f6b41be670d16cad87
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-08-22 13:01:41 -0700

    Edited loadBrython.js and runBrython.js to comply with Lint Code Base recommendations

--- py/selenium/webdriver/remote/loadBrython.js
+++ py/selenium/webdriver/remote/loadBrython.js
@@ -1,4 +1,4 @@
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "{}";
-document.head.appendChild(script);
+var script = document.createElement('script')
+script.type = 'text/javascript'
+script.src = '{}'
+document.head.appendChild(script)
--- py/selenium/webdriver/remote/runBrython.js
+++ py/selenium/webdriver/remote/runBrython.js
@@ -1,7 +1,7 @@
-var script_tag = document.createElement("script");
-script_tag.type = "text/python";
-var script = document.createTextNode("{}");
-script_tag.appendChild(script);
-document.body.appendChild(script_tag);
-brython();
-script_tag.remove();
+var scriptTag = document.createElement('script')
+scriptTag.type = 'text/python'
+var script = document.createTextNode('{}')
+script_tag.appendChild(script)
+document.body.appendChild(script_tag)
+brython()
+script_tag.remove()
