cb643ceab85910151404cf1647a2fb9d10b39d8f
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-08-22 14:19:49 -0700

    Fixed an error in execute_brython() when using \n, \t, etc in script argument

--- py/selenium/webdriver/remote/runBrython.js
+++ py/selenium/webdriver/remote/runBrython.js
@@ -1,6 +1,6 @@
 var scriptTag = document.createElement('script')
 scriptTag.type = 'text/python'
-var script = document.createTextNode('{}')
+var script = document.createTextNode({})
 scriptTag.appendChild(script)
 document.body.appendChild(scriptTag)
 
