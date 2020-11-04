a1e0b8afe14dc9ceaebc8e59a7dab05d40e5be8e
Diego Molina <diemol@gmail.com>
2020-11-04 10:38:07 +0100

    [java] Reverting default http client to `netty`

--- java/client/src/org/openqa/selenium/remote/http/HttpClient.java
+++ java/client/src/org/openqa/selenium/remote/http/HttpClient.java
@@ -67,7 +67,7 @@ public interface HttpClient extends HttpHandler {
      * {@see create}
      */
     static Factory createDefault() {
-      return create(System.getProperty("webdriver.http.factory", "reactor"));
+      return create(System.getProperty("webdriver.http.factory", "netty"));
     }
 
     /**
