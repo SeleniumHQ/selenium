8ab7f7381ae726ee11c4e8058314a649817e0cdc
Olly Dean <olly@nekofever.com>
2020-11-16 20:11:51 +0000

    Fix grammar typos in exceptions.py (#8876)

--- py/selenium/common/exceptions.py
+++ py/selenium/common/exceptions.py
@@ -158,7 +158,7 @@ class ElementNotVisibleException(InvalidElementStateException):
 class ElementNotInteractableException(InvalidElementStateException):
     """
     Thrown when an element is present in the DOM but interactions
-    with that element will hit another element do to paint order
+    with that element will hit another element due to paint order
     """
     pass
 
@@ -273,7 +273,7 @@ class ScreenshotException(WebDriverException):
 class ElementClickInterceptedException(WebDriverException):
     """
     The Element Click command could not be completed because the element receiving the events
-    is obscuring the element that was requested clicked.
+    is obscuring the element that was requested to be clicked.
     """
     pass
 
@@ -288,7 +288,7 @@ class InsecureCertificateException(WebDriverException):
 
 class InvalidCoordinatesException(WebDriverException):
     """
-    The coordinates provided to an interactions operation are invalid.
+    The coordinates provided to an interaction's operation are invalid.
     """
     pass
 
