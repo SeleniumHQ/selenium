f2f89b86d5b2951298adfa41843084ba53c79ac0
Alexei Barantsev <barancev@gmail.com>
2020-11-24 17:19:13 +0300

    [java] A silly mistake, forgot to remove old code that should be replaced by the new one

--- java/client/test/org/openqa/selenium/testing/IgnoreComparator.java
+++ java/client/test/org/openqa/selenium/testing/IgnoreComparator.java
@@ -48,7 +48,6 @@ public class IgnoreComparator {
   private boolean shouldIgnore(Stream<Ignore> ignoreList) {
     return ignoreList.anyMatch(
       driver -> (ignored.contains(driver.value()) || driver.value() == Browser.ALL)
-                && (!driver.travis() || TestUtilities.isOnTravis())
                 && ((!driver.travis() || TestUtilities.isOnTravis())
                     || (!driver.gitHubActions() || TestUtilities.isOnGitHubActions()))
                 && isOpen(driver.issue()));
