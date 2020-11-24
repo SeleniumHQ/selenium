3dbf71deb98e6895438b4216fd63dcc791d234f8
Alexei Barantsev <barancev@gmail.com>
2020-11-24 23:47:14 +0300

    [java] Fixing minor issues reported by IDEA code linter

--- java/client/src/org/openqa/selenium/net/UrlChecker.java
+++ java/client/src/org/openqa/selenium/net/UrlChecker.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
