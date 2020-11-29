f17c2b0d993e6a6890f74f571ed8eb211cac30ed
Alexei Barantsev <barancev@gmail.com>
2020-11-29 13:58:19 +0300

    [java] Deleting unused imports

--- java/server/src/org/openqa/selenium/grid/web/PathResource.java
+++ java/server/src/org/openqa/selenium/grid/web/PathResource.java
@@ -22,7 +22,6 @@ import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
