6c7f4afeb3ff236fab7437e8def2680df8823065
Puja Jagani <puja.jagani93@gmail.com>
2021-02-10 13:45:04 +0530

    [grid] Make event bus listener for Queuer and LocalQueue thread-safe

--- java/server/src/org/openqa/selenium/grid/sessionqueue/local/LocalNewSessionQueue.java
+++ java/server/src/org/openqa/selenium/grid/sessionqueue/local/LocalNewSessionQueue.java
@@ -138,8 +138,6 @@ public class LocalNewSessionQueue extends NewSessionQueue {
     Require.nonNull("New Session request", request);
 
     Span span = tracer.getCurrentContext().createSpan("local_sessionqueue.add");
-    boolean added = false;
-    SessionRequest sessionRequest = new SessionRequest(requestId, request);
 
     Lock writeLock = lock.writeLock();
     writeLock.lock();
@@ -148,63 +146,63 @@ public class LocalNewSessionQueue extends NewSessionQueue {
       attributeMap.put(AttributeKey.LOGGER_CLASS.getKey(),
         EventAttribute.setValue(getClass().getName()));
 
-      added = sessionRequests.offerLast(sessionRequest);
+      SessionRequest sessionRequest = new SessionRequest(requestId, request);
       addRequestHeaders(request, requestId);
+      boolean added = sessionRequests.offerLast(sessionRequest);
 
       attributeMap.put(
         AttributeKey.REQUEST_ID.getKey(), EventAttribute.setValue(requestId.toString()));
       attributeMap.put("request.added", EventAttribute.setValue(added));
       span.addEvent("Add new session request to the queue", attributeMap);
 
+      if (added) {
+        bus.fire(new NewSessionRequestEvent(requestId));
+      }
+
       return added;
     } finally {
       writeLock.unlock();
       span.close();
-      if (added) {
-        bus.fire(new NewSessionRequestEvent(requestId));
-      }
     }
   }
 
   @Override
   public boolean offerFirst(HttpRequest request, RequestId requestId) {
     Require.nonNull("New Session request", request);
-    boolean added = false;
-    SessionRequest sessionRequest = new SessionRequest(requestId, request);
-
     Lock writeLock = lock.writeLock();
     writeLock.lock();
     try {
-      added = sessionRequests.offerFirst(sessionRequest);
-      return added;
-    } finally {
-      writeLock.unlock();
+      SessionRequest sessionRequest = new SessionRequest(requestId, request);
+      boolean added = sessionRequests.offerFirst(sessionRequest);
       if (added) {
         executorService.schedule(() -> retryRequest(sessionRequest),
           super.retryInterval.getSeconds(), TimeUnit.SECONDS);
       }
+      return added;
+    } finally {
+      writeLock.unlock();
     }
   }
 
   private void retryRequest(SessionRequest sessionRequest) {
-    HttpRequest request = sessionRequest.getHttpRequest();
-    RequestId requestId = sessionRequest.getRequestId();
-    if (hasRequestTimedOut(request)) {
-      LOG.log(Level.INFO, "Request {0} timed out", requestId);
-      Lock writeLock = lock.writeLock();
-      writeLock.lock();
-      try {
+    Lock writeLock = lock.writeLock();
+    writeLock.lock();
+    try {
+      HttpRequest request = sessionRequest.getHttpRequest();
+      RequestId requestId = sessionRequest.getRequestId();
+      if (hasRequestTimedOut(request)) {
+        LOG.log(Level.INFO, "Request {0} timed out", requestId);
         sessionRequests.remove(sessionRequest);
-      } finally {
-        writeLock.unlock();
         bus.fire(new NewSessionRejectedEvent(
           new NewSessionErrorResponse(requestId, "New session request timed out")));
+      } else {
+        LOG.log(Level.INFO,
+          "Adding request back to the queue. All slots are busy. Request: {0}",
+          requestId);
+        bus.fire(new NewSessionRequestEvent(requestId));
       }
-    } else {
-      LOG.log(Level.INFO,
-        "Adding request back to the queue. All slots are busy. Request: {0}",
-        requestId);
-      bus.fire(new NewSessionRequestEvent(requestId));
+    } finally {
+      writeLock.unlock();
     }
   }
 
