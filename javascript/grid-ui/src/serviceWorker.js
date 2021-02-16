88279c820e2b8020d9209c11c4ac800bc70fb591
Sri Harsha <sri_harsha509@hotmail.com>
2021-02-16 13:37:03 +0530

    [JS] Removing duplicate license text updated by update_copyright.js

--- javascript/grid-ui/src/serviceWorker.js
+++ javascript/grid-ui/src/serviceWorker.js
@@ -15,6 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// This optional code is used to register a service worker.
+// register() is not called by default.
+
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on subsequent visits to a page, after all the
