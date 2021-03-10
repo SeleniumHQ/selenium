ea6db47e218a788f75111d9cedc6da67d60d5a38
AutomatedTester <david.burns@theautomatedtester.co.uk>
2021-03-10 10:50:12 +0000

    [build] Set up python for pinned browsers

--- py/conftest.py
+++ py/conftest.py
@@ -31,15 +31,15 @@ from test.selenium.webdriver.common.network import get_lan_ip
 from urllib.request import urlopen
 
 drivers = (
-    'Chrome',
-    'Edge',
-    'Firefox',
-    'Ie',
-    'Remote',
-    'Safari',
-    'WebKitGTK',
-    'ChromiumEdge',
-    'WPEWebKit',
+    'chrome',
+    'edge',
+    'firefox',
+    'ie',
+    'remote',
+    'safari',
+    'webkitgtk',
+    'chromiumedge',
+    'wpewebkit',
 )
 
 
@@ -74,7 +74,7 @@ def driver(request):
     kwargs = {}
 
     try:
-        driver_class = request.param
+        driver_class = request.param.capitalize()
     except AttributeError:
         raise Exception('This test requires a --driver to be specified.')
 
@@ -130,6 +130,9 @@ def driver(request):
             kwargs['executable_path'] = driver_path
         if options is not None:
             kwargs['options'] = options
+
+        print(f"The driver class is {driver_class}")
+        print(f"The kwargs is {kwargs}")
         driver_instance = getattr(webdriver, driver_class)(**kwargs)
     yield driver_instance
 
