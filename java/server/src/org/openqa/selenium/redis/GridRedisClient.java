06d22849fcc16b289245d242952000b57079d1b0
Puja Jagani <puja.jagani93@gmail.com>
2021-06-10 11:38:12 +0530

    [grid] Remove unused imports

--- java/server/src/org/openqa/selenium/redis/GridRedisClient.java
+++ java/server/src/org/openqa/selenium/redis/GridRedisClient.java
@@ -37,7 +37,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class GridRedisClient implements Closeable {
