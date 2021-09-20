2a03bdea8845beb3063d2b866a54a12e7b893178
Jim Evans <james.h.evans.jr@gmail.com>
2021-09-20 19:14:00 -0400

    [dotnet] Add transformation for network responses via CDP

--- dotnet/src/webdriver/NetworkManager.cs
+++ dotnet/src/webdriver/NetworkManager.cs
@@ -30,10 +30,11 @@ namespace OpenQA.Selenium
     {
         private Lazy<DevToolsSession> session;
         private List<NetworkRequestHandler> requestHandlers = new List<NetworkRequestHandler>();
+        private List<NetworkResponseHandler> responseHandlers = new List<NetworkResponseHandler>();
         private List<NetworkAuthenticationHandler> authenticationHandlers = new List<NetworkAuthenticationHandler>();
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RemoteNetwork"/> class.
+        /// Initializes a new instance of the <see cref="NetworkManager"/> class.
         /// </summary>
         /// <param name="driver">The <see cref="IWebDriver"/> instance on which the network should be monitored.</param>
         public NetworkManager(IWebDriver driver)
@@ -161,6 +162,34 @@ namespace OpenQA.Selenium
             this.authenticationHandlers.Clear();
         }
 
+        /// <summary>
+        /// Adds a <see cref="NetworkResponseHandler"/> to examine received network responses,
+        /// and optionally modify the response. 
+        /// </summary>
+        /// <param name="handler">The <see cref="NetworkResponseHandler"/> to add.</param>
+        public void AddResponseHandler(NetworkResponseHandler handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException("handler", "Request handler cannot be null");
+            }
+
+            if (handler.ResponseMatcher == null)
+            {
+                throw new ArgumentException("Matcher for response cannot be null", "handler");
+            }
+
+            this.responseHandlers.Add(handler);
+        }
+
+        /// <summary>
+        /// Clears all added <see cref="NetworkResponseHandler"/> instances.
+        /// </summary>
+        public void ClearResponseHandlers()
+        {
+            this.responseHandlers.Clear();
+        }
+
         private async void OnAuthRequired(object sender, AuthRequiredEventArgs e)
         {
             string requestId = e.RequestId;
@@ -213,12 +242,32 @@ namespace OpenQA.Selenium
 
         private async void OnResponsePaused(object sender, ResponsePausedEventArgs e)
         {
-            await this.session.Value.Domains.Network.AddResponseBody(e.ResponseData);
-            await this.session.Value.Domains.Network.ContinueResponseWithoutModification(e.ResponseData);
+            if (e.ResponseData.Headers.Count > 0)
+            {
+                // If no headers are present, the body cannot be retrieved.
+                await this.session.Value.Domains.Network.AddResponseBody(e.ResponseData);
+            }
+
             if (this.NetworkResponseReceived != null)
             {
                 this.NetworkResponseReceived(this, new NetworkResponseReceivedEventArgs(e.ResponseData));
             }
+
+            foreach (var handler in this.responseHandlers)
+            {
+                if (handler.ResponseMatcher.Invoke(e.ResponseData))
+                {
+                    // NOTE: We create a dummy HttpRequestData object here, because the ContinueRequestWithResponse
+                    // method demands one; however, the only property used by that method is the RequestId property.
+                    // It might be better to refactor that method signature to simply pass the request ID, or
+                    // alternatively, just pass the response data, which should also contain the request ID anyway.
+                    HttpRequestData requestData = new HttpRequestData() { RequestId = e.ResponseData.RequestId };
+                    await this.session.Value.Domains.Network.ContinueRequestWithResponse(requestData, handler.ResponseTransformer(e.ResponseData));
+                    return;
+                }
+            }
+
+            await this.session.Value.Domains.Network.ContinueResponseWithoutModification(e.ResponseData);
         }
     }
 }
