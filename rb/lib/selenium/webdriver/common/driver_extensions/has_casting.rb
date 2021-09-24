20b5cdb70d685c87b78e98acfafd140f2b106f4d
titusfortner <titusfortner@gmail.com>
2021-09-24 10:54:52 -0500

    [rb] add yardoc details for new driver extensions

--- rb/lib/selenium/webdriver/common/driver_extensions/has_casting.rb
+++ rb/lib/selenium/webdriver/common/driver_extensions/has_casting.rb
@@ -22,21 +22,51 @@ module Selenium
     module DriverExtensions
       module HasCasting
 
+        #
+        # What devices ("sinks") are available to be cast to.
+        #
+        # @return [Array] list of sinks available for casting with id and name values
+        #
+
         def cast_sinks
           @bridge.cast_sinks
         end
+
+        #
+        # Sets a specific sink, using its name, as a Cast session receiver target.
+        #
+        # @param [String] name the sink to use as the target
+        #
 
         def cast_sink_to_use=(name)
           @bridge.cast_sink_to_use = name
         end
 
+        #
+        # Starts a tab mirroring session on a specific receiver target.
+        #
+        # @param [String] name the sink to use as the target
+        #
+
         def start_cast_tab_mirroring(name)
           @bridge.start_cast_tab_mirroring(name)
         end
 
+        #
+        # Gets error messages when there is any issue in a Cast session.
+        #
+        # @return [String] the error message
+        #
+
         def cast_issue_message
           @bridge.cast_issue_message
         end
+
+        #
+        # Stops the existing Cast session on a specific receiver target.
+        #
+        # @param [String] name the sink to stop the Cast session
+        #
 
         def stop_casting(name)
           @bridge.stop_casting(name)
