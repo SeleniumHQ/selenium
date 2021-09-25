9efcd57391abc4a55f3ece89e5cc46ef42dc0a1b
titusfortner <titusfortner@gmail.com>
2021-09-25 17:53:47 -0500

    [py] implement remaining w3c compliant capabilities to Base Options class

--- py/selenium/webdriver/common/options.py
+++ py/selenium/webdriver/common/options.py
@@ -41,9 +41,42 @@ class BaseOptions(metaclass=ABCMeta):
         self._caps[name] = value
 
     @property
+    def browser_version(self) -> str:
+        """
+        :returns: the version of the browser if set, otherwise None.
+        """
+        return self._caps["browserVersion"]
+
+    @browser_version.setter
+    def browser_version(self, version: str) -> NoReturn:
+        """
+        Requires the major version of the browser to match provided value:
+        https://w3c.github.io/webdriver/#dfn-browser-version
+
+        :param version: The required version of the browser
+        """
+        self.set_capability("browserVersion", version)
+
+    @property
+    def platform_name(self) -> str:
+        """
+        :returns: The name of the platform
+        """
+        return self._caps["platformName"]
+
+    @platform_name.setter
+    def platform_name(self, platform: str) -> NoReturn:
+        """
+        Requires the platform to match the provided value: https://w3c.github.io/webdriver/#dfn-platform-name
+
+        :param platform: the required name of the platform
+        """
+        self.set_capability("platformName", platform)
+
+    @property
     def page_load_strategy(self) -> str:
         """
-        :returns: page load strategy if set, otherwise "normal"
+        :returns: page load strategy if set, the default is "normal"
         """
         return self._caps["pageLoadStrategy"]
 
@@ -60,6 +93,46 @@ class BaseOptions(metaclass=ABCMeta):
         else:
             raise ValueError("Strategy can only be one of the following: normal, eager, none")
 
+    @property
+    def unhandled_prompt_behavior(self) -> str:
+        """
+        :returns: unhandled prompt behavior if set, the default is "dismiss and notify"
+        """
+        return self._caps["unhandledPromptBehavior"]
+
+    @unhandled_prompt_behavior.setter
+    def unhandled_prompt_behavior(self, behavior: str) -> NoReturn:
+        """
+        How the driver should respond when an alert is present and the command sent is not handling the alert:
+        https://w3c.github.io/webdriver/#dfn-table-of-page-load-strategies
+
+        :param behavior: behavior to use when an alert is encountered
+        """
+        if behavior in ["dismiss", "accept", "dismiss and notify", "accept and notify", "ignore"]:
+            self.set_capability("unhandledPromptBehavior", behavior)
+        else:
+            raise ValueError("Behavior can only be one of the following: dismiss, accept, dismiss and notify, "
+                             "accept and notify, ignore")
+
+    @property
+    def timeouts(self) -> dict:
+        """
+        :returns: Values for implicit timeout, pageLoad timeout and script timeout if set (in milliseconds)
+        """
+        return self._caps["timeouts"]
+
+    @timeouts.setter
+    def timeouts(self, timeouts: dict) -> NoReturn:
+        """
+        How long the driver should wait for actions to complete before returning an error
+        https://w3c.github.io/webdriver/#timeouts
+
+        :param timeouts: values in milliseconds for implicit wait, page load and script timeout
+        """
+        if all(x in timeouts.keys() for x in ["implicit", "pageLoad", "script"]):
+            self.set_capability("timeouts", timeouts)
+        else:
+            raise ValueError("Timeout keys can only be one of the following: implicit, pageLoad, script")
 
     def enable_mobile(self, android_package: str = None, android_activity: str = None, device_serial: str = None):
         """
@@ -94,6 +167,39 @@ class BaseOptions(metaclass=ABCMeta):
         :param value: whether to accept insecure certificates
         """
         self._caps['acceptInsecureCerts'] = value
+
+    @property
+    def strict_file_interactability(self) -> bool:
+        """
+        :returns: whether session is strict about file interactability
+        """
+        return self._caps.get('strictFileInteractability')
+
+    @strict_file_interactability.setter
+    def strict_file_interactability(self, value: bool):
+        """
+        Whether interactability checks will be applied to file type input elements. The default is false.
+
+        :param value: whether file interactability is strict
+        """
+        self._caps['strictFileInteractability'] = value
+
+    @property
+    def set_window_rect(self) -> bool:
+        """
+        :returns: whether the remote end supports setting window size and position
+        """
+        return self._caps.get('setWindowRect')
+
+    @set_window_rect.setter
+    def set_window_rect(self, value: bool):
+        """
+        Whether the remote end supports all of the resizing and positioning commands. The default is false.
+        https://w3c.github.io/webdriver/#dfn-strict-file-interactability
+
+        :param value: whether remote end must support setting window resizing and repositioning
+        """
+        self._caps['setWindowRect'] = value
 
     @property
     def proxy(self) -> Proxy:
