5960548dd86a28ba590ac6543c4dda40dbb51ebb
AutomatedTester <david.burns@theautomatedtester.co.uk>
2021-10-20 19:17:00 +0100

    updating API docs

--- docs/api/py/_static/documentation_options.js
+++ docs/api/py/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.141',
+    VERSION: '4.0.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
