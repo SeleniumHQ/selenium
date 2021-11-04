1d4bb9b2bbefc4c620a3a0606a4260a3e790784a
Nick Gaya <nicholasgaya+github@gmail.com>
2021-11-04 11:40:40 +0000

    [Javascript] Ensure correct serialization of extensions in chromium.Options (#9495)
    Fixes #6676
    
    Co-authored-by: David Burns <david.burns@theautomatedtester.co.uk>

--- javascript/node/selenium-webdriver/test/chrome/options_test.js
+++ javascript/node/selenium-webdriver/test/chrome/options_test.js
@@ -19,10 +19,16 @@
 
 const assert = require('assert')
 const fs = require('fs')
+const path = require('path')
 
 const chrome = require('../../chrome')
 const symbols = require('../../lib/symbols')
 const test = require('../../lib/test')
+
+const WEBEXTENSION_CRX = path.join(
+  __dirname,
+  '../../lib/test/data/chrome/webextension.crx'
+)
 
 describe('chrome.Options', function () {
   describe('addArguments', function () {
@@ -65,7 +71,8 @@ describe('chrome.Options', function () {
       assert.strictEqual(options.options_.extensions, undefined)
 
       options.addExtensions('a', 'b')
-      assert.deepStrictEqual(options.options_.extensions, ['a', 'b'])
+      assert.deepStrictEqual(options.options_.extensions.extensions,
+                             ['a', 'b'])
     })
 
     it('flattens input arrays', function () {
@@ -73,7 +80,7 @@ describe('chrome.Options', function () {
       assert.strictEqual(options.options_.extensions, undefined)
 
       options.addExtensions(['a', 'b'], 'c', [1, 2], 3)
-      assert.deepStrictEqual(options.options_.extensions, [
+      assert.deepStrictEqual(options.options_.extensions.extensions, [
         'a',
         'b',
         'c',
@@ -86,16 +93,15 @@ describe('chrome.Options', function () {
 
   describe('serialize', function () {
     it('base64 encodes extensions', async function () {
-      let expected = fs.readFileSync(__filename, 'base64')
+      let expected = fs.readFileSync(WEBEXTENSION_CRX, 'base64')
       let wire = new chrome.Options()
-        .addExtensions(__filename)
+        .addExtensions(WEBEXTENSION_CRX)
         [symbols.serialize]()
 
-      assert.strictEqual(wire['goog:chromeOptions'].extensions.length, 1)
-      assert.strictEqual(
-        await wire['goog:chromeOptions'].extensions[0],
-        expected
-      )
+      let extensions = wire['goog:chromeOptions'].extensions
+        [symbols.serialize]()
+      assert.strictEqual(extensions.length, 1)
+      assert.strictEqual(await extensions[0], expected)
     })
   })
 
@@ -129,8 +135,12 @@ test.suite(
   function (env) {
     var driver
 
+    beforeEach(function () {
+      driver = null
+    })
+
     afterEach(function () {
-      return driver.quit()
+      return driver && driver.quit()
     })
 
     describe('Chrome options', function () {
@@ -146,6 +156,38 @@ test.suite(
         )
         assert.strictEqual(userAgent, 'foo;bar')
       })
+
+      it('can install an extension from path', async function () {
+        let options = new chrome.Options().addExtensions(WEBEXTENSION_CRX)
+
+        driver = await env.builder().forBrowser('chrome')
+          .setChromeOptions(options).build()
+
+        await driver.get(test.Pages.echoPage)
+        await verifyWebExtensionWasInstalled()
+      })
+
+      it('can install an extension from Buffer', async function () {
+        let options = new chrome.Options()
+          .addExtensions(fs.readFileSync(WEBEXTENSION_CRX))
+
+        driver = await env.builder().forBrowser('chrome')
+          .setChromeOptions(options).build()
+
+        await driver.get(test.Pages.echoPage)
+        await verifyWebExtensionWasInstalled()
+      })
+
+      async function verifyWebExtensionWasInstalled() {
+        let footer = await driver.findElement({
+          id: 'webextensions-selenium-example',
+        })
+        let text = await footer.getText()
+        assert.strictEqual(
+          text,
+          'Content injected by webextensions-selenium-example'
+        )
+      }
     })
   },
   { browsers: ['chrome'] }
