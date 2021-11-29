fc5326f8064b92f0a6431cd43b82769667dd89f8
titusfortner <titusfortner@gmail.com>
2021-11-29 10:38:00 -0600

    [rb] allow passing devices into Action Builder constructor in more extensible way
    Deprecates ordered parameters which will only be seen when not initializing via Driver class
    Creates default keyboard and mouse input object instances in constructor instead of in Bridge

--- rb/spec/unit/selenium/webdriver/common/interactions/pointer_actions_spec.rb
+++ rb/spec/unit/selenium/webdriver/common/interactions/pointer_actions_spec.rb
@@ -25,7 +25,7 @@ module Selenium
       let(:keyboard) { Interactions::KeyInput.new('keyboard') }
       let(:mouse) { Interactions::PointerInput.new(:mouse, name: 'pointer') }
       let(:bridge) { instance_double('Bridge').as_null_object }
-      let(:builder) { ActionBuilder.new(bridge, mouse, keyboard) }
+      let(:builder) { ActionBuilder.new(bridge, devices: [mouse, keyboard]) }
       let(:element) { Element.new(bridge, 'element') }
       let(:element2) { Element.new(bridge, 'element2') }
       let(:duration) { builder.default_move_duration }
