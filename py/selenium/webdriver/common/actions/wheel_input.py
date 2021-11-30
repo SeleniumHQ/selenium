60db0c1b9e3df4d9e47082b8f913b4e0a2f68c53
Alex <iampopovich@gmail.com>
2021-11-30 14:43:39 +0300

    [py] PEP 484 type hints for selenium.webdriver.common.actions (#10103)
    Co-authored-by: David Burns <david.burns@theautomatedtester.co.uk>

--- py/selenium/webdriver/common/actions/wheel_input.py
+++ py/selenium/webdriver/common/actions/wheel_input.py
@@ -16,26 +16,31 @@
 # under the License.
 from . import interaction
 from .input_device import InputDevice
+from typing import Union
 
 from selenium.webdriver.remote.webelement import WebElement
 
 
 class WheelInput(InputDevice):
 
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         super().__init__(name=name)
         self.name = name
         self.type = interaction.WHEEL
 
-    def encode(self):
+    def encode(self) -> dict:
         return {"type": self.type,
                 "id": self.name,
                 "actions": [acts for acts in self.actions]}
 
-    def create_scroll(self, x, y, delta_x, delta_y, duration, origin):
+    def create_scroll(self, x: int, y: int, delta_x: int,
+                      delta_y: int, duration: int, origin) -> None:
         if isinstance(origin, WebElement):
             origin = {"element-6066-11e4-a52e-4f735466cecf": origin.id}
-        self.add_action({"type": "scroll", "x": x, "y": y, "deltaX": delta_x, "deltaY": delta_y, "duration": duration, "origin": origin})
+        self.add_action({"type": "scroll", "x": x, "y": y, "deltaX": delta_x,
+                         "deltaY": delta_y, "duration": duration, 
+                         "origin": origin})
 
-    def create_pause(self, pause_duration):
-        self.add_action({"type": "pause", "duration": int(pause_duration * 1000)})
+    def create_pause(self, pause_duration: Union[int,float]) -> None:
+        self.add_action(
+            {"type": "pause", "duration": int(pause_duration * 1000)})
