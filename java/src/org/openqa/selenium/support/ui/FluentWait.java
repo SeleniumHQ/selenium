20cf90452273c500bbe88df708a0acadcd74b78e
Allon Murienik <mureinik@gmail.com>
2021-12-31 16:05:13 +0200

    [java] Fix FluentWait's sample usage wrt durations (#10208)
    Commit 502301edeb4c4caf27aad125e8e75da3f22691f1 removed some
    deprecated methods which used the deprecated TimeUnit class and kept
    only the overloaded methods that use Java's java.time.Duration class.
    
    However, it missed fixing the sample usage at the beginning of the
    javadoc that still referred to these methods.
    This patch fixes the issue and replaces the method calls in the sample
    usage with the newer methods that accept a Duration argument.
    
    Fixes #10207

--- java/src/org/openqa/selenium/support/ui/FluentWait.java
+++ java/src/org/openqa/selenium/support/ui/FluentWait.java
@@ -49,8 +49,8 @@ import java.util.function.Supplier;
  *   // Waiting 30 seconds for an element to be present on the page, checking
  *   // for its presence once every 5 seconds.
  *   Wait&lt;WebDriver&gt; wait = new FluentWait&lt;WebDriver&gt;(driver)
- *       .withTimeout(30, SECONDS)
- *       .pollingEvery(5, SECONDS)
+ *       .withTimeout(Duration.ofSeconds(30L))
+ *       .pollingEvery(Duration.ofSeconds(5L))
  *       .ignoring(NoSuchElementException.class);
  *
  *   WebElement foo = wait.until(new Function&lt;WebDriver, WebElement&gt;() {
