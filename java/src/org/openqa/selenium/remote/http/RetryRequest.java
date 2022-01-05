2de629be756436d30c029fb4978a281e4e1154c9
Puja Jagani <puja.jagani93@gmail.com>
2022-01-05 12:31:10 +0530

    [java] Fix the retry request filter's max duration value to allow retries

--- java/src/org/openqa/selenium/remote/http/RetryRequest.java
+++ java/src/org/openqa/selenium/remote/http/RetryRequest.java
@@ -42,7 +42,7 @@ public class RetryRequest implements Filter {
       .handleIf(failure -> failure.getCause() instanceof ConnectException)
       .withBackoff(1, 4, ChronoUnit.SECONDS)
       .withMaxRetries(3)
-      .withMaxDuration(Duration.ofSeconds(10))
+      .withMaxDuration(Duration.ofMinutes(1))
       .onRetry(e -> LOG.log(
         getDebugLogLevel(),
         "Connection failure #{0}. Retrying.",
@@ -54,7 +54,7 @@ public class RetryRequest implements Filter {
       .handle(TimeoutException.class)
       .withBackoff(1, 4, ChronoUnit.SECONDS)
       .withMaxRetries(3)
-      .withMaxDuration(Duration.ofSeconds(10))
+      .withMaxDuration(Duration.ofMinutes(10))
       .onRetry(e -> LOG.log(
         getDebugLogLevel(),
         "Read timeout #{0}. Retrying.",
@@ -68,7 +68,7 @@ public class RetryRequest implements Filter {
       .handleResultIf(response -> response.getStatus() == HTTP_UNAVAILABLE)
       .withBackoff(1, 2, ChronoUnit.SECONDS)
       .withMaxRetries(2)
-      .withMaxDuration(Duration.ofSeconds(3))
+      .withMaxDuration(Duration.ofMinutes(2))
       .onRetry(e -> LOG.log(
         getDebugLogLevel(),
         "Failure due to server error #{0}. Retrying.",
