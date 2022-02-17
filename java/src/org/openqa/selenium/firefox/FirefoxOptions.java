76647a07a4f9491a160f996d5dd88075464eccb7
titusfortner <titusfortner@gmail.com>
2022-02-17 15:00:00 -0600

    [java] add fix for Firefox socket origin header bug to Firefox Options by default
    effective for v98+

--- java/src/org/openqa/selenium/firefox/FirefoxOptions.java
+++ java/src/org/openqa/selenium/firefox/FirefoxOptions.java
@@ -64,6 +64,7 @@ public class FirefoxOptions extends AbstractDriverOptions<FirefoxOptions> {
   public FirefoxOptions() {
     setCapability(CapabilityType.BROWSER_NAME, FIREFOX.browserName());
     setAcceptInsecureCerts(true);
+    addPreference("remote.active-protocols", 2);
     setCapability("moz:debuggerAddress", true);
   }
 
