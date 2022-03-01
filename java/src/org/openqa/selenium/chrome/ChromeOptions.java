d3aa1ce326d53a763a1e13610fdc57bd88b13b9e
Diego Molina <diemol@gmail.com>
2022-03-01 16:50:43 +0100

    Commit to test if Java workflow is triggered

--- java/src/org/openqa/selenium/chrome/ChromeOptions.java
+++ java/src/org/openqa/selenium/chrome/ChromeOptions.java
@@ -17,12 +17,12 @@
 
 package org.openqa.selenium.chrome;
 
+import static org.openqa.selenium.remote.Browser.CHROME;
+
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.chromium.ChromiumOptions;
 import org.openqa.selenium.internal.Require;
 import org.openqa.selenium.remote.CapabilityType;
-
-import static org.openqa.selenium.remote.Browser.CHROME;
 
 /**
  * Class to manage options specific to {@link ChromeDriver}.
@@ -58,7 +58,7 @@ public class ChromeOptions extends ChromiumOptions<ChromeOptions> {
   }
 
   public ChromeOptions setLogLevel(ChromeDriverLogLevel logLevel){
-    this.logLevel = Require.nonNull("Log level", logLevel);
+    this.logLevel = Require.nonNull("ChromeDriverLogLevel level", logLevel);
     return this;
   }
 
