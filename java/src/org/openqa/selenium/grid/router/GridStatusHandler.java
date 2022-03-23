d20418e6357691c940df34ec851bc8bdbe89d685
Diego Molina <diemol@gmail.com>
2022-03-23 10:49:39 +0100

    [grid] Reverting fix for #10391
    The spec https://www.w3.org/TR/webdriver1/#status
    does not specify which http code should be
    returned, but it implies that this endpoint
    should be available to query the Grid status.
    
    Besides, returning 503 will break checks already
    implemented by users.

--- java/src/org/openqa/selenium/grid/router/GridStatusHandler.java
+++ java/src/org/openqa/selenium/grid/router/GridStatusHandler.java
@@ -40,7 +40,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeoutException;
 
-import static java.net.HttpURLConnection.HTTP_UNAVAILABLE;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
 import static org.openqa.selenium.grid.data.Availability.UP;
@@ -146,15 +145,10 @@ class GridStatusHandler implements HttpHandler {
 
       HttpResponse res = new HttpResponse()
         .setContent(asJson(ImmutableMap.of("value", value.build())));
-      if (ready) {
-        span.setStatus(Status.OK);
-      } else {
-        res.setStatus(HTTP_UNAVAILABLE);
-        span.setStatus(Status.UNAVAILABLE);
-      }
       HTTP_RESPONSE.accept(span, res);
       HTTP_RESPONSE_EVENT.accept(attributeMap, res);
       attributeMap.put("grid.status", EventAttribute.setValue(ready));
+      span.setStatus(Status.OK);
       span.addEvent("Computed grid status", attributeMap);
       return res;
     }
