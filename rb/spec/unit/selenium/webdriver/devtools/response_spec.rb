2e7f2f38a6b536d8654db7ed927038a2fa6e4e0b
Virendra Negi <virendra.negi@sugarboxnetworks.com>
2022-04-27 06:34:49 +0530

    silence rubocop warning and remove unwanted code

--- rb/spec/unit/selenium/webdriver/devtools/response_spec.rb
+++ rb/spec/unit/selenium/webdriver/devtools/response_spec.rb
@@ -19,17 +19,6 @@
 
 require File.expand_path('../spec_helper', __dir__)
 
-def self.from(id, encoded_body, params)
-  new(
-    id: id,
-    code: params['responseStatusCode'],
-    body: (Base64.strict_decode64(encoded_body) if encoded_body),
-    headers: (params['responseHeaders'] ||  []).each_with_object({}) do |header, hash|
-      hash[header['name']] = header['value']
-    end
-  )
-end
-
 module Selenium
   module WebDriver
     class DevTools
@@ -40,14 +29,12 @@ module Selenium
             response = Response.from(1, nil, params)
             expect(response.headers).to eq({'Connection' => 'Keep-Alive'})
           end
-          
+
           it 'should not raise error on empty responseHeaders' do
-            expect { 
-              Response.from(1, nil, {})
-            }.not_to raise_error
+            expect { Response.from(1, nil, {}) }.not_to raise_error
           end
         end
-      end # Request
+      end # Response
     end # DevTools
   end # WebDriver
 end # Selenium
