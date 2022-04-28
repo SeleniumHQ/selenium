669dd6249eacdb27550f987252ddbbe179a4bbd8
Diego Molina <diemol@gmail.com>
2022-04-28 10:35:24 +0200

    [dotnet] Adding Chrome DevTools 101 and removing 98

--- dotnet/src/webdriver/DevTools/v101/V101Target.cs
+++ dotnet/src/webdriver/DevTools/v101/V101Target.cs
@@ -1,4 +1,4 @@
-// <copyright file="V98Target.cs" company="WebDriver Committers">
+// <copyright file="V101Target.cs" company="WebDriver Committers">
 // Licensed to the Software Freedom Conservancy (SFC) under one
 // or more contributor license agreements. See the NOTICE file
 // distributed with this work for additional information
@@ -21,22 +21,22 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium.DevTools.V98.Target;
+using OpenQA.Selenium.DevTools.V101.Target;
 
-namespace OpenQA.Selenium.DevTools.V98
+namespace OpenQA.Selenium.DevTools.V101
 {
     /// <summary>
-    /// Class providing functionality for manipulating targets for version 98 of the DevTools Protocol
+    /// Class providing functionality for manipulating targets for version 101 of the DevTools Protocol
     /// </summary>
-    public class V98Target : DevTools.Target
+    public class V101Target : DevTools.Target
     {
         private TargetAdapter adapter;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="V98Target"/> class.
+        /// Initializes a new instance of the <see cref="V101Target"/> class.
         /// </summary>
         /// <param name="adapter">The adapter for the Target domain.</param>
-        public V98Target(TargetAdapter adapter)
+        public V101Target(TargetAdapter adapter)
         {
             this.adapter = adapter;
             adapter.DetachedFromTarget += OnDetachedFromTarget;
