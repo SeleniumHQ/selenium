97aeedba6a10e24947958bd0af5c05ce3e093a03
harsha509 <sriharsha_sureshbabu@epam.com>
2022-05-17 00:34:45 +0530

    [JS] Add support to switch frame by id

--- javascript/node/selenium-webdriver/test/frame_test.js
+++ javascript/node/selenium-webdriver/test/frame_test.js
@@ -50,4 +50,18 @@ test.suite(function (env) {
       'This page has iframes'
     )
   })
+
+  it('can switch to a frame by id', async function () {
+    await driver.get(test.Pages.iframePage)
+    await driver.switchTo().frame("iframe1")
+    assert.strictEqual(
+      await driver.executeScript('return document.title'),
+      'We Leave From Here'
+    )
+    await driver.switchTo().parentFrame()
+    assert.strictEqual(
+      await driver.executeScript('return document.title'),
+      'This page has iframes'
+    )
+  })
 })
