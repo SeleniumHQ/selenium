230468d1551ab1067565a8a40c5963280af4c488
titusfortner <titusfortner@gmail.com>
2022-05-31 14:17:36 -0500

    [py] allow setting pointer event attributes in move_by() and move_to_location()

--- py/selenium/webdriver/common/actions/pointer_actions.py
+++ py/selenium/webdriver/common/actions/pointer_actions.py
@@ -82,12 +82,24 @@ class PointerActions(Interaction):
                                         altitude_angle=altitude_angle, azimuth_angle=azimuth_angle)
         return self
 
-    def move_by(self, x, y):
-        self.source.create_pointer_move(origin=interaction.POINTER, duration=self._duration, x=int(x), y=int(y))
+    def move_by(self, x, y, width=None, height=None, pressure=None,
+                tangential_pressure=None, tilt_x=None, tilt_y=None, twist=None,
+                altitude_angle=None, azimuth_angle=None):
+        self.source.create_pointer_move(origin=interaction.POINTER, duration=self._duration, x=int(x), y=int(y),
+                                        width=width, height=height, pressure=pressure,
+                                        tangential_pressure=tangential_pressure,
+                                        tilt_x=tilt_x, tilt_y=tilt_y, twist=twist,
+                                        altitude_angle=altitude_angle, azimuth_angle=azimuth_angle)
         return self
 
-    def move_to_location(self, x, y):
-        self.source.create_pointer_move(origin='viewport', duration=self._duration, x=int(x), y=int(y))
+    def move_to_location(self, x, y, width=None, height=None, pressure=None,
+                tangential_pressure=None, tilt_x=None, tilt_y=None, twist=None,
+                altitude_angle=None, azimuth_angle=None):
+        self.source.create_pointer_move(origin='viewport', duration=self._duration, x=int(x), y=int(y),
+                                        width=width, height=height, pressure=pressure,
+                                        tangential_pressure=tangential_pressure,
+                                        tilt_x=tilt_x, tilt_y=tilt_y, twist=twist,
+                                        altitude_angle=altitude_angle, azimuth_angle=azimuth_angle)
         return self
 
     def click(self, element=None):
