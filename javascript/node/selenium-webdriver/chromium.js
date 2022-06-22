5f16d286fbea69b8372195c21f4f5d9aa40e35c1
Ravi Sawlani <ravisawlani04@gmail.com>
2022-06-22 20:07:52 +0530

    [JS] kill chromium service on quit

--- javascript/node/selenium-webdriver/chromium.js
+++ javascript/node/selenium-webdriver/chromium.js
@@ -661,9 +661,13 @@ class Driver extends webdriver.WebDriver {
    */
   static createSession(caps, opt_serviceExecutor) {
     let executor
+    let onQuit
     if (opt_serviceExecutor instanceof http.Executor) {
       executor = opt_serviceExecutor
       configureExecutor(executor, this.VENDOR_COMMAND_PREFIX)
+    } else if (opt_serviceExecutor instanceof remote.DriverService) {
+      executor = createExecutor(opt_serviceExecutor.start())
+      onQuit = () => opt_serviceExecutor.kill()
     } else {
       let service = opt_serviceExecutor || this.getDefaultService()
       executor = createExecutor(service.start(), this.VENDOR_COMMAND_PREFIX)
@@ -679,7 +683,7 @@ class Driver extends webdriver.WebDriver {
       }
     }
 
-    return /** @type {!Driver} */ (super.createSession(executor, caps))
+    return /** @type {!Driver} */ (super.createSession(executor, caps, onQuit))
   }
 
   /**
