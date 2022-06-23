fcd5193fb7afc8f46ae701347b004467cadaa4f7
titusfortner <titusfortner@gmail.com>
2022-06-23 09:11:58 -0500

    [py] remove warnings import

--- py/selenium/webdriver/common/actions/pointer_actions.py
+++ py/selenium/webdriver/common/actions/pointer_actions.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import warnings
 
 from selenium.webdriver.remote.webelement import WebElement
 from . import interaction
