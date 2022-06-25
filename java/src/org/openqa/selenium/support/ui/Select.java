751176f611eeea8161804f612c1d9d0d954b2d5a
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-06-26 00:19:56 +0300

    #10812 don't allow selecting a disabled option

--- java/src/org/openqa/selenium/support/ui/Select.java
+++ java/src/org/openqa/selenium/support/ui/Select.java
@@ -109,6 +109,8 @@ public class Select implements ISelect, WrapsElement {
    */
   @Override
   public void selectByVisibleText(String text) {
+    assertSelectIsEnabled();
+
     // try to find the option via XPATH ...
     List<WebElement> options =
       element.findElements(By.xpath(".//option[normalize-space(.) = " + Quotes.escape(text) + "]"));
@@ -173,6 +175,7 @@ public class Select implements ISelect, WrapsElement {
    */
   @Override
   public void selectByIndex(int index) {
+    assertSelectIsEnabled();
     setSelectedByIndex(index, true);
   }
 
@@ -187,6 +190,7 @@ public class Select implements ISelect, WrapsElement {
    */
   @Override
   public void selectByValue(String value) {
+    assertSelectIsEnabled();
     for (WebElement option : findOptionsByValue(value)) {
       setSelected(option, true);
       if (!isMultiple()) {
@@ -311,8 +315,21 @@ public class Select implements ISelect, WrapsElement {
    *          deselected (false)
    */
   private void setSelected(WebElement option, boolean select) {
+    assertOptionIsEnabled(option, select);
     if (option.isSelected() != select) {
       option.click();
+    }
+  }
+
+  private void assertOptionIsEnabled(WebElement option, boolean select) {
+    if (select && !option.isEnabled()) {
+      throw new UnsupportedOperationException("You may not select a disabled option");
+    }
+  }
+
+  private void assertSelectIsEnabled() {
+    if (!element.isEnabled()) {
+      throw new UnsupportedOperationException("You may not select an option in disabled select");
     }
   }
 
