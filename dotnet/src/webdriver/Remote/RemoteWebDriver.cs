51eba6a6657f5ad8726a2bbe2e4182dc076d8259
Puja Jagani <puja.jagani93@gmail.com>
2022-07-07 12:27:27 +0530

    Avoid formatting changes

--- dotnet/src/webdriver/Remote/RemoteWebDriver.cs
+++ dotnet/src/webdriver/Remote/RemoteWebDriver.cs
@@ -20,6 +20,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Internal;
 
 namespace OpenQA.Selenium.Remote
 {
