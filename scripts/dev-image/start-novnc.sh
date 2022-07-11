eaf3f3c2c882f0636efb79f119a4c4567faa134b
Diego Molina <diemol@gmail.com>
2022-07-11 21:10:08 +0200

    Installing noVNC

--- scripts/dev-image/start-novnc.sh
+++ scripts/dev-image/start-novnc.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-#
-# IMPORTANT: Change this file only in directory NodeBase!
 
-#/opt/bin/noVNC/utils/launch.sh --listen ${NO_VNC_PORT} --vnc localhost:${VNC_PORT}
-echo "noVNC won't start because SE_START_NO_VNC is false."
+/home/gitpod/selenium/noVNC/utils/novnc_proxy --listen ${NO_VNC_PORT} --vnc localhost:${VNC_PORT}
