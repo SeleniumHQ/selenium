dc43b9242f004e71bae442989bdca6ce810fca5b
Michael Mintz <mdmintz@gmail.com>
2022-08-18 12:28:44 -0400

    Update install_requires in setup.py with certifi

--- py/setup.py
+++ py/setup.py
@@ -70,7 +70,12 @@ setup_args = {
                  'selenium.webdriver.remote',
                  'selenium.webdriver.support', ],
     'include_package_data': True,
-    'install_requires': ["urllib3[socks]~=1.26", "trio~=0.17", "trio-websocket~=0.9"],
+    'install_requires': [
+        "urllib3[socks]~=1.26",
+        "trio~=0.17",
+        "trio-websocket~=0.9",
+        "certifi>=2021.10.8",
+    ],
     'zip_safe': False
 }
 
