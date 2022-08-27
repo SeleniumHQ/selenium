878feb7fcfd9758d90c03590486816255b10a73f
symonk <jackofspaces@gmail.com>
2022-08-27 14:42:56 +0100

    [py]: Sweeping typing improvements for all property setters

--- py/selenium/webdriver/common/options.py
+++ py/selenium/webdriver/common/options.py
@@ -14,10 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
+import typing
 from abc import ABCMeta, abstractmethod
-from selenium.webdriver.common.proxy import Proxy
+
 from selenium.common.exceptions import InvalidArgumentException
+from selenium.webdriver.common.proxy import Proxy
 
 
 class BaseOptions(metaclass=ABCMeta):
@@ -35,7 +36,7 @@ class BaseOptions(metaclass=ABCMeta):
     def capabilities(self):
         return self._caps
 
-    def set_capability(self, name, value):
+    def set_capability(self, name, value) -> None:
         """ Sets a capability """
         self._caps[name] = value
 
@@ -128,12 +129,13 @@ class BaseOptions(metaclass=ABCMeta):
 
         :param timeouts: values in milliseconds for implicit wait, page load and script timeout
         """
-        if all(x in timeouts.keys() for x in ["implicit", "pageLoad", "script"]):
+        if all(x in timeouts.keys() for x in ("implicit", "pageLoad", "script")):
             self.set_capability("timeouts", timeouts)
         else:
             raise ValueError("Timeout keys can only be one of the following: implicit, pageLoad, script")
 
-    def enable_mobile(self, android_package: str = None, android_activity: str = None, device_serial: str = None):
+    def enable_mobile(self, android_package: typing.Optional[str] = None, android_activity: typing.Optional[str] = None,
+                      device_serial: typing.Optional[str] = None) -> None:
         """
             Enables mobile browser use for browsers that support it
 
@@ -175,7 +177,7 @@ class BaseOptions(metaclass=ABCMeta):
         return self._caps.get('strictFileInteractability')
 
     @strict_file_interactability.setter
-    def strict_file_interactability(self, value: bool):
+    def strict_file_interactability(self, value: bool) -> None:
         """
         Whether interactability checks will be applied to file type input elements. The default is false.
 
@@ -191,7 +193,7 @@ class BaseOptions(metaclass=ABCMeta):
         return self._caps.get('setWindowRect')
 
     @set_window_rect.setter
-    def set_window_rect(self, value: bool):
+    def set_window_rect(self, value: bool) -> None:
         """
         Whether the remote end supports all of the resizing and positioning commands. The default is false.
         https://w3c.github.io/webdriver/#dfn-strict-file-interactability
@@ -208,7 +210,7 @@ class BaseOptions(metaclass=ABCMeta):
         return self._proxy
 
     @proxy.setter
-    def proxy(self, value: Proxy):
+    def proxy(self, value: Proxy) -> None:
         if not isinstance(value, Proxy):
             raise InvalidArgumentException("Only Proxy objects can be passed in.")
         self._proxy = value
