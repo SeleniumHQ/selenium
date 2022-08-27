4029f449b9ef09751b8b9a7452dee955867df580
colons <colons@colons.co>
2022-08-27 21:28:52 +0100

    Fix a bad import.

--- py/test/selenium/webdriver/support/event_firing_webdriver_tests.py
+++ py/test/selenium/webdriver/support/event_firing_webdriver_tests.py
@@ -27,7 +27,7 @@ from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEven
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.remote.webdriver import WebDriver
-from test.selenium.common.webserver import Pages
+from test.selenium.webdriver.common.webserver import Pages
 
 
 @pytest.fixture
