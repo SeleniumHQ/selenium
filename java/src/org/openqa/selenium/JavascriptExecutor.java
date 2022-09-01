b9cd870fbfec1cf0cb71a62c8263008184c899fb
Bartek Florczak <bflorczak@outlook.com>
2022-09-01 11:55:55 +0200

    Use more functional style

--- java/src/org/openqa/selenium/JavascriptExecutor.java
+++ java/src/org/openqa/selenium/JavascriptExecutor.java
@@ -19,7 +19,6 @@ package org.openqa.selenium;
 
 import org.openqa.selenium.internal.Require;
 
-import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -183,9 +182,8 @@ public interface JavascriptExecutor {
    * @return The {@link ScriptKey}s of all currently pinned scripts.
    */
   default Set<ScriptKey> getPinnedScripts() {
-    return Collections.unmodifiableSet(UnpinnedScriptKey.getPinnedScripts(this).stream()
-      .map(key -> (ScriptKey) key)
-      .collect(Collectors.toSet()));
+    return UnpinnedScriptKey.getPinnedScripts(this).stream()
+      .map(ScriptKey.class::cast).collect(Collectors.toUnmodifiableSet());
   }
 
   /**
