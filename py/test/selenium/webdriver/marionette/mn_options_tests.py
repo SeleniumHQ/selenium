4545ba8a5657cea1bd908b1b117567ffad5475f1
symonk <jackofspaces@gmail.com>
2022-10-01 13:29:54 +0100

    [py]: format python tests with `black`

--- py/test/selenium/webdriver/marionette/mn_options_tests.py
+++ py/test/selenium/webdriver/marionette/mn_options_tests.py
@@ -27,7 +27,7 @@ from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture
 def driver_kwargs(driver_kwargs):
-    driver_kwargs['options'] = Options()
+    driver_kwargs["options"] = Options()
     return driver_kwargs
 
 
