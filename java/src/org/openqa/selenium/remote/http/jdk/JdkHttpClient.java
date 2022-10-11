14b66e9fc2964716431e1a229fa054d924152f14
Simon Mavi Stewart <simon.m.stewart@gmail.com>
2022-10-11 16:46:39 +0100

    Add more logging to the JDK HTTP client

--- java/src/org/openqa/selenium/remote/http/jdk/JdkHttpClient.java
+++ java/src/org/openqa/selenium/remote/http/jdk/JdkHttpClient.java
@@ -57,10 +57,13 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static java.net.http.HttpClient.Redirect.ALWAYS;
 
 public class JdkHttpClient implements HttpClient {
+  public static final Logger LOG = Logger.getLogger(JdkHttpClient.class.getName());
   private final JdkHttpMessages messages;
   private final java.net.http.HttpClient client;
   private final Duration readTimeout;
@@ -127,9 +130,11 @@ public class JdkHttpClient implements HttpClient {
 
           @Override
           public CompletionStage<?> onText(java.net.http.WebSocket webSocket, CharSequence data, boolean last) {
+            LOG.fine("Text message received. Appending data");
             builder.append(data);
 
             if (last) {
+              LOG.fine("Final part of text message received. Calling listener with " + builder);
               listener.onText(builder.toString());
               builder.setLength(0);
             }
@@ -140,6 +145,7 @@ public class JdkHttpClient implements HttpClient {
 
           @Override
           public CompletionStage<?> onBinary(java.net.http.WebSocket webSocket, ByteBuffer data, boolean last) {
+            LOG.fine("Binary data received.");
             byte[] ary = new byte[data.remaining()];
             data.get(ary, 0, ary.length);
 
@@ -150,12 +156,14 @@ public class JdkHttpClient implements HttpClient {
 
           @Override
           public CompletionStage<?> onClose(java.net.http.WebSocket webSocket, int statusCode, String reason) {
+            LOG.fine("Closing websocket");
             listener.onClose(statusCode, reason);
             return null;
           }
 
           @Override
           public void onError(java.net.http.WebSocket webSocket, Throwable error) {
+            LOG.log(Level.FINE, error, () -> "An error has occurred: " + error.getMessage());
             listener.onError(error);
             webSocket.request(1);
           }
@@ -170,11 +178,14 @@ public class JdkHttpClient implements HttpClient {
 
         if (message instanceof BinaryMessage) {
           BinaryMessage binaryMessage = (BinaryMessage) message;
+          LOG.fine("Sending binary message");
           makeCall = () -> underlyingSocket.sendBinary(ByteBuffer.wrap(binaryMessage.data()), true);
         } else if (message instanceof TextMessage) {
           TextMessage textMessage = (TextMessage) message;
+          LOG.fine("Sending text message: " + textMessage.text());
           makeCall = () -> underlyingSocket.sendText(textMessage.text(), true);
         } else if (message instanceof CloseMessage) {
+          LOG.fine("Sending close message");
           CloseMessage closeMessage = (CloseMessage) message;
           makeCall = () -> underlyingSocket.sendClose(closeMessage.code(), closeMessage.reason());
         } else {
@@ -182,6 +193,7 @@ public class JdkHttpClient implements HttpClient {
         }
 
         synchronized (underlyingSocket) {
+          long start = System.currentTimeMillis();
           CompletableFuture<java.net.http.WebSocket> future = makeCall.get();
           try {
             future.get(readTimeout.toMillis(), TimeUnit.MILLISECONDS);
@@ -198,6 +210,8 @@ public class JdkHttpClient implements HttpClient {
             throw new WebDriverException(e.getMessage());
           } catch (java.util.concurrent.TimeoutException e) {
             throw new TimeoutException(e);
+          } finally {
+            LOG.fine(String.format("Websocket response to %s read in %sms", message, (System.currentTimeMillis() - start)));
           }
         }
         return this;
@@ -205,6 +219,7 @@ public class JdkHttpClient implements HttpClient {
 
       @Override
       public void close() {
+        LOG.fine("Closing websocket");
         underlyingSocket.sendClose(1000, "WebDriver closing socket");
       }
     };
@@ -231,6 +246,10 @@ public class JdkHttpClient implements HttpClient {
   @Override
   public HttpResponse execute(HttpRequest req) throws UncheckedIOException {
     Objects.requireNonNull(req, "Request");
+
+    LOG.fine("Executing request: " + req);
+    long start = System.currentTimeMillis();
+
     BodyHandler<byte[]> byteHandler = BodyHandlers.ofByteArray();
     try {
       return messages.createResponse(client.send(messages.createRequest(req), byteHandler));
@@ -241,7 +260,10 @@ public class JdkHttpClient implements HttpClient {
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(e);
+    } finally {
+      LOG.fine(String.format("Ending request %s in %sms", req, (System.currentTimeMillis() - start)));
     }
+
   }
 
   @AutoService(HttpClient.Factory.class)
