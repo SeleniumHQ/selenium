65015202ca774b8980b5b2ea09cd3e44b6b9ffe2
Qnner <qnner@qq.com>
2022-10-17 02:09:22 +0800

    fix lint issue

--- py/test/selenium/webdriver/support/expected_conditions_tests.py
+++ py/test/selenium/webdriver/support/expected_conditions_tests.py
@@ -89,15 +89,19 @@ def test_clickable_element_false(driver, pages):
 
 
 def test_text_to_be_present_in_element_attribute(driver, pages):
-    pages.loas('inputs.html')
+    pages.load('inputs.html')
     locator_test = (By.XPATH, "/html/body/form/input[1]")  # parameter is locator
     web_element_test = driver.find_element(*locator_test)  # parameter is WebElement
-    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_attribute(locator_test, "value", "input with no type"))
-    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_attribute(web_element_test, "value", "input with no type"))
+    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_attribute(locator_test,
+                                                                                "value",
+                                                                                "input with no type"))
+    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_attribute(web_element_test,
+                                                                                "value",
+                                                                                "input with no type"))
 
 
 def test_text_to_be_present_in_element(driver, pages):
-    pages.loas('simpleTest.html')
+    pages.load('simpleTest.html')
     locator_test = (By.XPATH, "/html/body/h1")  # parameter is locator
     web_element_test = driver.find_element(*locator_test)  # parameter is WebElement
     WebDriverWait(driver, 1).until(EC.text_to_be_present_in_element(locator_test, 'Heading'))
@@ -105,15 +109,17 @@ def test_text_to_be_present_in_element(driver, pages):
 
 
 def test_text_to_be_present_in_element_value(driver, pages):
-    pages.loas('inputs.html')
+    pages.load('inputs.html')
     locator_test = (By.XPATH, "/html/body/form/input[1]")  # parameter is locator
     web_element_test = driver.find_element(*locator_test)  # parameter is WebElement
-    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_value(locator_test, "input with no type"))
-    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_value(web_element_test, "input with no type"))
+    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_value(locator_test,
+                                                                            "input with no type"))
+    WebDriverWait(driver, 0.1).until(EC.text_to_be_present_in_element_value(web_element_test,
+                                                                            "input with no type"))
 
 
 def test_element_located_to_be_selected(driver, pages):
-    pages.loas('inputs.html')
+    pages.load('inputs.html')
     locator_test = (By.XPATH, "/html/body/form/input[10]")  # parameter is locator
     web_element_test = driver.find_element(*locator_test)  # parameter is WebElement
     WebDriverWait(driver, 0.1).until(EC.element_located_to_be_selected(locator_test))
@@ -121,7 +127,7 @@ def test_element_located_to_be_selected(driver, pages):
 
 
 def test_element_located_selection_state_to_be(driver, pages):
-    pages.loas('inputs.html')
+    pages.load('inputs.html')
 
     selected_locator_test = (By.XPATH, "/html/body/form/input[10]")  # parameter is locator
     selected_web_element_test = driver.find_element(*selected_locator_test)  # parameter is WebElement
@@ -136,7 +142,7 @@ def test_element_located_selection_state_to_be(driver, pages):
 
 
 def test_element_attribute_to_include(driver, pages):
-    pages.loads("inputs.html")
+    pages.load("inputs.html")
     locator_test = (By.XPATH, "/html/body/form/input[13]")
     web_element_test = driver.find_element(*locator_test)
     WebDriverWait(driver, 0.1).until(EC.element_attribute_to_include(locator_test, "name"))
