2589a32331f575a4d096de94a805cbb02547d992
Alex Rodionov <p0deje@gmail.com>
2022-11-26 14:19:33 -0800

    Fix RuboCop offenses in NightlyVersionGenerator

--- rb/lib/selenium/webdriver/support/nightly_version_generator.rb
+++ rb/lib/selenium/webdriver/support/nightly_version_generator.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
+# Licensed to the Software Freedom Conservancy (SFC) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The SFC licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
@@ -22,7 +33,7 @@ module Selenium
 
       class NightlyVersionGenerator
 
-        REGEXP = /VERSION = ['"]([\d\.]+)['"]/
+        REGEXP = /VERSION = ['"]([\d.]+)['"]/.freeze
 
         def self.call(version_file, version_suffix)
           version_suffix ||= Date.today.strftime('%Y%m%d')
