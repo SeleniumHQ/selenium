0818e99e321c9b86b9c9c09c08e57392bdb3c229
Alex Rodionov <p0deje@gmail.com>
2022-12-01 15:43:18 -0800

    Fix Ruby API documentation generator

--- generate_api_docs.sh
+++ generate_api_docs.sh
@@ -10,11 +10,7 @@ py)
   tox -c py/tox.ini -e docs || exit
   ;;
 rb)
-  cd rb || exit
-  bundle install || exit
-  cd ..
   bazel run //rb:docs || exit
-  git checkout rb/Gemfile.lock || true
   ;;
 *)
   echo "Selenium API docs generation"
@@ -45,7 +41,7 @@ py)
   ;;
 rb)
   rm -rf docs/api/rb
-  mv bazel-bin/rb/docs.runfiles/selenium/docs/api/rb docs/api/rb
+  mv "$(bazel cquery --output=files //rb:docs 2> /dev/null).runfiles/selenium/docs/api/rb" docs/api/rb
   ;;
 *)
   echo "ERROR: unknown parameter \"$API_DOCS_LANGUAGE\""
