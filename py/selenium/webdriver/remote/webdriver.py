3ce1b45baf56ae3a192374a34262efb0ad08d386
titusfortner <titusfortner@gmail.com>
2023-01-02 16:13:37 -0600

    [py] add name of what is being executed to large JS executions

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -842,7 +842,7 @@ class WebDriver(BaseWebDriver):
         if isinstance(by, RelativeBy):
             _pkg = ".".join(__name__.split(".")[:-1])
             raw_function = pkgutil.get_data(_pkg, "findElements.js").decode("utf8")
-            find_element_js = f"return ({raw_function}).apply(null, arguments);"
+            find_element_js = f"/* findElements */return ({raw_function}).apply(null, arguments);"
             return self.execute_script(find_element_js, by.to_dict())
 
         if by == By.ID:
