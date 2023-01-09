36c788c46532f99c1d671ab524d97178a7195b2f
Alex Rodionov <p0deje@gmail.com>
2023-01-09 15:37:04 -0800

    Overhaul CI in GitHub Actions [dotnet][java][js][py][rb][rust]
    This commit introduces few major changes to the Selenium CI, overhauling
    the current implementation to make it a bit more usable and maintainable.
    
    1. Introduces high-level orchestrating "CI" workflow that is run on
       every push to the main branch, every 12 hours, every pull request,
       and manually. This workflow checks which bindings are affected by the
       pushed changes and calls individual CI workflows, presenting all of
       the results on a single page -
       https://github.com/SeleniumHQ/selenium/actions/workflows/ci.yml.
    2. Reworks every "CI - <Bindings>" workflow to be callable from other
       workflows or run manually. The former is important for the "CI" workflow,
       the latter is matter of convenience so that individual bindings CI
       workflow can be run.
    3. Individual bindings CI workflow are also implemented in a similar
       manner with the following pipeline:
         Build -> Documentation / Lint / Unit Tests -> Integration Tests
       This allows to better balance the load across the CI runners queue,
       preventing an immediate burst of queued runs.
    4. Almost every CI pipeline step is implemented via a callable
       bazel.yml workflow. It is responsible for setting up the whole
       environment (browsers, Bazel caches) and greatly simplifies the
       individual bindings workflow. The only exceptions not using it are
       few JavaScript/Python tasks not migrated to Bazel and Rust pipeline.
    6. Removes unnecessary installation of JDK because it's already taken
       care of by Bazel.
    7. Allows to start interacting SSH session inside CI upon failure. This
       can be done by re-running a failed build with "Enable debug logging"
       on.
    8. Allows to force-run CI on pull requests if the title includes
       [<bindings>] text. We could also fetch commits on this PR, but it
       needs extra code and can be done later.

--- scripts/github-actions/check-bazel-targets.sh
+++ scripts/github-actions/check-bazel-targets.sh
@@ -4,46 +4,32 @@
 # Extracted from https://github.com/bazelbuild/bazel/blob/master/scripts/ci/ci.sh
 
 COMMIT_RANGE=${COMMIT_RANGE:-"HEAD^..HEAD"}
-BAZEL_TARGET_PREFIX=${BAZEL_TARGET_PREFIX:-""}
 
-echo ${COMMIT_RANGE}
+echo "${COMMIT_RANGE}"
 
 # Go to the root of the repo
 cd "$(git rev-parse --show-toplevel)" || true
 
 # Get list of affected files by the diff
-affected_files=$(git diff --name-only ${COMMIT_RANGE})
+affected_files=$(git diff --name-only "${COMMIT_RANGE}")
 
 # Get a list of the current targets in package form by querying Bazel.
 bazel_targets=()
 for bazel_target in $affected_files ; do
   bazel_targets+=($(bazel query "$bazel_target"))
-  bazel query "$bazel_target"
 done
-
-echo "bazel-targets=${bazel_targets[*]}" >> $GITHUB_OUTPUT
-echo "run-workflow=false" >> $GITHUB_OUTPUT
 
 if (( ${#bazel_targets[@]} == 0 )); then
   echo "No bazel targets found after checking the diff."
   exit 0
 fi
 
-if [[ " ${bazel_targets[*]} " == *"$BAZEL_TARGET_PREFIX"* ]]; then
-  echo "run-workflow=true" >> $GITHUB_OUTPUT
-  echo "Bazel targets found: ${bazel_targets[*]}"
-  exit 0
-fi
-
 # Now check if we need to run some tests based on this change
 # E.g. A change in Grid needs to trigger remote tests in other bindings
-tests=$(bazel query \
+echo "Checking test targets..."
+bazel_targets+=($(bazel query \
     --keep_going \
     --noshow_progress \
-    "kind(test, rdeps(//..., set(${bazel_targets[*]})))")
+    "kind(test, rdeps(//..., set(${bazel_targets[*]})))"))
 
-if [[ " ${tests[*]} " == *"$BAZEL_TARGET_PREFIX"* ]]; then
-  echo "run-workflow=true" >> $GITHUB_OUTPUT
-  echo "Test targets found: ${tests[*]}"
-  exit 0
-fi
+echo "bazel-targets='${bazel_targets[*]}'" | tee -a "$GITHUB_OUTPUT"
