4c8cd417f9fd7313e29b083097e16c60cd5e140e
RussiaVk <43515485+RussiaVk@users.noreply.github.com>
2023-01-17 22:28:50 +0800

    Update WebElementToJsonConverter.java

--- java/src/org/openqa/selenium/remote/internal/WebElementToJsonConverter.java
+++ java/src/org/openqa/selenium/remote/internal/WebElementToJsonConverter.java
@@ -22,6 +22,7 @@ import org.openqa.selenium.WrapsElement;
 import org.openqa.selenium.remote.Dialect;
 import org.openqa.selenium.remote.RemoteWebElement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
