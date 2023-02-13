1df49a4bbfbf6fa4151e3592cc5b98d2ebb53e56
Puja Jagani <puja.jagani93@gmail.com>
2023-02-13 12:07:16 +0530

    [java][bidi] Fix error message

--- java/src/org/openqa/selenium/firefox/FirefoxDriver.java
+++ java/src/org/openqa/selenium/firefox/FirefoxDriver.java
@@ -291,7 +291,7 @@ public class FirefoxDriver extends RemoteWebDriver
     }
 
     URI wsUri = biDiUri.orElseThrow(
-      () -> new BiDiException("This version of Firefox or geckodriver does not support CDP"));
+      () -> new BiDiException("This version of Firefox or geckodriver does not support BiDi"));
 
     HttpClient.Factory clientFactory = HttpClient.Factory.createDefault();
     ClientConfig wsConfig = ClientConfig.defaultConfig().baseUri(wsUri);
