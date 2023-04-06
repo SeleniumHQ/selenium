40d41614290212a194a10ed1535bc26320ad6666
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-04-07 01:22:57 +0300

    Copy manager binaries to tests output

--- dotnet/src/webdriver/SeleniumManager.cs
+++ dotnet/src/webdriver/SeleniumManager.cs
@@ -21,6 +21,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Newtonsoft.Json;
 using OpenQA.Selenium.Internal;
+using System.Reflection;
 
 #if !NET45 && !NET46 && !NET47
 using System.Runtime.InteropServices;
@@ -36,6 +37,7 @@ namespace OpenQA.Selenium
     /// </summary>
     public static class SeleniumManager
     {
+        private static string basePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         private static string binary;
         private static readonly List<string> KnownDrivers = new List<string>() {
             "geckodriver",
@@ -111,7 +113,7 @@ namespace OpenQA.Selenium
         private static string RunCommand(string fileName, string arguments)
         {
             Process process = new Process();
-            process.StartInfo.FileName = fileName;
+            process.StartInfo.FileName = $"{basePath}/{fileName}";
             process.StartInfo.Arguments = arguments;
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
