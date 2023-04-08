ff8e78912f77bedef5d7f0b068855c9e832cb3b4
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-04-08 12:51:00 +0300

    Revert "Trigger tests again"
    This reverts commit 6dde845cefa1a54027a4109f2ee84b24054d9d3b.

--- dotnet/src/webdriver/By.cs
+++ dotnet/src/webdriver/By.cs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-  
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
