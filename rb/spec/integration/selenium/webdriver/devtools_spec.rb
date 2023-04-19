e73a62bb08d7c930a67e2a9cb03672fcdd6e1aa3
Alex Rodionov <p0deje@gmail.com>
2023-04-19 16:45:40 -0700

    [rb] Maintain methods to classes map for DevTools
    Fixes #11912

--- rb/spec/integration/selenium/webdriver/devtools_spec.rb
+++ rb/spec/integration/selenium/webdriver/devtools_spec.rb
@@ -29,6 +29,12 @@ module Selenium
         expect(driver.title).to eq('XHTML Test Page')
       end
 
+      it 'maps methods to classes' do
+        expect(driver.devtools.css).not_to be_nil
+        expect(driver.devtools.dom).not_to be_nil
+        expect(driver.devtools.dom_debugger).not_to be_nil
+      end
+
       context 'when the devtools version is too high' do
         let(:existing_devtools_version) { driver.send(:devtools_version) }
         let(:imaginary_devtools_version) { existing_devtools_version + 1 }
