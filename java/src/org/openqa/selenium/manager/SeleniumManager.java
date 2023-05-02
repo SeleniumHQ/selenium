872145afedb14cd7c577e07e36779aa08edcf709
Diego Molina <diemol@gmail.com>
2023-05-02 16:21:42 +0000

    Addressing PR comments

--- java/src/org/openqa/selenium/manager/SeleniumManager.java
+++ java/src/org/openqa/selenium/manager/SeleniumManager.java
@@ -55,7 +55,7 @@ import static org.openqa.selenium.Platform.WINDOWS;
 @Beta
 public class SeleniumManager {
 
-    private static final Logger log = Logger.getLogger(SeleniumManager.class.getName());
+    private static final Logger LOG = Logger.getLogger(SeleniumManager.class.getName());
 
     private static final String SELENIUM_MANAGER = "selenium-manager";
     private static final String EXE = ".exe";
@@ -75,7 +75,7 @@ public class SeleniumManager {
                 try {
                     Files.delete(binary.toPath());
                 } catch (IOException e) {
-                    log.warning(String.format("%s deleting temporal file: %s",
+                    LOG.warning(String.format("%s deleting temporal file: %s",
                             e.getClass().getSimpleName(), e.getMessage()));
                 }
             }
@@ -96,7 +96,7 @@ public class SeleniumManager {
      * @return the standard output of the execution.
      */
     private static String runCommand(String... command) {
-      log.fine(String.format("Executing Process: %s", command));
+      LOG.fine(String.format("Executing Process: %s", command));
         String output = "";
         int code = 0;
         try {
@@ -107,11 +107,11 @@ public class SeleniumManager {
             output = CharStreams.toString(new InputStreamReader(
               process.getInputStream(), StandardCharsets.UTF_8));
         } catch (InterruptedException e) {
-            log.warning(String.format("Interrupted exception running command %s: %s",
+            LOG.warning(String.format("Interrupted exception running command %s: %s",
                                       Arrays.toString(command), e.getMessage()));
             Thread.currentThread().interrupt();
         } catch (Exception e) {
-            log.warning(String.format("%s running command %s: %s",
+            LOG.warning(String.format("%s running command %s: %s",
                                       e.getClass().getSimpleName(), Arrays.toString(command),
                                       e.getMessage()));
         }
@@ -125,10 +125,10 @@ public class SeleniumManager {
         jsonOutput.logs.stream()
           .forEach(logged -> {
             if(logged.level.equalsIgnoreCase(WARN)) {
-              log.warning(logged.message);
+              LOG.warning(logged.message);
             }
             if(logged.level.equalsIgnoreCase(DEBUG)) {
-              log.fine(logged.message);
+              LOG.fine(logged.message);
             }
           });
         return jsonOutput.result.message;
@@ -182,7 +182,7 @@ public class SeleniumManager {
                     Map<String, Object> vendorOptions = (Map<String, Object>) options.getCapability(vendorOptionsCapability);
                     return (String) vendorOptions.get("binary");
                 } catch (Exception e) {
-                    log.warning(String.format("Exception while retrieving the browser binary path. %s: %s",
+                    LOG.warning(String.format("Exception while retrieving the browser binary path. %s: %s",
                                               options, e.getMessage()));
                 }
             }
@@ -196,7 +196,7 @@ public class SeleniumManager {
      * @return the location of the driver.
      */
     public String getDriverPath(Capabilities options) {
-      log.info("applicable driver not found; attempting to install with Selenium Manager (Beta)");
+      LOG.info("applicable driver not found; attempting to install with Selenium Manager (Beta)");
       File binaryFile = getBinary();
         if (binaryFile == null) {
             return null;
@@ -206,7 +206,7 @@ public class SeleniumManager {
         commandList.add("--browser");
         commandList.add(options.getBrowserName());
         commandList.add("--output");
-        commandList.add("--json");
+        commandList.add("json");
         if (!options.getBrowserVersion().isEmpty()) {
           commandList.add("--browser-version");
           commandList.add(options.getBrowserVersion());
@@ -218,12 +218,12 @@ public class SeleniumManager {
           commandList.add(browserBinary);
         }
 
-        if (log.getLevel().intValue() > Level.FINE.intValue()) {
+        if (LOG.getLevel().intValue() > Level.FINE.intValue()) {
           commandList.add("--verbose");
         }
 
       String path = runCommand(commandList.toArray(new String[0]));
-      log.fine(String.format("Using driver at location: %s", path));
+      LOG.fine(String.format("Using driver at location: %s", path));
       return path;
     }
 }
