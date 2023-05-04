dfd1e528644954fd2699c701c058bc3250631300
Diego Molina <diemol@gmail.com>
2023-05-04 19:56:53 +0200

    Simplifying DriverFinder code

--- dotnet/src/webdriver/DriverFinder.cs
+++ dotnet/src/webdriver/DriverFinder.cs
@@ -33,30 +33,23 @@ namespace OpenQA.Selenium
         /// <param name="service">DriverService with the current path.</param>
         /// <param name="options">DriverOptions with the current browser options.</param>
         /// <returns>
-        /// The path of the driver.
+        /// The service with a verified driver executable path.
         /// </returns>
-        public static string GetPath(DriverService service, DriverOptions options)
+        public static DriverService VerifyDriverServicePath(DriverService service, DriverOptions options)
         {
             string executablePath = Path.Combine(service.DriverServicePath, service.DriverServiceExecutableName);
-            if (File.Exists(executablePath)) return executablePath;
+            if (File.Exists(executablePath)) return service;
             try
             {
-                return SeleniumManager.DriverPath(options);
+                string driverFullPath = SeleniumManager.DriverPath(options);
+                service.DriverServicePath = Path.GetDirectoryName(driverFullPath);
+                service.DriverServiceExecutableName = Path.GetFileName(driverFullPath);
+                return service;
             }
             catch (Exception e)
             {
                 throw new WebDriverException($"Unable to locate driver with path: {executablePath}, for more information on how to install drivers see https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/", e);
             }
         }
-
-        public static DriverService VerifyDriverServicePath(DriverService service, DriverOptions options)
-        {
-            string driverFullPath = GetPath(service, options);
-            service.DriverServicePath = Path.GetDirectoryName(driverFullPath);
-            service.DriverServiceExecutableName = Path.GetFileName(driverFullPath);
-
-            return service;
-        }
-
     }
 }
