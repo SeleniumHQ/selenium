018293f0c258046bdf8388bb82a9a21b2c09c4f3
Diego Molina <diemol@users.noreply.github.com>
2023-05-31 19:01:27 +0200

    [py] Removing browser filter when checking shadow root. (#12122)
    * [rb] Removing browser filter when checking shadow root.
    
    Only Python had it, fixes #11893
    
    During the meeting on [25.05.2023](https://www.selenium.dev/meetings/2023/tlc-05-25/)
    it was decided add support for everything in spec and let
    the drivers error when things do not work.
    
    * [rb] Addressing PR comments

--- py/selenium/webdriver/remote/webelement.py
+++ py/selenium/webdriver/remote/webelement.py
@@ -235,20 +235,12 @@ class WebElement(BaseWebElement):
     @property
     def shadow_root(self) -> ShadowRoot:
         """Returns a shadow root of the element if there is one or an error.
-        Only works from Chromium 96 and Firefox 96 onwards. Previous versions
-        of browsers will throw an assertion exception.
+        Only works from Chromium 96, Firefox 96, and Safari 16.4 onwards.
 
         :Returns:
           - ShadowRoot object or
           - NoSuchShadowRoot - if no shadow root was attached to element
         """
-        browser_main_version = int(self._parent.caps["browserVersion"].split(".")[0])
-        assert (
-            self._parent.caps["browserName"].lower() != "safari"
-        ), "This only currently works in Firefox and Chromium based browsers"
-        assert (
-            browser_main_version > 95
-        ), f"Please use Firefox or Chromium based browsers with version 96 or later. Version used {self._parent.caps['browserVersion']}"
         return self._execute(Command.GET_SHADOW_ROOT)["value"]
 
     # RenderedWebElement Items
