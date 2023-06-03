6ca6a893775f71793599de8c5a6d81d49ee8e1e3
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-06-03 20:24:13 +0530

    removed duplicate import

--- py/selenium/webdriver/support/select.py
+++ py/selenium/webdriver/support/select.py
@@ -20,7 +20,6 @@ from typing import List
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import UnexpectedTagNameException
 from selenium.webdriver.common.by import By
-from typing import List
 from selenium.webdriver.remote.webelement import WebElement
 
 
