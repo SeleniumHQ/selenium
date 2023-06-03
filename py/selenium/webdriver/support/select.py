d045d1321736bae556d246dab09b29e223380d84
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-06-03 18:18:27 +0530

    resolved merge conflict

--- py/selenium/webdriver/support/select.py
+++ py/selenium/webdriver/support/select.py
@@ -20,6 +20,7 @@ from typing import List
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import UnexpectedTagNameException
 from selenium.webdriver.common.by import By
+from typing import List
 from selenium.webdriver.remote.webelement import WebElement
 
 
