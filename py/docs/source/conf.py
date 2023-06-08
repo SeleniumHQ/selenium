8ace4ecca470c02ff30206714174dcf1079d75e4
Diego Molina <diemol@gmail.com>
2023-06-08 11:27:47 +0200

    [py] Adding draft configuration for ReadTheDocs

--- py/docs/source/conf.py
+++ py/docs/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Selenium'
-copyright = '2011, plightbo, simon.m.stewart, hbchai, jrhuggins, et al.'
+copyright = '2009-2023 Software Freedom Conservancy'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -228,9 +228,9 @@ man_pages = [
 
 # Bibliographic Dublin Core info.
 epub_title = 'Selenium'
-epub_author = 'plightbo, simon.m.stewart, hbchai, jrhuggins, et al.'
-epub_publisher = 'plightbo, simon.m.stewart, hbchai, jrhuggins, et al.'
-epub_copyright = '2011, plightbo, simon.m.stewart, hbchai, jrhuggins, et al.'
+epub_author = 'The Selenium Project'
+epub_publisher = 'The Selenium Project'
+epub_copyright = '2009-2023 Software Freedom Conservancy'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
