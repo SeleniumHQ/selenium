383bc41018b6ae74c0e915946b2ef3c844a7de9d
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-06-10 08:48:18 +0530

    [py] mapped each errorcode to corresponding exception

--- py/selenium/webdriver/remote/errorhandler.py
+++ py/selenium/webdriver/remote/errorhandler.py
@@ -50,6 +50,43 @@ from selenium.common.exceptions import UnknownMethodException
 from selenium.common.exceptions import WebDriverException
 
 
+class ExceptionMapping:
+    """maps each errorcode in ErrorCode object to corresponding exception."""
+
+    NO_SUCH_ELEMENT = NoSuchElementException
+    NO_SUCH_FRAME = NoSuchFrameException
+    NO_SUCH_SHADOW_ROOT = NoSuchShadowRootException
+    STALE_ELEMENT_REFERENCE = StaleElementReferenceException
+    ELEMENT_NOT_VISIBLE = ElementNotVisibleException
+    INVALID_ELEMENT_STATE = InvalidElementStateException
+    UNKNOWN_ERROR = WebDriverException
+    ELEMENT_IS_NOT_SELECTABLE = ElementNotSelectableException
+    JAVASCRIPT_ERROR = JavascriptException
+    TIMEOUT = TimeoutException
+    NO_SUCH_WINDOW = NoSuchWindowException
+    INVALID_COOKIE_DOMAIN = InvalidCookieDomainException
+    UNABLE_TO_SET_COOKIE = UnableToSetCookieException
+    UNEXPECTED_ALERT_OPEN = UnexpectedAlertPresentException
+    NO_ALERT_OPEN = NoAlertPresentException
+    SCRIPT_TIMEOUT = TimeoutException
+    IME_NOT_AVAILABLE = ImeNotAvailableException
+    IME_ENGINE_ACTIVATION_FAILED = ImeActivationFailedException
+    INVALID_SELECTOR = InvalidSelectorException
+    SESSION_NOT_CREATED = SessionNotCreatedException
+    MOVE_TARGET_OUT_OF_BOUNDS = MoveTargetOutOfBoundsException
+    INVALID_XPATH_SELECTOR = InvalidSelectorException
+    INVALID_XPATH_SELECTOR_RETURN_TYPER = InvalidSelectorException
+    ELEMENT_NOT_INTERACTABLE = ElementNotInteractableException
+    INSECURE_CERTIFICATE = InsecureCertificateException
+    INVALID_ARGUMENT = InvalidArgumentException
+    INVALID_COORDINATES = InvalidCoordinatesException
+    INVALID_SESSION_ID = InvalidSessionIdException
+    NO_SUCH_COOKIE = NoSuchCookieException
+    UNABLE_TO_CAPTURE_SCREEN = ScreenshotException
+    ELEMENT_CLICK_INTERCEPTED = ElementClickInterceptedException
+    UNKNOWN_METHOD = UnknownMethodException
+
+
 class ErrorCode:
     """Error codes defined in the WebDriver wire protocol."""
 
