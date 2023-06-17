4e7f19a3df932b96d747a70f52adcaddf18c37a8
Mathaus <mathaus273@gmail.com>
2023-06-17 19:48:39 -0300

    [Java] remove not needed imports

--- java/src/org/openqa/selenium/remote/RemoteWebDriver.java
+++ java/src/org/openqa/selenium/remote/RemoteWebDriver.java
@@ -97,8 +97,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
