fe1df0f8a6cc223f48f70121f00631526f0ec85a
Simon Mavi Stewart <simon.m.stewart@gmail.com>
2023-06-19 15:32:57 +0100

    [ci] Use the correct flag to set the ruby version

--- scripts/github-actions/ci-build.sh
+++ scripts/github-actions/ci-build.sh
@@ -8,9 +8,6 @@ set -x
 ./scripts/format.sh
 git diff --exit-code
 
-# We want to use a pre-built Ruby version
-echo 'RUBY_VERSION = "jruby-9.4.2.0"' >rb/ruby_version.bzl
-
 # The NPM repository rule wants to write to the HOME directory
 # but that's configured for the remote build machines, so run
 # that repository rule first so that the subsequent remote
