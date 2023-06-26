d09a4283946b0665ed655b84d7572fce138e1ee4
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-06-26 11:59:14 +0530

    [py] moved ConnectionType class outside Mobile class to avoid nesting in remote/mobile.py

--- py/selenium/webdriver/remote/mobile.py
+++ py/selenium/webdriver/remote/mobile.py
@@ -18,23 +18,25 @@
 from .command import Command
 
 
+class _ConnectionType:
+    def __init__(self, mask):
+        self.mask = mask
+
+    @property
+    def airplane_mode(self):
+        return self.mask % 2 == 1
+
+    @property
+    def wifi(self):
+        return (self.mask / 2) % 2 == 1
+
+    @property
+    def data(self):
+        return (self.mask / 4) > 0
+
+
 class Mobile:
-    class ConnectionType:
-        def __init__(self, mask):
-            self.mask = mask
-
-        @property
-        def airplane_mode(self):
-            return self.mask % 2 == 1
-
-        @property
-        def wifi(self):
-            return (self.mask / 2) % 2 == 1
-
-        @property
-        def data(self):
-            return (self.mask / 4) > 0
-
+    ConnectionType = _ConnectionType
     ALL_NETWORK = ConnectionType(6)
     WIFI_NETWORK = ConnectionType(2)
     DATA_NETWORK = ConnectionType(4)
