714e6457c73587a0649742009abb0c45df86b220
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-07-05 10:13:50 +0530

    [py] initialised functions with optional WebElement with None

--- py/selenium/webdriver/common/action_chains.py
+++ py/selenium/webdriver/common/action_chains.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""The ActionChains implementation,"""
+"""The ActionChains implementation."""
 from __future__ import annotations
 
 import warnings
