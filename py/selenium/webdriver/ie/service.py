6e2b5d79711a76a1cfaff650028ba2706184f1a4
sandeepsuryaprasad <gs.sandeep@yahoo.com>
2023-07-09 09:41:50 +0530

    [py] updated documentation links

--- py/selenium/webdriver/ie/service.py
+++ py/selenium/webdriver/ie/service.py
@@ -62,7 +62,7 @@ class Service(service.Service):
             executable_path,
             port=port,
             log_output=log_output,
-            start_error_message="Please download the binary from https://www.selenium.dev/downloads/ and https://www.selenium.dev/documentation/ie_driver_server/",
+            start_error_message="Please download the binary from https://www.selenium.dev/downloads/ and read up at https://www.selenium.dev/documentation/ie_driver_server/",
             **kwargs,
         )
 
