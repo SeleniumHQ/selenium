7de1fba3ad1740bb194a716903ab04d6e42ca8ce
Steve <steve@tracvc.com>
2023-08-14 15:06:50 -0600

    fix for tox/linting error

--- py/selenium/webdriver/common/selenium_manager.py
+++ py/selenium/webdriver/common/selenium_manager.py
@@ -54,9 +54,9 @@ class SeleniumManager:
 
         path = Path(__file__).parent.joinpath(directory, file)
 
-        if not path.is_file() and os.environ['CONDA_PREFIX']:
+        if not path.is_file() and os.environ["CONDA_PREFIX"]:
             # conda has a separate package selenium-manager, installs in bin
-            path = Path(os.path.join(os.environ['CONDA_PREFIX'], 'bin', file))
+            path = Path(os.path.join(os.environ["CONDA_PREFIX"], "bin", file))
             logger.debug(f"Conda environment detected, using `{path}`")
         if not path.is_file():
             raise WebDriverException(f"Unable to obtain working Selenium Manager binary; {path}")
