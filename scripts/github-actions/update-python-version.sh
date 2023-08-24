37e1d4b7b48d0cf800cfc2301c0ee38aa65440a4
Oscar Devora <oscar_devora@yahoo.com>
2023-08-24 01:58:12 -0500

    modify upgrade script to fit nightly release

--- scripts/github-actions/update-python-version.sh
+++ scripts/github-actions/update-python-version.sh
@@ -14,29 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-RELEASE_VERSION=$RELEASE_VERSION
+# example - selenium-4.11.1-javascript
+RELEASE_TAG=$1
 
-if [[ -z "$RELEASE_VERSION" ]]; then
-  echo "Usage: $0 <release-version>"
+if [[ -z "$RELEASE_TAG" ]]; then
+  echo "Usage: $0 <release-tag>"
   exit 1
 fi
 
-# Regular expression pattern for #.#.# format
-version_pattern="^[0-9]+\.[0-9]+\.[0-9]+$"
+# example - 4.11.1
+RELEASE_VERSION=$(echo "$RELEASE_TAG" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 
-if ! [[ "$RELEASE_VERSION" =~ $version_pattern ]]; then
-  echo "Invalid version format. Please use the format #.#.#"
-  exit 1
-fi
+# example - 4.12.0
+NEW_VERSION=$(echo "$RELEASE_VERSION" | awk -F. '{print $1"."$2+1".0"}')
+
+# example - 4.12.0.nightly.20230824
+NEW_VERSION_EXTRAS="$NEW_VERSION.nightly.$(date +%Y%m%d)"
 
 # Go to python dir
 cd "$(dirname "$0")"/../../py
 
+# would prefer to use sed -i but awk is more portable
 # Replace version in py/version.py
-awk -v new_version="$RELEASE_VERSION" '/SE_VERSION =/ && !found { sub(/SE_VERSION = "[0-9]+\.[0-9]+\.[0-9]+"/, "SE_VERSION = \"" new_version "\""); found = 1 } { print }' version.py > tmp.txt && mv tmp.txt version.py
+awk -v new_version="$NEW_VERSION_EXTRAS" '/SE_VERSION =/ && !found { sub(/SE_VERSION = "[0-9]+\.[0-9]+\.[0-9]+"/, "SE_VERSION = \"" new_version "\""); found = 1 } { print }' version.py > tmp.txt && mv tmp.txt version.py
 
 # Replace SE_VERSION in py/BUILD.bazel
-awk -v new_version="$RELEASE_VERSION" '/SE_VERSION =/ && !found { sub(/SE_VERSION = "[0-9]+\.[0-9]+\.[0-9]+"/, "SE_VERSION = \"" new_version "\""); found = 1 } { print }' BUILD.bazel > tmp.txt && mv tmp.txt BUILD.bazel
+awk -v new_version="$NEW_VERSION_EXTRAS" '/SE_VERSION =/ && !found { sub(/SE_VERSION = "[0-9]+\.[0-9]+\.[0-9]+"/, "SE_VERSION = \"" new_version "\""); found = 1 } { print }' BUILD.bazel > tmp.txt && mv tmp.txt BUILD.bazel
 
 # Replace selenium-#.#.#.tar.gz with selenium-RELEASE_VERSION.tar.gz in docs/source/index.rst
-awk -v new_version="$RELEASE_VERSION" '/selenium-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz/ && !found { sub(/selenium-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz/, "selenium-" new_version ".tar.gz"); found = 1 } { print }' docs/source/index.rst > tmp.txt && mv tmp.txt docs/source/index.rst
+awk -v new_version="$NEW_VERSION_EXTRAS" '/selenium-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz/ && !found { sub(/selenium-[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz/, "selenium-" new_version ".tar.gz"); found = 1 } { print }' docs/source/index.rst > tmp.txt && mv tmp.txt docs/source/index.rst
