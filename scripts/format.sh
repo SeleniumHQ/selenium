b9e416460c90cd337fcc803d33b373964d270cf2
Simon Mavi Stewart <simon.m.stewart@gmail.com>
2023-09-12 10:29:49 +0100

    [ci] Might as well just run rustfmt, as that's what we want to happen

--- scripts/format.sh
+++ scripts/format.sh
@@ -17,5 +17,5 @@ echo "    google-java-format" >&2
 find "$PWD/java" -type f -name '*.java' | xargs "$GOOGLE_JAVA_FORMAT" --replace
 
 section "Rust"
-echo "   rustfmt (fix with: bazel run @rules_rust//:rustfmt)" >&2
-bazel test //rust/... --test_tag_filters="rust-rustfmt" --build_tests_only
+echo "   rustfmt" >&2
+bazel run @rules_rust//:rustfmt
