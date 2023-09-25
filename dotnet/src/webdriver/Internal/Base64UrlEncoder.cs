7db931dcc03946bc431a653cd113a7280006db16
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-09-25 13:09:41 +0300

    Add standard copyright file notice

--- dotnet/src/webdriver/Internal/Base64UrlEncoder.cs
+++ dotnet/src/webdriver/Internal/Base64UrlEncoder.cs
@@ -1,5 +1,21 @@
-// Thanks to Microsoft.IdentityModel.Tokens package for this code.
-// https://raw.githubusercontent.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/6.19.0/src/Microsoft.IdentityModel.Tokens/Base64UrlEncoder.cs
+// <copyright file="Base64UrlEncoder.cs" company="WebDriver Committers">
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
 using System;
 
 namespace OpenQA.Selenium.Internal
