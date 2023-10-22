82b37c51a1f76e40ee8d2231e3bf2e85dd95ec0f
titusfortner <titus.fortner@gmail.com>
2023-10-22 18:05:49 -0500

    [java] fix bug in downloading files by ensuring browser vendor options in capabilities
    Not all bindings create an empty browser vendor options map by default

--- java/src/org/openqa/selenium/grid/node/local/LocalNode.java
+++ java/src/org/openqa/selenium/grid/node/local/LocalNode.java
@@ -551,9 +551,12 @@ public class LocalNode extends Node {
   @SuppressWarnings("unchecked")
   private Capabilities appendPrefs(
       Capabilities caps, String optionsKey, Map<String, Serializable> map) {
-    Map<String, Object> currentOptions =
-        (Map<String, Object>)
-            Optional.ofNullable(caps.getCapability(optionsKey)).orElse(new HashMap<>());
+    if (caps.getCapability(optionsKey) == null) {
+      MutableCapabilities mutableCaps = new MutableCapabilities();
+      mutableCaps.setCapability(optionsKey, new HashMap<>());
+      caps = caps.merge(mutableCaps);
+    }
+    Map<String, Object> currentOptions = (Map<String, Object>) caps.getCapability(optionsKey);
 
     ((Map<String, Serializable>) currentOptions.computeIfAbsent("prefs", k -> new HashMap<>()))
         .putAll(map);
