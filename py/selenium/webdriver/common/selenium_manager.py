SeleniumHQ/selenium

commit d4285d1f26acfd96ee77afbc5e9771b76433559b

fix for conda install of selenium-manager (#12536)
* fix for conda install of selenium-manager

conda doesn't seem to properly package selenium-manager, so it needs
to be install as a separate package (via conda). But this puts it in
the environment's bin folder.

This commit checks the path for the selenium-manager executable if
it isn't installed in the package's webdriver/common/<platform>/
folder.

fixes #11234 and #12084

* fix linting error

* use conda bin folder, rather than searching PATH

* fix for tox/linting error

py/selenium/webdriver/common/selenium_manager.py
@@ -16,6 +16,7 @@
 # under the License.
 import json
 import logging
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -53,6 +54,10 @@ class SeleniumManager:
 
         path = Path(__file__).parent.joinpath(directory, file)
 
+        if not path.is_file() and os.environ["CONDA_PREFIX"]:
+            # conda has a separate package selenium-manager, installs in bin
+            path = Path(os.path.join(os.environ["CONDA_PREFIX"], "bin", file))
+            logger.debug(f"Conda environment detected, using `{path}`")
         if not path.is_file():
             raise WebDriverException(f"Unable to obtain working Selenium Manager binary; {path}")
 

commit c158a799eae8d1838abfed08532d7fef6612564c

[py] Fix test for CONDA_PREFIX in selenium_manager
Accessing CONDA_PREFIX in os.environ fails with a KeyError when it is
not set. The condition should instead check for the existence of the
key, before accessing it in the following codepath

Fixes: d4285d1 ("fix for conda install of selenium-manager (#12536)")

py/selenium/webdriver/common/selenium_manager.py
@@ -57,7 +57,7 @@ class SeleniumManager:
 
             path = Path(__file__).parent.joinpath(directory, file)
 
-        if not path.is_file() and os.environ["CONDA_PREFIX"]:
+        if not path.is_file() and "CONDA_PREFIX" in os.environ:
             # conda has a separate package selenium-manager, installs in bin
             path = Path(os.path.join(os.environ["CONDA_PREFIX"], "bin", file))
             logger.debug("Conda environment detected, using `%s`", path)