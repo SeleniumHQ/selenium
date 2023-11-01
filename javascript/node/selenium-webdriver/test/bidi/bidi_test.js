5821614f338f132f6d54725fb97da0dd83e9bb59
Puja Jagani <puja.jagani93@gmail.com>
2023-11-01 14:38:33 +0530

    [bidi][js] Add browsing context events

--- javascript/node/selenium-webdriver/test/bidi/bidi_test.js
+++ javascript/node/selenium-webdriver/test/bidi/bidi_test.js
@@ -781,6 +781,93 @@ suite(
         assert.equal(navigationInfo.browsingContextId, browsingContext.id)
         assert(navigationInfo.url.includes('/bidi/logEntryAdded.html'))
       })
+
+      xit('can listen to navigation started event', async function () {
+        let navigationInfo = null
+        const browsingConextInspector = await BrowsingContextInspector(driver)
+
+        await browsingConextInspector.onNavigationStarted((entry) => {
+          navigationInfo = entry
+        })
+
+        const browsingContext = await BrowsingContext(driver, {
+          browsingContextId: await driver.getWindowHandle(),
+        })
+
+        await browsingContext.navigate(Pages.logEntryAdded, 'complete')
+
+        assert.equal(navigationInfo.browsingContextId, browsingContext.id)
+        assert(navigationInfo.url.includes('/bidi/logEntryAdded.html'))
+      })
+
+      it('can listen to fragment navigated event', async function () {
+        let navigationInfo = null
+        const browsingConextInspector = await BrowsingContextInspector(driver)
+
+        const browsingContext = await BrowsingContext(driver, {
+          browsingContextId: await driver.getWindowHandle(),
+        })
+        await browsingContext.navigate(Pages.linkedImage, 'complete')
+
+        await browsingConextInspector.onFragmentNavigated((entry) => {
+          navigationInfo = entry
+        })
+
+        await browsingContext.navigate(
+          Pages.linkedImage + '#linkToAnchorOnThisPage',
+          'complete'
+        )
+
+        assert.equal(navigationInfo.browsingContextId, browsingContext.id)
+        assert(navigationInfo.url.includes('linkToAnchorOnThisPage'))
+      })
+
+      xit('can listen to user prompt opened event', async function () {
+        let userpromptOpened = null
+        const browsingConextInspector = await BrowsingContextInspector(driver)
+
+        const browsingContext = await BrowsingContext(driver, {
+          browsingContextId: await driver.getWindowHandle(),
+        })
+
+        await driver.get(Pages.alertsPage)
+
+        await driver.findElement(By.id('alert')).click()
+
+        await driver.wait(until.alertIsPresent())
+
+        await browsingConextInspector.onUserPromptOpened((entry) => {
+          userpromptOpened = entry
+        })
+
+        assert.equal(userpromptOpened.browsingContextId, browsingContext.id)
+        assert.equal(userpromptOpened.type, 'alert')
+      })
+
+      xit('can listen to user prompt closed event', async function () {
+        let userpromptClosed = null
+        const browsingConextInspector = await BrowsingContextInspector(driver)
+
+        const browsingContext = await BrowsingContext(driver, {
+          browsingContextId: await driver.getWindowHandle(),
+        })
+
+        await driver.get(Pages.alertsPage)
+
+        await driver.findElement(By.id('prompt')).click()
+
+        await driver.wait(until.alertIsPresent())
+
+        await browsingConextInspector.onUserPromptClosed((entry) => {
+          userpromptClosed = entry
+        })
+
+        await browsingContext.handleUserPrompt(true, 'selenium')
+
+        assert.equal(userpromptClosed.browsingContextId, browsingContext.id)
+        assert.equal(userpromptClosed.accepted, true)
+        assert.equal(userpromptClosed.userText, 'selenium')
+      })
     })
 
     describe('Local Value', function () {
