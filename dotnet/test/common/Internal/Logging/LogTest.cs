9cb6a143ec5c1dcf44358a3776efcc31809617ee
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-11-28 21:41:06 +0300

    Update LogTest.cs

--- dotnet/test/common/Internal/Logging/LogTest.cs
+++ dotnet/test/common/Internal/Logging/LogTest.cs
@@ -1,28 +1,25 @@
-
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 
 namespace OpenQA.Selenium.Internal.Logging
 {
-    // mark it as non-parallelizable because of static nature of Log class
-    [NonParallelizable]
-    internal class LogTest
+    public class LogTest
     {
         private TestLogHandler testLogHandler;
+        private ILogger logger;
 
         [SetUp]
         public void SetUp()
         {
             testLogHandler = new TestLogHandler();
+            logger = Log.GetLogger<LogTest>();
         }
 
         [Test]
-        public void LoggerShouldEmitLogEvent()
+        public void LoggerShouldEmitEvent()
         {
             Log.SetMinimumLevel(LogEventLevel.Info).WithHandler(testLogHandler);
-
-            var logger = Log.GetLogger<LogTest>();
 
             logger.Info("test message");
 
@@ -33,6 +30,56 @@ namespace OpenQA.Selenium.Internal.Logging
             Assert.That(logEvent.Message, Is.EqualTo("test message"));
             Assert.That(logEvent.IssuedBy, Is.EqualTo(typeof(LogTest)));
             Assert.That(logEvent.Timestamp, Is.EqualTo(DateTimeOffset.Now).Within(100).Milliseconds);
+        }
+
+        [Test]
+        [TestCase(LogEventLevel.Trace)]
+        [TestCase(LogEventLevel.Debug)]
+        [TestCase(LogEventLevel.Info)]
+        [TestCase(LogEventLevel.Warn)]
+        [TestCase(LogEventLevel.Error)]
+        public void LoggerShouldEmitEventWithProperLevel(LogEventLevel level)
+        {
+            Log.SetMinimumLevel(level).WithHandler(testLogHandler);
+
+            switch (level)
+            {
+                case LogEventLevel.Trace:
+                    logger.Trace("test message");
+                    break;
+                case LogEventLevel.Debug:
+                    logger.Debug("test message");
+                    break;
+                case LogEventLevel.Info:
+                    logger.Info("test message");
+                    break;
+                case LogEventLevel.Warn:
+                    logger.Warn("test message");
+                    break;
+                case LogEventLevel.Error:
+                    logger.Error("test message");
+                    break;
+            }
+
+            Assert.That(testLogHandler.Events, Has.Count.EqualTo(1));
+
+            Assert.That(testLogHandler.Events[0].Level, Is.EqualTo(level));
+        }
+
+        [Test]
+        public void LoggerShouldNotEmitEventWhenLevelIsLess()
+        {
+            Log.SetMinimumLevel(LogEventLevel.Info).WithHandler(testLogHandler);
+
+            logger.Trace("test message");
+
+            Assert.That(testLogHandler.Events, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void A()
+        {
+            Log.CurrentContext.
         }
     }
 
