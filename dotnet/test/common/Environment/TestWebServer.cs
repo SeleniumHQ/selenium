94b566df18f474b1c1db0cfe6084b0ea703bd2b4
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-12-01 12:28:15 +0300

    Revert closing of test web server back
    We need completely different approach because it doesn't close child process

--- dotnet/test/common/Environment/TestWebServer.cs
+++ dotnet/test/common/Environment/TestWebServer.cs
@@ -150,8 +150,34 @@ namespace OpenQA.Selenium.Environment
 
         public void Stop()
         {
-            webserverProcess.Dispose();
-            webserverProcess = null;
+            HttpWebRequest request = WebRequest.Create(EnvironmentManager.Instance.UrlBuilder.LocalWhereIs("quitquitquit")) as HttpWebRequest;
+            try
+            {
+                request.GetResponse();
+            }
+            catch (WebException)
+            {
+            }
+
+            if (webserverProcess != null)
+            {
+                try
+                {
+                    webserverProcess.WaitForExit(10000);
+                    if (!webserverProcess.HasExited)
+                    {
+                        webserverProcess.Kill();
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    webserverProcess.Dispose();
+                    webserverProcess = null;
+                }
+            }
         }
     }
 }
