18bdea12d2c767b850d3543c44d030e549f8df16
titusfortner <titus.fortner@gmail.com>
2024-01-02 13:14:37 -0600

    [rb] move driver finding logic out of selenium manager class

--- rb/spec/unit/selenium/webdriver/chrome/driver_spec.rb
+++ rb/spec/unit/selenium/webdriver/chrome/driver_spec.rb
@@ -33,6 +33,7 @@ module Selenium
            body: {value: {sessionId: 0, capabilities: {browserName: 'chrome'}}}.to_json,
            headers: {content_type: 'application/json'}}
         end
+        let(:finder) { instance_double(DriverFinder, browser_path?: false, driver_path: '/path/to/driver') }
 
         def expect_request(body: nil, endpoint: nil)
           body = (body || {capabilities: {alwaysMatch: {browserName: 'chrome', 'goog:chromeOptions': {}}}}).to_json
@@ -45,42 +46,44 @@ module Selenium
         end
 
         it 'uses DriverFinder when provided Service without path' do
-          allow(DriverFinder).to receive(:path)
+          allow(DriverFinder).to receive(:new).and_return(finder)
           expect_request
           options = Options.new
 
           described_class.new(service: service, options: options)
-          expect(DriverFinder).to have_received(:path).with(options, service.class)
+          expect(finder).to have_received(:driver_path)
         end
 
         it 'does not use DriverFinder when provided Service with path' do
           expect_request
           allow(service).to receive(:executable_path).and_return('path')
-          allow(DriverFinder).to receive(:path)
+          allow(DriverFinder).to receive(:new).and_return(finder)
 
           described_class.new(service: service)
-          expect(DriverFinder).not_to have_received(:path)
+          expect(finder).not_to have_received(:driver_path)
         end
 
         it 'does not require any parameters' do
-          allow(DriverFinder).to receive(:path).and_return('path')
-          allow(Platform).to receive(:assert_file)
-          allow(Platform).to receive(:assert_executable)
-
+          allow(DriverFinder).to receive(:new).and_return(finder)
           expect_request
 
           expect { described_class.new }.not_to raise_exception
         end
 
         it 'accepts provided Options as sole parameter' do
-          allow(DriverFinder).to receive(:path).and_return('path')
-          allow(Platform).to receive(:assert_file)
-          allow(Platform).to receive(:assert_executable)
+          allow(DriverFinder).to receive(:new).and_return(finder)
 
           opts = {args: ['-f']}
           expect_request(body: {capabilities: {alwaysMatch: {browserName: 'chrome', 'goog:chromeOptions': opts}}})
 
           expect { described_class.new(options: Options.new(**opts)) }.not_to raise_exception
+        end
+
+        it 'raises an ArgumentError if parameter is not recognized' do
+          allow(DriverFinder).to receive(:new).and_return(finder)
+
+          msg = 'unknown keyword: :invalid'
+          expect { described_class.new(invalid: 'foo') }.to raise_error(ArgumentError, msg)
         end
 
         it 'does not accept Options of the wrong class' do
