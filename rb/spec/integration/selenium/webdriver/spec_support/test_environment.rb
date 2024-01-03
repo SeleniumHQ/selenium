cc85c78355bc83a18fa7eb213482ab42e1d9b8b0
titusfortner <titus.fortner@gmail.com>
2024-01-02 21:58:58 -0600

    [rb] change allowed types for logger methods

--- rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
@@ -28,7 +28,7 @@ module Selenium
           @create_driver_error_count = 0
 
           $LOAD_PATH.insert(0, root.join('bazel-bin/rb/lib').to_s) if File.exist?(root.join('bazel-bin/rb/lib'))
-          WebDriver.logger.ignore(%i[logger_info])
+          WebDriver.logger.ignore(:logger_info)
           SeleniumManager.bin_path = root.join('bazel-bin/rb/bin').to_s if File.exist?(root.join('bazel-bin/rb/bin'))
 
           @driver = ENV.fetch('WD_SPEC_DRIVER', 'chrome').tr('-', '_').to_sym
