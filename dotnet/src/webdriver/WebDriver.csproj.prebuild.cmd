68d40f5dfeaac5ed0965c6322e681452b8fad975
titusfortner <titus.fortner@gmail.com>
2024-01-18 10:14:54 -0600

    [build] remove conditionals in dotnet build scripts

--- dotnet/src/webdriver/WebDriver.csproj.prebuild.cmd
+++ dotnet/src/webdriver/WebDriver.csproj.prebuild.cmd
@@ -1,49 +1,16 @@
 @echo off
-if not exist "%1..\..\..\bazel-bin\javascript\webdriver\atoms\get-attribute.js" (
-  echo Building getAttribute atom
-  pushd "%1..\..\.."
-  bazel build //javascript/webdriver/atoms:get-attribute.js
-  popd
-)
+echo Building Selenium Manager binaries
+bazel build //dotnet/src/webdriver:manager-linux
+bazel build //dotnet/src/webdriver:manager-windows
+bazel build //dotnet/src/webdriver:manager-macos
 
-if not exist  "%1..\..\..\bazel-bin\javascript\atoms\fragments\is-displayed.js" (
-  echo Building isDisplayed atom
-  pushd "%1..\..\.."
-  bazel build //javascript/atoms/fragments:is-displayed.js
-  popd
-)
+echo Building atoms
+bazel build //javascript/webdriver/atoms:get-attribute.js
+bazel build //javascript/atoms/fragments:is-displayed.js
+bazel build //javascript/atoms/fragments:find-elements.js
 
-if not exist  "%1..\..\..\bazel-bin\javascript\atoms\fragments\find-elements.js" (
-  echo Building findElements atom
-  pushd "%1..\..\.."
-  bazel build //javascript/atoms/fragments:find-elements.js
-  popd
-)
-
-if not exist  "%1..\..\..\bazel-bin\dotnet\src\webdriver\cdp\v85\DevToolsSessionDomains.cs" (
-  echo Generating CDP code for version 85
-  pushd "%1..\..\.."
-  bazel build //dotnet/src/webdriver/cdp:generate-v85
-  popd
-)
-
-if not exist  "%1..\..\..\bazel-bin\dotnet\src\webdriver\cdp\v119\DevToolsSessionDomains.cs" (
-  echo Generating CDP code for version 119
-  pushd "%1..\..\.."
-  bazel build //dotnet/src/webdriver/cdp:generate-v119
-  popd
-)
-
-if not exist  "%1..\..\..\bazel-bin\dotnet\src\webdriver\cdp\v120\DevToolsSessionDomains.cs" (
-  echo Generating CDP code for version 120
-  pushd "%1..\..\.."
-  bazel build //dotnet/src/webdriver/cdp:generate-v120
-  popd
-)
-
-if not exist  "%1..\..\..\bazel-bin\dotnet\src\webdriver\cdp\v118\DevToolsSessionDomains.cs" (
-  echo Generating CDP code for version 118
-  pushd "%1..\..\.."
-  bazel build //dotnet/src/webdriver/cdp:generate-v118
-  popd
-)
+echo Generating CDP code
+bazel build //dotnet/src/webdriver/cdp:generate-v85
+bazel build //dotnet/src/webdriver/cdp:generate-v118
+bazel build //dotnet/src/webdriver/cdp:generate-v119
+bazel build //dotnet/src/webdriver/cdp:generate-v120
--- dotnet/src/webdriver/WebDriver.csproj.prebuild.sh
+++ dotnet/src/webdriver/WebDriver.csproj.prebuild.sh
@@ -4,44 +4,13 @@ bazel build //dotnet/src/webdriver:manager-linux
 bazel build //dotnet/src/webdriver:manager-windows
 bazel build //dotnet/src/webdriver:manager-macos
 
-if [[ ! -f "$1../../../bazel-bin/javascript/webdriver/atoms/get-attribute.js" ]]
-then
-  echo "Building getAttribute atom"
-  bazel build //javascript/webdriver/atoms:get-attribute.js
-fi
+echo "Building atoms"
+bazel build //javascript/webdriver/atoms:get-attribute.js
+bazel build //javascript/atoms/fragments:is-displayed.js
+bazel build //javascript/atoms/fragments:find-elements.js
 
-if [[ ! -f "$1../../../bazel-bin/javascript/atoms/fragments/is-displayed.js" ]]
-then
-  echo "Building isDisplayed atom"
-  bazel build //javascript/atoms/fragments:is-displayed.js
-fi
-
-if [[ ! -f "$1../../../bazel-bin/javascript/atoms/fragments/find-elements.js" ]]
-then
-  echo "Building findElements atom"
-  bazel build //javascript/atoms/fragments:find-elements.js
-fi
-
-if [[ ! -f "$1../../../bazel-bin/dotnet/src/webdriver/cdp/v85/DevToolsSessionDomains.cs" ]]
-then
-  echo "Generating CDP code for version 85"
-  bazel build //dotnet/src/webdriver/cdp:generate-v85
-fi
-
-if [[ ! -f "$1../../../bazel-bin/dotnet/src/webdriver/cdp/v119/DevToolsSessionDomains.cs" ]]
-then
-  echo "Generating CDP code for version 119"
-  bazel build //dotnet/src/webdriver/cdp:generate-v119
-fi
-
-if [[ ! -f "$1../../../bazel-bin/dotnet/src/webdriver/cdp/v120/DevToolsSessionDomains.cs" ]]
-then
-  echo "Generating CDP code for version 120"
-  bazel build //dotnet/src/webdriver/cdp:generate-v120
-fi
-
-if [[ ! -f "$1../../../bazel-bin/dotnet/src/webdriver/cdp/v118/DevToolsSessionDomains.cs" ]]
-then
-  echo "Generating CDP code for version 118"
-  bazel build //dotnet/src/webdriver/cdp:generate-v118
-fi
+echo "Generating CDP code"
+bazel build //dotnet/src/webdriver/cdp:generate-v85
+bazel build //dotnet/src/webdriver/cdp:generate-v118
+bazel build //dotnet/src/webdriver/cdp:generate-v119
+bazel build //dotnet/src/webdriver/cdp:generate-v120
