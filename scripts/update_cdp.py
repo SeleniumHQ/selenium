8957fb5759617bc1787b2733de4502f56bb8d7b5
titusfortner <titus.fortner@gmail.com>
2024-01-22 14:17:28 -0600

    [build] update which dotnet files need to be updated for new chrome versions

--- scripts/update_cdp.py
+++ scripts/update_cdp.py
@@ -147,8 +147,7 @@ def update_dotnet(chrome_milestone):
 
     files = [
         root_dir / "dotnet/selenium-dotnet-version.bzl",
-        root_dir / "dotnet/src/webdriver/WebDriver.csproj.prebuild.cmd",
-        root_dir / "dotnet/src/webdriver/WebDriver.csproj.prebuild.sh",
+        root_dir / "dotnet/src/webdriver/WebDriver.csproj",
         root_dir / "dotnet/src/webdriver/DevTools/DevToolsDomains.cs",
     ]
     for file in files:
