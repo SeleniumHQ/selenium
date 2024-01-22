d0c1cd40fda90c0db9285a67a429353c0a7516a1
Jörg Sautter <joerg.sautter@gmx.net>
2024-01-22 20:07:46 +0100

    [java] use the W3C state to detect errors

--- java/src/org/openqa/selenium/remote/ErrorHandler.java
+++ java/src/org/openqa/selenium/remote/ErrorHandler.java
@@ -17,8 +17,6 @@
 
 package org.openqa.selenium.remote;
 
-import static org.openqa.selenium.remote.ErrorCodes.SUCCESS;
-
 import java.lang.reflect.Constructor;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -82,7 +80,7 @@ public class ErrorHandler {
 
   @SuppressWarnings("unchecked")
   public Response throwIfResponseFailed(Response response, long duration) throws RuntimeException {
-    if (response.getStatus() == null || response.getStatus() == SUCCESS) {
+    if ("success".equals(response.getState())) {
       return response;
     }
 
