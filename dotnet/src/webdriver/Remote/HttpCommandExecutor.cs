eaf4e384b8c6a20c3a9b25eb2fa27926f026eb40
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2024-02-02 20:03:18 +0300

    Hm?

--- dotnet/src/webdriver/Remote/HttpCommandExecutor.cs
+++ dotnet/src/webdriver/Remote/HttpCommandExecutor.cs
@@ -311,8 +311,7 @@ namespace OpenQA.Selenium.Remote
             string body = responseInfo.Body;
             if (responseInfo.StatusCode.ToString().First() != '2')
             {
-                response.Status = WebDriverResult.UnhandledError;
-                response.Value = body;
+                response = Response.FromJson(body);
             }
             else if (responseInfo.ContentType != null && responseInfo.ContentType.StartsWith(JsonMimeType, StringComparison.OrdinalIgnoreCase))
             {
