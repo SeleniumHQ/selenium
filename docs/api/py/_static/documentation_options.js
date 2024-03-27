940404929649ffdcaa3c7223030c930671044ad5
Diego Molina <diemol@gmail.com>
2024-03-27 18:55:33 +0100

    updating all API docs

--- docs/api/py/_static/documentation_options.js
+++ docs/api/py/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.18.1',
+    VERSION: '4.20.0.dev202403271845',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
