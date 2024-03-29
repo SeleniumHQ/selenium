fc0a78fc71168138640e3030b857e8dea07ce133
Puja Jagani <puja.jagani93@gmail.com>
2024-03-29 18:31:56 +0530

    [js] Add JS doc comments

--- javascript/node/selenium-webdriver/bidi/urlPattern.js
+++ javascript/node/selenium-webdriver/bidi/urlPattern.js
@@ -22,16 +22,35 @@
 class UrlPattern {
   #map = new Map()
 
+  /**
+   * Sets the protocol for the URL pattern.
+   *
+   * @param {string} protocol - The protocol to set.
+   * @returns {UrlPattern} - Returns the updated instance of the URL pattern for chaining.
+   */
   protocol(protocol) {
     this.#map.set('protocol', protocol)
     return this
   }
 
+  /**
+   * Sets the hostname for the URL pattern.
+   *
+   * @param {string} hostname - The hostname to set.
+   * @returns {UrlPattern} - Returns the updated instance of the URL pattern for chaining.
+   */
   hostname(hostname) {
     this.#map.set('hostname', hostname)
     return this
   }
 
+  /**
+   * Sets the port for the URL pattern.
+   *
+   * @param {number} port - The port number to set.
+   * @returns {UrlPattern} - Returns the updated instance of the URL pattern for chaining.
+   * @throws {Error} - Throws an error if the port is not a number.
+   */
   port(port) {
     if (typeof port === 'number') {
       this.#map.set('port', port.toString())
@@ -41,11 +60,23 @@ class UrlPattern {
     return this
   }
 
+  /**
+   * Sets the pathname for the URL pattern.
+   *
+   * @param {string} pathname - The pathname to set.
+   * @returns {UrlPattern} - Returns the updated instance of the URL pattern for chaining.
+   */
   pathname(pathname) {
     this.#map.set('pathname', pathname)
     return this
   }
 
+  /**
+   * Sets the search parameter in the URL pattern.
+   *
+   * @param {string} search - The search parameter to be set.
+   * @returns {UrlPattern} - Returns the updated instance of the URL pattern for chaining.
+   */
   search(search) {
     this.#map.set('search', search)
     return this
