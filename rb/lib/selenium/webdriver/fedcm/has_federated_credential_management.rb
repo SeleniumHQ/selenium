0a84eac6072d40de32b09cf69e5f3b07642268b4
aguspe <agustin.pe94@gmail.com>
2024-05-09 21:23:00 +0200

    Update tests and add module

---
+++ rb/lib/selenium/webdriver/fedcm/has_federated_credential_management.rb
@@ -0,0 +1,27 @@
+module HasFederatedCredentialManagement
+  # Disables the promise rejection delay for FedCM.
+  #
+  # FedCM by default delays promise resolution in failure cases for privacy reasons.
+  # This method allows turning it off to let tests run faster where this is not relevant.
+  def set_delay_enabled(enabled)
+    # Implementation to disable the delay
+    # Placeholder for actual code to interact with browser APIs or other integrations.
+  end
+
+  # Resets the FedCM dialog cooldown.
+  #
+  # If a user agent triggers a cooldown when the account chooser is dismissed,
+  # this method resets that cooldown so that the dialog can be triggered again immediately.
+  def reset_cooldown
+    # Implementation to reset the cooldown
+    # Placeholder for actual code to interact with browser APIs or other integrations.
+  end
+
+  # Gets the currently open FedCM dialog, or nil if there is no dialog.
+  #
+  # This can be used similar to WebDriverWait in Selenium to wait until a dialog appears.
+  def get_federated_credential_management_dialog
+    # Implementation to retrieve the dialog
+    # Placeholder for actual code to check the dialog's presence.
+  end
+end
--- rb/spec/unit/selenium/webdriver/fedcm/fedcm_spec.rb
+++ rb/spec/unit/selenium/webdriver/fedcm/fedcm_spec.rb
@@ -2,19 +2,23 @@ require 'selenium-webdriver'
 require 'rspec'
 
 describe 'FederatedCredentialManagementTest' do
+  let(:options) do
+    Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument("host-resolver-rules=MAP localhost:443 localhost:#{get_secure_port}")
+      opts.add_argument('ignore-certificate-errors')
+    end
+  end
+
   let(:driver) { Selenium::WebDriver.for :chrome, options: options }
   let(:js_driver) { driver }
-  let(:fedcm_driver) { driver }
+  let(:fedcm_driver) { driver } # assuming driver includes HasFederatedCredentialManagement
 
-  before(:each) do
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('ignore-certificate-errors')
-    options.add_argument("host-resolver-rules=MAP localhost:443 localhost:#{get_secure_port}")
-    driver.get("https://#{app_server}/fedcm/fedcm.html")
+  before do
+    driver.navigate.to "https://#{app_server}/fedcm/fedcm.html"
   end
 
   def trigger_fed_cm
-    js_driver.execute_script("triggerFedCm()")
+    js_driver.execute_script('triggerFedCm()')
   end
 
   def wait_for_dialog
@@ -22,49 +26,45 @@ describe 'FederatedCredentialManagementTest' do
     wait.until { fedcm_driver.get_federated_credential_management_dialog != nil }
   end
 
-  def secure_port
+  def get_secure_port
     uri = URI("https://#{app_server}/")
     uri.port
+  rescue URI::InvalidURIError
+    0 # This should not happen
   end
 
-  describe '#test_dismiss_dialog' do
-    it 'dismisses the dialog correctly' do
-      fedcm_driver.set_delay_enabled(false)
-      expect(fedcm_driver.get_federated_credential_management_dialog).to be_nil
+  it 'dismisses dialog', skip: "https://github.com/SeleniumHQ/selenium/pull/12096#issuecomment-2017760822" do
+    fedcm_driver.set_delay_enabled(false)
+    expect(fedcm_driver.get_federated_credential_management_dialog).to be_nil
 
-      response = trigger_fed_cm
+    response = trigger_fed_cm
 
-      wait_for_dialog
+    wait_for_dialog
 
-      dialog = fedcm_driver.get_federated_credential_management_dialog
+    dialog = fedcm_driver.get_federated_credential_management_dialog
+    expect(dialog.title).to eq("Sign in to localhost with localhost")
+    expect(dialog.dialog_type).to eq("AccountChooser")
 
-      expect(dialog.title).to eq('Sign in to localhost with localhost')
-      expect(dialog.dialog_type).to eq('AccountChooser')
+    dialog.cancel_dialog
 
-      dialog.cancel_dialog
-
-      expect { js_driver.execute_script("await promise") }.to raise_error(Selenium::WebDriver::Error::JavascriptError)
-    end
+    expect { js_driver.execute_script('await promise') }.to raise_error(Selenium::WebDriver::Error::JavascriptError)
   end
 
-  describe '#test_select_account' do
-    it 'selects the account and returns a token' do
-      fedcm_driver.set_delay_enabled(false)
-      expect(fedcm_driver.get_federated_credential_management_dialog).to be_nil
+  it 'selects an account', skip: "https://github.com/SeleniumHQ/selenium/pull/12096#issuecomment-2017760822" do
+    fedcm_driver.set_delay_enabled(false)
+    expect(fedcm_driver.get_federated_credential_management_dialog).to be_nil
 
-      response = trigger_fed_cm
+    response = trigger_fed_cm
 
-      wait_for_dialog
+    wait_for_dialog
 
-      dialog = fedcm_driver.get_federated_credential_management_dialog
+    dialog = fedcm_driver.get_federated_credential_management_dialog
+    expect(dialog.title).to eq("Sign in to localhost with localhost")
+    expect(dialog.dialog_type).to eq("AccountChooser")
 
-      expect(dialog.title).to eq('Sign in to localhost with localhost')
-      expect(dialog.dialog_type).to eq('AccountChooser')
+    dialog.select_account(0)
 
-      dialog.select_account(0)
-
-      response = js_driver.execute_script("return await promise")
-      expect(response).to include('token' => 'a token')
-    end
+    response = js_driver.execute_script('return await promise')
+    expect(response).to include("token" => "a token")
   end
 end
