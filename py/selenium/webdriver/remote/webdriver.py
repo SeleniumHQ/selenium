850102fb79408de7f2d7b01fac24c720c28c7401
Alex Rodionov <p0deje@gmail.com>
2024-06-06 21:00:42 -0700

    [py] Add low-level sync API to use DevTools (#13977)

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -63,8 +63,10 @@ from .script_key import ScriptKey
 from .shadowroot import ShadowRoot
 from .switch_to import SwitchTo
 from .webelement import WebElement
+from .websocket_connection import WebSocketConnection
 
 cdp = None
+devtools = None
 
 
 def import_cdp():
@@ -207,6 +209,7 @@ class WebDriver(BaseWebDriver):
         self._authenticator_id = None
         self.start_client()
         self.start_session(capabilities)
+        self._websocket_connection = None
 
     def __repr__(self):
         return f'<{type(self).__module__}.{type(self).__name__} (session="{self.session_id}")>'
@@ -1017,6 +1020,32 @@ class WebDriver(BaseWebDriver):
                 driver.get_log('server')
         """
         return self.execute(Command.GET_LOG, {"type": log_type})["value"]
+
+    def start_devtools(self):
+        global devtools
+        if self._websocket_connection:
+            return devtools, self._websocket_connection
+        else:
+            global cdp
+            import_cdp()
+
+            if not devtools:
+                if self.caps.get("se:cdp"):
+                    ws_url = self.caps.get("se:cdp")
+                    version = self.caps.get("se:cdpVersion").split(".")[0]
+                else:
+                    version, ws_url = self._get_cdp_details()
+
+                if not ws_url:
+                    raise WebDriverException("Unable to find url to connect to from capabilities")
+
+                devtools = cdp.import_devtools(version)
+            self._websocket_connection = WebSocketConnection(ws_url)
+            targets = self._websocket_connection.execute(devtools.target.get_targets())
+            target_id = targets[0].target_id
+            session = self._websocket_connection.execute(devtools.target.attach_to_target(target_id, True))
+            self._websocket_connection.session_id = session
+            return devtools, self._websocket_connection
 
     @asynccontextmanager
     async def bidi_connection(self):
