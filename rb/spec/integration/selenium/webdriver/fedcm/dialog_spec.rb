799cbc241a90f5ac46852210365f22bbd907ed7b
aguspe <agustin.pe94@gmail.com>
2024-06-08 22:34:10 +0200

    Expand errors test

--- rb/spec/integration/selenium/webdriver/fedcm/dialog_spec.rb
+++ rb/spec/integration/selenium/webdriver/fedcm/dialog_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper'
 module Selenium
   module WebDriver
     module FedCM
-      describe Dialog, exclusive: {browser: :chrome} do
+      describe Dialog, exclusive: { browser: :chrome } do
         let(:url) { 'https://fedcm-rp-demo.glitch.me/' }
         let(:dialog) { driver.fedcm_dialog }
 
@@ -11,8 +11,30 @@ module Selenium
           driver.get(url)
         end
 
-        it 'throws an error when dialog is not present' do
-          expect { dialog.title }.to raise_error(Error::NoSuchAlertError)
+        context 'without dialog present' do
+          it 'throws an error when getting the title' do
+            expect { dialog.title }.to raise_error(Error::NoSuchAlertError)
+          end
+
+          it 'throws an error when getting the subtitle' do
+            expect { dialog.subtitle }.to raise_error(Error::NoSuchAlertError)
+          end
+
+          it 'throws an error when getting the type' do
+            expect { dialog.type }.to raise_error(Error::NoSuchAlertError)
+          end
+
+          it 'throws an error when getting the accounts' do
+            expect { dialog.accounts }.to raise_error(Error::NoSuchAlertError)
+          end
+
+          it 'throws an error when selecting an account' do
+            expect { dialog.select_account(0) }.to raise_error(Error::NoSuchAlertError)
+          end
+
+          it 'throws an error when cancelling the dialog' do
+            expect { dialog.cancel }.to raise_error(Error::NoSuchAlertError)
+          end
         end
       end
     end # FedCm
