c3cfcedf5dbc608722d01c8f3d1d99b67102c199
aguspe <agustin.pe94@gmail.com>
2024-06-09 16:03:02 +0200

    Add unit tests for dialog

---
+++ rb/spec/unit/selenium/webdriver/fedcm/account_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require File.expand_path('../spec_helper', __dir__)
+
+module Selenium
+  module WebDriver
+    module FedCM
+      describe Account do
+        let(:account) do
+          described_class.new(
+            'accountId' => '12341234',
+            'email' => 'fake@email.com',
+            'name' => 'Real Name',
+            'givenName' => 'Fake Name',
+            'pictureUrl' => 'picture-url',
+            'idpConfigUrl' => 'idp-config-url',
+            'loginState' => 'login-state',
+            'termsOfServiceUrl' => 'terms-of-service-url',
+            'privacyPolicyUrl' => 'privacy-policy-url'
+          )
+        end
+
+        it 'sets the provided attributes' do
+          expect(account.account_id).to eq('12341234')
+          expect(account.email).to eq('fake@email.com')
+          expect(account.name).to eq('Real Name')
+          expect(account.given_name).to eq('Fake Name')
+          expect(account.picture_url).to eq('picture-url')
+          expect(account.idp_config_url).to eq('idp-config-url')
+          expect(account.login_state).to eq('login-state')
+          expect(account.terms_of_service_url).to eq('terms-of-service-url')
+          expect(account.privacy_policy_url).to eq('privacy-policy-url')
+        end
+
+        it 'has a constant LOGIN_STATE_SIGNIN' do
+          expect(described_class::LOGIN_STATE_SIGNIN).to eq('SignIn')
+        end
+
+        it 'has a constant LOGIN_STATE_SIGNUP' do
+          expect(described_class::LOGIN_STATE_SIGNUP).to eq('SignUp')
+        end
+      end # Account
+    end # FedCM
+  end # WebDriver
+end # Selenium
---
+++ rb/spec/unit/selenium/webdriver/fedcm/dialog_spec.rb
@@ -0,0 +1,67 @@
+require File.expand_path('../spec_helper', __dir__)
+
+module Selenium
+  module WebDriver
+    module FedCM
+      describe Dialog do
+        let(:bridge) { instance_double(Selenium::WebDriver::Remote::Bridge) }
+        let(:dialog) { described_class.new(bridge) }
+
+        describe '#cancel' do
+          it 'calls cancel_fedcm_dialog on the bridge' do
+            allow(bridge).to receive(:cancel_fedcm_dialog).and_return(nil)
+            expect(dialog.cancel).to be_nil
+          end
+        end
+
+        describe '#select_account' do
+          it 'calls select_fedcm_account on the bridge with the given index' do
+            index = 1
+            allow(bridge).to receive(:select_fedcm_account).with(index).and_return(nil)
+            expect(dialog.select_account(index)).to be_nil
+          end
+        end
+
+        describe '#type' do
+          it 'returns the type of the open dialog' do
+            allow(bridge).to receive(:fedcm_dialog_type).and_return('AccountChooser')
+            expect(dialog.type).to eq('AccountChooser')
+          end
+        end
+
+        describe '#title' do
+          it 'returns the title of the dialog' do
+            allow(bridge).to receive(:fedcm_title).and_return('Sign in')
+            expect(dialog.title).to eq('Sign in')
+          end
+        end
+
+        describe '#subtitle' do
+          it 'returns the subtitle of the dialog' do
+            allow(bridge).to receive(:fedcm_subtitle).and_return('Choose an account')
+            expect(dialog.subtitle).to eq('Choose an account')
+          end
+
+          it 'returns nil if there is no subtitle' do
+            allow(bridge).to receive(:fedcm_subtitle).and_return(nil)
+            expect(dialog.subtitle).to be_nil
+          end
+        end
+
+        describe '#accounts' do
+          it 'returns the accounts shown in the account chooser' do
+            accounts_data = [{'name' => 'Account1', 'email' => 'account1@example.com'},
+                             {'name' => 'Account2', 'email' => 'account2@example.com'}]
+            allow(bridge).to receive(:fedcm_account_list).and_return(accounts_data)
+            accounts = dialog.accounts
+            expect(accounts.size).to eq(2)
+            expect(accounts[0].name).to eq('Account1')
+            expect(accounts[0].email).to eq('account1@example.com')
+            expect(accounts[1].name).to eq('Account2')
+            expect(accounts[1].email).to eq('account2@example.com')
+          end
+        end
+      end # Dialog
+    end # FedCM
+  end # WebDriver
+end # Selenium
