d914db359e05dc4f9d1517c1b71c75a1d58d6223
Alex <iampopovich@gmail.com>
2024-06-21 10:17:48 +0700

    DriverFinder docstring

--- py/selenium/webdriver/common/driver_finder.py
+++ py/selenium/webdriver/common/driver_finder.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""The DriverFinder implementation."""
 import logging
 from pathlib import Path
 
