3c6403558b5be9432f22e83ae84999e16bac93eb
aguspe <agustin.pe94@gmail.com>
2024-06-27 19:44:42 +0200

    Remove unnecessary attributes

--- rb/lib/selenium/webdriver/common/error.rb
+++ rb/lib/selenium/webdriver/common/error.rb
@@ -45,8 +45,6 @@ module Selenium
       }.freeze
 
       class WebDriverError < StandardError
-        attr_writer :backtrace_locations, :cause
-
         def initialize(msg = '')
           # Remove this conditional when all the error pages have been documented
           super(URLS[class_name] ? "#{msg}; #{SUPPORT_MSG} #{URLS[class_name]}" : msg)
