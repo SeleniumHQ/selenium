132fe2ad329dbd3854700eaf692fc290659910eb
Puja Jagani <puja.jagani93@gmail.com>
2024-07-10 15:58:01 +0530

    Remove semicolon

--- javascript/node/selenium-webdriver/lib/atoms/bidi-mutation-listener.js
+++ javascript/node/selenium-webdriver/lib/atoms/bidi-mutation-listener.js
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-;(channel) => {
+(channel) => {
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
       switch (mutation.type) {
