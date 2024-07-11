003f64484030d82d542721f9ba9b166ee0a3c025
Diego Molina <diemol@gmail.com>
2024-07-11 09:29:16 +0000

    Running format script

--- java/src/org/openqa/selenium/chromium/ChromiumOptions.java
+++ java/src/org/openqa/selenium/chromium/ChromiumOptions.java
@@ -31,7 +31,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Stream;
