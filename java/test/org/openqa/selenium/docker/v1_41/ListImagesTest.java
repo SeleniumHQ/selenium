f3724e661adb896a11160bb0816a42ce60df6916
Alex <iampopovich@gmail.com>
2024-07-18 14:10:09 +0700

    [java] Consistent UTF-8 Encoding and Code Enhancements (#14218)
    * made the use of UTF8 encoding consistent throughout
    
    * replaced for loop with enchanced for in DockerOptions
    
    * removed unnecessary unboxing in test noErrorNoCry
    
    * applied format.sh
    
    ---------
    
    Co-authored-by: Diego Molina <diemol@users.noreply.github.com>

--- java/test/org/openqa/selenium/docker/v1_41/ListImagesTest.java
+++ java/test/org/openqa/selenium/docker/v1_41/ListImagesTest.java
@@ -21,8 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.openqa.selenium.json.Json.MAP_TYPE;
 import static org.openqa.selenium.remote.http.Contents.utf8String;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -41,26 +41,22 @@ class ListImagesTest {
     HttpHandler handler =
         req -> {
           String filters = req.getQueryParameter("filters");
-          try {
-            String decoded = URLDecoder.decode(filters, "UTF-8");
-            Map<String, Object> raw = new Json().toType(decoded, MAP_TYPE);
+          String decoded = URLDecoder.decode(filters, StandardCharsets.UTF_8);
+          Map<String, Object> raw = new Json().toType(decoded, MAP_TYPE);
 
-            Map<?, ?> rawRef = (Map<?, ?>) raw.get("reference");
-            assertThat(rawRef.get("selenium/standalone-firefox:latest")).isEqualTo(true);
+          Map<?, ?> rawRef = (Map<?, ?>) raw.get("reference");
+          assertThat(rawRef.get("selenium/standalone-firefox:latest")).isEqualTo(true);
 
-            return new HttpResponse()
-                .addHeader("Content-Type", "application/json")
-                .setContent(
-                    utf8String(
-                        "[{\"Containers\":-1,\"Created\":1581716253,"
-                            + "\"Id\":\"sha256:bc24341497a00a3afbf04c518cb4bf98834d933ae331d1c5d3cd6f52c079049e\","
-                            + "\"Labels\":{\"authors\":\"SeleniumHQ\"},\"ParentId\":\"\","
-                            + "\"RepoDigests\":null,"
-                            + "\"RepoTags\":[\"selenium/standalone-firefox:latest\"],"
-                            + "\"SharedSize\":-1,\"Size\":765131593,\"VirtualSize\":765131593}]"));
-          } catch (UnsupportedEncodingException ignore) {
-            return null;
-          }
+          return new HttpResponse()
+              .addHeader("Content-Type", "application/json")
+              .setContent(
+                  utf8String(
+                      "[{\"Containers\":-1,\"Created\":1581716253,"
+                          + "\"Id\":\"sha256:bc24341497a00a3afbf04c518cb4bf98834d933ae331d1c5d3cd6f52c079049e\","
+                          + "\"Labels\":{\"authors\":\"SeleniumHQ\"},\"ParentId\":\"\","
+                          + "\"RepoDigests\":null,"
+                          + "\"RepoTags\":[\"selenium/standalone-firefox:latest\"],"
+                          + "\"SharedSize\":-1,\"Size\":765131593,\"VirtualSize\":765131593}]"));
         };
 
     Reference reference = Reference.parse("selenium/standalone-firefox:latest");
