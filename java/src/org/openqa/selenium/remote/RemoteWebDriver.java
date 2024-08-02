fafde8598144c20432e19f34d70a4490fda0f764
Puja Jagani <puja.jagani93@gmail.com>
2024-08-02 15:37:37 +0530

    [bidi][java] Add authentication handlers
    Related to #13993

--- java/src/org/openqa/selenium/remote/RemoteWebDriver.java
+++ java/src/org/openqa/selenium/remote/RemoteWebDriver.java
@@ -126,6 +126,8 @@ public class RemoteWebDriver
 
   private Script remoteScript;
 
+  private Network remoteNetwork;
+
   // For cglib
   protected RemoteWebDriver() {
     this.capabilities = init(new ImmutableCapabilities());
@@ -493,6 +495,13 @@ public class RemoteWebDriver
       this.remoteScript = new RemoteScript(this);
     }
     return this.remoteScript;
+  }
+
+  public Network network() {
+    if (this.remoteNetwork == null) {
+      this.remoteNetwork = new RemoteNetwork(this);
+    }
+    return this.remoteNetwork;
   }
 
   protected JsonToWebElementConverter getElementConverter() {
