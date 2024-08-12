f3444e75463db7ba220afc2fc2b613a04c7ce310
Boni Garcia <boni.garcia@uc3m.es>
2024-08-13 00:27:58 +0200

    [rust] Remove test data with incorrect browser path in macOS

--- rust/tests/browser_tests.rs
+++ rust/tests/browser_tests.rs
@@ -131,11 +131,6 @@ fn invalid_geckodriver_version_test() {
 #[case(
     "macos",
     "chrome",
-    r"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-)]
-#[case(
-    "macos",
-    "chrome",
     r"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 )]
 fn browser_path_test(#[case] os: String, #[case] browser: String, #[case] browser_path: String) {
