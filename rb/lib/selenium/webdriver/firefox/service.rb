13f03366fd5b8655169202dc0adf315b5e567407
aguspe <agustin.pe94@gmail.com>
2024-08-30 15:04:49 +0200

    Update RBS and gecko driver

--- rb/lib/selenium/webdriver/firefox/service.rb
+++ rb/lib/selenium/webdriver/firefox/service.rb
@@ -24,7 +24,7 @@ module Selenium
         DEFAULT_PORT = 4444
         EXECUTABLE = 'geckodriver'
         SHUTDOWN_SUPPORTED = false
-        PATH = ENV.fetch('SE_IEDRIVER', nil)
+        PATH = ENV.fetch('SE_GECKODRIVER', nil)
       end # Service
     end # Firefox
   end # WebDriver
