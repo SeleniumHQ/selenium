02b49377dc09ef30cee051d75355f518f73f45a1
aguspe <agustin.pe94@gmail.com>
2024-09-06 11:14:17 +0200

    Change test to capture the invalid session id error

--- rb/spec/integration/selenium/webdriver/error_spec.rb
+++ rb/spec/integration/selenium/webdriver/error_spec.rb
@@ -56,7 +56,7 @@ module Selenium
 
         driver.send(:bridge).instance_variable_set(:@session_id, 'fake_session_id')
         driver.window_handle
-      rescue WebDriver::Error::SessionNotCreatedError => e
+      rescue WebDriver::Error::InvalidSessionIdError => e
         expect(e.backtrace).not_to be_empty
       end
     end
