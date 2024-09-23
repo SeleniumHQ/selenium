873cbe4c1e7a6712c1180139f30498fe17b9c610
Diego Molina <diemol@users.noreply.github.com>
2024-09-23 23:49:23 +0200

    Api docs selenium 4.25.0 (#14529)
    * updating dotnet API docs
    
    * updating java API docs
    
    * updating javascript API docs
    
    * updating py API docs
    
    * updating rb API docs
    
    ---------
    
    Co-authored-by: Selenium CI Bot <selenium-ci@users.noreply.github.com>

--- docs/api/py/_static/documentation_options.js
+++ docs/api/py/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.24.0',
+    VERSION: '4.25.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
