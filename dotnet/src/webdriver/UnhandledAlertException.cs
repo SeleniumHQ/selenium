aa827cb07d768f70f45ce44327f9655cf589724c
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-10-05 21:04:36 +0300

    [dotnet] Remove all obsolete exception ctors in inherited exceptions

--- dotnet/src/webdriver/UnhandledAlertException.cs
+++ dotnet/src/webdriver/UnhandledAlertException.cs
@@ -73,18 +73,6 @@ namespace OpenQA.Selenium
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="UnhandledAlertException"/> class with serialized data.
-        /// </summary>
-        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized
-        /// object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual
-        /// information about the source or destination.</param>
-        protected UnhandledAlertException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-
-        /// <summary>
         /// Gets the text of the unhandled alert.
         /// </summary>
         public string AlertText
