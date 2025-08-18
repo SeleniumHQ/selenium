79916bc09d704cdee3bdd223e0674507a69d63d4
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-08-18 22:52:57 +0300

    Simplify

--- dotnet/src/webdriver/BiDi/Network/Cookie.cs
+++ dotnet/src/webdriver/BiDi/Network/Cookie.cs
@@ -18,15 +18,10 @@
 // </copyright>
 
 using System;
-using System.Text.Json.Serialization;
 
 namespace OpenQA.Selenium.BiDi.Network;
 
-public sealed record Cookie(string Name, BytesValue Value, string Domain, string Path, long Size, bool HttpOnly, bool Secure, SameSite SameSite)
-{
-    [JsonInclude]
-    public TimeSpan? Expiry { get; internal set; }
-}
+public sealed record Cookie(string Name, BytesValue Value, string Domain, string Path, long Size, bool HttpOnly, bool Secure, SameSite SameSite, TimeSpan? Expiry);
 
 public enum SameSite
 {
