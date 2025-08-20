10e714b7a6acc2ae3f1bc278f99d92d057f5e6bf
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2025-08-20 16:16:25 +0300

    Revert "[dotnet] [bidi] Make cookie expiry as TimeSpan (#16204)"
    This reverts commit ab9733c5f032dbfb11382ad75feca63a61401fad.

--- dotnet/src/webdriver/BiDi/Communication/Json/Converters/DateTimeOffsetConverter.cs
+++ dotnet/src/webdriver/BiDi/Communication/Json/Converters/DateTimeOffsetConverter.cs
@@ -27,7 +27,16 @@ internal class DateTimeOffsetConverter : JsonConverter<DateTimeOffset>
 {
     public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        return DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64());
+        // Workaround: it should be Int64, chrome uses double for `expiry` like "expiry":1737379944.308351
+
+        if (reader.TryGetInt64(out long unixTime) is false)
+        {
+            var doubleValue = reader.GetDouble();
+
+            unixTime = Convert.ToInt64(doubleValue);
+        }
+
+        return DateTimeOffset.FromUnixTimeMilliseconds(unixTime);
     }
 
     public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
--- dotnet/src/webdriver/BiDi/Network/Cookie.cs
+++ dotnet/src/webdriver/BiDi/Network/Cookie.cs
@@ -18,10 +18,15 @@
 // </copyright>
 
 using System;
+using System.Text.Json.Serialization;
 
 namespace OpenQA.Selenium.BiDi.Network;
 
-public sealed record Cookie(string Name, BytesValue Value, string Domain, string Path, long Size, bool HttpOnly, bool Secure, SameSite SameSite, TimeSpan? Expiry);
+public sealed record Cookie(string Name, BytesValue Value, string Domain, string Path, long Size, bool HttpOnly, bool Secure, SameSite SameSite)
+{
+    [JsonInclude]
+    public DateTimeOffset? Expiry { get; internal set; }
+}
 
 public enum SameSite
 {
