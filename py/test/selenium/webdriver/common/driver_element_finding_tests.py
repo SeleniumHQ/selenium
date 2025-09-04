b855bf68412008ba49fbe6418ee6de04e3256b88
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-09-04 16:00:42 -0400

    [py] Re-add some xfails

--- py/test/selenium/webdriver/common/driver_element_finding_tests.py
+++ py/test/selenium/webdriver/common/driver_element_finding_tests.py
@@ -189,14 +189,17 @@ def test_should_not_be_able_to_locate_by_tag_name_multiple_elements_that_do_not_
     assert len(elements) == 0
 
 
-@pytest.mark.xfail_safari(reason="unlike chrome, safari raises NoSuchElementException")
 @pytest.mark.xfail_firefox(reason="unlike chrome, firefox raises NoSuchElementException")
+@pytest.mark.xfail_remote(reason="some browsers raise NoSuchElementException")
+@pytest.mark.xfail_safari(reason="unlike chrome, safari raises NoSuchElementException")
 def test_finding_asingle_element_by_empty_tag_name_should_throw(driver, pages):
     pages.load("formPage.html")
     with pytest.raises(InvalidSelectorException):
         driver.find_element(By.TAG_NAME, "")
 
 
+@pytest.mark.xfail_firefox(reason="unlike chrome, firefox returns an empty list")
+@pytest.mark.xfail_remote(reason="some browsers return an empty list")
 @pytest.mark.xfail_safari(reason="unlike chrome, safari returns an empty list")
 def test_finding_multiple_elements_by_empty_tag_name_should_throw(driver, pages):
     pages.load("formPage.html")
