185ad8932a4f771efff21c6ee93c12b8703b0b68
pallavigotwork <info@5elementslearning.dev>
2025-09-24 14:05:47 +0530

    reverted frame exception obj, changed type from str to Any in the common exception file

--- py/selenium/webdriver/remote/switch_to.py
+++ py/selenium/webdriver/remote/switch_to.py
@@ -80,14 +80,13 @@ class SwitchTo:
                 driver.switch_to.frame(driver.find_elements(By.TAG_NAME, "iframe")[0])
         """
         if isinstance(frame_reference, str):
-            frame_name = frame_reference
             try:
                 frame_reference = self._driver.find_element(By.ID, frame_reference)
             except NoSuchElementException:
                 try:
                     frame_reference = self._driver.find_element(By.NAME, frame_reference)
                 except NoSuchElementException as exc:
-                    raise NoSuchFrameException(frame_name) from exc
+                    raise NoSuchFrameException(frame_reference) from exc
 
         self._driver.execute(Command.SWITCH_TO_FRAME, {"id": frame_reference})
 
