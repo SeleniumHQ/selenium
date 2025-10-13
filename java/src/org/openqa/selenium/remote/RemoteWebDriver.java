aa2e084c401be10fe33e2e6ff1386ed0e2ce0883
Diego Molina <diemol@gmail.com>
2025-10-13 17:17:01 +0200

    Code improvement, thanks to @valfirst

--- java/src/org/openqa/selenium/remote/RemoteWebDriver.java
+++ java/src/org/openqa/selenium/remote/RemoteWebDriver.java
@@ -574,11 +574,11 @@ public class RemoteWebDriver
         } else {
           // When this exception comes from a remote end, the real cause is usually hidden in the
           // cause. Let's try to rescue it and display it at the top level.
-          String cause = e.getCause() != null ? e.getCause().getMessage() : "";
+          String cause = e.getCause() != null ? " " + e.getCause().getMessage() : "";
           toThrow =
               new SessionNotCreatedException(
                   "Possible causes are invalid address of the remote server or browser start-up"
-                      + " failure. "
+                      + " failure."
                       + cause,
                   e);
         }
