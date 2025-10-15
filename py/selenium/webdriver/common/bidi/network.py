b4989913cd93d99fcaec9a83f6f5991a9098c1c0
Navin Chandra <navinchandra772@gmail.com>
2025-10-15 17:57:59 +0530

    add response handlers and data collectors

--- py/selenium/webdriver/common/bidi/network.py
+++ py/selenium/webdriver/common/bidi/network.py
@@ -15,7 +15,91 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import base64
+
 from selenium.webdriver.common.bidi.common import command_builder
+
+
+class StringValue:
+    """Represents a network.StringValue for valid UTF-8 data."""
+
+    def __init__(self, value: str):
+        self.type = "string"
+        self.value = value
+
+    def to_dict(self):
+        return {"type": self.type, "value": self.value}
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data["value"])
+
+    def to_bytes(self) -> bytes:
+        """Convert to bytes using UTF-8 encoding."""
+        return self.value.encode("utf-8")
+
+
+class Base64Value:
+    """Represents a network.Base64Value for binary data."""
+
+    def __init__(self, value: str):
+        self.type = "base64"
+        self.value = value
+
+    def to_dict(self):
+        return {"type": self.type, "value": self.value}
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data["value"])
+
+    def to_bytes(self) -> bytes:
+        """Convert to bytes by decoding base64."""
+        return base64.b64decode(self.value)
+
+    @classmethod
+    def from_bytes(cls, data: bytes):
+        """Create Base64Value from bytes."""
+        encoded = base64.b64encode(data).decode("ascii")
+        return cls(encoded)
+
+
+class BytesValue:
+    """Represents network.BytesValue that can be either StringValue or Base64Value."""
+
+    def __init__(self, value):
+        if isinstance(value, (StringValue, Base64Value)):
+            self._value = value
+        elif isinstance(value, str):
+            self._value = StringValue(value)
+        elif isinstance(value, bytes):
+            self._value = Base64Value.from_bytes(value)
+        else:
+            raise ValueError("Value must be StringValue, Base64Value, str, or bytes")
+
+    @property
+    def type(self):
+        return self._value.type
+
+    @property
+    def value(self):
+        return self._value.value
+
+    def to_dict(self):
+        return self._value.to_dict()
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        if data["type"] == "string":
+            return cls(StringValue.from_dict(data))
+        elif data["type"] == "base64":
+            return cls(Base64Value.from_dict(data))
+        else:
+            raise ValueError(f"Unknown BytesValue type: {data['type']}")
+
+    def to_bytes(self) -> bytes:
+        """Deserialize protocol bytes to byte sequence."""
+        return self._value.to_bytes()
 
 
 class NetworkEvent:
@@ -52,6 +136,7 @@ class Network:
         self.intercepts = []
         self.callbacks = {}
         self.subscriptions = {}
+        self.data_collectors = []
 
     def _add_intercept(self, phases=[], contexts=None, url_patterns=None):
         """Add an intercept to the network.
@@ -218,15 +303,157 @@ class Network:
         """Clear all request handlers from the network."""
 
         for event_name in self.subscriptions:
-            net_event = NetworkEvent(event_name)
-            for callback_id in self.subscriptions[event_name]:
+            for callback_id in self.subscriptions[event_name].copy():
+                net_event = NetworkEvent(event_name)
                 self.conn.remove_callback(net_event, callback_id)
-                self._remove_intercept(self.callbacks[callback_id])
-                del self.callbacks[callback_id]
+                if callback_id in self.callbacks:
+                    self._remove_intercept(self.callbacks[callback_id])
+                    del self.callbacks[callback_id]
+        self.subscriptions = {}
+
+    def add_response_handler(self, event, callback, url_patterns=None, contexts=None, intercept=False):
+        """Add a response handler to the network.
+
+        Parameters:
+        ----------
+            event (str): The event to subscribe to. Can be "response_started" or "response_completed".
+            callback (function): The callback function to execute on response event.
+                Takes Response object as argument.
+            url_patterns (list, optional): A list of URL patterns to match.
+                Default is None.
+            contexts (list, optional): A list of contexts to match.
+                Default is None.
+            intercept (bool, optional): Whether to create an intercept (block the response).
+                Only works with "response_started". Default is False.
+                Note: If intercept=True, you MUST call response.continue_response()
+                in your callback or navigation will hang.
+
+        Returns:
+        -------
+            int : callback id
+        """
+        try:
+            event_name = self.EVENTS[event]
+        except KeyError:
+            raise Exception(f"Event {event} not found")
+
+        # Only response_started can be intercepted (for continue_response)
+        intercept_id = None
+        if intercept:
+            if event not in self.PHASES:
+                raise ValueError(f"Event {event} does not support interception")
+            phase_name = self.PHASES[event]
+            result = self._add_intercept(phases=[phase_name], url_patterns=url_patterns, contexts=contexts)
+            intercept_id = result["intercept"]
+
+        callback_id = self._on_response(event_name, callback)
+
+        if event_name in self.subscriptions:
+            self.subscriptions[event_name].append(callback_id)
+        else:
             params = {}
             params["events"] = [event_name]
-            self.conn.execute(command_builder("session.unsubscribe", params))
-        self.subscriptions = {}
+            self.conn.execute(command_builder("session.subscribe", params))
+            self.subscriptions[event_name] = [callback_id]
+
+        if intercept_id:
+            self.callbacks[callback_id] = intercept_id
+        else:
+            self.callbacks[callback_id] = None
+
+        return callback_id
+
+    def remove_response_handler(self, event, callback_id):
+        """Remove a response handler from the network.
+
+        Parameters:
+        ----------
+            event (str): The event to unsubscribe from.
+            callback_id (int): The callback id to remove.
+        """
+        try:
+            event_name = self.EVENTS[event]
+        except KeyError:
+            raise Exception(f"Event {event} not found")
+
+        net_event = NetworkEvent(event_name)
+
+        self.conn.remove_callback(net_event, callback_id)
+
+        # Remove intercept if it was created for this handler
+        if callback_id in self.callbacks and self.callbacks[callback_id] is not None:
+            self._remove_intercept(self.callbacks[callback_id])
+            del self.callbacks[callback_id]
+
+        if event_name in self.subscriptions:
+            self.subscriptions[event_name].remove(callback_id)
+            if len(self.subscriptions[event_name]) == 0:
+                del self.subscriptions[event_name]
+
+    def clear_response_handlers(self):
+        """Clear all response handlers from the network."""
+        response_events = ["response_started", "response_completed"]
+
+        for event in response_events:
+            try:
+                event_name = self.EVENTS[event]
+                if event_name in self.subscriptions:
+                    for callback_id in self.subscriptions[event_name].copy():
+                        net_event = NetworkEvent(event_name)
+                        self.conn.remove_callback(net_event, callback_id)
+
+                        # Remove intercept if one exists
+                        if callback_id in self.callbacks and self.callbacks[callback_id] is not None:
+                            self._remove_intercept(self.callbacks[callback_id])
+                            del self.callbacks[callback_id]
+
+                    del self.subscriptions[event_name]
+            except KeyError:
+                # Event not found, skip
+                continue
+
+    def _on_response(self, event_name, callback):
+        """Set a callback function to subscribe to a response network event.
+
+        Parameters:
+        ----------
+            event_name (str): The event to subscribe to.
+            callback (function): The callback function to execute on event.
+                Takes Response object as argument.
+
+        Returns:
+        -------
+            int : callback id
+        """
+        event = NetworkEvent(event_name)
+
+        def _callback(event_data):
+            # Response events contain both request and response data in params
+            response_data = event_data.params.get("response", {})
+            request_data = event_data.params.get("request", {})
+            request_id = request_data.get("request")
+
+            # Create a Response object with the response data and request ID
+            response = Response(
+                network=self,
+                request_id=request_id,
+                url=response_data.get("url"),
+                status_code=response_data.get("status"),
+                status_text=response_data.get("statusText"),
+                headers=response_data.get("headers", []),
+                mime_type=response_data.get("mimeType"),
+                from_cache=response_data.get("fromCache", False),
+            )
+            callback(response)
+
+        callback_id = self.conn.add_callback(event, _callback)
+
+        if event_name in self.callbacks:
+            self.callbacks[event_name].append(callback_id)
+        else:
+            self.callbacks[event_name] = [callback_id]
+
+        return callback_id
 
     def add_auth_handler(self, username, password):
         """Add an authentication handler to the network.
@@ -256,6 +483,109 @@ class Network:
         """
         event = "auth_required"
         self.remove_request_handler(event, callback_id)
+
+    def add_data_collector(
+        self,
+        data_types: list,
+        max_encoded_data_size: int,
+        collector_type: str = "blob",
+        contexts: list = None,
+        user_contexts: list = None,
+    ):
+        """Add a data collector to the network.
+
+        Parameters:
+        ----------
+            data_types (list): A list of data types to collect (e.g., ["response"]).`
+            max_encoded_data_size (int): Maximum size of encoded data.
+            collector_type (str, optional): Type of collector. Default is "blob".
+            contexts (list, optional): A list of browsing contexts to intercept.
+                Default is None.
+            user_contexts (list, optional): A list of user contexts to intercept.
+                Default is None.
+
+        Returns:
+        -------
+            str : collector id
+
+        Raises:
+        ------
+            ValueError: If both contexts and user_contexts are provided.
+        """
+        if contexts is not None and user_contexts is not None:
+            raise ValueError("Cannot specify both contexts and user_contexts, specify one")
+
+        params = {
+            "dataTypes": data_types,
+            "maxEncodedDataSize": max_encoded_data_size,
+            "collectorType": collector_type,
+        }
+
+        if contexts is not None:
+            params["contexts"] = contexts
+        if user_contexts is not None:
+            params["userContexts"] = user_contexts
+
+        cmd = command_builder("network.addDataCollector", params)
+        result = self.conn.execute(cmd)
+        collector_id = result["collector"]
+        self.data_collectors.append(collector_id)
+        return collector_id
+
+    def remove_data_collector(self, collector_id: str):
+        """Remove a data collector from the network.
+
+        Parameters:
+        ----------
+            collector_id (str): The collector id to remove.
+
+        Raises:
+        ------
+            ValueError: If collector is not found.
+        """
+        if collector_id not in self.data_collectors:
+            raise ValueError(f"Collector {collector_id} not found")
+
+        params = {"collector": collector_id}
+        cmd = command_builder("network.removeDataCollector", params)
+        self.conn.execute(cmd)
+        self.data_collectors.remove(collector_id)
+
+    def get_data(self, data_type: str, request_id: str, collector_id: str = None, disown: bool = False):
+        """Retrieve network data for a request.
+
+        Parameters:
+        ----------
+            data_type (str): The type of data to retrieve (e.g., "response").
+            request_id (str): The request id to get data for.
+            collector_id (str, optional): The collector id to use.
+                Default is None.
+            disown (bool, optional): Whether to disown the data from the collector.
+                Default is False.
+
+        Returns:
+        -------
+            BytesValue : The network data as BytesValue.
+
+        Raises:
+        ------
+            ValueError: If disown is True but collector_id is None.
+        """
+        if disown and collector_id is None:
+            raise ValueError("Cannot disown data without specifying a collector")
+
+        params = {
+            "dataType": data_type,
+            "request": request_id,
+            "disown": disown,
+        }
+
+        if collector_id is not None:
+            params["collector"] = collector_id
+
+        cmd = command_builder("network.getData", params)
+        result = self.conn.execute(cmd)
+        return BytesValue.from_dict(result["bytes"])
 
 
 class Request:
@@ -338,3 +668,65 @@ class Request:
             params["credentials"] = {"type": "password", "username": username, "password": password}
 
         self.network.conn.execute(command_builder("network.continueWithAuth", params))
+
+
+class Response:
+    """Represents a network response."""
+
+    def __init__(
+        self,
+        network: Network,
+        request_id,
+        url=None,
+        status_code=None,
+        status_text=None,
+        headers=None,
+        mime_type=None,
+        from_cache=False,
+    ):
+        self.network = network
+        self.request_id = request_id
+        self.url = url
+        self.status_code = status_code
+        self.status_text = status_text
+        self.headers = headers or []
+        self.mime_type = mime_type
+        self.from_cache = from_cache
+
+    def continue_response(self, cookies=None, credentials=None, headers=None, reason_phrase=None, status_code=None):
+        """Continue a response blocked by a network intercept.
+
+        This can be called in the responseStarted phase to modify the status
+        and headers of the response.
+
+        Parameters:
+        ----------
+            cookies (list, optional): A list of Set-Cookie headers to set.
+            credentials (dict, optional): Authentication credentials with keys:
+                - type: "password"
+                - username: str
+                - password: str
+            headers (list, optional): A list of headers to set.
+            reason_phrase (str, optional): The HTTP status text (e.g., "OK", "Not Found").
+            status_code (int, optional): The HTTP status code (e.g., 200, 404).
+
+        Raises:
+        ------
+            ValueError: If request_id is not available.
+        """
+        if not self.request_id:
+            raise ValueError("Response not found.")
+
+        params = {"request": self.request_id}
+        if cookies is not None:
+            params["cookies"] = cookies
+        if credentials is not None:
+            params["credentials"] = credentials
+        if headers is not None:
+            params["headers"] = headers
+        if reason_phrase is not None:
+            params["reasonPhrase"] = reason_phrase
+        if status_code is not None:
+            params["statusCode"] = status_code
+
+        self.network.conn.execute(command_builder("network.continueResponse", params))
