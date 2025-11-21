ce185c64f8864e88ea190abf63504bbee0511844
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-11-20 21:35:52 -0500

    Fix typo in assertion

--- py/test/selenium/webdriver/common/bidi_browsing_context_tests.py
+++ py/test/selenium/webdriver/common/bidi_browsing_context_tests.py
@@ -400,7 +400,7 @@ def test_set_viewport_back_to_default(driver, pages):
         device_pixel_ratio = driver.execute_script("return window.devicePixelRatio")
 
         assert viewport_size[0] == default_viewport_size[0]
-        assert viewport_size[1] == default_viewport_size[0]
+        assert viewport_size[1] == default_viewport_size[1]
         assert device_pixel_ratio == default_device_pixel_ratio
     finally:
         driver.browsing_context.set_viewport(context=context_id, viewport=None, device_pixel_ratio=None)
