c669241e453bbd4d8dc4229485616b16eb8a77c4
Alex Popov <iampopovich@gmail.com>
2026-01-02 00:58:04 +0300

    [py] Validate command_executor type for CDP and BiDi support in WebDriver

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -1047,6 +1047,10 @@ class WebDriver(BaseWebDriver):
             return self._devtools, self._websocket_connection
         if self.caps["browserName"].lower() == "firefox":
             raise RuntimeError("CDP support for Firefox has been removed. Please switch to WebDriver BiDi.")
+        
+        if not isinstance(self.command_executor, RemoteConnection):
+            raise WebDriverException("command_executor must be a RemoteConnection instance for CDP support")
+        
         self._websocket_connection = WebSocketConnection(
             ws_url,
             self.command_executor.client_config.websocket_timeout,
@@ -1099,6 +1103,9 @@ class WebDriver(BaseWebDriver):
             ws_url = self.caps.get("webSocketUrl")
         else:
             raise WebDriverException("Unable to find url to connect to from capabilities")
+
+        if not isinstance(self.command_executor, RemoteConnection):
+            raise WebDriverException("command_executor must be a RemoteConnection instance for BiDi support")
 
         self._websocket_connection = WebSocketConnection(
             ws_url,
