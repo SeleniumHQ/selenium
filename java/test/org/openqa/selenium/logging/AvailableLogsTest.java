9c434b8167e71507711d5fce1c45f375538ae686
Titus Fortner <titus.fortner@gmail.com>
2026-01-10 15:55:00 -0600

    [java] remove unnecessary localDriver references

--- java/test/org/openqa/selenium/logging/AvailableLogsTest.java
+++ java/test/org/openqa/selenium/logging/AvailableLogsTest.java
@@ -24,9 +24,7 @@ import static org.openqa.selenium.testing.drivers.Browser.IE;
 import static org.openqa.selenium.testing.drivers.Browser.SAFARI;
 
 import java.util.Set;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.testing.Ignore;
 import org.openqa.selenium.testing.JupiterTestBase;
 
@@ -34,16 +32,6 @@ import org.openqa.selenium.testing.JupiterTestBase;
 @Ignore(FIREFOX)
 @Ignore(SAFARI)
 class AvailableLogsTest extends JupiterTestBase {
-
-  private WebDriver localDriver;
-
-  @AfterEach
-  public void quitDriver() {
-    if (localDriver != null) {
-      localDriver.quit();
-      localDriver = null;
-    }
-  }
 
   @Test
   void browserLogShouldBeEnabledByDefault() {
