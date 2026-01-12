88a8ff2e7e03f785285e9c5317aa560cb1bdded8
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-01-12 15:35:44 +0300

    Required source actions id

--- dotnet/src/webdriver/BiDi/Input/SourceActions.cs
+++ dotnet/src/webdriver/BiDi/Input/SourceActions.cs
@@ -27,14 +27,11 @@ using System.Text.Json.Serialization;
 namespace OpenQA.Selenium.BiDi.Input;
 
 [JsonConverter(typeof(InputSourceActionsConverter))]
-public abstract record SourceActions(string? Id = null)
-{
-    public string Id { get; init; } = Id ?? Guid.NewGuid().ToString();
-}
+public abstract record SourceActions(string Id);
 
 public interface ISourceAction;
 
-public abstract record SourceActions<T>(string? Id = null) : SourceActions(Id), IEnumerable<ISourceAction> where T : ISourceAction
+public abstract record SourceActions<T>(string Id) : SourceActions(Id), IEnumerable<ISourceAction> where T : ISourceAction
 {
     public IList<ISourceAction> Actions { get; set; } = [];
 
@@ -51,7 +48,7 @@ public abstract record SourceActions<T>(string? Id = null) : SourceActions(Id), 
 [JsonDerivedType(typeof(UpKey), "keyUp")]
 public interface IKeySourceAction : ISourceAction;
 
-public sealed record KeyActions(string? Id = null) : SourceActions<IKeySourceAction>(Id)
+public sealed record KeyActions(string Id) : SourceActions<IKeySourceAction>(Id)
 {
     public KeyActions Type(string text)
     {
@@ -72,7 +69,7 @@ public sealed record KeyActions(string? Id = null) : SourceActions<IKeySourceAct
 [JsonDerivedType(typeof(MovePointer), "pointerMove")]
 public interface IPointerSourceAction : ISourceAction;
 
-public sealed record PointerActions(string? Id = null) : SourceActions<IPointerSourceAction>(Id)
+public sealed record PointerActions(string Id) : SourceActions<IPointerSourceAction>(Id)
 {
     public PointerParameters? Options { get; set; }
 }
@@ -82,13 +79,13 @@ public sealed record PointerActions(string? Id = null) : SourceActions<IPointerS
 [JsonDerivedType(typeof(ScrollWheel), "scroll")]
 public interface IWheelSourceAction : ISourceAction;
 
-public sealed record WheelActions(string? Id = null) : SourceActions<IWheelSourceAction>(Id);
+public sealed record WheelActions(string Id) : SourceActions<IWheelSourceAction>(Id);
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
 [JsonDerivedType(typeof(Pause), "pause")]
 public interface INoneSourceAction : ISourceAction;
 
-public sealed record NoneActions(string? Id = null) : SourceActions<INoneSourceAction>(Id);
+public sealed record NoneActions(string Id) : SourceActions<INoneSourceAction>(Id);
 
 public abstract record Key : IKeySourceAction;
 
