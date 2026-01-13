e0ac8ad8aa5f620e95d9254a5ef7fe5ede1494b1
Andrei Solntsev <andrei.solntsev@gmail.com>
2026-01-13 23:53:41 +0200

    automatically save screenshot and page source in case of test failure

--- java/test/org/openqa/selenium/testing/SeleniumExtension.java
+++ java/test/org/openqa/selenium/testing/SeleniumExtension.java
@@ -17,9 +17,15 @@
 
 package org.openqa.selenium.testing;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Objects.requireNonNullElse;
+import static java.util.UUID.randomUUID;
 import static org.junit.platform.commons.util.AnnotationUtils.findAnnotation;
 import static org.junit.platform.commons.util.AnnotationUtils.findRepeatableAnnotations;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
 import java.time.Duration;
@@ -40,6 +46,8 @@ import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.internal.Require;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -201,6 +209,8 @@ public class SeleniumExtension
   @Override
   public void testFailed(ExtensionContext context, Throwable cause) {
     TestWatcher.super.testFailed(context, cause);
+
+    logWebdriverDetails(context);
 
     // ManageDriverRule.failed
     Browser current = Objects.requireNonNull(Browser.detect());
@@ -411,6 +421,72 @@ public class SeleniumExtension
         }
         currentInstances.driver.switchTo().defaultContent();
       }
+    }
+  }
+
+  private void logWebdriverDetails(ExtensionContext context) {
+    try {
+      Optional.ofNullable(instances.get())
+          .ifPresent(
+              (Instances current) -> {
+                System.err.println();
+                System.err.println("Test failure details:");
+                System.err.printf("  URL: %s%n", current.driver.getCurrentUrl());
+                System.err.printf("  Title: %s%n", current.driver.getTitle());
+                System.err.printf("  Page source: %s%n", savePageSource(current.driver, context));
+                System.err.printf("  Screenshot: %s%n", saveScreenshot(current.driver, context));
+              });
+    } catch (Exception e) {
+      LOG.log(Level.WARNING, "Failed to log webdriver details: " + e.getMessage(), e);
+    }
+  }
+
+  private File artifact(ExtensionContext context, String suffix, byte[] content)
+      throws IOException {
+    File parent = new File("build/failures/java/" + Browser.detect());
+    File testClassFolder =
+        new File(parent, context.getTestClass().map(Class::getName).orElse("-").replace('.', '/'));
+    if (!testClassFolder.exists()) {
+      if (!testClassFolder.mkdirs()) {
+        throw new IOException("Cannot create folder " + testClassFolder);
+      }
+    }
+    File result =
+        new File(
+            testClassFolder,
+            context.getTestMethod().map(Method::getName).orElse(randomUUID().toString()) + suffix);
+    try (FileOutputStream out = new FileOutputStream(result)) {
+      out.write(content);
+    }
+    return result;
+  }
+
+  private String savePageSource(WebDriver driver, ExtensionContext context) {
+    try {
+      String pageSource = requireNonNullElse(driver.getPageSource(), "");
+      return toURL(artifact(context, ".html", pageSource.getBytes(UTF_8)));
+    } catch (IOException | RuntimeException e) {
+      return String.format("- (%s)", e);
+    }
+  }
+
+  private String saveScreenshot(WebDriver driver, ExtensionContext context) {
+    if (!(driver instanceof TakesScreenshot)) {
+      return "-";
+    }
+    try {
+      byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      return toURL(artifact(context, ".png", screenshot));
+    } catch (IOException | RuntimeException e) {
+      return String.format("- (%s)", e);
+    }
+  }
+
+  private String toURL(File artifact) {
+    try {
+      return artifact.getCanonicalFile().toURI().toURL().toExternalForm();
+    } catch (IOException e) {
+      return artifact.getAbsolutePath();
     }
   }
 }
