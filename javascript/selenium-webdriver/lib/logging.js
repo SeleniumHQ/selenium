7ecc0a609f2ea52e4d028b596f596d6ccf889b87
Titus Fortner <titus.fortner@gmail.com>
2026-01-26 12:31:36 -0600

    [js] remove unnecessary seDebugWarningEmitted flag

--- javascript/selenium-webdriver/lib/logging.js
+++ javascript/selenium-webdriver/lib/logging.js
@@ -481,14 +481,12 @@ class LogManager {
 }
 
 const logManager = new LogManager()
-let seDebugWarningEmitted = false
 
 // Enable debug logging if SE_DEBUG or SELENIUM_VERBOSE environment variable is set
 if (typeof process !== 'undefined' && process.env && (process.env.SE_DEBUG || process.env.SELENIUM_VERBOSE)) {
   logManager.root_.setLevel(Level.ALL)
   logManager.root_.addHandler(consoleHandler)
-  if (process.env.SE_DEBUG && !seDebugWarningEmitted) {
-    seDebugWarningEmitted = true
+  if (process.env.SE_DEBUG) {
     logManager.root_.warning(
       'Environment Variable `SE_DEBUG` is set; Selenium is forcing verbose logging which may override user-specified settings.',
     )
