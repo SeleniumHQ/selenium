5ea94c9eede7ec86f35092e2a3e30193eb76ae59
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-02-03 21:05:41 +0300

    Make cached driver path local nullable

--- dotnet/src/webdriver/DriverFinder.cs
+++ dotnet/src/webdriver/DriverFinder.cs
@@ -105,7 +105,7 @@ public class DriverFinder
     /// <exception cref="NoSuchDriverException">If one of the paths does not exist.</exception>
     private Dictionary<string, string> BinaryPaths()
     {
-        if (paths.TryGetValue(DriverPathKey, out string driverPath) && !string.IsNullOrWhiteSpace(driverPath))
+        if (paths.TryGetValue(DriverPathKey, out string? cachedDriverPath) && !string.IsNullOrWhiteSpace(cachedDriverPath))
         {
             return paths;
         }
