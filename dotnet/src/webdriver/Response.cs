761fa279437f56d2b1bc803e7474f35d81e7c081
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-02-11 17:44:45 +0300

    Reword

--- dotnet/src/webdriver/Response.cs
+++ dotnet/src/webdriver/Response.cs
@@ -205,7 +205,7 @@ public class Response
 
 [JsonSerializable(typeof(object))]
 #pragma warning disable CS3016 // Arrays as attribute arguments is not CLS-compliant
- // This is not a part of a public API, so we can ignore the warning
+ // This is not part of the public API, so we can ignore this warning
 [JsonSourceGenerationOptions(Converters = [typeof(ResponseValueJsonConverter)])] // we still need it to make `Object` as `Dictionary`
 #pragma warning restore CS3016 // Arrays as attribute arguments is not CLS-compliant
 internal sealed partial class ResponseJsonSerializerContext : JsonSerializerContext;
