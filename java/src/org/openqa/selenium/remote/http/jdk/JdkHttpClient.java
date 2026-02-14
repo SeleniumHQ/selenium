66e830091380b3095e0bc47d37552d57a6657c0d
Andrei Solntsev <andrei.solntsev@gmail.com>
2026-02-14 16:31:06 +0200

    add details to error message when failed to establish WS connection

--- java/src/org/openqa/selenium/remote/http/jdk/JdkHttpClient.java
+++ java/src/org/openqa/selenium/remote/http/jdk/JdkHttpClient.java
@@ -161,7 +161,11 @@ public class JdkHttpClient implements HttpClient {
     try {
       uri = getWebSocketUri(request);
     } catch (URISyntaxException e) {
-      throw new ConnectionFailedException("JdkWebSocket initial request execution error", e);
+      String message =
+          String.format(
+              "JdkWebSocket initial request execution error (uri: %s)",
+              messages.getRawUri(request));
+      throw new ConnectionFailedException(message, e);
     }
 
     java.net.http.WebSocket.Builder builder = client.newWebSocketBuilder();
@@ -251,13 +255,16 @@ public class JdkHttpClient implements HttpClient {
     } catch (ExecutionException e) {
       Throwable cause = e.getCause();
       throw new ConnectionFailedException(
-          "JdkWebSocket initial request execution error", (cause != null) ? cause : e);
+          String.format("JdkWebSocket initial request execution error (uri: %s)", uri),
+          (cause != null) ? cause : e);
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
-      throw new ConnectionFailedException("JdkWebSocket initial request interrupted", e);
+      throw new ConnectionFailedException(
+          String.format("JdkWebSocket initial request interrupted (uri: %s)", uri), e);
     } catch (java.util.concurrent.TimeoutException e) {
       webSocketCompletableFuture.cancel(true);
-      throw new ConnectionFailedException("JdkWebSocket initial request timeout", e);
+      throw new ConnectionFailedException(
+          String.format("JdkWebSocket initial request timeout (uri: %s)", uri), e);
     }
 
     WebSocket websocket =
