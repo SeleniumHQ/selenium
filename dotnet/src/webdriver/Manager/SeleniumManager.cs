b3d09595e0957fc42a6d6656cce229c113aca08b
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-02-15 21:44:33 +0300

    Format

--- dotnet/src/webdriver/Manager/SeleniumManager.cs
+++ dotnet/src/webdriver/Manager/SeleniumManager.cs
@@ -359,7 +359,7 @@ public static partial class SeleniumManager
                 throw new WebDriverException(exceptionMessageBuilder.ToString());
             }
         }
-        catch (Exception ex)when (ex is not OperationCanceledException and not WebDriverException)
+        catch (Exception ex) when (ex is not OperationCanceledException and not WebDriverException)
         {
             throw new WebDriverException($"Error starting process: {process.StartInfo.FileName} {arguments}", ex);
         }
