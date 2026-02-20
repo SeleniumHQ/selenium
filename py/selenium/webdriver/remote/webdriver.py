61ca6308215dc050295f1e2c9442582caf71319b
Adam Dangoor <adamdangoor@gmail.com>
2026-02-20 10:13:28 +0000

    Change return type of execute_script to dict[str, Any]
    Updated the return type of execute_script method to dict[str, Any].

--- py/selenium/webdriver/remote/webdriver.py
+++ py/selenium/webdriver/remote/webdriver.py
@@ -506,7 +506,7 @@ class WebDriver(BaseWebDriver):
         """
         return list(self.pinned_scripts)
 
-    def execute_script(self, script: str, *args):
+    def execute_script(self, script: str, *args) -> dict[str, Any]:
         """Synchronously Executes JavaScript in the current window/frame.
 
         Args:
