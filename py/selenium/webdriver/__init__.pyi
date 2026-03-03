ce5cc9248e9674c84d0c497f5bef5c4183baa307
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2026-03-02 22:54:34 -0500

    Use new pyi

--- py/selenium/webdriver/__init__.pyi
+++ py/selenium/webdriver/__init__.pyi
@@ -1,4 +1,22 @@
-# This is an auto-generated stub for selenium.webdriver. DO NOT EDIT.
+# Licensed to the Software Freedom Conservancy (SFC) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The SFC licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Type stub with lazy import mapping from __init__.py."""
+
 # ruff: noqa: F401, F821, I001, UP037, RUF100
 
 from typing import TYPE_CHECKING
@@ -7,41 +25,63 @@ from typing import TYPE_CHECKING
 __version__: str
 
 if TYPE_CHECKING:
-    from selenium.webdriver.common.action_chains import ActionChains
-    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.common.options import Options
-    from selenium.webdriver.common.proxy import Proxy
-    from selenium.webdriver.common.service import Service
-    from selenium.webdriver.remote.webdriver import WebDriver
+    # Browser WebDrivers
+    from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver
+    from selenium.webdriver.edge.webdriver import WebDriver as EdgeWebDriver
+    from selenium.webdriver.firefox.webdriver import WebDriver as FirefoxWebDriver
+    from selenium.webdriver.ie.webdriver import WebDriver as IeWebDriver
+    from selenium.webdriver.safari.webdriver import WebDriver as SafariWebDriver
+    from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
+    from selenium.webdriver.webkitgtk.webdriver import WebDriver as WebKitGTKWebDriver
+    from selenium.webdriver.wpewebkit.webdriver import WebDriver as WPEWebKitWebDriver
 
-# Lazy imports (forward references)
-ActionChains: "ActionChains"
-Chrome: "WebDriver"
-ChromeOptions: "Options"
-ChromeService: "Service"
-ChromiumEdge: "WebDriver"
-DesiredCapabilities: "DesiredCapabilities"
-Edge: "WebDriver"
-EdgeOptions: "Options"
-EdgeService: "Service"
-Firefox: "WebDriver"
-FirefoxOptions: "Options"
-FirefoxProfile: "FirefoxProfile"
-FirefoxService: "Service"
-Ie: "WebDriver"
-IeOptions: "Options"
-IeService: "Service"
-Keys: "Keys"
-Proxy: "Proxy"
-Remote: "WebDriver"
-Safari: "WebDriver"
-SafariOptions: "Options"
-SafariService: "Service"
-WPEWebKit: "WebDriver"
-WPEWebKitOptions: "Options"
-WPEWebKitService: "Service"
-WebKitGTK: "WebDriver"
-WebKitGTKOptions: "Options"
-WebKitGTKService: "Service"
+    # Browser Options
+    from selenium.webdriver.chrome.options import Options as ChromeOptions
+    from selenium.webdriver.edge.options import Options as EdgeOptions
+    from selenium.webdriver.firefox.options import Options as FirefoxOptions
+    from selenium.webdriver.ie.options import Options as IeOptions
+    from selenium.webdriver.safari.options import Options as SafariOptions
+    from selenium.webdriver.webkitgtk.options import Options as WebKitGTKOptions
+    from selenium.webdriver.wpewebkit.options import Options as WPEWebKitOptions
+
+    # Browser Services
+    from selenium.webdriver.chrome.service import Service as ChromeService
+    from selenium.webdriver.edge.service import Service as EdgeService
+    from selenium.webdriver.firefox.service import Service as FirefoxService
+    from selenium.webdriver.ie.service import Service as IeService
+    from selenium.webdriver.safari.service import Service as SafariService
+    from selenium.webdriver.webkitgtk.service import Service as WebKitGTKService
+    from selenium.webdriver.wpewebkit.service import Service as WPEWebKitService
+
+# Forward references for lazy imports
+Chrome: "ChromeWebDriver"
+ChromeOptions: "ChromeOptions"
+ChromeService: "ChromeService"
+
+Edge: "EdgeWebDriver"
+ChromiumEdge: "EdgeWebDriver"
+EdgeOptions: "EdgeOptions"
+EdgeService: "EdgeService"
+
+Firefox: "FirefoxWebDriver"
+FirefoxOptions: "FirefoxOptions"
+FirefoxProfile: "selenium.webdriver.firefox.firefox_profile.FirefoxProfile"
+FirefoxService: "FirefoxService"
+
+Ie: "IeWebDriver"
+IeOptions: "IeOptions"
+IeService: "IeService"
+
+Safari: "SafariWebDriver"
+SafariOptions: "SafariOptions"
+SafariService: "SafariService"
+
+Remote: "RemoteWebDriver"
+
+WebKitGTK: "WebKitGTKWebDriver"
+WebKitGTKOptions: "WebKitGTKOptions"
+WebKitGTKService: "WebKitGTKService"
+
+WPEWebKit: "WPEWebKitWebDriver"
+WPEWebKitOptions: "WPEWebKitOptions"
+WPEWebKitService: "WPEWebKitService"
