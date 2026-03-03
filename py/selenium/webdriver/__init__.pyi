4dab62e752ceadfe5de6e9a5babe318d7f82f1c5
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2026-03-03 09:05:08 -0500

    Fix imports

--- py/selenium/webdriver/__init__.pyi
+++ py/selenium/webdriver/__init__.pyi
@@ -24,62 +24,61 @@ from typing import TYPE_CHECKING
 # Expose runtime version
 __version__: str
 
-if TYPE_CHECKING:
-    # Chrome
-    from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
-    from selenium.webdriver.chrome.options import Options as ChromeOptions
-    from selenium.webdriver.chrome.service import Service as ChromeService
+# Chrome
+from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-    # Edge
-    from selenium.webdriver.edge.webdriver import WebDriver as Edge
-    from selenium.webdriver.edge.webdriver import WebDriver as ChromiumEdge
-    from selenium.webdriver.edge.options import Options as EdgeOptions
-    from selenium.webdriver.edge.service import Service as EdgeService
+# Edge
+from selenium.webdriver.edge.webdriver import WebDriver as Edge
+from selenium.webdriver.edge.webdriver import WebDriver as ChromiumEdge
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.edge.service import Service as EdgeService
 
-    # Firefox
-    from selenium.webdriver.firefox.webdriver import WebDriver as Firefox
-    from selenium.webdriver.firefox.options import Options as FirefoxOptions
-    from selenium.webdriver.firefox.service import Service as FirefoxService
-    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile as FirefoxProfile
+# Firefox
+from selenium.webdriver.firefox.webdriver import WebDriver as Firefox
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile as FirefoxProfile
 
-    # IE
-    from selenium.webdriver.ie.webdriver import WebDriver as Ie
-    from selenium.webdriver.ie.options import Options as IeOptions
-    from selenium.webdriver.ie.service import Service as IeService
+# IE
+from selenium.webdriver.ie.webdriver import WebDriver as Ie
+from selenium.webdriver.ie.options import Options as IeOptions
+from selenium.webdriver.ie.service import Service as IeService
 
-    # Safari
-    from selenium.webdriver.safari.webdriver import WebDriver as Safari
-    from selenium.webdriver.safari.options import Options as SafariOptions
-    from selenium.webdriver.safari.service import Service as SafariService
+# Safari
+from selenium.webdriver.safari.webdriver import WebDriver as Safari
+from selenium.webdriver.safari.options import Options as SafariOptions
+from selenium.webdriver.safari.service import Service as SafariService
 
-    # Remote
-    from selenium.webdriver.remote.webdriver import WebDriver as Remote
+# Remote
+from selenium.webdriver.remote.webdriver import WebDriver as Remote
 
-    # WebKitGTK
-    from selenium.webdriver.webkitgtk.webdriver import WebDriver as WebKitGTK
-    from selenium.webdriver.webkitgtk.options import Options as WebKitGTKOptions
-    from selenium.webdriver.webkitgtk.service import Service as WebKitGTKService
+# WebKitGTK
+from selenium.webdriver.webkitgtk.webdriver import WebDriver as WebKitGTK
+from selenium.webdriver.webkitgtk.options import Options as WebKitGTKOptions
+from selenium.webdriver.webkitgtk.service import Service as WebKitGTKService
 
-    # WPEWebKit
-    from selenium.webdriver.wpewebkit.webdriver import WebDriver as WPEWebKit
-    from selenium.webdriver.wpewebkit.options import Options as WPEWebKitOptions
-    from selenium.webdriver.wpewebkit.service import Service as WPEWebKitService
+# WPEWebKit
+from selenium.webdriver.wpewebkit.webdriver import WebDriver as WPEWebKit
+from selenium.webdriver.wpewebkit.options import Options as WPEWebKitOptions
+from selenium.webdriver.wpewebkit.service import Service as WPEWebKitService
 
-    # Common utilities
-    import selenium.webdriver.common.action_chains.ActionChains as ActionChains
-    import selenium.webdriver.common.desired_capabilities.DesiredCapabilities as DesiredCapabilities
-    import selenium.webdriver.common.keys.Keys as Keys
-    import selenium.webdriver.common.proxy.Proxy as Proxy
+# Common utilities
+from selenium.webdriver.common.action_chains import ActionChains as ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities as DesiredCapabilities
+from selenium.webdriver.common.keys import Keys as Keys
+from selenium.webdriver.common.proxy import Proxy as Proxy
 
-    # Submodules
-    import selenium.webdriver.chrome as chrome
-    import selenium.webdriver.chromium as chromium
-    import selenium.webdriver.common as common
-    import selenium.webdriver.edge as edge
-    import selenium.webdriver.firefox as firefox
-    import selenium.webdriver.ie as ie
-    import selenium.webdriver.remote as remote
-    import selenium.webdriver.safari as safari
-    import selenium.webdriver.support as support
-    import selenium.webdriver.webkitgtk as webkitgtk
-    import selenium.webdriver.wpewebkit as wpewebkit
+# Submodules
+import selenium.webdriver.chrome as chrome
+import selenium.webdriver.chromium as chromium
+import selenium.webdriver.common as common
+import selenium.webdriver.edge as edge
+import selenium.webdriver.firefox as firefox
+import selenium.webdriver.ie as ie
+import selenium.webdriver.remote as remote
+import selenium.webdriver.safari as safari
+import selenium.webdriver.support as support
+import selenium.webdriver.webkitgtk as webkitgtk
+import selenium.webdriver.wpewebkit as wpewebkit
