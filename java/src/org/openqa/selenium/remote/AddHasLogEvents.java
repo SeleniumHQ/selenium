1c87c2ccd465abd8cc579edcd22c9323322a98ce
Andrei Solntsev <andrei.solntsev@gmail.com>
2026-03-09 19:26:38 +0200

    specify nullability in package `org.openqa.selenium.logging`
    Partially implements #14291

--- java/src/org/openqa/selenium/remote/AddHasLogEvents.java
+++ java/src/org/openqa/selenium/remote/AddHasLogEvents.java
@@ -22,6 +22,7 @@ import static org.openqa.selenium.remote.Browser.EDGE;
 import static org.openqa.selenium.remote.Browser.OPERA;
 
 import java.util.function.Predicate;
+import org.jspecify.annotations.NullMarked;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.devtools.HasDevTools;
@@ -47,6 +48,7 @@ public class AddHasLogEvents implements AugmenterProvider<HasLogEvents> {
   public HasLogEvents getImplementation(Capabilities capabilities, ExecuteMethod executeMethod) {
     return new HasLogEvents() {
       @Override
+      @NullMarked
       public <X> void onLogEvent(EventType<X> kind) {
         if (((RemoteExecuteMethod) executeMethod).getWrappedDriver() instanceof HasDevTools) {
           WebDriver driver = ((RemoteExecuteMethod) executeMethod).getWrappedDriver();
