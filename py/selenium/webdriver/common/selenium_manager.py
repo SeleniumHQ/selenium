SeleniumHQ/selenium

commit 5a7a2ac896d606dc8d315b2f77c20eb8dcbf3024

[py] allow bsd operating systems to use the linux binary
Log warning that it may not be supported
Throw exception if the OS is not supported
See: #13161

py/selenium/webdriver/common/selenium_manager.py
@@ -39,23 +39,31 @@ class SeleniumManager:
         """Determines the path of the correct Selenium Manager binary.
 
         :Returns: The Selenium Manager executable location
+        :Raises: WebDriverException if the platform is unsupported
         """
 
         if (path := os.getenv("SE_MANAGER_PATH")) is not None:
             return Path(path)
-        else:
-            platform = sys.platform
 
-            dirs = {
-                "darwin": "macos",
-                "win32": "windows",
-                "cygwin": "windows",
-            }
+        dirs = {
+            "darwin": "macos",
+            "win32": "windows",
+            "cygwin": "windows",
+            "linux": "linux",
+            "freebsd": "linux",
+            "openbsd": "linux"
+        }
 
-            directory = dirs.get(platform) if dirs.get(platform) else platform
-            file = "selenium-manager.exe" if directory == "windows" else "selenium-manager"
+        directory = dirs.get(sys.platform)
+        if directory is None:
+            raise WebDriverException(f"Unsupported platform: {sys.platform}")
 
-            path = Path(__file__).parent.joinpath(directory, file)
+        if sys.platform in ["freebsd", "openbsd"]:
+            logger.warning("Selenium Manager binary may not be compatible with %s; verify settings", sys.platform)
+
+        file = "selenium-manager.exe" if directory == "windows" else "selenium-manager"
+
+        path = Path(__file__).parent.joinpath(directory, file)
 
         if not path.is_file():
             raise WebDriverException(f"Unable to obtain working Selenium Manager binary; {path}")

commit c597ed90edb774810e1401df60f529713cc43d50

[py] fix selenium-manager binary selection on FreeBSD
Previously, sys.platform was checked to match "freebsd". However,
Py_GetPlatform on FreeBSD returns the version number along with it (e.g.
"freebsd15") [1]. So check sys.platform starts with "freebsd" instead.

Also, platform.machine() gives "amd64" on FreeBSD, not "x86_64", so fix
that.

Do the same with OpenBSD, and only allow Windows binary to be selected
if arch is correct ("AMD64" on Windows).

Update warning about Selenium Manager binary on FreeBSD to hint at user
what they need to do to get the Linux binary working on FreeBSD.

Fixes: 5a7a2ac896d6 ("[py] allow bsd operating systems to use the linux binary")

[1] https://python.readthedocs.io/fr/stable/c-api/init.html#c.Py_GetPlatform

py/selenium/webdriver/common/selenium_manager.py
@@ -87,21 +87,24 @@ class SeleniumManager:
         elif compiled_path.is_file():
             path = compiled_path
         else:
-            allowed = {
-                ("darwin", "any"): "macos/selenium-manager",
-                ("win32", "any"): "windows/selenium-manager.exe",
-                ("cygwin", "any"): "windows/selenium-manager.exe",
-                ("linux", "x86_64"): "linux/selenium-manager",
-                ("freebsd", "x86_64"): "linux/selenium-manager",
-                ("openbsd", "x86_64"): "linux/selenium-manager",
-            }
+            arch = platform.machine()
 
-            arch = platform.machine() if sys.platform in ("linux", "freebsd", "openbsd") else "any"
-            if sys.platform in ["freebsd", "openbsd"]:
-                logger.warning(f"Selenium Manager binary may not be compatible with {sys.platform}; verify settings")
-
-            location = allowed.get((sys.platform, arch))
-            if location is None:
+            if sys.platform == "darwin":
+                location = "macos/selenium-manager"
+            elif sys.platform in ("win32", "cygwin") and arch == "AMD64":
+                location = "windows/selenium-manager.exe"
+            elif sys.platform == "linux" and arch == "x86_64":
+                location = "linux/selenium-manager"
+            elif sys.platform.startswith("freebsd") and arch == "amd64":
+                logger.warning(
+                    "Selenium Manager binary may not be compatible with FreeBSD; you may need to run "
+                    "'brandelf -t linux' on it and load linux64.ko"
+                )
+                location = "linux/selenium-manager"
+            elif sys.platform.startswith("openbsd") and arch == "amd64":
+                logger.warning("Selenium Manager binary may not be compatible with OpenBSD")
+                location = "linux/selenium-manager"
+            else:
                 raise WebDriverException(f"Unsupported platform/architecture combination: {sys.platform}/{arch}")
 
             path = Path(__file__).parent.joinpath(location)