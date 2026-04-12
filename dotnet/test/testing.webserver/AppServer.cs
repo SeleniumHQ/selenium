93e072d121bcf7e68eccc8c67632743e495a8bff
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-04-12 13:37:15 +0300

    Update AppServer.cs

--- dotnet/test/testing.webserver/AppServer.cs
+++ dotnet/test/testing.webserver/AppServer.cs
@@ -29,7 +29,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using OpenQA.Selenium.Testing.WebServer.Handlers;
