8352b7838a32eab22af7b43a722aa5bd1e05f747
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2026-04-23 22:35:28 +0300

    [dotnet] [bidi] Obsolete Type(string) method in Input module

--- dotnet/src/webdriver/BiDi/Input/SourceActions.cs
+++ dotnet/src/webdriver/BiDi/Input/SourceActions.cs
@@ -43,7 +43,7 @@ public interface IKeySourceAction : ISourceAction;
 public sealed record KeySourceActions(string Id, IEnumerable<IKeySourceAction> Actions)
     : SourceActions<IKeySourceAction>(Id, Actions)
 {
-    // TODO move out as extension method
+    [Obsolete("This helper method will be removed in v4.45. Use KeyDown and KeyUp actions directly instead.")]
     public KeySourceActions Type(string text) => this with
     {
         Actions = [.. Actions, .. text.SelectMany<char, IKeySourceAction>(c => [new KeyDownAction(c), new KeyUpAction(c)])]
