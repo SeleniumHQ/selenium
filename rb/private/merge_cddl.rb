399b820aa68a596a9c72bc26d5cf489d57ecfb86
pallavigotwork <write2pallavi@gmail.com>
2026-05-20 13:44:44 +0530

    run go format

--- rb/private/merge_cddl.rb
+++ rb/private/merge_cddl.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Licensed to the Software Freedom Conservancy (SFC) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,6 +16,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 
 #!/usr/bin/env ruby
 # frozen_string_literal: true
