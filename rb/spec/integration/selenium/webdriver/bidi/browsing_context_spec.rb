213e48aab9cf169d2861ac236a961eb17a7ef6ed
Titus Fortner <titus.fortner@gmail.com>
2026-05-30 09:06:19 -0500

    [rb] setting device_pixel_ratio is not supposed to change window.devicePixelRatio value

--- rb/spec/integration/selenium/webdriver/bidi/browsing_context_spec.rb
+++ rb/spec/integration/selenium/webdriver/bidi/browsing_context_spec.rb
@@ -78,7 +78,6 @@ module Selenium
           browsing_context = described_class.new(bridge)
           browsing_context.set_viewport(width: 800, height: 600, device_pixel_ratio: 2.0)
           expect(driver.execute_script('return [window.innerWidth, window.innerHeight]')).to eq([800, 600])
-          expect(driver.execute_script('return window.devicePixelRatio')).to eq(2.0)
         end
 
         it 'accepts users prompts without text',
