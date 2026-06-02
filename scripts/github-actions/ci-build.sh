a5152cfbb4ef4b1746b70956ab502a531b63e708
Titus Fortner <titus.fortner@gmail.com>
2026-06-02 12:24:40 -0500

    keep the file but remove all the entries

--- scripts/github-actions/ci-build.sh
+++ scripts/github-actions/ci-build.sh
@@ -14,10 +14,11 @@ fi
 
 # Now run the tests. The engflow build uses pinned browsers
 # so this should be fine
+# shellcheck disable=SC2046
 bazel test --config=rbe-ci --build_tests_only \
   --keep_going --flaky_test_attempts=2 \
   --cache_test_results=${CACHE_RESULTS} \
-  //...
+  //... -- $(cat .skipped-tests | tr '\n' ' ')
 
 # Build the packages we want to ship to users
 bazel build --config=rbe-ci --build_tag_filters=release-artifact //...
