9049a8fe62853f4d88c5a5c717f7215c3ff66c5a
Titus Fortner <titus.fortner@gmail.com>
2026-06-26 10:11:56 -0500

    [build] add copyright headers to rbs, pyi, ts, mjs, and erb files

--- py/selenium/webdriver/__init__.pyi
+++ py/selenium/webdriver/__init__.pyi
@@ -4,7 +4,7 @@
 # regarding copyright ownership.  The SFC licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
-# with the License.
+# with the License.  You may obtain a copy of the License at
 #
 #   http://www.apache.org/licenses/LICENSE-2.0
 #
