2dd69481eb997a4cdef4f4ce0d4ce6fb29832c71
Puja Jagani <puja.jagani93@gmail.com>
2026-07-07 11:19:35 +0530

    [java] Move BiDi creation to RemoteWebDriver. Add enable BiDi to SafariOptions.

--- java/src/org/openqa/selenium/remote/RemoteWebDriver.java
+++ java/src/org/openqa/selenium/remote/RemoteWebDriver.java
@@ -80,6 +80,7 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.bidi.BiDi;
+import org.openqa.selenium.bidi.BiDiException;
 import org.openqa.selenium.bidi.Connection;
 import org.openqa.selenium.bidi.HasBiDi;
 import org.openqa.selenium.devtools.DevTools;
@@ -434,38 +435,25 @@ public class RemoteWebDriver
 
   private Optional<BiDi> createBiDi() {
     Object rawUrl = this.capabilities.getCapability("webSocketUrl");
-    if (!(rawUrl instanceof String)) {
-      return Optional.empty();
+    if (!(rawUrl instanceof String)
+        || (!((String) rawUrl).startsWith("ws://") && !((String) rawUrl).startsWith("wss://"))) {
+      throw new BiDiException(
+          "Check if this browser version supports BiDi and if the"
+              + " 'webSocketUrl: true' capability is set.");
     }
-    String webSocketUrl = ((String) rawUrl).trim();
-    URI wsUri;
+    String webSocketUrl = (String) rawUrl;
     try {
-      wsUri = new URI(webSocketUrl);
-    } catch (URISyntaxException e) {
-      LOG.log(
-          Level.WARNING,
-          "BiDi was requested but the remote end returned an invalid webSocketUrl.",
-          e);
-      return Optional.empty();
-    }
-    String scheme = wsUri.getScheme();
-    if (scheme == null || (!scheme.equalsIgnoreCase("ws") && !scheme.equalsIgnoreCase("wss"))) {
-      LOG.warning("BiDi was requested but the remote end did not return a valid webSocketUrl.");
-      return Optional.empty();
-    }
-    HttpClient.Factory clientFactory = HttpClient.Factory.createDefault();
-    ClientConfig wsConfig = this.clientConfig.baseUri(wsUri);
-    HttpClient wsClient = clientFactory.createClient(wsConfig);
-    try {
+      URI wsUri = new URI(webSocketUrl);
+      HttpClient.Factory clientFactory = HttpClient.Factory.createDefault();
+      ClientConfig wsConfig = this.clientConfig.baseUri(wsUri);
+      HttpClient wsClient = clientFactory.createClient(wsConfig);
       Connection biDiConnection = new Connection(wsClient, wsUri.toString());
       return Optional.of(new BiDi(biDiConnection, wsConfig.wsTimeout()));
-    } catch (RuntimeException e) {
-      wsClient.close();
-      LOG.log(
-          Level.WARNING,
-          "BiDi was requested but the WebSocket connection could not be established.",
+    } catch (URISyntaxException e) {
+      throw new BiDiException(
+          "Check if this browser version supports BiDi and if the"
+              + " 'webSocketUrl: true' capability is set.",
           e);
-      return Optional.empty();
     }
   }
 
